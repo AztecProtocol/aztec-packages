d893c72408ee364a715bcf95faf7d5fc8ef3403e
Adam Domurad <adam@aztecprotocol.com>
2023-03-07 09:57:12 -0500

    feat(json-rpc): make client a proxy object

--- yarn-project/json-rpc/src/client/json_rpc_client.ts
+++ yarn-project/json-rpc/src/client/json_rpc_client.ts
@@ -8,75 +8,68 @@ import { RemoteObject } from 'comlink';
 import { ClassConverter, ClassConverterInput } from '../class_converter.js';
 import { convertFromJsonObj, convertToJsonObj } from '../convert.js';
 
-export class JsonRpcClient<T extends object> {
-  private classConverter: ClassConverter;
-  public readonly rpc: RemoteObject<T>;
-  private id = 0;
+export async function defaultFetch(host: string, method: string, body: any) {
+  logTrace(`JsonRpcClient.fetch`, host, method, '<-', body);
+  const resp = await fetch(`${host}/${method}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { 'content-type': 'application/json' },
+  });
 
-  constructor(private host: string, classMap: ClassConverterInput, private netMustSucceed = false) {
-    // the rpc object has syntax sugar over 'request'
-    // for accessing the remote methods
-    this.rpc = this.createRpcProxy();
-    this.classConverter = new ClassConverter(classMap);
+  if (!resp.ok) {
+    throw new Error(resp.statusText);
   }
-  /**
-   * Creates a Proxy object that delegates over RPC
-   * and satisfies RemoteObject<T>
-   */
-  private createRpcProxy(): RemoteObject<T> {
-    return new Proxy(
-      {},
-      {
-        get:
-          (_, method: string) =>
-          (...params: any[]) => {
-            logTrace(`JsonRpcClient.constructor`, 'proxy', method, '<-', params);
-            return this.request(method, params);
-          },
-      },
-    ) as RemoteObject<T>;
+
+  const text = await resp.text();
+  try {
+    return JSON.parse(text);
+  } catch (err) {
+    throw new Error(`Failed to parse body as JSON: ${text}`);
   }
-  public async request(method: string, params: any[]): Promise<any> {
+}
+
+export async function mustSucceedFetch(host: string, method: string, body: any) {
+  return await retry(() => defaultFetch(host, method, body), 'JsonRpcClient request');
+}
+/**
+ * Creates a Proxy object that delegates over RPC
+ * and satisfies RemoteObject<T>
+ * The server should have ran new JsonRpcServer()
+ */
+export function createJsonRpcClient<T extends object>(
+  host: string,
+  classMap: ClassConverterInput,
+  fetch = defaultFetch,
+) {
+  const classConverter = new ClassConverter(classMap);
+  let id = 0;
+  const request = async (method: string, params: any[]): Promise<any> => {
     const body = {
       jsonrpc: '2.0',
-      id: this.id++,
+      id: id++,
       method,
-      params: params.map(param => convertToJsonObj(this.classConverter, param)),
+      params: params.map(param => convertToJsonObj(classConverter, param)),
     };
     logTrace(`JsonRpcClient.request`, method, '<-', params);
-    const res = await this.fetch(method, body);
+    const res = await fetch(host, method, body);
     logTrace(`JsonRpcClient.request`, method, '->', res);
     if (res.error) {
       throw res.error;
     }
-    return convertFromJsonObj(this.classConverter, res.result);
-  }
+    return convertFromJsonObj(classConverter, res.result);
+  };
 
-  private async fetch(method: string, body: any) {
-    const tryOnce = async () => {
-      logTrace(`JsonRpcClient.fetch`, this.host, method, '<-', body);
-      const resp = await fetch(`${this.host}/${method}`, {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: { 'content-type': 'application/json' },
-      });
-
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
-
-      const text = await resp.text();
-      try {
-        return JSON.parse(text);
-      } catch (err) {
-        throw new Error(`Failed to parse body as JSON: ${text}`);
-      }
-    };
-
-    if (this.netMustSucceed) {
-      return await retry(tryOnce, 'JsonRpcClient request');
-    }
-
-    return await tryOnce();
-  }
+  // Intercept any RPC methods with a proxy
+  // This wraps 'request' with a method-call syntax wrapper
+  return new Proxy(
+    {},
+    {
+      get:
+        (_, method: string) =>
+        (...params: any[]) => {
+          logTrace(`JsonRpcClient.constructor`, 'proxy', method, '<-', params);
+          return request(method, params);
+        },
+    },
+  ) as RemoteObject<T>;
 }
