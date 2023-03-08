2ce30aa6773e2ce18caee89f6cb15164632e0d76
Adam Domurad <adam@aztecprotocol.com>
2023-03-07 19:18:25 -0500

    doc(wasm-worker): doc fixes

--- yarn-project/log/src/index.ts
+++ yarn-project/log/src/index.ts
@@ -1,5 +1,12 @@
+/**
+ * Type for a logger.
+ */
 export type DebugLogger = (...args: any[]) => void;
+/**
+ * Creates a logger.
+ * TODO port aztec2 logger over
+ * @param moduleName - name of module for logging or filtering purposes
+ */
 export function createDebugLogger(moduleName: string): DebugLogger {
-  // TODO port aztec2 logger over
   return (...args: any[]) => console.log(moduleName, ...args);
 }
--- yarn-project/wasm-worker/src/worker/data_store.ts
+++ yarn-project/wasm-worker/src/worker/data_store.ts
@@ -1,3 +1,6 @@
+/**
+ * Simple read/write interface for wasm modules
+ */
 export interface DataStore {
   get(key: string): Promise<Buffer | undefined>;
   set(key: string, value: Buffer): Promise<void>;
