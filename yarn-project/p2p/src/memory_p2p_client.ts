2b8cc9bcbbc8cde57998ebe0fb2f44a49e733cb7
spypsy <spypsy@outlook.com>
2023-03-10 12:10:54 +0000

    WIP adding basic properties to p2p client

--- yarn-project/p2p/src/memory_p2p_client.ts
+++ yarn-project/p2p/src/memory_p2p_client.ts
@@ -2,18 +2,68 @@ import { P2P } from './p2p_client.js';
 import { Tx } from './tx.js';
 
 /**
+ * Interface defining data contained in a rollup object.
+ */
+interface Rollup {
+  /**
+   * The ID of the rollup (block height in L1 terminology).
+   */
+  id: number;
+
+  /**
+   * Timestamp of an L1 block in which the settlement tx containing this rollup was included.
+   */
+  settlementTimestamp: number;
+}
+
+/**
+ * Interface of classes allowing for the retrieval of all the relevant rollup information.
+ */
+interface RollupSource {
+  /**
+   * Gets the ID of the last rollup.
+   * @returns The ID of the last rollup.
+   **/
+  getLastRollupId(): number;
+
+  /**
+   * Gets the `take` rollups starting from ID `from`.
+   * @param from - If of the first rollup to return (inclusive).
+   * @param take - The number of rollups to return.
+   * @returns The requested rollups.
+   */
+  getRollups(from: number, take: number): Rollup[];
+}
+
+/**
  * An in-memory implementation of the P2P client.
  */
 export class MemoryP2PCLient implements P2P {
   /**
-   * Property that gets updated when the client starts/stops running.
+   * Property that indicates whether the client is running.
    */
   private running = false;
 
   /**
-   * In-memory P2P client constructor.
+   * Property that indicates whether the client is currently syncing with a block source.
    */
-  constructor() {}
+  private syncing = false;
+
+  /**
+   * Property that indicates whether the client is ready to receive new txs.
+   */
+  private ready = false;
+
+  /**
+   * The JS promise that will be running during initial sync. Can be interrupted if the client is stopped.
+   */
+  private runningSyncPromise!: Promise<void>;
+
+  /**
+   * In-memory P2P client constructor.
+   * @param rollupSource - P2P client's source for fetching existing rollup data.
+   */
+  constructor(private rollupSource: RollupSource) {}
 
   /**
    * Starts the P2P client.
