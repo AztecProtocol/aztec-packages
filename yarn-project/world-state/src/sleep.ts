9dcae599d145da6383323a3b746b8e040d0295a1
benesjan <janbenes1234@gmail.com>
2023-03-16 10:43:19 -0600

    docs: fixed docs in InterruptableSleep

--- yarn-project/world-state/src/sleep.ts
+++ yarn-project/world-state/src/sleep.ts
@@ -1,8 +1,15 @@
+/**
+ * A class which allows you to sleep for a given number of milliseconds, and interrupt that sleep when necessary.
+ */
 export class InterruptableSleep {
   private interruptResolve: (shouldThrow: boolean) => void = () => {};
   private interruptPromise = new Promise<boolean>(resolve => (this.interruptResolve = resolve));
   private timeouts: NodeJS.Timeout[] = [];
 
+  /**
+   * If awaited, will sleep for the given number of milliseconds or until interrupted.
+   * @param ms - The number of milliseconds to sleep for.
+   */
   public async sleep(ms: number) {
     let timeout!: NodeJS.Timeout;
     const promise = new Promise<boolean>(resolve => (timeout = setTimeout(() => resolve(false), ms)));
@@ -15,12 +22,22 @@ export class InterruptableSleep {
     }
   }
 
+  /**
+   * Interrupts the sleep function.
+   * @param sleepShouldThrow - If true, the sleep function will throw an error if the `interruptPromise` is resolved
+   * first.
+   */
   public interrupt(sleepShouldThrow = false) {
     this.interruptResolve(sleepShouldThrow);
     this.interruptPromise = new Promise(resolve => (this.interruptResolve = resolve));
   }
 }
 
+/**
+ * Sleeps for the given number of milliseconds.
+ * @param ms - The number of milliseconds to sleep for.
+ * @returns A promise that resolves after the given number of milliseconds.
+ */
 export function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
