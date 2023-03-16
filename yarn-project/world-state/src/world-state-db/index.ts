06b2d9e1514179aa55a1355e905a9288322888be
benesjan <janbenes1234@gmail.com>
2023-03-16 10:55:14 -0600

    docs: fixed WorldStatedB docs

--- yarn-project/world-state/src/world-state-db/index.ts
+++ yarn-project/world-state/src/world-state-db/index.ts
@@ -1,16 +1,38 @@
-import { HashPath } from '@aztec/merkle-tree';
-
+/**
+ * Defines what kind of tree we are dealing with.
+ */
 export enum WorldStateTreeId {
   CONTRACT_TREE = 0,
 }
 
+/**
+ * Defines tree information.
+ */
 export interface TreeInfo {
+  /**
+   * The tree ID.
+   */
   treeId: WorldStateTreeId;
+  /**
+   * The tree root.
+   */
   root: Buffer;
+  /**
+   * The number of leaves.
+   */
   size: number;
 }
 
+/**
+ * Defines a batch update.
+ */
 export interface BatchUpdate {
+  /**
+   * The ID of a tree to be updated.
+   */
   treeId: WorldStateTreeId;
+  /**
+   * The leaves to be updated.
+   */
   elements: Buffer[];
 }
