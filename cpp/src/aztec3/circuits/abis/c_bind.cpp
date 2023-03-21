51985c2c30754319b978be141f99cfa52d8a2560
David Banks <47112877+dbanks12@users.noreply.github.com>
2023-03-21 08:23:09 -0400

    Cbind for `compute_function_tree_root` (#65)
    * cbind for compute_function_tree_root
    
    * fix func tree root test for wasm
    
    * compute_tree_root_native fix in bberg
    
    * use NT random_element for random fields instead of engine get_random_256()
    
    * template with compute_root_of_partial_left_tree logic which will be used by multiple tree root cbinds
    
    * unused include in constants, bad import in cbind
    
    * update bb/aztec3
    
    ---------
    
    Co-authored-by: Suyash Bagad <suyashnbagad1997@gmail.com>

--- cpp/src/aztec3/circuits/abis/c_bind.cpp
+++ cpp/src/aztec3/circuits/abis/c_bind.cpp
@@ -5,18 +5,63 @@
 #include <aztec3/constants.hpp>
 
 #include <aztec3/utils/types/native_types.hpp>
+#include <barretenberg/stdlib/merkle_tree/membership.hpp>
 #include <barretenberg/crypto/keccak/keccak.hpp>
 #include <barretenberg/common/serialize.hpp>
 
 namespace {
-using aztec3::GeneratorIndex;
+
 using aztec3::circuits::abis::FunctionLeafPreimage;
 using aztec3::circuits::abis::TxRequest;
 using NT = aztec3::utils::types::NativeTypes;
+
+// Cbind helper functions
+/**
+ * @brief Compute an imperfect merkle tree's root from leaves.
+ *
+ * @details given a `uint8_t const*` buffer representing a merkle tree's leaves,
+ * compute the corresponding tree's root and return the serialized results
+ * in the `output` buffer. "Partial left tree" here means that the tree's leaves
+ * are filled strictly from left to right, but there may be empty leaves on the right
+ * end of the tree.
+ *
+ * @tparam TREE_HEIGHT height of the tree used to determine max leaves and used when computing root
+ * @tparam LeafPreimage the preimage type with a `.hash()` function to generate empty/zero-leaves
+ * @param leaves_buf a buffer of bytes representing the leaves of the tree, where each leaf is
+ * assumed to be a field and is interpreted using `NT::fr::serialize_from_buffer(leaf_ptr)`
+ * @param num_leaves the number of leaves in leaves_buf
+ * @returns a field (`NT::fr`) containing the computed merkle tree root
+ */
+template <size_t TREE_HEIGHT, typename LeafPreimage>
+NT::fr compute_root_of_partial_left_tree(uint8_t const* leaves_buf, uint8_t num_leaves)
+{
+    const size_t max_leaves = 2 << (TREE_HEIGHT - 1);
+    // cant exceed max leaves
+    ASSERT(num_leaves <= max_leaves);
+
+    // initialize the vector of leaves to a complete-tree-sized vector of zero-leaves
+    NT::fr zero_leaf = LeafPreimage().hash(); // hash of empty/0 preimage
+    std::vector<NT::fr> leaves(max_leaves, zero_leaf);
+
+    // Iterate over the input buffer, extracting each leaf and serializing it from buffer to field
+    // Insert each leaf field into the vector
+    // If num_leaves < perfect tree, remaining leaves will be `zero_leaf`
+    for (size_t l = 0; l < num_leaves; l++) {
+        // each iteration skips to over some number of `fr`s to get to the // next leaf
+        uint8_t const* cur_leaf_ptr = leaves_buf + sizeof(NT::fr) * l;
+        NT::fr leaf = NT::fr::serialize_from_buffer(cur_leaf_ptr);
+        leaves[l] = leaf;
+    }
+
+    // compute the root of this complete tree, return
+    return plonk::stdlib::merkle_tree::compute_tree_root_native(leaves);
+}
+
 } // namespace
 
 #define WASM_EXPORT __attribute__((visibility("default")))
 
+// WASM Cbinds
 extern "C" {
 
 /**
@@ -84,4 +129,31 @@ WASM_EXPORT void abis__compute_function_leaf(uint8_t const* function_leaf_preima
     leaf_preimage.hash();
     NT::fr::serialize_to_buffer(leaf_preimage.hash(), output);
 }
+
+/**
+ * @brief Compute a function tree root from its leaves.
+ * This is a WASM-export that can be called from Typescript.
+ *
+ * @details given a `uint8_t const*` buffer representing a function tree's leaves,
+ * compute the corresponding tree's root and return the serialized results
+ * in the `output` buffer.
+ *
+ * @param function_leaves_buf a buffer of bytes representing the function leaves of the
+ * tree for which the root is being computed
+ * @param function_leaves_buf a buffer of bytes representing the leaves of the function tree,
+ * where each leaf is assumed to be a serialized field
+ * @param num_leaves the number of leaves in leaves_buf
+ * @param output buffer that will contain the output. The serialized function tree root.
+ */
+WASM_EXPORT void abis__compute_function_tree_root(uint8_t const* function_leaves_buf,
+                                                  uint8_t num_leaves,
+                                                  uint8_t* output)
+{
+    NT::fr root = compute_root_of_partial_left_tree<aztec3::FUNCTION_TREE_HEIGHT, FunctionLeafPreimage<NT>>(
+        function_leaves_buf, num_leaves);
+
+    // serialize and return root
+    NT::fr::serialize_to_buffer(root, output);
+}
+
 } // extern "C"
