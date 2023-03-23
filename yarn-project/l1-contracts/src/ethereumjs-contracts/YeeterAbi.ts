68d9267f4d959644e97e815ae5c684951145cace
Santiago Palladino <spalladino@gmail.com>
2023-03-23 14:08:27 -0300

    Format

--- yarn-project/l1-contracts/src/ethereumjs-contracts/YeeterAbi.ts
+++ yarn-project/l1-contracts/src/ethereumjs-contracts/YeeterAbi.ts
@@ -1,94 +1,94 @@
 import { ContractAbi } from '@aztec/ethereum.js/contract';
 export default new ContractAbi([
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": true,
-        "internalType": "bytes32",
-        "name": "aztecAddress",
-        "type": "bytes32"
+        indexed: true,
+        internalType: 'bytes32',
+        name: 'aztecAddress',
+        type: 'bytes32',
       },
       {
-        "indexed": true,
-        "internalType": "address",
-        "name": "portalAddress",
-        "type": "address"
+        indexed: true,
+        internalType: 'address',
+        name: 'portalAddress',
+        type: 'address',
       },
       {
-        "indexed": false,
-        "internalType": "bytes",
-        "name": "acir",
-        "type": "bytes"
-      }
+        indexed: false,
+        internalType: 'bytes',
+        name: 'acir',
+        type: 'bytes',
+      },
     ],
-    "name": "ContractDeploymentYeet",
-    "type": "event"
+    name: 'ContractDeploymentYeet',
+    type: 'event',
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "l2blockNum",
-        "type": "uint256"
+        indexed: true,
+        internalType: 'uint256',
+        name: 'l2blockNum',
+        type: 'uint256',
       },
       {
-        "indexed": true,
-        "internalType": "address",
-        "name": "sender",
-        "type": "address"
+        indexed: true,
+        internalType: 'address',
+        name: 'sender',
+        type: 'address',
       },
       {
-        "indexed": false,
-        "internalType": "bytes",
-        "name": "blabber",
-        "type": "bytes"
-      }
+        indexed: false,
+        internalType: 'bytes',
+        name: 'blabber',
+        type: 'bytes',
+      },
     ],
-    "name": "Yeet",
-    "type": "event"
+    name: 'Yeet',
+    type: 'event',
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "_l2blockNum",
-        "type": "uint256"
+        internalType: 'uint256',
+        name: '_l2blockNum',
+        type: 'uint256',
       },
       {
-        "internalType": "bytes",
-        "name": "_blabber",
-        "type": "bytes"
-      }
+        internalType: 'bytes',
+        name: '_blabber',
+        type: 'bytes',
+      },
     ],
-    "name": "yeet",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: 'yeet',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "bytes32",
-        "name": "_aztecAddress",
-        "type": "bytes32"
+        internalType: 'bytes32',
+        name: '_aztecAddress',
+        type: 'bytes32',
       },
       {
-        "internalType": "address",
-        "name": "_portalAddress",
-        "type": "address"
+        internalType: 'address',
+        name: '_portalAddress',
+        type: 'address',
       },
       {
-        "internalType": "bytes",
-        "name": "_acir",
-        "type": "bytes"
-      }
+        internalType: 'bytes',
+        name: '_acir',
+        type: 'bytes',
+      },
     ],
-    "name": "yeetContractDeployment",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
+    name: 'yeetContractDeployment',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
 ]);
