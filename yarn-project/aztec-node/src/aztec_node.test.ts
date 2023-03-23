9dfc56e870f3487633a08e64b763fdc7ab7b902e
PhilWindle <philip.windle@gmail.com>
2023-03-23 16:21:42 +0000

    WIP

--- yarn-project/aztec-node/src/aztec_node.test.ts
+++ yarn-project/aztec-node/src/aztec_node.test.ts
@@ -1,15 +1,44 @@
 import { EthAddress } from '@aztec/ethereum.js/eth_address';
 import { AccumulatedTxData, Tx } from '@aztec/p2p';
 import { AztecNode } from './index.js';
+import { WalletProvider } from '@aztec/ethereum.js/provider';
+import { Rollup, Yeeter } from '@aztec/l1-contracts';
 
 const ETHEREUM_HOST = 'http://localhost:8545/';
-const ROLLUP_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-const YEETER_ADDRESS = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+
+const deployRollupContract = async (provider: WalletProvider) => {
+  // Deploy.
+  const deployAccount = provider.getAccount(0);
+  const contract = new Rollup(provider, undefined, { from: deployAccount, gas: 1000000 });
+  await contract.deploy().send().getReceipt();
+  return contract.address;
+};
+
+const deployYeeterContract = async (provider: WalletProvider) => {
+  // Deploy.
+  const deployAccount = provider.getAccount(0);
+  const contract = new Yeeter(provider, undefined, { from: deployAccount, gas: 1000000 });
+  await contract.deploy().send().getReceipt();
+  return contract.address;
+};
+
+const createProvider = () => {
+  const walletProvider = WalletProvider.fromHost(ETHEREUM_HOST);
+  walletProvider.addAccountsFromMnemonic('test test test test test test test test test test test junk', 1);
+  return walletProvider;
+};
 
 describe('AztecNode', () => {
+  let rollupAddress: EthAddress | undefined = undefined;
+  let yeeterAddress: EthAddress | undefined = undefined;
+  beforeAll(async () => {
+    const provider = createProvider();
+    rollupAddress = await deployRollupContract(provider);
+    yeeterAddress = await deployYeeterContract(provider);
+  });
   it('should start and stop all services', async () => {
     const node = new AztecNode();
-    await node.init(ETHEREUM_HOST, EthAddress.fromString(ROLLUP_ADDRESS), EthAddress.fromString(YEETER_ADDRESS));
+    await node.init(ETHEREUM_HOST, rollupAddress!, yeeterAddress!);
     const isReady = await node.isReady();
     expect(isReady).toBeTruthy();
     await node.stop();
@@ -17,7 +46,7 @@ describe('AztecNode', () => {
 
   it('should accept a transaction', async () => {
     const node = new AztecNode();
-    await node.init(ETHEREUM_HOST, EthAddress.fromString(ROLLUP_ADDRESS), EthAddress.fromString(YEETER_ADDRESS));
+    await node.init(ETHEREUM_HOST, rollupAddress!, yeeterAddress!);
     const isReady = await node.isReady();
     expect(isReady).toBeTruthy();
     const tx: Tx = new Tx(AccumulatedTxData.random());
