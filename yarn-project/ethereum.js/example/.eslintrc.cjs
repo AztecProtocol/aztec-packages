0d641de5fac5dbf14e6eca0013067139df9c58ea
Adam Domurad <adam@aztecprotocol.com>
2023-03-23 11:30:12 -0400

    fix: undo bundled changes

--- yarn-project/ethereum.js/example/.eslintrc.cjs
+++ yarn-project/ethereum.js/example/.eslintrc.cjs
@@ -1,1 +1,6 @@
-module.exports = require('@aztec/foundation/eslint');
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  extends: ['@aztec/eslint-config'],
+  parserOptions: { tsconfigRootDir: __dirname },
+};
