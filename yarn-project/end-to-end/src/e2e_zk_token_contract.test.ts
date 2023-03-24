5fb38663fd3d574e81bd24b1d74d79e22fc50a2f
Leila Wang <leizciw@gmail.com>
2023-03-24 10:31:56 +0000

    Add e2e test.

--- yarn-project/end-to-end/src/e2e_zk_token_contract.test.ts
+++ yarn-project/end-to-end/src/e2e_zk_token_contract.test.ts
@@ -4,22 +4,35 @@ import { createTestAztecRPCClient } from './create_aztec_rpc_client.js';
 
 describe('e2e_zk_token', () => {
   let arc: AztecRPCClient;
-  let account: AztecAddress;
+  let accounts: AztecAddress[];
+  let contract: Contract;
+  const initialBalance = 987n;
+  const mintAmount = 65n;
+
+  const expectBalance = async (accountIdx: number, expectedBalance: bigint) => {
+    const balance = await contract.methods.getBalance().call({ from: accounts[accountIdx] });
+    console.log(`Account ${accountIdx} balance: ${balance}`);
+    expect(balance).toBe(expectedBalance);
+  };
 
   beforeAll(async () => {
-    arc = await createTestAztecRPCClient();
-    [account] = await arc.getAccounts();
-  });
-
-  it('should deploy zk token contract with initial token minted to the account', async () => {
-    const initialBalance = 987n;
+    arc = await createTestAztecRPCClient(2);
+    accounts = await arc.getAccounts();
 
     const deployer = new ContractDeployer(abi, arc);
     const receipt = await deployer.deploy(initialBalance).send().getReceipt();
     const contractAddress = receipt.contractAddress!;
-    const contract = new Contract(contractAddress, abi, arc);
+    contract = new Contract(contractAddress, abi, arc);
+  });
 
-    const balance = await contract.methods.getBalance(account);
-    expect(balance).toBe(initialBalance);
+  it('should call mint and increase account balance', async () => {
+    await expectBalance(0, initialBalance);
+    await expectBalance(1, 0n);
+
+    const receipt = await contract.methods.mint(mintAmount).send({ from: accounts[1] }).getReceipt();
+    expect(receipt.status).toBe(true);
+
+    await expectBalance(0, initialBalance);
+    await expectBalance(1, mintAmount);
   });
 });
