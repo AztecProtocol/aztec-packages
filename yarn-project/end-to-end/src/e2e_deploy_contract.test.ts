5a23e3fefa4c42ed584a0854211fa6a4403e1a54
Leila Wang <leizciw@gmail.com>
2023-03-26 08:39:20 +0000

    Revert test.

--- yarn-project/end-to-end/src/e2e_deploy_contract.test.ts
+++ yarn-project/end-to-end/src/e2e_deploy_contract.test.ts
@@ -1,31 +1,7 @@
-import { AztecAddress, AztecRPCClient, ContractDeployer } from '@aztec/aztec.js';
-import abi from '@aztec/noir-contracts/examples/test_contract.json';
-import { createTestAztecRPCClient } from './create_aztec_rpc_client.js';
+import { Aztec } from '@aztec/aztec.js';
 
 describe('e2e_deploy_contract', () => {
-  let arc: AztecRPCClient;
-  let accounts: AztecAddress[];
-
-  beforeAll(async () => {
-    arc = await createTestAztecRPCClient(1);
-    accounts = await arc.getAccounts();
-  });
-
-  it('should deploy a contract', async () => {
-    const deployer = new ContractDeployer(abi, arc);
-    const receipt = await deployer.deploy().send().getReceipt();
-    expect(receipt).toEqual(
-      expect.objectContaining({
-        from: accounts[0],
-        to: undefined,
-        status: true,
-        error: '',
-      }),
-    );
-
-    const contractAddress = receipt.contractAddress!;
-    const constructor = abi.functions.find(f => f.name === 'constructor')!;
-    const bytecode = await arc.getCode(contractAddress);
-    expect(bytecode).toEqual(constructor.bytecode);
+  it('should deploy a contract', () => {
+    expect(() => new Aztec()).not.toThrow();
   });
 });
