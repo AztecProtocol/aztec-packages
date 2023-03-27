c99043ebf6e37fee3ed4322ea321a0d9a6b3aee9
Leila Wang <leizciw@gmail.com>
2023-03-27 17:25:12 +0000

    Add l2-block.

---
+++ yarn-project/l2-block/src/index.ts
@@ -0,0 +1,4 @@
+export * from './contract_data.js';
+export * from './l2_block.js';
+export * from './l2_block_calldata.js';
+export * from './l2_block_source.js';
---
+++ yarn-project/l2-block/src/l2_block_source.ts
@@ -0,0 +1,32 @@
+import { L2Block } from './l2_block.js';
+
+/**
+ * Interface of classes allowing for the retrieval of L2 blocks.
+ */
+export interface L2BlockSource {
+  /**
+   * Gets the number of the latest L2 block processed by the block source implementation.
+   * @returns The number of the latest L2 block processed by the block source implementation.
+   */
+  getLatestBlockNum(): Promise<number>;
+
+  /**
+   * Gets the `take` amount of L2 blocks starting from `from`.
+   * @param from - If of the first rollup to return (inclusive).
+   * @param take - The number of blocks to return.
+   * @returns The requested L2 blocks.
+   */
+  getL2Blocks(from: number, take: number): Promise<L2Block[]>;
+
+  /**
+   * Starts the L2 block source.
+   * @returns A promise signalling completion of the start process.
+   */
+  start(): Promise<void>;
+
+  /**
+   * Stops the L2 block source.
+   * @returns A promise signalling completion of the stop process.
+   */
+  stop(): Promise<void>;
+}
