d5bba36df1308a7d75fcf1928740dec2d0624689
spalladino <santiago@aztecprotocol.com>
2023-03-30 10:32:49 +0000

    Empty prover injects agg objects to public inputs

--- yarn-project/sequencer-client/src/prover/empty.ts
+++ yarn-project/sequencer-client/src/prover/empty.ts
@@ -7,20 +7,25 @@ import {
   RootRollupPublicInputs,
   UInt8Vector,
 } from '@aztec/circuits.js';
+import { makeEmptyAggregationObject } from '../deps/aggregation_object.js';
 import { Prover } from './index.js';
 
 /* eslint-disable */
 
 const EMPTY_PROOF_SIZE = 42;
 
+// TODO: Silently modifying one of the inputs is horrible. Rethink these interfaces.
 export class EmptyProver implements Prover {
   async getBaseRollupProof(input: BaseRollupInputs, publicInputs: BaseRollupPublicInputs): Promise<UInt8Vector> {
+    publicInputs.endAggregationObject = makeEmptyAggregationObject();
     return new UInt8Vector(Buffer.alloc(EMPTY_PROOF_SIZE, 0));
   }
   async getMergeRollupProof(input: MergeRollupInputs, publicInputs: MergeRollupPublicInputs): Promise<UInt8Vector> {
+    publicInputs.endAggregationObject = makeEmptyAggregationObject();
     return new UInt8Vector(Buffer.alloc(EMPTY_PROOF_SIZE, 0));
   }
   async getRootRollupProof(input: RootRollupInputs, publicInputs: RootRollupPublicInputs): Promise<UInt8Vector> {
+    publicInputs.endAggregationObject = makeEmptyAggregationObject();
     return new UInt8Vector(Buffer.alloc(EMPTY_PROOF_SIZE, 0));
   }
 }
