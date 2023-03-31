11efd677aa4d4620aa405c5c69044fbb9988fc63
spalladino <santiago@aztecprotocol.com>
2023-03-31 10:23:26 +0000

    Make empty previous kernel data in kernel prover

--- yarn-project/sequencer-client/src/deps/tx.ts
+++ yarn-project/sequencer-client/src/deps/tx.ts
@@ -1,97 +1,10 @@
 import { pedersenCompressInputs } from '@aztec/barretenberg.js/crypto';
 import { BarretenbergWasm } from '@aztec/barretenberg.js/wasm';
-import {
-  AccumulatedData,
-  AggregationObject,
-  AztecAddress,
-  CircuitsWasm,
-  ConstantData,
-  ContractDeploymentData,
-  EMITTED_EVENTS_LENGTH,
-  EthAddress,
-  Fq,
-  Fr,
-  FunctionData,
-  KERNEL_L1_MSG_STACK_LENGTH,
-  KERNEL_NEW_COMMITMENTS_LENGTH,
-  KERNEL_NEW_CONTRACTS_LENGTH,
-  KERNEL_NEW_NULLIFIERS_LENGTH,
-  KERNEL_OPTIONALLY_REVEALED_DATA_LENGTH,
-  KERNEL_PRIVATE_CALL_STACK_LENGTH,
-  KERNEL_PUBLIC_CALL_STACK_LENGTH,
-  NewContractData,
-  OldTreeRoots,
-  OptionallyRevealedData,
-  PrivateKernelPublicInputs,
-  TxContext,
-  UInt8Vector,
-} from '@aztec/circuits.js';
+import { CircuitsWasm, NewContractData, PrivateKernelPublicInputs, UInt8Vector } from '@aztec/circuits.js';
 import { Tx } from '@aztec/tx';
-import times from 'lodash.times';
-
-function frZero() {
-  return Fr.fromBuffer(Buffer.alloc(32, 0));
-}
-
-function fqZero() {
-  return Fq.fromBuffer(Buffer.alloc(32, 0));
-}
-
-function makeEmptyEthAddress() {
-  return new EthAddress(Buffer.alloc(20, 0));
-}
-
-function makeEmptyNewContractData(): NewContractData {
-  return new NewContractData(AztecAddress.ZERO, makeEmptyEthAddress(), frZero());
-}
-
-function makeEmptyTxContext(): TxContext {
-  const deploymentData = new ContractDeploymentData(frZero(), frZero(), frZero(), makeEmptyEthAddress());
-  return new TxContext(false, false, true, deploymentData);
-}
-
-function makeEmptyOldTreeRoots(): OldTreeRoots {
-  return new OldTreeRoots(frZero(), frZero(), frZero(), frZero());
-}
-
-function makeEmptyConstantData(): ConstantData {
-  return new ConstantData(makeEmptyOldTreeRoots(), makeEmptyTxContext());
-}
-
-function makeEmptyOptionallyRevealedData(): OptionallyRevealedData {
-  return new OptionallyRevealedData(
-    frZero(),
-    new FunctionData(Buffer.alloc(4), true, true),
-    times(EMITTED_EVENTS_LENGTH, frZero),
-    frZero(),
-    makeEmptyEthAddress(),
-    false,
-    false,
-    false,
-    false,
-  );
-}
-
-function makeEmptyAccumulatedData(): AccumulatedData {
-  return new AccumulatedData(
-    AggregationObject.makeFake(),
-    frZero(),
-    times(KERNEL_NEW_COMMITMENTS_LENGTH, frZero),
-    times(KERNEL_NEW_NULLIFIERS_LENGTH, frZero),
-    times(KERNEL_PRIVATE_CALL_STACK_LENGTH, frZero),
-    times(KERNEL_PUBLIC_CALL_STACK_LENGTH, frZero),
-    times(KERNEL_L1_MSG_STACK_LENGTH, frZero),
-    times(KERNEL_NEW_CONTRACTS_LENGTH, makeEmptyNewContractData),
-    times(KERNEL_OPTIONALLY_REVEALED_DATA_LENGTH, makeEmptyOptionallyRevealedData),
-  );
-}
 
 function makeEmptyProof() {
   return new UInt8Vector(Buffer.alloc(0));
-}
-
-function makeEmptyPrivateKernelPublicInputs() {
-  return new PrivateKernelPublicInputs(makeEmptyAccumulatedData(), makeEmptyConstantData(), true);
 }
 
 function makeEmptyUnverifiedData() {
@@ -100,7 +13,7 @@ function makeEmptyUnverifiedData() {
 
 export function makeEmptyTx(): Tx {
   const isEmpty = true;
-  return new Tx(makeEmptyPrivateKernelPublicInputs(), makeEmptyProof(), makeEmptyUnverifiedData(), isEmpty);
+  return new Tx(PrivateKernelPublicInputs.makeEmpty(), makeEmptyProof(), makeEmptyUnverifiedData(), isEmpty);
 }
 
 export function hashNewContractData(wasm: CircuitsWasm | BarretenbergWasm, cd: NewContractData) {
