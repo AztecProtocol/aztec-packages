95bd856994a45694411dcc223d6208f879c0fb04
PhilWindle <philip.windle@gmail.com>
2023-04-06 17:49:20 +0000

    Some TX wasm refactoring

--- yarn-project/circuits.js/src/abis/abis.ts
+++ yarn-project/circuits.js/src/abis/abis.ts
@@ -4,26 +4,41 @@ import { CircuitsWasm } from '../wasm/index.js';
 import { FunctionData, FUNCTION_SELECTOR_NUM_BYTES, TxRequest, NewContractData } from '../index.js';
 import { serializeToBuffer } from '../utils/serialize.js';
 
+export async function wasmCall<T>(
+  wasm: CircuitsWasm,
+  fnName: string,
+  input: { toBuffer: () => Buffer },
+  expectedOutputLength: number,
+  output?: { fromBuffer: (buf: Buffer) => T },
+): Promise<T | Buffer> {
+  const inputData = input.toBuffer();
+  const outputBuf = wasm.call('bbmalloc', expectedOutputLength);
+  const inputBuf = wasm.call('bbmalloc', inputData.length);
+  wasm.writeMemory(inputBuf, inputData);
+  await wasm.asyncCall(fnName, inputBuf, outputBuf);
+  const buf = Buffer.from(wasm.getMemorySlice(outputBuf, outputBuf + expectedOutputLength));
+  wasm.call('bbfree', outputBuf);
+  wasm.call('bbfree', inputBuf);
+  return output?.fromBuffer(buf) ?? buf;
+}
+
 export async function hashTxRequest(wasm: CircuitsWasm, txRequest: TxRequest) {
-  const data = txRequest.toBuffer();
   wasm.call('pedersen__init');
-  wasm.writeMemory(0, data);
-  await wasm.asyncCall('abis__hash_tx_request', 0, data.length);
-  return Buffer.from(wasm.getMemorySlice(data.length, data.length + 32));
+  return await wasmCall(wasm, 'abis__hash_tx_request', txRequest, 32);
 }
 
 export async function computeFunctionSelector(wasm: CircuitsWasm, funcSig: string) {
-  const buf = Buffer.from(funcSig);
-  wasm.writeMemory(0, buf);
-  await wasm.asyncCall('abis__compute_function_selector', 0, buf.length);
-  return Buffer.from(wasm.getMemorySlice(buf.length, buf.length + FUNCTION_SELECTOR_NUM_BYTES));
+  return await wasmCall(
+    wasm,
+    'abis__compute_function_selector',
+    { toBuffer: () => Buffer.from(funcSig) },
+    FUNCTION_SELECTOR_NUM_BYTES,
+  );
 }
 
 export async function hashVK(wasm: CircuitsWasm, vkBuf: Buffer) {
   wasm.call('pedersen__init');
-  wasm.writeMemory(0, vkBuf);
-  await wasm.asyncCall('abis__hash_vk', 0, vkBuf.length);
-  return Buffer.from(wasm.getMemorySlice(vkBuf.length, vkBuf.length + 32));
+  return await wasmCall(wasm, 'abis__hash_vk', { toBuffer: () => vkBuf }, 32);
 }
 
 export async function computeFunctionLeaf(wasm: CircuitsWasm, fnLeaf: Buffer) {
@@ -40,6 +55,28 @@ export async function computeFunctionTreeRoot(wasm: CircuitsWasm, fnLeafs: Buffe
   await wasm.asyncCall('abis__compute_function_tree_root', 0, fnLeafs.length, inputVector.length);
   return Buffer.from(wasm.getMemorySlice(inputVector.length, inputVector.length + 32));
 }
+
+// not yet working
+// export async function inputBuffersToOutputBuffer(
+//   wasm: CircuitsWasm,
+//   fnName: string,
+//   buffers: Buffer[],
+//   expectedOutputLength: number,
+// ) {
+//   const offsets: number[] = [];
+//   const totalLength = buffers.reduce((total, cur) => {
+//     offsets.push(total);
+//     return total + cur.length;
+//   }, 0);
+//   const inputBuf = wasm.call('bbmalloc', totalLength);
+//   const outputBuf = wasm.call('bbmalloc', expectedOutputLength);
+//   wasm.writeMemory(inputBuf, Buffer.concat(buffers));
+//   await wasm.asyncCall(fnName, ...offsets.map(x => x + inputBuf), outputBuf);
+//   const output = Buffer.from(wasm.getMemorySlice(outputBuf, outputBuf + expectedOutputLength));
+//   wasm.call('bbfree', inputBuf);
+//   wasm.call('bbfree', outputBuf);
+//   return output;
+// }
 
 export async function hashConstructor(
   wasm: CircuitsWasm,
@@ -59,6 +96,14 @@ export async function hashConstructor(
   wasm.writeMemory(memLoc2, constructorVKHash);
   await wasm.asyncCall('abis__hash_constructor', 0, memLoc1, memLoc2, memLoc3);
   return Buffer.from(wasm.getMemorySlice(memLoc3, memLoc3 + 32));
+
+  // wasm.call('pedersen__init');
+  // return await inputBuffersToOutputBuffer(
+  //   wasm,
+  //   'abis__hash_constructor',
+  //   [functionData.toBuffer(), serializeToBuffer(args.map(fr => fr.toBuffer())), constructorVKHash],
+  //   32,
+  // );
 }
 
 export async function computeContractAddress(
@@ -85,13 +130,5 @@ export async function computeContractAddress(
 
 export async function computeContractLeaf(wasm: CircuitsWasm, cd: NewContractData) {
   wasm.call('pedersen__init');
-  const data = cd.toBuffer();
-  const outputBuf = wasm.call('bbmalloc', 32);
-  const inputBuf = wasm.call('bbmalloc', data.length);
-  wasm.writeMemory(inputBuf, data);
-  await wasm.asyncCall('abis__compute_contract_leaf', inputBuf, outputBuf);
-  const buf = Buffer.from(wasm.getMemorySlice(outputBuf, outputBuf + 32));
-  wasm.call('bbfree', outputBuf);
-  wasm.call('bbfree', inputBuf);
-  return Fr.fromBuffer(buf);
+  return await wasmCall(wasm, 'abis__compute_contract_leaf', { toBuffer: () => cd.toBuffer() }, 32, Fr);
 }
