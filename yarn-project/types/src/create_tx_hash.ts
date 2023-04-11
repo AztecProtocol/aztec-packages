fb5853afaea0f3c263bd947243caae5bfcb8ed32
Leila Wang <leizciw@gmail.com>
2023-04-11 14:18:41 +0000

    Update comment.

--- yarn-project/types/src/create_tx_hash.ts
+++ yarn-project/types/src/create_tx_hash.ts
@@ -1,7 +1,7 @@
 import { Fr, keccak } from '@aztec/foundation';
 import { TxHash } from './tx_hash.js';
 
-export interface TxData {
+interface TxData {
   newCommitments: Fr[];
   newNullifiers: Fr[];
   newContracts: Fr[];
@@ -9,15 +9,15 @@ export interface TxData {
 
 /**
  * Utility function to generate tx hash.
- * @param tx - The transaction from which to generate the hash.
+ * @param txData - The data from which to generate the hash.
  * @returns A hash of the tx data that identifies the tx.
  */
-export function createTxHash(tx: TxData) {
+export function createTxHash({ newCommitments, newNullifiers, newContracts }: TxData) {
   const data = Buffer.concat(
     [
-      tx.newCommitments.map(x => x.toBuffer()),
-      tx.newNullifiers.map(x => x.toBuffer()),
-      tx.newContracts.map(x => x.toBuffer()),
+      newCommitments.map(x => x.toBuffer()),
+      newNullifiers.map(x => x.toBuffer()),
+      newContracts.map(x => x.toBuffer()),
     ].flat(),
   );
   return new TxHash(keccak(data));
