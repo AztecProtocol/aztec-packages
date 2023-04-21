4287a005401a60e4ee139dc91c6720ccbe63c262
benesjan <janbenes1234@gmail.com>
2023-04-21 09:54:35 +0000

    chore: reenabling tsdoc in merkle-tree

--- yarn-project/merkle-tree/.eslintrc.cjs
+++ yarn-project/merkle-tree/.eslintrc.cjs
@@ -1,1 +1,7 @@
-module.exports = require('@aztec/foundation/eslint-legacy');
+// module.exports = require('@aztec/foundation/eslint-legacy');
+
+// TODO: uncomment first line and remove the following once docs are re-enabled globally
+const config = require('@aztec/foundation/eslint-legacy');
+config.plugins.push('eslint-plugin-tsdoc', 'jsdoc');
+config.rules['tsdoc/syntax'] = 'error';
+module.exports = config;
