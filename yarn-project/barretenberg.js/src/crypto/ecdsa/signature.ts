bcbb5bcc53e730f861b34374cb0d7014f1dfd014
Suyash Bagad <suyashnbagad1997@gmail.com>
2023-04-26 23:12:38 +0000

    Add ecdsa ts.

---
+++ yarn-project/barretenberg.js/src/crypto/ecdsa/signature.ts
@@ -0,0 +1,51 @@
+import { randomBytes } from '@aztec/foundation';
+
+/**
+ * ECDSA signature used for transactions.
+ * @see cpp/barretenberg/cpp/src/barretenberg/crypto/ecdsa/ecdsa.hpp
+ */
+export class EcdsaSignature {
+  constructor(
+    /**
+     * The r byte-array (32 bytes) in an ECDSA signature.
+     */
+    public r: Buffer,
+    /**
+     * The s byte-array (32 bytes) in an ECDSA signature.
+     */
+    public s: Buffer,
+    /**
+     * The recovery id (1 byte) in an ECDSA signature.
+     */
+    public v: Buffer,
+  ) {
+    // TODO: add these assertions, somehow!
+    // assertLength(this, 'r', 32);
+    // assertLength(this, 's', 32);
+    // assertLength(this, 'v', 1);
+  }
+
+  /**
+   * Converts an ECDSA signature to a buffer.
+   * @returns A buffer.
+   */
+  toBuffer() {
+    return Buffer.concat([this.r, this.s, this.v]);
+  }
+
+  /**
+   * Generate a random ECDSA signature for testing.
+   * @returns A randomly generated ECDSA signature (not a valid one).
+   */
+  public static random() {
+    return new EcdsaSignature(randomBytes(32), randomBytes(32), Buffer.from([27]));
+  }
+
+  /**
+   * Convert an ECDSA signature to a buffer.
+   * @returns A 65-character string of the form 0x<r><s><v>.
+   */
+  toString() {
+    return `0x${this.toBuffer().toString('hex')}`;
+  }
+}
