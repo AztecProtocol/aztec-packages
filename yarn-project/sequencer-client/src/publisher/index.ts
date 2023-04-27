7256ac820d2e164f43b030cd8ec0f0303d7bf189
benesjan <janbenes1234@gmail.com>
2023-04-27 10:19:43 +0000

    chore: cleanup 3

--- yarn-project/sequencer-client/src/publisher/index.ts
+++ yarn-project/sequencer-client/src/publisher/index.ts
@@ -7,6 +7,5 @@ export { L1Publisher } from './l1-publisher.js';
 export { PublisherConfig } from './config.js';
 
 export function getL1Publisher(config: PublisherConfig & TxSenderConfig): L1Publisher {
-  // return new L1Publisher(new EthereumjsTxSender(config), config);
   return new L1Publisher(new ViemTxSender(config), config);
 }
