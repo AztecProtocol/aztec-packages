d58fed61449f53ae35dca3b174e867a654ae7c46
dbanks12 <david@aztecprotocol.com>
2023-05-05 16:57:24 +0000

    c++ fix

--- circuits/cpp/src/aztec3/circuits/mock/native_mock_kernel_circuit.hpp
+++ circuits/cpp/src/aztec3/circuits/mock/native_mock_kernel_circuit.hpp
@@ -14,13 +14,11 @@ KernelCircuitPublicInputs<NT> native_mock_kernel_circuit(DummyComposer&,
                                                          KernelCircuitPublicInputs<NT> const& public_inputs)
 {
     {
-        std::vector<uint32_t> dummy_witness_indices;
         // 16 is the number of values added to `proof_witness_indices` at the end of `verify_proof`.
-        constexpr size_t num_witness_indices = 16;
-        for (size_t i = 0; i < num_witness_indices; ++i) {
-            dummy_witness_indices.push_back(i);
+        constexpr uint32_t num_witness_indices = 16;
+        for (uint32_t i = 0; i < num_witness_indices; ++i) {
+            public_inputs.end.aggregation_object.proof_witness_indices.push_back(i);
         }
-        public_inputs.end.aggregation_object.proof_witness_indices = dummy_witness_indices;
     }
     public_inputs.set_public();
 
