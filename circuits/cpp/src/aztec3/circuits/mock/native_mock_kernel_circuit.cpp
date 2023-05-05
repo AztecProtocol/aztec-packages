a7d9c86d87ced9be953b4179920ebda7d9ff16be
dbanks12 <david@aztecprotocol.com>
2023-05-05 17:37:50 +0000

    fix

---
+++ circuits/cpp/src/aztec3/circuits/mock/native_mock_kernel_circuit.cpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "native_mock_kernel_circuit.hpp"
+
+#include "aztec3/circuits/abis/kernel_circuit_public_inputs.hpp"
+#include "aztec3/utils/dummy_composer.hpp"
+#include "aztec3/utils/types/native_types.hpp"
+
+namespace aztec3::circuits::mock {
+
+using aztec3::circuits::abis::KernelCircuitPublicInputs;
+using NT = aztec3::utils::types::NativeTypes;
+using DummyComposer = aztec3::utils::DummyComposer;
+
+KernelCircuitPublicInputs<NT> native_mock_kernel_circuit(DummyComposer& /* dummycomposer */,
+                                                         KernelCircuitPublicInputs<NT>& public_inputs)
+{
+    {
+        // 16 is the number of values added to `proof_witness_indices` at the end of `verify_proof`.
+        constexpr uint32_t num_witness_indices = 16;
+        for (uint32_t i = 0; i < num_witness_indices; ++i) {
+            public_inputs.end.aggregation_object.proof_witness_indices.push_back(i);
+        }
+    }
+    public_inputs.set_public();
+
+    return public_inputs;
+}
+
+}  // namespace aztec3::circuits::mock
--- circuits/cpp/src/aztec3/circuits/mock/native_mock_kernel_circuit.hpp
+++ circuits/cpp/src/aztec3/circuits/mock/native_mock_kernel_circuit.hpp
@@ -1,8 +1,7 @@
 #pragma once
-#include <aztec3/circuits/abis/kernel_circuit_public_inputs.hpp>
-#include <aztec3/utils/dummy_composer.hpp>
-#include <aztec3/utils/types/circuit_types.hpp>
-#include <aztec3/utils/types/native_types.hpp>
+#include "aztec3/circuits/abis/kernel_circuit_public_inputs.hpp"
+#include "aztec3/utils/dummy_composer.hpp"
+#include "aztec3/utils/types/native_types.hpp"
 
 namespace aztec3::circuits::mock {
 
@@ -10,19 +9,7 @@ using aztec3::circuits::abis::KernelCircuitPublicInputs;
 using NT = aztec3::utils::types::NativeTypes;
 using DummyComposer = aztec3::utils::DummyComposer;
 
-KernelCircuitPublicInputs<NT> native_mock_kernel_circuit(DummyComposer&,
-                                                         KernelCircuitPublicInputs<NT> const& public_inputs)
-{
-    {
-        // 16 is the number of values added to `proof_witness_indices` at the end of `verify_proof`.
-        constexpr uint32_t num_witness_indices = 16;
-        for (uint32_t i = 0; i < num_witness_indices; ++i) {
-            public_inputs.end.aggregation_object.proof_witness_indices.push_back(i);
-        }
-    }
-    public_inputs.set_public();
-
-    return public_inputs;
-}
+KernelCircuitPublicInputs<NT> native_mock_kernel_circuit(DummyComposer& dummycomposer,
+                                                         KernelCircuitPublicInputs<NT>& public_inputs);
 
 }  // namespace aztec3::circuits::mock
