22d1c86941a8e92ae685baf610e7f3b2a4b516a2
Santiago Palladino <santiago@aztecprotocol.com>
2023-05-10 08:43:31 -0300

    Remove msg sender from call stack items in public processor (#534)
    * Set call stack item hash to zero if empty
    
    * Fix public call stack check in public kernel
    
    * Ignore noir contracts in prettier

--- yarn-project/circuits.js/src/structs/function_data.ts
+++ yarn-project/circuits.js/src/structs/function_data.ts
@@ -1,14 +1,18 @@
 import { BufferReader } from '@aztec/foundation/serialize';
 import { serializeToBuffer } from '../utils/serialize.js';
 
+const FUNCTION_SELECTOR_LENGTH = 4;
+
 /**
  * Function description for circuit.
  * @see abis/function_data.hpp
  */
 export class FunctionData {
   constructor(public functionSelector: Buffer, public isPrivate = true, public isConstructor = false) {
-    if (functionSelector.byteLength !== 4) {
-      throw new Error(`Function selector must be 4 bytes long, got ${functionSelector.byteLength} bytes.`);
+    if (functionSelector.byteLength !== FUNCTION_SELECTOR_LENGTH) {
+      throw new Error(
+        `Function selector must be ${FUNCTION_SELECTOR_LENGTH} bytes long, got ${functionSelector.byteLength} bytes.`,
+      );
     }
   }
   /**
@@ -19,8 +23,16 @@ export class FunctionData {
     return serializeToBuffer(this.functionSelector, this.isPrivate, this.isConstructor);
   }
 
+  /**
+   * Returns whether this instance is empty.
+   * @returns True if the function selector is zero.
+   */
+  isEmpty() {
+    return this.functionSelector.equals(Buffer.alloc(FUNCTION_SELECTOR_LENGTH, 0));
+  }
+
   public static empty(args?: { isPrivate?: boolean; isConstructor?: boolean }) {
-    return new FunctionData(Buffer.alloc(4, 0), args?.isPrivate, args?.isConstructor);
+    return new FunctionData(Buffer.alloc(FUNCTION_SELECTOR_LENGTH, 0), args?.isPrivate, args?.isConstructor);
   }
 
   /**
@@ -29,6 +41,6 @@ export class FunctionData {
    */
   static fromBuffer(buffer: Buffer | BufferReader): FunctionData {
     const reader = BufferReader.asReader(buffer);
-    return new FunctionData(reader.readBytes(4), reader.readBoolean(), reader.readBoolean());
+    return new FunctionData(reader.readBytes(FUNCTION_SELECTOR_LENGTH), reader.readBoolean(), reader.readBoolean());
   }
 }
