9358979f563d8b4ff85746b1c9977b66135875c2
LHerskind <lasse.herskind@gmail.com>
2023-05-16 17:01:27 +0000

    chore: forge fmt

--- l1-contracts/test/DecoderHelper.sol
+++ l1-contracts/test/DecoderHelper.sol
@@ -14,7 +14,11 @@ contract DecoderHelper is Decoder {
     return _decode(_l2Block);
   }
 
-  function computeDiffRootAndMessagesHash(bytes calldata _l2Block) external pure returns (bytes32, bytes32) {
+  function computeDiffRootAndMessagesHash(bytes calldata _l2Block)
+    external
+    pure
+    returns (bytes32, bytes32)
+  {
     (bytes32 diffRoot,,, bytes32 l1ToL2MessagesHash) = _computeConsumables(_l2Block);
     return (diffRoot, l1ToL2MessagesHash);
   }
