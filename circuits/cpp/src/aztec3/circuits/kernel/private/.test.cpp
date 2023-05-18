55856fa75ead7e7db53e71e15576053999fc91c3
ludamad <domuradical@gmail.com>
2023-05-18 19:31:10 -0400

    clang-tidy: fix error

--- circuits/cpp/src/aztec3/circuits/kernel/private/.test.cpp
+++ circuits/cpp/src/aztec3/circuits/kernel/private/.test.cpp
@@ -601,7 +601,8 @@ TEST(private_kernel_tests, cbind_private_kernel__dummy_previous_kernel)
     auto func = [] { return aztec3::circuits::kernel::private_kernel::utils::dummy_previous_kernel(); };
     auto [actual, expected] = call_func_and_wrapper(func, private_kernel__dummy_previous_kernel);
     // TODO(AD): investigate why direct operator== didn't work
-    std::stringstream actual_ss, expected_ss;
+    std::stringstream actual_ss;
+    std::stringstream expected_ss;
     actual_ss << actual;
     expected_ss << expected;
     EXPECT_EQ(actual_ss.str(), expected_ss.str());
