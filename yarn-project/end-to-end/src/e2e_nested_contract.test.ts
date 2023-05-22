26cfde0cf0b4ae27bda8c670fbb8c0868b4db99e
Santiago Palladino <santiago@aztecprotocol.com>
2023-05-22 12:49:05 -0300

    Cache public state writes within the same block

--- yarn-project/end-to-end/src/e2e_nested_contract.test.ts
+++ yarn-project/end-to-end/src/e2e_nested_contract.test.ts
@@ -1,14 +1,14 @@
 import { AztecNode, getConfigEnvVars } from '@aztec/aztec-node';
 import { AztecAddress, AztecRPCServer, Contract, ContractDeployer, Fr, TxStatus } from '@aztec/aztec.js';
-import { createDebugLogger } from '@aztec/foundation/log';
 import { ContractAbi } from '@aztec/foundation/abi';
+import { createDebugLogger } from '@aztec/foundation/log';
 import { ChildAbi, ParentAbi } from '@aztec/noir-contracts/examples';
 
+import { toBigInt } from '@aztec/foundation/serialize';
 import { mnemonicToAccount } from 'viem/accounts';
 import { createAztecRpcServer } from './create_aztec_rpc_client.js';
 import { deployL1Contracts } from './deploy_l1_contracts.js';
 import { MNEMONIC } from './fixtures.js';
-import { toBigInt } from '@aztec/foundation/serialize';
 
 const logger = createDebugLogger('aztec:e2e_nested_contract');
 
@@ -67,7 +67,7 @@ describe('e2e_nested_contract', () => {
   /**
    * Milestone 3.
    */
-  it('should mine transactions that perform nested calls', async () => {
+  it('performs nested calls', async () => {
     const tx = parentContract.methods
       .entryPoint(addressToField(childContract.address), Fr.fromBuffer(childContract.methods.value.selector).value)
       .send({ from: accounts[0] });
@@ -78,7 +78,7 @@ describe('e2e_nested_contract', () => {
     expect(receipt.status).toBe(TxStatus.MINED);
   }, 100_000);
 
-  it('should mine transactions that perform public nested calls', async () => {
+  it('performs public nested calls', async () => {
     const tx = parentContract.methods
       .pubEntryPoint(
         addressToField(childContract.address),
@@ -93,7 +93,7 @@ describe('e2e_nested_contract', () => {
     expect(receipt.status).toBe(TxStatus.MINED);
   }, 100_000);
 
-  it('should mine transactions that enqueue public calls', async () => {
+  it('enqueues a public call', async () => {
     const tx = parentContract.methods
       .enqueueCallToChild(
         addressToField(childContract.address),
@@ -109,7 +109,25 @@ describe('e2e_nested_contract', () => {
     expect(await getChildStoredValue(childContract)).toEqual(42n);
   }, 100_000);
 
-  it('should mine transactions that enqueue a public call with nested public calls', async () => {
+  // Fails with "solver opcode resolution error: cannot solve opcode: expression has too many unknowns %EXPR [ 0 ]%"
+  // See https://github.com/noir-lang/noir/issues/1347
+  it.skip('enqueues multiple public calls', async () => {
+    const tx = parentContract.methods
+      .enqueueCallToChildTwice(
+        addressToField(childContract.address),
+        Fr.fromBuffer(childContract.methods.pubStoreValue.selector).value,
+        42n,
+      )
+      .send({ from: accounts[0] });
+
+    await tx.isMined(0, 0.1);
+    const receipt = await tx.getReceipt();
+    expect(receipt.status).toBe(TxStatus.MINED);
+
+    expect(await getChildStoredValue(childContract)).toEqual(85n);
+  }, 100_000);
+
+  it('enqueues a public call with nested public calls', async () => {
     const tx = parentContract.methods
       .enqueueCallToPubEntryPoint(
         addressToField(childContract.address),
@@ -125,7 +143,9 @@ describe('e2e_nested_contract', () => {
     expect(await getChildStoredValue(childContract)).toEqual(42n);
   }, 100_000);
 
-  it.skip('should mine transactions that enqueue multiple public calls with nested public calls', async () => {
+  // Fails with "solver opcode resolution error: cannot solve opcode: expression has too many unknowns %EXPR [ 0 ]%"
+  // See https://github.com/noir-lang/noir/issues/1347
+  it.skip('enqueues multiple public calls with nested public calls', async () => {
     const tx = parentContract.methods
       .enqueueCallsToPubEntryPoint(
         addressToField(childContract.address),
@@ -138,6 +158,25 @@ describe('e2e_nested_contract', () => {
     const receipt = await tx.getReceipt();
     expect(receipt.status).toBe(TxStatus.MINED);
 
-    expect(await getChildStoredValue(childContract)).toEqual(84);
+    expect(await getChildStoredValue(childContract)).toEqual(84n);
+  }, 100_000);
+
+  // Regression for https://github.com/AztecProtocol/aztec-packages/issues/640
+  // Fails with "solver opcode resolution error: cannot solve opcode: expression has too many unknowns %EXPR [ 0 ]%"
+  // See https://github.com/noir-lang/noir/issues/1347
+  it.skip('reads fresh value after write within the same tx', async () => {
+    const tx = parentContract.methods
+      .pubEntryPointTwice(
+        addressToField(childContract.address),
+        Fr.fromBuffer(childContract.methods.pubStoreValue.selector).value,
+        42n,
+      )
+      .send({ from: accounts[0] });
+
+    await tx.isMined(0, 0.1);
+    const receipt = await tx.getReceipt();
+
+    expect(receipt.status).toBe(TxStatus.MINED);
+    expect(await getChildStoredValue(childContract)).toEqual(85n);
   }, 100_000);
 });
