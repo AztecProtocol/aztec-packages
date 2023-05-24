e5cf247c664b850c4261d179faef8c8ee50d95d8
PhilWindle <philip.windle@gmail.com>
2023-05-24 22:13:01 +0000

    Merge fixes

--- l1-contracts/src/periphery/UnverifiedDataEmitter.sol
+++ l1-contracts/src/periphery/UnverifiedDataEmitter.sol
@@ -12,34 +12,6 @@ import {IUnverifiedDataEmitter} from "./interfaces/IUnverifiedDataEmitter.sol";
  */
 contract UnverifiedDataEmitter is IUnverifiedDataEmitter {
   /**
-   * @notice Links L1 and L2 addresses and stores the acir bytecode of the L2 contract
-   * @param l2BlockNum - The L2 block number that the information is related to
-   * @param aztecAddress - The address of the L2 counterparty
-   * @param portalAddress - The address of the L1 counterparty
-   * @param l2BlockHash - The hash of the L2 block that this is related to
-   * @param acir - The acir bytecode of the L2 contract
-   */
-  event ContractDeployment(
-    uint256 indexed l2BlockNum,
-    bytes32 indexed aztecAddress,
-    address indexed portalAddress,
-    bytes32 l2BlockHash,
-    bytes acir
-  );
-
-  /**
-   * @notice Used to share data which are not required to advance the state but are needed for other purposes
-   * @param l2BlockNum - The L2 block number that the information is related to
-   * @param sender - The address of the account sharing the information
-   * @param l2BlockHash - The hash of the L2 block that this is related to.
-   * @param data - The information represented as raw bytes
-   * @dev Typically contains `TxAuxData` (preimage, contract address and contract slot)
-   */
-  event UnverifiedData(
-    uint256 indexed l2BlockNum, address indexed sender, bytes32 indexed l2BlockHash, bytes data
-  );
-
-  /**
    * @notice Logs data on chain
    * @dev Emits an `UnverifiedData` event
    * @param _l2BlockNum - The l2 block number that the unverified data is related to
