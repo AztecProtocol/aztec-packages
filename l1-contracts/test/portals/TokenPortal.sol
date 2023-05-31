c14ba197c2b7e91494919c2e56afe559bb12f5db
LHerskind <lasse.herskind@gmail.com>
2023-05-31 16:39:49 +0000

    feat: combine contracts for l1 -> l2 and l2 -> l1

--- l1-contracts/test/portals/TokenPortal.sol
+++ l1-contracts/test/portals/TokenPortal.sol
@@ -50,4 +50,21 @@ contract TokenPortal {
     // Send message to rollup
     return inbox.sendL2Message{value: msg.value}(actor, _deadline, contentHash, _secretHash);
   }
+
+  function withdraw(uint256 _amount, address _recipient) external returns (bytes32) {
+    DataStructures.L2ToL1Msg memory message = DataStructures.L2ToL1Msg({
+      sender: DataStructures.L2Actor(l2TokenAddress, 1),
+      recipient: DataStructures.L1Actor(address(this), block.chainid),
+      content: Hash.sha256ToField(
+        abi.encodeWithSignature("withdraw(uint256,address)", _amount, _recipient)
+        )
+    });
+
+    // @todo: (issue #624) handle different versions
+    bytes32 entryKey = registry.getOutbox().consume(message);
+
+    underlying.transfer(_recipient, _amount);
+
+    return entryKey;
+  }
 }
