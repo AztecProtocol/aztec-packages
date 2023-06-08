197f6319dad88f24486cc9a506a63dc649a481fd
Rahul Kothari <rahul.kothari.201@gmail.com>
2023-06-08 09:43:48 +0000

    don't pass secret as default val

--- yarn-project/aztec.js/src/utils/secrets.ts
+++ yarn-project/aztec.js/src/utils/secrets.ts
@@ -2,11 +2,11 @@ import { CircuitsWasm, Fr } from '@aztec/circuits.js';
 import { computeSecretMessageHash } from '@aztec/circuits.js/abis';
 
 /**
- * Given a secret, it computes its pederson hash - used to send l1 to l2 messages
- * @param secret - the secret to hash (defaults to a random field element)
+ * Given a secret, it computes its pedersen hash - used to send l1 to l2 messages
+ * @param secret - the secret to hash - secret could be generated however you want e.g. `Fr.random()`
  * @returns the hash
  */
-export async function computeMessageSecretHash(secret = Fr.random()): Promise<Fr> {
+export async function computeMessageSecretHash(secret: Fr): Promise<Fr> {
   const wasm = await CircuitsWasm.get();
   return computeSecretMessageHash(wasm, secret);
 }
