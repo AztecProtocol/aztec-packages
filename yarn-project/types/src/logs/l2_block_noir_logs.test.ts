4903f7d3381e80c09f5407af7211d3507f759bb2
benesjan <janbenes1234@gmail.com>
2023-06-12 12:33:00 +0000

    feat: l2_block_noir_logs

---
+++ yarn-project/types/src/logs/l2_block_noir_logs.test.ts
@@ -0,0 +1,12 @@
+import { L2BlockNoirLogs } from './l2_block_noir_logs.js';
+
+describe('L2BlockNoirLogs', () => {
+  it('can encode NoirLogs to buffer and back', () => {
+    const noirLogs = L2BlockNoirLogs.random(3, 6, 2);
+
+    const buffer = noirLogs.toBuffer();
+    const recovered = L2BlockNoirLogs.fromBuffer(buffer);
+
+    expect(recovered).toEqual(noirLogs);
+  });
+});
