f8e41a3c1f2076cd87f82d94af52da97cee48204
benesjan <janbenes1234@gmail.com>
2023-06-12 13:00:32 +0000

    WIP

--- yarn-project/types/src/logs/l2_logs_source.ts
+++ yarn-project/types/src/logs/l2_logs_source.ts
@@ -1,16 +1,16 @@
-import { NoirLogs } from './noir_logs.js';
+import { TxL2Logs } from './tx_l2_logs.js';
 
 /**
  * Interface of classes allowing for the retrieval of encrypted logs.
  */
-export interface NoirLogsSource {
+export interface L2LogsSource {
   /**
    * Gets the `take` amount of encrypted logs starting from `from`.
    * @param from - Number of the L2 block to which corresponds the first `encryptedLogs` to be returned.
    * @param take - The number of `encryptedLogs` to return.
    * @returns The requested `encryptedLogs`.
    */
-  getEncryptedLogs(from: number, take: number): Promise<NoirLogs[]>;
+  getEncryptedLogs(from: number, take: number): Promise<TxL2Logs[]>;
 
   /**
    * Starts the encrypted logs source.
