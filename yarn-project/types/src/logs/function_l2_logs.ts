9d958dd0859242126a465ed9295f3e3ae1a51dd9
benesjan <janbenes1234@gmail.com>
2023-06-13 11:15:31 +0000

    feat: populating the hash in "PrivateFunctionExecution.run"

--- yarn-project/types/src/logs/function_l2_logs.ts
+++ yarn-project/types/src/logs/function_l2_logs.ts
@@ -1,5 +1,7 @@
 import { BufferReader, serializeBufferToVector } from '@aztec/foundation/serialize';
 import { randomBytes } from 'crypto';
+import { sha256 } from '@aztec/foundation/crypto';
+import { Fr } from '@aztec/circuits.js';
 
 /**
  * Data container of logs emitted in 1 function invocation (corresponds to 1 kernel iteration).
@@ -31,6 +33,28 @@ export class FunctionL2Logs {
     // Adding 4 to each log's length to account for the size stored in the serialized buffer and then one more time
     // adding 4 for the resulting buffer length.
     return this.logs.reduce((acc, log) => acc + log.length + 4, 0) + 4;
+  }
+
+  /**
+   * Calculates hash of serialized logs.
+   * @returns 2 fields containing all 256 bits of information of sha256 hash.
+   */
+  public hash(): [Fr, Fr] {
+    const hash = sha256(this.toBuffer());
+
+    // TS version of https://github.com/AztecProtocol/aztec-packages/blob/e2e3bf1dbeda5199060fb1711200d20414557cd4/circuits/cpp/src/aztec3/circuits/hash.hpp#L330
+    // Split the hash into two fields, a high and a low
+    const buf1: Buffer = Buffer.alloc(32);
+    const buf2: Buffer = Buffer.alloc(32);
+
+    for (let i = 0; i < 16; i++) {
+      buf1[i] = 0;
+      buf1[16 + i] = hash[i];
+      buf2[i] = 0;
+      buf2[16 + i] = hash[i + 16];
+    }
+
+    return [Fr.fromBuffer(buf1), Fr.fromBuffer(buf2)];
   }
 
   /**
