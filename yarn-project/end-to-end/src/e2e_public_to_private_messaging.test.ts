ff196c7287542a858f50a946e309f7c30f89f07d
PhilWindle <philip.windle@gmail.com>
2023-06-22 10:08:12 +0000

    Generators fix

--- yarn-project/end-to-end/src/e2e_public_to_private_messaging.test.ts
+++ yarn-project/end-to-end/src/e2e_public_to_private_messaging.test.ts
@@ -51,7 +51,6 @@ describe('e2e_public_to_private_messaging', () => {
   /**
    * Milestone 5.4: Intra-contract Public -\> Private calls.
    */
-  // TODO(#662): Reenable once we can create more than one account.
   it('5.4: Should be able to create a commitment in a public function and spend in a private function', async () => {
     const mintAmount = 100n;
 
