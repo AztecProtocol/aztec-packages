91559e0bd250443ee805a481c98c77cce5725e1d
spypsy <spypsy@outlook.com>
2023-06-27 14:11:57 +0000

    Wait for Ethereum RPC server to become available

--- yarn-project/aztec-sandbox/src/index.ts
+++ yarn-project/aztec-sandbox/src/index.ts
@@ -1,9 +1,11 @@
 import http from 'http';
 import { foundry } from 'viem/chains';
+import { http as httpViemTransport, createPublicClient, HDAccount } from 'viem';
 
 import { mnemonicToAccount } from 'viem/accounts';
 import { getHttpRpcServer } from '@aztec/aztec-rpc';
 import { createDebugLogger } from '@aztec/foundation/log';
+import { retryUntil } from '@aztec/foundation/retry';
 import { AztecNodeConfig, getConfigEnvVars } from '@aztec/aztec-node';
 import { deployL1Contracts } from '@aztec/ethereum';
 
@@ -16,14 +18,47 @@ const logger = createDebugLogger('aztec:sandbox');
 export const localAnvil = foundry;
 
 /**
+ * Helper function that waits for the Ethereum RPC server to respond before deploying L1 contracts.
+ */
+async function waitThenDeploy(rpcUrl: string, hdAccount: HDAccount) {
+  // wait for ETH RPC to respond to a request.
+  const publicClient = createPublicClient({
+    chain: foundry,
+    transport: httpViemTransport(rpcUrl),
+  });
+  const chainID = await retryUntil(
+    async () => {
+      let chainId = 0;
+      try {
+        chainId = await publicClient.getChainId();
+      } catch (err) {
+        logger(`Failed to get Chain ID. Retrying...`);
+      }
+      return chainId;
+    },
+    'isEthRpcReady',
+    30,
+    1,
+  );
+
+  if (!chainID) {
+    throw Error(`ETH RPC server unresponsive at ${rpcUrl}.`);
+  }
+
+  // Deploy L1 contracts
+  const deployedL1Contracts = await deployL1Contracts(rpcUrl, hdAccount, localAnvil, logger);
+  return deployedL1Contracts;
+}
+
+/**
  * Create and start a new Aztec RCP HTTP Server
  */
 async function main() {
   const aztecNodeConfig: AztecNodeConfig = getConfigEnvVars();
-
   const hdAccount = mnemonicToAccount(MNEMONIC);
   const privKey = hdAccount.getHdKey().privateKey;
-  const deployedL1Contracts = await deployL1Contracts(aztecNodeConfig.rpcUrl, hdAccount, localAnvil, logger);
+
+  const deployedL1Contracts = await waitThenDeploy(aztecNodeConfig.rpcUrl, hdAccount);
   aztecNodeConfig.publisherPrivateKey = Buffer.from(privKey!);
   aztecNodeConfig.rollupContract = deployedL1Contracts.rollupAddress;
   aztecNodeConfig.contractDeploymentEmitterContract = deployedL1Contracts.contractDeploymentEmitterAddress;
