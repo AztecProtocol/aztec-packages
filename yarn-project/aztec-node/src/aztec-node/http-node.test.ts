a04da315577799318c81443003c0aad508f2845b
benesjan <janbenes1234@gmail.com>
2023-06-29 13:21:11 +0000

    test: WIP2

--- yarn-project/aztec-node/src/aztec-node/http-node.test.ts
+++ yarn-project/aztec-node/src/aztec-node/http-node.test.ts
@@ -1,6 +1,7 @@
-import { L2Block } from '@aztec/types';
+import { ContractPublicData, L2Block } from '@aztec/types';
 import { HttpNode } from './http-node.js';
 import { jest } from '@jest/globals';
+import { AztecAddress } from '@aztec/circuits.js';
 
 jest.spyOn(global, 'fetch');
 
@@ -60,6 +61,74 @@ describe('HttpNode', () => {
 
       expect(fetch).toHaveBeenCalledWith(`${URL}get-blocks?from=0&take=2`);
       expect(result).toEqual([]);
+    });
+  });
+
+  describe('getBlockHeight', () => {
+    it('should fetch and return the block height', async () => {
+      const response = { blockHeight: 100 };
+      setFetchMock(response);
+
+      const result = await httpNode.getBlockHeight();
+
+      expect(fetch).toHaveBeenCalledWith(`${URL}get-block-height`);
+      expect(result).toBe(100);
+    });
+  });
+
+  describe('getVersion', () => {
+    it('should fetch and return the version', async () => {
+      const response = { version: 5 };
+      setFetchMock(response);
+
+      const result = await httpNode.getVersion();
+
+      expect(fetch).toHaveBeenCalledWith(`${URL}get-version`);
+      expect(result).toBe(5);
+    });
+  });
+
+  describe('getChainId', () => {
+    it('should fetch and return the chain ID', async () => {
+      const response = { chainId: 1234 };
+      setFetchMock(response);
+
+      const result = await httpNode.getChainId();
+
+      expect(fetch).toHaveBeenCalledWith(`${URL}get-chain-id`);
+      expect(result).toBe(1234);
+    });
+  });
+
+  describe('getContractData', () => {
+    it('should fetch and return contract public data', async () => {
+      const contractData = ContractPublicData.random();
+      const response = {
+        contractData: contractData.toBuffer(),
+      };
+
+      setFetchMock(response);
+
+      const result = await httpNode.getContractData(contractData.contractData.contractAddress);
+
+      expect(fetch).toHaveBeenCalledWith(
+        `${URL}contract-data?address=${contractData.contractData.contractAddress.toString()}`,
+      );
+      expect(result).toEqual(contractData);
+    });
+
+    it('should return undefined if contract data is not available', async () => {
+      const response = {
+        contractData: undefined,
+      };
+      setFetchMock(response);
+
+      const randomAddress = AztecAddress.random();
+
+      const result = await httpNode.getContractData(randomAddress);
+
+      expect(fetch).toHaveBeenCalledWith(`${URL}contract-data?address=${randomAddress.toString()}`);
+      expect(result).toEqual(undefined);
     });
   });
 });
