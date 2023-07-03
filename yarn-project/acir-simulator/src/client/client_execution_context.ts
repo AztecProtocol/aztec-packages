89e610d97b3fac5029f1f7dc1b26b7884d960de8
dbanks12 <david@aztecprotocol.com>
2023-07-03 16:24:24 +0000

    cleanup

--- yarn-project/acir-simulator/src/client/client_execution_context.ts
+++ yarn-project/acir-simulator/src/client/client_execution_context.ts
@@ -53,53 +53,45 @@ export class ClientTxExecutionContext {
 
   /**
    * Gets the notes for a contract address and storage slot.
-   * Returns note preimages and their indices in the private data tree.
-   * Note that indices are not passed to app circuit. They forwarded to
+   * Returns note preimages and their leaf indices in the private data tree.
+   *
+   * Details:
+   * Check for pending notes with matching address/slot.
+   * If limit isn't reached after pending notes are all checked,
+   * fetchNotes from DB with modified limit.
+   * Real notes coming from DB will have a leafIndex which
+   * represents their index in the private data tree.
+   * Real pending notes will have a leafIndex set to -1
+   * since they don't exist (yet) in the private data tree.
+   *
+   * leaf indices are not passed to app circuit. They are forwarded to
    * the kernel prover which uses them to compute witnesses to pass
    * to the private kernel.
    *
    * @param contractAddress - The contract address.
    * @param storageSlot - The storage slot.
    * @param limit - The amount of notes to get.
-   * @returns An array of ACVM fields for the note count and the requested note preimages,
-   * and another array of indices corresponding to each note
+   * @returns An array of ACVM fields for the note count and the requested note preimages
+   * (padded to reach limit), and another array of indices corresponding to each _real_ note's
+   * position in the private data tree.
    */
   public async getNotes(contractAddress: AztecAddress, storageSlot: ACVMField, limit: number) {
-    // Check for pending notes with matching address/slot.
-    // If limit isn't yet reached, fetchNotes with modified limit.
-    // Real notes coming from DB will have a leafIndex which
-    // represents their index in the private data tree.
-    // Real pending notes will have a leafIndex set to -1
-    // since they don't exist (yet) in the private data tree.
-
-    this.log(`Checking ${this.pendingNotes.length} pending notes for matches...`);
-    this.log(`Looking for notes with contract address: ${contractAddress}`);
-    this.log(`Looking for notes with storage slot: ${storageSlot}`);
     const pendingPreimages: ACVMField[][] = [];
     for (const note of this.pendingNotes) {
-      this.log(`Checking note with value: ${note.preimage[0]}`);
-      this.log(`Checking note with contract address: ${note.contractAddress}`);
-      this.log(`Checking note with storage slot: ${note.storageSlot}`);
       if (pendingPreimages.length == limit) {
         break;
       }
-      this.log(`contract address match: ${note.contractAddress.equals(contractAddress)}`);
-      this.log(`storage slot match: ${note.storageSlot == storageSlot}`);
       if (note.contractAddress.equals(contractAddress) && note.storageSlot == storageSlot) {
         // TODO(dbanks12): flag as pending and separately provide "hint" of
         // which "new_commitment" in which kernel this read maps to
-        this.log(`note.preimage.length: ${note.preimage.length}`);
         pendingPreimages.push(note.preimage);
       }
     }
-    this.log(`After loop, before len`);
     const numPendingNotes = pendingPreimages.length;
-    this.log(`Before placeholders, got len: ${numPendingNotes}`);
     const pendingLeafIndexPlaceholders: bigint[] = Array(numPendingNotes).fill(BigInt(-1));
 
     // may still need to get some notes from db
     const remainingLimit = limit - numPendingNotes;
-    this.log(`Before fetch`);
     const { realCount: numDbRealNotes, notes: dbNotes } = await this.fetchNotes(
       contractAddress,
       storageSlot,
@@ -113,10 +105,6 @@ export class ClientTxExecutionContext {
     // all pending notes and notes found in db
     const numRealNotes = numPendingNotes + numDbRealNotes;
     // all preimages (including pending, dummy, and real)
-    this.log(`pendingPreimages: ${pendingPreimages.length}`);
-    this.log(`pendingPreimages.flat(): ${pendingPreimages.flat().length}`);
-    this.log(`dbAllPreimages: ${dbAllPreimages.length}`);
-    this.log(`dbAllPreimages.flat(): ${dbAllPreimages.flat().length}`);
     const allPreimages = [...pendingPreimages, ...dbAllPreimages];
     // leaf indices for all "real" notes
     // this includes placeholder indices (-1) for real pending notes
