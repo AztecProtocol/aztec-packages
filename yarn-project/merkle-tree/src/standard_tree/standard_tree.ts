97d8cee85d67a1b888681a84aa2337e5e9c45536
benesjan <janbenes1234@gmail.com>
2023-07-03 15:11:57 +0000

    docs: batch insertion algo

--- yarn-project/merkle-tree/src/standard_tree/standard_tree.ts
+++ yarn-project/merkle-tree/src/standard_tree/standard_tree.ts
@@ -9,16 +9,22 @@ export class StandardTree extends TreeBase implements AppendOnlyTree {
    * Appends the given leaves to the tree.
    * @param leaves - The leaves to append.
    * @returns Empty promise.
+   *
+   * @remarks The batch insertion algorithm works as follows:
+   *          1. Insert all the leaves,
+   *          2. start iterating over levels from the bottom up,
+   *          3. on each level iterate over all the affected nodes (i.e. nodes whose preimages have changed),
+   *          4. fetch the preimage, hash it and insert the updated value.
+   * @remarks This algorithm is optimal when it comes to the number of hashing operations. It might not be optimal when
+   *          it comes to the number of database reads, but that should be irrelevant given that most of the time
+   *          `getLatestValueAtIndex` will return a value from cache (because at least one of the 2 children was
+   *          touched in previous iteration).
    */
   public async appendLeaves(leaves: Buffer[]): Promise<void> {
     const numLeaves = this.getNumLeaves(true);
     if (numLeaves + BigInt(leaves.length) - 1n > this.maxIndex) {
       throw Error(`Can't append beyond max index. Max index: ${this.maxIndex}`);
     }
-
-    // 1. Insert all the leaves,
-    // 2. on the level above, update all values from Math.floor(firstIndex/2) to Math.floor(lastIndex/2)
-    //    --> if Math.floor(firstIndex/2) == Math.floor(lastIndex/2) then we can switch to standard hash to root method
 
     // 1. Insert all the leaves
     let firstIndex = numLeaves;
@@ -28,12 +34,12 @@ export class StandardTree extends TreeBase implements AppendOnlyTree {
       this.cache[cacheKey] = leaves[i];
     }
 
-    // 2. Start hashing layers above
     let lastIndex = firstIndex + BigInt(leaves.length);
+    // 2. Iterate over all the levels from the bottom up
     while (level > 0) {
       firstIndex >>= 1n;
       lastIndex >>= 1n;
-      // Iterate over all the affected nodes at this level
+      // 3.Iterate over all the affected nodes at this level and update them
       for (let index = firstIndex; index <= lastIndex; index++) {
         const lhs = await this.getLatestValueAtIndex(level, index * 2n, true);
         const rhs = await this.getLatestValueAtIndex(level, index * 2n + 1n, true);
