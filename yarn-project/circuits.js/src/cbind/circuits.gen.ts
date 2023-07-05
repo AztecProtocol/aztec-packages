2fd0e02a9f57a761338922508294f7b9c1e9f177
jeanmon <jean@aztecprotocol.com>
2023-07-05 12:30:20 +0000

    906 - re-generate circuits.gen.ts

--- yarn-project/circuits.js/src/cbind/circuits.gen.ts
+++ yarn-project/circuits.js/src/cbind/circuits.gen.ts
@@ -126,7 +126,7 @@ export function fromNativeAggregationState(o: NativeAggregationState): MsgpackNa
 
 interface MsgpackMembershipWitness {
   leaf_index: Buffer;
-  sibling_path: Tuple<Buffer, 8>;
+  sibling_path: Tuple<Buffer, 16>;
 }
 
 export function toMembershipWitness(o: MsgpackMembershipWitness): MembershipWitness {
