a8e64f877992c1bb65e89a463c6ad2a20e79310c
ludamad <domuradical@gmail.com>
2023-07-07 10:49:54 +0200

    fix: MembershipWitness disambiguation

--- yarn-project/circuits.js/src/cbind/types.ts
+++ yarn-project/circuits.js/src/cbind/types.ts
@@ -1,6 +1,43 @@
 // Type mappings for cbinds
 // Can either export things directly or handle
 // naming differences with the 'as' syntax
+import { Tuple } from '@aztec/foundation/serialize';
+import { Fr, MembershipWitness } from '../structs/index.js';
+import { toBufferBE } from '@aztec/foundation/bigint-buffer';
+
+/**
+ * Alias for msgpack which expects a MembershipWitness + N name.
+ */
+export class MembershipWitness16 extends MembershipWitness<16> {
+  constructor(
+    /**
+     * Index of a leaf in the Merkle tree.
+     */
+    leafIndex: Fr,
+    /**
+     * Sibling path of the leaf in the Merkle tree.
+     */
+    siblingPath: Tuple<Fr, 16>,
+  ) {
+    super(16, leafIndex.toBigInt(), siblingPath);
+  }
+}
+
+/**
+ * Coerce a variety of types to a buffer.
+ * Makes msgpack output easier to manage as this can handle a few cases.
+ * @param bufferable - The value to coerce.
+ */
+export function toBuffer(bufferable: { toBuffer(): Buffer } | bigint | Buffer) {
+  if (typeof bufferable === 'bigint') {
+    return toBufferBE(bufferable, 32);
+  } else if (bufferable instanceof Buffer) {
+    return bufferable;
+  } else {
+    return bufferable.toBuffer();
+  }
+}
+
 export {
   AggregationObject as NativeAggregationState,
   AztecAddress as Address,
@@ -14,7 +51,6 @@ export {
   OptionallyRevealedData,
   PublicDataRead,
   PublicDataUpdateRequest,
-  MembershipWitness,
   CombinedAccumulatedData,
   PrivateHistoricTreeRoots,
   CombinedHistoricTreeRoots,
