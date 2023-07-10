e0de7c662b10b30c0ef926964660b5640b23ad93
Rahul Kothari <rahul.kothari.201@gmail.com>
2023-07-10 17:12:13 +0000

    add multi transfer (initial version)
    simplify test.
    
    name fix.
    
    Use non-deterministic unenc log in zk token contract
    
    Add multi-transfer test to cci.
    
    Fix e2e build but multi transfer fails.
    
    Fix batchTx call.
    
    Co-authored-by: Jean M <jeanmon@users.noreply.github.com>
    
    fix multi test compilation
    
    contracts compile.
    
    Fix e2e multi compilation.
    
    minor syntax change.
    
    minor syntax change.
    
    contracts compile.
    
    Fix e2e test compilation.
    
    fix multi contract.
    
    Fix e2e multi test compilation.
    
    Remove files that must be gitignored.

---
+++ yarn-project/end-to-end/src/e2e_multi_transfer.test.ts
@@ -0,0 +1,109 @@
+import { AztecNodeService } from '@aztec/aztec-node';
+import { AztecRPCServer } from '@aztec/aztec-rpc';
+import { AztecAddress, Contract, Fr, Wallet } from '@aztec/aztec.js';
+import { DebugLogger } from '@aztec/foundation/log';
+import { PrivateTokenAirdropContract } from '@aztec/noir-contracts/types';
+import { MultiTransferContract } from '@aztec/noir-contracts/types';
+import { AztecRPC } from '@aztec/types';
+
+import {
+  expectUnencryptedLogsFromLastBlockToBe,
+  expectsNumOfEncryptedLogsInTheLastBlockToBe,
+  setup,
+} from './fixtures/utils.js';
+
+describe('multi-transfer payments', () => {
+  const numberOfAccounts = 6;
+
+  let aztecNode: AztecNodeService | undefined;
+  let aztecRpcServer: AztecRPC;
+  let wallet: Wallet;
+  let accounts: AztecAddress[];
+  let logger: DebugLogger;
+  let ownerAddress: AztecAddress;
+  const recipients: AztecAddress[] = [];
+
+  let zkTokenContract: PrivateTokenAirdropContract;
+  let multiTransferContract: MultiTransferContract;
+
+  beforeEach(async () => {
+    ({ aztecNode, aztecRpcServer, accounts, logger, wallet } = await setup(numberOfAccounts + 1)); // 1st being the `owner`
+    ownerAddress = accounts[0];
+
+    for (let i = 1; i < accounts.length; i++) {
+      const account = accounts[i];
+      recipients.push(account);
+    }
+  }, 100_000);
+
+  afterEach(async () => {
+    await aztecNode?.stop();
+    if (aztecRpcServer instanceof AztecRPCServer) {
+      await aztecRpcServer?.stop();
+    }
+  });
+
+  const deployZkTokenContract = async (initialBalance: bigint, owner: AztecAddress) => {
+    logger(`Deploying zk token contract...`);
+    zkTokenContract = await PrivateTokenAirdropContract.deploy(wallet, initialBalance, owner).send().deployed();
+    logger(`zk token contract deployed at ${zkTokenContract.address}`);
+  };
+
+  const deployMultiTransferContract = async () => {
+    logger(`Deploying multi-transfer contract...`);
+    multiTransferContract = await MultiTransferContract.deploy(wallet).send().deployed();
+    logger(`multi-transfer contract deployed at ${multiTransferContract.address}`);
+  };
+
+  const expectBalance = async (tokenContract: Contract, owner: AztecAddress, expectedBalance: bigint) => {
+    const [balance] = await tokenContract.methods.getBalance(owner).view({ from: owner });
+    logger(`Account ${owner} balance: ${balance}`);
+    expect(balance).toBe(expectedBalance);
+  };
+
+  it('12 transfers per transactions should work', async () => {
+    const initialNote = 1000n;
+
+    logger(`Deploying zk token contract...`);
+    await deployZkTokenContract(initialNote, ownerAddress);
+    logger(`Deploying multi-transfer contract...`);
+    await deployMultiTransferContract();
+
+    logger(`self batchTransfer()`);
+    const batchTransferTx = zkTokenContract.methods
+      .batchTransfer(ownerAddress, [400n, 300n, 200n], [ownerAddress, ownerAddress, ownerAddress], 0, 0)
+      .send({ origin: ownerAddress });
+    await batchTransferTx.isMined();
+    const batchTransferTxReceipt = await batchTransferTx.getReceipt();
+    logger(`consumption Receipt status: ${batchTransferTxReceipt.status}`);
+    await expectBalance(zkTokenContract, ownerAddress, initialNote);
+    await expectsNumOfEncryptedLogsInTheLastBlockToBe(aztecNode, 1);
+    await expectUnencryptedLogsFromLastBlockToBe(aztecNode, ['Balance set in constructor']);
+
+    const amounts: bigint[] = [50n, 50n, 50n, 20n, 20n, 20n, 15n, 15n, 15n, 30n, 30n, 30n];
+    const amountSum = amounts.reduce((a, b) => a + b, 0n);
+
+    logger(`multiTransfer()...`);
+    const multiTransferTx = multiTransferContract.methods
+      .multiTransfer(
+        zkTokenContract.address.toField(),
+        recipients.concat(recipients),
+        amounts,
+        ownerAddress,
+        Fr.fromBuffer(zkTokenContract.methods.batchTransfer.selector),
+      )
+      .send({ origin: ownerAddress });
+    await multiTransferTx.isMined();
+    const multiTransferTxReceipt = await multiTransferTx.getReceipt();
+    logger(`Consumption Receipt status: ${multiTransferTxReceipt.status}`);
+    await expectBalance(zkTokenContract, ownerAddress, initialNote - amountSum);
+    await expectBalance(zkTokenContract, recipients[0], amounts[0] + amounts[numberOfAccounts]);
+    await expectBalance(zkTokenContract, recipients[1], amounts[1] + amounts[numberOfAccounts + 1]);
+    await expectBalance(zkTokenContract, recipients[2], amounts[2] + amounts[numberOfAccounts + 2]);
+    await expectBalance(zkTokenContract, recipients[3], amounts[3] + amounts[numberOfAccounts + 3]);
+    await expectBalance(zkTokenContract, recipients[4], amounts[4] + amounts[numberOfAccounts + 4]);
+    await expectBalance(zkTokenContract, recipients[5], amounts[5] + amounts[numberOfAccounts + 5]);
+    await expectsNumOfEncryptedLogsInTheLastBlockToBe(aztecNode, 1);
+    await expectUnencryptedLogsFromLastBlockToBe(aztecNode, ['Balance set in constructor']);
+  }, 240_000);
+});
