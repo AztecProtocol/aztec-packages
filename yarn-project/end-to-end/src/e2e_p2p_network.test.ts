1b81302155f0553a9a29da394b54dc2967f01411
benesjan <janbenes1234@gmail.com>
2023-07-11 14:23:45 +0000

    refactor: renamed from to origin in SendMethodOptions

--- yarn-project/end-to-end/src/e2e_p2p_network.test.ts
+++ yarn-project/end-to-end/src/e2e_p2p_network.test.ts
@@ -127,7 +127,7 @@ describe('e2e_p2p_network', () => {
     const txs: SentTx[] = [];
     for (let i = 0; i < numTxs; i++) {
       const deployer = new ContractDeployer(TestContractAbi, aztecRpcServer);
-      const tx = deployer.deploy().send({ from: account, contractAddressSalt: Fr.random() });
+      const tx = deployer.deploy().send({ origin: account, contractAddressSalt: Fr.random() });
       logger(`Tx sent with hash ${await tx.getTxHash()}`);
       const receipt = await tx.getReceipt();
       expect(receipt).toEqual(
