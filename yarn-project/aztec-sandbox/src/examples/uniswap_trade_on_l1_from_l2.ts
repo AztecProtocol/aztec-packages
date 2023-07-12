d5d62b03cd020bb719d3f87ae0dbc024cb88b1e6
benesjan <janbenes1234@gmail.com>
2023-07-12 13:54:54 +0000

    Revert "Revert "Remove to/from from txdao, replace with 'origin' (#1024)""
    This reverts commit cd7d823d0e4ff7ba05abd8a403da30b42c27dba3.

--- yarn-project/aztec-sandbox/src/examples/uniswap_trade_on_l1_from_l2.ts
+++ yarn-project/aztec-sandbox/src/examples/uniswap_trade_on_l1_from_l2.ts
@@ -172,7 +172,7 @@ const transferWethOnL2 = async (
       (await aztecRpcClient.getAccountPublicKey(ownerAddress)).toBigInts(),
       (await aztecRpcClient.getAccountPublicKey(receiver)).toBigInts(),
     )
-    .send({ from: ownerAddress });
+    .send({ origin: ownerAddress });
   await transferTx.isMined(0, 0.5);
   const transferReceipt = await transferTx.getReceipt();
   // expect(transferReceipt.status).toBe(TxStatus.MINED);
@@ -241,7 +241,7 @@ async function main() {
   // Call the mint tokens function on the noir contract
   const consumptionTx = wethL2Contract.methods
     .mint(wethAmountToBridge, ownerPub, owner, messageKey, secret, ethAccount.toField())
-    .send({ from: owner });
+    .send({ origin: owner });
   await consumptionTx.isMined(0, 0.5);
   const consumptionReceipt = await consumptionTx.getReceipt();
   // expect(consumptionReceipt.status).toBe(TxStatus.MINED);
@@ -276,7 +276,7 @@ async function main() {
       uniswapPortalAddress,
       ethAccount.toField(),
     )
-    .send({ from: owner });
+    .send({ origin: owner });
   await withdrawTx.isMined(0, 0.5);
   const withdrawReceipt = await withdrawTx.getReceipt();
   // expect(withdrawReceipt.status).toBe(TxStatus.MINED);
@@ -327,7 +327,7 @@ async function main() {
   // Call the mint tokens function on the noir contract
   const daiMintTx = daiL2Contract.methods
     .mint(daiAmountToBridge, ownerPub, owner, depositDaiMessageKey, secret, ethAccount.toField())
-    .send({ from: owner });
+    .send({ origin: owner });
   await daiMintTx.isMined(0, 0.5);
   const daiMintTxReceipt = await daiMintTx.getReceipt();
   // expect(daiMintTxReceipt.status).toBe(TxStatus.MINED);
