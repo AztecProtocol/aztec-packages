ee41ea20cac51ee70e9cdc63c0224f3743eb2cd6
spypsy <spypsy@outlook.com>
2023-07-12 11:17:17 +0000

    update contracttree import

--- yarn-project/aztec-rpc/src/contract_data_oracle/index.ts
+++ yarn-project/aztec-rpc/src/contract_data_oracle/index.ts
@@ -1,7 +1,8 @@
 import { AztecNode } from '@aztec/aztec-node';
-import { ContractTree } from '@aztec/aztec.js';
 import { AztecAddress, CircuitsWasm, MembershipWitness, VK_TREE_HEIGHT } from '@aztec/circuits.js';
 import { ContractDatabase } from '@aztec/types';
+
+import { ContractTree } from '../contract_tree/index.js';
 
 /**
  * ContractDataOracle serves as a data manager and retriever for noir contracts.
