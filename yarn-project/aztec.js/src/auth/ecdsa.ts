d1411875937e2dcd4bcd3a656f7b66305e5544d2
spypsy <spypsy@outlook.com>
2023-07-14 10:32:07 +0000

    remove dummy import

--- yarn-project/aztec.js/src/auth/ecdsa.ts
+++ yarn-project/aztec.js/src/auth/ecdsa.ts
@@ -1,7 +1,6 @@
 import { EcdsaSignature } from '@aztec/circuits.js/barretenberg';
 
 import { secp256k1 } from '@noble/curves/secp256k1';
-import { account } from 'viem/accounts';
 
 import { EntrypointPayload } from '../account_impl/account_contract.js';
 import { AztecAddress } from '../index.js';
