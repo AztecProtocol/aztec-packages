c3f67f8ee4fba54bba3c6112bd2219f900f0fb32
benesjan <janbenes1234@gmail.com>
2023-07-14 08:22:01 +0000

    feat: generating Noir constants

--- yarn-project/circuits.js/src/cbind/constants.in.ts
+++ yarn-project/circuits.js/src/cbind/constants.in.ts
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { callCbind } from './cbind.js';
 import { CircuitsWasm } from '../wasm/circuits_wasm.js';
 
 /**
- * Convert the C++ constants to TypeScript.
+ * Convert the C++ constants to TypeScript and Noir.
  */
 async function main(): Promise<void> {
   const wasm = await CircuitsWasm.get();
@@ -14,15 +14,28 @@ async function main(): Promise<void> {
   const privateStateNoteGeneratorIndexEnum = callCbind(wasm, 'get_circuit_private_state_note_generator_index', []);
   const privateStateTypeEnum = callCbind(wasm, 'get_circuit_private_state_type', []);
 
-  const result: string =
-    '/* eslint-disable */\n// GENERATED FILE - DO NOT EDIT, RUN yarn remake-constants\n' +
-    processConstants(constants) +
-    processEnum('GeneratorIndex', generatorIndexEnum) +
-    processEnum('PrivateStateNoteGeneratorIndex', privateStateNoteGeneratorIndexEnum) +
-    processEnum('PrivateStateType', privateStateTypeEnum);
-
   const __dirname = dirname(fileURLToPath(import.meta.url));
-  fs.writeFileSync(__dirname + '/constants.gen.ts', result);
+
+  // Typescript
+  const resultTS: string =
+    '/* eslint-disable */\n// GENERATED FILE - DO NOT EDIT, RUN yarn remake-constants\n' +
+    processConstantsTS(constants) +
+    processEnumTS('GeneratorIndex', generatorIndexEnum) +
+    processEnumTS('PrivateStateNoteGeneratorIndex', privateStateNoteGeneratorIndexEnum) +
+    processEnumTS('PrivateStateType', privateStateTypeEnum);
+
+  fs.writeFileSync(__dirname + '/constants.gen.ts', resultTS);
+
+  // Noir
+  const resultNoir: string =
+    '/* eslint-disable */\n// GENERATED FILE - DO NOT EDIT, RUN yarn remake-constants in circuits.js\n' +
+    processConstantsNoir(constants) +
+    processConstantsNoir(generatorIndexEnum) + // Note: Noir doesn't support enums so we export them as constants
+    processConstantsNoir(privateStateNoteGeneratorIndexEnum) +
+    processConstantsNoir(privateStateTypeEnum);
+
+  const noirTargetPath = join(__dirname, '../../../noir-contracts/src/libs/noir-aztec/src/constants.gen.nr');
+  fs.writeFileSync(noirTargetPath, resultNoir);
 }
 
 /**
@@ -31,7 +44,7 @@ async function main(): Promise<void> {
  * @param constants - An object containing key-value pairs representing constants.
  * @returns A string containing code that exports the constants as TypeScript constants.
  */
-function processConstants(constants: { [key: string]: number }): string {
+function processConstantsTS(constants: { [key: string]: number }): string {
   const code: string[] = [];
   Object.entries(constants).forEach(([key, value]) => {
     code.push(`export const ${key} = ${value};`);
@@ -46,7 +59,7 @@ function processConstants(constants: { [key: string]: number }): string {
  * @param enumValues - An object containing key-value pairs representing enum values.
  * @returns A string containing code that exports the enum as a TypeScript enum.
  */
-function processEnum(enumName: string, enumValues: { [key: string]: number }): string {
+function processEnumTS(enumName: string, enumValues: { [key: string]: number }): string {
   const code: string[] = [];
 
   code.push(`export enum ${enumName} {`);
@@ -58,6 +71,20 @@ function processEnum(enumName: string, enumValues: { [key: string]: number }): s
   code.push('}');
 
   return code.join('\n');
+}
+
+/**
+ * Processes a collection of constants and generates code to export them as Noir constants.
+ *
+ * @param constants - An object containing key-value pairs representing constants.
+ * @returns A string containing code that exports the constants as Noir constants.
+ */
+function processConstantsNoir(constants: { [key: string]: number }): string {
+  const code: string[] = [];
+  Object.entries(constants).forEach(([key, value]) => {
+    code.push(`global ${key}: comptime Field = ${value};`);
+  });
+  return code.join('\n') + '\n';
 }
 
 // eslint-disable-next-line no-console
