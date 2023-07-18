9951a896b7be29e89581a57c4abc570bdc54e38e
Suyash Bagad <suyashnbagad1997@gmail.com>
2023-07-18 19:07:15 +0000

    Revert unwanted changes from circuits.gen.ts

--- yarn-project/circuits.js/src/cbind/circuits.gen.ts
+++ yarn-project/circuits.js/src/cbind/circuits.gen.ts
@@ -1,8 +1,10 @@
 /* eslint-disable */
 // GENERATED FILE DO NOT EDIT, RUN yarn remake-bindings
+import { Tuple, mapTuple } from '@aztec/foundation/serialize';
 import { IWasmModule } from '@aztec/foundation/wasm';
 
 import { Buffer } from 'buffer';
+import mapValues from 'lodash.mapvalues';
 
 import { callCbind } from './cbind.js';
 import {
@@ -40,8 +42,6 @@ import {
   toBuffer,
 } from './types.js';
 
-import { Tuple, mapTuple } from '@aztec/foundation/serialize';
-import mapValues from 'lodash.mapvalues';
 interface MsgpackG1AffineElement {
   x: Buffer;
   y: Buffer;
