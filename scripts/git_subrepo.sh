9fa1a9730454cb8382ed8ff319fb968ea3c08569
ludamad <adam.domurad@gmail.com>
2023-07-19 19:52:12 -0400

    feat: Bundle git subrepo for CI use

--- scripts/git_subrepo.sh
+++ scripts/git_subrepo.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -eu
 
-VERSION="0.4.6"
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-
-if ! [ -f "$SCRIPT_DIR"/git-subrepo-"$VERSION"/lib/git-subrepo ] ; then
-  url="https://github.com/ingydotnet/git-subrepo/archive/refs/tags/$VERSION.tar.gz"
-  
-  # Download the archive
-  curl -L -o "$SCRIPT_DIR"/git-subrepo.tar.gz $url
-  
-  # Extract the archive
-  tar -xzvf "$SCRIPT_DIR"/git-subrepo.tar.gz -C "$SCRIPT_DIR"
-fi
-"$SCRIPT_DIR"/git-subrepo-"$VERSION"/lib/git-subrepo $@
+"$SCRIPT_DIR"/git-subrepo/lib/git-subrepo $@
 
