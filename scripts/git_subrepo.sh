2e89f3f52a418cd1ad1d238f82f23cea2e89a991
ludamad <adam.domurad@gmail.com>
2023-07-19 19:30:52 -0400

    fix: script improvements

--- scripts/git_subrepo.sh
+++ scripts/git_subrepo.sh
@@ -2,14 +2,16 @@
 set -eu
 
 VERSION="0.4.6"
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-if ! [ -f git-subrepo-"$VERSION"/lib/git-subrepo ] ; then
+if ! [ -f "$SCRIPT_DIR"/git-subrepo-"$VERSION"/lib/git-subrepo ] ; then
   url="https://github.com/ingydotnet/git-subrepo/archive/refs/tags/$VERSION.tar.gz"
   
   # Download the archive
-  curl -L -o git-subrepo.tar.gz $url
+  curl -L -o "$SCRIPT_DIR"/git-subrepo.tar.gz $url
   
   # Extract the archive
-  tar -xzvf git-subrepo.tar.gz
+  tar -xzvf "$SCRIPT_DIR"/git-subrepo.tar.gz -C "$SCRIPT_DIR"
 fi
-git-subrepo-"$VERSION"/lib/git-subrepo $@
+"$SCRIPT_DIR"/git-subrepo-"$VERSION"/lib/git-subrepo $@
+
