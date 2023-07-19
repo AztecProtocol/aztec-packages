f674fa3decb335f775c8b2c28c98e3533d41fab7
PhilWindle <philip.windle@gmail.com>
2023-07-19 16:02:41 +0000

    Debugging

--- yarn-project/foundation/src/json-rpc/server/json_rpc_server.ts
+++ yarn-project/foundation/src/json-rpc/server/json_rpc_server.ts
@@ -92,7 +92,9 @@ export class JsonRpcServer {
     } else {
       // "JSON RPC mode" where a single endpoint is used and the method is given in the request body
       router.post('/', async (ctx: Koa.Context) => {
+        this.log(`RECEIVED RPC CALL `);
         const { params = [], jsonrpc, id, method } = ctx.request.body as any;
+        this.log(`RECEIVED RPC CALL ${method}`);
         // Ignore if not a function
         if (
           method === 'constructor' ||
