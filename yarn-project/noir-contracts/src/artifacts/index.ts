2f466d38a8cb262401f67a89babd5d28444b8994
benesjan <janbenes1234@gmail.com>
2023-07-20 16:25:59 +0000

    style: formatting fix

--- yarn-project/noir-contracts/src/artifacts/index.ts
+++ yarn-project/noir-contracts/src/artifacts/index.ts
@@ -3,10 +3,10 @@ import { ContractAbi } from '@aztec/foundation/abi';
 
 import ChildJson from './child_contract.json' assert { type: 'json' };
 import EcdsaAccountContractJson from './ecdsa_account_contract.json' assert { type: 'json' };
-import PokeableTokenContractJson from './pokeable_token_contract.json' assert { type: 'json' };
 import NonNativeTokenContractJson from './non_native_token_contract.json' assert { type: 'json' };
 import ParentJson from './parent_contract.json' assert { type: 'json' };
 import PendingCommitmentsContractJson from './pending_commitments_contract.json' assert { type: 'json' };
+import PokeableTokenContractJson from './pokeable_token_contract.json' assert { type: 'json' };
 import PublicTokenContractJson from './public_token_contract.json' assert { type: 'json' };
 import SchnorrMultiKeyAccountContractJson from './schnorr_multi_key_account_contract.json' assert { type: 'json' };
 import SchnorrSingleKeyAccountContractJson from './schnorr_single_key_account_contract.json' assert { type: 'json' };
