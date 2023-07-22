d7ff59740677d61e0b0084034ab58f44baa10794
ludamad <adam.domurad@gmail.com>
2023-07-21 21:37:32 -0400

    doc: beefier comment

--- scripts/git_subrepo.sh
+++ scripts/git_subrepo.sh
@@ -9,6 +9,14 @@ if ! git diff-index --quiet HEAD --; then
     exit 1
 fi
 
+# git subrepo is quite nice, but has one flaw in our workflow:
+# We frequently squash commits in PRs, and we might update the .gitrepo file
+# with a parent commit that later does not exist. 
+# A backup heuristic is used to later find the squashed commit's parent
+# using the .gitrepo file's git history. This might be brittle 
+# in the face of e.g. a .gitrepo whitespace change, but it's a fallback, 
+# we only have this issue in master, and the file should only be edited
+# generally by subrepo commands.
 SUBREPO_PATH="${2:-}"
 if [ -d "$SUBREPO_PATH" ] ; then
     # Read parent commit from .gitrepo file
