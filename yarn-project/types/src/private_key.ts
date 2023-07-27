36ef72ff6832e58d435b1cb8aff5d95fd0bc9fe8
benesjan <janbenes1234@gmail.com>
2023-07-27 09:28:06 +0000

    feat: initial impl of private key type + tests

---
+++ yarn-project/types/src/private_key.ts
@@ -0,0 +1,38 @@
+import { randomBytes } from '@aztec/foundation/crypto';
+
+/**
+ * A wrapper around a buffer representation of a private key with validation checks.
+ */
+export class PrivateKey {
+  constructor(
+    /** A buffer containing the private key. */
+    public key: Buffer,
+  ) {
+    if (key.length != 32) {
+      throw new Error(`Invalid private key length. Got ${key.length}, expected 32.`);
+    }
+  }
+
+  /**
+   * Creates a PrivateKey instance from a hex string.
+   * @param keyString - A hex string representation of the private key.
+   * @returns A PrivateKey instance.
+   */
+  public static fromHexString(keyString: string): PrivateKey {
+    if (keyString.startsWith('0x')) {
+      keyString = keyString.slice(2);
+    }
+    if (keyString.length != 64) {
+      throw new Error(`Invalid private key string length. Got ${keyString.length}, expected 64.`);
+    }
+    return new PrivateKey(Buffer.from(keyString, 'hex'));
+  }
+
+  /**
+   * Creates a random PrivateKey instance.
+   * @returns A PrivateKey instance.
+   */
+  public static random(): PrivateKey {
+    return new PrivateKey(randomBytes(32));
+  }
+}
