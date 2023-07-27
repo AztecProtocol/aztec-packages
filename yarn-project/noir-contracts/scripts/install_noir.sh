128d2f56932632be1380552bf831aeb9a541006c
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2023-07-27 17:46:56 +0000

    fix: add noir compilation dockerfile

---
+++ yarn-project/noir-contracts/scripts/install_noir.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -eu
+
+export NARGO_HOME="$(pwd)/.nargo"
+NARGO_BIN_DIR="$NARGO_HOME/bin"
+BIN_URL="https://raw.githubusercontent.com/noir-lang/noirup/master/noirup"
+BIN_PATH="$NARGO_BIN_DIR/noirup"
+NARGO_MAN_DIR="$NARGO_HOME/share/man/man1"
+
+# Clean
+rm -rf $NARGO_HOME
+
+# Install noirup.
+mkdir -p $NARGO_BIN_DIR
+
+echo ""
+echo ""
+echo ""
+echo $NARGO_BIN_DIR
+echo ""
+echo ""
+echo ""
+
+mkdir -p $NARGO_MAN_DIR
+
+curl -# -L $BIN_URL -o $BIN_PATH
+chmod +x $BIN_PATH
+export PATH=$NARGO_BIN_DIR:$PATH
+
+# Install nargo
+noirup
