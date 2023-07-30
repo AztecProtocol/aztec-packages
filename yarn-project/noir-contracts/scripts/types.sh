a1312a805b41c036e26c481726a35f93075ed2d6
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2023-07-30 23:45:55 +0000

    fix

--- yarn-project/noir-contracts/scripts/types.sh
+++ yarn-project/noir-contracts/scripts/types.sh
@@ -14,14 +14,30 @@
 # Exit on error (set -e), treat unset variables as an error (set -u),
 # and propagate the exit status of the first failing command in a pipeline (set -o pipefail).
 set -eu;
+artifacts_dir="src/artifacts"
+
 
 # Create output directories
 mkdir -p src/types
-mkdir -p src/artifacts
+mkdir -p $artifacts_dir
+
 
 ROOT=$(pwd)
 
-# Trap any ERR signal and call the custom error handler
+write_import() {
+    NAME=$1
+    CONTRACT_NAME=`echo $NAME | sed -r 's/(^|_)(.)/\U\2/g'`
+    echo "import ${CONTRACT_NAME}Json from './${NAME}_contract.json' assert { type: 'json' };"  >> "$artifacts_dir/index.ts";
+}
+
+write_export() {
+    NAME=$1
+    CONTRACT_NAME=`echo $NAME | sed -r 's/(^|_)(.)/\U\2/g'`
+    echo "export const ${CONTRACT_NAME}ContractAbi = ${CONTRACT_NAME}Json as ContractAbi;"  >> "$artifacts_dir/index.ts";
+    echo "Written typescript for $NAME"
+}
+
+
 process() {
   CONTRACT_NAME=$1
 
@@ -50,6 +66,20 @@ done
 
 # Wait for all background processes to finish
 wait
+
+# Write the index ts stuff
+# Remove the output file
+rm $artifacts_dir/index.ts || true
+
+echo "// Auto generated module\n" > "$artifacts_dir/index.ts";
+echo "import { ContractAbi } from '@aztec/foundation/abi';"  >> "$artifacts_dir/index.ts";
+for CONTRACT_NAME in "$@"; do
+    write_import $CONTRACT_NAME
+done
+
+for CONTRACT_NAME in "$@"; do
+    write_export $CONTRACT_NAME
+done
 
 # only run the rest when the full flag is set
 format
--- yarn-project/noir-contracts/scripts/types_ci.sh
+++ yarn-project/noir-contracts/scripts/types_ci.sh
@@ -1,3 +1,2 @@
 # Run the types script for all files
-mkdir -p src/types
 ./scripts/types.sh $(./scripts/get_all_contracts.sh)
