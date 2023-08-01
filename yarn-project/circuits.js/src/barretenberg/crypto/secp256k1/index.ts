78f6f36ec5aed9b0031cd94b6aca5dcd7b6818ef
benesjan <janbenes1234@gmail.com>
2023-08-01 06:57:21 +0000

    Revert "chore: removed secp256k1"
    This reverts commit f39e6a65768625fb31909243c85ccd9da99ac100.

---
+++ yarn-project/circuits.js/src/barretenberg/crypto/secp256k1/index.ts
@@ -0,0 +1,68 @@
+import { IWasmModule } from '@aztec/foundation/wasm';
+
+import { CircuitsWasm, Fr, Point, PrivateKey } from '../../../index.js';
+import { Curve } from '../curve/index.js';
+
+/**
+ * Secp256k1 elliptic curve operations.
+ */
+export class Secp256k1 implements Curve {
+  /**
+   * Creates a new Secp256k1 instance.
+   * @returns New Secp256k1 instance.
+   */
+  public static async new() {
+    return new this(await CircuitsWasm.get());
+  }
+
+  constructor(private wasm: IWasmModule) {}
+
+  // prettier-ignore
+  static generator = Point.fromBuffer(Buffer.from([
+    0x79, 0xbe, 0x66, 0x7e, 0xf9, 0xdc, 0xbb, 0xac, 0x55, 0xa0, 0x62, 0x95, 0xce, 0x87, 0x0b, 0x07,
+    0x02, 0x9b, 0xfc, 0xdb, 0x2d, 0xce, 0x28, 0xd9, 0x59, 0xf2, 0x81, 0x5b, 0x16, 0xf8, 0x17, 0x98,
+    0x48, 0x3a, 0xda, 0x77, 0x26, 0xa3, 0xc4, 0x65, 0x5d, 0xa4, 0xfb, 0xfc, 0x0e, 0x11, 0x08, 0xa8,
+    0xfd, 0x17, 0xb4, 0x48, 0xa6, 0x85, 0x54, 0x19, 0x9c, 0x47, 0xd0, 0x8f, 0xfb, 0x10, 0xd4, 0xb8,
+  ]));
+
+  /**
+   * Point generator
+   * @returns The generator for the curve.
+   */
+  public generator(): Point {
+    return Secp256k1.generator;
+  }
+
+  /**
+   * Multiplies a point by a private key (adds the point `privateKey` amount of time).
+   * @param point - Point to multiply.
+   * @param privateKey - Private key to multiply by.
+   * @returns Result of the multiplication.
+   */
+  public mul(point: Point, privateKey: PrivateKey): Point {
+    this.wasm.writeMemory(0, point.toBuffer());
+    this.wasm.writeMemory(64, privateKey.value);
+    this.wasm.call('ecc_secp256k1__mul', 0, 64, 96);
+    return Point.fromBuffer(Buffer.from(this.wasm.getMemorySlice(96, 160)));
+  }
+
+  /**
+   * Gets a random field element.
+   * @returns Random field element.
+   */
+  public getRandomFr(): Fr {
+    this.wasm.call('ecc_secp256k1__get_random_scalar_mod_circuit_modulus', 0);
+    return Fr.fromBuffer(Buffer.from(this.wasm.getMemorySlice(0, 32)));
+  }
+
+  /**
+   * Converts a 512 bits long buffer to a field.
+   * @param uint512Buf - The buffer to convert.
+   * @returns A field element.
+   */
+  public reduce512BufferToFr(uint512Buf: Buffer): Fr {
+    this.wasm.writeMemory(0, uint512Buf);
+    this.wasm.call('ecc_secp256k1__reduce512_buffer_mod_circuit_modulus', 0, 64);
+    return Fr.fromBuffer(Buffer.from(this.wasm.getMemorySlice(64, 96)));
+  }
+}
