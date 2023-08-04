a9b7b67aeb123803b3c308ff535e9311def8eefe
PhilWindle <philip.windle@gmail.com>
2023-08-04 16:28:13 +0000

    Set dependencies to deployed version

---
+++ canary/scripts/update_packages.sh
@@ -0,0 +1,10 @@
+set -e
+
+VERSION=$1
+
+echo "Updating Aztec dependencies to version $VERSION"
+
+TMP=$(mktemp)
+for PKG in $(jq --raw-output ".dependencies | keys[] | select(contains(\"@aztec/\"))" package.json); do
+  jq --arg v $VERSION ".dependencies[\"$PKG\"] = \$v" package.json > $TMP && mv $TMP package.json
+done
