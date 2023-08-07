b70776552588d8e97eb3d99862ede335be1c50e0
codygunton <codygunton@gmail.com>
2023-08-07 20:37:04 +0000

    Compare Pedersen benchmarks.
    Benchmark                                              Time             CPU      Time Old      Time New       CPU Old       CPU New
    -----------------------------------------------------------------------------------------------------------------------------------
    pedersen_compress_pair/10/repeats:1                 +0.2210         +0.2209         77876         95085         77887         95095
    pedersen_compress_array/10/repeats:1                +0.0200         +0.0199        426862        435396        426919        435432
    OVERALL_GEOMEAN                                     +0.1160         +0.1159             0             0             0             0

---
+++ circuits/cpp/barretenberg/cpp/src/barretenberg/benchmark/simulator_bench/native.bench.cpp
@@ -0,0 +1,76 @@
+#include <benchmark/benchmark.h>
+
+#include "barretenberg/benchmark/benchmark_utilities.hpp"
+#include "barretenberg/proof_system/circuit_builder/circuit_simulator.hpp"
+#include "barretenberg/stdlib/commitment/pedersen/pedersen.hpp"
+
+using namespace benchmark;
+
+namespace simulator_bench {
+
+using Simulator = proof_system::CircuitSimulatorBN254;
+using witness_ct = proof_system::plonk::stdlib::witness_t<Simulator>;
+using field_ct = proof_system::plonk::stdlib::field_t<Simulator>;
+
+// Number of times to perform operation of interest in the benchmark circuits, e.g. # of hashes to perform
+constexpr size_t MIN_NUM_ITERATIONS = bench_utils::BenchParams::MIN_NUM_ITERATIONS;
+constexpr size_t MAX_NUM_ITERATIONS = bench_utils::BenchParams::MAX_NUM_ITERATIONS;
+// Number of times to repeat each benchmark
+constexpr size_t NUM_REPETITIONS = bench_utils::BenchParams::NUM_REPETITIONS;
+
+namespace {
+auto& engine = numeric::random::get_debug_engine();
+}
+
+/**
+ * @brief Benchmark: Construction of a Ultra Honk proof for a circuit determined by the provided circuit function
+ */
+void pedersen_compress_pair(State& state) noexcept
+{
+    for (auto _ : state) {
+        state.PauseTiming();
+        Simulator simulator;
+
+        fr left_in = fr::random_element();
+        fr right_in = fr::random_element();
+
+        // ensure left has skew 1, right has skew 0
+        if ((left_in.from_montgomery_form().data[0] & 1) == 1) {
+            left_in += fr::one();
+        }
+        if ((right_in.from_montgomery_form().data[0] & 1) == 0) {
+            right_in += fr::one();
+        }
+
+        field_ct left = witness_ct(&simulator, left_in);
+        field_ct right = witness_ct(&simulator, right_in);
+        state.ResumeTiming();
+        fr result = crypto::pedersen_commitment::compress_native({ left.get_value(), right.get_value() });
+        DoNotOptimize(result);
+    }
+};
+
+void pedersen_compress_array(State& state) noexcept
+{
+    for (auto _ : state) {
+        state.PauseTiming();
+        const size_t num_input_bytes = 351;
+
+        Simulator simulator;
+
+        std::vector<uint8_t> input;
+        input.reserve(num_input_bytes);
+        for (size_t i = 0; i < num_input_bytes; ++i) {
+            input.push_back(engine.get_random_uint8());
+        }
+
+        state.ResumeTiming();
+        fr result = crypto::pedersen_commitment::compress_native(input);        
+        DoNotOptimize(result);
+    }
+};
+
+
+BENCHMARK(pedersen_compress_pair)->DenseRange(MIN_NUM_ITERATIONS, MAX_NUM_ITERATIONS)->Repetitions(NUM_REPETITIONS);
+BENCHMARK(pedersen_compress_array)->DenseRange(MIN_NUM_ITERATIONS, MAX_NUM_ITERATIONS)->Repetitions(NUM_REPETITIONS);
+} // namespace simulator_bench
