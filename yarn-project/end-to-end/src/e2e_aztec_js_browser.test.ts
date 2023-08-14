6167034bc524f9f28dc185d8f0b772fa19fbde72
PhilWindle <philip.windle@gmail.com>
2023-08-14 18:41:35 +0000

    Increased test timeout

--- yarn-project/end-to-end/src/e2e_aztec_js_browser.test.ts
+++ yarn-project/end-to-end/src/e2e_aztec_js_browser.test.ts
@@ -111,7 +111,7 @@ conditionalDescribe()('e2e_aztec.js_browser', () => {
     const accounts = await testClient.getAccounts();
     const stringAccounts = accounts.map(acc => acc.toString());
     expect(stringAccounts.includes(result)).toBeTruthy();
-  });
+  }, 30_000);
 
   it('Deploys Private Token contract', async () => {
     const txHash = await page.evaluate(
