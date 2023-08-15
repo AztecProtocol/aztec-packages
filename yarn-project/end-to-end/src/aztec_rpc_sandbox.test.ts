45f340d5a0d09bb09fad88eb271f00944f0502d1
benesjan <janbenes1234@gmail.com>
2023-08-15 14:43:01 +0000

    naming fix

--- yarn-project/end-to-end/src/aztec_rpc_sandbox.test.ts
+++ yarn-project/end-to-end/src/aztec_rpc_sandbox.test.ts
@@ -3,4 +3,4 @@ import { createAztecRpcClient } from '@aztec/aztec.js';
 
 const { SANDBOX_URL = 'http://localhost:8080' } = process.env;
 
-aztecRpcTestSuite('AztecRPCServer', createAztecRpcClient(SANDBOX_URL));
+aztecRpcTestSuite('aztec_rpc_sandbox', createAztecRpcClient(SANDBOX_URL));
