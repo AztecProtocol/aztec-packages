f77802ccc5574609846a758c34d6143841cd8455
Santiago Palladino <santiago@aztecprotocol.com>
2023-08-17 19:39:10 -0300

    Introduce ordering index for enqueued public calls

--- yarn-project/end-to-end/src/e2e_ordering.test.ts
+++ yarn-project/end-to-end/src/e2e_ordering.test.ts
@@ -2,7 +2,7 @@
 import { AztecNodeService } from '@aztec/aztec-node';
 import { AztecRPCServer } from '@aztec/aztec-rpc';
 import { BatchCall, Wallet } from '@aztec/aztec.js';
-import { Fr } from '@aztec/circuits.js';
+import { Fr, PublicCallRequest } from '@aztec/circuits.js';
 import { toBigInt } from '@aztec/foundation/serialize';
 import { ChildContract, ParentContract } from '@aztec/noir-contracts/types';
 import { AztecRPC, FunctionCall, L2BlockL2Logs } from '@aztec/types';
@@ -35,32 +35,50 @@ describe('e2e_ordering', () => {
       child = await ChildContract.deploy(wallet).send().deployed();
     });
 
-    const getChildStoredValue = () =>
-      aztecRpcServer.getPublicStorageAt(child.address, new Fr(1)).then(x => toBigInt(x!));
+    describe('enqueued public calls ordering', () => {
+      const sendBatch = async (actions: FunctionCall[]) => {
+        const batch = new BatchCall(wallet, actions);
+        const tx = await batch.simulate();
+        await batch.send().wait();
+        const logs = await aztecRpcServer.getUnencryptedLogs(1, 10).then(L2BlockL2Logs.unrollLogs);
+        const value = await aztecRpcServer.getPublicStorageAt(child.address, new Fr(1)).then(x => toBigInt(x!));
+        return { tx, logs, value };
+      };
 
-    // Fails since current value at the end of execution is 10, not 20
-    it.skip('orders public function execution requests when nested call is last', async () => {
-      const actions: FunctionCall[] = [
-        child.methods.pubSetValue(10).request(),
-        parent.methods.enqueueCallToChild(child.address, child.methods.pubSetValue.selector, 20).request(),
-      ];
+      const callsToHashes = (enqueuedPublicCalls: PublicCallRequest[]) =>
+        Promise.all(enqueuedPublicCalls.map(call => call.toPublicCallStackItem().then(item => item.hash())));
 
-      await new BatchCall(wallet, actions).send().wait();
-      expect(await getChildStoredValue()).toEqual(20n);
-      const logs = await aztecRpcServer.getUnencryptedLogs(1, 10).then(L2BlockL2Logs.unrollLogs);
-      expect(logs).toEqual([[10], [20]].map(Buffer.from));
-    });
+      it('orders public function execution requests when nested call is last', async () => {
+        const actions: FunctionCall[] = [
+          child.methods.pubSetValue(10).request(),
+          parent.methods.enqueueCallToChild(child.address, child.methods.pubSetValue.selector, 20).request(),
+        ];
 
-    it('orders public function execution requests when nested call is first', async () => {
-      const actions: FunctionCall[] = [
-        parent.methods.enqueueCallToChild(child.address, child.methods.pubSetValue.selector, 10).request(),
-        child.methods.pubSetValue(20).request(),
-      ];
+        const { tx, logs, value } = await sendBatch(actions);
+        const { enqueuedPublicFunctionCalls: enqueuedPublicCalls } = tx;
 
-      await new BatchCall(wallet, actions).send().wait();
-      expect(await getChildStoredValue()).toEqual(20n);
-      const logs = await aztecRpcServer.getUnencryptedLogs(1, 10).then(L2BlockL2Logs.unrollLogs);
-      expect(logs).toEqual([[10], [20]].map(Buffer.from));
+        expect(enqueuedPublicCalls.length).toEqual(2);
+        expect(tx.data.end.publicCallStack.slice(0, 2)).toEqual(await callsToHashes(enqueuedPublicCalls));
+        expect(enqueuedPublicCalls.map(c => c.args[0].toBigInt())).toEqual([20n, 10n]);
+        expect(logs).toEqual([[10], [20]].map(Buffer.from));
+        expect(value).toEqual(20n);
+      });
+
+      it('orders public function execution requests when nested call is first', async () => {
+        const actions: FunctionCall[] = [
+          parent.methods.enqueueCallToChild(child.address, child.methods.pubSetValue.selector, 10).request(),
+          child.methods.pubSetValue(20).request(),
+        ];
+
+        const { tx, logs, value } = await sendBatch(actions);
+        const { enqueuedPublicFunctionCalls: enqueuedPublicCalls } = tx;
+
+        expect(enqueuedPublicCalls.length).toEqual(2);
+        expect(tx.data.end.publicCallStack.slice(0, 2)).toEqual(await callsToHashes(enqueuedPublicCalls));
+        expect(enqueuedPublicCalls.map(c => c.args[0].toBigInt())).toEqual([20n, 10n]);
+        expect(logs).toEqual([[10], [20]].map(Buffer.from));
+        expect(value).toEqual(20n);
+      });
     });
   });
 });
