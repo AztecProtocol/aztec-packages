63fd995c8d7518cd68d31f82fd631d258f8c3b14
Santiago Palladino <santiago@aztecprotocol.com>
2023-08-17 17:04:04 -0300

    Fix ordering by removing stack reverse

--- yarn-project/end-to-end/src/e2e_ordering.test.ts
+++ yarn-project/end-to-end/src/e2e_ordering.test.ts
@@ -38,8 +38,7 @@ describe('e2e_ordering', () => {
     const getChildStoredValue = () =>
       aztecRpcServer.getPublicStorageAt(child.address, new Fr(1)).then(x => toBigInt(x!));
 
-    // Transaction is dropped due to incorrect public call hash
-    // aztec:sequencer:public-processor Error processing tx 24bde0a0a97663544394135c1b7665ff84f5baf74a1bcd9305d431f25fd6bf00: Error: calculated public_call_hash (0x14f75518391d0170eab12a72d211820d4336a6d690e57339ac4442822805a1d7) does not match provided public_call_hash (0x0a266bd9f749d04f4c55cb92126dbc060ad0c94a36e31703cc8071f0a8bc1b14) at the top of the call stack
+    // Fails since current value at the end of execution is 10, not 20
     it.skip('orders public function execution requests when nested call is last', async () => {
       const actions: FunctionCall[] = [
         child.methods.pubSetValue(10).request(),
@@ -52,9 +51,7 @@ describe('e2e_ordering', () => {
       expect(logs).toEqual([[10], [20]].map(Buffer.from));
     });
 
-    // Transaction is dropped due to incorrect public call hash
-    // aztec:sequencer:public-processor Error processing tx 045ff26096541cda6683de3b98795ad4327eb0df50e9aa2c4f270be7a37bc1ff: Error: calculated public_call_hash (0x04974297d43d77cb75fd74d0c72594d6d9ec3ec20c644590216296dc7a0bc1e0) does not match provided public_call_hash (0x19180b1dd6a14efeba6bfdb48b80b1f5fd11c78a67d5336cf6473b9c53033a01) at the top of the call stack
-    it.skip('orders public function execution requests when nested call is first', async () => {
+    it('orders public function execution requests when nested call is first', async () => {
       const actions: FunctionCall[] = [
         parent.methods.enqueueCallToChild(child.address, child.methods.pubSetValue.selector, 10).request(),
         child.methods.pubSetValue(20).request(),
