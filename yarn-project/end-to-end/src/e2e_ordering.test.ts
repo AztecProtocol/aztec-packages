99897e1908130abc6cf085c180044cfc164e1b49
Santiago Palladino <santiago@aztecprotocol.com>
2023-08-18 11:15:00 -0300

    Fix ordering test

--- yarn-project/end-to-end/src/e2e_ordering.test.ts
+++ yarn-project/end-to-end/src/e2e_ordering.test.ts
@@ -1,11 +1,11 @@
 // Test suite for testing proper ordering of side effects
 import { AztecNodeService } from '@aztec/aztec-node';
 import { AztecRPCServer } from '@aztec/aztec-rpc';
-import { BatchCall, Wallet } from '@aztec/aztec.js';
-import { Fr, PublicCallRequest } from '@aztec/circuits.js';
+import { Wallet } from '@aztec/aztec.js';
+import { Fr } from '@aztec/circuits.js';
 import { toBigInt } from '@aztec/foundation/serialize';
 import { ChildContract, ParentContract } from '@aztec/noir-contracts/types';
-import { AztecRPC, FunctionCall, L2BlockL2Logs } from '@aztec/types';
+import { AztecRPC, L2BlockL2Logs } from '@aztec/types';
 
 import { setup } from './fixtures/utils.js';
 
@@ -29,56 +29,52 @@ describe('e2e_ordering', () => {
   describe('with parent and child contract', () => {
     let parent: ParentContract;
     let child: ChildContract;
+    let pubSetValueSelector: Buffer;
 
     beforeEach(async () => {
       parent = await ParentContract.deploy(wallet).send().deployed();
       child = await ChildContract.deploy(wallet).send().deployed();
+      pubSetValueSelector = child.methods.pubSetValue.selector;
     });
 
     describe('enqueued public calls ordering', () => {
-      const sendBatch = async (actions: FunctionCall[]) => {
-        const batch = new BatchCall(wallet, actions);
-        const tx = await batch.simulate();
-        await batch.send().wait();
-        const logs = await aztecRpcServer.getUnencryptedLogs(1, 10).then(L2BlockL2Logs.unrollLogs);
-        const value = await aztecRpcServer.getPublicStorageAt(child.address, new Fr(1)).then(x => toBigInt(x!));
-        return { tx, logs, value };
-      };
+      const nestedValue = 10n;
+      const directValue = 20n;
 
-      const callsToHashes = (enqueuedPublicCalls: PublicCallRequest[]) =>
-        Promise.all(enqueuedPublicCalls.map(call => call.toPublicCallStackItem().then(item => item.hash())));
+      const expectedOrders = {
+        enqueueCallsToChildWithNestedFirst: [nestedValue, directValue],
+        enqueueCallsToChildWithNestedLast: [directValue, nestedValue],
+      } as const;
 
-      it('orders public function execution requests when nested call is last', async () => {
-        const actions: FunctionCall[] = [
-          child.methods.pubSetValue(10).request(),
-          parent.methods.enqueueCallToChild(child.address, child.methods.pubSetValue.selector, 20).request(),
-        ];
+      it.each(['enqueueCallsToChildWithNestedFirst', 'enqueueCallsToChildWithNestedLast'] as const)(
+        'orders public function execution in %s',
+        async method => {
+          const expectedOrder = expectedOrders[method];
+          const action = parent.methods[method](child.address, pubSetValueSelector);
+          const tx = await action.simulate();
+          await action.send().wait();
 
-        const { tx, logs, value } = await sendBatch(actions);
-        const { enqueuedPublicFunctionCalls: enqueuedPublicCalls } = tx;
+          // There are two enqueued calls
+          const enqueuedPublicCalls = tx.enqueuedPublicFunctionCalls;
+          expect(enqueuedPublicCalls.length).toEqual(2);
 
-        expect(enqueuedPublicCalls.length).toEqual(2);
-        expect(tx.data.end.publicCallStack.slice(0, 2)).toEqual(await callsToHashes(enqueuedPublicCalls));
-        expect(enqueuedPublicCalls.map(c => c.args[0].toBigInt())).toEqual([20n, 10n]);
-        expect(logs).toEqual([[10], [20]].map(Buffer.from));
-        expect(value).toEqual(20n);
-      });
+          // The call stack hashes in the output of the kernel proof match the tx enqueuedPublicFunctionCalls
+          const hashes = await Promise.all(enqueuedPublicCalls.map(c => c.toPublicCallStackItem().then(i => i.hash())));
+          expect(tx.data.end.publicCallStack.slice(0, 2)).toEqual(hashes);
 
-      it('orders public function execution requests when nested call is first', async () => {
-        const actions: FunctionCall[] = [
-          parent.methods.enqueueCallToChild(child.address, child.methods.pubSetValue.selector, 10).request(),
-          child.methods.pubSetValue(20).request(),
-        ];
+          // The enqueued public calls are in the expected order based on the argument they set (stack is reversed!)
+          expect(enqueuedPublicCalls.map(c => c.args[0].toBigInt())).toEqual([...expectedOrder].reverse());
 
-        const { tx, logs, value } = await sendBatch(actions);
-        const { enqueuedPublicFunctionCalls: enqueuedPublicCalls } = tx;
+          // Logs are emitted in the expected order
+          const logs = await aztecRpcServer.getUnencryptedLogs(1, 10).then(L2BlockL2Logs.unrollLogs);
+          const expectedLogs = expectedOrder.map(x => Buffer.from([Number(x)]));
+          expect(logs).toEqual(expectedLogs);
 
-        expect(enqueuedPublicCalls.length).toEqual(2);
-        expect(tx.data.end.publicCallStack.slice(0, 2)).toEqual(await callsToHashes(enqueuedPublicCalls));
-        expect(enqueuedPublicCalls.map(c => c.args[0].toBigInt())).toEqual([20n, 10n]);
-        expect(logs).toEqual([[10], [20]].map(Buffer.from));
-        expect(value).toEqual(20n);
-      });
+          // The final value of the child is the last one set
+          const value = await aztecRpcServer.getPublicStorageAt(child.address, new Fr(1)).then(x => toBigInt(x!));
+          expect(value).toEqual(expectedOrder[1]);
+        },
+      );
     });
   });
 });
