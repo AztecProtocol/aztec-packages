6e10187d465a12183a20a70b291bb573756ff5f3
Koby <kobyhall@proton.me>
2023-08-22 11:31:40 +0200

    fix: test execution with explicit exit code

--- circuits/cpp/barretenberg/ts/bin-test/bin-test.sh
+++ circuits/cpp/barretenberg/ts/bin-test/bin-test.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+echo "NodeJS Version: $(node --version)"
+
 node ./test.js
+
+if [ $? -eq 0 ]; then
+  echo "Tests passed successfully."
+  exit 0
+else
+  echo "Tests failed."
+  exit 1
+fi
