efbddf84f027861261fae2052fc23806cdf5d7b3
Koby <kobyhall@proton.me>
2023-08-22 10:53:03 +0200

    fix: test execution

--- circuits/cpp/barretenberg/ts/bin-test/test.js
+++ circuits/cpp/barretenberg/ts/bin-test/test.js
@@ -16,7 +16,10 @@ const solvedWitnessLocation = path.join(scriptDirectory, 'target/witness.tr');
 const expectedBarretenbergWasmLocation = path.join(scriptDirectory, '../dest/barretenberg.wasm');
 const expectedBarretenbergWasmThreadsLocation = path.join(scriptDirectory, '../dest/barretenberg-threads.wasm');
 const possibleBarretenbergWasmLocation = path.join(scriptDirectory, '../../cpp/build-wasm/bin/barretenberg.wasm');
-const possibleBarretenbergWasmThreadsLocation = path.join(scriptDirectory, '../../cpp/build-wasm-threads/bin/barretenberg.wasm');
+const possibleBarretenbergWasmThreadsLocation = path.join(
+  scriptDirectory,
+  '../../cpp/build-wasm-threads/bin/barretenberg.wasm',
+);
 
 // Skips warnings, ie. experimental features
 process.removeAllListeners('warning');
@@ -26,12 +29,11 @@ function getScriptDirectory() {
   const modulePath = fileURLToPath(moduleURL);
   const scriptDirectory = path.dirname(modulePath);
 
-  return scriptDirectory
+  return scriptDirectory;
 }
 
 function copyBBWASMIfNotPresent(expectedLocation, possibleBuildLocation) {
-
-  if (!fs.existsSync(expectedLocation)) {    
+  if (!fs.existsSync(expectedLocation)) {
     try {
       fs.copyFileSync(possibleBuildLocation, expectedLocation);
       console.log(`${possibleBuildLocation} copied to ${expectedLocation}`);
@@ -41,111 +43,83 @@ function copyBBWASMIfNotPresent(expectedLocation, possibleBuildLocation) {
       throw error;
     }
   }
-
 }
 
 function throwMissingFile(filePath, message) {
-
   const fileExists = fs.existsSync(filePath);
   if (!fileExists) {
-    console.error(`File does not exist at ${filePath}, ${message}.`); 
+    console.error(`File does not exist at ${filePath}, ${message}.`);
     throw Error(`File does not exist at ${filePath}, ${message}.`);
   }
-
 }
 
 function checkArtifactsPresent() {
-
   copyBBWASMIfNotPresent(expectedBarretenbergWasmLocation, possibleBarretenbergWasmLocation);
   copyBBWASMIfNotPresent(expectedBarretenbergWasmThreadsLocation, possibleBarretenbergWasmThreadsLocation);
-  
+
   throwMissingFile(executionScript, "execute 'yarn build:ts:node' first");
 
   throwMissingFile(compiledNoirProgramLocation, "execute 'nargo compile' first");
 
   throwMissingFile(solvedWitnessLocation, "execute 'nargo execute' first");
+}
 
+function executeBBCli(command, expectedOutputTemplate, done) {
+  // const extendedEnv = Object.assign({}, process.env, {
+  //   DEBUG: '*',
+  // });
+  exec(
+    command,
+    {
+      cwd: scriptDirectory,
+      // env: extendedEnv,
+    },
+    function (error, stdout, stderr) {
+      if (error) {
+        done(error);
+        return;
+      }
+
+      // Assert that the stdout matches the expected output template
+      assert.match(stdout.trim(), expectedOutputTemplate);
+
+      // Since no error from execution and assert matched expected template 
+      // this is now understood as correct execution. 
+      done();
+    },
+  );
 }
 
 describe('Test bb subcommands', () => {
-
   before(checkArtifactsPresent);
 
-  it('get gate count using `bb gates` command', (done) => {
-    
-    const expectedOutputTemplate = /^gates: \d+$/;
-    const executionDirectory = getScriptDirectory();
+  it('get gate count using `bb gates` command', done => {
+    const expectedOutputTemplate = /^\d+$/gm;
     const command = `${executionScript} gates`;
 
-    exec(command, { cwd: executionDirectory }, function(error, stdout, stderr) {
-      if (error) {
-        done(error);
-        return;
-      }
-
-      // Assert that the stdout matches the expected output template
-      assert.match(stdout.trim(), expectedOutputTemplate);
-
-      done();
-    });
+    executeBBCli(command, expectedOutputTemplate, done);
   });
 
-  it('creates proof with `bb prove -o proof` command', (done) => {
-    
-    const expectedOutputTemplate = /^proof written to: proof$/;
-    const executionDirectory = getScriptDirectory();
+  it('creates proof with `bb prove -o proof` command', done => {
+    // We expect any character stream since binary print is result of this command
+    const expectedOutputTemplate = /.+/;
     const command = `${executionScript} prove -o proof`;
 
-    exec(command, { cwd: executionDirectory }, function(error, stdout, stderr) {
-      if (error) {
-        done(error);
-        return;
-      }
-
-      // Assert that the stdout matches the expected output template
-      assert.match(stdout.trim(), expectedOutputTemplate);
-
-      done();
-    });
+    executeBBCli(command, expectedOutputTemplate, done);
   });
 
-  it('writes verification key with `bb write_vk -o vk` command', (done) => {
-    
-    const expectedOutputTemplate = /^vk written to: vk$/;
-    const executionDirectory = getScriptDirectory();
+  it('writes verification key with `bb write_vk -o vk` command', done => {
+    // We expect any character stream since binary print is result of this command
+    const expectedOutputTemplate = /.+/;
     const command = `${executionScript} write_vk -o vk`;
 
-    exec(command, { cwd: executionDirectory }, function(error, stdout, stderr) {
-      if (error) {
-        done(error);
-        return;
-      }
-
-      // Assert that the stdout matches the expected output template
-      assert.match(stdout.trim(), expectedOutputTemplate);
-
-      done();
-    });
+    executeBBCli(command, expectedOutputTemplate, done);
   });
 
-  it('verifies proof with `bb verify -k vk -p proof` command', (done) => {
-    
-    const expectedOutputTemplate = /^verified: true$/;
-    const executionDirectory = getScriptDirectory();
+  it('verifies proof with `bb verify -k vk -p proof` command', done => {
+    const expectedOutputTemplate = /true/;
     const command = `${executionScript} verify -k vk -p proof`;
 
-    exec(command, { cwd: executionDirectory }, function(error, stdout, stderr) {
-      if (error) {
-        done(error);
-        return;
-      }
-
-      // Assert that the stdout matches the expected output template
-      assert.match(stdout.trim(), expectedOutputTemplate);
-
-      done();
-    });
+    executeBBCli(command, expectedOutputTemplate, done);
   });
-  
-  
 });
