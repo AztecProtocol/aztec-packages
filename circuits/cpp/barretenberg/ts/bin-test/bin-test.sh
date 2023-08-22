7ecf666dcd2bb9f23f093582ac6e93c51f423fde
Koby <kobyhall@proton.me>
2023-08-22 12:13:38 +0200

    fix: remove version check

--- circuits/cpp/barretenberg/ts/bin-test/bin-test.sh
+++ circuits/cpp/barretenberg/ts/bin-test/bin-test.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-echo "node --version"
-node --version
-
 node ./test.js
-
-if [ $? -eq 0 ]; then
-  echo "Tests passed successfully."
-  exit 0
-else
-  echo "Tests failed."
-  exit $?
-fi
