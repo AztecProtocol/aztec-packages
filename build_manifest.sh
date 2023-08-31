9d1a993213ee06e7d956fc3b0efeb048391ae241
Charlie Lye <karl.lye@gmail.com>
2023-08-31 08:16:04 +0000

    Let's try just testing bb.js tests at same time as build for simplicitiy. We have acir_tests still separate.

--- build_manifest.sh
+++ build_manifest.sh
@@ -2,10 +2,10 @@
 # Source this file to define the PROJECTS variable, needed by build_local, used by bootstrap_docker.sh.
 #
 # PROJECT elements have structure PROJECT_DIR_NAME:WORKING_DIR:DOCKERFILE:REPO.
-#  PROJECT_DIR_NAME: Should reflect the projects directory name.
+#  PROJECT_NAME: A name by which one can reference this project via the boostrap_docker.sh script.
 #  WORKING_DIR: Everything within this directory is copied into the docker context (excluding paths in .dockerignore).
 #  DOCKERFILE: Defaults to Dockerfile. However some projects have multiple build Dockerfiles located in subdirs.
-#  REPO: Defaults to <PROJECT_DIR_NAME>. The docker repository name, used to name the resulting docker image.
+#  REPO: Defaults to <PROJECT_NAME>. The docker repository name, used to name the resulting docker image.
 #
 # This file tells bootstrap_docker.sh which and in which order to build projects for locally testing the docker builds.
 # To check *most* of the build works as expected, we can just do the minimum to produce the e2e tests, and run them
