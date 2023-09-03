64f022e574ef7faab8926596f0c835a9fcc46321
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2023-09-03 16:17:16 +0000

    fix: always rebuild

--- docs/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Temporarily copy the docker ignore into the core context
-cp .dockerignore .. 
-
-# Build in the root context
-(cd .. && docker build . -f docs/Dockerfile)
-
-rm ../.dockerignore
