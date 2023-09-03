d3f8ff86e84c642b55812644b771ce5128eafd40
ludamad <adam.domurad@gmail.com>
2023-09-03 00:19:53 -0400

    fix: more content hash

--- circuits/cpp/barretenberg/cpp/scripts/bin-test.sh
+++ circuits/cpp/barretenberg/cpp/scripts/bin-test.sh
@@ -4,7 +4,10 @@ set -eu
 
 $(aws ecr get-login --region us-east-2 --no-include-email) 2> /dev/null
 
-IMAGE_URI=278380418400.dkr.ecr.us-east-2.amazonaws.com/barretenberg-x86_64-linux-clang-assert:cache-$COMMIT_HASH
+export PATH="$PATH:$(git rev-parse --show-toplevel)/build-system/scripts"
+REPOSITORY=barretenberg-x86_64-linux-clang-assert
+CONTENT_HASH=$(calculate_content_hash $REPOSITORY)
+IMAGE_URI=278380418400.dkr.ecr.us-east-2.amazonaws.com/$REPOSITORY:cache-$CONTENT_HASH
 
 docker pull $IMAGE_URI
 
