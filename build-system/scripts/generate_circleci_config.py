e761de6c09cefe6c9f972d05d79485c1faf78545
ludamad <adam.domurad@gmail.com>
2023-09-03 22:21:21 -0400

    First attempt at filtering circleci config

---
+++ build-system/scripts/generate_circleci_config.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import json
+import sys
+
+def remove_jobs_from_workflow(jobs, to_remove):
+    """
+    Removes jobs from a given CircleCI JSON workflow.
+    
+    Parameters:
+        jobs (dict): The JSON object representing the CircleCI workflow jobs dependencies portion.
+        to_remove (list): The list of jobs to be removed from the workflow.
+    
+    Returns:
+        dict: The new JSON object with specified jobs removed.
+    """
+
+    new_jobs = []
+    # Remove specified jobs
+    for job in jobs:
+        key = next(iter(job))
+        if key in to_remove:
+            continue
+        print(job[key])
+        # remove our filtered jobs from the dependency graph via the requires attribute
+        job[key]["requires"] = [r for r in job[key].get("requires", []) if r not in jobs_to_remove]
+        new_jobs.append(job)
+    return new_jobs
+
+if __name__ == '__main__':
+    # The CircleCI workflow as a JSON string (Replace this with your actual workflow)
+    workflow_json_str = sys.stdin.read()
+    
+    # Convert the JSON string to a Python dictionary
+    workflow_dict = json.loads(workflow_json_str)
+    
+    # List of jobs to remove
+    jobs_to_remove = ["e2e-sandbox-example", "e2e-multi-transfer-contract", "deploy-end"]
+    
+    # Remove the jobs and get the new workflow
+    workflow_dict["workflows"]["system"]["jobs"] = remove_jobs_from_workflow(workflow_dict["workflows"]["system"]["jobs"], jobs_to_remove)
+    workflow_dict["workflows"]["system"]["when"] = {"equal":["system","<< pipeline.parameters.workflow >>"]}
+    # Convert the new workflow back to JSON string
+    new_workflow_json_str = json.dumps(workflow_dict, indent=2)
+    print(new_workflow_json_str)
