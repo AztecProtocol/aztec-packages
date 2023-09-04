e0f1f45cbbed51f7c289c86c1ceb259549c8600b
Charlie Lye <karl.lye@gmail.com>
2023-09-04 19:35:32 +0000

    Back to master.

--- circuits/cpp/barretenberg/acir_tests/run_acir_tests.sh
+++ circuits/cpp/barretenberg/acir_tests/run_acir_tests.sh
@@ -7,7 +7,7 @@ set -eu
 BIN=${BIN:-../cpp/build/bin/bb}
 FLOW=${FLOW:-prove_and_verify}
 CRS_PATH=~/.bb-crs
-BRANCH=kw/fix-double-verify-proof
+BRANCH=master
 VERBOSE=${VERBOSE:-}
 NAMED_TEST=${1:-}
 
