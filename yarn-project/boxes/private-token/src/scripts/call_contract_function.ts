95faf3d37ba3ae5af9231351b55d9a4d13226f5c
Dan Lee <dan@aztecprotocol.com>
2023-09-05 15:53:56 -0700

    sketch of calling contract methods

--- yarn-project/boxes/private-token/src/scripts/call_contract_function.ts
+++ yarn-project/boxes/private-token/src/scripts/call_contract_function.ts
@@ -1,5 +1,6 @@
-import { AztecAddress, AztecRPC } from '@aztec/aztec.js';
+import { AztecAddress, AztecRPC, CompleteAddress, Contract, getSandboxAccountsWallet } from '@aztec/aztec.js';
 import { ContractAbi } from '@aztec/foundation/abi';
+import { convertArgs } from './arg_conversion.js';
 
 // REMOVE THIS
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -9,7 +10,21 @@ export async function callContractFunction(
   functionName: string,
   args: any,
   rpc: AztecRPC,
+  wallet: CompleteAddress,
 ) {
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  return AztecAddress.random();
+  // TODO: pass this in?
+  const realWallet = await getSandboxAccountsWallet(rpc);
+  // TODO: switch to the generated typescript class?
+  const contract = await Contract.at(address, abi, realWallet);
+
+  const functionAbi = abi.functions.find(f => f.name === functionName);
+  // false to skip the foundation encoder - need to look into why passing the address as an Fr fails on re-encoding
+  const typedArgs = convertArgs(functionAbi!, args, false);
+  // TODO: put in actual function instead of hardcoded test method
+  // const returnVal = await contract.methods[functionName]
+  const returnVal = await contract.methods
+    .transfer(...typedArgs)
+    .send({ origin: wallet.address })
+    .wait();
+  return returnVal;
 }
--- yarn-project/boxes/private-token/src/scripts/view_contract_function.ts
+++ yarn-project/boxes/private-token/src/scripts/view_contract_function.ts
@@ -1,4 +1,4 @@
-import { AztecAddress, AztecRPC, CompleteAddress, Contract, Fr, getSandboxAccountsWallet } from '@aztec/aztec.js';
+import { AztecAddress, AztecRPC, CompleteAddress, Contract, getSandboxAccountsWallet } from '@aztec/aztec.js';
 import { ContractAbi } from '@aztec/foundation/abi';
 import { convertArgs } from './arg_conversion.js';
 
@@ -12,21 +12,21 @@ export async function viewContractFunction(
   rpc: AztecRPC,
   wallet: CompleteAddress,
 ) {
-  // todo: pass this in?
+  // TODO: pass this in?
   const realWallet = await getSandboxAccountsWallet(rpc);
   // console.log('wallet', realWallet);
   const contract = await Contract.at(address, abi, realWallet);
 
   const functionAbi = abi.functions.find(f => f.name === functionName);
+  // false to skip the foundation encoder - need to look into why passing the address as an Fr fails on re-encoding
   const typedArgs = convertArgs(functionAbi!, args, false);
 
   // console.log('address buffer', wallet.address.toBuffer());
   console.log('viewing from wallet', wallet.address.toString());
 
   const balance = await contract.methods[functionName](...typedArgs).view({ from: wallet.address });
+  // hardcoded versions that work:
   // const balance = await contract.methods[functionName](wallet.address).view({ from: wallet.address });
   // const balance = await contract.methods.getBalance(wallet.address).view({ from: wallet.address });
   return balance;
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  return [Fr.random()];
 }
