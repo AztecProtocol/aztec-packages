7d9674c64c7760dad34959b949cd2fb7960f32fb
Dan Lee <dan@aztecprotocol.com>
2023-09-06 10:44:54 -0700

    upate build_manifest.json, add dockerfile and arg converter util

---
+++ yarn-project/boxes/private-token/src/scripts/arg_conversion.ts
@@ -0,0 +1,19 @@
+import { FunctionAbi, encodeArguments } from '@aztec/foundation/abi';
+// hack: addresses are stored as string in the form to avoid bigint compatibility issues with formik
+// convert those back to bigints before sending
+export function convertArgs(functionAbi: FunctionAbi, args: any, encode: boolean = true) {
+  const untypedArgs = functionAbi.parameters.map(param => {
+    // param => args[param.name],
+    switch (param.type.kind) {
+      case 'field':
+        return BigInt(args[param.name]);
+      default:
+        return args[param.name];
+    }
+  });
+  if (!encode) {
+    return untypedArgs;
+  }
+  const typedArgs = encodeArguments(functionAbi, untypedArgs);
+  return typedArgs;
+}
