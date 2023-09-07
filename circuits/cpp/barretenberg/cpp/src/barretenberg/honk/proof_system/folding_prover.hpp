07492897d6f3a3f407fc9f2d6b0cda03cc9cc47c
maramihali <mara@aztecprotocol.com>
2023-09-07 14:52:51 +0000

    even more wip

---
+++ circuits/cpp/barretenberg/cpp/src/barretenberg/honk/proof_system/folding_prover.hpp
@@ -0,0 +1,5 @@
+#include "barretenberg/proof_system/flavor/flavor.hpp"
+namespace proof_system::honk {
+template <UltraFlavor Flavor> class FoldingProver_ {};
+// the folding prover returns the new prover polynomials and the new public inputs(does the verifier do anything)
+} // namespace proof_system::honk
---
+++ circuits/cpp/barretenberg/cpp/src/barretenberg/honk/proof_system/folding_verifier.hpp
@@ -0,0 +1,4 @@
+#include "barretenberg/proof_system/flavor/flavor.hpp"
+namespace proof_system::honk {
+template <UltraFlavor Flavor> class FoldingVerifier_ {};
+} // namespace proof_system::honk
