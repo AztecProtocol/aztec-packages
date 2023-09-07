1fd2f8f004ddad8c491ebaff6c57fe5c3c1837cb
Dan Lee <dan@aztecprotocol.com>
2023-09-07 16:31:00 -0400

    wait instead of is_mined()

--- yarn-project/boxes/private-token/src/tests/sandbox.test.ts
+++ yarn-project/boxes/private-token/src/tests/sandbox.test.ts
@@ -4,7 +4,6 @@ import {
   Contract,
   GrumpkinScalar,
   Wallet,
-  createAztecRpcClient,
   getSandboxAccountsWallet,
 } from '@aztec/aztec.js';
 import { createDebugLogger } from '@aztec/foundation/log';
@@ -15,20 +14,15 @@ const logger = createDebugLogger('aztec:http-rpc-client');
 
 export const privateKey = GrumpkinScalar.fromString('ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80');
 
-const url = 'http://localhost:8080';
-
-const aztecRpcClient = createAztecRpcClient(url);
-let wallet: Wallet;
-
 const INITIAL_BALANCE = 333n;
 const SECONDARY_AMOUNT = 33n;
 
-async function deployZKContract(owner: AztecAddress) {
+async function deployZKContract(owner: AztecAddress, wallet: Wallet) {
   logger('Deploying L2 contract...');
-  const tx = PrivateTokenContract.deploy(aztecRpcClient, INITIAL_BALANCE, owner).send();
+  const tx = PrivateTokenContract.deploy(rpcClient, INITIAL_BALANCE, owner).send();
   const receipt = await tx.getReceipt();
   const contract = await PrivateTokenContract.at(receipt.contractAddress!, wallet);
-  await tx.isMined();
+  await tx.wait();
   logger('L2 contract deployed');
   return contract;
 }
@@ -44,11 +38,11 @@ describe('ZK Contract Tests', () => {
   let zkContract: Contract;
 
   beforeAll(async () => {
-    const wallet = await getSandboxAccountsWallet(rpcClient);
+    wallet = await getSandboxAccountsWallet(rpcClient);
     const accounts = await rpcClient.getAccounts();
     [owner, account2] = accounts;
 
-    zkContract = await deployZKContract(owner.address);
+    zkContract = await deployZKContract(owner.address, wallet);
   });
 
   test('Initial balance is correct', async () => {
@@ -58,14 +52,14 @@ describe('ZK Contract Tests', () => {
 
   test('Balance after mint is correct', async () => {
     const mintTx = zkContract.methods.mint(SECONDARY_AMOUNT, owner.address).send({ origin: owner.address });
-    await mintTx.isMined({ interval: 0.5 });
+    await mintTx.wait({ interval: 0.5 });
     const balanceAfterMint = await getBalance(zkContract, owner.address);
     expect(balanceAfterMint).toEqual(INITIAL_BALANCE + SECONDARY_AMOUNT);
   });
 
   test('Balance after transfer is correct for both sender and receiver', async () => {
     const transferTx = zkContract.methods.transfer(SECONDARY_AMOUNT, account2.address).send({ origin: owner.address });
-    await transferTx.isMined({ interval: 0.5 });
+    await transferTx.wait({ interval: 0.5 });
     const balanceAfterTransfer = await getBalance(zkContract, owner.address);
     const receiverBalance = await getBalance(zkContract, account2.address);
     expect(balanceAfterTransfer).toEqual(INITIAL_BALANCE + SECONDARY_AMOUNT - SECONDARY_AMOUNT);
