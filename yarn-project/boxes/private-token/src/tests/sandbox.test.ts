3ddb28b6b94e1c3cde263049f21cd2af9c6e36b8
Dan Lee <dan@aztecprotocol.com>
2023-09-12 16:41:18 -0400

    revert e2d dep in test, update test to use FE helpers

--- yarn-project/boxes/private-token/src/tests/sandbox.test.ts
+++ yarn-project/boxes/private-token/src/tests/sandbox.test.ts
@@ -1,23 +1,19 @@
-import { AztecNodeConfig, AztecNodeService, getConfigEnvVars } from '@aztec/aztec-node';
 import {
   AccountWallet,
   AztecAddress,
   AztecRPC,
-  CheatCodes,
   CompleteAddress,
   Contract,
-  EthCheatCodes,
   Fr,
   Wallet,
   createAztecRpcClient,
   makeFetch,
   waitForSandbox,
 } from '@aztec/aztec.js';
-import { DeployL1Contracts } from '@aztec/ethereum';
-import { DebugLogger, createDebugLogger } from '@aztec/foundation/log';
+import { createDebugLogger } from '@aztec/foundation/log';
 import { PrivateTokenContract } from '../artifacts/PrivateToken.js';
-import { deployContract } from '../scripts/deploy_contract.js';
-import { getWallet } from '../scripts/util.js';
+import { rpcClient } from '../config.js';
+import { callContractFunction, deployContract, getWallet, viewContractFunction } from '../scripts/index.js';
 
 const logger = createDebugLogger('aztec:http-rpc-client');
 
@@ -33,99 +29,16 @@ const INITIAL_BALANCE = getRandomInt(MIN_INITIAL_BALANCE, MAX_INITIAL_BALANCE);
 const TRANSFER_AMOUNT = 44n;
 const MINT_AMOUNT = 11n;
 
-/**
- * Sets up the environment for the end-to-end tests.
- * @param numberOfAccounts - The number of new accounts to be created once the RPC server is initiated.
- */
-export async function setup(
-  numberOfAccounts = 1,
-  stateLoad: string | undefined = undefined,
-): Promise<{
-  /**
-   * The Aztec Node service.
-   */
-  aztecNode: AztecNodeService | undefined;
-  /**
-   * The Aztec RPC server.
-   */
-  aztecRpcServer: AztecRPC;
-  /**
-   * Return values from deployL1Contracts function.
-   */
-  deployL1ContractsValues: DeployL1Contracts;
-  /**
-   * The accounts created by the RPC server.
-   */
-  accounts: CompleteAddress[];
-  /**
-   * The Aztec Node configuration.
-   */
-  config: AztecNodeConfig;
-  /**
-   * The first wallet to be used.
-   */
-  wallet: AccountWallet;
-  /**
-   * The wallets to be used.
-   */
-  wallets: AccountWallet[];
-  /**
-   * Logger instance named as the current test.
-   */
-  logger: DebugLogger;
-  /**
-   * The cheat codes.
-   */
-  cheatCodes: CheatCodes;
-}> {
-  const config = getConfigEnvVars();
-
-  if (stateLoad) {
-    const ethCheatCodes = new EthCheatCodes(config.rpcUrl);
-    await ethCheatCodes.loadChainState(stateLoad);
-  }
-
-  const logger = getLogger();
-  const hdAccount = mnemonicToAccount(MNEMONIC);
-
-  const deployL1ContractsValues = await setupL1Contracts(config.rpcUrl, hdAccount, logger);
-  const privKeyRaw = hdAccount.getHdKey().privateKey;
-  const publisherPrivKey = privKeyRaw === null ? null : Buffer.from(privKeyRaw);
-
-  config.publisherPrivateKey = `0x${publisherPrivKey!.toString('hex')}`;
-  config.rollupContract = deployL1ContractsValues.rollupAddress;
-  config.contractDeploymentEmitterContract = deployL1ContractsValues.contractDeploymentEmitterAddress;
-  config.inboxContract = deployL1ContractsValues.inboxAddress;
-
-  const aztecNode = await createAztecNode(config, logger);
-
-  const { aztecRpcServer, accounts, wallets } = await setupAztecRPCServer(numberOfAccounts, aztecNode, logger);
-
-  const cheatCodes = await CheatCodes.create(config.rpcUrl, aztecRpcServer!);
-
-  return {
-    aztecNode,
-    aztecRpcServer,
-    deployL1ContractsValues,
-    accounts,
-    config,
-    wallet: wallets[0],
-    wallets,
-    logger,
-    cheatCodes,
-  };
-}
-
+// assumes sandbox is running locally, which this script does not trigger
 const setupSandbox = async () => {
   const { SANDBOX_URL = 'http://localhost:8080' } = process.env;
-  await setup(3);
   const aztecRpc = createAztecRpcClient(SANDBOX_URL, makeFetch([1, 2, 3], true));
   await waitForSandbox(aztecRpc);
   return aztecRpc;
 };
 
 async function deployZKContract(owner: CompleteAddress, wallet: Wallet, rpcClient: AztecRPC) {
-  logger('Deploying L2 contract...');
+  logger('Deploying PrivateToken contract...');
   // const constructorFunctionAbi = PrivateTokenContract.abi.functions.find(f => f.name === 'constructor');
   const constructorArgs = {
     // eslint-disable-next-line camelcase
@@ -139,8 +52,47 @@ async function deployZKContract(owner: CompleteAddress, wallet: Wallet, rpcClien
   return contract;
 }
 
-async function getBalance(contract: Contract, ownerAddress: AztecAddress) {
-  return await contract.methods.getBalance(ownerAddress).view({ from: ownerAddress });
+async function getBalance(contractAddress: AztecAddress, privateTokenContract: Contract, owner: CompleteAddress) {
+  return await viewContractFunction(
+    contractAddress,
+    privateTokenContract.abi,
+    'getBalance',
+    [owner.address],
+    rpcClient,
+    owner,
+  );
+}
+
+async function mint(
+  contractAddress: AztecAddress,
+  privateTokenContract: Contract,
+  from: CompleteAddress,
+  to: CompleteAddress,
+) {
+  return await callContractFunction(
+    contractAddress,
+    privateTokenContract.abi,
+    'mint',
+    [MINT_AMOUNT, to.address],
+    rpcClient,
+    from,
+  );
+}
+
+async function transfer(
+  contractAddress: AztecAddress,
+  privateTokenContract: Contract,
+  from: CompleteAddress,
+  to: CompleteAddress,
+) {
+  return await callContractFunction(
+    contractAddress,
+    privateTokenContract.abi,
+    'transfer',
+    [TRANSFER_AMOUNT, to.address],
+    rpcClient,
+    from,
+  );
 }
 
 describe('ZK Contract Tests', () => {
@@ -149,6 +101,7 @@ describe('ZK Contract Tests', () => {
   let account2: CompleteAddress;
   let _account3: CompleteAddress;
   let privateTokenContract: Contract;
+  let contractAddress: AztecAddress;
   let rpcClient: AztecRPC;
 
   beforeAll(async () => {
@@ -159,29 +112,30 @@ describe('ZK Contract Tests', () => {
     wallet = await getWallet(owner, rpcClient);
 
     privateTokenContract = await deployZKContract(owner, wallet, rpcClient);
+    contractAddress = privateTokenContract.address;
   }, 60000);
 
   test('Initial balance is correct', async () => {
-    const balance = await getBalance(privateTokenContract, owner.address);
+    const balance = await getBalance(contractAddress, privateTokenContract, owner);
     expect(balance).toEqual(INITIAL_BALANCE);
   }, 40000);
 
   test('Balance after mint is correct', async () => {
-    const mintTx = privateTokenContract.methods.mint(MINT_AMOUNT, owner.address).send();
-    await mintTx.wait({ interval: 0.5 });
+    const mintTx = mint(contractAddress, privateTokenContract, owner, owner);
+    await mintTx;
 
-    const balanceAfterMint = await getBalance(privateTokenContract, owner.address);
+    const balanceAfterMint = await getBalance(contractAddress, privateTokenContract, owner);
     expect(balanceAfterMint).toEqual(INITIAL_BALANCE + MINT_AMOUNT);
   }, 40000);
 
   test('Balance after transfer is correct for both sender and receiver', async () => {
-    const transferTx = privateTokenContract.methods.transfer(TRANSFER_AMOUNT, account2.address).send();
-    await transferTx.wait({ interval: 0.5 });
+    const transferTx = transfer(contractAddress, privateTokenContract, owner, account2);
+    await transferTx;
 
-    const balanceAfterTransfer = await getBalance(privateTokenContract, owner.address);
+    const balanceAfterTransfer = await getBalance(contractAddress, privateTokenContract, owner);
     expect(balanceAfterTransfer).toEqual(INITIAL_BALANCE + MINT_AMOUNT - TRANSFER_AMOUNT);
 
-    const receiverBalance = await getBalance(privateTokenContract, account2.address);
+    const receiverBalance = await getBalance(contractAddress, privateTokenContract, account2);
     expect(receiverBalance).toEqual(TRANSFER_AMOUNT);
   }, 40000);
 });
