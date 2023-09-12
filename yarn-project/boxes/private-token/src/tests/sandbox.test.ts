35793e0aaa30fb008102e5eef59e7295851347a1
Dan Lee <dan@aztecprotocol.com>
2023-09-12 16:18:00 -0400

    try copying setup from end-to-end

--- yarn-project/boxes/private-token/src/tests/sandbox.test.ts
+++ yarn-project/boxes/private-token/src/tests/sandbox.test.ts
@@ -1,17 +1,20 @@
+import { AztecNodeConfig, AztecNodeService, getConfigEnvVars } from '@aztec/aztec-node';
 import {
   AccountWallet,
   AztecAddress,
   AztecRPC,
+  CheatCodes,
   CompleteAddress,
   Contract,
+  EthCheatCodes,
   Fr,
   Wallet,
   createAztecRpcClient,
   makeFetch,
   waitForSandbox,
 } from '@aztec/aztec.js';
-import { setup } from '@aztec/end-to-end';
-import { createDebugLogger } from '@aztec/foundation/log';
+import { DeployL1Contracts } from '@aztec/ethereum';
+import { DebugLogger, createDebugLogger } from '@aztec/foundation/log';
 import { PrivateTokenContract } from '../artifacts/PrivateToken.js';
 import { deployContract } from '../scripts/deploy_contract.js';
 import { getWallet } from '../scripts/util.js';
@@ -29,6 +32,89 @@ const MAX_INITIAL_BALANCE = 600;
 const INITIAL_BALANCE = getRandomInt(MIN_INITIAL_BALANCE, MAX_INITIAL_BALANCE);
 const TRANSFER_AMOUNT = 44n;
 const MINT_AMOUNT = 11n;
+
+/**
+ * Sets up the environment for the end-to-end tests.
+ * @param numberOfAccounts - The number of new accounts to be created once the RPC server is initiated.
+ */
+export async function setup(
+  numberOfAccounts = 1,
+  stateLoad: string | undefined = undefined,
+): Promise<{
+  /**
+   * The Aztec Node service.
+   */
+  aztecNode: AztecNodeService | undefined;
+  /**
+   * The Aztec RPC server.
+   */
+  aztecRpcServer: AztecRPC;
+  /**
+   * Return values from deployL1Contracts function.
+   */
+  deployL1ContractsValues: DeployL1Contracts;
+  /**
+   * The accounts created by the RPC server.
+   */
+  accounts: CompleteAddress[];
+  /**
+   * The Aztec Node configuration.
+   */
+  config: AztecNodeConfig;
+  /**
+   * The first wallet to be used.
+   */
+  wallet: AccountWallet;
+  /**
+   * The wallets to be used.
+   */
+  wallets: AccountWallet[];
+  /**
+   * Logger instance named as the current test.
+   */
+  logger: DebugLogger;
+  /**
+   * The cheat codes.
+   */
+  cheatCodes: CheatCodes;
+}> {
+  const config = getConfigEnvVars();
+
+  if (stateLoad) {
+    const ethCheatCodes = new EthCheatCodes(config.rpcUrl);
+    await ethCheatCodes.loadChainState(stateLoad);
+  }
+
+  const logger = getLogger();
+  const hdAccount = mnemonicToAccount(MNEMONIC);
+
+  const deployL1ContractsValues = await setupL1Contracts(config.rpcUrl, hdAccount, logger);
+  const privKeyRaw = hdAccount.getHdKey().privateKey;
+  const publisherPrivKey = privKeyRaw === null ? null : Buffer.from(privKeyRaw);
+
+  config.publisherPrivateKey = `0x${publisherPrivKey!.toString('hex')}`;
+  config.rollupContract = deployL1ContractsValues.rollupAddress;
+  config.contractDeploymentEmitterContract = deployL1ContractsValues.contractDeploymentEmitterAddress;
+  config.inboxContract = deployL1ContractsValues.inboxAddress;
+
+  const aztecNode = await createAztecNode(config, logger);
+
+  const { aztecRpcServer, accounts, wallets } = await setupAztecRPCServer(numberOfAccounts, aztecNode, logger);
+
+  const cheatCodes = await CheatCodes.create(config.rpcUrl, aztecRpcServer!);
+
+  return {
+    aztecNode,
+    aztecRpcServer,
+    deployL1ContractsValues,
+    accounts,
+    config,
+    wallet: wallets[0],
+    wallets,
+    logger,
+    cheatCodes,
+  };
+}
 
 const setupSandbox = async () => {
   const { SANDBOX_URL = 'http://localhost:8080' } = process.env;
