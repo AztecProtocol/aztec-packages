63a7d3c387a56dc88c91be062c6f14e1c683b580
Charlie Lye <karl.lye@gmail.com>
2023-09-17 21:18:56 +0000

    wip

--- build-system/cci_gen/src/index.ts
+++ build-system/cci_gen/src/index.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-import { parseDocument, YAMLMap, Pair, Scalar, Alias } from "yaml";
+import { parseDocument, YAMLMap, Pair, Scalar, Alias, YAMLSeq } from "yaml";
 
 function toCircleJob(spot: boolean) {
   return {
@@ -28,7 +28,7 @@ function toBuildJob(job: string, { spot }: any) {
   run.name = "Build";
   run.command = spot ? `cond_spot_run_build ${job} ${spot}` : `build ${job}`;
 
-  const jobNode = parseDocument(JSON.stringify(jobObject)).toJSON();
+  const jobNode: any = jobObject;
   jobNode.steps[0] = new Alias("checkout");
   jobNode.steps[1] = new Alias("setup_env");
 
@@ -43,11 +43,20 @@ function toTestJob(job: string, { spot, command }: any) {
     ? `cond_spot_run_test ${job} ${spot} ${command}`
     : `cond_run_test ${job} ${command}`;
 
-  const jobNode = parseDocument(JSON.stringify(jobObject)).toJSON();
+  const jobNode: any = jobObject;
   jobNode.steps[0] = new Alias("checkout");
   jobNode.steps[1] = new Alias("setup_env");
 
   return jobNode;
+}
+
+function toWorkflowJob(job: string, requires: string[]) {
+  return {
+    [`${job}`]: {
+      requires,
+      "<<": new Alias("defaults"),
+    },
+  };
 }
 
 function updateConfig() {
@@ -57,14 +66,19 @@ function updateConfig() {
 
   // Step 2: Read and parse the config.yml file to an AST
   const configYamlContent = fs.readFileSync(
-    "../../.circleci/config.yml",
+    "../../.circleci/config.template.yml",
     "utf-8"
   );
-  const configYamlDoc = parseDocument(configYamlContent);
+  const configYamlDoc: any = parseDocument(configYamlContent);
+  configYamlDoc.commentBefore =
+    " WARNING: THIS IS AUTO-GENERATED FROM config.template.yml AND build-system/cci_gen! DO NOT MODIFY DIRECTLY!\n \n" +
+    configYamlDoc.commentBefore;
 
   // Step 3: Populate jobs node in the AST with the new job entries.
   const jobsNode = new YAMLMap();
   configYamlDoc.set("jobs", jobsNode);
+
+  const workflowJobs = new YAMLSeq();
 
   for (const [index, [job, json]] of Object.entries(
     yamlObject
@@ -72,24 +86,29 @@ function updateConfig() {
     const k = new Scalar(job);
     k.spaceBefore = index > 0;
     jobsNode.items.push(new Pair(k, toBuildJob(job, json)));
+    workflowJobs.items.push(toWorkflowJob(job, json.dependencies));
 
     if (json.tests) {
-      for (const [index, [testName, test]] of Object.entries(
-        json.tests
-      ).entries()) {
+      for (const [testName, test] of Object.entries(json.tests)) {
         const k = new Scalar(testName);
         k.spaceBefore = true;
         jobsNode.items.push(new Pair(k, toTestJob(job, test)));
+        workflowJobs.items.push(toWorkflowJob(testName, [job]));
       }
     }
   }
+
+  ((configYamlDoc.get("workflows") as YAMLMap).get("system") as YAMLMap).set(
+    "jobs",
+    workflowJobs
+  );
 
   // Step 4: Stringify the modified AST back to a YAML string
   const newYamlContent = configYamlDoc.toString({ lineWidth: 0 });
   // const newYamlContent = String(configYamlDoc);
 
   // Step 5: Write the modified YAML string back to the config.yml file
-  fs.writeFileSync("../../.circleci/config.new.yml", newYamlContent, "utf-8");
+  fs.writeFileSync("../../.circleci/config.yml", newYamlContent, "utf-8");
 }
 
 updateConfig();
