61b2b80076c9fa73e910d9c001c14a8d3e5be0ac
Charlie Lye <karl.lye@gmail.com>
2023-09-17 10:21:34 +0000

    wip

--- build-system/cci_gen/src/index.ts
+++ build-system/cci_gen/src/index.ts
@@ -9,7 +9,7 @@ function toCircleJob(spot: boolean) {
           resource_class: "small",
         }
       : {
-          machine: [{ image: "ubuntu-2004:202010-01" }],
+          machine: [{ image: "ubuntu-2204:2023.07.2" }],
           resource_class: "large",
         }),
     steps: [
