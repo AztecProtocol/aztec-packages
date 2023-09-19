23ec461a60bf55d25c0d1810af9974c84836a849
Santiago Palladino <santiago@aztecprotocol.com>
2023-09-19 16:33:07 -0300

    Wait for sandbox before running test

--- yarn-project/end-to-end/src/guides/up_quick_start.test.ts
+++ yarn-project/end-to-end/src/guides/up_quick_start.test.ts
@@ -1,8 +1,11 @@
+import { waitForSandbox } from '@aztec/aztec.js';
+
 import { execSync } from 'child_process';
 
 // Entrypoint for running the up-quick-start script on the CI
 describe('guides/up_quick_start', () => {
-  it('works', () => {
+  it('works', async () => {
+    await waitForSandbox();
     execSync(`PATH=$PATH:../node_modules/.bin ./src/guides/up_quick_start.sh`, { shell: '/bin/bash', stdio: 'pipe' });
   }, 90_000);
 });
