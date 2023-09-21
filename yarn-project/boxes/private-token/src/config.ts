14f8b4f38ee91607bf06021ce950c05233d402c0
Dan Lee <dan@aztecprotocol.com>
2023-09-21 15:19:32 +0100

    small cleanups

--- yarn-project/boxes/private-token/src/config.ts
+++ yarn-project/boxes/private-token/src/config.ts
@@ -1,8 +1,8 @@
 import { AztecRPC, createAztecRpcClient } from '@aztec/aztec.js';
 import { ContractAbi } from '@aztec/foundation/abi';
-import { TokenContractAbi } from './artifacts/token.js'; // update this if using a different contract
+import { PrivateTokenContractAbi } from './artifacts/private_token.js'; // update this if using a different contract
 
-export const contractAbi: ContractAbi = TokenContractAbi;
+export const contractAbi: ContractAbi = PrivateTokenContractAbi;
 
 export const SANDBOX_URL: string = process.env.SANDBOX_URL || 'http://localhost:8080';
 export const rpcClient: AztecRPC = createAztecRpcClient(SANDBOX_URL);
