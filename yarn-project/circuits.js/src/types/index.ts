61d346fb97c71848eabd0f596e126bba5b7a7e09
benesjan <janbenes1234@gmail.com>
2023-09-22 13:24:11 +0000

    redundant import fix

--- yarn-project/circuits.js/src/types/index.ts
+++ yarn-project/circuits.js/src/types/index.ts
@@ -1,5 +1,4 @@
 export * from './contract_function_dao.js';
-export { CompleteAddress } from '../structs/index.js';
 export * from './deployment_info.js';
 export * from './partial_address.js';
 export * from './grumpkin_private_key.js';
