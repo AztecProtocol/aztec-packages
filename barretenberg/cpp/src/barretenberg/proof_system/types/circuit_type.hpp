103972e2685ba06b5eca026a7deb7dfad71d25cb
ledwards2225 <l.edwards.d@gmail.com>
2023-09-25 15:31:14 +0000

    issue todo for explicity enum vals

--- barretenberg/cpp/src/barretenberg/proof_system/types/circuit_type.hpp
+++ barretenberg/cpp/src/barretenberg/proof_system/types/circuit_type.hpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 
 namespace proof_system {
+// TODO(#731): Changing the explicit value of these enum elements breaks brittle and outdated tests in circuits/cpp.
 enum class CircuitType : uint32_t { STANDARD = 0, ULTRA = 2, UNDEFINED = 3 };
 
 template <typename T, typename... U>
