fecbc51410e9d0f8be36e9e60589c166a652598f
Dan Lee <dan@aztecprotocol.com>
2023-10-03 13:53:23 +0100

    add new blank contract

--- yarn-project/noir-contracts/src/scripts/copy_output.ts
+++ yarn-project/noir-contracts/src/scripts/copy_output.ts
@@ -20,6 +20,7 @@ const PROJECT_CONTRACTS = [
   { name: 'SchnorrSingleKeyAccount', target: '../aztec.js/src/abis/', exclude: [] },
   { name: 'SchnorrAccount', target: '../aztec.js/src/abis/', exclude: [] },
   { name: 'EcdsaAccount', target: '../aztec.js/src/abis/', exclude: [] },
+  { name: 'PrivateToken', target: '../boxes/private-token/src/artifacts', exclude: [] },
 ];
 
 const INTERFACE_CONTRACTS = ['private_token', 'private_token_airdrop', 'non_native_token', 'test'];
