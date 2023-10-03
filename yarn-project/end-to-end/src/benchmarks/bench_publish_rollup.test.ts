26660eb82ef8bbee608957a382ac96c3d397831c
benesjan <janbenes1234@gmail.com>
2023-10-03 13:59:31 +0000

    test fix

--- yarn-project/end-to-end/src/benchmarks/bench_publish_rollup.test.ts
+++ yarn-project/end-to-end/src/benchmarks/bench_publish_rollup.test.ts
@@ -1,7 +1,9 @@
 /* eslint-disable camelcase */
+import { AztecNodeService } from '@aztec/aztec-node';
 import { AztecAddress } from '@aztec/aztec.js';
 import { sleep } from '@aztec/foundation/sleep';
 import { TokenContract } from '@aztec/noir-contracts/types';
+import { SequencerClient } from '@aztec/sequencer-client';
 
 import times from 'lodash.times';
 
@@ -14,13 +16,18 @@ describe('benchmarks/publish_rollup', () => {
   let token: TokenContract;
   let owner: AztecAddress;
   let recipient: AztecAddress;
+  let sequencer: SequencerClient;
 
   beforeEach(async () => {
     context = await setup(2, { maxTxsPerBlock: 1024 });
+
+    if (!(context.aztecNode instanceof AztecNodeService)) throw new Error('Aztec node is not a service');
+    sequencer = context.aztecNode!.getSequencer();
+
     [owner, recipient] = context.accounts.map(a => a.address);
     token = await TokenContract.deploy(context.wallet, owner).send().deployed();
     await token.methods.mint_public(owner, 10000n).send().wait();
-    await context.aztecNode?.getSequencer().stop();
+    await sequencer.stop();
   }, 60_000);
 
   it.each(ROLLUP_SIZES)(
@@ -36,7 +43,7 @@ describe('benchmarks/publish_rollup', () => {
       // And then wait a bit more just in case
       await sleep(100);
       // Restart sequencer to process all txs together
-      context.aztecNode?.getSequencer().restart();
+      sequencer.restart();
       // Wait for the last tx to be processed
       await sentTxs[sentTxs.length - 1].wait({ timeout: 600_00 });
     },
