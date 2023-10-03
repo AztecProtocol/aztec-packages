26a5f75a57db6cee070f0d3d73a894fdc21e356c
zac-williamson <blorktronics@gmail.com>
2023-10-03 10:53:52 +0000

    wip

--- circuits/cpp/src/aztec3/utils/types/circuit_types.hpp
+++ circuits/cpp/src/aztec3/utils/types/circuit_types.hpp
@@ -60,23 +60,12 @@ template <typename Builder> struct CircuitTypes {
         return plonk::stdlib::pedersen_commitment<Builder>::hash(inputs_vec, hash_index);
     }
 
-    template <size_t SIZE> static fr hash(std::array<fr, SIZE> const& inputs, const size_t hash_index = 0)
-    {
-        std::vector<fr> const inputs_vec(std::begin(inputs), std::end(inputs));
-        return plonk::stdlib::pedersen_plookup_commitment<Builder>::hash(inputs_vec, hash_index);
-    }
-
     static fr hash(std::vector<fr> const& inputs,
                    std::vector<size_t> const& hash_sub_indices,
                    const size_t hash_index = 0)
     {
         return plonk::stdlib::pedersen_commitment<Builder>::hash(inputs, hash_sub_indices, hash_index);
     }
-
-    static fr hash(const std::vector<std::pair<fr, crypto::generators::generator_index_t>>& input_pairs)
-    {
-        return plonk::stdlib::pedersen_commitment<Builder>::hash(input_pairs);
-    };
 
     /**
      * @brief Compute the hash for a pair of left and right nodes in a merkle tree.
@@ -97,11 +86,6 @@ template <typename Builder> struct CircuitTypes {
     static grumpkin_point commit(const std::vector<fr>& inputs, const size_t hash_index = 0)
     {
         return plonk::stdlib::pedersen_commitment<Builder>::commit(inputs, hash_index);
-    };
-
-    static grumpkin_point commit(const std::vector<std::pair<fr, crypto::generators::generator_index_t>>& input_pairs)
-    {
-        return plonk::stdlib::pedersen_commitment<Builder>::commit(input_pairs);
     };
 
     static byte_array blake2s(const byte_array& input) { return plonk::stdlib::blake2s(input); }
