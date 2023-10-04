6a1781f1e5d3f9447cecd353a5dcf4c13f08c6bd
ledwards2225 <l.edwards.d@gmail.com>
2023-10-04 21:28:38 +0000

    cleanup

--- barretenberg/cpp/src/barretenberg/honk/pcs/zeromorph/zeromorph.test.cpp
+++ barretenberg/cpp/src/barretenberg/honk/pcs/zeromorph/zeromorph.test.cpp
@@ -262,12 +262,12 @@ TYPED_TEST(ZeroMorphTest, ProveAndVerifySingle)
     using Fr = typename TypeParam::ScalarField;
     using Commitment = typename TypeParam::AffineElement;
     using ZeroMorphProver = ZeroMorphProver<TypeParam>;
-    size_t n = 16;
-    size_t log_n = numeric::get_msb(n);
+    size_t N = 16;
+    size_t log_N = numeric::get_msb(N);
 
     // Construct a random multilinear polynomial f, and (u,v) such that f(u) = v.
-    auto f_polynomial = this->random_polynomial(n);
-    auto u_challenge = this->random_evaluation_point(log_n);
+    auto f_polynomial = this->random_polynomial(N);
+    auto u_challenge = this->random_evaluation_point(log_N);
     auto v_evaluation = f_polynomial.evaluate_mle(u_challenge);
     auto f_commitment = this->commit(f_polynomial);
 
@@ -281,8 +281,8 @@ TYPED_TEST(ZeroMorphTest, ProveAndVerifySingle)
 
         // Compute and send commitments C_{q_k} = [q_k], k = 0,...,d-1
         std::vector<Commitment> q_k_commitments;
-        q_k_commitments.reserve(log_n);
-        for (size_t idx = 0; idx < log_n; ++idx) {
+        q_k_commitments.reserve(log_N);
+        for (size_t idx = 0; idx < log_N; ++idx) {
             q_k_commitments[idx] = this->commit(quotients[idx]);
             std::string label = "ZM:C_q_" + std::to_string(idx);
             prover_transcript.send_to_verifier(label, q_k_commitments[idx]);
@@ -292,7 +292,7 @@ TYPED_TEST(ZeroMorphTest, ProveAndVerifySingle)
         auto y_challenge = prover_transcript.get_challenge("ZM:y");
 
         // Compute the batched, lifted-degree quotient \hat{q}
-        auto batched_quotient = ZeroMorphProver::compute_batched_lifted_degree_quotient(quotients, y_challenge, n);
+        auto batched_quotient = ZeroMorphProver::compute_batched_lifted_degree_quotient(quotients, y_challenge, N);
 
         // Compute and send the commitment C_q = [\hat{q}]
         auto q_commitment = this->commit(batched_quotient);
@@ -324,8 +324,8 @@ TYPED_TEST(ZeroMorphTest, ProveAndVerifySingle)
     {
         // Receive commitments [q_k]
         std::vector<Commitment> C_q_k;
-        C_q_k.reserve(log_n);
-        for (size_t i = 0; i < log_n; ++i) {
+        C_q_k.reserve(log_N);
+        for (size_t i = 0; i < log_N; ++i) {
             C_q_k.emplace_back(
                 verifier_transcript.template receive_from_prover<Commitment>("ZM:C_q_" + std::to_string(i)));
         }
@@ -340,15 +340,15 @@ TYPED_TEST(ZeroMorphTest, ProveAndVerifySingle)
         auto [x_challenge, z_challenge] = verifier_transcript.get_challenges("ZM:x", "ZM:z");
 
         // Compute commitment C_{v,x}
-        auto C_v_x = Commitment::one() * v_evaluation * this->Phi(x_challenge, log_n);
+        auto C_v_x = Commitment::one() * v_evaluation * this->Phi(x_challenge, log_N);
 
         // Compute commitment C_{\zeta_x}
         auto C_zeta_x = C_q;
-        for (size_t k = 0; k < log_n; ++k) {
-            size_t deg_k = (1ULL << k) - 1;
+        for (size_t k = 0; k < log_N; ++k) {
+            auto deg_k = static_cast<size_t>((1 << k) - 1);
             // Compute scalar y^k * x^{N - deg_k - 1}
             auto scalar = y_challenge.pow(k);
-            scalar *= x_challenge.pow(n - deg_k - 1);
+            scalar *= x_challenge.pow(N - deg_k - 1);
             scalar *= Fr(-1);
 
             C_zeta_x = C_zeta_x + C_q_k[k] * scalar;
@@ -356,12 +356,12 @@ TYPED_TEST(ZeroMorphTest, ProveAndVerifySingle)
 
         // Compute commitment C_{Z_x}
         auto C_Z_x = f_commitment + C_v_x * Fr(-1); // C_f - C_{v,x}
-        for (size_t k = 0; k < log_n; ++k) {
+        for (size_t k = 0; k < log_N; ++k) {
             // Compute scalar x^{2^k} * \Phi_{n-k-1}(x^{2^{k+1}}) - u_k *  \Phi_{n-k}(x^{2^k})
             auto x_pow_2k = x_challenge.pow(1 << k);         // x^{2^k}
             auto x_pow_2kp1 = x_challenge.pow(1 << (k + 1)); // x^{2^{k+1}}
-            auto scalar = x_pow_2k * this->Phi(x_pow_2kp1, log_n - k - 1);
-            scalar -= u_challenge[k] * this->Phi(x_pow_2k, log_n - k);
+            auto scalar = x_pow_2k * this->Phi(x_pow_2kp1, log_N - k - 1);
+            scalar -= u_challenge[k] * this->Phi(x_pow_2k, log_N - k);
             scalar *= Fr(-1);
 
             C_Z_x = C_Z_x + C_q_k[k] * scalar;
@@ -383,217 +383,5 @@ TYPED_TEST(ZeroMorphTest, ProveAndVerifySingle)
         EXPECT_TRUE(verified);
     }
 }
-
-/**
- * @brief Full Prover/Verifier protocol for proving multilinear evaluation f(u) = v
- *
- */
-// TYPED_TEST(ZeroMorphTest, SingleZxOnly)
-// {
-//     using Fr = typename TypeParam::ScalarField;
-//     using Commitment = typename TypeParam::AffineElement;
-//     using ZeroMorphProver = ZeroMorphProver<TypeParam>;
-//     size_t n = 16;
-//     size_t log_n = numeric::get_msb(n);
-
-//     // Construct a random multilinear polynomial f, and (u,v) such that f(u) = v.
-//     auto multilinear_f = this->random_polynomial(n);
-//     auto u_challenge = this->random_evaluation_point(log_n);
-//     auto v_evaluation = multilinear_f.evaluate_mle(u_challenge);
-
-//     // Initialize an empty ProverTranscript
-//     auto prover_transcript = ProverTranscript<Fr>::init_empty();
-
-//     // Execute Prover protocol
-
-//     // WORKTODO: this is a little funny. [f] will have been sent previously. It must be in hash used to generate u.
-//     // Compute and send commitment C = [f] and evaluation v = f(u)
-//     Commitment f_commitment = this->commit(multilinear_f);
-//     prover_transcript.send_to_verifier("ZM:C", f_commitment);
-//     prover_transcript.send_to_verifier("ZM:v", v_evaluation);
-
-//     // Compute the multilinear quotients q_k = q_k(X_0, ..., X_{k-1})
-//     auto quotients = ZeroMorphProver::compute_multilinear_quotients(multilinear_f, u_challenge);
-
-//     // Compute and send commitments C_{q_k} = [q_k], k = 0,...,d-1
-//     std::vector<Commitment> q_k_commitments;
-//     q_k_commitments.reserve(log_n);
-//     for (size_t idx = 0; idx < log_n; ++idx) {
-//         q_k_commitments[idx] = this->commit(quotients[idx]);
-//         std::string label = "ZM:C_q_" + std::to_string(idx);
-//         prover_transcript.send_to_verifier(label, q_k_commitments[idx]);
-//     }
-
-//     // Get challenge x
-//     auto x_challenge = prover_transcript.get_challenge("ZM:x");
-
-//     // Compute ZeroMorph identity polynomial Z partially evaluated at x
-//     auto Z_x = ZeroMorphProver::compute_partially_evaluated_zeromorph_identity_polynomial(
-//         multilinear_f, quotients, v_evaluation, u_challenge, x_challenge);
-
-//     // Compute and send proof commitment pi
-//     Z_x.factor_roots(x_challenge);
-//     auto pi_commitment = this->commit(Z_x);
-//     prover_transcript.send_to_verifier("ZM:PI", pi_commitment);
-
-//     auto verifier_transcript = VerifierTranscript<Fr>::init_empty(prover_transcript);
-
-//     // Execute Verifier protocol
-
-//     // Receive commitments to f and q_k, and evaluation v = f(u)
-//     auto C_f = verifier_transcript.template receive_from_prover<Commitment>("ZM:C");
-//     auto v_eval = verifier_transcript.template receive_from_prover<Fr>("ZM:v");
-
-//     std::vector<Commitment> C_q_k;
-//     C_q_k.reserve(log_n);
-//     for (size_t i = 0; i < log_n; ++i) {
-//         C_q_k.emplace_back(verifier_transcript.template receive_from_prover<Commitment>("ZM:C_q_" +
-//         std::to_string(i)));
-//     }
-
-//     // Challenge y
-//     auto x_chal = verifier_transcript.get_challenge("ZM:x");
-
-//     // Compute commitment C_{v,x}
-//     auto C_v_x = Commitment::one() * v_eval * this->Phi(x_chal, log_n);
-
-//     // Compute commitment C_{Z_x}
-//     auto C_Z_x = C_f + C_v_x * Fr(-1); // C_f - C_{v,x}
-//     for (size_t k = 0; k < log_n; ++k) {
-//         auto x_pow_2k = x_chal.pow(1 << k);         // x^{2^k}
-//         auto x_pow_2kp1 = x_chal.pow(1 << (k + 1)); // x^{2^{k+1}}
-//         // Compute scalar x^{2^k} * \Phi_{n-k-1}(x^{2^{k+1}}) - u_k *  \Phi_{n-k}(x^{2^k})
-//         auto scalar = x_pow_2k * this->Phi(x_pow_2kp1, log_n - k - 1);
-//         scalar -= u_challenge[k] * this->Phi(x_pow_2k, log_n - k);
-//         C_Z_x = C_Z_x + C_q_k[k] * scalar * Fr(-1);
-//     }
-
-//     // Receive proof commitment \pi
-//     auto C_pi = verifier_transcript.template receive_from_prover<Commitment>("ZM:PI");
-
-//     // Construct pairing check inputs
-//     auto P0 = C_Z_x + C_pi * x_chal;
-//     auto P1 = C_pi * Fr(-1);
-
-//     // Do pairing check
-//     auto verified = this->vk()->pairing_check(P0, P1);
-//     EXPECT_TRUE(verified);
-
-//     // prover_transcript.print();
-//     // verifier_transcript.print();
-//     EXPECT_EQ(prover_transcript.get_manifest(), verifier_transcript.get_manifest());
-// }
-
-// /**
-//  * @brief Full Prover/Verifier protocol for proving multilinear evaluation f(u) = v
-//  *
-//  */
-// TYPED_TEST(ZeroMorphTest, SingleZetaxOnly)
-// {
-//     using Fr = typename TypeParam::ScalarField;
-//     using Commitment = typename TypeParam::AffineElement;
-//     using ZeroMorphProver = ZeroMorphProver<TypeParam>;
-//     size_t n = 16;
-//     size_t log_n = numeric::get_msb(n);
-
-//     // Construct a random multilinear polynomial f, and (u,v) such that f(u) = v.
-//     auto multilinear_f = this->random_polynomial(n);
-//     auto u_challenge = this->random_evaluation_point(log_n);
-//     // auto v_evaluation = multilinear_f.evaluate_mle(u_challenge);
-
-//     // Initialize an empty ProverTranscript
-//     auto prover_transcript = ProverTranscript<Fr>::init_empty();
-
-//     // Execute Prover protocol
-
-//     // Compute the multilinear quotients q_k = q_k(X_0, ..., X_{k-1})
-//     auto quotients = ZeroMorphProver::compute_multilinear_quotients(multilinear_f, u_challenge);
-
-//     // Compute and send commitments C_{q_k} = [q_k], k = 0,...,d-1
-//     std::vector<Commitment> q_k_commitments;
-//     q_k_commitments.reserve(log_n);
-//     for (size_t idx = 0; idx < log_n; ++idx) {
-//         q_k_commitments[idx] = this->commit(quotients[idx]);
-//         std::string label = "ZM:C_q_" + std::to_string(idx);
-//         prover_transcript.send_to_verifier(label, q_k_commitments[idx]);
-//     }
-
-//     // Get challenge y
-//     auto y_challenge = prover_transcript.get_challenge("ZM:y");
-
-//     // Compute the batched, lifted-degree quotient \hat{q}
-//     auto batched_quotient = ZeroMorphProver::compute_batched_lifted_degree_quotient(quotients, y_challenge, n);
-
-//     // Compute and send the commitment C_q = [\hat{q}]
-//     auto q_commitment = this->commit(batched_quotient);
-//     prover_transcript.send_to_verifier("ZM:C_q", q_commitment);
-
-//     // Get challenges x and z
-//     auto x_challenge = prover_transcript.get_challenge("ZM:x");
-
-//     // Compute degree check polynomial \zeta partially evaluated at x
-//     auto zeta_x = ZeroMorphProver::compute_partially_evaluated_degree_check_polynomial(
-//         batched_quotient, quotients, y_challenge, x_challenge);
-
-//     auto commitment_zeta_x = this->commit(zeta_x);
-
-//     // Compute and send proof commitment pi
-//     zeta_x.factor_roots(x_challenge);
-//     auto pi_commitment = this->commit(zeta_x);
-//     prover_transcript.send_to_verifier("ZM:PI", pi_commitment);
-
-//     auto verifier_transcript = VerifierTranscript<Fr>::init_empty(prover_transcript);
-
-//     // Execute Verifier protocol
-//     // Receive commitments to f and q_k, and evaluation v = f(u)
-//     std::vector<Commitment> C_q_k;
-//     C_q_k.reserve(log_n);
-//     for (size_t i = 0; i < log_n; ++i) {
-//         C_q_k.emplace_back(verifier_transcript.template receive_from_prover<Commitment>("ZM:C_q_" +
-//         std::to_string(i)));
-//     }
-
-//     // Challenge y
-//     auto y_chal = verifier_transcript.get_challenge("ZM:y");
-//     (void)y_chal;
-
-//     // Receive commitment C_{q}
-//     auto C_q = verifier_transcript.template receive_from_prover<Commitment>("ZM:C_q");
-//     (void)C_q;
-
-//     // Challenges x, z
-//     auto x_chal = verifier_transcript.get_challenge("ZM:x");
-
-//     // Compute commitment C_{\zeta_x}
-//     auto C_zeta_x = C_q;
-//     for (size_t k = 0; k < log_n; ++k) {
-//         size_t deg_k = (1ULL << k) - 1;
-//         // y^k * x^{N - deg_k - 1}
-//         auto scalar = y_chal.pow(k);
-//         scalar *= x_chal.pow(n - deg_k - 1);
-//         scalar *= Fr(-1);
-//         C_zeta_x = C_zeta_x + C_q_k[k] * scalar;
-//     }
-
-//     EXPECT_EQ(commitment_zeta_x, C_zeta_x);
-
-//     // Receive proof commitment \pi
-//     auto C_pi = verifier_transcript.template receive_from_prover<Commitment>("ZM:PI");
-//     (void)C_pi;
-
-//     // Construct pairing check inputs
-//     auto P0 = C_zeta_x + C_pi * x_chal;
-//     auto P1 = -C_pi;
-
-//     auto verified = this->vk()->pairing_check(P0, P1);
-//     (void)verified;
-
-//     // Do pairing check
-//     EXPECT_TRUE(verified);
-
-//     // prover_transcript.print();
-//     // verifier_transcript.print();
-//     EXPECT_EQ(prover_transcript.get_manifest(), verifier_transcript.get_manifest());
-// }
 
 } // namespace proof_system::honk::pcs::zeromorph
