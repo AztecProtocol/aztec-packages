b031801f5964becd7c5946874c766237b7c71255
benesjan <janbenes1234@gmail.com>
2023-10-04 08:21:40 +0000

    fix

--- yarn-project/deploy_dockerhub.sh
+++ yarn-project/deploy_dockerhub.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+extract_repo yarn-project /usr/src project
+
+cd project/src/yarn-project/aztec-sandbox
 deploy_dockerhub aztec-sandbox x86_64
 deploy_dockerhub aztec-sandbox arm64
 create_dockerhub_manifest aztec-sandbox x86_64,arm64
 
+cd ../pxe
 deploy_dockerhub pxe x86_64
 deploy_dockerhub pxe arm64
 create_dockerhub_manifest pxe x86_64,arm64
