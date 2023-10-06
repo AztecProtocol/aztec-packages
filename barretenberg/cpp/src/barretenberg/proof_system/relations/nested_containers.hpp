ad5050ba93e0740d6271b5ac98d42ea861ad733a
codygunton <codygunton@gmail.com>
2023-10-06 15:31:45 +0000

    Credit

--- barretenberg/cpp/src/barretenberg/proof_system/relations/nested_containers.hpp
+++ barretenberg/cpp/src/barretenberg/proof_system/relations/nested_containers.hpp
@@ -4,14 +4,17 @@
 
 namespace proof_system {
 
-// Generic templates for constructing a container of containers of varying length,
+/**
+ * @brief Generic templates for constructing a container of containers of varying length,
 // where the various lengths are specified in an array.
+ *
+ * @details Credit: https://stackoverflow.com/a/60440611
+ */
 template <template <typename, size_t> typename InnerContainer,
           typename ValueType,
           auto LENGTHS,
           typename IS = decltype(std::make_index_sequence<LENGTHS.size()>())>
 struct TupleOfContainersOverArray;
-
 template <template <typename, size_t> typename InnerContainer, typename ValueType, auto LENGTHS, std::size_t... I>
 struct TupleOfContainersOverArray<InnerContainer, ValueType, LENGTHS, std::index_sequence<I...>> {
     using type = std::tuple<InnerContainer<ValueType, LENGTHS[I]>...>;
