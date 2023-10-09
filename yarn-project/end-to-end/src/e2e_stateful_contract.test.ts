4eaa9453f413f3bb61bf21b211a778efeb89c44f
LHerskind <lasse.herskind@gmail.com>
2023-10-09 18:28:53 +0000

    feat: initial dirty slow_inclusion map

--- yarn-project/end-to-end/src/e2e_stateful_contract.test.ts
+++ yarn-project/end-to-end/src/e2e_stateful_contract.test.ts
@@ -8,12 +8,19 @@ import {
   Wallet,
   isContractDeployed,
 } from '@aztec/aztec.js';
-import { CompleteAddress, getContractDeploymentInfo } from '@aztec/circuits.js';
+import { CircuitsWasm, CompleteAddress, getContractDeploymentInfo } from '@aztec/circuits.js';
+import { pedersenHashInputs, pedersenPlookupCommitInputs } from '@aztec/circuits.js/barretenberg';
 import { DebugLogger } from '@aztec/foundation/log';
+import { INITIAL_LEAF, SparseTree, UpdateOnlyTree, newTree } from '@aztec/merkle-tree';
 import { StatefulTestContract } from '@aztec/noir-contracts/types';
-import { PXE, TxStatus } from '@aztec/types';
+import { Hasher, PXE, TxStatus } from '@aztec/types';
+
+import { default as levelup } from 'levelup';
+import { type MemDown, default as memdown } from 'memdown';
 
 import { setup } from './fixtures/utils.js';
+
+export const createMemDown = () => (memdown as any)() as MemDown<any, any>;
 
 describe('e2e_stateful', () => {
   let pxe: PXE;
@@ -34,7 +41,7 @@ describe('e2e_stateful', () => {
 
   afterAll(() => teardown());
 
-  it('Messing around with noir slow tree', async () => {
+  it('Messing around with noir slow struct', async () => {
     // [next_change, rootBefore, rootAfter, structBefore, structAfter]
     logger(await contract.methods.read_all_slow().view());
 
@@ -60,4 +67,53 @@ describe('e2e_stateful', () => {
     const tx = await contract.methods.is_matching(1).send().wait();
     expect(tx.status).toBe(TxStatus.MINED);
   });
+
+  class PedersenHasher implements Hasher {
+    private readonly circuitsWasm: CircuitsWasm;
+
+    constructor(circuitsWasm: CircuitsWasm) {
+      this.circuitsWasm = circuitsWasm;
+    }
+    compressInputs(_inputs: Buffer[]): Buffer {
+      throw new Error('Method not implemented.');
+    }
+    hashToField(_data: Uint8Array): Buffer {
+      throw new Error('Method not implemented.');
+    }
+    hashToTree(_leaves: Buffer[]): Promise<Buffer[]> {
+      throw new Error('Method not implemented.');
+    }
+
+    public compress(lhs: Buffer, rhs: Buffer): Buffer {
+      return pedersenPlookupCommitInputs(this.circuitsWasm, [lhs, rhs]);
+    }
+  }
+
+  it.only('Messing around with noir slow tree', async () => {
+    const circuitsWasm = await CircuitsWasm.get();
+    const hasher = new PedersenHasher(circuitsWasm);
+
+    const db = levelup(createMemDown());
+    const depth = 4;
+    const tree = await newTree(SparseTree, db, hasher, 'test', depth);
+
+    const path = (await tree.getSiblingPath(0n, true)).toFieldArray();
+
+    const root = tree.getRoot(true);
+
+    logger(await contract.methods.read_whitelist_inner().view());
+    logger(await contract.methods.read_leaf_at(0).view());
+
+    await contract.methods.set_initial_root(root).send().wait();
+
+    logger(await contract.methods.read_whitelist_inner().view());
+    logger(await contract.methods.read_leaf_at(0).view());
+
+    await contract.methods.is_on_whitelist(0, 0, path).send().wait();
+
+    await contract.methods.add_to_whitelist(0, accounts[0].address, path).send().wait();
+
+    logger(await contract.methods.read_whitelist_inner().view());
+    logger(await contract.methods.read_leaf_at(0).view());
+  }, 60_000);
 });
