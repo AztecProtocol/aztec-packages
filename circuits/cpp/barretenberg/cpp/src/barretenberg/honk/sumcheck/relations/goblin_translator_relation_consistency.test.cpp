0658b81bc8c2d8db0dd47097a9eeeed3cdf7561f
Rumata888 <isennovskiy@gmail.com>
2023-10-09 15:10:02 +0000

    main relation consistency

--- circuits/cpp/barretenberg/cpp/src/barretenberg/honk/sumcheck/relations/goblin_translator_relation_consistency.test.cpp
+++ circuits/cpp/barretenberg/cpp/src/barretenberg/honk/sumcheck/relations/goblin_translator_relation_consistency.test.cpp
@@ -4,6 +4,7 @@
 #include "barretenberg/honk/sumcheck/relations/goblin_translator_decomposition_relation.hpp"
 #include "barretenberg/honk/sumcheck/relations/goblin_translator_extra_relations.hpp"
 #include "barretenberg/honk/sumcheck/relations/goblin_translator_gen_perm_sort_relation.hpp"
+#include "barretenberg/honk/sumcheck/relations/goblin_translator_main_relation.hpp"
 #include "permutation_relation.hpp"
 #include "relation_parameters.hpp"
 
@@ -1018,6 +1019,199 @@ TEST_F(GoblinTranslatorRelationConsistency, ExtraRelations)
                              accumulator_transfer_relation,
                              extended_edges,
                              relation_parameters);
+    };
+    run_test(/* is_random_input=*/true);
+    run_test(/* is_random_input=*/false);
+};
+
+/**
+ * @brief Check the consistency of GoblinTranslator Main Relation
+ *
+ * @details
+ *
+ */
+TEST_F(GoblinTranslatorRelationConsistency, MainRelation)
+{
+    using Flavor = honk::flavor::GoblinTranslatorBasic;
+    using FF = typename Flavor::FF;
+    static constexpr size_t FULL_RELATION_LENGTH = 5;
+    using ExtendedEdges = typename Flavor::template ExtendedEdges<FULL_RELATION_LENGTH>;
+    static const size_t NUM_POLYNOMIALS = Flavor::NUM_ALL_ENTITIES;
+
+    const auto relation_parameters = compute_mock_relation_parameters();
+    auto run_test = [&relation_parameters](bool is_random_input) {
+        size_t NUM_LIMB_BITS = 68;
+        FF shift = FF(uint256_t(1) << NUM_LIMB_BITS);
+        FF shiftx2 = FF(uint256_t(1) << (NUM_LIMB_BITS * 2));
+        FF shiftx3 = FF(uint256_t(1) << (NUM_LIMB_BITS * 3));
+        uint512_t MODULUS_U512 = uint512_t(curve::BN254::BaseField::modulus);
+        uint512_t BINARY_BASIS_MODULUS = uint512_t(1) << (NUM_LIMB_BITS << 2);
+        uint512_t NEGATIVE_PRIME_MODULUS = BINARY_BASIS_MODULUS - MODULUS_U512;
+        std::array<FF, 5> NEGATIVE_MODULUS_LIMBS = {
+            FF(NEGATIVE_PRIME_MODULUS.slice(0, NUM_LIMB_BITS).lo),
+            FF(NEGATIVE_PRIME_MODULUS.slice(NUM_LIMB_BITS, NUM_LIMB_BITS * 2).lo),
+            FF(NEGATIVE_PRIME_MODULUS.slice(NUM_LIMB_BITS * 2, NUM_LIMB_BITS * 3).lo),
+            FF(NEGATIVE_PRIME_MODULUS.slice(NUM_LIMB_BITS * 3, NUM_LIMB_BITS * 4).lo),
+            -FF(curve::BN254::BaseField::modulus)
+        };
+        ExtendedEdges extended_edges;
+        std::array<Univariate<FF, INPUT_UNIVARIATE_LENGTH>, NUM_POLYNOMIALS> input_polynomials;
+        if (!is_random_input) {
+            // evaluation form, i.e. input_univariate(0) = 1, input_univariate(1) = 2,.. The polynomial is x+1.
+            for (size_t i = 0; i < NUM_POLYNOMIALS; ++i) {
+                input_polynomials[i] = Univariate<FF, INPUT_UNIVARIATE_LENGTH>({ 1, 2 });
+            }
+            compute_mock_extended_edges<FULL_RELATION_LENGTH>(extended_edges, input_polynomials);
+        } else {
+            // input_univariates are random polynomials of degree one
+            for (size_t i = 0; i < NUM_POLYNOMIALS; ++i) {
+                input_polynomials[i] =
+                    Univariate<FF, INPUT_UNIVARIATE_LENGTH>({ FF::random_element(), FF::random_element() });
+            }
+            compute_mock_extended_edges<FULL_RELATION_LENGTH>(extended_edges, input_polynomials);
+        };
+        auto relation = GoblinTranslatorMainRelation<FF>();
+
+        // Compute expected full length Univariates using straight forward expressions
+        constexpr std::size_t NUM_SUBRELATIONS = std::tuple_size_v<decltype(relation)::RelationUnivariates>;
+        auto expected_full_length_univariates = std::array<Univariate<FF, FULL_RELATION_LENGTH>, NUM_SUBRELATIONS>();
+        auto& op = extended_edges.op;
+        auto& p_x_low_limbs = extended_edges.p_x_low_limbs;
+        auto& p_y_low_limbs = extended_edges.p_y_low_limbs;
+        auto& p_x_high_limbs = extended_edges.p_x_high_limbs;
+        auto& p_y_high_limbs = extended_edges.p_y_high_limbs;
+        auto& accumulators_binary_limbs_0 = extended_edges.accumulators_binary_limbs_0;
+        auto& accumulators_binary_limbs_1 = extended_edges.accumulators_binary_limbs_1;
+        auto& accumulators_binary_limbs_2 = extended_edges.accumulators_binary_limbs_2;
+        auto& accumulators_binary_limbs_3 = extended_edges.accumulators_binary_limbs_3;
+        auto& z_lo_limbs = extended_edges.z_lo_limbs;
+        auto& z_hi_limbs = extended_edges.z_hi_limbs;
+        auto& quotient_lo_binary_limbs = extended_edges.quotient_lo_binary_limbs;
+        auto& quotient_hi_binary_limbs = extended_edges.quotient_hi_binary_limbs;
+        auto& p_x_low_limbs_shift = extended_edges.p_x_low_limbs_shift;
+        auto& p_y_low_limbs_shift = extended_edges.p_y_low_limbs_shift;
+        auto& p_x_high_limbs_shift = extended_edges.p_x_high_limbs_shift;
+        auto& p_y_high_limbs_shift = extended_edges.p_y_high_limbs_shift;
+        auto& accumulators_binary_limbs_0_shift = extended_edges.accumulators_binary_limbs_0_shift;
+        auto& accumulators_binary_limbs_1_shift = extended_edges.accumulators_binary_limbs_1_shift;
+        auto& accumulators_binary_limbs_2_shift = extended_edges.accumulators_binary_limbs_2_shift;
+        auto& accumulators_binary_limbs_3_shift = extended_edges.accumulators_binary_limbs_3_shift;
+        auto& z_lo_limbs_shift = extended_edges.z_lo_limbs_shift;
+        auto& z_hi_limbs_shift = extended_edges.z_hi_limbs_shift;
+        auto& quotient_lo_binary_limbs_shift = extended_edges.quotient_lo_binary_limbs_shift;
+        auto& quotient_hi_binary_limbs_shift = extended_edges.quotient_hi_binary_limbs_shift;
+        auto& relation_wide_limbs = extended_edges.relation_wide_limbs;
+        auto& relation_wide_limbs_shift = extended_edges.relation_wide_limbs_shift;
+        auto& lagrange_odd = extended_edges.lagrange_odd;
+
+        expected_full_length_univariates[0] =
+            (accumulators_binary_limbs_0_shift * relation_parameters.evaluation_input_x[0] + op +
+             p_x_low_limbs * relation_parameters.batching_challenge_v[0][0] +
+             p_y_low_limbs * relation_parameters.batching_challenge_v[1][0] +
+             z_lo_limbs * relation_parameters.batching_challenge_v[2][0] +
+             z_lo_limbs_shift * relation_parameters.batching_challenge_v[3][0] +
+             quotient_lo_binary_limbs * NEGATIVE_MODULUS_LIMBS[0] - accumulators_binary_limbs_0 +
+             (accumulators_binary_limbs_1_shift * relation_parameters.evaluation_input_x[0] +
+              accumulators_binary_limbs_0_shift * relation_parameters.evaluation_input_x[1] +
+              p_x_low_limbs * relation_parameters.batching_challenge_v[0][1] +
+              p_x_low_limbs_shift * relation_parameters.batching_challenge_v[0][0] +
+              p_y_low_limbs * relation_parameters.batching_challenge_v[1][1] +
+              p_y_low_limbs_shift * relation_parameters.batching_challenge_v[1][0] +
+              z_lo_limbs * relation_parameters.batching_challenge_v[2][1] +
+              z_hi_limbs * relation_parameters.batching_challenge_v[2][0] +
+              z_lo_limbs_shift * relation_parameters.batching_challenge_v[3][1] +
+              z_hi_limbs_shift * relation_parameters.batching_challenge_v[3][0] +
+              quotient_lo_binary_limbs * NEGATIVE_MODULUS_LIMBS[1] +
+              quotient_lo_binary_limbs_shift * NEGATIVE_MODULUS_LIMBS[0] - accumulators_binary_limbs_1) *
+                 shift -
+             relation_wide_limbs * shiftx2) *
+            lagrange_odd;
+
+        expected_full_length_univariates[1] =
+            (relation_wide_limbs + accumulators_binary_limbs_2_shift * relation_parameters.evaluation_input_x[0] +
+             accumulators_binary_limbs_1_shift * relation_parameters.evaluation_input_x[1] +
+             accumulators_binary_limbs_0_shift * relation_parameters.evaluation_input_x[2] +
+             p_x_high_limbs * relation_parameters.batching_challenge_v[0][0] +
+             p_x_low_limbs_shift * relation_parameters.batching_challenge_v[0][1] +
+             p_x_low_limbs * relation_parameters.batching_challenge_v[0][2] +
+             p_y_high_limbs * relation_parameters.batching_challenge_v[1][0] +
+             p_y_low_limbs_shift * relation_parameters.batching_challenge_v[1][1] +
+             p_y_low_limbs * relation_parameters.batching_challenge_v[1][2] +
+             z_hi_limbs * relation_parameters.batching_challenge_v[2][1] +
+             z_lo_limbs * relation_parameters.batching_challenge_v[2][2] +
+             z_hi_limbs_shift * relation_parameters.batching_challenge_v[3][1] +
+             z_lo_limbs_shift * relation_parameters.batching_challenge_v[3][2] +
+             quotient_hi_binary_limbs * NEGATIVE_MODULUS_LIMBS[0] +
+             quotient_lo_binary_limbs_shift * NEGATIVE_MODULUS_LIMBS[1] +
+             quotient_lo_binary_limbs * NEGATIVE_MODULUS_LIMBS[2] - accumulators_binary_limbs_2 +
+             (accumulators_binary_limbs_3_shift * relation_parameters.evaluation_input_x[0] +
+              accumulators_binary_limbs_2_shift * relation_parameters.evaluation_input_x[1] +
+              accumulators_binary_limbs_1_shift * relation_parameters.evaluation_input_x[2] +
+              accumulators_binary_limbs_0_shift * relation_parameters.evaluation_input_x[3] +
+              p_x_high_limbs_shift * relation_parameters.batching_challenge_v[0][0] +
+              p_x_high_limbs * relation_parameters.batching_challenge_v[0][1] +
+              p_x_low_limbs_shift * relation_parameters.batching_challenge_v[0][2] +
+              p_x_low_limbs * relation_parameters.batching_challenge_v[0][3] +
+              p_y_high_limbs_shift * relation_parameters.batching_challenge_v[1][0] +
+              p_y_high_limbs * relation_parameters.batching_challenge_v[1][1] +
+              p_y_low_limbs_shift * relation_parameters.batching_challenge_v[1][2] +
+              p_y_low_limbs * relation_parameters.batching_challenge_v[1][3] +
+              z_hi_limbs * relation_parameters.batching_challenge_v[2][2] +
+              z_lo_limbs * relation_parameters.batching_challenge_v[2][3] +
+              z_hi_limbs_shift * relation_parameters.batching_challenge_v[3][2] +
+              z_lo_limbs_shift * relation_parameters.batching_challenge_v[3][3] +
+              quotient_hi_binary_limbs_shift * NEGATIVE_MODULUS_LIMBS[0] +
+              quotient_hi_binary_limbs * NEGATIVE_MODULUS_LIMBS[1] +
+              quotient_lo_binary_limbs_shift * NEGATIVE_MODULUS_LIMBS[2] +
+              quotient_lo_binary_limbs * NEGATIVE_MODULUS_LIMBS[3] - accumulators_binary_limbs_3) *
+                 shift -
+             relation_wide_limbs_shift * shiftx2) *
+            lagrange_odd;
+        auto reconstructed_p_x =
+            (p_x_low_limbs + p_x_low_limbs_shift * shift + p_x_high_limbs * shiftx2 + p_x_high_limbs_shift * shiftx3);
+        auto reconstructed_p_y =
+            (p_y_low_limbs + p_y_low_limbs_shift * shift + p_y_high_limbs * shiftx2 + p_y_high_limbs_shift * shiftx3);
+        auto reconstructed_previous_accumulator =
+            (accumulators_binary_limbs_0_shift + accumulators_binary_limbs_1_shift * shift +
+             accumulators_binary_limbs_2_shift * shiftx2 + accumulators_binary_limbs_3_shift * shiftx3);
+        auto reconstructed_current_accumulator =
+            (accumulators_binary_limbs_0 + accumulators_binary_limbs_1 * shift + accumulators_binary_limbs_2 * shiftx2 +
+             accumulators_binary_limbs_3 * shiftx3);
+        auto reconstructed_z1 = (z_lo_limbs + z_hi_limbs * shift);
+        auto reconstructed_z2 = (z_lo_limbs_shift + z_hi_limbs_shift * shift);
+        auto reconstructed_quotient = (quotient_lo_binary_limbs + quotient_lo_binary_limbs_shift * shift +
+                                       quotient_hi_binary_limbs * shiftx2 + quotient_hi_binary_limbs_shift * shiftx3);
+
+        auto reconstructed_batching_evaluation_v =
+            (relation_parameters.batching_challenge_v[0][0] + relation_parameters.batching_challenge_v[0][1] * shift +
+             relation_parameters.batching_challenge_v[0][2] * shiftx2 +
+             relation_parameters.batching_challenge_v[0][3] * shiftx3);
+        auto reconstructed_batching_evaluation_v2 =
+            (relation_parameters.batching_challenge_v[1][0] + relation_parameters.batching_challenge_v[1][1] * shift +
+             relation_parameters.batching_challenge_v[1][2] * shiftx2 +
+             relation_parameters.batching_challenge_v[1][3] * shiftx3);
+        auto reconstructed_batching_evaluation_v3 =
+            (relation_parameters.batching_challenge_v[2][0] + relation_parameters.batching_challenge_v[2][1] * shift +
+             relation_parameters.batching_challenge_v[2][2] * shiftx2 +
+             relation_parameters.batching_challenge_v[2][3] * shiftx3);
+        auto reconstructed_batching_evaluation_v4 =
+            (relation_parameters.batching_challenge_v[3][0] + relation_parameters.batching_challenge_v[3][1] * shift +
+             relation_parameters.batching_challenge_v[3][2] * shiftx2 +
+             relation_parameters.batching_challenge_v[3][3] * shiftx3);
+        auto reconstructed_evaluation_input_x =
+            (relation_parameters.evaluation_input_x[0] + relation_parameters.evaluation_input_x[1] * shift +
+             relation_parameters.evaluation_input_x[2] * shiftx2 + relation_parameters.evaluation_input_x[3] * shiftx3);
+
+        expected_full_length_univariates[2] =
+            (reconstructed_previous_accumulator * reconstructed_evaluation_input_x + op +
+             reconstructed_p_x * reconstructed_batching_evaluation_v +
+             reconstructed_p_y * reconstructed_batching_evaluation_v2 +
+             reconstructed_z1 * reconstructed_batching_evaluation_v3 +
+             reconstructed_z2 * reconstructed_batching_evaluation_v4 +
+             reconstructed_quotient * NEGATIVE_MODULUS_LIMBS[4] - reconstructed_current_accumulator) *
+            lagrange_odd;
+
+        validate_evaluations(expected_full_length_univariates, relation, extended_edges, relation_parameters);
     };
     run_test(/* is_random_input=*/true);
     run_test(/* is_random_input=*/false);
