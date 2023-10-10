7a83f84c951c1e0b92412945ac19f7372884ad08
Rahul Kothari <rahul.kothari.201@gmail.com>
2023-10-10 13:17:29 +0000

    dockerfile to copy entire l1-contracts

--- yarn-project/deploy_npm.sh
+++ yarn-project/deploy_npm.sh
@@ -11,7 +11,7 @@ function deploy_package() {
     REPOSITORY=$1
     cd $REPOSITORY
 
-    VERSION=$(extract_tag_version $REPOSITORY true)
+    VERSION=$(extract_tag_version $REPOSITORY false)
     echo "Deploying $REPOSITORY $VERSION"
 
     # If the commit tag itself has a dist-tag (e.g. v2.1.0-testnet.123), extract the dist-tag.
