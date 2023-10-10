f3f9f57c5233be5555753c24bf99a261a2489b10
kevaundray <kevtheappdev@gmail.com>
2023-10-10 23:51:36 +0000

    refactor test

--- yarn-project/noir-private-kernel/src/index.test.ts
+++ yarn-project/noir-private-kernel/src/index.test.ts
@@ -1,11 +1,98 @@
 import { DebugLogger, createDebugLogger } from '@aztec/foundation/log';
 
 import { WitnessMap, executeCircuit } from '@noir-lang/acvm_js';
-import { abiEncode } from '@noir-lang/noirc_abi';
+import { abiEncode, abiDecode } from '@noir-lang/noirc_abi';
 
-import { InputType as InitInputType } from './types/private_kernel_init_types.js';
+// TODO(Tom): This should be exported from noirc_abi
+export type DecodedInputs = { inputs: Record<string, any>; return_value: any };
+
+import { InputType as InitInputType, ReturnType as Rt, ReadRequestMembershipWitness, PrivateCallStackItem, FixedLengthArray, Field } from './types/private_kernel_init_types.js';
 
 import { PrivateKernelInitArtifact } from './index.js';
+
+/* eslint-disable */
+const privateCallStackItem : PrivateCallStackItem= {
+  inner: {
+    contract_address: {
+      inner: '0'
+    },
+    public_inputs: {
+      call_context: {
+        msg_sender: {
+          inner: '0'
+        },
+        storage_contract_address: {
+          inner: '0'
+        },
+        portal_contract_address: {
+          inner: '0'
+        },
+        function_selector: {
+          inner: '0'
+        },
+        is_delegate_call: false,
+        is_static_call: false,
+        is_contract_deployment: false
+      },
+      args_hash: '0',
+      return_values: ['1', '2', '3', '4'] as FixedLengthArray<Field, 4>,
+      read_requests: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22','23', '24', '25', '26', '27', '28', '29', '30', '31', '32'] as FixedLengthArray<Field, 32>,
+      new_commitments: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16'] as FixedLengthArray<Field, 16>,
+      new_nullifiers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16'] as FixedLengthArray<Field, 16>,
+      nullified_commitments: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16'] as FixedLengthArray<Field, 16>,
+      private_call_stack: ['1', '2', '3', '4'] as FixedLengthArray<Field, 4>,
+      public_call_stack: ['1', '2', '3', '4'] as FixedLengthArray<Field, 4>,
+      new_l2_to_l1_msgs: ['1', '2'] as FixedLengthArray<Field, 2>,
+      encrypted_logs_hash: ['1', '2'] as FixedLengthArray<Field, 2>,
+      unencrypted_logs_hash: ['1', '2'] as FixedLengthArray<Field, 2>,
+      encrypted_log_preimages_length: '0',
+      unencrypted_log_preimages_length: '0',
+      historical_block_data: {
+        blocks_tree_root: '0',
+        block: {
+          private_data_tree_root: '0',
+          nullifier_tree_root: '0',
+          contract_tree_root: '0',
+          l1_to_l2_data_tree_root: '0',
+          public_data_tree_root: '0',
+          global_variables_hash: '0'
+        },
+        private_kernel_vk_tree_root: '0'
+      },
+      contract_deployment_data: {
+        deployer_public_key: {
+          x: '0',
+          y: '0'
+        },
+        constructor_vk_hash: '0',
+        function_tree_root: '0',
+        contract_address_salt: '0',
+        portal_contract_address: {
+          inner: '0'
+        }
+      },
+      chain_id: '0',
+      version: '0'
+    },
+    is_execution_request: false,
+    function_data: {
+      selector: {
+        inner: '0'
+      },
+      is_internal: false,
+      is_private: false,
+      is_constructor: false
+    }
+  }
+}
+
+/* eslint-disable */
+const readRequestMembershipWitness : ReadRequestMembershipWitness= {
+          leaf_index: '0',
+          sibling_path: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22','23', '24', '25', '26', '27', '28', '29', '30', '31', '32'] as FixedLengthArray<Field, 32>,
+          is_transient: false,
+          hint_to_commitment: '0',
+}
 
 /* eslint-disable */
 const defaultInputType: InitInputType = {
@@ -68,16 +155,16 @@ const defaultInputType: InitInputType = {
               is_contract_deployment: false
             },
             args_hash: '0',
-            return_values: [],
-            read_requests: [],
-            new_commitments: [],
-            new_nullifiers: [],
-            nullified_commitments: [],
-            private_call_stack: [],
-            public_call_stack: [],
-            new_l2_to_l1_msgs: [],
-            encrypted_logs_hash: [],
-            unencrypted_logs_hash: [],
+            return_values: ['1', '2', '3', '4'] as FixedLengthArray<Field, 4>,
+            read_requests: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22','23', '24', '25', '26', '27', '28', '29', '30', '31', '32'] as FixedLengthArray<Field, 32>,
+            new_commitments: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16'] as FixedLengthArray<Field, 16>,
+            new_nullifiers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16'] as FixedLengthArray<Field, 16>,
+            nullified_commitments: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16'] as FixedLengthArray<Field, 16>,
+            private_call_stack: ['1', '2', '3', '4'] as FixedLengthArray<Field, 4>,
+            public_call_stack: ['1', '2', '3', '4'] as FixedLengthArray<Field, 4>,
+            new_l2_to_l1_msgs: ['1', '2'] as FixedLengthArray<Field, 2>,
+            encrypted_logs_hash: ['1', '2'] as FixedLengthArray<Field, 2>,
+            unencrypted_logs_hash: ['1', '2'] as FixedLengthArray<Field, 2>,
             encrypted_log_preimages_length: '0',
             unencrypted_log_preimages_length: '0',
             historical_block_data: {
@@ -118,20 +205,20 @@ const defaultInputType: InitInputType = {
           }
         }
       },
-      private_call_stack_preimages: [],
+      private_call_stack_preimages: [privateCallStackItem, privateCallStackItem, privateCallStackItem, privateCallStackItem] as FixedLengthArray<PrivateCallStackItem, 4>,
       proof: {},
       vk: {},
       function_leaf_membership_witness: {
-        leaf_index: '0',
-        sibling_path: []
+          leaf_index: '0',
+          sibling_path: ['1', '2', '3', '4'] as FixedLengthArray<Field, 4>
       },
       contract_leaf_membership_witness: {
-        leaf_index: '0',
-        sibling_path: []
+          leaf_index: '0',
+          sibling_path: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16'] as FixedLengthArray<Field, 16>,
       },
-      read_request_membership_witnesses: [],
+      read_request_membership_witnesses: [readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness,readRequestMembershipWitness] as FixedLengthArray<ReadRequestMembershipWitness, 32>,
       portal_contract_address: {
-        inner: '0'
+          inner: '0'
       },
       acir_hash: '0'
     }
@@ -166,9 +253,24 @@ describe('Private kernel', () => {
   it.only('Executes private kernel init circuit with abi all zeroes', async () => {
     // logger('Initialized Noir instance with private kernel init circuit');
 
+    // Encode the initial witness values into the witness map
+    const initialWitnessMap = abiEncode(PrivateKernelInitArtifact.abi, defaultInputType, null);
+
+    // Execute the circuit on those initial witness values
+    //
+    // Decode the bytecode from base64 since the acvm does not know about base64 encoding
     const decodedBytecode = Buffer.from(PrivateKernelInitArtifact.bytecode, 'base64');
+    //
+    // Execute the circuit
+    const _witnessMap = await executeCircuit(decodedBytecode, initialWitnessMap, () => {
+      throw Error('unexpected oracle during execution');
+    });
 
-     const witnessMap = abiEncode(PrivateKernelInitArtifact.abi, defaultInputType, null);
-
+    // Decode the witness map into two fields, the return values and the inputs
+    const decodedInputs: DecodedInputs = abiDecode(PrivateKernelInitArtifact.abi, _witnessMap);
+    
+    // Cast the inputs as the return type
+    const return_type = decodedInputs.return_value as Rt;
+    console.log(return_type)
   });
 });
