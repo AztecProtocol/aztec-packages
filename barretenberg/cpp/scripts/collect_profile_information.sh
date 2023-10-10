08bdbf7c83db9dd3e4c982b3d00f7be9c7a63a2a
ludamad <adam@aztecprotocol.com>
2023-10-10 09:43:55 +0000

    feat: flesh out xray profile presets

--- barretenberg/cpp/scripts/collect_profile_information.sh
+++ barretenberg/cpp/scripts/collect_profile_information.sh
@@ -1,26 +1,25 @@
-#!/bin/bash
+!/bin/bash
 set -eu
+
+PRESET=${1:-xray-1thread} # can also be 'xray'
 
 # Move above script dir.
 cd $(dirname $0)/..
 
 # Configure and build with xray preset.
-cmake --preset xray
-cmake --build --preset xray
+cmake --preset $PRESET
+cmake --build --preset $PRESET
 
-cd build-xray
+cd build-$PRESET
 
-# Clear old benchmarks.
-rm -f xray-log.ultra_honk_bench.*
+# Clear old profile data.
+rm -f xray-log.honk_bench_main_simple.*
 
-# Run ultra-honk with benchmarking.
-XRAY_OPTIONS="patch_premain=true xray_mode=xray-basic verbosity=1" ./bin/ultra_honk_bench --benchmark_min_time=10
+# Run benchmark with profiling.
+XRAY_OPTIONS="patch_premain=true xray_mode=xray-basic verbosity=1" ./bin/honk_bench_main_simple
 
 # Process benchmark file.
-llvm-xray-16 stack xray-log.ultra_honk_bench.* \
-  --instr_map=./bin/ultra_plonk_bench --stack-format=flame --aggregation-type=time --all-stacks \
+llvm-xray-16 stack xray-log.honk_bench_main_simple.* \
+  --instr_map=./bin/honk_bench_main_simple --stack-format=flame --aggregation-type=time --all-stacks \
+   | ../scripts/corrector_of_llvm_xray_stack_flame/target/debug/corrector-of-llvm-xray-stack-flame \
    | ../scripts/flamegraph.pl > xray.svg
-   #| ../scripts/corrector_of_llvm_xray_stack_flame/target/debug/corrector-of-llvm-xray-stack-flame \
-
-llvm-xray-16 account xray-log.ultra_honk_bench.* \
-  --instr_map=./bin/ultra_plonk_bench > xray.account
