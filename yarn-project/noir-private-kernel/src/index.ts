5ad55c89899ec6336ab6b19afb91e761b4a8d384
sirasistant <sirasistant@gmail.com>
2023-10-10 08:29:22 +0000

    docs: improve comments

--- yarn-project/noir-private-kernel/src/index.ts
+++ yarn-project/noir-private-kernel/src/index.ts
@@ -1,11 +1,10 @@
 import PrivateKernelInitJson from './target/private_kernel_init.json' assert { type: 'json' };
-// Todo add types for noir circuit artifacts
 import PrivateKernelInnerJson from './target/private_kernel_inner.json' assert { type: 'json' };
-// Todo add types for noir circuit artifacts
 import PrivateKernelOrderingJson from './target/private_kernel_ordering.json' assert { type: 'json' };
 
-export const PrivateKernelInitArtifact = PrivateKernelInitJson; // TODO add types for noir circuit artifacts
+// TODO add types for noir circuit artifacts
+export const PrivateKernelInitArtifact = PrivateKernelInitJson;
 
-export const PrivateKernelInnerArtifact = PrivateKernelInnerJson; // TODO add types for noir circuit artifacts
+export const PrivateKernelInnerArtifact = PrivateKernelInnerJson;
 
-export const PrivateKernelOrderingArtifact = PrivateKernelOrderingJson; // TODO add types for noir circuit artifacts
+export const PrivateKernelOrderingArtifact = PrivateKernelOrderingJson;
