046642fe97f32429fb4d7ec25f08f0c2a245cc3e
zac-williamson <blorktronics@gmail.com>
2023-10-11 15:56:15 +0000

    wip

--- barretenberg/cpp/src/barretenberg/crypto/pedersen_commitment/c_bind.cpp
+++ barretenberg/cpp/src/barretenberg/crypto/pedersen_commitment/c_bind.cpp
@@ -44,6 +44,22 @@ WASM_EXPORT void pedersen___commit(fr::vec_in_buf inputs_buffer, fr::out_buf out
 
     serialize::write(output, pedersen_hash);
 }
+WASM_EXPORT void pedersen___plookup_commit(fr::vec_in_buf inputs_buffer, fr::out_buf output)
+{
+    pedersen___commit(inputs_buffer, output);
+}
+WASM_EXPORT void pedersen___plookup_commit_with_hash_index(fr::vec_in_buf inputs_buffer,
+                                                           uint32_t const* hash_index,
+                                                           fr::out_buf output)
+{
+    std::vector<grumpkin::fq> to_compress;
+    read(inputs_buffer, to_compress);
+    const size_t generator_offset = ntohl(*hash_index);
+    crypto::GeneratorContext<curve::Grumpkin> ctx; // todo fix
+    ctx.offset = generator_offset;
+    auto commitment = crypto::pedersen_commitment::commit_native(to_compress, ctx);
+    serialize::write(output, commitment);
+}
 
 WASM_EXPORT void pedersen___buffer_to_field(uint8_t const* data, fr::out_buf r)
 {
