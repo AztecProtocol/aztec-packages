4dc052623f025089424769f611b29c5e31471000
sirasistant <sirasistant@gmail.com>
2023-10-11 07:15:38 +0000

    style: formatting

--- yarn-project/circuits.js/src/structs/index.ts
+++ yarn-project/circuits.js/src/structs/index.ts
@@ -41,4 +41,4 @@ export { FunctionSelector } from '@aztec/foundation/abi';
 // This is not great as we are exporting from tests.
 // Its okay for now and before merging into master, we should
 // remove this line.
-export {makePrivateKernelInputsInit} from '../tests/factories.js';
+export { makePrivateKernelInputsInit } from '../tests/factories.js';
