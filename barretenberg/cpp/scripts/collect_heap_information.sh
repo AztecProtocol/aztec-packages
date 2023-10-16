b42035d8a4f81ab7b66095cefecbbb23e6dedd47
ludamad <adam@aztecprotocol.com>
2023-10-16 14:35:20 +0000

    feat: gperftools

---
+++ barretenberg/cpp/scripts/collect_heap_information.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -eu
+
+PRESET=gperftools
+ONLY_PROCESS=${1:-}
+
+# Move above script dir.
+cd $(dirname $0)/..
+
+# Configure and build with heap profiling preset.
+cmake --preset $PRESET
+cmake --build --preset $PRESET
+
+cd build-$PRESET
+
+if [ -z "$ONLY_PROCESS" ]; then
+  # Clear old heap profile data.
+  rm -f honk_bench_main_simple.heap.*
+
+  # Run application with heap profiling.
+HEAPPROFILE=./honk_bench_main_simple ./bin/honk_bench_main_simple
+fi
+
+# Download and install Go
+if [ ! -d ~/go ]; then
+  ARCHIVE=go1.21.3.linux-amd64.tar.gz
+  echo "Downloading and installing Go..."
+  curl -O https://golang.org/dl/$ARCHIVE
+  tar -C ~/ -xvf $ARCHIVE
+  rm $ARCHIVE
+  export PATH=$PATH:~/go/bin
+fi
+
+# Install pprof
+if [ ! -f ~/go/bin/pprof ]; then
+    echo "Installing pprof..."
+    ~/go/bin/go install github.com/google/pprof@latest
+fi
+
+# Process the heap profile with pprof
+~/go/bin/pprof --text ./bin/honk_bench_main_simple honk_bench_main_simple.0001.heap
