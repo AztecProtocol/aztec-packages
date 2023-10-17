2e04fba0e9798a5e6bcdc4c7b5378d5ce9f1cb7d
dan-aztec <dan@aztecprotocol.com>
2023-10-17 16:00:33 +0000

    fix scripts

--- yarn-project/boxes/bootstrap.sh
+++ yarn-project/boxes/bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # should be run from yarn-project/boxes
 original_path=$(pwd)
 
@@ -9,13 +8,11 @@ for dir in *; do
   # Check if it's a directory
   if [ -d "${dir}" ]; then
     
+    cd "${dir}"
     # Run the compile command
     echo "Running compile command inside ${dir}..."
 
-    # Change directory to ../cli
-    cd ../cli
-    
-    # Run ts-node command to use latest "compile" code.  this uses the yarn command to use the subpackage ts-node dep
+    # Runs ts-node command from "../cli" to use latest "compile" code.  this uses the yarn command to use the subpackage ts-node dep
     yarn compile:local
     
     # Change back to the original directory
