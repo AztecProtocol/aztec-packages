fbe3b19f2a6549f256e42ce73f24cc160354e65b
ludamad <adam.domurad@gmail.com>
2023-10-18 15:16:23 -0400

    fix: widget bench

--- barretenberg/cpp/src/barretenberg/benchmark/relations_bench/widget.bench.cpp
+++ barretenberg/cpp/src/barretenberg/benchmark/relations_bench/widget.bench.cpp
@@ -12,25 +12,23 @@ auto& engine = numeric::random::get_debug_engine();
 
 namespace proof_system::plonk {
 
-using FF = barretenberg::fr;
-
 template <typename Flavor, typename Widget> void execute_widget(::benchmark::State& state)
 {
     barretenberg::srs::init_crs_factory("../srs_db/ignition");
-    auto inner_composer = plonk::StandardComposer();
-    auto builder = typename plonk::StandardComposer::CircuitBuilder();
+    auto inner_composer = plonk::UltraComposer();
+    auto builder = typename plonk::UltraComposer::CircuitBuilder();
     bench_utils::generate_basic_arithmetic_circuit(builder, 80);
     auto inner_prover = inner_composer.create_prover(builder);
     auto inner_proof = inner_prover.construct_proof();
     for (auto _ : state) {
-        Widget value(inner_composer.circuit_proving_key.get());
-        value.compute_quotient_contribution(FF::random_element(), inner_prover.transcript);
+        Widget widget(inner_composer.circuit_proving_key.get());
+        widget.compute_quotient_contribution(barretenberg::fr::random_element(), inner_prover.transcript);
     }
 }
 
 void plookup_auxiliary_widget(::benchmark::State& state) noexcept
 {
-    execute_widget<honk::flavor::Ultra, ProverPlookupAuxiliaryWidget<FF>>(state);
+    execute_widget<honk::flavor::Ultra, ProverPlookupAuxiliaryWidget<ultra_settings>>(state);
 }
 BENCHMARK(plookup_auxiliary_widget);
 
