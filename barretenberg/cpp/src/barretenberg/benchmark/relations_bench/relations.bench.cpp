d574ba7395869ca5b65deefd1a2dd65e65c37d46
ludamad <adam@aztecprotocol.com>
2023-10-18 21:39:13 +0000

    Revert relations

--- barretenberg/cpp/src/barretenberg/benchmark/relations_bench/relations.bench.cpp
+++ barretenberg/cpp/src/barretenberg/benchmark/relations_bench/relations.bench.cpp
@@ -31,9 +31,7 @@ template <typename Flavor, typename Relation> void execute_relation(::benchmark:
     // Evaluate each constraint in the relation and check that each is satisfied
 
     for (auto _ : state) {
-        for (int i = 0; i < 1000; i++) {
-            Relation::accumulate(accumulator, new_value, params, 1);
-        }
+        Relation::accumulate(accumulator, new_value, params, 1);
     }
 }
 
@@ -42,5 +40,41 @@ void auxiliary_relation(::benchmark::State& state) noexcept
     execute_relation<honk::flavor::Ultra, AuxiliaryRelation<FF>>(state);
 }
 BENCHMARK(auxiliary_relation);
+
+void elliptic_relation(::benchmark::State& state) noexcept
+{
+    execute_relation<honk::flavor::Ultra, EllipticRelation<FF>>(state);
+}
+BENCHMARK(elliptic_relation);
+
+void ecc_op_queue_relation(::benchmark::State& state) noexcept
+{
+    execute_relation<honk::flavor::GoblinUltra, EccOpQueueRelation<FF>>(state);
+}
+BENCHMARK(ecc_op_queue_relation);
+
+void gen_perm_sort_relation(::benchmark::State& state) noexcept
+{
+    execute_relation<honk::flavor::Ultra, GenPermSortRelation<FF>>(state);
+}
+BENCHMARK(gen_perm_sort_relation);
+
+void lookup_relation(::benchmark::State& state) noexcept
+{
+    execute_relation<honk::flavor::Ultra, LookupRelation<FF>>(state);
+}
+BENCHMARK(lookup_relation);
+
+void ultra_permutation_relation(::benchmark::State& state) noexcept
+{
+    execute_relation<honk::flavor::Ultra, UltraPermutationRelation<FF>>(state);
+}
+BENCHMARK(ultra_permutation_relation);
+
+void ultra_arithmetic_relation(::benchmark::State& state) noexcept
+{
+    execute_relation<honk::flavor::Ultra, UltraArithmeticRelation<FF>>(state);
+}
+BENCHMARK(ultra_arithmetic_relation);
 
 } // namespace proof_system::benchmark::relations
