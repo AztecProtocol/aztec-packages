a1fc07c520f367dab36b8786e57b92f73cbf4bc3
ludamad <adam@aztecprotocol.com>
2023-10-18 16:48:30 +0000

    Benchmark fixes

--- barretenberg/cpp/src/barretenberg/benchmark/relations_bench/widget.bench.cpp
+++ barretenberg/cpp/src/barretenberg/benchmark/relations_bench/widget.bench.cpp
@@ -1,6 +1,7 @@
 #include "barretenberg/benchmark/honk_bench/benchmark_utilities.hpp"
 #include "barretenberg/honk/flavor/goblin_ultra.hpp"
 #include "barretenberg/honk/flavor/ultra.hpp"
+#include "barretenberg/plonk/composer/standard_composer.hpp"
 #include "barretenberg/plonk/composer/ultra_composer.hpp"
 #include "barretenberg/plonk/proof_system/widgets/transition_widgets/plookup_auxiliary_widget.hpp"
 #include <benchmark/benchmark.h>
@@ -15,21 +16,15 @@ using FF = barretenberg::fr;
 
 template <typename Flavor, typename Widget> void execute_widget(::benchmark::State& state)
 {
-    // Generate beta and gamma
-    auto inner_composer = plonk::UltraComposer();
-    auto builder = typename plonk::UltraComposer::CircuitBuilder();
-    bench_utils::generate_basic_arithmetic_circuit(builder, 1);
+    barretenberg::srs::init_crs_factory("../srs_db/ignition");
+    auto inner_composer = plonk::StandardComposer();
+    auto builder = typename plonk::StandardComposer::CircuitBuilder();
+    bench_utils::generate_basic_arithmetic_circuit(builder, 80);
     auto inner_prover = inner_composer.create_prover(builder);
     auto inner_proof = inner_prover.construct_proof();
-    auto inner_verifier = inner_composer.create_verifier(builder);
-
-    const size_t num_inner_public_inputs = builder.get_public_inputs().size();
-    transcript::StandardTranscript transcript(inner_proof.proof_data,
-                                              plonk::UltraComposer::create_manifest(num_inner_public_inputs),
-                                              transcript::HashType::PlookupPedersenBlake3s);
-    Widget value;
     for (auto _ : state) {
-        value.compute_quotient_contribution(FF::random_element(), transcript);
+        Widget value(inner_composer.circuit_proving_key.get());
+        value.compute_quotient_contribution(FF::random_element(), inner_prover.transcript);
     }
 }
 
