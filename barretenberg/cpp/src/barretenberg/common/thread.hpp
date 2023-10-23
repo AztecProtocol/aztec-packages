2fbe7613c7bdd20af2c5e2decaa15e1030c82a82
ludamad <adam@aztecprotocol.com>
2023-10-23 16:49:14 +0000

    Simplify batched parallel_for

--- barretenberg/cpp/src/barretenberg/common/thread.hpp
+++ barretenberg/cpp/src/barretenberg/common/thread.hpp
@@ -28,15 +28,8 @@ void parallel_for(size_t num_iterations, const std::function<void(size_t)>& func
  * A modified parallel_for optimized for work being done in batches.
  * This is more appropriate for work with small granularity, to avoid thread caching issues and overhead.
  */
-inline void parallel_for_batched(size_t num_iterations, auto&& func, size_t min_num_iterations = 800)
+inline void parallel_for_batched(size_t num_iterations, auto&& func)
 {
-    if (num_iterations <= min_num_iterations) {
-        // Don't bother with overhead of splitting into threads if small
-        for (size_t i = 0; i < num_iterations; i++) {
-            func(i);
-        }
-        return;
-    }
     size_t num_threads = get_num_cpus_pow2();
     size_t batch_size = (num_iterations + num_threads - 1) / num_threads; // round up division
     // We will use parallel_for to dispatch the batches
