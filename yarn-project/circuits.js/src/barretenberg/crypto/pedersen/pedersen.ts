552d2efa28fb2f9ab227f2bb1213f4f4a18f2adf
kevaundray <kevtheappdev@gmail.com>
2023-10-25 15:05:40 +0000

    replace pedersenCompress methods with pedersenHash

--- yarn-project/circuits.js/src/barretenberg/crypto/pedersen/pedersen.ts
+++ yarn-project/circuits.js/src/barretenberg/crypto/pedersen/pedersen.ts
@@ -5,76 +5,20 @@ import { Buffer } from 'buffer';
 import { deserializeArrayFromVector, deserializeField, serializeBufferArrayToVector } from '../../serialize.js';
 
 /**
- * Compresses two 32-byte hashes.
+ * Hashes an array of buffers.
  * @param wasm - The barretenberg module.
- * @param lhs - The first hash.
- * @param rhs - The second hash.
- * @returns The new 32-byte hash.
- * @deprecated Don't call pedersen directly in production code. Instead, create suitably-named functions for specific
- * purposes.
- */
-export function pedersenCompress(wasm: IWasmModule, lhs: Uint8Array, rhs: Uint8Array): Buffer {
-  // If not done already, precompute constants.
-  wasm.call('pedersen__init');
-  if (lhs.length !== 32 || rhs.length !== 32) {
-    throw new Error(`Pedersen lhs and rhs inputs must be 32 bytes (got ${lhs.length} and ${rhs.length} respectively)`);
-  }
-  wasm.writeMemory(0, lhs);
-  wasm.writeMemory(32, rhs);
-  wasm.call('pedersen__hash_pair', 0, 32, 64);
-  return Buffer.from(wasm.getMemorySlice(64, 96));
-}
-
-/**
- * Combine an array of hashes using pedersen hash.
- * @param wasm - The barretenberg module.
- * @param lhs - The first hash.
- * @param rhs - The second hash.
- * @returns The new 32-byte hash.
- * @deprecated Don't call pedersen directly in production code. Instead, create suitably-named functions for specific
- * purposes.
- */
-export function pedersenHashInputs(wasm: IWasmModule, inputs: Buffer[]): Buffer {
-  // If not done already, precompute constants.
-  wasm.call('pedersen__init');
-  const inputVectors = serializeBufferArrayToVector(inputs);
-  wasm.writeMemory(0, inputVectors);
-  wasm.call('pedersen__hash_multiple', 0, 0);
-  return Buffer.from(wasm.getMemorySlice(0, 32));
-}
-
-/**
- * Compresses an array of buffers.
- * @param wasm - The barretenberg module.
- * @param inputs - The array of buffers to compress.
- * @returns The resulting 32-byte hash.
- * @deprecated Don't call pedersen directly in production code. Instead, create suitably-named functions for specific
- * purposes.
- */
-export function pedersenCompressInputs(wasm: IWasmModule, inputs: Buffer[]): Buffer {
-  // If not done already, precompute constants.
-  wasm.call('pedersen__init');
-  const inputVectors = serializeBufferArrayToVector(inputs);
-  wasm.writeMemory(0, inputVectors);
-  wasm.call('pedersen__compress', 0, 0);
-  return Buffer.from(wasm.getMemorySlice(0, 32));
-}
-
-/**
- * Compresses an array of buffers.
- * @param wasm - The barretenberg module.
- * @param inputs - The array of buffers to compress.
+ * @param inputs - The array of buffers to hash.
  * @param hashIndex - Hash index of the generator to use (See GeneratorIndex enum).
  * @returns The resulting 32-byte hash.
  * @deprecated Don't call pedersen directly in production code. Instead, create suitably-named functions for specific
  * purposes.
  */
-export function pedersenCompressWithHashIndex(wasm: IWasmModule, inputs: Buffer[], hashIndex: number): Buffer {
+export function pedersenHashWithHashIndex(wasm: IWasmModule, inputs: Buffer[], hashIndex: number): Buffer {
   // If not done already, precompute constants.
   wasm.call('pedersen__init');
   const inputVectors = serializeBufferArrayToVector(inputs);
   wasm.writeMemory(0, inputVectors);
-  wasm.call('pedersen__compress_with_hash_index', 0, 0, hashIndex);
+  wasm.call('pedersen__hash_with_hash_index', 0, 0, hashIndex);
   return Buffer.from(wasm.getMemorySlice(0, 32));
 }
 
@@ -102,7 +46,7 @@ export function pedersenGetHash(wasm: IWasmModule, data: Buffer): Buffer {
  *
  * E.g.
  * Input:  [1][2][3][4]
- * Output: [1][2][3][4][compress(1,2)][compress(3,4)][compress(5,6)].
+ * Output: [1][2][3][4][hash(1,2)][hash(3,4)][hash(5,6)].
  *
  * @param wasm - The barretenberg module.
  * @param values - The 32 byte pedersen leaves.
