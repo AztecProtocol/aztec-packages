bdacc6596d2b69bfb88535b1016b3c7869c6aa6e
ludamad <adam.domurad@gmail.com>
2023-10-26 19:17:33 -0400

    Time to be bold

--- barretenberg/cpp/scripts/benchmarks.sh
+++ barretenberg/cpp/scripts/benchmarks.sh
@@ -10,8 +10,18 @@ cmake --build --preset clang16
 
 cd build
 
+echo "<details><summary>Standard Plonk</summary>"
 ./bin/standard_plonk_bench | tee standard_plonk_bench.out
+echo "</details/>"
+echo "<details><summary>Ultra Honk</summary>"
 ./bin/ultra_honk_rounds_bench | tee ultra_honk_rounds_bench.out
+echo "</details/>"
+echo "<details><summary>Ultra Plonk</summary>"
 ./bin/ultra_plonk_rounds_bench | tee ultra_plonk_rounds_bench.out
+echo "</details/>"
+echo "<details><summary>Ultra Honk</summary>"
 ./bin/ultra_honk_bench | tee ultra_honk_bench.out
+echo "</details/>"
+echo "<details><summary>Ultra Plonk</summary>"
 ./bin/ultra_plonk_bench | tee ultra_plonk_bench.out
+echo "</details/>"
