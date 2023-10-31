3d449db606b5bfd45acb0dc9cb431062a1895e1c
ledwards2225 <l.edwards.d@gmail.com>
2023-10-31 21:07:07 +0000

    get rid of inheritance altogether in arith classes

--- barretenberg/cpp/src/barretenberg/proof_system/arithmetization/arithmetization.hpp
+++ barretenberg/cpp/src/barretenberg/proof_system/arithmetization/arithmetization.hpp
@@ -29,80 +29,74 @@ namespace arithmetization {
  *
  * We should only do this if it becomes necessary or convenient.
  */
-template <typename FF> class Arithmetization {
-  protected:
-    using SelectorType = std::vector<FF, barretenberg::ContainerSlabAllocator<FF>>;
-    using DataType = std::vector<SelectorType>;
-
-    DataType selectors;
-
-  public:
-    Arithmetization(size_t n)
-        : selectors(n)
-    {}
-    size_t size() const { return selectors.size(); };
-    typename DataType::const_iterator begin() const { return selectors.begin(); };
-    typename DataType::iterator begin() { return selectors.begin(); };
-    typename DataType::const_iterator end() const { return selectors.end(); };
-    typename DataType::iterator end() { return selectors.end(); };
-
-    void reserve(size_t size_hint)
-    {
-        for (auto& p : this->selectors) {
-            p.reserve(size_hint);
-        }
-    }
-};
 
 // These are not magic numbers and they should not be written with global constants. These parameters are not accessible
 // through clearly named static class members.
-template <typename _FF> class Standard : public Arithmetization<_FF> {
+template <typename _FF> class Standard {
   public:
     static constexpr size_t NUM_WIRES = 3;
     static constexpr size_t num_selectors = 5;
     using FF = _FF;
-    using SelectorType = typename Arithmetization<FF>::SelectorType;
+    using SelectorType = std::vector<FF, barretenberg::ContainerSlabAllocator<FF>>;
 
-    SelectorType& q_m() { return this->selectors[0]; };
-    SelectorType& q_1() { return this->selectors[1]; };
-    SelectorType& q_2() { return this->selectors[2]; };
-    SelectorType& q_3() { return this->selectors[3]; };
-    SelectorType& q_c() { return this->selectors[4]; };
+    std::vector<SelectorType> selectors;
+
+    SelectorType& q_m() { return selectors[0]; };
+    SelectorType& q_1() { return selectors[1]; };
+    SelectorType& q_2() { return selectors[2]; };
+    SelectorType& q_3() { return selectors[3]; };
+    SelectorType& q_c() { return selectors[4]; };
 
     Standard()
-        : Arithmetization<FF>(num_selectors)
+        : selectors(num_selectors)
     {}
 
-    const auto& get() const { return this->selectors; };
+    const auto& get() const { return selectors; };
+
+    void reserve(size_t size_hint)
+    {
+        for (auto& p : selectors) {
+            p.reserve(size_hint);
+        }
+    }
 
     // Note: These are needed for Plonk only (for poly storage in a std::map). Must be in same order as above struct.
     inline static const std::vector<std::string> selector_names = { "q_m", "q_1", "q_2", "q_3", "q_c" };
 };
 
-template <typename _FF> class Ultra : public Arithmetization<_FF> {
+template <typename _FF> class Ultra {
   public:
     static constexpr size_t NUM_WIRES = 4;
     static constexpr size_t num_selectors = 11;
     using FF = _FF;
-    using SelectorType = typename Arithmetization<FF>::SelectorType;
+    using SelectorType = std::vector<FF, barretenberg::ContainerSlabAllocator<FF>>;
 
-    SelectorType& q_m() { return this->selectors[0]; };
-    SelectorType& q_c() { return this->selectors[1]; };
-    SelectorType& q_1() { return this->selectors[2]; };
-    SelectorType& q_2() { return this->selectors[3]; };
-    SelectorType& q_3() { return this->selectors[4]; };
-    SelectorType& q_4() { return this->selectors[5]; };
-    SelectorType& q_arith() { return this->selectors[6]; };
-    SelectorType& q_sort() { return this->selectors[7]; };
-    SelectorType& q_elliptic() { return this->selectors[8]; };
-    SelectorType& q_aux() { return this->selectors[9]; };
-    SelectorType& q_lookup_type() { return this->selectors[10]; };
+    std::vector<SelectorType> selectors;
+
+    SelectorType& q_m() { return selectors[0]; };
+    SelectorType& q_c() { return selectors[1]; };
+    SelectorType& q_1() { return selectors[2]; };
+    SelectorType& q_2() { return selectors[3]; };
+    SelectorType& q_3() { return selectors[4]; };
+    SelectorType& q_4() { return selectors[5]; };
+    SelectorType& q_arith() { return selectors[6]; };
+    SelectorType& q_sort() { return selectors[7]; };
+    SelectorType& q_elliptic() { return selectors[8]; };
+    SelectorType& q_aux() { return selectors[9]; };
+    SelectorType& q_lookup_type() { return selectors[10]; };
 
     Ultra()
-        : Arithmetization<FF>(num_selectors)
+        : selectors(num_selectors)
     {}
 
-    const auto& get() const { return this->selectors; };
+    const auto& get() const { return selectors; };
+
+    void reserve(size_t size_hint)
+    {
+        for (auto& p : selectors) {
+            p.reserve(size_hint);
+        }
+    }
 
     // Note: These are needed for Plonk only (for poly storage in a std::map). Must be in same order as above struct.
     inline static const std::vector<std::string> selector_names = { "q_m",        "q_c",   "q_1",       "q_2",
