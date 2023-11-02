f1de40cd0607b7187755b320a04723d172eaa62f
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2023-11-02 12:42:36 +0000

    temp

---
+++ barretenberg/acir_tests/run_acir_tests_sol.sh
@@ -0,0 +1,6 @@
+## Generate the solidity verifier with bb, then run a test through it
+
+
+## Issue here, work out where the public inputs are included in the witness
+## If they are included in the proof output, then we can get away with just extracting them
+## Maybe worth making something that can extract the public inputs from the .tz
