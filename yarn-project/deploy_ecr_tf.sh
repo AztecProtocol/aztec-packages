f26602d38363e9b59091b11482dbd1c5f3aa69f9
spypsy <spypsy@outlook.com>
2023-11-06 16:44:20 +0000

    rename infura api key

--- yarn-project/deploy_ecr_tf.sh
+++ yarn-project/deploy_ecr_tf.sh
@@ -4,7 +4,7 @@ extract_repo yarn-project /usr/src project
 PROJECT_ROOT=$(pwd)/project/src/
 
 # export some tf variables
-export TF_VAR_API_KEY=$API_KEY
+export TF_VAR_API_KEY=$INFURA_API_KEY
 
 for REPOSITORY in "p2p-bootstrap" "aztec-node" "aztec-faucet"; do
   echo "Deploying $REPOSITORY"
