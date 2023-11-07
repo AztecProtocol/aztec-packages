94f43be7d8b4ecccb811520640165ad5e58a1bfe
benesjan <janbenes1234@gmail.com>
2023-11-07 10:17:31 +0000

    WIP

--- yarn-project/end-to-end/src/e2e_liquidity_mining.test.ts
+++ yarn-project/end-to-end/src/e2e_liquidity_mining.test.ts
@@ -1,19 +1,13 @@
-import {
-  AccountWallet,
-  CheatCodes,
-  CompleteAddress,
-  DebugLogger
-} from '@aztec/aztec.js';
+import { AccountWallet, CheatCodes, CompleteAddress, DebugLogger } from '@aztec/aztec.js';
 import { CircuitsWasm } from '@aztec/circuits.js';
 import { Pedersen, SparseTree, newTree } from '@aztec/merkle-tree';
-import { SlowTreeContract } from '@aztec/noir-contracts/types';
+import { LiquidityMiningContract } from '@aztec/noir-contracts/types';
 
 import { jest } from '@jest/globals';
 import levelup from 'levelup';
-import { default as memdown, type MemDown } from 'memdown';
+import { type MemDown, default as memdown } from 'memdown';
 
 import { setup } from './fixtures/utils.js';
-import { TokenSimulator } from './simulators/token_simulator.js';
 
 export const createMemDown = () => (memdown as any)() as MemDown<any, any>;
 
@@ -23,31 +17,26 @@ describe('e2e_liquidity_mining', () => {
   jest.setTimeout(TIMEOUT);
 
   let teardown: () => Promise<void>;
+  let logger: DebugLogger;
   let wallets: AccountWallet[];
   let accounts: CompleteAddress[];
-  let logger: DebugLogger;
-
-  let tokenSim: TokenSimulator;
-
-  let tree: SparseTree;
-
   let cheatCodes: CheatCodes;
+
+  let contract: LiquidityMiningContract;
+
+  let simulatorTree: SparseTree;
 
   beforeAll(async () => {
     ({ teardown, logger, wallets, accounts, cheatCodes } = await setup(4));
 
-    slowTree = await SlowTreeContract.deploy(wallets[0]).send().deployed();
+    contract = await LiquidityMiningContract.deploy(wallets[0]).send().deployed();
 
     const db = levelup(createMemDown());
     const hasher = new Pedersen(await CircuitsWasm.get());
     const depth = 254;
-    tree = await newTree(SparseTree, db, hasher, 'test', depth);
-
+    simulatorTree = await newTree(SparseTree, db, hasher, 'test', depth);
   }, 100_000);
 
   afterAll(() => teardown());
 
-  afterEach(async () => {
-    await tokenSim.check();
-  }, TIMEOUT);
 });
