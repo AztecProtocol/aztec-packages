1f38e3981ae5448cc7d4217310ec1ea522e0e9ff
Dan Lee <dan@aztecprotocol.com>
2023-11-08 09:15:13 +0300

    revert more protocol circuits changes

--- yarn-project/noir-protocol-circuits/src/scripts/compile_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# meant to be run from the root of the subpackage
-echo "Compiling all contracts"
-./src/scripts/compile.sh $(./src/scripts/get_all_contracts.sh)
--- yarn-project/noir-protocol-circuits/src/scripts/get_all_contracts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Utility to get the names of all contracts
-echo $(ls -d src/crates/*/Nargo.toml | sed -r "s/src\\/crates\\/(.+)\\/Nargo.toml/\\1/")
