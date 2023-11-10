04b355796276d406d4ba888e1143e138032730ba
ludamad <adam@aztecprotocol.com>
2023-11-10 12:35:16 +0000

    fix: size asserts

--- barretenberg/cpp/src/barretenberg/flavor/goblin_ultra_recursive.hpp
+++ barretenberg/cpp/src/barretenberg/flavor/goblin_ultra_recursive.hpp
@@ -180,8 +180,6 @@ template <typename BuilderType> class GoblinUltraRecursive_ {
                             &ecc_op_wire_3,
                             &ecc_op_wire_4,
                             &calldata,
-                            &calldata,
-                            &calldata,
                             &calldata_read_counts)
 
         std::vector<HandleType> get_wires() override { return { w_l, w_r, w_o, w_4 }; };
