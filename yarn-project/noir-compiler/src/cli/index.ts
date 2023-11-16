af5832f1ac6b0eab4e5ec562493bf03ff430f102
Dan Lee <dan@aztecprotocol.com>
2023-11-16 15:00:58 +0300

    update compileContract name

--- yarn-project/noir-compiler/src/cli/index.ts
+++ yarn-project/noir-compiler/src/cli/index.ts
@@ -1,3 +1,3 @@
-export { compileContract } from './compileNoir.js';
+export { compileNoir } from './compileNoir.js';
 export { generateNoirInterface } from './noir-interface.js';
 export { generateTypescriptInterface } from './typescript.js';
