87a4438d5e0658d17fc643cdb31929cf4786c0ea
codygunton <codygunton@gmail.com>
2023-11-19 04:33:40 +0000

    Revert "Remove one unneeded layer of copying."
    This reverts commit 835e77e47edf32f6e92eaae13cadbe4dc39ad583.

--- barretenberg/cpp/src/barretenberg/translator_vm/goblin_translator_composer.cpp
+++ barretenberg/cpp/src/barretenberg/translator_vm/goblin_translator_composer.cpp
@@ -62,26 +62,126 @@ void GoblinTranslatorComposer::compute_circuit_size_parameters(CircuitBuilder& c
  * @return std::vector<Polynomial>
  * */
 
-void GoblinTranslatorComposer::construct_wire_polynomials_base_goblin_translator(const CircuitBuilder& circuit_builder)
+std::vector<Polynomial> construct_wire_polynomials_base_goblin_translator(const CircuitBuilder& circuit_builder,
+                                                                          const size_t dyadic_circuit_size)
 {
+    const size_t num_gates = circuit_builder.num_gates;
+
+    std::vector<Polynomial> wire_polynomials;
+
     // Populate the wire polynomials with values from conventional wires
-    // WORKTODO: leave note on moving these vectors instead
-    for (auto [poly, wire] : zip_view(proving_key->get_wires(), circuit_builder.wires)) {
-        std::copy(wire.begin(), wire.end(), poly.begin());
+    for (size_t wire_idx = 0; wire_idx < Flavor::NUM_WIRES; ++wire_idx) {
+
+        // Expect all values to be set to 0 initially
+        // TODO(kesha): fix for sparse polynomials; WORKTODO: what's this?
+        Polynomial w_lagrange(dyadic_circuit_size);
+
+        // Insert conventional gate wire values into the wire polynomial
+        for (size_t i = 0; i < num_gates; ++i) {
+            auto& wire = circuit_builder.wires[wire_idx];
+            w_lagrange[i] = circuit_builder.get_variable(wire[i]);
+        }
+
+        wire_polynomials.push_back(std::move(w_lagrange));
     }
+    return wire_polynomials;
 }
 
 /**
  * @brief Compute witness polynomials
+ *
  */
-void GoblinTranslatorComposer::compute_witness(const CircuitBuilder& circuit_builder)
+void GoblinTranslatorComposer::compute_witness(CircuitBuilder& circuit_builder)
 {
     if (computed_witness) {
         return;
     }
 
     // Construct the conventional wire polynomials
-    construct_wire_polynomials_base_goblin_translator(circuit_builder);
+    auto wire_polynomials = construct_wire_polynomials_base_goblin_translator(circuit_builder, dyadic_circuit_size);
+
+    // Most of the witness polynomials are the original wire polynomials
+    proving_key->op = wire_polynomials[0];
+    proving_key->x_lo_y_hi = wire_polynomials[1];
+    proving_key->x_hi_z_1 = wire_polynomials[2];
+    proving_key->y_lo_z_2 = wire_polynomials[3];
+    proving_key->p_x_low_limbs = wire_polynomials[4];
+    proving_key->p_x_low_limbs_range_constraint_0 = wire_polynomials[5];
+    proving_key->p_x_low_limbs_range_constraint_1 = wire_polynomials[6];
+    proving_key->p_x_low_limbs_range_constraint_2 = wire_polynomials[7];
+    proving_key->p_x_low_limbs_range_constraint_3 = wire_polynomials[8];
+    proving_key->p_x_low_limbs_range_constraint_4 = wire_polynomials[9];
+    proving_key->p_x_low_limbs_range_constraint_tail = wire_polynomials[10];
+    proving_key->p_x_high_limbs = wire_polynomials[11];
+    proving_key->p_x_high_limbs_range_constraint_0 = wire_polynomials[12];
+    proving_key->p_x_high_limbs_range_constraint_1 = wire_polynomials[13];
+    proving_key->p_x_high_limbs_range_constraint_2 = wire_polynomials[14];
+    proving_key->p_x_high_limbs_range_constraint_3 = wire_polynomials[15];
+    proving_key->p_x_high_limbs_range_constraint_4 = wire_polynomials[16];
+    proving_key->p_x_high_limbs_range_constraint_tail = wire_polynomials[17];
+    proving_key->p_y_low_limbs = wire_polynomials[18];
+    proving_key->p_y_low_limbs_range_constraint_0 = wire_polynomials[19];
+    proving_key->p_y_low_limbs_range_constraint_1 = wire_polynomials[20];
+    proving_key->p_y_low_limbs_range_constraint_2 = wire_polynomials[21];
+    proving_key->p_y_low_limbs_range_constraint_3 = wire_polynomials[22];
+    proving_key->p_y_low_limbs_range_constraint_4 = wire_polynomials[23];
+    proving_key->p_y_low_limbs_range_constraint_tail = wire_polynomials[24];
+    proving_key->p_y_high_limbs = wire_polynomials[25];
+    proving_key->p_y_high_limbs_range_constraint_0 = wire_polynomials[26];
+    proving_key->p_y_high_limbs_range_constraint_1 = wire_polynomials[27];
+    proving_key->p_y_high_limbs_range_constraint_2 = wire_polynomials[28];
+    proving_key->p_y_high_limbs_range_constraint_3 = wire_polynomials[29];
+    proving_key->p_y_high_limbs_range_constraint_4 = wire_polynomials[30];
+    proving_key->p_y_high_limbs_range_constraint_tail = wire_polynomials[31];
+    proving_key->z_low_limbs = wire_polynomials[32];
+    proving_key->z_low_limbs_range_constraint_0 = wire_polynomials[33];
+    proving_key->z_low_limbs_range_constraint_1 = wire_polynomials[34];
+    proving_key->z_low_limbs_range_constraint_2 = wire_polynomials[35];
+    proving_key->z_low_limbs_range_constraint_3 = wire_polynomials[36];
+    proving_key->z_low_limbs_range_constraint_4 = wire_polynomials[37];
+    proving_key->z_low_limbs_range_constraint_tail = wire_polynomials[38];
+    proving_key->z_high_limbs = wire_polynomials[39];
+    proving_key->z_high_limbs_range_constraint_0 = wire_polynomials[40];
+    proving_key->z_high_limbs_range_constraint_1 = wire_polynomials[41];
+    proving_key->z_high_limbs_range_constraint_2 = wire_polynomials[42];
+    proving_key->z_high_limbs_range_constraint_3 = wire_polynomials[43];
+    proving_key->z_high_limbs_range_constraint_4 = wire_polynomials[44];
+    proving_key->z_high_limbs_range_constraint_tail = wire_polynomials[45];
+    proving_key->accumulators_binary_limbs_0 = wire_polynomials[46];
+    proving_key->accumulators_binary_limbs_1 = wire_polynomials[47];
+    proving_key->accumulators_binary_limbs_2 = wire_polynomials[48];
+    proving_key->accumulators_binary_limbs_3 = wire_polynomials[49];
+    proving_key->accumulator_low_limbs_range_constraint_0 = wire_polynomials[50];
+    proving_key->accumulator_low_limbs_range_constraint_1 = wire_polynomials[51];
+    proving_key->accumulator_low_limbs_range_constraint_2 = wire_polynomials[52];
+    proving_key->accumulator_low_limbs_range_constraint_3 = wire_polynomials[53];
+    proving_key->accumulator_low_limbs_range_constraint_4 = wire_polynomials[54];
+    proving_key->accumulator_low_limbs_range_constraint_tail = wire_polynomials[55];
+    proving_key->accumulator_high_limbs_range_constraint_0 = wire_polynomials[56];
+    proving_key->accumulator_high_limbs_range_constraint_1 = wire_polynomials[57];
+    proving_key->accumulator_high_limbs_range_constraint_2 = wire_polynomials[58];
+    proving_key->accumulator_high_limbs_range_constraint_3 = wire_polynomials[59];
+    proving_key->accumulator_high_limbs_range_constraint_4 = wire_polynomials[60];
+    proving_key->accumulator_high_limbs_range_constraint_tail = wire_polynomials[61];
+    proving_key->quotient_low_binary_limbs = wire_polynomials[62];
+    proving_key->quotient_high_binary_limbs = wire_polynomials[63];
+    proving_key->quotient_low_limbs_range_constraint_0 = wire_polynomials[64];
+    proving_key->quotient_low_limbs_range_constraint_1 = wire_polynomials[65];
+    proving_key->quotient_low_limbs_range_constraint_2 = wire_polynomials[66];
+    proving_key->quotient_low_limbs_range_constraint_3 = wire_polynomials[67];
+    proving_key->quotient_low_limbs_range_constraint_4 = wire_polynomials[68];
+    proving_key->quotient_low_limbs_range_constraint_tail = wire_polynomials[69];
+    proving_key->quotient_high_limbs_range_constraint_0 = wire_polynomials[70];
+    proving_key->quotient_high_limbs_range_constraint_1 = wire_polynomials[71];
+    proving_key->quotient_high_limbs_range_constraint_2 = wire_polynomials[72];
+    proving_key->quotient_high_limbs_range_constraint_3 = wire_polynomials[73];
+    proving_key->quotient_high_limbs_range_constraint_4 = wire_polynomials[74];
+    proving_key->quotient_high_limbs_range_constraint_tail = wire_polynomials[75];
+    proving_key->relation_wide_limbs = wire_polynomials[76];
+    proving_key->relation_wide_limbs_range_constraint_0 = wire_polynomials[77];
+    proving_key->relation_wide_limbs_range_constraint_1 = wire_polynomials[78];
+    proving_key->relation_wide_limbs_range_constraint_2 = wire_polynomials[79];
+    proving_key->relation_wide_limbs_range_constraint_3 = wire_polynomials[80];
 
     // We construct concatenated versions of range constraint polynomials, where several polynomials are concatenated
     // into one. These polynomials are not commited to.
