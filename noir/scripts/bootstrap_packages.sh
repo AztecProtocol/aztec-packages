ff22ed05c61f7259fca262c936d170ba52e3a356
Charlie Lye <karl.lye@gmail.com>
2023-11-24 17:47:44 +0000

    Fix.

--- noir/scripts/bootstrap_packages.sh
+++ noir/scripts/bootstrap_packages.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 set -eu
 
-cd $(dirname "$0")/..
+cd $(dirname "$0")
 
-# If this project has been subrepod into another project, set build data manually.
-if [ -f ".gitrepo" ]; then
-  export SOURCE_DATE_EPOCH=$(date +%s)
-  export GIT_DIRTY=false
-  export GIT_COMMIT=$(awk '/commit =/ {print $3}' .gitrepo)
-fi
-
-# Install wasm-bindgen-cli.
-if [ "$(wasm-bindgen --version | cut -d' ' -f2)" != "0.2.86" ]; then
-  echo "Building wasm-bindgen..."
-  RUSTFLAGS="-Ctarget-feature=-crt-static" cargo install -f wasm-bindgen-cli --version 0.2.86
-fi
-
-# Build wasm and packages.
-yarn
-export cargoExtraArgs="--features noirc_frontend/aztec"
-yarn workspace @noir-lang/acvm_js build
-yarn workspace @noir-lang/noir_wasm build
-yarn workspace @noir-lang/source-resolver build
-yarn workspace @noir-lang/types build
-yarn workspace @noir-lang/noirc_abi build
-yarn workspace @noir-lang/backend_barretenberg build
-yarn workspace @noir-lang/noir_js build
+./bootstrap_package.sh @noir-lang/acvm_js
+./bootstrap_package.sh @noir-lang/noir_wasm
+./bootstrap_package.sh @noir-lang/types
+./bootstrap_package.sh @noir-lang/noirc_abi
+./bootstrap_package.sh @noir-lang/source-resolver
+./bootstrap_package.sh @noir-lang/backend_barretenberg
+./bootstrap_package.sh @noir-lang/noir_js
