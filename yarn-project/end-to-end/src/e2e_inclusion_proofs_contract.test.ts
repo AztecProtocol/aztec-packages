cf14859b8937be5106668bcd8454cc3b5c9e8d00
benesjan <janbenes1234@gmail.com>
2023-11-24 09:40:22 +0000

    cleanup

--- yarn-project/end-to-end/src/e2e_inclusion_proofs_contract.test.ts
+++ yarn-project/end-to-end/src/e2e_inclusion_proofs_contract.test.ts
@@ -1,4 +1,4 @@
-import { AccountWallet, CompleteAddress, DebugLogger, PXE } from '@aztec/aztec.js';
+import { AccountWallet, CompleteAddress, PXE } from '@aztec/aztec.js';
 import { InclusionProofsContract } from '@aztec/noir-contracts/types';
 
 import { jest } from '@jest/globals';
@@ -15,14 +15,13 @@ describe('e2e_inclusion_proofs_contract', () => {
 
   let pxe: PXE;
   let teardown: () => Promise<void>;
-  let logger: DebugLogger;
   let wallets: AccountWallet[];
   let accounts: CompleteAddress[];
 
   let contract: InclusionProofsContract;
 
   beforeAll(async () => {
-    ({ pxe, teardown, logger, wallets, accounts } = await setup(1));
+    ({ pxe, teardown, wallets, accounts } = await setup(1));
 
     contract = await InclusionProofsContract.deploy(wallets[0]).send().deployed();
   }, 100_000);
@@ -48,7 +47,10 @@ describe('e2e_inclusion_proofs_contract', () => {
       // Prove note inclusion in a given block.
       // We prove the note existence at current block number because we don't currently have historical data
       const blockNumber = await pxe.getBlockNumber();
-      const receipt = await contract.methods.proveNoteInclusion(accounts[0].address, blockNumber).send().wait({ debug: true });
+      const receipt = await contract.methods
+        .proveNoteInclusion(accounts[0].address, blockNumber)
+        .send()
+        .wait({ debug: true });
       const { newNullifiers } = receipt.debugInfo!;
       expect(newNullifiers.length).toBe(2); // tx hash and note nullifier
     }
