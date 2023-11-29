944ac6129d441a20ae710b4eef2b562a09529a1e
Charlie Lye <karl.lye@gmail.com>
2023-11-29 09:08:05 +0000

    Initial.

--- barretenberg/ts/src/barretenberg_wasm/index.ts
+++ barretenberg/ts/src/barretenberg_wasm/index.ts
@@ -1,10 +1,25 @@
 import { proxy } from 'comlink';
 import createDebug from 'debug';
 import { createMainWorker } from './barretenberg_wasm_main/factory/node/index.js';
-import { getRemoteBarretenbergWasm } from './helpers/node/index.js';
+import { getRemoteBarretenbergWasm, getSharedMemoryAvailable } from './helpers/node/index.js';
 import { BarretenbergWasmMain, BarretenbergWasmMainWorker } from './barretenberg_wasm_main/index.js';
+import { fetchCode } from './fetch_code/index.js';
 
 const debug = createDebug('bb.js:wasm');
+
+export async function fetchModule(threads?: number) {
+  const shared = getSharedMemoryAvailable();
+  if (!shared) {
+    if (!threads) {
+      threads = 1;
+    } else if (threads > 1) {
+      throw new Error('Cannot request > 1 thread when no shared memory available.');
+    }
+  }
+  const code = await fetchCode(shared);
+  const module = await WebAssembly.compile(code);
+  return module;
+}
 
 export class BarretenbergWasm extends BarretenbergWasmMain {
   /**
@@ -14,7 +29,8 @@ export class BarretenbergWasm extends BarretenbergWasmMain {
   public static async new(threads?: number) {
     const worker = createMainWorker();
     const wasm = getRemoteBarretenbergWasm<BarretenbergWasmMainWorker>(worker);
-    await wasm.init(threads, proxy(debug));
+    const module = await fetchModule(threads);
+    await wasm.init(module, threads, proxy(debug));
     return { worker, wasm };
   }
 }
