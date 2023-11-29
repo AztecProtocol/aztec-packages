e55011d6d6cf96a7587eb4eb2f71a40eedd050fe
sirasistant <sirasistant@gmail.com>
2023-11-29 11:06:12 +0000

    chore: fix paths for test programs

--- barretenberg/acir_tests/clone_test_vectors.sh
+++ barretenberg/acir_tests/clone_test_vectors.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -eu
 
-TEST_SRC=${TEST_SRC:-../../noir/test_programs}
+TEST_SRC=${TEST_SRC:-../../noir/test_programs/acir_artifacts}
 
 if [ ! -d acir_tests ]; then
-  cp -R $TEST_SRC/acir_artifacts acir_tests
+  cp -R $TEST_SRC acir_tests
 fi
-
-cp $TEST_SRC/rebuild.sh .
