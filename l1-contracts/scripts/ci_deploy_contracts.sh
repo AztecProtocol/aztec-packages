e413b9c1642d633e4d50ed6e75de4661b2a5948d
spypsy <spypsy@outlook.com>
2023-12-04 15:13:36 +0000

    fix(ci): l1-contract directories

--- l1-contracts/scripts/ci_deploy_contracts.sh
+++ l1-contracts/scripts/ci_deploy_contracts.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 export ETHEREUM_HOST=https://$DEPLOY_TAG-mainnet-fork.aztec.network:8545/$FORK_API_KEY
-# export ETHEREUM_HOST=http://localhost:8545
 
 REPOSITORY="l1-contracts"
 
-# CONTENT_HASH=$(calculate_content_hash $REPOSITORY)
+CONTENT_HASH=$(calculate_content_hash $REPOSITORY)
 
-# echo "Last successfully published commit: $CONTENT_HASH"
+echo "Last successfully published commit: $CONTENT_HASH"
 
-# # Check if image hash has alredy been deployed.
-# if check_rebuild "cache-$CONTENT_HASH-$DEPLOY_TAG-deployed" $REPOSITORY; then
-#   echo "No changes detected, no contract deploy necessary."
-#   exit 0
-# fi
+# Check if image hash has alredy been deployed.
+if check_rebuild "cache-$CONTENT_HASH-$DEPLOY_TAG-deployed" $REPOSITORY; then
+  echo "No changes detected, no contract deploy necessary."
+  exit 0
+fi
 
 # Login to pull our ecr images with docker.
 ecr_login
@@ -21,7 +20,7 @@ ecr_login
 mkdir -p serve
 # Contract addresses will be mounted in the serve directory
 docker run \
-  -v $(pwd)/serve:/usr/src/contracts/serve \
+  -v $(pwd)/serve:/usr/src/l1-contracts/serve \
   -e ETHEREUM_HOST=$ETHEREUM_HOST -e PRIVATE_KEY=$CONTRACT_PUBLISHER_PRIVATE_KEY \
   "$ECR_URL/l1-contracts:cache-$CONTENT_HASH"-x86_64 \
   ./scripts/deploy_contracts.sh
@@ -29,11 +28,11 @@ docker run \
 # Write the contract addresses as terraform variables
 for KEY in ROLLUP_CONTRACT_ADDRESS REGISTRY_CONTRACT_ADDRESS INBOX_CONTRACT_ADDRESS OUTBOX_CONTRACT_ADDRESS; do
   VALUE=$(jq -r .$KEY ./serve/contract_addresses.json)
-  export TF_VAR_$KEY=$VALUE
+  echo "TF_VAR_$KEY=$VALUE"
 done
 
 # # Write TF state variables
-# deploy_terraform l1-contracts ./terraform
+deploy_terraform l1-contracts ./terraform
 
 # # Tag the image as deployed.
-# retry tag_remote_image $REPOSITORY cache-$CONTENT_HASH cache-$CONTENT_HASH-$DEPLOY_TAG-deployed
+retry tag_remote_image $REPOSITORY cache-$CONTENT_HASH cache-$CONTENT_HASH-$DEPLOY_TAG-deployed
