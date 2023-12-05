1a16e10a123195002fe13522bf7cced00310636d
ludamad <adam@aztecprotocol.com>
2023-12-05 20:10:26 +0000

    temporary simple docker sanity test

--- barretenberg/cpp/src/barretenberg/benchmark/honk_bench/ultra_honk.simple.cpp
+++ barretenberg/cpp/src/barretenberg/benchmark/honk_bench/ultra_honk.simple.cpp
@@ -1,21 +1,69 @@
-#include "barretenberg/benchmark/honk_bench/benchmark_utilities.hpp"
-#include "barretenberg/proof_system/circuit_builder/ultra_circuit_builder.hpp"
-#include "barretenberg/ultra_honk/ultra_composer.hpp"
+#include "barretenberg/numeric/uint256/uint256.hpp"
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
 
-using namespace benchmark;
-using namespace proof_system;
+// Function for heavy math computation
+static numeric::uint256_t heavyMathOperation(size_t n)
+{
+    numeric::uint256_t result = 1;
+    for (size_t i = 1; i <= n; ++i) {
+        result *= i;
+        // Additional computation layer
+        for (size_t j = 0; j <= n; ++j) {
+            result += j;
+        }
+    }
+    return result;
+}
+
+static std::mutex mutex;
+
+// Thread function
+static void threadFunction(int threadId, size_t n)
+{
+    // Start timing
+    auto start = std::chrono::high_resolution_clock::now();
+
+    auto result = heavyMathOperation(n);
+
+    // End timing
+    auto end = std::chrono::high_resolution_clock::now();
+    // Calculate elapsed time
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    mutex.lock();
+    std::cout << "Thread " << threadId << " result: " << result << std::endl;
+    std::cout << "Elapsed time: " << duration.count() << " ms" << std::endl;
+    mutex.unlock();
+}
 
 int main()
 {
-    barretenberg::srs::init_crs_factory("../srs_db/ignition");
+    // Start timing
+    auto start = std::chrono::high_resolution_clock::now();
+    const int numThreads = 128;
 
-    honk::UltraComposer composer;
-    size_t log2_of_gates = 20;
-    auto prover = bench_utils::get_prover(
-        composer, &bench_utils::generate_basic_arithmetic_circuit<UltraCircuitBuilder>, log2_of_gates);
-    for (int i = 0; i < 10; i++) {
-        // Construct proof
-        auto proof = prover.construct_proof();
+    // Creating threads
+    std::vector<std::thread> threads;
+    threads.reserve(numThreads);
+    for (int i = 0; i < numThreads; ++i) {
+        threads.emplace_back(threadFunction, i, 100000 + i);
     }
+
+    // Joining threads
+    for (auto& th : threads) {
+        th.join();
+    }
+    // End timing
+    auto end = std::chrono::high_resolution_clock::now();
+    // Calculate elapsed time
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    mutex.lock();
+    std::cout << "Total time: " << duration.count() << " ms" << std::endl;
+    mutex.unlock();
+
     return 0;
 }
