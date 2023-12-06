dc5f3f9955f3cd813072b26f62ed25775544dad7
lucasxia01 <lucasxia01@gmail.com>
2023-12-06 22:17:01 +0000

    hash_buffers function to native poseidon2 and update to hash input type

--- barretenberg/cpp/src/barretenberg/crypto/poseidon2/poseidon2.hpp
+++ barretenberg/cpp/src/barretenberg/crypto/poseidon2/poseidon2.hpp
@@ -11,6 +11,39 @@ template <typename Params> class Poseidon2 {
     using FF = typename Params::FF;
 
     using Sponge = FieldSponge<FF, Params::t - 1, 1, Params::t, Poseidon2Permutation<Params>>;
-    static FF hash(std::span<FF> input) { return Sponge::hash_fixed_length(input); }
+    static FF hash(const std::vector<FF>& input)
+    {
+        auto input_span = input;
+        return Sponge::hash_fixed_length(input_span);
+    }
+    static FF hash_buffer(const std::vector<uint8_t>& input)
+    {
+        const size_t num_bytes = input.size();
+        const size_t bytes_per_element = 31;
+        size_t num_elements = static_cast<size_t>(num_bytes % bytes_per_element != 0) + (num_bytes / bytes_per_element);
+
+        const auto slice = [](const std::vector<uint8_t>& data, const size_t start, const size_t slice_size) {
+            uint256_t result(0);
+            for (size_t i = 0; i < slice_size; ++i) {
+                result = (result << uint256_t(8));
+                result += uint256_t(data[i + start]);
+            }
+            return FF(result);
+        };
+
+        std::vector<FF> converted;
+        for (size_t i = 0; i < num_elements; ++i) {
+            size_t bytes_to_slice = 0;
+            if (i == num_elements - 1) {
+                bytes_to_slice = num_bytes - (i * bytes_per_element);
+            } else {
+                bytes_to_slice = bytes_per_element;
+            }
+            FF element = slice(input, i * bytes_per_element, bytes_to_slice);
+            converted.emplace_back(element);
+        }
+
+        return hash(converted);
+    }
 };
 } // namespace crypto
--- barretenberg/cpp/src/barretenberg/crypto/poseidon2/poseidon2.test.cpp
+++ barretenberg/cpp/src/barretenberg/crypto/poseidon2/poseidon2.test.cpp
@@ -39,7 +39,7 @@ TEST(Poseidon2, ConsistencyCheck)
     barretenberg::fr c(std::string("0x9a807b615c4d3e2fa0b1c2d3e4f56789fedcba9876543210abcdef0123456789"));
     barretenberg::fr d(std::string("0x9a807b615c4d3e2fa0b1c2d3e4f56789fedcba9876543210abcdef0123456789"));
 
-    std::array<barretenberg::fr, 4> input{ a, b, c, d };
+    std::vector<barretenberg::fr> input{ a, b, c, d };
     auto result = crypto::Poseidon2<crypto::Poseidon2Bn254ScalarFieldParams>::hash(input);
 
     barretenberg::fr expected(std::string("0x150c19ae11b3290c137c7a4d760d9482a6581d731535f560c3601d6a766b0937"));
