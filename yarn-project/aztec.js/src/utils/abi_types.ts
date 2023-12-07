6eab0fb48465d0b881d508eb0964235ae86f2a56
benesjan <janbenes1234@gmail.com>
2023-12-07 15:04:13 +0000

    more useful FunctionSelectorLike

--- yarn-project/aztec.js/src/utils/abi_types.ts
+++ yarn-project/aztec.js/src/utils/abi_types.ts
@@ -10,4 +10,4 @@ export type EthAddressLike = { /** Wrapped address */ address: FieldLike } | Eth
 export type AztecAddressLike = { /** Wrapped address */ address: FieldLike } | AztecAddress;
 
 /** Any type that can be converted into an FunctionSelector Aztec.nr struct. */
-export type FunctionSelectorLike = { /** Wrapped selector */ selector: FieldLike } | FunctionSelector;
+export type FunctionSelectorLike = FieldLike | FunctionSelector;
