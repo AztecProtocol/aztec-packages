79fc7dd740be35ebc786130e6c076a0b1db1379b
ledwards2225 <l.edwards.d@gmail.com>
2023-12-13 21:23:17 +0000

    templated necessary acir format fctns

--- barretenberg/cpp/src/barretenberg/dsl/acir_proofs/acir_composer.hpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_proofs/acir_composer.hpp
@@ -50,6 +50,7 @@ class AcirComposer {
 
   private:
     acir_format::Builder builder_;
+    // acir_format::GoblinBuilder goblin_builder_;
     Goblin goblin;
     size_t size_hint_;
     size_t exact_circuit_size_;
