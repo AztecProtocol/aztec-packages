15caf36739e2b11af3067875dda8ad6b889bb096
ledwards2225 <l.edwards.d@gmail.com>
2023-12-17 21:01:26 +0000

    debug utility uses labels and db relation passes

--- barretenberg/cpp/src/barretenberg/proof_system/debug_utility.hpp
+++ barretenberg/cpp/src/barretenberg/proof_system/debug_utility.hpp
@@ -7,33 +7,37 @@ namespace debug_utility {
 // Determine whether a polynomial has at least one non-zero coefficient
 bool is_non_zero(auto& polynomial)
 {
-    bool has_non_zero_coefficient = false;
     for (auto& coeff : polynomial) {
-        has_non_zero_coefficient |= !coeff.is_zero();
+        if (!coeff.is_zero()) {
+            return true;
+        }
     }
-    return has_non_zero_coefficient;
+    return false;
 }
 
+/**
+ * @brief Utility for indicating which polynomials in a prover instance are identically zero
+ *
+ * @param prover_instance
+ */
 void inspect_instance(auto& prover_instance)
 {
-    bool prover_polys_nonzero = true;
-    size_t poly_idx = 0;
-    std::vector<size_t> zero_polys;
-    for (auto poly : prover_instance->prover_polynomials.get_all()) {
+    auto& prover_polys = prover_instance->prover_polynomials;
+    std::vector<std::string> zero_polys;
+    for (auto [label, poly] : zip_view(prover_polys.get_labels(), prover_polys.get_all())) {
         if (!is_non_zero(poly)) {
-            prover_polys_nonzero = false;
-            zero_polys.emplace_back(poly_idx);
+            zero_polys.emplace_back(label);
         }
-        poly_idx++;
     }
-    if (prover_polys_nonzero) {
+    if (zero_polys.empty()) {
         info("\nDebug Utility: All prover polynomials are non-zero.");
     } else {
-        info("\nDebug Utility: The following prover polynomials are idenitcally zero: ");
-        for (size_t poly_idx : zero_polys) {
-            info("\tPolynomial index: ", poly_idx);
+        info("\nDebug Utility: The following prover polynomials are identically zero: ");
+        for (const std::string& label : zero_polys) {
+            info("\t", label);
         }
     }
+    info();
 }
 
 } // namespace debug_utility
