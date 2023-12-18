a219cc7ba481bb0925f3041fd8a9899b36667b75
sirasistant <sirasistant@gmail.com>
2023-12-18 15:21:04 +0000

    git subrepo commit (merge) noir
    subrepo:
      subdir:   "noir"
      merged:   "3dc256911"
    upstream:
      origin:   "https://github.com/noir-lang/noir"
      branch:   "aztec-packages"
      commit:   "2d265b430"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

--- noir/tooling/nargo/src/ops/compile.rs
+++ noir/tooling/nargo/src/ops/compile.rs
@@ -1,4 +1,4 @@
-use acvm::{acir::circuit::Opcode, Language};
+use acvm::Language;
 use fm::FileManager;
 use noirc_driver::{CompilationResult, CompileOptions, CompiledContract, CompiledProgram};
 
@@ -18,22 +18,17 @@ pub fn compile_workspace(
     binary_packages: &[Package],
     contract_packages: &[Package],
     np_language: Language,
-    is_opcode_supported: impl Fn(&Opcode) -> bool + std::marker::Sync,
     compile_options: &CompileOptions,
 ) -> Result<(Vec<CompiledProgram>, Vec<CompiledContract>), CompileError> {
     // Compile all of the packages in parallel.
     let program_results: Vec<(FileManager, CompilationResult<CompiledProgram>)> = binary_packages
         .par_iter()
-        .map(|package| {
-            compile_program(workspace, package, compile_options, np_language, &is_opcode_supported)
-        })
+        .map(|package| compile_program(workspace, package, compile_options, np_language))
         .collect();
     let contract_results: Vec<(FileManager, CompilationResult<CompiledContract>)> =
         contract_packages
             .par_iter()
-            .map(|package| {
-                compile_contract(package, compile_options, np_language, &is_opcode_supported)
-            })
+            .map(|package| compile_contract(package, compile_options, np_language))
             .collect();
 
     // Report any warnings/errors which were encountered during compilation.
@@ -68,10 +63,8 @@ pub fn compile_program(
     package: &Package,
     compile_options: &CompileOptions,
     np_language: Language,
-    is_opcode_supported: &impl Fn(&Opcode) -> bool,
 ) -> (FileManager, CompilationResult<CompiledProgram>) {
-    let (mut context, crate_id) =
-        prepare_package(package, Box::new(|path| std::fs::read_to_string(path)));
+    let (mut context, crate_id) = prepare_package(package);
 
     let program_artifact_path = workspace.package_build_path(package);
     let mut debug_artifact_path = program_artifact_path.clone();
@@ -85,22 +78,8 @@ pub fn compile_program(
             }
         };
 
-    // TODO: we say that pedersen hashing is supported by all backends for now
-    let is_opcode_supported_pedersen_hash = |opcode: &Opcode| -> bool {
-        if let Opcode::BlackBoxFuncCall(
-            acvm::acir::circuit::opcodes::BlackBoxFuncCall::PedersenHash { .. },
-        ) = opcode
-        {
-            true
-        } else {
-            is_opcode_supported(opcode)
-        }
-    };
-
     // Apply backend specific optimizations.
-    let optimized_program =
-        crate::ops::optimize_program(program, np_language, &is_opcode_supported_pedersen_hash)
-            .expect("Backend does not support an opcode that is in the IR");
+    let optimized_program = crate::ops::optimize_program(program, np_language);
 
     (context.file_manager, Ok((optimized_program, warnings)))
 }
@@ -109,10 +88,8 @@ fn compile_contract(
     package: &Package,
     compile_options: &CompileOptions,
     np_language: Language,
-    is_opcode_supported: &impl Fn(&Opcode) -> bool,
 ) -> (FileManager, CompilationResult<CompiledContract>) {
-    let (mut context, crate_id) =
-        prepare_package(package, Box::new(|path| std::fs::read_to_string(path)));
+    let (mut context, crate_id) = prepare_package(package);
     let (contract, warnings) =
         match noirc_driver::compile_contract(&mut context, crate_id, compile_options) {
             Ok(contracts_and_warnings) => contracts_and_warnings,
@@ -121,9 +98,7 @@ fn compile_contract(
             }
         };
 
-    let optimized_contract =
-        crate::ops::optimize_contract(contract, np_language, &is_opcode_supported)
-            .expect("Backend does not support an opcode that is in the IR");
+    let optimized_contract = crate::ops::optimize_contract(contract, np_language);
 
     (context.file_manager, Ok((optimized_contract, warnings)))
 }
