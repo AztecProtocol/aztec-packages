a3872fe7861c057c055d43678e2452c8a9246335
ludamad <adam@aztecprotocol.com>
2023-12-18 18:16:40 +0000

    merge

--- barretenberg/cpp/src/barretenberg/goblin/goblin.cpp
+++ barretenberg/cpp/src/barretenberg/goblin/goblin.cpp
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-=======
-// WORKTODO(ADAM)?
->>>>>>> origin/cg-lde/expose-goblin
 // NB: This file is here so that goblin_objects will be created
 // WORKTODO: actually just implement some stuff in here.
