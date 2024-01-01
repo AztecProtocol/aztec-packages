35c7ee5bbfae11dd025416bef10fe24e1b9bded8
Charlie Lye <karl.lye@gmail.com>
2024-01-01 15:25:29 +0000

    cleanup

--- yarn-project/noir-contracts/scripts/generate-types.sh
+++ yarn-project/noir-contracts/scripts/generate-types.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-OUT_DIR="./src/types"
+OUT_DIR="./src"
 INDEX="$OUT_DIR/index.ts"
 
 rm -rf $OUT_DIR && mkdir -p $OUT_DIR
 
 # Generate index.ts header.
-echo "// Auto generated module - do not edit!\n" > $INDEX
+# echo "// Auto generated module - do not edit!\n" > $INDEX
 
 for ABI in $(find target -maxdepth 1 -type f ! -name 'debug_*' -name '*.json'); do
   CONTRACT=$(jq -r .name $ABI)
   DEBUG_INFO="$(dirname $ABI)/debug_$(basename $ABI)"
 
   echo "Creating types for $CONTRACT in $ABI..."
-  node --no-warnings ../noir-compiler/dest/cli.js generate-typescript -o ./src/types $ABI
+  node --no-warnings ../noir-compiler/dest/cli.js generate-typescript -o $OUT_DIR $ABI
 
   # Add contract import/export to index.ts.
-  echo "export * from './${CONTRACT}.js';" >> $INDEX
+  # echo "export * from './${CONTRACT}.js';" >> $INDEX
 done
 
 echo "Formatting..."
-yarn run -T prettier -w $OUT_DIR
+yarn formatting:fix
