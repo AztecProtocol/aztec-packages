453ce511648ddf4748a9c5ed3f67b28f5f052e9a
benesjan <janbenes1234@gmail.com>
2024-01-02 15:54:32 +0000

    formatting fix

--- yarn-project/types/src/interfaces/nullifier_tree.ts
+++ yarn-project/types/src/interfaces/nullifier_tree.ts
@@ -29,10 +29,6 @@ export class NullifierMembershipWitness {
    * @returns A field array representation of a nullifier witness.
    */
   public toFieldArray(): Fr[] {
-    return [
-      new Fr(this.index),
-      ...this.leafPreimage.toFieldArray(),
-      ...this.siblingPath.toFieldArray(),
-    ];
+    return [new Fr(this.index), ...this.leafPreimage.toFieldArray(), ...this.siblingPath.toFieldArray()];
   }
 }
