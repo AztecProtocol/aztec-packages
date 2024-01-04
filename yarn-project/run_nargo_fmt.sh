56f4793e7a2145f76dadf071a2c4800f325567dd
benesjan <janbenes1234@gmail.com>
2024-01-04 11:10:20 +0000

    using nargo from /noir/target/release

--- yarn-project/run_nargo_fmt.sh
+++ yarn-project/run_nargo_fmt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+nargo_executable="$(git rev-parse --show-toplevel)/noir/target/release/nargo"
+
 # Find all Nargo.toml files and run 'nargo fmt'
 find . -name "Nargo.toml" | while read -r file; do
     # Extract the directory from the file path
@@ -9,7 +11,7 @@ find . -name "Nargo.toml" | while read -r file; do
     cd "$dir" || exit
 
     # Run 'nargo fmt' in the directory and pass in the input param 1
-    nargo fmt $1
+    "$nargo_executable" fmt $1
 
     # Change back to the original directory
     cd - > /dev/null
