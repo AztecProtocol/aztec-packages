3f0fad0de81d472cf71c03df11cf01ed6e7e09e0
ludamad <adam@aztecprotocol.com>
2024-01-04 02:49:50 +0000

    Doxygen CI

--- barretenberg/cpp/docs/deploy_doxygen.sh
+++ barretenberg/cpp/docs/deploy_doxygen.sh
@@ -8,7 +8,7 @@ extract_repo barretenberg-docs /usr/src extracted-repo
 
 BUCKET_NAME="aztec-ci-artifacts"
 COMMIT_HASH="${COMMIT_HASH:-$(git rev-parse HEAD)}"
-TARGET_FOLDER="doxygen/$COMMIT_HASH"
+TARGET_FOLDER="barretenberg-doxygen/$COMMIT_HASH"
 
 echo "Uploading to s3://$BUCKET_NAME/$TARGET_FOLDER"
 aws s3 cp extracted-repo/src/barretenberg/cpp/docs/build "s3://$BUCKET_NAME/$TARGET_FOLDER" --recursive
