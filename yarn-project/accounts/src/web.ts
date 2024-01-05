867350ae4ced9004a35d8aba625c0ec6bb157757
Santiago Palladino <santiago@aztecprotocol.com>
2024-01-05 15:12:49 -0300

    Remove unused web build entrypoint for account package

--- yarn-project/accounts/src/web.ts
+++
@@ -1,6 +0,0 @@
-// Index file with all exports for web build
-export * from './defaults/index.js';
-export * from './ecdsa/index.js';
-export * from './schnorr/index.js';
-export * from './single_key/index.js';
-export * from './testing/index.js';
