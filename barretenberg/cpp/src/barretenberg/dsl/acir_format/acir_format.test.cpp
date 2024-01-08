a248f882c2a6a4c205771f35a6a3faca05a5710f
ledwards2225 <l.edwards.d@gmail.com>
2024-01-08 21:46:17 +0000

    fix merge conflict

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/acir_format.test.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/acir_format.test.cpp
@@ -459,7 +459,7 @@ TEST_F(AcirFormatTests, TestKeccakPermutation)
                            18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
                            35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50 };
 
-    auto builder = create_circuit_with_witness(constraint_system, witness);
+    auto builder = create_circuit(constraint_system, /*size_hint=*/0, witness);
 
     auto composer = Composer();
     auto prover = composer.create_ultra_with_keccak_prover(builder);
