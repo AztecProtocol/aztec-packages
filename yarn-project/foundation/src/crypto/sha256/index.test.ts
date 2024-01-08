06ec4cbe998f28a3e1a39d11fae607f179197a41
Dan Lee <dan@aztecprotocol.com>
2024-01-08 16:43:08 -0500

    bad math, dropping byte is actually doing >> 8

--- yarn-project/foundation/src/crypto/sha256/index.test.ts
+++ yarn-project/foundation/src/crypto/sha256/index.test.ts
@@ -1,8 +1,7 @@
 import { createHash, randomBytes } from 'crypto';
 
 import { toBigIntBE } from '../../bigint-buffer/index.js';
-import { Fr } from '../../fields/fields.js';
-import { sha256, sha256ToField } from './index.js';
+import { sha256, sha256TruncateToField } from './index.js';
 
 describe('sha256', () => {
   it('should correctly hash data using hash.js', () => {
@@ -20,11 +19,11 @@ describe('sha256ToField', () => {
     const data = randomBytes(67);
     const shaBytes = sha256(data);
 
-    const unModulated = toBigIntBE(shaBytes) + Fr.MODULUS;
+    expect(shaBytes.byteLength).toEqual(32);
 
-    // const expected = Fr.fromBufferReduce(shaBytes);
+    const bitShifted = toBigIntBE(shaBytes) >> 8n;
 
-    const result = sha256ToField(data);
-    expect(result.toBigInt()).toEqual(unModulated % Fr.MODULUS); //expected);
+    const result = sha256TruncateToField(data);
+    expect(result.toBigInt()).toEqual(bitShifted);
   });
 });
