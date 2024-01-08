a2497685718fef22663931317a51f003b1d51ba5
Santiago Palladino <santiago@aztecprotocol.com>
2024-01-08 11:09:50 -0300

    Add missing folders in bootstrap_cache

--- bootstrap_cache.sh
+++ bootstrap_cache.sh
@@ -4,5 +4,12 @@ cd "$(dirname "$0")"
 
 source ./build-system/scripts/setup_env '' '' mainframe_$USER > /dev/null
 
-extract_repo bb.js /usr/src/barretenberg/ts/dest ./barretenberg/ts
+echo -e "\033[1mBootstrapping bb.js from remote cache...\033[0m"
+extract_repo bb.js \
+  /usr/src/barretenberg/ts/dest ./barretenberg/ts \
+  /usr/src/barretenberg/cpp/build-wasm/bin ./barretenberg/cpp/build-wasm \
+  /usr/src/barretenberg/cpp/build-wasm-threads/bin ./barretenberg/cpp/build-wasm-threads
+
+echo -e "\033[1mBootstrapping Noir from remote cache...\033[0m"
 extract_repo noir-packages /usr/src/noir/packages ./noir
+extract_repo noir /usr/src/noir/target/release ./noir/target
