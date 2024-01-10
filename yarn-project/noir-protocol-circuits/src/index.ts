bc8ad13409967274909f7289af9179a2bde342e6
sirasistant <sirasistant@gmail.com>
2024-01-10 15:38:49 +0000

    chore: remove extraneous comment

--- yarn-project/noir-protocol-circuits/src/index.ts
+++ yarn-project/noir-protocol-circuits/src/index.ts
@@ -140,31 +140,6 @@ export async function executeOrdering(
     input: mapPrivateKernelInputsOrderingToNoir(privateKernelInputsOrdering),
   };
 
-  // console.log(
-  //   privateKernelInputsOrdering.previousKernel.publicInputs.end.newNullifiers
-  //     .filter(sideffect => !sideffect.value.isZero())
-  //     .map(sideffect => ({
-  //       val: sideffect.value.toString(),
-  //       counter: sideffect.counter.toString(),
-  //     })),
-  // );
-  // console.log(
-  //   privateKernelInputsOrdering.previousKernel.publicInputs.end.newCommitments
-  //     .filter(sideffect => !sideffect.value.isZero())
-  //     .map(sideffect => ({
-  //       val: sideffect.value.toString(),
-  //       counter: sideffect.counter.toString(),
-  //     })),
-  // );
-  // console.log(
-  //   privateKernelInputsOrdering.previousKernel.publicInputs.end.readRequests
-  //     .filter(sideffect => !sideffect.value.isZero())
-  //     .map(sideffect => ({
-  //       val: sideffect.value.toString(),
-  //       counter: sideffect.counter.toString(),
-  //     })),
-  // );
-
   const returnType = await executePrivateKernelOrderingWithACVM(params);
 
   return mapKernelCircuitPublicInputsFinalFromNoir(returnType);
