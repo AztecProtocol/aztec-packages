d5629ab56c4037c0e5a0ac35fe3b3ceaa062287d
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-01-10 19:52:13 +0000

    feat: Journal interface

---
+++ yarn-project/acir-simulator/src/avm/journal/host_storage.ts
@@ -0,0 +1,13 @@
+import { CommitmentsDB, PublicContractsDB, PublicStateDB } from "../../index.js";
+
+export class HostStorage {
+    public readonly stateDb: PublicStateDB;
+    public readonly contractsDb: PublicContractsDB;
+    public readonly commitmentsDb: CommitmentsDB;
+
+    constructor(stateDb: PublicStateDB, contractsDb: PublicContractsDB, commitmentsDb: CommitmentsDB) {
+        this.stateDb = stateDb;
+        this.contractsDb = contractsDb;
+        this.commitmentsDb = commitmentsDb;
+    }
+}
---
+++ yarn-project/acir-simulator/src/avm/journal/journal.test.ts
@@ -0,0 +1,17 @@
+
+describe("journal", () => {
+
+    it("Should write to storage", () => {
+
+    });
+
+
+    it("Should read from storage", () => {
+
+    });
+
+    it("Should merge two journals together", () => {
+
+    });
+
+});
