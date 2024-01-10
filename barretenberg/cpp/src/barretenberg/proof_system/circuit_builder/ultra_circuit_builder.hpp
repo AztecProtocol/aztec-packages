a7838395bcad8e743bdacb267862befc69792794
ledwards2225 <l.edwards.d@gmail.com>
2024-01-10 20:20:54 +0000

    comments and new serialize arith fctn

--- barretenberg/cpp/src/barretenberg/proof_system/circuit_builder/ultra_circuit_builder.hpp
+++ barretenberg/cpp/src/barretenberg/proof_system/circuit_builder/ultra_circuit_builder.hpp
@@ -681,11 +681,13 @@ class UltraCircuitBuilder_ : public CircuitBuilderBase<typename Arithmetization:
         w_r().reserve(size_hint);
         w_o().reserve(size_hint);
         w_4().reserve(size_hint);
+
         // TODO(https://github.com/AztecProtocol/barretenberg/issues/816): Once the hardcoded +1 offset is removed from
-        // noir, we'll need to move the addition of the const zwero variable to after the addition of the acir witness
-        // to variables.
+        // noir, we'll need to move the addition of the const zero variable to after the acir witness has been
+        // incorporated into variables.
         this->zero_idx = put_constant_variable(FF::zero());
         this->tau.insert({ DUMMY_TAG, DUMMY_TAG }); // TODO(luke): explain this
+
         for (size_t idx = 0; idx < varnum; ++idx) {
             // Zeros are added for variables whose existence is known but whose values are not yet known. The values may
             // be "set" later on via the assert_equal mechanism.
