0760be882821bf94feeef1f007b409fb3371efaa
codygunton <codygunton@gmail.com>
2024-01-11 19:14:53 +0000

    Implement benmchmark
    % taskset -c 0-15 ./bin/protogalaxy_bench                                                                                                                                            13s ~/barretenberg-cpp/build cg/pg-bench + mainframe
    2024-01-11T19:13:25+00:00
    Running ./bin/protogalaxy_bench
    Run on (128 X 2531.93 MHz CPU s)
    CPU Caches:
      L1 Data 32 KiB (x64)
      L1 Instruction 32 KiB (x64)
      L2 Unified 512 KiB (x64)
      L3 Unified 32768 KiB (x8)
    Load Average: 11.20, 44.69, 40.91
    ------------------------------------------------------
    Benchmark            Time             CPU   Iterations
    ------------------------------------------------------
    fold_one/13        286 ms          188 ms            5
    fold_one/14        427 ms          275 ms            3
    fold_one/15       1654 ms          608 ms            2
    fold_one/16       2318 ms         1101 ms            1
    fold_one/17       4659 ms         2256 ms            1
    fold_one/18       7917 ms         4106 ms            1
    % taskset -c 0-31 ./bin/protogalaxy_bench                                                                                                                                            30s ~/barretenberg-cpp/build cg/pg-bench + mainframe

--- barretenberg/cpp/src/barretenberg/benchmark/protogalaxy_bench/protogalaxy.bench.cpp
+++ barretenberg/cpp/src/barretenberg/benchmark/protogalaxy_bench/protogalaxy.bench.cpp
@@ -5,19 +5,47 @@
 #include "barretenberg/ultra_honk/ultra_composer.hpp"
 
 using namespace benchmark;
-using namespace proof_system;
 
-/**
- * @brief Benchmark: Construction of a Ultra Plonk proof with 2**n gates
- */
-static void construct_proof_ultrahonk_power_of_2(State& state) noexcept
+namespace proof_system::honk {
+using Flavor = flavor::Ultra;
+using Instance = ProverInstance_<Flavor>;
+using Instances = ProverInstances_<Flavor, 2>;
+using ProtoGalaxyProver = ProtoGalaxyProver_<Instances>;
+using FF = Flavor::FF;
+using Affine = Flavor::Commitment;
+using Projective = Flavor::GroupElement;
+using Builder = Flavor::CircuitBuilder;
+using Polynomial = typename Flavor::Polynomial;
+using ProverPolynomials = Flavor::ProverPolynomials;
+using RelationParameters = proof_system::RelationParameters<FF>;
+using WitnessCommitments = typename Flavor::WitnessCommitments;
+using CommitmentKey = Flavor::CommitmentKey;
+using PowPolynomial = barretenberg::PowPolynomial<FF>;
+
+// Fole one instance into an accumulator.
+void fold_one(State& state) noexcept
 {
-    auto log2_of_gates = static_cast<size_t>(state.range(0));
-    bench_utils::construct_proof_with_specified_num_iterations<honk::UltraComposer>(
-        state, &bench_utils::generate_basic_arithmetic_circuit<UltraCircuitBuilder>, log2_of_gates);
+    barretenberg::srs::init_crs_factory("../srs_db/ignition");
+
+    auto log2_num_gates = static_cast<size_t>(state.range(0));
+    auto composer = UltraComposer();
+
+    const auto construct_instance = [&]() {
+        auto builder = typename Flavor::CircuitBuilder();
+        bench_utils::generate_basic_arithmetic_circuit(builder, log2_num_gates);
+        return composer.create_instance(builder);
+    };
+
+    auto instance_1 = construct_instance();
+    auto instance_2 = construct_instance();
+    // auto instances = std::vector<std::shared_ptr<Instance>>{ instance_1, instance_2 };
+
+    auto folding_prover = composer.create_folding_prover({ instance_1, instance_2 }, composer.commitment_key);
+
+    for (auto _ : state) {
+        auto proof = folding_prover.fold_instances();
+    }
 }
 
-BENCHMARK(construct_proof_ultrahonk_power_of_2)
-    // 2**13 gates to 2**18 gates
-    ->DenseRange(13, 18)
-    ->Unit(kMillisecond);
+BENCHMARK(fold_one)->/* vary the circuit size */ DenseRange(13, 18)->Unit(kMillisecond);
+} // namespace proof_system::honk
