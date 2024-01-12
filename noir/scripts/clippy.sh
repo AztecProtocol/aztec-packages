7da93c2eb632b5f654e6f1d797ad1a5048ca8afe
Tom French <tom@tomfren.ch>
2024-01-12 12:27:49 +0000

    chore: copy bootstrap scripts

--- noir/scripts/clippy.sh
+++ noir/scripts/clippy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -eu
 
+cd $(dirname "$0")/..
+
+# If this project has been subrepod into another project, set build data manually.
 export SOURCE_DATE_EPOCH=$(date +%s)
 export GIT_DIRTY=false
-export GIT_COMMIT=$(git rev-parse --verify HEAD)
+if [ -f ".gitrepo" ]; then
+  export GIT_COMMIT=$(awk '/commit =/ {print $3}' .gitrepo)
+else
+  export GIT_COMMIT=$(git rev-parse --verify HEAD)
+fi
 
 cargo clippy --workspace --release
