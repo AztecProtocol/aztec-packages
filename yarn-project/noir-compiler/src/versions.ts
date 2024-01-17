89ef4f3fecd8f2c9edbbdaf44529588a643f4465
Santiago Palladino <santiago@aztecprotocol.com>
2024-01-17 16:44:11 -0300

    Remove noir version exported from noir-compiler

--- yarn-project/noir-compiler/src/versions.ts
+++
@@ -1,5 +0,0 @@
-import { readFile } from 'node:fs/promises';
-
-// read package.json at runtime instead of compile time so that we keep rootDir as-is in tsconfig
-const pkg = JSON.parse(await readFile(new URL('../package.json', import.meta.url), 'utf-8'));
-export const NoirWasmVersion = pkg.dependencies['@noir-lang/noir_wasm'];
