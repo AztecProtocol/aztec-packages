9de7f0de0f699a137acee38f5805810d02c0d2f1
dbanks12 <david@aztecprotocol.com>
2024-01-18 17:12:51 +0000

    encode & decode

--- yarn-project/acir-simulator/src/avm/index.test.ts
+++ yarn-project/acir-simulator/src/avm/index.test.ts
@@ -5,26 +5,9 @@ import { mock } from 'jest-mock-extended';
 import { AvmMachineState } from './avm_machine_state.js';
 import { AvmStateManager } from './avm_state_manager.js';
 import { AvmInterpreter } from './interpreter/interpreter.js';
-import { Add } from './opcodes/arithmetic.js';
-import { Return } from './opcodes/control_flow.js';
-import { interpretBytecode } from './opcodes/from_bytecode.js';
-import { CalldataCopy } from './opcodes/memory.js';
+import { decodeBytecode } from './opcodes/decode_bytecode.js';
+import { encodeToBytecode } from './opcodes/encode_to_bytecode.js';
 import { Opcode } from './opcodes/opcodes.js';
-
-function genInstructionBytecode(opcode: Opcode, args: number[], assertArgsLength: number): Buffer {
-  expect(args.length).toBe(assertArgsLength);
-  // 1 byte for opcode, 4 bytes for each argument
-  const bytecode = Buffer.alloc(1 + args.length * 4);
-
-  let byteOffset = 0;
-  bytecode.writeUInt8(opcode as number, byteOffset);
-  byteOffset++;
-  for (let i = 0; i < args.length; i++) {
-    bytecode.writeUInt32BE(args[i], byteOffset);
-    byteOffset += 4;
-  }
-  return bytecode;
-}
 
 describe('avm', () => {
   it('Should execute bytecode', () => {
@@ -36,17 +19,13 @@ describe('avm', () => {
     const addArgs = [0, 1, 2];
     const returnArgs = [2, 1];
 
-    const calldataCopyBytecode = genInstructionBytecode(
-      Opcode.CALLDATACOPY,
-      calldataCopyArgs,
-      CalldataCopy.numberOfOperands,
-    );
-    const addBytecode = genInstructionBytecode(Opcode.ADD, addArgs, Add.numberOfOperands);
-    const returnBytecode = genInstructionBytecode(Opcode.RETURN, returnArgs, Return.numberOfOperands);
+    const calldataCopyBytecode = encodeToBytecode(Opcode.CALLDATACOPY, calldataCopyArgs);
+    const addBytecode = encodeToBytecode(Opcode.ADD, addArgs);
+    const returnBytecode = encodeToBytecode(Opcode.RETURN, returnArgs);
     const fullBytecode = Buffer.concat([calldataCopyBytecode, addBytecode, returnBytecode]);
 
     // Decode bytecode into instructions
-    const instructions = interpretBytecode(fullBytecode);
+    const instructions = decodeBytecode(fullBytecode);
 
     // Execute instructions
     const context = new AvmMachineState(calldata);
