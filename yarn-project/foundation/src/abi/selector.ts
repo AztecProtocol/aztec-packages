caf4053758483aa0afc9248b4c5e42c3d2f9534d
Leila Wang <leizciw@gmail.com>
2024-01-21 15:58:30 +0000

    Add nullifier_key_validation_requests to public inputs.

--- yarn-project/foundation/src/abi/selector.ts
+++ yarn-project/foundation/src/abi/selector.ts
@@ -1,5 +1,5 @@
 import { fromHex, toBigIntBE, toBufferBE } from '@aztec/foundation/bigint-buffer';
-import { BufferReader } from '@aztec/foundation/serialize';
+import { BufferReader, FieldReader } from '@aztec/foundation/serialize';
 
 import { keccak } from '../crypto/keccak/index.js';
 import { Fr } from '../fields/index.js';
@@ -102,6 +102,11 @@ export class FunctionSelector extends Selector {
    */
   static fromField(fr: Fr) {
     return new FunctionSelector(Number(fr.toBigInt()));
+  }
+
+  static fromFields(fields: Fr[] | FieldReader) {
+    const reader = FieldReader.asReader(fields);
+    return FunctionSelector.fromField(reader.readField());
   }
 
   /**
