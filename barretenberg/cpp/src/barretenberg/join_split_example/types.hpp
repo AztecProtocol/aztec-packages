2b104ec59ad73a7606b28f181260f4676c571613
ludamad <adam.domurad@gmail.com>
2024-01-22 14:44:55 -0500

    fix schnorr forward declares

--- barretenberg/cpp/src/barretenberg/join_split_example/types.hpp
+++ barretenberg/cpp/src/barretenberg/join_split_example/types.hpp
@@ -39,8 +39,6 @@ using bn254 = bb::stdlib::bn254<Builder>;
 
 using hash_path_ct = bb::stdlib::merkle_tree::hash_path<Builder>;
 
-namespace schnorr {
 using schnorr_signature_bits = bb::stdlib::schnorr_signature_bits<Builder>;
-} // namespace schnorr
 
 } // namespace bb::join_split_example
