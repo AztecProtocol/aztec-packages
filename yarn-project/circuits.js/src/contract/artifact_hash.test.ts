e7731280e7c95338e5a6e16a8535bce897eb4b0a
Santiago Palladino <santiago@aztecprotocol.com>
2024-01-24 17:59:33 -0300

    Fix dependency on artifact

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -1,12 +1,10 @@
-// Cannot list noir-contracts as a devDependency since it'll trigger a circular dependency in tsconfigs
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { BenchmarkingContractArtifact } from '@aztec/noir-contracts/Benchmarking';
-
+import { getSampleContractArtifact } from '../tests/fixtures.js';
 import { getArtifactHash } from './artifact_hash.js';
 
 describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
-    expect(getArtifactHash(BenchmarkingContractArtifact).toString()).toMatchInlineSnapshot(
+    const artifact = getSampleContractArtifact();
+    expect(getArtifactHash(artifact).toString()).toMatchInlineSnapshot(
       `"0x1cd31b12181cf7516720f4675ffea13c8c538dc4875232776adb8bbe8364ed5c"`,
     );
   });
--- yarn-project/circuits.js/src/contract/contract_class.test.ts
+++ yarn-project/circuits.js/src/contract/contract_class.test.ts
@@ -1,13 +1,13 @@
 import { Fr } from '@aztec/foundation/fields';
 import { toFriendlyJSON } from '@aztec/foundation/serialize';
-import { BenchmarkingContractArtifact } from '@aztec/noir-contracts/Benchmarking';
 
+import { getSampleContractArtifact } from '../tests/fixtures.js';
 import { createContractClassFromArtifact } from './contract_class.js';
 
 describe('ContractClass', () => {
   it('creates a contract class from a contract compilation artifact', () => {
     const contractClass = createContractClassFromArtifact({
-      ...BenchmarkingContractArtifact,
+      ...getSampleContractArtifact(),
       artifactHash: Fr.fromString('0x1234'),
     });
     expect(toFriendlyJSON(contractClass)).toMatchSnapshot();
