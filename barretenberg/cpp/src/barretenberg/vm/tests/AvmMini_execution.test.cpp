17ff26bd24ead9552c87e8bfd1706d4455a040a5
jeanmon <jean@aztecprotocol.com>
2024-01-26 16:53:34 +0000

    4209 - unit test on internal_call and internal_return

--- barretenberg/cpp/src/barretenberg/vm/tests/AvmMini_execution.test.cpp
+++ barretenberg/cpp/src/barretenberg/vm/tests/AvmMini_execution.test.cpp
@@ -222,6 +222,79 @@ TEST_F(AvmMiniExecutionTests, powerWithMulOpcodes)
     EXPECT_TRUE(verifier.verify_proof(proof));
 }
 
+// Positive test about a single internal_call and internal_return
+// Code of internal routine is SET U32 value 123456789 at memory address 7
+// The bytecode execution is:
+// SET U32 val. 222111000 at memory address 4
+// CALL internal routine
+// ADD M[4] with M[7] and output in M[9]
+// Internal routine bytecode is at the end.
+// Bytecode layout: SET INTERNAL_CALL ADD RETURN SET INTERNAL_RETURN
+//                   0        1        2     3    4         5
+TEST_F(AvmMiniExecutionTests, simpleInternalCall)
+{
+    std::string bytecode_hex = "27"       // SET 39 = 0x27
+                               "03"       // U32
+                               "0D3D2518" // val 222111000 = 0xD3D2518
+                               "00000004" // dst_offset 4
+                               "25"       // INTERNALCALL 37
+                               "00000004" // jmp_dest
+                               "00"       // ADD
+                               "03"       // U32
+                               "00000004" // addr a 4
+                               "00000007" // addr b 7
+                               "00000009" // addr c9
+                               "34"       // RETURN
+                               "00000000" // ret offset 0
+                               "00000000" // ret size 0
+                               "27"       // SET 39 = 0x27
+                               "03"       // U32
+                               "075BCD15" // val 123456789 = 0x75BCD15
+                               "00000007" // dst_offset 7
+                               "26"       // INTERNALRETURN 38
+        ;
+
+    auto bytecode = hex_to_bytes(bytecode_hex);
+    auto instructions = Execution::parse(bytecode);
+
+    EXPECT_EQ(instructions.size(), 6);
+
+    // We test parsing step for INTERNALCALL and INTERNALRETURN.
+
+    // INTERNALCALL
+    EXPECT_EQ(instructions.at(1).op_code, OpCode::INTERNALCALL);
+    EXPECT_EQ(instructions.at(1).operands.size(), 1);
+    EXPECT_EQ(instructions.at(1).operands.at(0), 4);
+
+    // INTERNALRETURN
+    EXPECT_EQ(instructions.at(5).op_code, OpCode::INTERNALRETURN);
+
+    auto trace = Execution::gen_trace(instructions, std::vector<FF>{});
+
+    // Find the first row enabling the addition selector.
+    auto row = std::ranges::find_if(trace.begin(), trace.end(), [](Row r) { return r.avmMini_sel_op_add == 1; });
+    EXPECT_EQ(row->avmMini_ic, 345567789);
+
+    auto trace_verif = trace;
+    validate_trace_proof(std::move(trace));
+
+    // Expected PC sequence during execution
+    std::vector<FF> pc_sequence{ 0, 1, 4, 5, 2, 3 };
+
+    for (size_t i = 0; i < 6; i++) {
+        EXPECT_EQ(trace_verif.at(i + 1).avmMini_pc, pc_sequence.at(i));
+    }
+
+    auto circuit_builder = AvmMiniCircuitBuilder();
+    circuit_builder.set_trace(std::move(trace_verif));
+    auto composer = honk::AvmMiniComposer();
+    auto verifier = composer.create_verifier(circuit_builder);
+
+    auto proof = Execution::run_and_prove(bytecode, std::vector<FF>{});
+
+    EXPECT_TRUE(verifier.verify_proof(proof));
+}
+
 // Negative test detecting an invalid opcode byte.
 TEST_F(AvmMiniExecutionTests, invalidOpcode)
 {
