610f51af8c0dd8720c63ef51e58fb6c0537a81bc
ludamad <adam@aztecprotocol.com>
2024-01-29 19:50:06 +0000

    new-style cli

--- barretenberg/cpp/scripts/benchmarks_wasm.sh
+++ barretenberg/cpp/scripts/benchmarks_wasm.sh
@@ -6,13 +6,13 @@ cd $(dirname $0)/..
 
 # Configure and build.
 cmake --preset wasm-bench
-cmake --build --preset wasm-bench --target commit_bench --target goblin_bench
+cmake --build --preset wasm-bench --target commit_bench --target commit_bench
 
 cd build-wasm-bench
 # github markdown style, works in comments and descriptions
 echo -e "<details><summary>Commit</summary>"
 echo -e '\n```'
-wasmtime run --wasm-features=threads --wasi-modules=experimental-wasi-threads ./bin/goblin_bench
+wasmtime run -Wthreads=y -Sthreads=y ./bin/commit_bench
 echo -e '```\n'
 echo -e "</details>"
 echo -e '\n```'
