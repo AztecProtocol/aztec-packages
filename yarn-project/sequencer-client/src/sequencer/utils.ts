d31f10dad16d40b221356d761e2058d326ddf1fb
benesjan <janbenes1234@gmail.com>
2024-01-29 13:05:28 +0000

    buildInitialHeader func cleanup

--- yarn-project/sequencer-client/src/sequencer/utils.ts
+++ yarn-project/sequencer-client/src/sequencer/utils.ts
@@ -3,16 +3,10 @@ import { MerkleTreeOperations } from '@aztec/world-state';
 
 /**
  * Builds the initial header by reading the roots from the database.
+ *
+ * TODO(#4148) Proper genesis state. If the state is empty, we allow anything for now.
  */
-export async function buildInitialHeader(
-  db: MerkleTreeOperations,
-  prevBlockGlobalVariables: GlobalVariables = GlobalVariables.empty(), // TODO(benesjan): this should most likely be removed
-) {
+export async function buildInitialHeader(db: MerkleTreeOperations) {
   const state = await db.getStateReference();
-  return new Header(
-    AppendOnlyTreeSnapshot.empty(), // TODO(benesjan): is it correct that last archive is 0?
-    Buffer.alloc(32, 0),
-    state,
-    prevBlockGlobalVariables,
-  );
+  return new Header(AppendOnlyTreeSnapshot.empty(), Buffer.alloc(32, 0), state, GlobalVariables.empty());
 }
