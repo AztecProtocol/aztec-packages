659311a222ee6a3a10a7b44fc135620cdbcabd2e
ludamad <adam@aztecprotocol.com>
2024-01-31 01:09:51 +0000

    fix

--- barretenberg/cpp/src/barretenberg/goblin/full_goblin_recursion.test.cpp
+++ barretenberg/cpp/src/barretenberg/goblin/full_goblin_recursion.test.cpp
@@ -10,10 +10,10 @@
 #include "barretenberg/ultra_honk/ultra_composer.hpp"
 
 #include <gtest/gtest.h>
-using namespace bb;
-using namespace bb
 
-    class GoblinRecursionTests : public ::testing::Test {
+using namespace bb;
+
+class GoblinRecursionTests : public ::testing::Test {
   protected:
     static void SetUpTestSuite()
     {
