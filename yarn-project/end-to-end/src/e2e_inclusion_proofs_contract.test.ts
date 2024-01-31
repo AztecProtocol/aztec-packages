d3a3fda13ff635c6c0b928419065ec006e005f59
LHerskind <lasse.herskind@gmail.com>
2024-01-31 11:34:37 +0000

    chore: fix inclusion check + err

--- yarn-project/end-to-end/src/e2e_inclusion_proofs_contract.test.ts
+++ yarn-project/end-to-end/src/e2e_inclusion_proofs_contract.test.ts
@@ -157,11 +157,10 @@ describe('e2e_inclusion_proofs_contract', () => {
     it('public value existence failure case', async () => {
       // Choose random block number between first block and current block number to test archival node
       const blockNumber = await getRandomBlockNumber();
-
       const randomPublicValue = Fr.random();
       await expect(
         contract.methods.test_public_value_inclusion_proof(randomPublicValue, blockNumber).send().wait(),
-      ).rejects.toThrow(/Public value does not match value in witness/);
+      ).rejects.toThrow('Public value does not match the witness');
     });
 
     it('proves existence of uninitialized public value', async () => {
