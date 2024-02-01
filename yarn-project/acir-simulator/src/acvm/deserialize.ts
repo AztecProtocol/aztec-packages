d14c88f3e70fcfbc234b9f9f9c4da43b86db387b
benesjan <janbenes1234@gmail.com>
2024-02-01 14:37:02 +0000

    nuking extractPublicCircuitPublicInputs and extractPrivateCircuitPublicInputs

--- yarn-project/acir-simulator/src/acvm/deserialize.ts
+++ yarn-project/acir-simulator/src/acvm/deserialize.ts
@@ -1,6 +1,4 @@
-import { PrivateCircuitPublicInputs, PublicCircuitPublicInputs } from '@aztec/circuits.js';
 import { Fr } from '@aztec/foundation/fields';
-import { FieldReader } from '@aztec/foundation/serialize';
 
 import { getReturnWitness } from '@noir-lang/acvm_js';
 
@@ -34,37 +32,4 @@ export function extractReturnWitness(acir: Buffer, partialWitness: ACVMWitness):
   const returnWitness = getReturnWitness(acir, partialWitness);
   const sortedKeys = [...returnWitness.keys()].sort((a, b) => a - b);
   return sortedKeys.map(key => returnWitness.get(key)!).map(fromACVMField);
-}
-
-/**
- * Create a reader for the public inputs of the ACVM generated partial witness.
- */
-function createPublicInputsReader(witness: ACVMWitness, acir: Buffer) {
-  const fields = extractReturnWitness(acir, witness);
-  return new FieldReader(fields);
-}
-
-/**
- * Extracts the public inputs from the ACVM generated partial witness.
- * @param partialWitness - The partial witness.
- * @param acir - The ACIR bytecode.
- * @returns The public inputs.
- */
-export function extractPrivateCircuitPublicInputs(
-  partialWitness: ACVMWitness,
-  acir: Buffer,
-): PrivateCircuitPublicInputs {
-  const witnessReader = createPublicInputsReader(partialWitness, acir);
-  return PrivateCircuitPublicInputs.fromFields(witnessReader);
-}
-
-/**
- * Extracts the public circuit public inputs from the ACVM generated partial witness.
- * @param partialWitness - The partial witness.
- * @param acir - The ACIR bytecode.
- * @returns The public inputs.
- */
-export function extractPublicCircuitPublicInputs(partialWitness: ACVMWitness, acir: Buffer): PublicCircuitPublicInputs {
-  const witnessReader = createPublicInputsReader(partialWitness, acir);
-  return PublicCircuitPublicInputs.fromFields(witnessReader);
 }
