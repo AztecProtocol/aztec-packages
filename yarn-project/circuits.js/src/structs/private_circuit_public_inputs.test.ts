60b470b6175a389af94cda9cbcd551cc57d844dd
benesjan <janbenes1234@gmail.com>
2024-02-01 09:08:41 +0000

    improved PrivateCircuitPublicInputs test

--- yarn-project/circuits.js/src/structs/private_circuit_public_inputs.test.ts
+++ yarn-project/circuits.js/src/structs/private_circuit_public_inputs.test.ts
@@ -1,6 +1,14 @@
+import { makePrivateCircuitPublicInputs } from '../tests/factories.js';
 import { PrivateCircuitPublicInputs } from './private_circuit_public_inputs.js';
 
 describe('PrivateCircuitPublicInputs', () => {
+  it('serializes to buffer and back', () => {
+    const target = makePrivateCircuitPublicInputs(100);
+    const buffer = target.toBuffer();
+    const result = PrivateCircuitPublicInputs.fromBuffer(buffer);
+    expect(result).toEqual(target);
+  });
+
   it(`initializes an empty PrivateCircuitPublicInputs`, () => {
     const target = PrivateCircuitPublicInputs.empty();
     expect(target.isEmpty()).toBe(true);
