297299e1c4959880e82608a736d24106b5d026f4
guipublic <guipublic@gmail.com>
2024-02-02 09:42:47 +0000

    big endian bytes for big field and reduce

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/bigint_constraint.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/bigint_constraint.cpp
@@ -310,20 +310,21 @@ void create_bigint_from_le_bytes_constraint(Builder& builder,
                                  .modulus_1 = modulus_limbs[1],
                                  .modulus_2 = modulus_limbs[2],
                                  .modulus_3 = modulus_limbs[3] };
-    bb::stdlib::byte_array<Builder> bytes = bb::stdlib::byte_array<Builder>(&builder, 32);
+    bb::stdlib::byte_array<Builder> rev_bytes = bb::stdlib::byte_array<Builder>(&builder, 32);
     for (size_t i = 0; i < 32; ++i) {
         if (i < input.inputs.size()) {
             field_ct element = field_ct::from_witness_index(&builder, input.inputs[i]);
             byte_array_ct element_bytes(element, 1);
-            bytes.write_at(element_bytes, i);
+            rev_bytes.write_at(element_bytes, i);
         } else {
-            bytes[i] = 0;
+            rev_bytes[i] = 0;
         }
     }
+    bb::stdlib::byte_array<Builder> bytes = rev_bytes.reverse();
 
     auto modulus_id = modulus_param_to_id(modulus);
-    switch (modulus_id) {
 
+    switch (modulus_id) {
     case BN254_FQ: {
         auto big = big_bn254_fq(bytes);
         dsl_bigints.set_bn254_fq(big, input.result);
@@ -373,39 +374,44 @@ void create_bigint_to_le_bytes_constraint(Builder& builder,
     using big_secp256k1_fr = bb::stdlib::bigfield<Builder, secp256k1::Secp256k1FrParams>;
     using big_secp256r1_fq = bb::stdlib::bigfield<Builder, secp256r1::Secp256r1FqParams>;
     using big_secp256r1_fr = bb::stdlib::bigfield<Builder, secp256r1::Secp256r1FrParams>;
-    using field_ct = bb::stdlib::field_t<Builder>;
 
     auto modulus_id = dsl_bigints.get_modulus_id(input.input);
     bb::stdlib::byte_array<Builder> byte_array;
     switch (modulus_id) {
     case BN254_FQ: {
         big_bn254_fq big = dsl_bigints.bn254_fq(input.input);
+        big.self_reduce();
         byte_array = big.to_byte_array();
 
         break;
     }
     case BN254_FR: {
         big_bn254_fr big = dsl_bigints.bn254_fr(input.input);
+        big.self_reduce();
         byte_array = big.to_byte_array();
         break;
     }
     case SECP256K1_FQ: {
         big_secp256k1_fq big = dsl_bigints.secp256k1_fq(input.input);
+        big.self_reduce();
         byte_array = big.to_byte_array();
         break;
     }
     case SECP256K1_FR: {
         big_secp256k1_fr big = dsl_bigints.secp256k1_fr(input.input);
+        big.self_reduce();
         byte_array = big.to_byte_array();
         break;
     }
     case SECP256R1_FQ: {
         big_secp256r1_fq big = dsl_bigints.secp256r1_fq(input.input);
+        big.self_reduce();
         byte_array = big.to_byte_array();
         break;
     }
     case SECP256R1_FR: {
         big_secp256r1_fr big = dsl_bigints.secp256r1_fr(input.input);
+        big.self_reduce();
         byte_array = big.to_byte_array();
         break;
     }
@@ -414,14 +420,29 @@ void create_bigint_to_le_bytes_constraint(Builder& builder,
         ASSERT(false);
         break;
     }
-    // TODO: check both sizes are equal
-    for (size_t i = 0; i < input.result.size(); ++i) {
-        info(byte_array[i].normalize().witness_index);
-        info(byte_array[i].normalize().get_value());
-        info(input.result[i]);
-        field_ct result = field_ct::from_witness_index(&builder, input.result[i]);
-        info(result.get_value());
-        builder.assert_equal(byte_array[i].normalize().witness_index, input.result[i]);
+    byte_array = byte_array.reverse();
+    ASSERT(input.result.size() <= byte_array.size());
+    for (size_t i = 0; i < byte_array.size(); ++i) {
+        if (i < input.result.size()) {
+
+            // This should instead use assert_equal: builder.assert_equal(byte_array[i].normalize().witness_index,
+            // input.result[i]); but unit tests require this because they do not constraint the witness, and then if we
+            // use assert_equal in that case, we can generate a proof for non matching values (cf test_assert_equal in
+            // field.test.cpp). We should check that Noir always constraint the results of to_bytes
+            poly_triple assert_equal{
+                .a = byte_array[i].normalize().witness_index,
+                .b = input.result[i],
+                .c = 0,
+                .q_m = 0,
+                .q_l = 1,
+                .q_r = -1,
+                .q_o = 0,
+                .q_c = 0,
+            };
+            builder.create_poly_gate(assert_equal);
+        } else {
+            byte_array[i].normalize().is_zero();
+        }
     }
 }
 
--- barretenberg/cpp/src/barretenberg/dsl/acir_format/bigint_constraint.test.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/bigint_constraint.test.cpp
@@ -14,41 +14,197 @@ class BigIntTests : public ::testing::Test {
   protected:
     static void SetUpTestSuite() { bb::srs::init_crs_factory("../srs_db/ignition"); }
 };
+using fr = field<Bn254FrParams>;
 
-std::pair<BigIntOperation, BigIntToLeBytes> generate_big_int_op_constraint(BigIntOperationType op,
-                                                                           uint8_t lhs,
-                                                                           uint8_t rhs,
-                                                                           WitnessVector& witness_values)
+std::tuple<BigIntFromLeBytes, BigIntFromLeBytes, BigIntOperation, BigIntToLeBytes>
+generate_big_int_op_constraint_with_modulus(
+    BigIntOperationType op, fr lhs, fr rhs, WitnessVector& witness_values, const std::vector<uint32_t>& modulus)
 {
-    uint32_t result = static_cast<uint32_t>(witness_values.size());
+    // CAUTION We assume here the operands and the result fit into one byte!
+    // So trying to divide 7/2 won't work, but 8/2 will do.
+    auto lhs_id = static_cast<uint32_t>(witness_values.size());
+    witness_values.push_back(lhs);
+    auto rhs_id = static_cast<uint32_t>(witness_values.size());
+    witness_values.push_back(rhs);
+    BigIntFromLeBytes from_le_bytes_constraint_bigint_lhs{
+        .inputs = { lhs_id },
+        .modulus = modulus,
+        .result = lhs_id,
+    };
+    BigIntFromLeBytes from_le_bytes_constraint_bigint_rhs{
+        .inputs = { rhs_id },
+        .modulus = modulus,
+        .result = rhs_id,
+    };
+
+    auto result = static_cast<uint32_t>(witness_values.size());
     BigIntOperation constraint{
-        .lhs = lhs,
-        .rhs = rhs,
+        .lhs = lhs_id,
+        .rhs = rhs_id,
         .result = result,
         .opcode = op,
     };
+    // Expecting the result to be just one byte long
     BigIntToLeBytes to_bytes{
         .input = result,
         .result = { static_cast<uint32_t>(witness_values.size()) },
     };
-    // overflow is NOT supported, you have to make sure there is no overflow.
+    // overflow is NOT supported, you have to make sure there is no overflow/underflow.
+    fr value = 0;
     switch (op) {
     case Add:
-        witness_values.push_back(witness_values[lhs] + witness_values[rhs]);
+        value = witness_values[lhs_id] + witness_values[rhs_id];
         break;
     case Neg:
-        witness_values.push_back(witness_values[lhs] - witness_values[rhs]);
+        value = witness_values[lhs_id] - witness_values[rhs_id];
         break;
     case Mul:
-        witness_values.push_back(witness_values[lhs] * witness_values[rhs]);
+        value = witness_values[lhs_id] * witness_values[rhs_id];
         break;
     case Div:
+        value = witness_values[lhs_id] / witness_values[rhs_id];
+        break;
     default:
         ASSERT(false);
         break;
     }
 
-    return std::pair<BigIntOperation, BigIntToLeBytes>(constraint, to_bytes);
+    witness_values.push_back(value);
+    return { from_le_bytes_constraint_bigint_lhs, from_le_bytes_constraint_bigint_rhs, constraint, to_bytes };
+}
+
+std::tuple<BigIntFromLeBytes, BigIntFromLeBytes, BigIntOperation, BigIntToLeBytes> generate_big_int_op_constraint(
+    BigIntOperationType op, fr lhs, fr rhs, WitnessVector& witness_values)
+{
+    // modulus is bn254/fq
+    return generate_big_int_op_constraint_with_modulus(
+        op,
+        lhs,
+        rhs,
+        witness_values,
+        {
+            0x47, 0xFD, 0x7C, 0xD8, 0x16, 0x8C, 0x20, 0x3C, 0x8d, 0xca, 0x71, 0x68, 0x91, 0x6a, 0x81, 0x97,
+            0x5d, 0x58, 0x81, 0x81, 0xb6, 0x45, 0x50, 0xb8, 0x29, 0xa0, 0x31, 0xe1, 0x72, 0x4e, 0x64, 0x30,
+        });
+}
+
+std::tuple<BigIntFromLeBytes, BigIntFromLeBytes, BigIntOperation, BigIntToLeBytes>
+generate_big_int_op_constraint_secpk1_fr(BigIntOperationType op, fr lhs, fr rhs, WitnessVector& witness_values)
+{
+    return generate_big_int_op_constraint_with_modulus(
+        op, lhs, rhs, witness_values, { 0x41, 0x41, 0x36, 0xD0, 0x8C, 0x5E, 0xD2, 0xBF, 0x3B, 0xA0, 0x48,
+                                        0xAF, 0xE6, 0xDC, 0xAE, 0xBA, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                                        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF });
+}
+
+std::tuple<BigIntFromLeBytes, BigIntFromLeBytes, BigIntOperation, BigIntToLeBytes>
+generate_big_int_op_constraint_secpk1_fq(BigIntOperationType op, fr lhs, fr rhs, WitnessVector& witness_values)
+{
+    return generate_big_int_op_constraint_with_modulus(
+        op, lhs, rhs, witness_values, { 0x2F, 0xFC, 0xFF, 0xFF, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                                        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                                        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF });
+}
+void apply_constraints(AcirFormat& constraint_system,
+                       std::tuple<BigIntFromLeBytes, BigIntFromLeBytes, BigIntOperation, BigIntToLeBytes> constraints)
+{
+    constraint_system.bigint_from_le_bytes_constraints.push_back(get<0>(constraints));
+    constraint_system.bigint_from_le_bytes_constraints.push_back(get<1>(constraints));
+    constraint_system.bigint_to_le_bytes_constraints.push_back(get<3>(constraints));
+    constraint_system.bigint_operations.push_back(get<2>(constraints));
+}
+
+std::tuple<BigIntOperation, BigIntToLeBytes> generate_big_int_op_constraint_with_id(BigIntOperationType op,
+                                                                                    uint32_t lhs_id,
+                                                                                    uint32_t rhs_id,
+                                                                                    WitnessVector& witness_values)
+{
+    // lhs_id, rhs_id are big int it, so we can generate the operation directly
+    auto result = static_cast<uint32_t>(witness_values.size());
+    BigIntOperation constraint{
+        .lhs = lhs_id,
+        .rhs = rhs_id,
+        .result = result,
+        .opcode = op,
+    };
+    // Expecting the result to be just one byte long
+    BigIntToLeBytes to_bytes{
+        .input = result,
+        .result = { static_cast<uint32_t>(witness_values.size()) },
+    };
+    // overflow is NOT supported, you have to make sure there is no overflow/underflow.
+    fr value = 0;
+    switch (op) {
+    case Add:
+        value = witness_values[lhs_id] + witness_values[rhs_id];
+        break;
+    case Neg:
+        value = witness_values[lhs_id] - witness_values[rhs_id];
+        break;
+    case Mul:
+        value = witness_values[lhs_id] * witness_values[rhs_id];
+        break;
+    case Div:
+        value = witness_values[lhs_id] / witness_values[rhs_id];
+        break;
+    default:
+        ASSERT(false);
+        break;
+    }
+
+    witness_values.push_back(value);
+    return { constraint, to_bytes };
+}
+
+// Based on TestBigIntConstraintSimple, we generate constraints for multiple operations at the same time.
+TEST_F(BigIntTests, TestBigIntConstraintMultiple)
+{
+    WitnessVector witness;
+    auto contraints = generate_big_int_op_constraint(BigIntOperationType::Add, fr(3), fr(1), witness);
+    auto contraints2 = generate_big_int_op_constraint(BigIntOperationType::Add, fr(3), fr(1), witness);
+    auto contraints3 = generate_big_int_op_constraint(BigIntOperationType::Neg, fr(5), fr(2), witness);
+    auto contraints4 = generate_big_int_op_constraint(BigIntOperationType::Mul, fr(5), fr(3), witness);
+    auto contraints5 = generate_big_int_op_constraint(BigIntOperationType::Div, fr(8), fr(2), witness);
+    AcirFormat constraint_system{
+        .varnum = static_cast<uint32_t>(witness.size() + 1),
+        .public_inputs = {},
+        .logic_constraints = {},
+        .range_constraints = {},
+        .sha256_constraints = {},
+        .schnorr_constraints = {},
+        .ecdsa_k1_constraints = {},
+        .ecdsa_r1_constraints = {},
+        .blake2s_constraints = {},
+        .blake3_constraints = {},
+        .keccak_constraints = {},
+        .keccak_var_constraints = {},
+        .keccak_permutations = {},
+        .pedersen_constraints = {},
+        .pedersen_hash_constraints = {},
+        .fixed_base_scalar_mul_constraints = {},
+        .ec_add_constraints = {},
+        .recursion_constraints = {},
+        .bigint_from_le_bytes_constraints = {},
+        .bigint_to_le_bytes_constraints = {},
+        .bigint_operations = {},
+        .constraints = {},
+        .block_constraints = {},
+    };
+    apply_constraints(constraint_system, contraints);
+    apply_constraints(constraint_system, contraints2);
+    apply_constraints(constraint_system, contraints3);
+    apply_constraints(constraint_system, contraints4);
+    apply_constraints(constraint_system, contraints5);
+    constraint_system.varnum = static_cast<uint32_t>(witness.size() + 1);
+
+    auto builder = create_circuit(constraint_system, /*size_hint*/ 0, witness);
+
+    auto composer = Composer();
+    auto prover = composer.create_ultra_with_keccak_prover(builder);
+    auto proof = prover.construct_proof();
+    EXPECT_TRUE(builder.check_circuit());
+    auto verifier = composer.create_ultra_with_keccak_verifier(builder);
+    EXPECT_EQ(verifier.verify_proof(proof), true);
 }
 
 TEST_F(BigIntTests, TestBigIntConstraintSimple)
@@ -102,10 +258,9 @@ TEST_F(BigIntTests, TestBigIntConstraintSimple)
     };
 
     WitnessVector witness{
-        0, 3, 4, 3, 0,
+        0, 3, 6, 3, 0,
     };
     auto builder = create_circuit(constraint_system, /*size_hint*/ 0, witness);
-    // ASSERT(false);
     auto composer = Composer();
     auto prover = composer.create_ultra_with_keccak_prover(builder);
     auto proof = prover.construct_proof();
@@ -114,84 +269,19 @@ TEST_F(BigIntTests, TestBigIntConstraintSimple)
     EXPECT_EQ(verifier.verify_proof(proof), true);
 }
 
-TEST_F(BigIntTests, TestBigIntConstraintDummy)
+// Based on TestBigIntConstraintMultiple, we generate constraints re-using the bigfields created by the first two
+// operations
+TEST_F(BigIntTests, TestBigIntConstraintReuse)
 {
-    // 3 + 3 = 6
-    // 3 = bigint(1) = from_bytes(w(1))
-    // 6 = bigint(2) = to_bytes(w(2))
-    BigIntOperation add_constraint{
-        .lhs = 1,
-        .rhs = 1,
-        .result = 2,
-        .opcode = BigIntOperationType::Add,
-    };
-    // 3 + 0 = 3 = bigint(4) = to_bytes(w(1))
-    // 3 = bigint(1)
-    // 0 = bigint(3) = from_bytes(w(4))
-    BigIntOperation add_constraint2{
-        .lhs = 1,
-        .rhs = 3,
-        .result = 4,
-        .opcode = BigIntOperationType::Add,
-    };
-    // BigIntOperation neg_constraint{
-    //     .lhs = 2,
-    //     .rhs = 1,
-    //     .result = 3,
-    //     .opcode = BigIntOperationType::Neg,
-    // };
-    // BigIntOperation mul_constraint{
-    //     .lhs = 1,
-    //     .rhs = 4,
-    //     .result = 5,
-    //     .opcode = BigIntOperationType::Mul,
-    // };
-    // BigIntOperation div_constraint{
-    //     .lhs = 1,
-    //     .rhs = 2,
-    //     .result = 3,
-    //     .opcode = BigIntOperationType::Div,
-    // };
-    BigIntFromLeBytes from_le_bytes_constraint_bigint1{
-        .inputs = { 1 },
-        .modulus = { 0x47, 0xFD, 0x7C, 0xD8, 0x16, 0x8C, 0x20, 0x3C, 0x8d, 0xca, 0x71, 0x68, 0x91, 0x6a, 0x81, 0x97, 
-  0x5d, 0x58, 0x81, 0x81, 0xb6, 0x45, 0x50, 0xb8, 0x29, 0xa0, 0x31, 0xe1, 0x72, 0x4e, 0x64, 0x30, },
-        .result = 1,
-    };
-    //     BigIntFromLeBytes from_le_bytes_constraint_bigint2{
-    //         .inputs = { 2 },
-    //         .modulus = { 0x47, 0xFD, 0x7C, 0xD8, 0x16, 0x8C, 0x20, 0x3C, 0x8d, 0xca, 0x71, 0x68, 0x91, 0x6a, 0x81,
-    //         0x97,
-    //   0x5d, 0x58, 0x81, 0x81, 0xb6, 0x45, 0x50, 0xb8, 0x29, 0xa0, 0x31, 0xe1, 0x72, 0x4e, 0x64, 0x30, },
-    //         .result = 2,
-    //     };
-    BigIntFromLeBytes from_le_bytes_constraint_bigint3{
-            .inputs = { 4 },
-            .modulus = { 0x47, 0xFD, 0x7C, 0xD8, 0x16, 0x8C, 0x20, 0x3C, 0x8d, 0xca, 0x71, 0x68, 0x91, 0x6a, 0x81,
-            0x97,
-      0x5d, 0x58, 0x81, 0x81, 0xb6, 0x45, 0x50, 0xb8, 0x29, 0xa0, 0x31, 0xe1, 0x72, 0x4e, 0x64, 0x30, },
-            .result = 3,
-        };
-    BigIntToLeBytes result2_to_le_bytes{
-        .input = 2, .result = { 2 }, // 3+3=6
-    };
-    BigIntToLeBytes result4_to_le_bytes{
-        .input = 3,
-        .result = { 1 },
-    };
-    //  poly_triple constrain_3_is_1{
-    //     .a = 1,
-    //     .b = 3,
-    //     .c = 0,
-    //     .q_m = 0,
-    //     .q_l = 1,
-    //     .q_r = -1,
-    //     .q_o = 0,
-    //     .q_c = 0,
-    // };
+    WitnessVector witness;
+    auto contraints = generate_big_int_op_constraint_secpk1_fr(BigIntOperationType::Add, fr(3), fr(1), witness);
+    auto contraints2 = generate_big_int_op_constraint_secpk1_fr(BigIntOperationType::Neg, fr(5), fr(2), witness);
+    auto contraints3 = generate_big_int_op_constraint_with_id(BigIntOperationType::Mul, 0, 5, witness);
+    auto contraints4 = generate_big_int_op_constraint_with_id(BigIntOperationType::Div, 0, 1, witness);
+    auto contraints5 = generate_big_int_op_constraint_with_id(BigIntOperationType::Neg, 7, 1, witness);
 
     AcirFormat constraint_system{
-        .varnum = 5,
+        .varnum = static_cast<uint32_t>(witness.size() + 1),
         .public_inputs = {},
         .logic_constraints = {},
         .range_constraints = {},
@@ -209,28 +299,83 @@ TEST_F(BigIntTests, TestBigIntConstraintDummy)
         .fixed_base_scalar_mul_constraints = {},
         .ec_add_constraints = {},
         .recursion_constraints = {},
-        .bigint_from_le_bytes_constraints = { from_le_bytes_constraint_bigint1,
-                                              from_le_bytes_constraint_bigint3 }, // from_le_bytes_constraint_bigint2 },
-        .bigint_to_le_bytes_constraints = { result2_to_le_bytes, result4_to_le_bytes }, // result_to_le_bytes_3 },
-        .bigint_operations = { add_constraint, add_constraint2 }, // neg_constraint }, //, neg_constraint,
-                                                                  // mul_constraint, div_constraint
-                                                                  // },
+        .bigint_from_le_bytes_constraints = {},
+        .bigint_to_le_bytes_constraints = {},
+        .bigint_operations = {},
         .constraints = {},
         .block_constraints = {},
-
     };
+    apply_constraints(constraint_system, contraints);
+    apply_constraints(constraint_system, contraints2);
+    constraint_system.bigint_to_le_bytes_constraints.push_back(get<1>(contraints3));
+    constraint_system.bigint_operations.push_back(get<0>(contraints3));
+    constraint_system.bigint_to_le_bytes_constraints.push_back(get<1>(contraints4));
+    constraint_system.bigint_operations.push_back(get<0>(contraints4));
+    constraint_system.bigint_to_le_bytes_constraints.push_back(get<1>(contraints5));
+    constraint_system.bigint_operations.push_back(get<0>(contraints5));
+    constraint_system.varnum = static_cast<uint32_t>(witness.size() + 1);
 
-    WitnessVector witness{
-        0, 3, 6, 3, 0,
-    };
     auto builder = create_circuit(constraint_system, /*size_hint*/ 0, witness);
-    // ASSERT(false);
+
     auto composer = Composer();
     auto prover = composer.create_ultra_with_keccak_prover(builder);
     auto proof = prover.construct_proof();
     EXPECT_TRUE(builder.check_circuit());
     auto verifier = composer.create_ultra_with_keccak_verifier(builder);
+    EXPECT_EQ(verifier.verify_proof(proof), true);
+}
 
+TEST_F(BigIntTests, TestBigIntConstraintReuse2)
+{
+    WitnessVector witness;
+    auto contraints = generate_big_int_op_constraint_secpk1_fq(BigIntOperationType::Add, fr(3), fr(1), witness);
+    auto contraints2 = generate_big_int_op_constraint_secpk1_fq(BigIntOperationType::Neg, fr(5), fr(2), witness);
+    auto contraints3 = generate_big_int_op_constraint_with_id(BigIntOperationType::Add, 0, 5, witness);
+    auto contraints4 = generate_big_int_op_constraint_with_id(BigIntOperationType::Neg, 0, 1, witness);
+    auto contraints5 = generate_big_int_op_constraint_with_id(BigIntOperationType::Neg, 7, 1, witness);
+
+    AcirFormat constraint_system{
+        .varnum = static_cast<uint32_t>(witness.size() + 1),
+        .public_inputs = {},
+        .logic_constraints = {},
+        .range_constraints = {},
+        .sha256_constraints = {},
+        .schnorr_constraints = {},
+        .ecdsa_k1_constraints = {},
+        .ecdsa_r1_constraints = {},
+        .blake2s_constraints = {},
+        .blake3_constraints = {},
+        .keccak_constraints = {},
+        .keccak_var_constraints = {},
+        .keccak_permutations = {},
+        .pedersen_constraints = {},
+        .pedersen_hash_constraints = {},
+        .fixed_base_scalar_mul_constraints = {},
+        .ec_add_constraints = {},
+        .recursion_constraints = {},
+        .bigint_from_le_bytes_constraints = {},
+        .bigint_to_le_bytes_constraints = {},
+        .bigint_operations = {},
+        .constraints = {},
+        .block_constraints = {},
+    };
+    apply_constraints(constraint_system, contraints);
+    apply_constraints(constraint_system, contraints2);
+    constraint_system.bigint_to_le_bytes_constraints.push_back(get<1>(contraints3));
+    constraint_system.bigint_operations.push_back(get<0>(contraints3));
+    constraint_system.bigint_to_le_bytes_constraints.push_back(get<1>(contraints4));
+    constraint_system.bigint_operations.push_back(get<0>(contraints4));
+    constraint_system.bigint_to_le_bytes_constraints.push_back(get<1>(contraints5));
+    constraint_system.bigint_operations.push_back(get<0>(contraints5));
+    constraint_system.varnum = static_cast<uint32_t>(witness.size() + 1);
+
+    auto builder = create_circuit(constraint_system, /*size_hint*/ 0, witness);
+
+    auto composer = Composer();
+    auto prover = composer.create_ultra_with_keccak_prover(builder);
+    auto proof = prover.construct_proof();
+    EXPECT_TRUE(builder.check_circuit());
+    auto verifier = composer.create_ultra_with_keccak_verifier(builder);
     EXPECT_EQ(verifier.verify_proof(proof), true);
 }
 
