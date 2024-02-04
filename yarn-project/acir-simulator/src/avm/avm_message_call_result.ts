42f97e9ae7d5f98471434225641a1380fdb9e00d
dbanks12 <david@aztecprotocol.com>
2024-02-04 22:13:39 +0000

    comments

--- yarn-project/acir-simulator/src/avm/avm_message_call_result.ts
+++ yarn-project/acir-simulator/src/avm/avm_message_call_result.ts
@@ -1,12 +1,13 @@
 import { Fr } from '@aztec/foundation/fields';
 
 /**
- * AVM contract call results.
+ * Results of an contract call's execution in the AVM.
  */
 export class AvmContractCallResults {
   public readonly reverted: boolean;
   public readonly output: Fr[];
 
+  /** For exceptional halts */
   public readonly revertReason: Error | undefined;
 
   constructor(reverted: boolean, output: Fr[], revertReason?: Error) {
@@ -15,6 +16,9 @@ export class AvmContractCallResults {
     this.revertReason = revertReason;
   }
 
+  /**
+   * Generate a string representation of call results.
+   */
   toString(): string {
     let resultsStr = `reverted: ${this.reverted}, output: ${this.output}`;
     if (this.revertReason) {
