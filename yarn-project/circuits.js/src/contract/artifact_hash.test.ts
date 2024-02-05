3b0eef945377662317fc3879b4d6dd2e97fb9240
Santiago Palladino <santiago@aztecprotocol.com>
2024-02-05 16:54:11 -0300

    feat: Emit single functions from class registerer
    Adds functions to broadcast individual private and unconstrained
    functions from the class registerer contract. The node does not yet
    capture these events.

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -1,10 +1,10 @@
 import { getSampleContractArtifact } from '../tests/fixtures.js';
-import { getArtifactHash } from './artifact_hash.js';
+import { computeArtifactHash } from './artifact_hash.js';
 
 describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
     const artifact = getSampleContractArtifact();
-    expect(getArtifactHash(artifact).toString()).toMatchInlineSnapshot(
+    expect(computeArtifactHash(artifact).toString()).toMatchInlineSnapshot(
       `"0x1cd31b12181cf7516720f4675ffea13c8c538dc4875232776adb8bbe8364ed5c"`,
     );
   });
