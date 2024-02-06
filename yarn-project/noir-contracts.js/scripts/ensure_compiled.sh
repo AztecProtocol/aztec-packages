6e9454f436bbc6a7ec5e00559029efad96e86785
spypsy <spypsy@outlook.com>
2024-02-06 12:14:00 +0000

    copy contract artifacts in noir-contracts.js

--- yarn-project/noir-contracts.js/scripts/ensure_compiled.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if ! ls ../../noir-contracts/target/*.json >/dev/null 2>&1; then
-  echo "Error: No .json files found in ../target folder"
-  exit 1
-fi
--- yarn-project/noir-contracts.js/scripts/generate-types.sh
+++ yarn-project/noir-contracts.js/scripts/generate-types.sh
@@ -6,14 +6,28 @@ INDEX="$OUT_DIR/index.ts"
 
 rm -rf $OUT_DIR && mkdir -p $OUT_DIR
 
+#
+if ! ls ../../noir-contracts/target/*.json >/dev/null 2>&1; then
+  echo "Error: No .json files found in noir-contracts/target folder."
+  echo "Make sure noir-contracts is built before running this script."
+  exit 1
+fi
+
 # Generate index.ts header.
 echo "// Auto generated module - do not edit!" >$INDEX
+
+# Ensure the artifacts directory exists
+mkdir -p artifacts
 
 for ABI in $(find ../../noir-contracts/target -maxdepth 1 -type f ! -name 'debug_*' -name '*.json'); do
   CONTRACT=$(jq -r .name $ABI)
 
   echo "Creating types for $CONTRACT in $ABI..."
   node --no-warnings ../noir-compiler/dest/cli.js codegen -o $OUT_DIR --ts $ABI
+
+  # Copy the .json file to the "artifacts" folder
+  echo "Copying $ABI to artifacts..."
+  cp "$ABI" artifacts/
 
   # Add contract import/export to index.ts.
   echo "export * from './${CONTRACT}.js';" >>$INDEX
