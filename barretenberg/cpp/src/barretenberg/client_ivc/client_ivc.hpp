68f3e89eb925377d814cf65a55254f22c1cf9089
ledwards2225 <l.edwards.d@gmail.com>
2024-02-06 17:29:20 +0000

    functionality in place; cleanup needed

--- barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.hpp
+++ barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.hpp
@@ -2,6 +2,8 @@
 
 #include "barretenberg/eccvm/eccvm_composer.hpp"
 #include "barretenberg/flavor/goblin_ultra.hpp"
+#include "barretenberg/goblin/goblin.hpp"
+#include "barretenberg/goblin/mock_circuits.hpp"
 #include "barretenberg/proof_system/circuit_builder/eccvm/eccvm_circuit_builder.hpp"
 #include "barretenberg/proof_system/circuit_builder/goblin_translator_circuit_builder.hpp"
 #include "barretenberg/proof_system/circuit_builder/goblin_ultra_circuit_builder.hpp"
@@ -15,287 +17,46 @@
 namespace bb {
 
 class ClientIVC {
-    using GoblinUltraCircuitBuilder = bb::GoblinUltraCircuitBuilder;
-
-    using Commitment = GoblinUltraFlavor::Commitment;
-    using FF = GoblinUltraFlavor::FF;
-
-  public:
-    using Builder = GoblinUltraCircuitBuilder;
-    using Fr = bb::fr;
-    using Transcript = bb::BaseTranscript;
-
-    using GoblinUltraComposer = bb::UltraComposer_<GoblinUltraFlavor>;
-    using GoblinUltraVerifier = bb::UltraVerifier_<GoblinUltraFlavor>;
-    using OpQueue = bb::ECCOpQueue;
-    using ECCVMFlavor = bb::ECCVMFlavor;
-    using ECCVMBuilder = bb::ECCVMCircuitBuilder<ECCVMFlavor>;
-    using ECCVMComposer = bb::ECCVMComposer;
-    using ECCVMProver = bb::ECCVMProver_<ECCVMFlavor>;
-    using TranslatorBuilder = bb::GoblinTranslatorCircuitBuilder;
-    using TranslatorComposer = bb::GoblinTranslatorComposer;
-    using RecursiveMergeVerifier = bb::stdlib::recursion::goblin::MergeRecursiveVerifier_<GoblinUltraCircuitBuilder>;
-    using MergeProver = bb::MergeProver;
-    using MergeVerifier = bb::MergeVerifier;
-    /**
-     * @brief Output of goblin::accumulate; an Ultra proof and the corresponding verification key
-     *
-     */
-    struct AccumulationOutput {
-        HonkProof proof;
-        std::shared_ptr<GoblinUltraFlavor::VerificationKey> verification_key;
-    };
-
-    struct Proof {
-        HonkProof merge_proof;
-        HonkProof eccvm_proof;
-        HonkProof translator_proof;
-        TranslationEvaluations translation_evaluations;
-        std::vector<Fr> to_buffer()
-        {
-            // ACIRHACK: so much copying and duplication added here and elsewhere
-            std::vector<Fr> translation_evaluations_buf; // = translation_evaluations.to_buffer();
-            size_t proof_size =
-                merge_proof.size() + eccvm_proof.size() + translator_proof.size() + translation_evaluations_buf.size();
-
-            std::vector<Fr> result(proof_size);
-            const auto insert = [&result](const std::vector<Fr>& buf) {
-                result.insert(result.end(), buf.begin(), buf.end());
-            };
-            insert(merge_proof);
-            insert(eccvm_proof);
-            insert(translator_proof);
-            insert(translation_evaluations_buf);
-            return result;
-        }
-    };
-
-    std::shared_ptr<OpQueue> op_queue = std::make_shared<OpQueue>();
-
-    HonkProof merge_proof;
-    Proof goblin_proof;
-
-    // on the first call to accumulate there is no merge proof to verify
-    bool merge_proof_exists{ false };
-
-  private:
-    // TODO(https://github.com/AztecProtocol/barretenberg/issues/798) unique_ptr use is a hack
-    std::unique_ptr<ECCVMBuilder> eccvm_builder;
-    std::unique_ptr<TranslatorBuilder> translator_builder;
-    std::unique_ptr<ECCVMComposer> eccvm_composer;
-    std::unique_ptr<ECCVMProver> eccvm_prover;
-    std::unique_ptr<TranslatorComposer> translator_composer;
-
-    AccumulationOutput accumulator; // Used only for ACIR methods for now
+    using Flavor = GoblinUltraFlavor;
+    using FF = Flavor::FF;
+    using FoldingOutput = FoldingResult<Flavor>;
+    using FoldProof = std::vector<FF>;
+    using Accumulator = std::shared_ptr<ProverInstance_<Flavor>>;
+    using Instance = ProverInstance_<GoblinUltraFlavor>;
+    using Composer = GoblinUltraComposer;
 
   public:
-    /**
-     * @brief Construct a GUH proof and a merge proof for the present circuit.
-     * @details If there is a previous merge proof, recursively verify it.
-     *
-     * @param circuit_builder
-     */
-    AccumulationOutput accumulate(GoblinUltraCircuitBuilder& circuit_builder)
+    using ClientCircuit = GoblinUltraCircuitBuilder; // can only be GoblinUltra
+    Goblin goblin;
+    Accumulator accumulator;
+
+    ClientIVC()
     {
-        // Complete the circuit logic by recursively verifying previous merge proof if it exists
-        if (merge_proof_exists) {
-            RecursiveMergeVerifier merge_verifier{ &circuit_builder };
-            [[maybe_unused]] auto pairing_points = merge_verifier.verify_proof(merge_proof);
-        }
-
-        // Construct a Honk proof for the main circuit
-        GoblinUltraComposer composer;
-        auto instance = composer.create_instance(circuit_builder);
-        auto prover = composer.create_prover(instance);
-        auto ultra_proof = prover.construct_proof();
-
-        // Construct and store the merge proof to be recursively verified on the next call to accumulate
-        MergeProver merge_prover{ op_queue };
-        merge_proof = merge_prover.construct_proof();
-
-        if (!merge_proof_exists) {
-            merge_proof_exists = true;
-        }
-
-        return { ultra_proof, instance->verification_key };
-    };
-
-    /** WORKTODO
-     * @brief Similar to "accumulate" but only does merge-related stuff, no proving
-     * @details When this method is used, the "prover" functionality of the IVC scheme must be performed explicitly, but
-     * this method has to be called first so that the recursive merge verifier can be "appended" to the circuit being
-     * accumulated
-     *
-     * @param circuit_builder
-     */
-    void merge(GoblinUltraCircuitBuilder& circuit_builder)
-    {
-        // Complete the circuit logic by recursively verifying previous merge proof if it exists
-        if (merge_proof_exists) {
-            RecursiveMergeVerifier merge_verifier{ &circuit_builder };
-            [[maybe_unused]] auto pairing_points = merge_verifier.verify_proof(merge_proof);
-        }
-
-        // Construct and store the merge proof to be recursively verified on the next call to accumulate
-        MergeProver merge_prover{ op_queue };
-        merge_proof = merge_prover.construct_proof();
-
-        if (!merge_proof_exists) {
-            merge_proof_exists = true;
-        }
-    };
-
-    /**
-     * @brief Construct an ECCVM proof and the translation polynomial evaluations
-     *
-     */
-    void prove_eccvm()
-    {
-        eccvm_builder = std::make_unique<ECCVMBuilder>(op_queue);
-        eccvm_composer = std::make_unique<ECCVMComposer>();
-        eccvm_prover = std::make_unique<ECCVMProver>(eccvm_composer->create_prover(*eccvm_builder));
-        goblin_proof.eccvm_proof = eccvm_prover->construct_proof();
-        goblin_proof.translation_evaluations = eccvm_prover->translation_evaluations;
-    };
-
-    /**
-     * @brief Construct a translator proof
-     *
-     */
-    void prove_translator()
-    {
-        translator_builder = std::make_unique<TranslatorBuilder>(
-            eccvm_prover->translation_batching_challenge_v, eccvm_prover->evaluation_challenge_x, op_queue);
-        translator_composer = std::make_unique<TranslatorComposer>();
-        auto translator_prover = translator_composer->create_prover(*translator_builder, eccvm_prover->transcript);
-        goblin_proof.translator_proof = translator_prover.construct_proof();
-    };
-
-    /**
-     * @brief Constuct a full Goblin proof (ECCVM, Translator, merge)
-     * @details The merge proof is assumed to already have been constucted in the last accumulate step. It is simply
-     * moved into the final proof here.
-     *
-     * @return Proof
-     */
-    Proof prove()
-    {
-        goblin_proof.merge_proof = std::move(merge_proof);
-        prove_eccvm();
-        prove_translator();
-        return goblin_proof;
-    };
-
-    /**
-     * @brief Verify a full Goblin proof (ECCVM, Translator, merge)
-     *
-     * @param proof
-     * @return true
-     * @return false
-     */
-    bool verify(const Proof& proof)
-    {
-        MergeVerifier merge_verifier;
-        bool merge_verified = merge_verifier.verify_proof(proof.merge_proof);
-
-        auto eccvm_verifier = eccvm_composer->create_verifier(*eccvm_builder);
-        bool eccvm_verified = eccvm_verifier.verify_proof(proof.eccvm_proof);
-
-        auto translator_verifier = translator_composer->create_verifier(*translator_builder, eccvm_verifier.transcript);
-        bool accumulator_construction_verified = translator_verifier.verify_proof(proof.translator_proof);
-        // TODO(https://github.com/AztecProtocol/barretenberg/issues/799): Ensure translation_evaluations are passed
-        // correctly
-        bool translation_verified = translator_verifier.verify_translation(proof.translation_evaluations);
-
-        return merge_verified && eccvm_verified && accumulator_construction_verified && translation_verified;
-    };
-
-    // The methods below this point are to be used only for ACIR. They exist while the interface is in flux. Eventually
-    // there will be agreement and no acir-specific methods should be needed.
-
-    /**
-     * @brief Construct a GUH proof for the given circuit. (No merge proof for now)
-     *
-     * @param circuit_builder
-     * @return std::vector<bb::fr>
-     */
-    std::vector<bb::fr> accumulate_for_acir(GoblinUltraCircuitBuilder& circuit_builder)
-    {
-        // TODO(https://github.com/AztecProtocol/barretenberg/issues/811): no merge prover for now
-        // // Complete the circuit logic by recursively verifying previous merge proof if it exists
-        // if (merge_proof_exists) {
-        //     RecursiveMergeVerifier merge_verifier{ &circuit_builder };
-        //     [[maybe_unused]] auto pairing_points = merge_verifier.verify_proof(merge_proof);
-        // }
-
-        // Construct a Honk proof for the main circuit
-        GoblinUltraComposer composer;
-        auto instance = composer.create_instance(circuit_builder);
-        auto prover = composer.create_prover(instance);
-        auto ultra_proof = prover.construct_proof();
-
-        accumulator = { ultra_proof, instance->verification_key };
-
-        // TODO(https://github.com/AztecProtocol/barretenberg/issues/811): no merge prover for now since we're not
-        // mocking the first set of ecc ops
-        // // Construct and store the merge proof to be recursively verified on the next call to accumulate
-        // MergeProver merge_prover{ op_queue };
-        // merge_proof = merge_prover.construct_proof();
-
-        // if (!merge_proof_exists) {
-        //     merge_proof_exists = true;
-        // }
-
-        return ultra_proof;
-    };
-
-    /**
-     * @brief Verify a GUH proof
-     *
-     * @param proof_buf
-     * @return true
-     * @return false
-     */
-    bool verify_accumulator_for_acir(const std::vector<bb::fr>& proof_buf) const
-    {
-        GoblinUltraVerifier verifier{ accumulator.verification_key };
-        HonkProof proof{ proof_buf };
-        bool verified = verifier.verify_proof(proof);
-
-        return verified;
+        // TODO(https://github.com/AztecProtocol/barretenberg/issues/723):
+        GoblinMockCircuits::perform_op_queue_interactions_for_mock_first_circuit(goblin.op_queue);
     }
 
-    /**
-     * @brief Construct a Goblin proof
-     *
-     * @return Proof
-     */
-    Proof prove_for_acir() { return prove(); };
-
-    /**
-     * @brief Verify a Goblin proof (excluding the merge proof for now)
-     *
-     * @return true
-     * @return false
-     */
-    bool verify_for_acir() const
+  private:
+  public:
+    void initialize(ClientCircuit& circuit)
     {
-        // TODO(https://github.com/AztecProtocol/barretenberg/issues/811): No merge proof for now
-        // MergeVerifier merge_verifier;
-        // bool merge_verified = merge_verifier.verify_proof(goblin_proof.merge_proof);
+        merge(circuit);
+        Composer composer;
+        accumulator = composer.create_instance(circuit);
+    }
 
-        auto eccvm_verifier = eccvm_composer->create_verifier(*eccvm_builder);
-        bool eccvm_verified = eccvm_verifier.verify_proof(goblin_proof.eccvm_proof);
+    FoldProof accumulate(ClientCircuit& circuit)
+    {
+        merge(circuit);
+        Composer composer;
+        auto instance = composer.create_instance(circuit);
+        std::vector<std::shared_ptr<Instance>> instances{ accumulator, instance };
+        auto folding_prover = composer.create_folding_prover(instances);
+        FoldingOutput output = folding_prover.fold_instances();
+        accumulator = output.accumulator;
+        return output.folding_data;
+    }
 
-        auto translator_verifier = translator_composer->create_verifier(*translator_builder, eccvm_verifier.transcript);
-        bool translation_accumulator_construction_verified =
-            translator_verifier.verify_proof(goblin_proof.translator_proof);
-        // TODO(https://github.com/AztecProtocol/barretenberg/issues/799): Ensure translation_evaluations are passed
-        // correctly
-        bool translation_verified = translator_verifier.verify_translation(goblin_proof.translation_evaluations);
-
-        return /* merge_verified && */ eccvm_verified && translation_accumulator_construction_verified &&
-               translation_verified;
-    };
+    void merge(ClientCircuit& circuit_builder) { goblin.merge(circuit_builder); }
 };
 } // namespace bb
--- barretenberg/cpp/src/barretenberg/client_ivc/ivc_folding.test.cpp
+++ barretenberg/cpp/src/barretenberg/client_ivc/ivc_folding.test.cpp
@@ -20,52 +20,40 @@ class ClientIVCTests : public ::testing::Test {
     using Flavor = GoblinUltraFlavor;
     using Curve = typename Flavor::Curve;
     using FF = typename Flavor::FF;
-    using Builder = GoblinUltraCircuitBuilder;
+    using Builder = ClientIVC::ClientCircuit;
     using Composer = GoblinUltraComposer;
     using Instance = ProverInstance_<GoblinUltraFlavor>;
-    using AccumulationProof = HonkProof;
-    using AccumulatorWithProof = FoldingResult<GoblinUltraFlavor>;
 
     using GURecursiveFlavor = GoblinUltraRecursiveFlavor_<Builder>;
     using RecursiveVerifierInstances = ::bb::VerifierInstances_<GURecursiveFlavor, 2>;
     using FoldingRecursiveVerifier =
         bb::stdlib::recursion::honk::ProtoGalaxyRecursiveVerifier_<RecursiveVerifierInstances>;
 
-    using ClientCircuit = typename ClientIVC::Circuit;
+    // WORKTODO: temporary stuff to be eventiually handled internally by ivc
+
+    using FoldingOutput = FoldingResult<GoblinUltraFlavor>;
+    using Accumulator = std::shared_ptr<ProverInstance_<Flavor>>;
+    using FoldProof = std::vector<FF>;
 
     // Currently default sized to 2^16 to match kernel. (Note: op gates will bump size to next power of 2)
     static Builder create_mock_circuit(ClientIVC& ivc, size_t num_gates = 1 << 15)
     {
-        ClientCircuit circuit; // WORKTODO: write constructor
+        Builder circuit{ ivc.goblin.op_queue }; // WORKTODO: write constructor
         GoblinMockCircuits::construct_arithmetic_circuit(circuit, num_gates);
         GoblinMockCircuits::construct_goblin_ecc_op_circuit(circuit);
         return circuit;
     }
 
-    // WORKTODO: this moves in
-    static AccumulatorWithProof fold(std::shared_ptr<Instance>& accumulator, Builder& circuit_to_fold)
+    // Currently default sized to 2^16 to match kernel. (Note: op gates will bump size to next power of 2)
+    static void create_mock_function_circuit(Builder& builder, size_t num_gates = 1 << 15)
     {
-        Composer composer;
-        auto instance = composer.create_instance(circuit_to_fold);
-        std::vector<std::shared_ptr<Instance>> instances{ accumulator, instance };
-        auto folding_prover = composer.create_folding_prover(instances);
-        AccumulatorWithProof output = folding_prover.fold_instances();
-        return output;
-    }
-
-    static bool decide_and_verify(std::shared_ptr<Instance>& accumulator)
-    {
-        Composer composer;
-        auto decider_prover = composer.create_decider_prover(accumulator);
-        auto decider_verifier = composer.create_decider_verifier(accumulator);
-        auto decider_proof = decider_prover.construct_proof();
-        auto verified = decider_verifier.verify_proof(decider_proof);
-        return verified;
+        GoblinMockCircuits::construct_arithmetic_circuit(builder, num_gates);
+        GoblinMockCircuits::construct_goblin_ecc_op_circuit(builder);
     }
 
     static void construct_mock_folding_kernel(Builder& builder,
-                                              AccumulationProof& fctn_fold_proof,
-                                              AccumulationProof& kernel_fold_proof)
+                                              FoldProof& fctn_fold_proof,
+                                              FoldProof& kernel_fold_proof)
     {
         FoldingRecursiveVerifier verifier_1{ &builder };
         verifier_1.verify_folding_proof(fctn_fold_proof);
@@ -75,60 +63,156 @@ class ClientIVCTests : public ::testing::Test {
     }
 
     // DEBUG only: perform native fold verification and run decider prover/verifier
-    static void EXEPECT_FOLDING_AND_DECIDING_VERIFIED(AccumulatorWithProof& folding_output)
+    static void EXEPECT_FOLDING_AND_DECIDING_VERIFIED(const Accumulator& accumulator, const FoldProof& fold_proof)
     {
         // Verify fold proof
         Composer composer;
         auto folding_verifier = composer.create_folding_verifier();
-        bool folding_verified = folding_verifier.verify_folding_proof(folding_output.folding_data);
+        bool folding_verified = folding_verifier.verify_folding_proof(fold_proof);
         EXPECT_TRUE(folding_verified);
 
         // Run decider
-        auto decider_prover = composer.create_decider_prover(folding_output.accumulator);
-        auto decider_verifier = composer.create_decider_verifier(folding_output.accumulator);
+        auto decider_prover = composer.create_decider_prover(accumulator);
+        auto decider_verifier = composer.create_decider_verifier(accumulator);
         auto decider_proof = decider_prover.construct_proof();
         bool decision = decider_verifier.verify_proof(decider_proof);
         EXPECT_TRUE(decision);
     }
 };
 
+// /**
+//  * @brief A full Goblin test using PG that mimicks the basic aztec client architecture
+//  * @details
+//  */
+// TEST_F(ClientIVCTests, Full)
+// {
+//     // WORKTODO move these up
+//     const auto EXPECT_ACCUMULATION_VERIFIED = [](AccumulatorWithProof& folding_output) {
+//         EXPECT_TRUE(ivc.verify_accumulation(folding_output));
+//     };
+
+//     const auto EXPECT_ACCUMULATOR_VERIFIED = [](AccumulatorWithProof& folding_output) {
+//         EXPECT_TRUE(ivc.verify_accumulator(folding_output));
+//     };
+
+//     ClientIVC ivc; // mock opqueue  happens inside
+//     Builder function_circuit = create_mock_circuit(ivc);
+//     ivc.initialize(function_circuit);
+
+//     Builder kernel_circuit = create_mock_circuit(ivc);
+//     AccumulatorWithProof kernel_acc_with_proof = ivc.accumulate(kernel_circuit); // handles merge base case
+//     EXPECT_ACCUMULATION_VERIFIED(kernel_acc_with_proof);
+
+//     size_t NUM_KERNEL_ITERATIONS_MINUS_1 = 1;
+//     for (size_t circuit_idx = 0; circuit_idx < NUM_KERNEL_ITERATIONS_MINUS_1; ++circuit_idx) {
+//         // Construct and accumulate a mock function circuit
+//         Builder function_circuit = create_mock_circuit(ivc);
+//         function_folding_output = ivc.accumulate(function_circuit);
+//         EXEPECT_FOLDING_AND_DECIDING_VERIFIED(function_folding_output);
+
+//         // Construct and accumulate the mock kernel circuit
+//         // WORKNOTE: this is the reason to track two
+//         Builder kernel_circuit =
+//             construct_mock_folding_kernel(function_folding_output.folding_data, kernel_acc_with_proof.folding_data);
+//         kernel_acc_with_proof = ivc.accumulate(kernel_circuit);
+//         EXEPECT_FOLDING_AND_DECIDING_VERIFIED(kernel_acc_with_proof);
+//     }
+
+//     EXPECT_ACCUMULATOR_VERIFIED(kernel_accumulation_output);
+// }
+
 /**
  * @brief A full Goblin test using PG that mimicks the basic aztec client architecture
  * @details
  */
-TEST_F(ClientIVCTests, Full)
+TEST_F(ClientIVCTests, Working)
 {
-    // WORKTODO move these up
-    const auto EXPECT_ACCUMULATION_VERIFIED = [](AccumulatorWithProof& folding_output) {
-        EXPECT_TRUE(ivc.verify_accumulation(folding_output));
-    };
+    ClientIVC ivc;
 
-    const auto EXPECT_ACCUMULATOR_VERIFIED = [](AccumulatorWithProof& folding_output) {
-        EXPECT_TRUE(ivc.verify_accumulator(folding_output));
-    };
-
-    ClientIVC ivc; // mock opqueue  happens inside
+    // "Round 0"
+    // Initialize accumulator with function instance
     Builder function_circuit = create_mock_circuit(ivc);
     ivc.initialize(function_circuit);
+    info("function circuit num_gates = ", ivc.accumulator->proving_key->circuit_size);
 
+    // Fold kernel circuit into function instance
     Builder kernel_circuit = create_mock_circuit(ivc);
-    AccumulatorWithProof kernel_acc_with_proof = ivc.accumulate(kernel_circuit); // handles merge base case
-    EXPECT_ACCUMULATION_VERIFIED(kernel_acc_with_proof);
+    FoldProof kernel_fold_proof = ivc.accumulate(kernel_circuit);
+    info("kernel circuit num_gates = ", ivc.accumulator->instance_size);
+    EXEPECT_FOLDING_AND_DECIDING_VERIFIED(ivc.accumulator, kernel_fold_proof);
 
-    size_t NUM_KERNEL_ITERATIONS_MINUS_1 = 1;
-    for (size_t circuit_idx = 0; circuit_idx < NUM_KERNEL_ITERATIONS_MINUS_1; ++circuit_idx) {
+    // "Round i"
+    size_t NUM_CIRCUITS = 1;
+    for (size_t circuit_idx = 0; circuit_idx < NUM_CIRCUITS; ++circuit_idx) {
         // Construct and accumulate a mock function circuit
         Builder function_circuit = create_mock_circuit(ivc);
-        function_folding_output = ivc.accumulate(function_circuit);
-        EXEPECT_FOLDING_AND_DECIDING_VERIFIED(function_folding_output);
+        FoldProof function_fold_proof = ivc.accumulate(function_circuit);
+        EXEPECT_FOLDING_AND_DECIDING_VERIFIED(ivc.accumulator, function_fold_proof);
 
         // Construct and accumulate the mock kernel circuit
-        // WORKNOTE: this is the reason to track two
-        Builder kernel_circuit =
-            construct_mock_folding_kernel(function_folding_output.folding_data, kernel_acc_with_proof.folding_data);
-        kernel_acc_with_proof = ivc.accumulate(kernel_circuit);
-        EXEPECT_FOLDING_AND_DECIDING_VERIFIED(kernel_acc_with_proof);
+        Builder kernel_circuit{ ivc.goblin.op_queue };
+        construct_mock_folding_kernel(kernel_circuit, function_fold_proof, kernel_fold_proof);
+        FoldProof kernel_fold_proof = ivc.accumulate(kernel_circuit);
+        EXEPECT_FOLDING_AND_DECIDING_VERIFIED(ivc.accumulator, kernel_fold_proof);
     }
 
-    EXPECT_ACCUMULATOR_VERIFIED(kernel_accumulation_output);
+    // Verify the goblin proof (eccvm, translator, merge)
+    Goblin::Proof proof = ivc.goblin.prove();
+    bool goblin_verified = ivc.goblin.verify(proof);
+    EXPECT_TRUE(goblin_verified);
 }
+
+// /**
+//  * @brief A full Goblin test using PG that mimicks the basic aztec client architecture
+//  * @details
+//  */
+// TEST_F(ClientIVCTests, WorkingOriginl)
+// {
+//     Goblin goblin;
+
+//     // TODO(https://github.com/AztecProtocol/barretenberg/issues/723):
+//     GoblinMockCircuits::perform_op_queue_interactions_for_mock_first_circuit(goblin.op_queue);
+
+//     // "Round 0"
+//     // Initialize accumulator with function instance
+//     Builder function_circuit{ goblin.op_queue };
+//     create_mock_function_circuit(function_circuit);
+//     FoldingOutput function_folding_output;
+//     Composer composer;
+//     function_folding_output.accumulator = composer.create_instance(function_circuit);
+//     info("function circuit num_gates = ", function_folding_output.accumulator->proving_key->circuit_size);
+
+//     // Fold kernel circuit into function instance
+//     Builder kernel_circuit{ goblin.op_queue };
+//     create_mock_function_circuit(kernel_circuit);
+//     FoldingOutput kernel_folding_output =
+//         construct_fold_proof_and_update_accumulator(function_folding_output.accumulator, kernel_circuit);
+//     info("kernel circuit num_gates = ", kernel_folding_output.accumulator->instance_size);
+//     EXEPECT_FOLDING_AND_DECIDING_VERIFIED(kernel_folding_output);
+
+//     // "Round i"
+//     size_t NUM_CIRCUITS = 1;
+//     for (size_t circuit_idx = 0; circuit_idx < NUM_CIRCUITS; ++circuit_idx) {
+//         // Construct and accumulate a mock function circuit
+//         Builder function_circuit{ goblin.op_queue };
+//         create_mock_function_circuit(function_circuit);
+//         goblin.merge(function_circuit); // must be called prior to folding
+//         function_folding_output =
+//             construct_fold_proof_and_update_accumulator(kernel_folding_output.accumulator, function_circuit);
+//         EXEPECT_FOLDING_AND_DECIDING_VERIFIED(function_folding_output);
+
+//         // Construct and accumulate the mock kernel circuit
+//         Builder kernel_circuit{ goblin.op_queue };
+//         construct_mock_folding_kernel(
+//             kernel_circuit, function_folding_output.folding_data, kernel_folding_output.folding_data);
+//         goblin.merge(kernel_circuit); // must be called prior to folding
+//         kernel_folding_output =
+//             construct_fold_proof_and_update_accumulator(function_folding_output.accumulator, kernel_circuit);
+//         EXEPECT_FOLDING_AND_DECIDING_VERIFIED(kernel_folding_output);
+//     }
+
+//     // Verify the goblin proof (eccvm, translator, merge)
+//     Goblin::Proof proof = goblin.prove();
+//     bool goblin_verified = goblin.verify(proof);
+//     EXPECT_TRUE(goblin_verified);
+// }
