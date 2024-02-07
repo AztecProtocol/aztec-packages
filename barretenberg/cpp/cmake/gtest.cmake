97162458f9d9689a756c438c7003a6395a3bc4cd
ludamad <adam.domurad@gmail.com>
2024-02-07 13:10:32 -0500

    Update gtest.cmake

--- barretenberg/cpp/cmake/gtest.cmake
+++ barretenberg/cpp/cmake/gtest.cmake
@@ -1,15 +1,15 @@
 include(GoogleTest)
 include(FetchContent)
 
+set(BUILD_GMOCK ON CACHE BOOL "Build with gMock enabled")
+set(INSTALL_GTEST OFF CACHE BOOL "gTest installation disabled")
+
 FetchContent_Declare(
     GTest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG v1.13.0 #v1.14.0 does not compile with gcc (compiler bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105329)
     FIND_PACKAGE_ARGS
 )
-
-set(BUILD_GMOCK ON CACHE BOOL "Build with gMock enabled")
-set(INSTALL_GTEST OFF CACHE BOOL "gTest installation disabled")
 
 FetchContent_MakeAvailable(GTest)
 
