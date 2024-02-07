5875c4ec42614c629a143f00bae1a23652f015d4
Santiago Palladino <santiago@aztecprotocol.com>
2024-02-07 09:55:33 -0300

    Nicer API for broadcast functions

--- yarn-project/aztec.js/src/deployment/register_class.ts
+++ yarn-project/aztec.js/src/deployment/register_class.ts
@@ -1,10 +1,9 @@
 import { MAX_PACKED_PUBLIC_BYTECODE_SIZE_IN_FIELDS, getContractClassFromArtifact } from '@aztec/circuits.js';
 import { ContractArtifact, bufferAsFields } from '@aztec/foundation/abi';
-import { getCanonicalClassRegisterer } from '@aztec/protocol-contracts/class-registerer';
 
 import { ContractFunctionInteraction } from '../contract/contract_function_interaction.js';
-import { UnsafeContract } from '../contract/unsafe_contract.js';
 import { Wallet } from '../wallet/index.js';
+import { getRegistererContract } from './protocol_contracts.js';
 
 /** Sets up a call to register a contract class given its artifact. */
 export function registerContractClass(wallet: Wallet, artifact: ContractArtifact): ContractFunctionInteraction {
@@ -13,10 +12,4 @@ export function registerContractClass(wallet: Wallet, artifact: ContractArtifact
   const encodedBytecode = bufferAsFields(packedBytecode, MAX_PACKED_PUBLIC_BYTECODE_SIZE_IN_FIELDS);
   const registerer = getRegistererContract(wallet);
   return registerer.methods.register(artifactHash, privateFunctionsRoot, publicBytecodeCommitment, encodedBytecode);
-}
-
-/** Returns a Contract wrapper for the class registerer. */
-function getRegistererContract(wallet: Wallet) {
-  const { artifact, instance } = getCanonicalClassRegisterer();
-  return new UnsafeContract(instance, artifact, wallet);
 }
