4cfcfd0c572cf5aa5ff6702b9cb1ef05fa561143
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-02-08 01:38:30 +0000

    fix: update constants

--- l1-contracts/src/core/libraries/ConstantsGen.sol
+++ l1-contracts/src/core/libraries/ConstantsGen.sol
@@ -94,7 +94,7 @@ library Constants {
   uint256 internal constant PRIVATE_CALL_STACK_ITEM_LENGTH = 212;
   uint256 internal constant CONTRACT_STORAGE_UPDATE_REQUEST_LENGTH = 2;
   uint256 internal constant CONTRACT_STORAGE_READ_LENGTH = 2;
-  uint256 internal constant PUBLIC_CIRCUIT_PUBLIC_INPUTS_LENGTH = 203;
+  uint256 internal constant PUBLIC_CIRCUIT_PUBLIC_INPUTS_LENGTH = 187;
   uint256 internal constant GET_NOTES_ORACLE_RETURN_LENGTH = 674;
   uint256 internal constant COMMITMENTS_NUM_BYTES_PER_BASE_ROLLUP = 2048;
   uint256 internal constant NULLIFIERS_NUM_BYTES_PER_BASE_ROLLUP = 2048;
