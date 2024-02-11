9a28126a91e86090e23447e88afaa8844423d60e
ludamad <adam@aztecprotocol.com>
2024-02-11 11:39:29 +0000

    fix: gcc

--- barretenberg/cpp/src/barretenberg/ecc/fields/field_declarations.hpp
+++ barretenberg/cpp/src/barretenberg/ecc/fields/field_declarations.hpp
@@ -335,8 +335,17 @@ template <class Params_> struct alignas(32) field {
                 split_into_endomorphism_scalars_no_shift(k);
             k1.data[0] = ret.first[0];
             k1.data[1] = ret.first[1];
+
+            // TODO(AD): We should move away from this hack by adapting split_into_endomorphism_scalars_no_shift
+#if !defined(__clang__) && defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Warray-bounds"
+#endif
             k2.data[0] = ret.second[0];
             k2.data[1] = ret.second[1];
+#if !defined(__clang__) && defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
         }
     }
 
