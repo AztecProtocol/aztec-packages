6d898516253a493d769b22e209f34d575ba96721
guipublic <guipublic@gmail.com>
2024-02-13 16:54:50 +0000

    fix poseidon2 test case

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/poseidon2_constraint.test.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/poseidon2_constraint.test.cpp
@@ -31,6 +31,7 @@ TEST_F(Poseidon2Tests, TestPoseidon2Permutation)
                                   .logic_constraints = {},
                                   .range_constraints = {},
                                   .sha256_constraints = {},
+                                  .sha256_compression = {},
                                   .schnorr_constraints = {},
                                   .ecdsa_k1_constraints = {},
                                   .ecdsa_r1_constraints = {},
