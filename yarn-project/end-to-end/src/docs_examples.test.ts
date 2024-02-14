9f4ff3f0df1e2a4a4ddd4753522369b2f6a15a20
Cat McGee <hello@mcgee.cat>
2024-02-14 22:34:07 +0900

    fix some things

--- yarn-project/end-to-end/src/docs_examples.test.ts
+++ yarn-project/end-to-end/src/docs_examples.test.ts
@@ -1,32 +1,47 @@
+// docs:start:create_account_imports
 import { getSchnorrAccount } from '@aztec/accounts/schnorr';
 import { GrumpkinScalar, createPXEClient } from '@aztec/aztec.js';
+// docs:end:create_account_imports
+// docs:start:import_contract
 import { Contract } from "@aztec/aztec.js";
-import { TokenContract } from '@aztec/noir-contracts.js/TokenContract';
+// docs:end:import_contract
+// docs:start:import_token_contract
+import { TokenContract, TokenContractArtifact } from '@aztec/noir-contracts.js/Token';
+// docs:end:import_token_contract
 
+// docs:start:define_account_vars
 const PXE_URL = process.env.PXE_URL || 'http://localhost:8080';
 const encryptionPrivateKey = GrumpkinScalar.random();
 const signingPrivateKey = GrumpkinScalar.random();
 const pxe = createPXEClient(PXE_URL);
+// docs:end:define_account_vars
 
+// docs:start:create_wallet
 const wallet = await getSchnorrAccount(
-    pxe,
+    pxe, 
     encryptionPrivateKey,
     signingPrivateKey
   ).waitDeploy();
   console.log(`New account deployed at ${wallet.getAddress()}`);
-
+  // docs:end:create_wallet
+  
+// docs:start:deploy_contract
 const deployedContract = await TokenContract.deploy(
-    wallet, 
-    TokenContract, 
-    'TokenName', 
-    'TokenSymbol', 
-    18)
+    wallet, // wallet instance
+    wallet.getAddress(), // account
+    'TokenName', // constructor arg1
+    'TokenSymbol', // constructor arg2
+    18) // constructor arg3
   .send()
   .deployed();
 console.log(`New contract deployed at ${deployedContract.address}`)
+// docs:end:deploy_contract
 
-const contract = await Contract.at(deployedContract.address, TokenContract, wallet);
+// docs:start:get_contract
+const contract = await Contract.at(deployedContract.address, TokenContractArtifact, wallet);
+// docs:end:get_contract
 
+// docs:start:send_transaction
 const tx = await contract.methods
   .transfer(1, wallet)
   .send()
@@ -34,6 +49,9 @@ const tx = await contract.methods
 console.log(
   `Transferred 1 to ${wallet.getAddress()} on block ${tx.blockNumber}`
 );
+// docs:end:send_transaction
 
+// docs:start:call_view_function
 const balance = await contract.methods.getBalance(wallet.getAddress()).view();
 console.log(`Account balance is ${balance}`);
+// docs:end:call_view_function
