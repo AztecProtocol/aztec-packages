0794ec3b9a8c98e77816d45121f56d29a3367538
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-02-14 22:34:03 +0000

    fix: get e2e hashing working

--- yarn-project/simulator/src/avm/opcodes/hashing.test.ts
+++ yarn-project/simulator/src/avm/opcodes/hashing.test.ts
@@ -17,17 +17,19 @@ describe('Hashing Opcodes', () => {
     it('Should (de)serialize correctly', () => {
       const buf = Buffer.from([
         Poseidon2.opcode, // opcode
+        1, // indirect
         ...Buffer.from('12345678', 'hex'), // dstOffset
         ...Buffer.from('23456789', 'hex'), // hashOffset
         ...Buffer.from('3456789a', 'hex'), // hashSize
       ]);
-      const inst = new Poseidon2(/*dstOffset=*/ 0x12345678, /*hashOffset=*/ 0x23456789, /*hashSize=*/ 0x3456789a);
+      const inst = new Poseidon2(/*indirect=*/ 1, /*dstOffset=*/ 0x12345678, /*hashOffset=*/ 0x23456789, /*hashSize=*/ 0x3456789a);
 
       expect(Poseidon2.deserialize(buf)).toEqual(inst);
       expect(inst.serialize()).toEqual(buf);
     });
 
-    it('Should hash correctly', async () => {
+    it('Should hash correctly - direct', async () => {
+      const indirect = 0;
       const args = [new Field(1n), new Field(2n), new Field(3n)];
       const hashOffset = 0;
       context.machineState.memory.setSlice(hashOffset, args);
@@ -35,29 +37,32 @@ describe('Hashing Opcodes', () => {
       const dstOffset = 3;
 
       const expectedHash = poseidonHash(args.map(field => field.toBuffer()));
-      await new Poseidon2(dstOffset, hashOffset, args.length).execute(context);
+      await new Poseidon2(indirect, dstOffset, hashOffset, args.length).execute(context);
 
       const result = context.machineState.memory.get(dstOffset);
       expect(result).toEqual(new Field(toBigIntBE(expectedHash)));
     });
+    // TODO: indirect
   });
 
   describe('Keccak', () => {
     it('Should (de)serialize correctly', () => {
       const buf = Buffer.from([
         Keccak.opcode, // opcode
+        1, // indirect
         ...Buffer.from('12345678', 'hex'), // dstOffset
         ...Buffer.from('23456789', 'hex'), // hashOffset
         ...Buffer.from('3456789a', 'hex'), // hashSize
       ]);
-      const inst = new Keccak(/*dstOffset=*/ 0x12345678, /*hashOffset=*/ 0x23456789, /*hashSize=*/ 0x3456789a);
+      const inst = new Keccak(/*indirect=*/1, /*dstOffset=*/ 0x12345678, /*hashOffset=*/ 0x23456789, /*hashSize=*/ 0x3456789a);
 
       expect(Keccak.deserialize(buf)).toEqual(inst);
       expect(inst.serialize()).toEqual(buf);
     });
 
-    it('Should hash correctly', async () => {
+    it('Should hash correctly - direct', async () => {
       const args = [new Field(1n), new Field(2n), new Field(3n)];
+      const indirect = 0;
       const hashOffset = 0;
       context.machineState.memory.setSlice(hashOffset, args);
 
@@ -65,24 +70,26 @@ describe('Hashing Opcodes', () => {
 
       const inputBuffer = Buffer.concat(args.map(field => field.toBuffer()));
       const expectedHash = keccak(inputBuffer);
-      await new Keccak(dstOffset, hashOffset, args.length).execute(context);
+      await new Keccak(indirect, dstOffset, hashOffset, args.length).execute(context);
 
       const result = context.machineState.memory.getSliceAs<Field>(dstOffset, 2);
       const combined = Buffer.concat([result[0].toBuffer().subarray(16, 32), result[1].toBuffer().subarray(16, 32)]);
 
       expect(combined).toEqual(expectedHash);
     });
+    // TODO: indirect
   });
 
   describe('Sha256', () => {
     it('Should (de)serialize correctly', () => {
       const buf = Buffer.from([
         Sha256.opcode, // opcode
+        1, // indirect
         ...Buffer.from('12345678', 'hex'), // dstOffset
         ...Buffer.from('23456789', 'hex'), // hashOffset
         ...Buffer.from('3456789a', 'hex'), // hashSize
       ]);
-      const inst = new Sha256(/*dstOffset=*/ 0x12345678, /*hashOffset=*/ 0x23456789, /*hashSize=*/ 0x3456789a);
+      const inst = new Sha256(/*indirect=*/1, /*dstOffset=*/ 0x12345678, /*hashOffset=*/ 0x23456789, /*hashSize=*/ 0x3456789a);
 
       expect(Sha256.deserialize(buf)).toEqual(inst);
       expect(inst.serialize()).toEqual(buf);
@@ -91,13 +98,14 @@ describe('Hashing Opcodes', () => {
     it('Should hash correctly', async () => {
       const args = [new Field(1n), new Field(2n), new Field(3n)];
       const hashOffset = 0;
+      const indirect = 0;
       context.machineState.memory.setSlice(hashOffset, args);
 
       const dstOffset = 3;
 
       const inputBuffer = Buffer.concat(args.map(field => field.toBuffer()));
       const expectedHash = sha256(inputBuffer);
-      await new Sha256(dstOffset, hashOffset, args.length).execute(context);
+      await new Sha256(indirect, dstOffset, hashOffset, args.length).execute(context);
 
       const result = context.machineState.memory.getSliceAs<Field>(dstOffset, 2);
       const combined = Buffer.concat([result[0].toBuffer().subarray(16, 32), result[1].toBuffer().subarray(16, 32)]);
@@ -110,11 +118,12 @@ describe('Hashing Opcodes', () => {
     it('Should (de)serialize correctly', () => {
       const buf = Buffer.from([
         Pedersen.opcode, // opcode
+        1, // indirect
         ...Buffer.from('12345678', 'hex'), // dstOffset
         ...Buffer.from('23456789', 'hex'), // hashOffset
         ...Buffer.from('3456789a', 'hex'), // hashSize
       ]);
-      const inst = new Pedersen(/*dstOffset=*/ 0x12345678, /*hashOffset=*/ 0x23456789, /*hashSize=*/ 0x3456789a);
+      const inst = new Pedersen(/*indirect=*/ 1,/*dstOffset=*/ 0x12345678, /*hashOffset=*/ 0x23456789, /*hashSize=*/ 0x3456789a);
 
       expect(Sha256.deserialize(buf)).toEqual(inst);
       expect(inst.serialize()).toEqual(buf);
@@ -123,13 +132,14 @@ describe('Hashing Opcodes', () => {
     it('Should hash correctly', async () => {
       const args = [new Field(1n), new Field(2n), new Field(3n)];
       const hashOffset = 0;
+      const indirect = 0;
       context.machineState.memory.setSlice(hashOffset, args);
 
       const dstOffset = 3;
 
       const inputBuffer = args.map(field => field.toBuffer());
       const expectedHash = pedersenHash(inputBuffer);
-      await new Pedersen(dstOffset, hashOffset, args.length).execute(context);
+      await new Pedersen(indirect, dstOffset, hashOffset, args.length).execute(context);
 
       const result = context.machineState.memory.get(dstOffset);
       expect(result).toEqual(new Field(toBigIntBE(expectedHash)));
