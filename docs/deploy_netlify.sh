2b409d38f27d9c79e2bd314e4cd1c7ab0cefbc75
José Pedro Sousa <outgoing@zpedro.dev>
2024-02-14 13:22:35 +0000

    trying to get some docs previews

--- docs/deploy_netlify.sh
+++ docs/deploy_netlify.sh
@@ -5,5 +5,16 @@ set -eu
 extract_repo docs /usr/src extracted-repo
 cd extracted-repo/src/docs
 npm install netlify-cli -g
-netlify deploy --site aztec-docs-dev
-netlify deploy --site aztec-docs-dev --prod
+
+DEPLOY_OUTPUT=""
+
+# Check if we're on master
+if [ "$1" = "master" ]; then
+    # Deploy to production if the argument is "master"
+    DEPLOY_OUTPUT=$(netlify deploy --site aztec-docs-dev --prod)
+else
+    # Regular deploy if the argument is not "master"
+    DEPLOY_OUTPUT=$(netlify deploy --site aztec-docs-dev)
+    UNIQUE_DEPLOY_URL=$(echo "$DEPLOY_OUTPUT" | grep -o 'Unique deploy URL:.*' | awk '{print $4}')
+    echo "$UNIQUE_DEPLOY_URL"
+fi
