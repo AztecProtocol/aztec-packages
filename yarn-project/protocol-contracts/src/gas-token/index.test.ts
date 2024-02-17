f425663eabaff2d03aa5ec013872763ee9c0b01f
Mitchell Tracy <mitchell@aztecprotocol.com>
2024-02-16 20:12:48 -0500

    remove the snapshot and dynamically compute contract address

--- yarn-project/protocol-contracts/src/gas-token/index.test.ts
+++ yarn-project/protocol-contracts/src/gas-token/index.test.ts
@@ -1,13 +1,8 @@
-import omit from 'lodash.omit';
-
 import { GasTokenAddress, getCanonicalGasToken } from './index.js';
 
 describe('GasToken', () => {
   it('returns canonical protocol contract', () => {
     const contract = getCanonicalGasToken();
-    contract.contractClass.privateFunctions.sort((a, b) => a.selector.value - b.selector.value);
-    contract.contractClass.publicFunctions.sort((a, b) => a.selector.value - b.selector.value);
-    expect(omit(contract, 'artifact')).toMatchSnapshot();
     expect(contract.address.toString()).toEqual(GasTokenAddress.toString());
   });
 });
--- yarn-project/protocol-contracts/src/gas-token/index.ts
+++ yarn-project/protocol-contracts/src/gas-token/index.ts
@@ -1,5 +1,3 @@
-import { AztecAddress } from '@aztec/circuits.js';
-
 import { ProtocolContract, getCanonicalProtocolContract } from '../protocol_contract.js';
 import { GasTokenArtifact } from './artifact.js';
 
@@ -8,6 +6,4 @@ export function getCanonicalGasToken(): ProtocolContract {
   return getCanonicalProtocolContract(GasTokenArtifact, 1);
 }
 
-export const GasTokenAddress = AztecAddress.fromString(
-  '0x02704522409ff3c5bc18844e8152bb9f55c06ffde58fbba48ab985e7abf866d8',
-);
+export const GasTokenAddress = getCanonicalGasToken().address;
