33899c52edff43a0479589f949c97b1b3848a46c
ludamad <adam@aztecprotocol.com>
2024-02-21 15:23:24 +0000

    bench wasm

---
+++ barretenberg/cpp/scripts/ci/bench_wasm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+[ -n "${BUILD_SYSTEM_DEBUG:-}" ] && set -x # conditionally trace
+set -eu
+
+# enter script folder
+cd "$(dirname $0)"
+
+cd ../../srs_db
+./download_ignition.sh 1
+./download_grumpkin.sh
+cd ../build
+
+function bench() {
+  wasmtime run -Wthreads=y -Sthreads=y ./bin/$1 --benchmark_format=json --benchmark_counters_tabular=true $2 > $1.json
+  cat $1.json
+}
+
+bench ultra_honk_bench --benchmark_filter=construct_proof_ultrahonk_power_of_2/20
+bench goblin_ultra_honk_bench --benchmark_filter=construct_proof_goblinultrahonk_power_of_2/20
+bench ivc_bench --benchmark_filter=IvcBench/Full/2
