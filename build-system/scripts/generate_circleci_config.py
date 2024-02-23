5a8648f408a3777c563d8c878761e7fa231d2cdb
ludamad <domuradical@gmail.com>
2024-02-23 11:38:58 +0000

    basic test

--- build-system/scripts/generate_circleci_config.py
+++ build-system/scripts/generate_circleci_config.py
@@ -43,14 +43,19 @@ def _get_already_built_manifest_job_names(manifest_name):
 def get_already_built_manifest_job_names():
     manifest_names = get_manifest_job_names()
 
-    with ProcessPoolExecutor() as executor:
-        futures = [executor.submit(_get_already_built_manifest_job_names, key) for key in manifest_names]
-        for future in as_completed(futures):
-            eprint('fut', future)
-            result = future.result()
-            eprint('res', result)
-            if result is not None:
-                yield result
+    for key in manifest_names:
+        res = _get_already_built_manifest_job_names(key)
+        if res is not None:
+            yield res
+
+    # with ProcessPoolExecutor() as executor:
+    #     futures = [executor.submit(_get_already_built_manifest_job_names, key) for key in manifest_names]
+    #     for future in as_completed(futures):
+    #         eprint('fut', future)
+    #         result = future.result()
+    #         eprint('res', result)
+    #         if result is not None:
+    #             yield result
 
 def remove_jobs_from_workflow(jobs, to_remove):
     """
