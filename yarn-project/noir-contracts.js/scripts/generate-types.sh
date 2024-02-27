37134e6c35ef26659c0ed35c0221ccf3558e3ae2
spypsy <spypsy@outlook.com>
2024-02-27 12:17:36 +0000

    parallelize noir-contracts.js codegen

--- yarn-project/noir-contracts.js/scripts/generate-types.sh
+++ yarn-project/noir-contracts.js/scripts/generate-types.sh
@@ -4,37 +4,32 @@ set -euo pipefail
 OUT_DIR="./src"
 INDEX="$OUT_DIR/index.ts"
 
-rm -rf $OUT_DIR && mkdir -p $OUT_DIR
+rm -rf "$OUT_DIR" && mkdir -p "$OUT_DIR"
 
-#
+# Check for .json files existence
 if ! ls ../../noir-projects/noir-contracts/target/*.json >/dev/null 2>&1; then
   echo "Error: No .json files found in noir-contracts/target folder."
   echo "Make sure noir-contracts is built before running this script."
   exit 1
 fi
 
-# Generate index.ts header.
-echo "// Auto generated module - do not edit!" >$INDEX
+# Generate index.ts header
+echo "// Auto generated module - do not edit!" >"$INDEX"
 
 # Ensure the artifacts directory exists
 mkdir -p artifacts
 
-for ABI in $(find ../../noir-projects/noir-contracts/target -maxdepth 1 -type f ! -name 'debug_*' -name '*.json'); do
-  # Extract the filename from the path
-  filename=$(basename "$ABI")
-
-  # Copy the JSON file to the artifacts folder
-  cp "$ABI" "artifacts/$filename"
-
-  # Generate the contract name for referencing in the codegen command and index
+# Parallel codegen
+export OUT_DIR
+export INDEX
+find ../../noir-projects/noir-contracts/target -maxdepth 1 -type f ! -name 'debug_*' -name '*.json' | parallel '
+  filename=$(basename {})
+  cp {} "artifacts/$filename"
   CONTRACT=$(jq -r .name "artifacts/$filename")
-
   echo "Creating types for $CONTRACT using artifacts/$filename..."
   node --no-warnings ../noir-compiler/dest/cli.js codegen -o $OUT_DIR --ts "artifacts/$filename"
-
-  # Add contract import/export to index.ts.
-  echo "export * from './${CONTRACT}.js';" >>$INDEX
-done
+  echo "export * from '\''./${CONTRACT}.js'\'';" >> $INDEX
+'
 
 echo "Formatting..."
 yarn formatting:fix
