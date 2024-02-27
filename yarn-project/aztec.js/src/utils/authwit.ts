d031e5bd525e5203a25b6b0920b9eb2b289f9e63
LHerskind <lasse.herskind@gmail.com>
2024-02-27 15:48:28 +0000

    feat: initial authwit cancellation support

--- yarn-project/aztec.js/src/utils/authwit.ts
+++ yarn-project/aztec.js/src/utils/authwit.ts
@@ -1,24 +1,50 @@
 import { FunctionCall, PackedArguments } from '@aztec/circuit-types';
-import { AztecAddress, GeneratorIndex } from '@aztec/circuits.js';
+import { AztecAddress, Fr, GeneratorIndex } from '@aztec/circuits.js';
 import { pedersenHash } from '@aztec/foundation/crypto';
 
 // docs:start:authwit_computeAuthWitMessageHash
 /**
  * Compute an authentication witness message hash from a caller and a request
- * H(caller: AztecAddress, target: AztecAddress, selector: Field, args_hash: Field)
+ * H(target: AztecAddress, H(caller: AztecAddress, selector: Field, args_hash: Field))
  * @param caller - The caller approved to make the call
  * @param request - The request to be made (function call)
  * @returns The message hash for the witness
  */
 export const computeAuthWitMessageHash = (caller: AztecAddress, request: FunctionCall) => {
-  return pedersenHash(
-    [
+  return computeOuterAuthWitHash(
+    request.to.toField(),
+    computeInnerAuthWitHash([
       caller.toField(),
-      request.to.toField(),
       request.functionData.selector.toField(),
       PackedArguments.fromArgs(request.args).hash,
-    ].map(fr => fr.toBuffer()),
-    GeneratorIndex.AUTHWIT,
+    ]),
   );
 };
 // docs:end:authwit_computeAuthWitMessageHash
+
+/**
+ * Compute the inner hash for an authentication witness.
+ * This is the value provided to the contract function as input.
+ * @param args - The arguments to hash
+ * @returns The inner hash for the witness
+ */
+export const computeInnerAuthWitHash = (args: Fr[]) => {
+  return pedersenHash(
+    args.map(fr => fr.toBuffer()),
+    GeneratorIndex.AUTHWIT_INNER,
+  );
+};
+
+/**
+ * Compute the outer hash for an authentication witness.
+ * This is the value to sign over.
+ * @param to - The target of the witness (the consumer)
+ * @param innerHash - The inner hash for the witness
+ * @returns The outer hash for the witness
+ */
+export const computeOuterAuthWitHash = (to: AztecAddress, innerHash: Fr) => {
+  return pedersenHash(
+    [to.toField(), innerHash].map(fr => fr.toBuffer()),
+    GeneratorIndex.AUTHWIT_OUTER,
+  );
+};
