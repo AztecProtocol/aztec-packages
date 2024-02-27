ttaramandi/aztec-kiw

feat(avm-simulator): add NULLIFIEREXISTS opcode to avm simulator, transpiler, noir test, TS tests

avm-transpiler/src/transpile.rs
@@ -239,6 +239,7 @@ fn handle_foreign_call(
 ) {
     match function.as_str() {
         "emitNoteHash" => emit_emit_note_hash(avm_instrs, destinations, inputs),
+        "nullifierExists" => emit_nullifier_exists(avm_instrs, destinations, inputs),
         "emitNullifier" => emit_emit_nullifier(avm_instrs, destinations, inputs),
         "keccak256" | "sha256" => {
             emit_2_field_hash_instruction(avm_instrs, function, destinations, inputs)
@@ -271,6 +272,40 @@ fn emit_emit_note_hash(
         operands: vec![AvmOperand::U32 {
             value: note_hash_offset_operand,
         }],
+        ..Default::default()
+    });
+}
+
+/// Emit an AVM NULLIFIEREXISTS instruction
+/// (a nullifierExists brillig foreign call was encountered)
+/// Adds the new instruction to the avm instructions list.
+fn emit_nullifier_exists(
+    avm_instrs: &mut Vec<AvmInstruction>,
+    destinations: &Vec<ValueOrArray>,
+    inputs: &Vec<ValueOrArray>,
+) {
+    if destinations.len() != 1 || inputs.len() != 1 {
+        panic!("Transpiler expects ForeignCall::CHECKNULLIFIEREXISTS to have 1 destinations and 1 input, got {} and {}", destinations.len(), inputs.len());
+    }
+    let nullifier_offset_operand = match &inputs[0] {
+        ValueOrArray::MemoryAddress(offset) => offset.to_usize() as u32,
+        _ => panic!("Transpiler does not know how to handle ForeignCall::EMITNOTEHASH with HeapArray/Vector inputs"),
+    };
+    let exists_offset_operand = match &destinations[0] {
+        ValueOrArray::MemoryAddress(offset) => offset.to_usize() as u32,
+        _ => panic!("Transpiler does not know how to handle ForeignCall::EMITNOTEHASH with HeapArray/Vector inputs"),
+    };
+    avm_instrs.push(AvmInstruction {
+        opcode: AvmOpcode::EMITNULLIFIER,
+        indirect: Some(ALL_DIRECT),
+        operands: vec![
+            AvmOperand::U32 {
+                value: nullifier_offset_operand,
+            },
+            AvmOperand::U32 {
+                value: exists_offset_operand,
+            },
+        ],
         ..Default::default()
     });
 }

AztecProtocol/aztec-packages

avm-transpiler/src/transpile.rs
@@ -244,6 +244,7 @@ fn handle_foreign_call(
             destinations,
             inputs,
         ),
+        "nullifierExists" => handle_nullifier_exists(avm_instrs, destinations, inputs),
         "keccak256" | "sha256" => {
             handle_2_field_hash_instruction(avm_instrs, function, destinations, inputs)
         }
@@ -294,6 +295,40 @@ fn handle_emit_note_hash_or_nullifier(
         operands: vec![AvmOperand::U32 {
             value: offset_operand,
         }],
+        ..Default::default()
+    });
+}
+
+/// Handle an AVM NULLIFIEREXISTS instruction
+/// (a nullifierExists brillig foreign call was encountered)
+/// Adds the new instruction to the avm instructions list.
+fn handle_nullifier_exists(
+    avm_instrs: &mut Vec<AvmInstruction>,
+    destinations: &Vec<ValueOrArray>,
+    inputs: &Vec<ValueOrArray>,
+) {
+    if destinations.len() != 1 || inputs.len() != 1 {
+        panic!("Transpiler expects ForeignCall::CHECKNULLIFIEREXISTS to have 1 destinations and 1 input, got {} and {}", destinations.len(), inputs.len());
+    }
+    let nullifier_offset_operand = match &inputs[0] {
+        ValueOrArray::MemoryAddress(offset) => offset.to_usize() as u32,
+        _ => panic!("Transpiler does not know how to handle ForeignCall::EMITNOTEHASH with HeapArray/Vector inputs"),
+    };
+    let exists_offset_operand = match &destinations[0] {
+        ValueOrArray::MemoryAddress(offset) => offset.to_usize() as u32,
+        _ => panic!("Transpiler does not know how to handle ForeignCall::EMITNOTEHASH with HeapArray/Vector inputs"),
+    };
+    avm_instrs.push(AvmInstruction {
+        opcode: AvmOpcode::NULLIFIEREXISTS,
+        indirect: Some(ALL_DIRECT),
+        operands: vec![
+            AvmOperand::U32 {
+                value: nullifier_offset_operand,
+            },
+            AvmOperand::U32 {
+                value: exists_offset_operand,
+            },
+        ],
         ..Default::default()
     });
 }