c127417e5243a35b9bdb1c276997397201b0c0aa
Charlie Lye <karl.lye@gmail.com>
2024-02-28 18:13:27 +0000

    fix

--- noir/scripts/test_js_packages.sh
+++ noir/scripts/test_js_packages.sh
@@ -8,7 +8,7 @@ cd $(dirname "$0")/../noir-repo
 # Set build data manually.
 export SOURCE_DATE_EPOCH=$(date +%s)
 export GIT_DIRTY=false
-export GIT_COMMIT=$(git rev-parse --verify HEAD)
+export GIT_COMMIT=${COMMIT_HASH:-$(git rev-parse --verify HEAD)}
 
 cargo build --release
 export PATH="${PATH}:/usr/src/noir/noir-repo/target/release/"
