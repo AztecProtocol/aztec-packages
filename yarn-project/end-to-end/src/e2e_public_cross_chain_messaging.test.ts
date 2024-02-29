092081a02b17c8faf2f730ef8e5ad7ed9947ac29
benesjan <janbenes1234@gmail.com>
2024-02-29 09:28:40 +0000

    diff cleanup

--- yarn-project/end-to-end/src/e2e_public_cross_chain_messaging.test.ts
+++ yarn-project/end-to-end/src/e2e_public_cross_chain_messaging.test.ts
@@ -47,7 +47,7 @@ describe('e2e_public_cross_chain_messaging', () => {
     user1Wallet = wallets[0];
     user2Wallet = wallets[1];
     await publicDeployAccounts(wallets[0], accounts.slice(0, 2));
-  }, 60_000);
+  });
 
   beforeEach(async () => {
     crossChainTestHarness = await CrossChainTestHarness.new(
