07987cf20590c5ccc53680d5d98c35b6e987a5b6
Sarkoxed <sarkoxed2013@yandex.ru>
2024-03-01 12:32:38 +0000

    Removed xor test from ffiterm leaving only arithmetic + fr include

--- barretenberg/cpp/src/barretenberg/smt_verification/terms/ffiterm.test.cpp
+++ barretenberg/cpp/src/barretenberg/smt_verification/terms/ffiterm.test.cpp
@@ -98,25 +98,4 @@ TEST(integermod, division)
     ASSERT_EQ(bvals, yvals);
 }
 
-TEST(integermod, xor)
-{
-    bb::fr a = bb::fr::random_element();
-    bb::fr b = bb::fr::random_element();
-    bb::fr c = a ^ b;
-    Solver s("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001", { true, 0 }, 16);
-
-    FFITerm x = FFITerm::Var("x", &s);
-    FFITerm y = FFITerm::Var("y", &s);
-    FFITerm bval = FFITerm(b, &s);
-    FFITerm z = x ^ y;
-    
-    z == c;
-    x == a;
-    ASSERT_TRUE(s.check());
-
-    std::string yvals = s.s.getValue(y.term).getIntegerValue();
-    std::string bvals = s.s.getValue(bval.term).getIntegerValue();
-    ASSERT_EQ(bvals, yvals);
-}
-
-// TODO(alex): range_constraints
+// TODO(alex): range_constraints, xor
