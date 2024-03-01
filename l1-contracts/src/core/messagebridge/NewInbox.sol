418e1c44cc645d3171995a046ec748ebf5a419e4
benesjan <janbenes1234@gmail.com>
2024-03-01 13:12:11 +0000

    WIP

--- l1-contracts/src/core/messagebridge/NewInbox.sol
+++ l1-contracts/src/core/messagebridge/NewInbox.sol
@@ -22,9 +22,76 @@ import {MessageBox} from "../libraries/MessageBox.sol";
 contract NewInbox {
   IRegistry public immutable REGISTRY;
 
+  uint256 public immutable HEIGHT;
+  uint256 public immutable SIZE;
+  bytes32 private immutable ZERO;
+
+  uint256 private toInclude = 0;
+  uint256 private inProgress = 1;
+
+  mapping(uint256 treeNumber => address tree) public frontier;
+
   event LeafInserted(uint256 indexed treeNumber, uint256 indexed index, bytes32 value);
 
-  constructor(address _registry) {
+  constructor(address _registry, uint256 _height, bytes32 _zero) {
     REGISTRY = IRegistry(_registry);
+
+    HEIGHT = _height;
+    SIZE = 2**_height;
+    ZERO = _zero;
   }
 }
+
+// class Inbox:
+//   STATE_TRANSITIONER: immutable(address)
+//   ZERO: immutable(bytes32)
+
+//   HEIGHT: immutable(uint256)
+//   SIZE: immutable(uint256)
+
+//   trees: HashMap[uint256, FrontierTree]
+
+//   to_include: uint256 = 0
+//   in_progress: uint256 = 1
+
+//   def __init__(self, _height: uint256, _zero: bytes32, _state_transitioner: address):
+//     self.HEIGHT = _height
+//     self.SIZE = 2**_height
+//     self.ZERO = _zero
+//     self.STATE_TRANSITIONER = _state_transitioner
+
+//     self.trees[1] = FrontierTree(self.HEIGHT)
+ 
+//   def insert(self, message: L1ToL2Message) -> bytes32:
+//     '''
+//     Insert into the next FrontierTree. If the tree is full, creates a new one
+//     '''
+//     if self.trees[self.in_progress].next_index == 2**self.HEIGHT:
+//       self.in_progress += 1
+//       self.trees[self.in_progress] = FrontierTree(self.HEIGHT)
+
+//     message.sender.actor = msg.sender
+//     message.sender.chain_id = block.chainid
+
+//     leaf = message.hash_to_field()
+//     self.trees[self.in_progress].insert(leaf)
+//     return leaf
+
+//   def consume(self) -> bytes32:
+//     '''
+//     Consumes the current tree, and starts a new one if needed
+//     '''
+//     assert msg.sender == self.STATE_TRANSITIONER
+
+//     root = self.ZERO
+//     if self.to_include > 0:
+//       root = self.trees[self.to_include].root()
+
+//     # If we are "catching up" we can skip the creation as it is already there
+//     if self.to_include + 1 == self.in_progress:
+//       self.in_progress += 1
+//       self.trees[self.in_progress] = FrontierTree(self.HEIGHT)
+
+//     self.to_include += 1
+
+//     return root
