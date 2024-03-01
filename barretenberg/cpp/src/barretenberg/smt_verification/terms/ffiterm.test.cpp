5b3a4dc83807621c54964e47325a2df32a552010
Sarkoxed <sarkoxed2013@yandex.ru>
2024-03-01 12:00:48 +0000

    Added tests for symbolic variables classes

---
+++ barretenberg/cpp/src/barretenberg/smt_verification/terms/ffiterm.test.cpp
@@ -0,0 +1,122 @@
+#include <unordered_map>
+
+#include "ffiterm.hpp"
+
+#include <gtest/gtest.h>
+
+namespace {
+auto& engine = bb::numeric::get_debug_randomness();
+}
+
+using namespace smt_terms;
+
+TEST(integermod, addition)
+{
+    bb::fr a = bb::fr::random_element();
+    bb::fr b = bb::fr::random_element();
+    bb::fr c = a + b;
+    Solver s("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001", { true, 0 }, 16);
+
+    FFITerm x = FFITerm::Var("x", &s);
+    FFITerm y = FFITerm::Var("y", &s);
+    FFITerm bval = FFITerm(b, &s);
+    FFITerm z = x + y;
+    z.mod();
+    
+    z == c;
+    x == a;
+    ASSERT_TRUE(s.check());
+
+    std::string yvals = s.s.getValue(y.term).getIntegerValue();
+    std::string bvals = s.s.getValue(bval.term).getIntegerValue();
+    ASSERT_EQ(bvals, yvals);
+}
+
+TEST(integermod, subtraction)
+{
+    bb::fr a = bb::fr::random_element();
+    bb::fr b = bb::fr::random_element();
+    bb::fr c = a - b;
+    Solver s("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001", { true, 0 }, 16);
+
+    FFITerm x = FFITerm::Var("x", &s);
+    FFITerm y = FFITerm::Var("y", &s);
+    FFITerm bval = FFITerm(b, &s);
+    FFITerm z = x - y;
+    z.mod();
+    
+    z == c;
+    x == a;
+    ASSERT_TRUE(s.check());
+
+    std::string yvals = s.s.getValue(y.term).getIntegerValue();
+    std::string bvals = s.s.getValue(bval.term).getIntegerValue();
+    ASSERT_EQ(bvals, yvals);
+}
+
+TEST(integermod, multiplication)
+{
+    bb::fr a = bb::fr::random_element();
+    bb::fr b = bb::fr::random_element();
+    bb::fr c = a * b;
+    Solver s("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001", { true, 0 }, 16);
+
+    FFITerm x = FFITerm::Var("x", &s);
+    FFITerm y = FFITerm::Var("y", &s);
+    FFITerm bval = FFITerm(b, &s);
+    FFITerm z = x * y;
+    z.mod();
+    
+    z == c;
+    x == a;
+    ASSERT_TRUE(s.check());
+
+    std::string yvals = s.s.getValue(y.term).getIntegerValue();
+    std::string bvals = s.s.getValue(bval.term).getIntegerValue();
+    ASSERT_EQ(bvals, yvals);
+}
+
+TEST(integermod, division)
+{
+    bb::fr a = bb::fr::random_element();
+    bb::fr b = bb::fr::random_element();
+    bb::fr c = a / b;
+    Solver s("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001", { true, 0 }, 16);
+
+    FFITerm x = FFITerm::Var("x", &s);
+    FFITerm y = FFITerm::Var("y", &s);
+    FFITerm bval = FFITerm(b, &s);
+    FFITerm z = x / y;
+    //z.mod(); TODO(alex): something bad
+    
+    z == c;
+    x == a;
+    ASSERT_TRUE(s.check());
+
+    std::string yvals = s.s.getValue(y.term).getIntegerValue();
+    std::string bvals = s.s.getValue(bval.term).getIntegerValue();
+    ASSERT_EQ(bvals, yvals);
+}
+
+TEST(integermod, xor)
+{
+    bb::fr a = bb::fr::random_element();
+    bb::fr b = bb::fr::random_element();
+    bb::fr c = a ^ b;
+    Solver s("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001", { true, 0 }, 16);
+
+    FFITerm x = FFITerm::Var("x", &s);
+    FFITerm y = FFITerm::Var("y", &s);
+    FFITerm bval = FFITerm(b, &s);
+    FFITerm z = x ^ y;
+    
+    z == c;
+    x == a;
+    ASSERT_TRUE(s.check());
+
+    std::string yvals = s.s.getValue(y.term).getIntegerValue();
+    std::string bvals = s.s.getValue(bval.term).getIntegerValue();
+    ASSERT_EQ(bvals, yvals);
+}
+
+// TODO(alex): range_constraints
