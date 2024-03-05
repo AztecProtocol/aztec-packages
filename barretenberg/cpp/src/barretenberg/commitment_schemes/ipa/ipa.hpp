e45d844b2e92685586acf9cb6881514e09d5c337
Rumata888 <isennovskiy@gmail.com>
2024-03-05 21:15:07 +0000

    Docs

--- barretenberg/cpp/src/barretenberg/commitment_schemes/ipa/ipa.hpp
+++ barretenberg/cpp/src/barretenberg/commitment_schemes/ipa/ipa.hpp
@@ -12,46 +12,59 @@
 namespace bb {
 
 /**
- * @brief IPA (inner-product argument) commitment scheme class. Conforms to the specification
- * https://hackmd.io/q-A8y6aITWyWJrvsGGMWNA?view. Given:
+ * @brief IPA (inner product argument) commitment scheme class.
+ *
+ *@details This implementation of IPA uses the optimized version that only multiplies half of the elements of each
+ *vector in each prover round. The implementation uses:
  *
  *1. An SRS (Structured Reference String) \f$\vec{G}=(G_0,G_1...,G_{d-1})\f$ with \f$G_i ∈ E(\mathbb{F}_p)\f$ and
- *\f$\mathbb{F}_r\f$ - the scalar field of the elliptic curve;
- *2. A polynomial \f$p(x)=\sum_{i=0}^{d-1}a_ix^i\f$ over field \f$F_r\f$, where the polynomial degree \f$d-1\f$ is such
+ *\f$\mathbb{F}_r\f$ - the scalar field of the elliptic curve as well as \f$G\f$ which is an independent generator on
+ *the same curve.
+ *2. A polynomial \f$f(x)=\sum_{i=0}^{d-1}f_ix^i\f$ over field \f$F_r\f$, where the polynomial degree \f$d-1\f$ is such
  *that \f$d=2^k\f$
  *
- *The prover commits to the polynomial \f$p(x)\f$ by computing the scalar product
- *\f$[p(x)]=\langle\vec{p},\vec{G}\rangle\f$, where \f$\vec{p}=(a_0, a_1,..., a_{d-1})\f$​
+ *The opening and verification procedures expect that there already exists a commitment to \f$f(x)\f$ which is the
+ *scalar product \f$[f(x)]=\langle\vec{f},\vec{G}\rangle\f$, where \f$\vec{f}=(f_0, f_1,..., f_{d-1})\f$​
  *
- *For a vector \f$\vec{v}=(v_0,v_1,...,v_{2n-1})\f$ of length \f$2n\f$ we'll denote
- *\f$\vec{v}_{low}=(v_0,v_1,...,v_{n-1})\f$ and \f$\vec{v}_{high}=(v_{n},v_{n+1},...v_{2n-1})\f$
+ * The opening procedure documentation can be found in the description of \link IPA::compute_opening_proof
+ compute_opening_proof \endlink. The verification procedure documentation is in \link IPA::verify verify \endlink
  *
- **compute_opening_proof* procedure using 4 arguments:
+ * @tparam Curve
  *
- *1. A commitment key *ck* containing the srs element
- *2. An opening pair \f$(\beta, p(\beta))\f$
- *3. The polynomial \f$p(x)\f$ in monomial form
- *4. The transcript for sending values to the verifier and receiving challenges
+ * @remark IPA is not a very intuitive algorithm, so here are a few things that might help internalize it:
  *
- *The procedure runs as follows:
+ *1. Originally we have two vectors \f$\vec{a}\f$ and \f$\vec{b}\f$, which the product of which we want to prove, but
+ *the prover can't just send vector \f$\vec{a}\f$ to the verifier, it can only provide a commitment
+ \f$langle\vec{a},\vec{G}\rangle\f$ *2. The verifier computes the \f$C'=C+f(\beta)\cdot U\f$ to "bind" together the
+ commitment and the evaluation *3. The prover wants to reduce the problem of verifying the inner product of
+ \f$\vec{a}\f$, \f$\vec{b}\f$ of length
+ *\f$n\f$ to a problem of verifying the IPA of 2 vectors \f$\vec{a}_{new}\f$, \f$\vec{b}_{new}\f$ of size
+ *\f$\frac{n}{2}\f$​
+ *4. If \f$\vec{a}_{new}=\vec{a}_{low}+\alpha\cdot \vec{a}_{high}\f$ and \f$\vec{b}_{new}=\vec{b}_{low}+\alpha^{-1}\cdot
+ \vec{b}_{high}\f$, then \f$\langle \vec{a}_{new},\vec{b}_{new}\rangle = \langle\vec{a}_{low},\vec{b}_{low}\rangle +
+ \alpha^{-1}\langle\vec{a}_{low},\vec{b}_{high}\rangle+\alpha \langle \vec{a}_{high},\vec{b}_{low}\rangle +
+ \langle\vec{a}_{high},\vec{b}_{high}\rangle=
+ \langle\vec{a},\vec{b}\rangle+\alpha^{-1}\langle\vec{a}_{low},\vec{b}_{high}\rangle+\alpha \langle
+ \vec{a}_{high},\vec{b}_{low}\rangle\f$, so if we provide commitments to
+ \f$\langle\vec{a}_{low},\vec{b}_{high}\rangle\f$ and \f$\langle \vec{a}_{high},\vec{b}_{low}\rangle\f$  the verifier
+ can reduce initial commitment to the result \f$\langle \vec{a},\vec{b}\rangle U\f$ to the new commitment \f$\langle
+ \vec{a}_{new},\vec{b}_{new}\rangle U\f$
+ *5. Analogously, if \f$\vec{G}_{new}=\vec{G}_{low}+\alpha^{-1}\vec{G}_{high}\f$, then we can reduce the initial
+ *commitment to \f$\vec{a}\f$ by providing  \f$\langle\vec{a}_{low},\vec{G}_{high}\rangle\f$ and \f$\langle
+ \vec{a}_{high},\vec{G}_{low}\rangle\f$. \f$\langle \vec{a}_{new},\vec{G}_{new}\rangle =
+ \langle\vec{a},\vec{G}\rangle+\alpha^{-1}\langle\vec{a}_{low},\vec{G}_{high}\rangle+\alpha \langle
+ \vec{a}_{high},\vec{G}_{low}\rangle\f$
+ *6. We can batch the two reductions together \f$\langle \vec{a}_{new},\vec{b}_{new}\rangle U + \langle
+ \vec{a}_{new},\vec{G}_{new}\rangle= \langle\vec{a},\vec{b}\rangle U+ \langle\vec{a},\vec{G}\rangle+
+ \alpha^{-1}(\langle\vec{a}_{low},\vec{b}_{high}\rangle U +\langle\vec{a}_{low},\vec{G}_{high}\rangle) +\alpha (\langle
+ \vec{a}_{high},\vec{b}_{low}\rangle U+\langle \vec{a}_{high},\vec{G}_{low}\rangle)\f$​
+ *7. After \f$k\f$ steps of reductions we are left with \f$\langle \vec{a}_{0},\vec{b}_{0}\rangle U + \langle
+ \vec{a}_{0},\vec{G}_{s}\rangle= a_0 b_0 U+a_0G_s\f$. The prover provides \f$a_0\f$. The independence of \f$U\f$ and
+ \f$\vec{G}\f$ from which we construct \f$G_s\f$ ensures that \f$a_0\f$ is constructed from \f$\vec{a}_k=\vec{p}\f$
+ *correctly, while the correctness of \f$a_0 b_0 U\f$ ensures that the polynomial opening is indeed \f$f(\beta)\f$
  *
- *1. Send the degree of \f$p(x)\f$ plus one, equal to \f$d\f$ to the verifier
- *2. Receive the generator challenge \f$u\f$ from the verifier
- *3. Compute the auxiliary generator \f$U=u\cdot G\f$, where \f$G\f$ is a generator of \f$E(\mathbb{F}_p)\f$​
- *4. Set \f$\vec{G}_0=\vec{G}\f$, \f$\vec{a}_0=\vec{p}\f$
- *5. Compute the vector \f$\vec{b}_0=(1,\beta,\beta^2,...,\beta^{d-1})\f$
- *6. Perform \f$k\f$ rounds (for \f$i \in \{0,...,k-1\}\f$) of:
- *   1. Compute
- *\f$L_i=\langle\vec{a}_{i\_low},\vec{G}_{i\_high}\rangle+\langle\vec{a}_{i\_low},\vec{b}_{i\_high}\rangle\cdot U\f$​
- *   2. Compute
- *\f$R_i=\langle\vec{a}_{i\_high},\vec{G}_{i\_low}\rangle+\langle\vec{a}_{i\_high},\vec{b}_{i\_low}\rangle\cdot U\f$
- *   3. Send \f$L_i\f$ and \f$R_i\f$ to the verifier
- *   4. Receive round challenge \f$r_i\f$ from the verifier​
- *   5. Compute \f$\vec{a}_{i+1}=\vec{a}_{i\_low}+r_i\cdot \vec{a}_{i\_high}\f$
- *   6. Compute \f$\vec{b}_{i+1}=\vec{b}_{i\_low}+r_i^{-1}\cdot \vec{b}_{i\_high}\f$​
- *   7. Compute \f$\vec{G}_{i+1}=\vec{G}_{i\_low}+r_i^{-1}\cdot \vec{G}_{i\_high}\f$
- *
- *7. Send the final \f$\vec{a}_{k-1}\f$ of length 1 to the verifier
+ * The old version of documentation is available at <a href="https://hackmd.io/q-A8y6aITWyWJrvsGGMWNA?view">Old IPA
+ documentation </a>
  */
 template <typename Curve> class IPA {
     using Fr = typename Curve::ScalarField;
@@ -70,6 +83,30 @@ template <typename Curve> class IPA {
      * @param polynomial The witness polynomial whose opening proof needs to be computed
      * @param transcript Prover transcript
      * https://github.com/AztecProtocol/aztec-packages/pull/3434
+     *
+     *@details For a vector \f$\vec{v}=(v_0,v_1,...,v_{2n-1})\f$ of length \f$2n\f$ we'll denote
+     *\f$\vec{v}_{low}=(v_0,v_1,...,v_{n-1})\f$ and \f$\vec{v}_{high}=(v_{n},v_{n+1},...v_{2n-1})\f$. The procedure runs
+     *as follows:
+     *
+     *1. Send the degree of \f$f(x)\f$ plus one, equal to \f$d\f$ to the verifier
+     *2. Receive the generator challenge \f$u\f$ from the verifier
+     *3. Compute the auxiliary generator \f$U=u\cdot G\f$, where \f$G\f$ is a generator of \f$E(\mathbb{F}_p)\f$​
+     *4. Set \f$\vec{G}_{k}=\vec{G}\f$, \f$\vec{a}_{k}=\vec{p}\f$
+     *5. Compute the vector \f$\vec{b}_{k}=(1,\beta,\beta^2,...,\beta^{d-1})\f$
+     *6. Perform \f$k\f$ rounds (for \f$i \in \{k,...,1\}\f$) of:
+     *   1. Compute
+     *\f$L_{i-1}=\langle\vec{a}_{i\_low},\vec{G}_{i\_high}\rangle+\langle\vec{a}_{i\_low},\vec{b}_{i\_high}\rangle\cdot
+     *U\f$​
+     *   2. Compute
+     *\f$R_{i-1}=\langle\vec{a}_{i\_high},\vec{G}_{i\_low}\rangle+\langle\vec{a}_{i\_high},\vec{b}_{i\_low}\rangle\cdot
+     *U\f$
+     *   3. Send \f$L_{i-1}\f$ and \f$R_{i-1}\f$ to the verifier
+     *   4. Receive round challenge \f$u_{i-1}\f$ from the verifier​
+     *   5. Compute \f$\vec{a}_{i-1}=\vec{a}_{i\_low}+u_{i-1}\cdot \vec{a}_{i\_high}\f$
+     *   6. Compute \f$\vec{b}_{i-1}=\vec{b}_{i\_low}+u_{i-1}^{-1}\cdot \vec{b}_{i\_high}\f$​
+     *   7. Compute \f$\vec{G}_{i-1}=\vec{G}_{i\_low}+u_{i-1}^{-1}\cdot \vec{G}_{i\_high}\f$
+     *
+     *7. Send the final \f$\vec{a}_{0} = (a_0)\f$ to the verifier
      */
     static void compute_opening_proof(const std::shared_ptr<CK>& ck,
                                       const OpeningPair<Curve>& opening_pair,
@@ -216,6 +253,22 @@ template <typename Curve> class IPA {
      * @param pub_input Data required to verify the proof
      *
      * @return true/false depending on if the proof verifies
+     *
+     * @details The procedure runs as follows:
+     *
+     *1. Receive \f$d\f$ (polynomial degree plus one) from the prover
+     *2. Receive the generator challenge \f$u\f$ and computes \f$U=u\cdot G\f$
+     *3. Compute  \f$C'=C+f(\beta)\cdot U\f$
+     *4. Receive \f$L_j, R_j\f$ and compute challenges \f$u_j\f$ for \f$j \in {k-1,..,0}\f$
+     *5. Compute \f$C_0 = C' + \sum_{j=0}^{k-1}(u_j^{-1}L_j + u_jR_j)\f$
+     *6. Compute \f$b_0=g(\beta)=\prod_{i=0}^{k-1}(1+u_{i}^{-1}x^{2^{i}})\f$
+     *7. Compute vector \f$\vec{s}=(1,u_{0}^{-1},u_{1}^{-1},u_{0}^{-1}u_{1}^{-1},...,\prod_{i=0}^{k-1}u_{i}^{-1})\f$
+     *8. Compute \f$G_s=\langle \vec{s},\vec{G}\rangle\f$
+     *9. Receive \f$\vec{a}_{k-1}\f$ of length 1
+     *10. Compute \f$C_{right}=a_{0}G_{s}+a_{0}b_{0}U\f$
+     *11. Check that \f$C_{right} = C_0\f$. If they match, return true. Otherwise return false.
+     *
+     *
      */
     static bool verify(const std::shared_ptr<VK>& vk,
                        const OpeningClaim<Curve>& opening_claim,
@@ -256,13 +309,12 @@ template <typename Curve> class IPA {
             &msm_scalars[0], &msm_elements[0], pippenger_size, vk->pippenger_runtime_state);
         GroupElement C_zero = C_prime + LR_sums;
 
-        /**
-         * Compute b_zero where b_zero can be computed using the polynomial:
-         *
-         * g(X) = ∏_{i ∈ [k]} (1 + u_{k-i}^{-1}.X^{2^{i-1}}).
-         *
-         * b_zero = g(evaluation) = ∏_{i ∈ [k]} (1 + u_{k-i}^{-1}. (evaluation)^{2^{i-1}})
-         */
+        //  Compute b_zero where b_zero can be computed using the polynomial:
+
+        //  g(X) = ∏_{i ∈ [k]} (1 + u_{k-i}^{-1}.X^{2^{i-1}}).
+
+        //  b_zero = g(evaluation) = ∏_{i ∈ [k]} (1 + u_{k-i}^{-1}. (evaluation)^{2^{i-1}})
+
         Fr b_zero = Fr::one();
         for (size_t i = 0; i < log_poly_degree_plus_1; i++) {
             auto exponent = static_cast<uint64_t>(Fr(2).pow(i));
