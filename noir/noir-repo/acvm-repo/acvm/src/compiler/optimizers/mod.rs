40838d0905efc2ba55265ea86c8f9916b3a64346
Tom French <tom@tomfren.ch>
2024-03-05 15:33:59 +0000

    chore: fix bad merge

--- noir/noir-repo/acvm-repo/acvm/src/compiler/optimizers/mod.rs
+++ noir/noir-repo/acvm-repo/acvm/src/compiler/optimizers/mod.rs
@@ -70,3 +70,6 @@ pub(super) fn optimize_internal(acir: Circuit) -> (Circuit, Vec<usize>) {
     // ConstantBackpropagationOptimizer::backpropagate_constants(acir, acir_opcode_positions);
 
     info!("Number of opcodes after: {}", acir.opcodes.len());
+
+    (acir, acir_opcode_positions)
+}
