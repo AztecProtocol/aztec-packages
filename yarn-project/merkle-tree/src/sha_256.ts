cc0c9db6fbfe1b416208486423d5d1e653f0995e
Esau <esau@aztecprotocol.com>
2024-03-06 14:26:07 +0100

    fix

--- yarn-project/merkle-tree/src/sha_256.ts
+++ yarn-project/merkle-tree/src/sha_256.ts
@@ -2,13 +2,13 @@ import { sha256 } from '@aztec/foundation/crypto';
 import { Hasher } from '@aztec/types/interfaces';
 
 /**
- * A helper class encapsulating Pedersen hash functionality.
- * @deprecated Don't call pedersen directly in production code. Instead, create suitably-named functions for specific
+ * A helper class encapsulating SHA256 hash functionality.
+ * @deprecated Don't call SHA256 directly in production code. Instead, create suitably-named functions for specific
  * purposes.
  */
 export class SHA256 implements Hasher {
   /*
-   * @deprecated Don't call pedersen directly in production code. Instead, create suitably-named functions for specific
+   * @deprecated Don't call SHA256 directly in production code. Instead, create suitably-named functions for specific
    * purposes.
    */
   public hash(lhs: Uint8Array, rhs: Uint8Array): Buffer {
@@ -16,7 +16,7 @@ export class SHA256 implements Hasher {
   }
 
   /*
-   * @deprecated Don't call pedersen directly in production code. Instead, create suitably-named functions for specific
+   * @deprecated Don't call SHA256 directly in production code. Instead, create suitably-named functions for specific
    * purposes.
    */
   public hashInputs(inputs: Buffer[]): Buffer {
