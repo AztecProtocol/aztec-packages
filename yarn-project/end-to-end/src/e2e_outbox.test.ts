5cc60d8bfe36a60e9523f799cc8a23938cd48f17
Esau <esau@aztecprotocol.com>
2024-03-06 18:35:37 +0100

    fix test

--- yarn-project/end-to-end/src/e2e_outbox.test.ts
+++ yarn-project/end-to-end/src/e2e_outbox.test.ts
@@ -1,46 +1,58 @@
-import { AztecNode, Fr, SiblingPath } from '@aztec/aztec.js';
+import { AccountWalletWithPrivateKey, AztecNode, EthAddress, Fr, SiblingPath } from '@aztec/aztec.js';
 import { SHA256 } from '@aztec/merkle-tree';
 
 import { beforeEach, describe, expect, it } from '@jest/globals';
 
 import { setup } from './fixtures/utils.js';
+import { TestContract } from '@aztec/noir-contracts.js';
 
 describe('E2E Outbox Tests', () => {
   let teardown: () => void;
   let aztecNode: AztecNode;
   const sha256 = new SHA256();
+  let contract: TestContract;
+  let wallets: AccountWalletWithPrivateKey[];
+
 
   beforeEach(async () => {
-    ({ teardown, aztecNode } = await setup(1));
+    ({ teardown, aztecNode, wallets } = await setup(1));
+
+    const receipt = await TestContract.deploy(wallets[0]).send().wait();
+    contract = receipt.contract;
   }, 100_000);
 
   afterAll(() => teardown());
 
-  it('Checks the sibling path of the empty setup of two blocks of two empty tx effects each', async () => {
-    const blockNumber = await aztecNode.getBlockNumber();
+  it('Inserts a new out message, and verifies sibling paths of both the new message, and its zeroed sibling', async () => {
+    const { blockNumber } = await contract.methods.create_l2_to_l1_message_arbitrary_recipient_public(42069, EthAddress.ZERO).send().wait();
 
-    console.log(blockNumber);
-
-    const block = await aztecNode.getBlock(1);
+    const block = await aztecNode.getBlock(blockNumber!);
 
     const l2ToL1Messages = block?.body.txEffects.flatMap(txEffect =>
       txEffect.l2ToL1Msgs.map(l2ToL1Message => l2ToL1Message.toBuffer()),
     );
 
-    console.log(block?.body.txEffects);
-
-    expect(block?.body.txEffects.length).toBe(2);
-
-    expect(l2ToL1Messages?.length).toBe(4);
-
-    const siblingPath = await aztecNode.getL2ToL1MessageSiblingPath(blockNumber, Fr.ZERO);
+    const siblingPath = await aztecNode.getL2ToL1MessageSiblingPath(blockNumber!, Fr.ZERO);
 
     expect(siblingPath.pathSize).toBe(2);
 
     const expectedSiblingPath = new SiblingPath(siblingPath.pathSize, [
+      l2ToL1Messages![0],
+      sha256.hash(Fr.ZERO.toBuffer(), Fr.ZERO.toBuffer()),
+    ]);
+
+    expect(siblingPath.toString()).toBe(expectedSiblingPath.toString());
+
+    const siblingPathAlt = await aztecNode.getL2ToL1MessageSiblingPath(blockNumber!, Fr.fromBuffer(l2ToL1Messages![0]));
+
+    expect(siblingPathAlt.pathSize).toBe(2);
+
+    const expectedSiblingPathAlt = new SiblingPath(siblingPath.pathSize, [
       Fr.ZERO.toBuffer(),
       sha256.hash(Fr.ZERO.toBuffer(), Fr.ZERO.toBuffer()),
     ]);
-    expect(siblingPath.toString()).toBe(expectedSiblingPath.toString());
-  }, 36_000);
+
+    expect(siblingPathAlt.toString()).toBe(expectedSiblingPathAlt.toString());
+
+  }, 360_000);
 });
