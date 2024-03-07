181e0b41eb1beeaa8708345db993a6b1a81884e9
IlyasRidhuan <ilyasridhuan@gmail.com>
2024-03-07 15:22:52 +0000

    chore: more dockerfiles from noir

--- barretenberg/acir_tests/Dockerfile.bb.sol
+++ barretenberg/acir_tests/Dockerfile.bb.sol
@@ -2,14 +2,21 @@ FROM aztecprotocol/barretenberg-x86_64-linux-clang-assert
 FROM aztecprotocol/barretenberg-x86_64-linux-clang-sol
 FROM aztecprotocol/noir-compile-acir-tests as noir-acir-tests
 
-FROM node:18.19.0-alpine
-RUN apk update && apk add git bash curl jq
+FROM node:18.19.0
+RUN apt update && apt install git bash curl jq -y
 COPY --from=0 /usr/src/barretenberg/cpp/build /usr/src/barretenberg/cpp/build
 COPY --from=1 /usr/src/barretenberg/sol/src/ultra/BaseUltraVerifier.sol /usr/src/barretenberg/sol/src/ultra/BaseUltraVerifier.sol
 COPY --from=noir-acir-tests /usr/src/noir/noir-repo/test_programs /usr/src/noir/noir-repo/test_programs
-COPY --from=ghcr.io/foundry-rs/foundry:latest /usr/local/bin/anvil /usr/local/bin/anvil
+# COPY --from=ghcr.io/foundry-rs/foundry:latest /usr/local/bin/anvil /usr/local/bin/anvil
+
+RUN curl -L https://foundry.paradigm.xyz | bash
+ENV PATH="${PATH}:/root/.foundry/bin"
+RUN foundryup
+
 WORKDIR /usr/src/barretenberg/acir_tests
 COPY . .
 # Run every acir test through a solidity verifier.
 RUN (cd sol-test && yarn)
-RUN PARALLEL=1 FLOW=sol ./run_acir_tests.sh
+CMD ["/bin/bash"]
+# RUN PARALLEL=1 FLOW=sol ./run_acir_tests.sh
+
