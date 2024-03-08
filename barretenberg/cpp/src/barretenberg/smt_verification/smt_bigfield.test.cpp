465d70b2cf07e9e4a5cd053c63ef37d9fdaf214c
Sarkoxed <sarkoxed2013@yandex.ru>
2024-03-08 17:19:33 +0000

    Rename the unique_witness function

--- barretenberg/cpp/src/barretenberg/smt_verification/smt_bigfield.test.cpp
+++ barretenberg/cpp/src/barretenberg/smt_verification/smt_bigfield.test.cpp
@@ -208,7 +208,7 @@ TEST(bigfield, unique_square)
     Solver s(circuit_info.modulus);
 
     std::pair<Circuit<FFTerm>, Circuit<FFTerm>> cs =
-        unique_witness<FFTerm>(circuit_info,
+        unique_witness_ext<FFTerm>(circuit_info,
                                &s,
                                { "a_limb_0", "a_limb_1", "a_limb_2", "a_limb_3" },
                                { "c_limb_0", "c_limb_1", "c_limb_2", "c_limb_3" });
