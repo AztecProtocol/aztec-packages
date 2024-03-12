e74ba1daf27ef06cd9157f568ea744403d30cbb0
Esau <esau@aztecprotocol.com>
2024-03-12 01:34:37 +0100

    fix

--- l1-contracts/src/core/libraries/Merkle.sol
+++ l1-contracts/src/core/libraries/Merkle.sol
@@ -10,22 +10,20 @@ import {Errors} from "../libraries/Errors.sol";
  * @notice
  */
 library Merkle {
-  /*
+  /**
   * @notice Verifies the membership of a leaf and path against an expected root.
   * @dev In the case of a mismatched root, and subsequent inability to verify membership, this function throws.
   * @param _path - The sibling path of the message as a leaf, used to prove message inclusion
   * @param _leaf - The hash of the message we are trying to prove inclusion for
   * @param _index - The index of the message inside the L2 to L1 message tree
   * @param _expectedRoot - The expected root to check the validity of the message and sibling path with.
-  * @remark -
+  * @notice -
   * E.g. A sibling path for a leaf at index 3 in a tree of depth 3 (between 5 and 8 leafs) consists of the 3 elements denoted as *'s
   * d0:                                            [ root ]
   * d1:                      [ ]                                               [*]
   * d2:         [*]                      [ ]                       [ ]                     [ ]
   * d3:   [ ]         [ ]          [*]         [ ]           [ ]         [ ]          [ ]        [ ].
   * And the elements would be ordered as: [ d3_index_2, d2_index_0, d1_index_1 ].
-  * @remark - We use the indexAtHeight to see whether our child of the next subtree is at the left or the right side, as any odd indexes are right-sided children.
-  * This is important and we have assigned it into its own variable isRight, because this affects the way we concat our two children to then hash and calculate the root.
   */
   function verifyMembership(
     bytes32[] memory _path,
@@ -34,16 +32,18 @@ library Merkle {
     bytes32 _expectedRoot
   ) internal pure {
     bytes32 subtreeRoot = _leaf;
+    /// @notice - We use the indexAtHeight to see whether our child of the next subtree is at the left or the right side
     uint256 indexAtHeight = _index;
 
     for (uint256 height = 0; height < _path.length; height++) {
+      /// @notice - This affects the way we concatenate our two children to then hash and calculate the root, as any odd indexes (index bit-masked with least significant bit) are right-sided children.
       bool isRight = (indexAtHeight & 1) == 1;
 
       subtreeRoot = isRight
         ? sha256(bytes.concat(_path[height], subtreeRoot))
         : sha256(bytes.concat(subtreeRoot, _path[height]));
-
-      indexAtHeight /= 2;
+      /// @notice - We divide by two here to get the index of the parent of the current subtreeRoot in its own layer
+      indexAtHeight >>= 1;
     }
 
     if (subtreeRoot != _expectedRoot) {
