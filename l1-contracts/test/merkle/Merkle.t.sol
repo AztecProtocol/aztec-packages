4675c791142812e157a9f0a673a46357134c6a0b
benesjan <janbenes1234@gmail.com>
2024-03-13 10:00:00 +0000

    WIP

--- l1-contracts/test/merkle/Merkle.t.sol
+++ l1-contracts/test/merkle/Merkle.t.sol
@@ -29,7 +29,7 @@ contract MerkleTest is Test {
   }
 
   function testRootMatchesBaseParity() public {
-    uint256[4] memory baseRoots = [
+    uint256[4] memory msgs = [
       0x151de48ca3efbae39f180fe00b8f472ec9f25be10b4f283a87c6d78393537039,
       0x14c2ea9dedf77698d4afe23bc663263eed0bf9aa3a8b17d9b74812f185610f9e,
       0x1570cc6641699e3ae87fa258d80a6d853f7b8ccb211dc244d017e2ca6530f8a1,
@@ -39,9 +39,9 @@ contract MerkleTest is Test {
     // We can't use Constants.NUM_MSGS_PER_BASE_PARITY directly when defining the array so we do the check here to
     // ensure it does not get outdated.
     assertEq(
-      baseRoots.length,
+      msgs.length,
       Constants.NUM_MSGS_PER_BASE_PARITY,
-      "NUM_MSGS_PER_BASE_PARITY changed, update baseRoots."
+      "NUM_MSGS_PER_BASE_PARITY changed, update msgs."
     );
 
     uint256 treeHeight = 2; // log_2(NUM_MSGS_PER_BASE_PARITY)
@@ -55,8 +55,8 @@ contract MerkleTest is Test {
 
     FrontierMerkle frontier = new FrontierMerkle(treeHeight);
 
-    for (uint256 i = 0; i < baseRoots.length; i++) {
-      frontier.insertLeaf(bytes32(baseRoots[i]));
+    for (uint256 i = 0; i < msgs.length; i++) {
+      frontier.insertLeaf(bytes32(msgs[i]));
     }
 
     bytes32 expectedRoot = 0xb3a3fc1968999f2c2d798b900bdf0de41311be2a4d20496a7e792a521fc8abac;
