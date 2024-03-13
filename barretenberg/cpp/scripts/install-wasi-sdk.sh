92281aeb4946ce5caf7c703d2984ac451695951f
ludamad <domuradical@gmail.com>
2024-03-13 18:04:53 +0000

    fix path

--- barretenberg/cpp/scripts/install-wasi-sdk.sh
+++ barretenberg/cpp/scripts/install-wasi-sdk.sh
@@ -22,6 +22,7 @@ fi
 # Install the webassembly toolchain.
 mkdir -p src
 cd ./src
+# TODO(https://github.com/AztecProtocol/barretenberg/issues/865): is this needed?
 curl -s -L https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-20/wasi-sdk-20.0-$OS.tar.gz | tar zxf -
 if [ "$OS" == "linux" ]; then
   # Exceptional linux case that provides an llvm build that works on Ubuntu 20.
@@ -34,5 +35,5 @@ else
   # Can remove once future releases are more stable.
   curl -s -L https://wasi-sdk.s3.eu-west-2.amazonaws.com/yamt-wasi-sysroot-20.0.threads.tgz | tar zxf -
 fi
-# TODO(AD): in the future this should use eartly and a 'SAVE ARTIFACT wasi-sdk AS LOCAL wasi-sdk'
-mv wasi-sdk-* wasi-sdk
+# TODO(https://github.com/AztecProtocol/barretenberg/issues/906): in the future this should use eartlhy and a 'SAVE ARTIFACT wasi-sdk AS LOCAL wasi-sdk'
+mv wasi-sdk-20+threads wasi-sdk
