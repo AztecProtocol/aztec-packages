034eb77e3768fe85e91c0c14fac135fb82d2ede0
Mitchell Tracy <mitchell@aztecprotocol.com>
2024-03-13 16:32:31 -0400

    fix conflicts

--- yarn-project/sequencer-client/src/sequencer/public_processor.test.ts
+++ yarn-project/sequencer-client/src/sequencer/public_processor.test.ts
@@ -407,9 +407,9 @@ describe('public_processor', () => {
         baseContractAddressSeed,
         baseContractAddressSeed,
       ].map(makePublicCallRequest);
-      callRequests[0].callContext.startSideEffectCounter = 2;
-      callRequests[1].callContext.startSideEffectCounter = 3;
-      callRequests[2].callContext.startSideEffectCounter = 4;
+      callRequests[0].callContext.sideEffectCounter = 2;
+      callRequests[1].callContext.sideEffectCounter = 3;
+      callRequests[2].callContext.sideEffectCounter = 4;
 
       const kernelOutput = makePrivateKernelTailCircuitPublicInputs(0x10);
       kernelOutput.end.unencryptedLogsHash = [Fr.ZERO, Fr.ZERO];
@@ -511,9 +511,9 @@ describe('public_processor', () => {
         baseContractAddressSeed,
         baseContractAddressSeed,
       ].map(makePublicCallRequest);
-      callRequests[0].callContext.startSideEffectCounter = 2;
-      callRequests[1].callContext.startSideEffectCounter = 3;
-      callRequests[2].callContext.startSideEffectCounter = 4;
+      callRequests[0].callContext.sideEffectCounter = 2;
+      callRequests[1].callContext.sideEffectCounter = 3;
+      callRequests[2].callContext.sideEffectCounter = 4;
 
       const kernelOutput = makePrivateKernelTailCircuitPublicInputs(0x10);
       kernelOutput.end.unencryptedLogsHash = [Fr.ZERO, Fr.ZERO];
