284f1805454d354609953ea8bffe6b5552c0c3b9
Charlie Lye <karl.lye@gmail.com>
2024-03-18 23:18:59 +0000

    Run bb tests in ga.

---
+++ scripts/setup_env.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Setup environment variables
+echo "Setting up environment variables..."
+echo FORCE_COLOR=1 >> $GITHUB_ENV
+echo DOCKER_HOST=ssh://build-instance-$1.aztecprotocol.com >> $GITHUB_ENV
+
+# Docker login
+echo "Logging in to Docker..."
+echo $2 | docker login -u aztecprotocolci --password-stdin
+
+# Configure SSH
+echo "Configuring SSH..."
+mkdir -p ~/.ssh
+echo $3 | base64 -d > ~/.ssh/build_instance_key
+chmod 600 ~/.ssh/build_instance_key
+cat > ~/.ssh/config <<EOF
+IdentityFile ~/.ssh/build_instance_key
+StrictHostKeyChecking no
+User ubuntu
+EOF
+
+# Install earthly
+wget https://github.com/earthly/earthly/releases/latest/download/earthly-linux-amd64 -O /usr/local/bin/earthly
+chmod +x /usr/local/bin/earthly
+echo EARTHLY_CONFIG=$(dirname $0)/earthly-config.yml >> $GITHUB_ENV
