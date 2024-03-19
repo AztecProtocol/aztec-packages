3230eef56e413e5d5094bfc7010b1b39945b5394
vezenovm <mvezenov@gmail.com>
2024-03-19 14:39:02 +0000

    use mem take

--- noir/noir-repo/tooling/nargo/src/ops/transform.rs
+++ noir/noir-repo/tooling/nargo/src/ops/transform.rs
@@ -11,7 +11,7 @@ pub fn transform_program(
 ) -> CompiledProgram {
     // TODO: Work to get rid of these clones by borrowing `Circuit` in the acvm compiler or by accepted a `Program`
     let (optimized_circuit, location_map) =
-        acvm::compiler::compile(compiled_program.program.functions[0].clone(), expression_width);
+        acvm::compiler::compile( std::mem::take(&mut compiled_program.program.functions[0]), expression_width);
 
     compiled_program.program.functions[0] = optimized_circuit;
     compiled_program.debug.update_acir(location_map);
@@ -24,7 +24,7 @@ pub fn transform_contract(
 ) -> CompiledContract {
     let functions = vecmap(contract.functions, |mut func| {
         let (optimized_bytecode, location_map) =
-            acvm::compiler::compile(func.bytecode.functions[0].clone(), expression_width);
+            acvm::compiler::compile(std::mem::take(&mut compiled_program.program.functions[0]), expression_width);
         func.bytecode.functions[0] = optimized_bytecode;
         func.debug.update_acir(location_map);
         func
