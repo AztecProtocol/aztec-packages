2a07951370281915bf06ab5a4ab5a2ac83361708
vezenovm <mvezenov@gmail.com>
2024-03-19 16:02:21 +0000

    remove extra expression_width param passed to optimize

--- noir/noir-repo/tooling/nargo/src/ops/optimize.rs
+++ noir/noir-repo/tooling/nargo/src/ops/optimize.rs
@@ -5,10 +5,8 @@ use noirc_driver::{CompiledContract, CompiledProgram};
 /// multiple ACIR functions
 
 pub fn optimize_program(mut compiled_program: CompiledProgram) -> CompiledProgram {
-       let (optimized_circuit, location_map) = acvm::compiler::optimize(
-        std::mem::take(&mut compiled_program.program.functions[0]),
-        expression_width,
-    );
+    let (optimized_circuit, location_map) =
+        acvm::compiler::optimize(std::mem::take(&mut compiled_program.program.functions[0]));
     compiled_program.program.functions[0] = optimized_circuit;
     compiled_program.debug.update_acir(location_map);
     compiled_program
@@ -16,10 +14,8 @@ pub fn optimize_program(mut compiled_program: CompiledProgram) -> CompiledProgra
 
 pub fn optimize_contract(contract: CompiledContract) -> CompiledContract {
     let functions = vecmap(contract.functions, |mut func| {
-        let (optimized_bytecode, location_map) = acvm::compiler::optimize(
-            std::mem::take(&mut func.bytecode.functions[0]),
-            expression_width,
-        );
+        let (optimized_bytecode, location_map) =
+            acvm::compiler::optimize(std::mem::take(&mut func.bytecode.functions[0]));
         func.bytecode.functions[0] = optimized_bytecode;
         func.debug.update_acir(location_map);
         func
