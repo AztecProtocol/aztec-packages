fb7554428825f73a06f245a06ba1b3a0d5eca223
vezenovm <mvezenov@gmail.com>
2024-03-19 14:42:22 +0000

    cargo fmt

--- noir/noir-repo/tooling/nargo/src/ops/transform.rs
+++ noir/noir-repo/tooling/nargo/src/ops/transform.rs
@@ -10,8 +10,10 @@ pub fn transform_program(
     expression_width: ExpressionWidth,
 ) -> CompiledProgram {
     // TODO: Work to get rid of these clones by borrowing `Circuit` in the acvm compiler or by accepted a `Program`
-    let (optimized_circuit, location_map) =
-        acvm::compiler::compile( std::mem::take(&mut compiled_program.program.functions[0]), expression_width);
+    let (optimized_circuit, location_map) = acvm::compiler::compile(
+        std::mem::take(&mut compiled_program.program.functions[0]),
+        expression_width,
+    );
 
     compiled_program.program.functions[0] = optimized_circuit;
     compiled_program.debug.update_acir(location_map);
@@ -23,8 +25,10 @@ pub fn transform_contract(
     expression_width: ExpressionWidth,
 ) -> CompiledContract {
     let functions = vecmap(contract.functions, |mut func| {
-        let (optimized_bytecode, location_map) =
-            acvm::compiler::compile(std::mem::take(&mut compiled_program.program.functions[0]), expression_width);
+        let (optimized_bytecode, location_map) = acvm::compiler::compile(
+            std::mem::take(&mut compiled_program.program.functions[0]),
+            expression_width,
+        );
         func.bytecode.functions[0] = optimized_bytecode;
         func.debug.update_acir(location_map);
         func
