7d42587a84f86de382d28dee6b46d99c7a28f85b
ludamad <domuradical@gmail.com>
2024-03-20 20:35:46 +0000

    bindings

--- barretenberg/cpp/src/barretenberg/crypto/pedersen_hash/c_bind.hpp
+++ barretenberg/cpp/src/barretenberg/crypto/pedersen_hash/c_bind.hpp
@@ -5,7 +5,9 @@
 
 extern "C" {
 
-WASM_EXPORT void pedersen_hash(bb::fr::vec_in_buf inputs_buffer, uint32_t const* hash_index, bb::fr::out_buf output);
-WASM_EXPORT void pedersen_hashes(bb::fr::vec_in_buf inputs_buffer, uint32_t const* hash_index, bb::fr::out_buf output);
-WASM_EXPORT void pedersen_hash_buffer(uint8_t const* input_buffer, uint32_t const* hash_index, bb::fr::out_buf output);
+using namespace bb;
+
+WASM_EXPORT void pedersen_hash(fr::vec_in_buf inputs_buffer, uint32_t const* hash_index, fr::out_buf output);
+WASM_EXPORT void pedersen_hashes(fr::vec_in_buf inputs_buffer, uint32_t const* hash_index, fr::out_buf output);
+WASM_EXPORT void pedersen_hash_buffer(uint8_t const* input_buffer, uint32_t const* hash_index, fr::out_buf output);
 }
--- barretenberg/cpp/src/barretenberg/crypto/poseidon2/c_bind.hpp
+++ barretenberg/cpp/src/barretenberg/crypto/poseidon2/c_bind.hpp
@@ -5,6 +5,8 @@
 
 extern "C" {
 
-WASM_EXPORT void poseidon_hash(bb::fr::vec_in_buf inputs_buffer, bb::fr::out_buf output);
-WASM_EXPORT void poseidon_hashes(bb::fr::vec_in_buf inputs_buffer, bb::fr::out_buf output);
+using namespace bb;
+
+WASM_EXPORT void poseidon_hash(fr::vec_in_buf inputs_buffer, fr::out_buf output);
+WASM_EXPORT void poseidon_hashes(fr::vec_in_buf inputs_buffer, fr::out_buf output);
 }
