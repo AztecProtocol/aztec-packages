fd364f39d06271ae778797a77acb8d66a379e271
sklppy88 <esau@aztecprotocol.com>
2024-03-21 17:05:32 +0000

    add test

--- yarn-project/end-to-end/src/e2e_note_getter.test.ts
+++ yarn-project/end-to-end/src/e2e_note_getter.test.ts
@@ -1,4 +1,4 @@
-import { AztecAddress, Comparator, Fr, Wallet, toBigInt } from '@aztec/aztec.js';
+import { AztecAddress, AztecNode, Comparator, Fr, Wallet, toBigInt } from '@aztec/aztec.js';
 import { DocsExampleContract, TestContract } from '@aztec/noir-contracts.js';
 
 import { setup } from './fixtures/utils.js';
@@ -16,11 +16,12 @@ function unwrapOptions<T>(options: NoirOption<T>[]): T[] {
 }
 
 describe('e2e_note_getter', () => {
+  let aztecNode: AztecNode;
   let wallet: Wallet;
   let teardown: () => Promise<void>;
 
   beforeAll(async () => {
-    ({ teardown, wallet } = await setup());
+    ({ teardown, wallet, aztecNode } = await setup());
   }, 25_000);
 
   afterAll(() => teardown());
@@ -251,6 +252,31 @@ describe('e2e_note_getter', () => {
         // however that both view_notes and get_notes get the exact same result.
         expect(viewNotesManyResult).toEqual(getNotesManyResult);
         expect(viewNotesManyResult.sort()).toEqual([BigInt(VALUE), BigInt(VALUE + 1)]);
+      }, 45_000);
+
+      it('returns nullified notes', async () => {
+        await contract.methods.call_create_note(VALUE, owner, storageSlot).send().wait();
+        await contract.methods.call_destroy_note(storageSlot).send().wait();
+
+        await assertNoteIsReturned(storageSlot, VALUE, activeOrNullified);
+      }, 30_000);
+
+      it('get_notes should collapse sparse arrays', async () => {
+        // We call the function that creates a sparse array in get_notes_internal, using the filter
+        // It then gets the notes from the set and confirms that the array is not sparse and has been handled by get_notes
+        const tx = await contract.methods.create_and_get_many_notes_with_filter().send().wait();
+        const block = await aztecNode.getBlock(tx.blockNumber!);
+
+        // We want to verify that there are:
+        // 2 tx's in the block
+        expect(block!.body.unencryptedLogs.txLogs.length).toStrictEqual(2);
+        // our first tx has two function logs
+        expect(block?.body.unencryptedLogs.txLogs[0].functionLogs.length).toStrictEqual(2);
+        // our second function logs has 7 individual logs that were emitted from create_and_get_notes_many_with_filter
+        expect(block!.body.unencryptedLogs.txLogs[0].functionLogs[1].logs.length).toStrictEqual(7);
+
+        const unencryptedLogs = block?.body.unencryptedLogs.txLogs.flatMap(txLog => txLog.functionLogs.flatMap(functionLog => functionLog.logs.map(log => log.toString('hex').substring(log.length * 2 -2))));
+        expect(unencryptedLogs).toStrictEqual(['00', '01', '02', '03', '04', '05','06']);
       }, 45_000);
     });
   });
