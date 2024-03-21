202b7f9dc3ec8c58116efe515f73ce749a58d521
PhilWindle <philip.windle@gmail.com>
2024-03-21 16:48:22 +0000

    Logging

--- yarn-project/aztec-node/src/aztec-node/simulator-factory.ts
+++ yarn-project/aztec-node/src/aztec-node/simulator-factory.ts
@@ -12,7 +12,7 @@ export async function getSimulationProvider(
     try {
       await fs.access(acvmBinaryPath, fs.constants.R_OK);
       await fs.mkdir(acvmWorkingDirectory, { recursive: true });
-      logger?.(`Using native ACVM at ${acvmBinaryPath}`);
+      logger?.(`Using native ACVM at ${acvmBinaryPath} and working directory ${acvmWorkingDirectory}`);
       return new NativeACVMSimulator(acvmWorkingDirectory, acvmBinaryPath);
     } catch {
       logger?.(`Failed to access ACVM at ${acvmBinaryPath}, falling back to WASM`);
