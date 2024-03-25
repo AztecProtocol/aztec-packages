78e0674be5dd813407977f8f258c9707102245bc
Santiago Palladino <santiago@aztecprotocol.com>
2024-03-25 13:02:03 -0300

    fix: Broadcasting unconstrained function with empty sibling
    We were accidentally removing zero elements from the beginning of the
    sibling path when broadcasting unconstrained functions.

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -1,9 +1,9 @@
-import { getSampleContractArtifact } from '../tests/fixtures.js';
+import { getBenchmarkContractArtifact } from '../tests/fixtures.js';
 import { computeArtifactHash } from './artifact_hash.js';
 
 describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
-    const artifact = getSampleContractArtifact();
+    const artifact = getBenchmarkContractArtifact();
     expect(computeArtifactHash(artifact).toString()).toMatchInlineSnapshot(
       `"0x19dcd971117d72ceed658023cf16036d912de56c75a54da414d2d6bd645c99f2"`,
     );
