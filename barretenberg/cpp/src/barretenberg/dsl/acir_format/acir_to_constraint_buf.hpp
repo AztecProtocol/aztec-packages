905f830386d938c0a74de041b5233a72698ada79
vezenovm <mvezenov@gmail.com>
2024-03-26 20:37:25 +0000

    test ultra_honk_program and use last witness stack item for existing workflow

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/acir_to_constraint_buf.hpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/acir_to_constraint_buf.hpp
@@ -18,6 +18,7 @@
 #include "barretenberg/plonk_honk_shared/arithmetization/gate_data.hpp"
 #include "serde/index.hpp"
 #include <iterator>
+#include <utility>
 
 namespace acir_format {
 
@@ -363,12 +364,8 @@ void handle_memory_op(Program::Opcode::MemoryOp const& mem_op, BlockConstraint& 
     block.trace.push_back(acir_mem_op);
 }
 
-AcirFormat circuit_buf_to_acir_format(std::vector<uint8_t> const& buf)
+AcirFormat circuit_serde_to_acir_format(Program::Circuit const& circuit)
 {
-    // TODO(maxim): Handle the new `Program` structure once ACVM supports a function call stack.
-    // For now we expect a single ACIR function
-    auto circuit = Program::Program::bincodeDeserialize(buf).functions[0];
-
     AcirFormat af;
     // `varnum` is the true number of variables, thus we add one to the index which starts at zero
     af.varnum = circuit.current_witness_index + 1;
@@ -406,6 +403,15 @@ AcirFormat circuit_buf_to_acir_format(std::vector<uint8_t> const& buf)
     return af;
 }
 
+AcirFormat circuit_buf_to_acir_format(std::vector<uint8_t> const& buf)
+{
+    // TODO(maxim): Handle the new `Program` structure once ACVM supports a function call stack.
+    // For now we expect a single ACIR function
+    auto circuit = Program::Program::bincodeDeserialize(buf).functions[0];
+
+    return circuit_serde_to_acir_format(circuit);
+}
+
 /**
  * @brief Converts from the ACIR-native `WitnessMap` format to Barretenberg's internal `WitnessVector` format.
  *
@@ -419,7 +425,8 @@ WitnessVector witness_buf_to_witness_data(std::vector<uint8_t> const& buf)
     // TODO(maxim): Handle the new `WitnessStack` structure once ACVM supports a function call stack
     // A `StackItem` contains an index to an ACIR circuit and its respective ACIR-native `WitnessMap`.
     // For now we expect the `WitnessStack` to contain a single witness.
-    auto w = WitnessStack::WitnessStack::bincodeDeserialize(buf).stack[0].witness;
+    auto witness_stack = WitnessStack::WitnessStack::bincodeDeserialize(buf);
+    auto w = witness_stack.stack[witness_stack.stack.size() - 1].witness;
 
     WitnessVector wv;
     size_t index = 0;
@@ -435,6 +442,51 @@ WitnessVector witness_buf_to_witness_data(std::vector<uint8_t> const& buf)
         index++;
     }
     return wv;
+}
+
+std::vector<AcirFormat> program_buf_to_acir_format(std::vector<uint8_t> const& buf)
+{
+    auto program = Program::Program::bincodeDeserialize(buf);
+
+    std::vector<AcirFormat> constraint_systems;
+    constraint_systems.reserve(program.functions.size());
+    for (auto const& function : program.functions) {
+        constraint_systems.emplace_back(circuit_serde_to_acir_format(function));
+    }
+
+    return constraint_systems;
+}
+
+std::pair<uint32_t, WitnessVector> stack_item_buf_to_stack_item_data(WitnessStack::StackItem const& stack_item)
+{
+    auto w = stack_item.witness;
+
+    WitnessVector wv;
+    size_t index = 0;
+    for (auto& e : w.value) {
+        // ACIR uses a sparse format for WitnessMap where unused witness indices may be left unassigned.
+        // To ensure that witnesses sit at the correct indices in the `WitnessVector`, we fill any indices
+        // which do not exist within the `WitnessMap` with the dummy value of zero.
+        while (index < e.first.value) {
+            wv.push_back(bb::fr(0));
+            index++;
+        }
+        wv.push_back(bb::fr(uint256_t(e.second)));
+        index++;
+    }
+    return std::make_pair(stack_item.index, wv);
+}
+
+WitnessVectorStack witness_buf_to_witness_stack(std::vector<uint8_t> const& buf)
+{
+    auto witness_stack = WitnessStack::WitnessStack::bincodeDeserialize(buf);
+    WitnessVectorStack witness_vector_stack;
+    witness_vector_stack.reserve(witness_stack.stack.size());
+    for (auto const& stack_item : witness_stack.stack) {
+        auto serialized_stack_item = stack_item_buf_to_stack_item_data(stack_item);
+        witness_vector_stack.emplace_back(serialized_stack_item);
+    }
+    return witness_vector_stack;
 }
 
 } // namespace acir_format
