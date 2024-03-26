7bb6c6861b78712a443948930bc9edd984d31b1d
thunkar <gregojquiros@gmail.com>
2024-03-26 19:24:34 +0000

    updated snapshot

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -5,7 +5,7 @@ describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
     const artifact = getBenchmarkContractArtifact();
     expect(computeArtifactHash(artifact).toString()).toMatchInlineSnapshot(
-      `"0x19296a8280861c15b09b73699d7df675ca2ed8390d4607964e42ca955b508268"`,
+      `"0x2bc6a0e23550be996b215a1f0ac0dfd2d995849ca55c331278b727da280f18fa"`,
     );
   });
 });
