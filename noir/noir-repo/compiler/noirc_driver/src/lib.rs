aceb1ca3e58612ed1c8a041dea51d0eac41d3528
vezenovm <mvezenov@gmail.com>
2024-03-28 21:08:52 +0000

    update stale comment

--- noir/noir-repo/compiler/noirc_driver/src/lib.rs
+++ noir/noir-repo/compiler/noirc_driver/src/lib.rs
@@ -491,7 +491,7 @@ pub fn compile_no_check(
         // TODO(https://github.com/noir-lang/noir/issues/4428)
         program,
         // TODO(https://github.com/noir-lang/noir/issues/4428)
-        // Debug info is only relevant for errors at execution time which this PR does not yet support
+        // Debug info is only relevant for errors at execution time which is not yet supported
         // The CompileProgram `debug` field is used in multiple places and is better
         // left to be updated once execution of multiple ACIR functions is enabled
         debug: debug[0].clone(),
