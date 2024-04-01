3af34ca3537045d887509d0e9b7339fe322b5563
sklppy88 <esau@aztecprotocol.com>
2024-04-01 14:10:30 +0000

    initial
    test
    
    hi
    
    working
    
    test
    
    fix
    
    fix
    
    test
    
    test
    
    test

--- yarn-project/end-to-end/src/e2e_state_vars.test.ts
+++ yarn-project/end-to-end/src/e2e_state_vars.test.ts
@@ -20,163 +20,200 @@ describe('e2e_state_vars', () => {
   afterAll(() => teardown());
 
   describe('SharedImmutable', () => {
+    // it('private read of uninitialized SharedImmutable', async () => {
+    //   const tx = contract.methods.test_dynamic_array().send();
+    //   const extendedLogs = await tx.getUnencryptedLogs();
+
+    //   expect(extendedLogs.logs.map(log => log.log.data.toString('hex').slice(-2))).toStrictEqual([
+    //     '45',
+    //     '00',
+    //   ])
+      
+    //   await contract.methods.test_dynamic_array_2().send().wait();
+
+    //   const numResponse = await contract.methods.test_dynamic_array_3().view();
+    //   const lengthResponse = await contract.methods.test_dynamic_array_4().view();
+
+    //   expect([numResponse, lengthResponse]).toStrictEqual([420n, 1n]);
+    // });
+
     it('private read of uninitialized SharedImmutable', async () => {
-      const s = await contract.methods.get_shared_immutable().view();
-
-      // Send the transaction and wait for it to be mined (wait function throws if the tx is not mined)
-      await contract.methods.match_shared_immutable(s.account, s.points).send().wait();
+      const tx = contract.methods.test_dynamic_array_priv().send();
+      const extendedLogs = await tx.getUnencryptedLogs();
+      
+      const cardNote = await contract.methods.test_dynamic_array_priv_view().view();
     });
 
-    it('private read of initialized SharedImmutable', async () => {
-      await contract.methods.initialize_shared_immutable(1).send().wait();
-      const s = await contract.methods.get_shared_immutable().view();
+    // it('private read of uninitialized SharedImmutable', async () => {
+    //   await contract.methods.test_fixed_array().send().wait();
+    //   const tx = await contract.methods.test_fixed_array_2().send();
 
-      await contract.methods.match_shared_immutable(s.account, s.points).send().wait();
-    }, 200_000);
+    //   const extendedLogs = (await tx.getUnencryptedLogs()).logs;
 
-    it('initializing SharedImmutable the second time should fail', async () => {
-      // Jest executes the tests sequentially and the first call to initialize_shared_immutable was executed
-      // in the previous test, so the call bellow should fail.
-      await expect(contract.methods.initialize_shared_immutable(1).send().wait()).rejects.toThrow(
-        "Assertion failed: SharedImmutable already initialized 'fields_read[0] == 0'",
-      );
-    }, 100_000);
+    //   expect(extendedLogs.map(extendedLog => extendedLog.log.data.toString('hex').slice(-2))).toStrictEqual([
+    //     '00',
+    //     '01',
+    //     '02',
+    //     '03',
+    //     '04',
+    //     '05',
+    //     '09',
+    //     '07',
+    //     '08',
+    //     '09',
+    //   ])
+    // });
+
+    // it('private read of initialized SharedImmutable', async () => {
+    //   await contract.methods.initialize_shared_immutable(1).send().wait();
+    //   const s = await contract.methods.get_shared_immutable().view();
+
+    //   await contract.methods.match_shared_immutable(s.account, s.points).send().wait();
+    // }, 200_000);
+
+    // it('initializing SharedImmutable the second time should fail', async () => {
+    //   // Jest executes the tests sequentially and the first call to initialize_shared_immutable was executed
+    //   // in the previous test, so the call bellow should fail.
+    //   await expect(contract.methods.initialize_shared_immutable(1).send().wait()).rejects.toThrow(
+    //     "Assertion failed: SharedImmutable already initialized 'fields_read[0] == 0'",
+    //   );
+    // }, 100_000);
   });
 
-  describe('PublicImmutable', () => {
-    it('initialize and read public immutable', async () => {
-      const numPoints = 1n;
+  // describe('PublicImmutable', () => {
+  //   it('initialize and read public immutable', async () => {
+  //     const numPoints = 1n;
 
-      await contract.methods.initialize_public_immutable(numPoints).send().wait();
-      const p = await contract.methods.get_public_immutable().view();
+  //     await contract.methods.initialize_public_immutable(numPoints).send().wait();
+  //     const p = await contract.methods.get_public_immutable().view();
 
-      expect(p.account).toEqual(wallet.getCompleteAddress().address);
-      expect(p.points).toEqual(numPoints);
-    }, 200_000);
+  //     expect(p.account).toEqual(wallet.getCompleteAddress().address);
+  //     expect(p.points).toEqual(numPoints);
+  //   }, 200_000);
 
-    it('initializing PublicImmutable the second time should fail', async () => {
-      // Jest executes the tests sequentially and the first call to initialize_public_immutable was executed
-      // in the previous test, so the call bellow should fail.
-      await expect(contract.methods.initialize_public_immutable(1).send().wait()).rejects.toThrow(
-        "Assertion failed: PublicImmutable already initialized 'fields_read[0] == 0'",
-      );
-    }, 100_000);
-  });
+  //   it('initializing PublicImmutable the second time should fail', async () => {
+  //     // Jest executes the tests sequentially and the first call to initialize_public_immutable was executed
+  //     // in the previous test, so the call bellow should fail.
+  //     await expect(contract.methods.initialize_public_immutable(1).send().wait()).rejects.toThrow(
+  //       "Assertion failed: PublicImmutable already initialized 'fields_read[0] == 0'",
+  //     );
+  //   }, 100_000);
+  // });
 
-  describe('PrivateMutable', () => {
-    it('fail to read uninitialized PrivateMutable', async () => {
-      expect(await contract.methods.is_legendary_initialized().view()).toEqual(false);
-      await expect(contract.methods.get_legendary_card().view()).rejects.toThrow();
-    });
+  // describe('PrivateMutable', () => {
+  //   it('fail to read uninitialized PrivateMutable', async () => {
+  //     expect(await contract.methods.is_legendary_initialized().view()).toEqual(false);
+  //     await expect(contract.methods.get_legendary_card().view()).rejects.toThrow();
+  //   });
 
-    it('initialize PrivateMutable', async () => {
-      expect(await contract.methods.is_legendary_initialized().view()).toEqual(false);
-      // Send the transaction and wait for it to be mined (wait function throws if the tx is not mined)
-      const { debugInfo } = await contract.methods.initialize_private(RANDOMNESS, POINTS).send().wait({ debug: true });
+  //   it('initialize PrivateMutable', async () => {
+  //     expect(await contract.methods.is_legendary_initialized().view()).toEqual(false);
+  //     // Send the transaction and wait for it to be mined (wait function throws if the tx is not mined)
+  //     const { debugInfo } = await contract.methods.initialize_private(RANDOMNESS, POINTS).send().wait({ debug: true });
 
-      // 1 for the tx, another for the initializer
-      expect(debugInfo!.nullifiers.length).toEqual(2);
-      expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
-    });
+  //     // 1 for the tx, another for the initializer
+  //     expect(debugInfo!.nullifiers.length).toEqual(2);
+  //     expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
+  //   });
 
-    it('fail to reinitialize', async () => {
-      expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
-      await expect(contract.methods.initialize_private(RANDOMNESS, POINTS).send().wait()).rejects.toThrow();
-      expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
-    });
+  //   it('fail to reinitialize', async () => {
+  //     expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
+  //     await expect(contract.methods.initialize_private(RANDOMNESS, POINTS).send().wait()).rejects.toThrow();
+  //     expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
+  //   });
 
-    it('read initialized PrivateMutable', async () => {
-      expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
-      const { points, randomness } = await contract.methods.get_legendary_card().view();
-      expect(points).toEqual(POINTS);
-      expect(randomness).toEqual(RANDOMNESS);
-    });
+  //   it('read initialized PrivateMutable', async () => {
+  //     expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
+  //     const { points, randomness } = await contract.methods.get_legendary_card().view();
+  //     expect(points).toEqual(POINTS);
+  //     expect(randomness).toEqual(RANDOMNESS);
+  //   });
 
-    it('replace with same value', async () => {
-      expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
-      const noteBefore = await contract.methods.get_legendary_card().view();
-      const { debugInfo } = await contract.methods
-        .update_legendary_card(RANDOMNESS, POINTS)
-        .send()
-        .wait({ debug: true });
+  //   it('replace with same value', async () => {
+  //     expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
+  //     const noteBefore = await contract.methods.get_legendary_card().view();
+  //     const { debugInfo } = await contract.methods
+  //       .update_legendary_card(RANDOMNESS, POINTS)
+  //       .send()
+  //       .wait({ debug: true });
 
-      expect(debugInfo!.noteHashes.length).toEqual(1);
-      // 1 for the tx, another for the nullifier of the previous note
-      expect(debugInfo!.nullifiers.length).toEqual(2);
+  //     expect(debugInfo!.noteHashes.length).toEqual(1);
+  //     // 1 for the tx, another for the nullifier of the previous note
+  //     expect(debugInfo!.nullifiers.length).toEqual(2);
 
-      const noteAfter = await contract.methods.get_legendary_card().view();
+  //     const noteAfter = await contract.methods.get_legendary_card().view();
 
-      expect(noteBefore.owner).toEqual(noteAfter.owner);
-      expect(noteBefore.points).toEqual(noteAfter.points);
-      expect(noteBefore.randomness).toEqual(noteAfter.randomness);
-      expect(noteBefore.header.contract_address).toEqual(noteAfter.header.contract_address);
-      expect(noteBefore.header.storage_slot).toEqual(noteAfter.header.storage_slot);
-      expect(noteBefore.header.is_transient).toEqual(noteAfter.header.is_transient);
-      // !!! Nonce must be different
-      expect(noteBefore.header.nonce).not.toEqual(noteAfter.header.nonce);
-    });
+  //     expect(noteBefore.owner).toEqual(noteAfter.owner);
+  //     expect(noteBefore.points).toEqual(noteAfter.points);
+  //     expect(noteBefore.randomness).toEqual(noteAfter.randomness);
+  //     expect(noteBefore.header.contract_address).toEqual(noteAfter.header.contract_address);
+  //     expect(noteBefore.header.storage_slot).toEqual(noteAfter.header.storage_slot);
+  //     expect(noteBefore.header.is_transient).toEqual(noteAfter.header.is_transient);
+  //     // !!! Nonce must be different
+  //     expect(noteBefore.header.nonce).not.toEqual(noteAfter.header.nonce);
+  //   });
 
-    it('replace PrivateMutable with other values', async () => {
-      expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
-      const { debugInfo } = await contract.methods
-        .update_legendary_card(RANDOMNESS + 2n, POINTS + 1n)
-        .send()
-        .wait({ debug: true });
+  //   it('replace PrivateMutable with other values', async () => {
+  //     expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
+  //     const { debugInfo } = await contract.methods
+  //       .update_legendary_card(RANDOMNESS + 2n, POINTS + 1n)
+  //       .send()
+  //       .wait({ debug: true });
 
-      expect(debugInfo!.noteHashes.length).toEqual(1);
-      // 1 for the tx, another for the nullifier of the previous note
-      expect(debugInfo!.nullifiers.length).toEqual(2);
+  //     expect(debugInfo!.noteHashes.length).toEqual(1);
+  //     // 1 for the tx, another for the nullifier of the previous note
+  //     expect(debugInfo!.nullifiers.length).toEqual(2);
 
-      const { points, randomness } = await contract.methods.get_legendary_card().view();
-      expect(points).toEqual(POINTS + 1n);
-      expect(randomness).toEqual(RANDOMNESS + 2n);
-    });
+  //     const { points, randomness } = await contract.methods.get_legendary_card().view();
+  //     expect(points).toEqual(POINTS + 1n);
+  //     expect(randomness).toEqual(RANDOMNESS + 2n);
+  //   });
 
-    it('replace PrivateMutable dependent on prior value', async () => {
-      expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
-      const noteBefore = await contract.methods.get_legendary_card().view();
-      const { debugInfo } = await contract.methods.increase_legendary_points().send().wait({ debug: true });
+  //   it('replace PrivateMutable dependent on prior value', async () => {
+  //     expect(await contract.methods.is_legendary_initialized().view()).toEqual(true);
+  //     const noteBefore = await contract.methods.get_legendary_card().view();
+  //     const { debugInfo } = await contract.methods.increase_legendary_points().send().wait({ debug: true });
 
-      expect(debugInfo!.noteHashes.length).toEqual(1);
-      // 1 for the tx, another for the nullifier of the previous note
-      expect(debugInfo!.nullifiers.length).toEqual(2);
+  //     expect(debugInfo!.noteHashes.length).toEqual(1);
+  //     // 1 for the tx, another for the nullifier of the previous note
+  //     expect(debugInfo!.nullifiers.length).toEqual(2);
 
-      const { points, randomness } = await contract.methods.get_legendary_card().view();
-      expect(points).toEqual(noteBefore.points + 1n);
-      expect(randomness).toEqual(noteBefore.randomness);
-    });
-  });
+  //     const { points, randomness } = await contract.methods.get_legendary_card().view();
+  //     expect(points).toEqual(noteBefore.points + 1n);
+  //     expect(randomness).toEqual(noteBefore.randomness);
+  //   });
+  // });
 
-  describe('PrivateImmutable', () => {
-    it('fail to read uninitialized PrivateImmutable', async () => {
-      expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(false);
-      await expect(contract.methods.view_imm_card().view()).rejects.toThrow();
-    });
+  // describe('PrivateImmutable', () => {
+  //   it('fail to read uninitialized PrivateImmutable', async () => {
+  //     expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(false);
+  //     await expect(contract.methods.view_imm_card().view()).rejects.toThrow();
+  //   });
 
-    it('initialize PrivateImmutable', async () => {
-      expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(false);
-      const { debugInfo } = await contract.methods
-        .initialize_private_immutable(RANDOMNESS, POINTS)
-        .send()
-        .wait({ debug: true });
+  //   it('initialize PrivateImmutable', async () => {
+  //     expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(false);
+  //     const { debugInfo } = await contract.methods
+  //       .initialize_private_immutable(RANDOMNESS, POINTS)
+  //       .send()
+  //       .wait({ debug: true });
 
-      expect(debugInfo!.noteHashes.length).toEqual(1);
-      // 1 for the tx, another for the initializer
-      expect(debugInfo!.nullifiers.length).toEqual(2);
-      expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(true);
-    });
+  //     expect(debugInfo!.noteHashes.length).toEqual(1);
+  //     // 1 for the tx, another for the initializer
+  //     expect(debugInfo!.nullifiers.length).toEqual(2);
+  //     expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(true);
+  //   });
 
-    it('fail to reinitialize', async () => {
-      expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(true);
-      await expect(contract.methods.initialize_private_immutable(RANDOMNESS, POINTS).send().wait()).rejects.toThrow();
-      expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(true);
-    });
+  //   it('fail to reinitialize', async () => {
+  //     expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(true);
+  //     await expect(contract.methods.initialize_private_immutable(RANDOMNESS, POINTS).send().wait()).rejects.toThrow();
+  //     expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(true);
+  //   });
 
-    it('read initialized PrivateImmutable', async () => {
-      expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(true);
-      const { points, randomness } = await contract.methods.view_imm_card().view();
-      expect(points).toEqual(POINTS);
-      expect(randomness).toEqual(RANDOMNESS);
-    });
-  });
+  //   it('read initialized PrivateImmutable', async () => {
+  //     expect(await contract.methods.is_priv_imm_initialized().view()).toEqual(true);
+  //     const { points, randomness } = await contract.methods.view_imm_card().view();
+  //     expect(points).toEqual(POINTS);
+  //     expect(randomness).toEqual(RANDOMNESS);
+  //   });
+  // });
 });
