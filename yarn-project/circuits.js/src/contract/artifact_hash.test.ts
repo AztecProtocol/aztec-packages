2859f5f489f2b29303f546f0d5d32eced860e536
thunkar <gregojquiros@gmail.com>
2024-04-01 06:34:00 +0000

    updated artifact hash

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -5,7 +5,7 @@ describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
     const artifact = getBenchmarkContractArtifact();
     expect(computeArtifactHash(artifact).toString()).toMatchInlineSnapshot(
-      `"0x0e5000b5ebf1b4642300adbdae8f4eb0f7d6a04551f797bcfd814a281efa277c"`,
+      `"0x2f281ca9a1c4739ed0da8d0d01a91ba443eb35007e266b3f23956b9bec5968b8"`,
     );
   });
 });
