281519e190c50fe5f210cf40121a435837a476e7
sirasistant <sirasistant@gmail.com>
2024-04-01 10:38:18 +0000

    wip: more removal of pedersen

--- yarn-project/circuits.js/src/structs/function_data.ts
+++ yarn-project/circuits.js/src/structs/function_data.ts
@@ -1,9 +1,9 @@
 import { FunctionAbi, FunctionSelector, FunctionType } from '@aztec/foundation/abi';
-import { pedersenHash } from '@aztec/foundation/crypto';
+import { poseidonHash } from '@aztec/foundation/crypto';
 import { Fr } from '@aztec/foundation/fields';
 import { BufferReader, FieldReader, serializeToBuffer } from '@aztec/foundation/serialize';
 
-import { FUNCTION_DATA_LENGTH, GeneratorIndex } from '../constants.gen.js';
+import { FUNCTION_DATA_LENGTH } from '../constants.gen.js';
 import { ContractFunctionDao } from '../types/contract_function_dao.js';
 
 /** Function description for circuit. */
@@ -87,6 +87,6 @@ export class FunctionData {
   }
 
   hash(): Fr {
-    return pedersenHash(this.toFields(), GeneratorIndex.FUNCTION_DATA);
+    return poseidonHash(this.toFields());
   }
 }
