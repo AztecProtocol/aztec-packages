2d188743ad0097956e51fc492ffbe77f9bf3f49e
thunkar <gregojquiros@gmail.com>
2024-04-02 08:43:34 +0000

    guess what

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -5,7 +5,7 @@ describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
     const artifact = getBenchmarkContractArtifact();
     expect(computeArtifactHash(artifact).toString()).toMatchInlineSnapshot(
-      `"0x225f865e3ac2c7d1fc32ef78a0e7528be944ff60343eb5dec0a853166d9eb077"`,
+      `"0x2f281ca9a1c4739ed0da8d0d01a91ba443eb35007e266b3f23956b9bec5968b8"`,
     );
   });
 });
