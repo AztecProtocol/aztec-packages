6c64e89373e695da64a26f19c76ba82a872fae4b
lucasxia01 <lucasxia01@gmail.com>
2024-04-02 22:54:46 +0000

    added benchmark for folding multiple instances

--- barretenberg/cpp/src/barretenberg/benchmark/protogalaxy_bench/protogalaxy.bench.cpp
+++ barretenberg/cpp/src/barretenberg/benchmark/protogalaxy_bench/protogalaxy.bench.cpp
@@ -39,8 +39,41 @@ template <typename Flavor> void fold_one(State& state) noexcept
     }
 }
 
+// Fold twos instances into an accumulator.
+template <typename Flavor> void fold_two(State& state) noexcept
+{
+    using ProverInstance = ProverInstance_<Flavor>;
+    using Instance = ProverInstance;
+    using Instances = ProverInstances_<Flavor, 3>;
+    using ProtoGalaxyProver = ProtoGalaxyProver_<Instances>;
+    using Builder = typename Flavor::CircuitBuilder;
+
+    bb::srs::init_crs_factory("../srs_db/ignition");
+
+    auto log2_num_gates = static_cast<size_t>(state.range(0));
+
+    const auto construct_instance = [&]() {
+        Builder builder;
+        MockCircuits::construct_arithmetic_circuit(builder, log2_num_gates);
+        return std::make_shared<ProverInstance>(builder);
+    };
+
+    std::shared_ptr<Instance> instance_1 = construct_instance();
+    std::shared_ptr<Instance> instance_2 = construct_instance();
+    std::shared_ptr<Instance> instance_3 = construct_instance();
+
+    ProtoGalaxyProver folding_prover({ instance_1, instance_2, instance_3 });
+
+    for (auto _ : state) {
+        auto proof = folding_prover.fold_instances();
+    }
+}
+
 BENCHMARK(fold_one<UltraFlavor>)->/* vary the circuit size */ DenseRange(14, 20)->Unit(kMillisecond);
 BENCHMARK(fold_one<GoblinUltraFlavor>)->/* vary the circuit size */ DenseRange(14, 20)->Unit(kMillisecond);
+
+BENCHMARK(fold_two<UltraFlavor>)->/* vary the circuit size */ DenseRange(14, 20)->Unit(kMillisecond);
+BENCHMARK(fold_two<GoblinUltraFlavor>)->/* vary the circuit size */ DenseRange(14, 20)->Unit(kMillisecond);
 } // namespace bb
 
 BENCHMARK_MAIN();
