2e61cda16ebc08002f31685b9aec392e9e2ea132
thunkar <gregojquiros@gmail.com>
2024-04-02 09:12:40 +0000

    increased test machine size, hash

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -5,7 +5,7 @@ describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
     const artifact = getBenchmarkContractArtifact();
     expect(computeArtifactHash(artifact).toString()).toMatchInlineSnapshot(
-      `"0x063667fca66ca9545974f22133f8f357ca905d09dc3724c3b9a1c423722c38ef"`,
+      `"0x300a02f7b81e239129c55ff79c65b3461b700b7345bf94743b65b5f765282061"`,
     );
   });
 });
