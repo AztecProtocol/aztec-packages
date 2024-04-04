db2a41170e60e6ca4ad253551fab6a85742b7d76
thunkar <gregojquiros@gmail.com>
2024-04-04 06:10:37 +0000

    hash

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -5,7 +5,7 @@ describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
     const artifact = getBenchmarkContractArtifact();
     expect(computeArtifactHash(artifact).toString()).toMatchInlineSnapshot(
-      `"0x2f13ba1f63ab3f36fede3db7beb1eef6941606b30a881753a631dc8c32f05dcb"`,
+      `"0x1bf24d2106579f84afb2c55647aa66d0c37401e82952577ada09044c02220f81"`,
     );
   });
 });
