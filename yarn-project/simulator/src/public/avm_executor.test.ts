bc80039e73eea13cf8e1a667e7334597c0aa73c4
IlyasRidhuan <ilyasridhuan@gmail.com>
2024-04-05 12:33:13 +0000

    feat(avm): enable contract testing with bb binary

--- yarn-project/simulator/src/public/avm_executor.test.ts
+++ yarn-project/simulator/src/public/avm_executor.test.ts
@@ -9,6 +9,7 @@ import { type MockProxy, mock } from 'jest-mock-extended';
 import { type CommitmentsDB, type PublicContractsDB, type PublicStateDB } from './db.js';
 import { type PublicExecution } from './execution.js';
 import { PublicExecutor } from './executor.js';
+import { initContext, initExecutionEnvironment } from '../avm/fixtures/index.js';
 
 describe('AVM WitGen and Proof Generation', () => {
   let publicState: MockProxy<PublicStateDB>;
@@ -35,34 +36,16 @@ describe('AVM WitGen and Proof Generation', () => {
     header = makeHeader(randomInt(1000000));
   }, 10000);
 
-  it('Should prove valid execution of bytecode that performs addition', async () => {
-    const args: Fr[] = [new Fr(1), new Fr(2)];
-    // Bytecode for the following contract is encoded:
-    // const bytecode = encodeToBytecode([
-    //    new CalldataCopy(/*indirect=*/ 0, /*cdOffset=*/ 0, /*copySize=*/ 2, /*dstOffset=*/ 0),
-    //    new Add(/*indirect=*/ 0, TypeTag.FIELD, /*aOffset=*/ 0, /*bOffset=*/ 1, /*dstOffset=*/ 2),
-    //    new Return(/*indirect=*/ 0, /*returnOffset=*/ 2, /*copySize=*/ 1),
-    // ]);
-    const bytecode: Buffer = Buffer.from('IAAAAAAAAAAAAgAAAAAAAAYAAAAAAAAAAQAAAAI5AAAAAAIAAAAB', 'base64');
-    publicContracts.getBytecode.mockResolvedValue(bytecode);
-    const executor = new PublicExecutor(publicState, publicContracts, commitmentsDb, header);
-    const functionData = FunctionData.empty();
-    const execution: PublicExecution = { contractAddress, functionData, args, callContext };
-    const [proof, vk] = await executor.getAvmProof(execution);
-    const valid = await executor.verifyAvmProof(vk, proof);
-    expect(valid).toBe(true);
-  });
-
-  // This is skipped as we require MOV to be implemented in the AVM
-  it.skip('Should prove valid execution contract function that performs addition', async () => {
-    const args: Fr[] = [new Fr(1), new Fr(2)];
-
+  it('Should prove valid execution contract function that performs addition', async () => {
     const addArtifact = AvmTestContractArtifact.functions.find(f => f.name === 'add_args_return')!;
     const bytecode = addArtifact.bytecode;
     publicContracts.getBytecode.mockResolvedValue(bytecode);
-    const functionData = FunctionData.fromAbi(addArtifact);
-    const execution: PublicExecution = { contractAddress, functionData, args, callContext };
 
+    const functionData = FunctionData.fromAbi(addArtifact);
+    const args: Fr[] = [new Fr(99), new Fr(12)];
+    const context = initContext({ env: initExecutionEnvironment({ calldata: args }) });
+
+    const execution: PublicExecution = { contractAddress, functionData, args: context.environment.calldata, callContext };
     const executor = new PublicExecutor(publicState, publicContracts, commitmentsDb, header);
     const [proof, vk] = await executor.getAvmProof(execution);
     const valid = await executor.verifyAvmProof(vk, proof);
