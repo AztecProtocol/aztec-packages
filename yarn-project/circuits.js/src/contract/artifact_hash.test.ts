6de71679b0f6c9ddfa864462208c964cde3fd93c
sirasistant <sirasistant@gmail.com>
2024-04-09 08:12:54 +0000

    chore: update snapshots

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -5,7 +5,7 @@ describe('ArtifactHash', () => {
   it('calculates the artifact hash', () => {
     const artifact = getBenchmarkContractArtifact();
     expect(computeArtifactHash(artifact).toString()).toMatchInlineSnapshot(
-      `"0x2000991c126ffc45142660f7c3ad26f65aeb853a3b9c7e5906ba8205a2556128"`,
+      `"0x10238237ef0f27101d6b113978a6e669b479c8868ab0f06f444d4262f84da3dc"`,
     );
   });
 });
