ab909ee724e77d58be9052818b23330aad826a06
ludamad <domuradical@gmail.com>
2024-04-09 22:00:54 +0000

    try better cache settings

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -10,4 +10,3 @@ echo $1 | docker login -u aztecprotocolci --password-stdin
 
 # Make earthly-cloud and earthly-cloud-bench scripts available
 echo "PATH=$(dirname $(realpath $0)):$PATH" >> $GITHUB_ENV
-echo "EARTHLY_CONFIG=$(git rev-parse --show-toplevel)/.github/earthly-ci-config.yml" >> $GITHUB_ENV
