ae0d8a91adaeea15db95fa45a90ab83271fe6209
Maxim Vezenov <mvezenov@gmail.com>
2024-04-16 16:27:01 +0100

    Update noir/noir-repo/compiler/noirc_evaluator/src/ssa/acir_gen/acir_ir/acir_variable.rs
    Co-authored-by: jfecher <jfecher11@gmail.com>

--- noir/noir-repo/compiler/noirc_evaluator/src/ssa/acir_gen/acir_ir/acir_variable.rs
+++ noir/noir-repo/compiler/noirc_evaluator/src/ssa/acir_gen/acir_ir/acir_variable.rs
@@ -1594,7 +1594,7 @@ impl AcirContext {
         }
 
         // Otherwise we must generate ACIR for it and execute at runtime.
-        let mut b_outputs = Vec::new();
+        let mut brillig_outputs = Vec::new();
         let outputs_var = vecmap(outputs, |output| match output {
             AcirType::NumericType(_) => {
                 let witness_index = self.acir_ir.next_witness_index();
