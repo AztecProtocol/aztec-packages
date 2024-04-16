161f8c6168c0c0cf4e17f0d146a1bee371826274
vezenovm <mvezenov@gmail.com>
2024-04-16 19:52:18 +0000

    update codegen for brillig main to use brillig call

--- avm-transpiler/src/utils.rs
+++ avm-transpiler/src/utils.rs
@@ -1,35 +1,37 @@
 use acvm::acir::brillig::Opcode as BrilligOpcode;
 use log::debug;
 
-use acvm::acir::circuit::brillig::{Brillig, BrilligBytecode};
 use acvm::acir::circuit::{Opcode, Program};
 
 use crate::instructions::AvmInstruction;
 
-/// Extract the Brillig program from its ACIR wrapper instruction.
-/// An Noir unconstrained function compiles to one ACIR instruction
-/// wrapping a Brillig program. This function just extracts that Brillig
-/// assuming the 0th ACIR opcode is the wrapper.
-pub fn extract_brillig_from_acir(opcodes: &Vec<Opcode>) -> &Brillig {
-    if opcodes.len() != 1 {
-        panic!("An AVM program should be contained entirely in only a single ACIR opcode flagged as 'Brillig'");
-    }
-    let opcode = &opcodes[0];
-    match opcode {
-        Opcode::Brillig(brillig) => brillig,
-        _ => panic!("Tried to extract a Brillig program from its ACIR wrapper opcode, but the opcode doesn't contain Brillig!"),
-    }
-}
-
+/// Extract the Brillig program from its `Program` wrapper.
+/// Noir entry point unconstrained functions are compiled to their own list contained
+/// as part of a full program. Function calls are then accessed through a function
+/// pointer opcode in ACIR that fetches those unconstrained functions from the main list.
+/// This function just extracts Brillig bytecode, with the assumption that the
+/// 0th unconstrained function in the full `Program` structure.
 pub fn extract_brillig_from_acir_program(program: &Program) -> &[BrilligOpcode] {
-    assert_eq!(program.functions.len(), 1, "An AVM program should have only a single ACIR function flagged as 'BrilligCall'");
+    assert_eq!(
+        program.functions.len(),
+        1,
+        "An AVM program should have only a single ACIR function flagged as 'BrilligCall'"
+    );
     let opcodes = &program.functions[0].opcodes;
-    assert_eq!(opcodes.len(), 1, "An AVM program should have only a single ACIR function flagged as 'BrilligCall'");
+    assert_eq!(
+        opcodes.len(),
+        1,
+        "An AVM program should have only a single ACIR function flagged as 'BrilligCall'"
+    );
     match opcodes[0] {
-        Opcode::BrilligCall { id, .. } => {}
+        Opcode::BrilligCall { .. } => {}
         _ => panic!("Tried to extract a Brillig program from its ACIR wrapper opcode, but the opcode doesn't contain Brillig!"),
     }
-    assert_eq!(program.unconstrained_functions.len(), 1, "An AVM program should be contained entirely in only a single `Brillig` function");
+    assert_eq!(
+        program.unconstrained_functions.len(),
+        1,
+        "An AVM program should be contained entirely in only a single `Brillig` function"
+    );
     &program.unconstrained_functions[0].bytecode
 }
 
