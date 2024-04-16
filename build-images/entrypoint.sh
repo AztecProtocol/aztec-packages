ff94eb44f325c8c028073814466526a8317e9d83
Charlie Lye <karl.lye@gmail.com>
2024-04-16 11:43:18 +0100

    mac doesn't need manual id alignment

--- build-images/entrypoint.sh
+++ build-images/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -eu
+set -e
 
 # Modify the uid and gid of aztec-dev to match that of the host users.
-groupmod -g $LOCAL_GROUP_ID aztec-dev
-usermod -u $LOCAL_USER_ID aztec-dev &> /dev/null
+[ -n "$LOCAL_GROUP_ID" ] && groupmod -g $LOCAL_GROUP_ID aztec-dev
+[ -n "$LOCAL_USER_ID" ] && usermod -u $LOCAL_USER_ID aztec-dev &> /dev/null
 
 # Find the group id of the docker socket, add aztec-dev to that group, or create the group and add aztec-dev.
 SOCKET_GID=$(stat -c %g /var/run/docker.sock)
--- build-images/run.sh
+++ build-images/run.sh
@@ -7,11 +7,14 @@ if [ ! -d "$HOME/.devbox" ]; then
   cp -R ./home $HOME/.devbox
 fi
 
+if [[ "$OSTYPE" == "linux"* ]]; then
+  ID_ARGS="-e LOCAL_USER_ID=$(id -u) -e LOCAL_GROUP_ID=$(id -g)"
+fi
+
 docker run \
   -ti --rm \
   --hostname devbox \
-  -e LOCAL_USER_ID=$(id -u) \
-  -e LOCAL_GROUP_ID=$(id -g) \
+  ${ID_ARGS:-} \
   -v$HOME/.devbox:/home/aztec-dev \
   -v$HOME/.ssh:/home/aztec-dev/.ssh:ro \
   -v/var/run/docker.sock:/var/run/docker.sock \
