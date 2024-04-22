2d3a95f66de8c88538480f8f1bb1a75223490f90
sklppy88 <esau@aztecprotocol.com>
2024-04-22 20:58:38 +0000

    asdf

--- yarn-project/circuit-types/src/keys/index.ts
+++ yarn-project/circuit-types/src/keys/index.ts
@@ -1,3 +1,2 @@
 export * from './key_pair.js';
 export * from './key_store.js';
-export * from './address_book.js';
--- yarn-project/key-store/src/index.ts
+++ yarn-project/key-store/src/index.ts
@@ -1,2 +1,1 @@
 export * from './test_key_store.js';
-export * from './address_book.js';
