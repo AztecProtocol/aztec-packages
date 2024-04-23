570896498f564d00347cc746d48e7542190e2598
sklppy88 <esau@aztecprotocol.com>
2024-04-23 17:10:12 +0000

    remake consts

--- l1-contracts/src/core/libraries/ConstantsGen.sol
+++ l1-contracts/src/core/libraries/ConstantsGen.sol
@@ -95,7 +95,7 @@ library Constants {
   uint256 internal constant DEFAULT_MAX_FEE_PER_GAS = 10;
   uint256 internal constant DEFAULT_INCLUSION_FEE = 0;
   uint256 internal constant CANONICAL_KEY_REGISTRY_ADDRESS =
-    0x0fde809aaeb2210ca367381dfa9a46fb4542d67bcb27538e8de72151ae6d545c;
+    0x18f5db899cb197c08a3246d1318f395278f1beaf5dd08e0ded51482eed93ce7a;
   uint256 internal constant AZTEC_ADDRESS_LENGTH = 1;
   uint256 internal constant GAS_FEES_LENGTH = 3;
   uint256 internal constant GAS_LENGTH = 3;
