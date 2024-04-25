aef5b930d402be1d36c0c01ecd8e87abb7b67c73
benesjan <janbenes1234@gmail.com>
2024-04-25 08:18:38 +0000

    PackedValues cleanup

--- yarn-project/circuit-types/src/packed_values.ts
+++ yarn-project/circuit-types/src/packed_values.ts
@@ -1,7 +1,6 @@
 import { Fr, Vector } from '@aztec/circuits.js';
 import { computeVarArgsHash } from '@aztec/circuits.js/hash';
 import { BufferReader, serializeToBuffer } from '@aztec/foundation/serialize';
-import { type FieldsOf } from '@aztec/foundation/types';
 
 /**
  * Packs a set of values into a hash.
@@ -17,14 +16,6 @@ export class PackedValues {
      */
     public readonly hash: Fr,
   ) {}
-
-  static getFields(fields: FieldsOf<PackedValues>) {
-    return [fields.values, fields.hash] as const;
-  }
-
-  static from(fields: FieldsOf<PackedValues>): PackedValues {
-    return new PackedValues(...PackedValues.getFields(fields));
-  }
 
   static fromValues(values: Fr[]) {
     return new PackedValues(values, computeVarArgsHash(values));
