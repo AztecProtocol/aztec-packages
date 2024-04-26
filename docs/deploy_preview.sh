65aac8f57749b092e80286d8c0d990c0a6d18562
just-mitch <mitchell@aztecprotocol.com>
2024-04-26 14:13:44 +0000

    clean up comments scripts

--- docs/deploy_preview.sh
+++ docs/deploy_preview.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-PR_URL="$1"
+PR_NUMBER=$1
 AZTEC_BOT_COMMENTER_GITHUB_TOKEN="$2"
 
-API_URL="${PR_URL/github.com/api.github.com/repos}"
-API_URL="${API_URL/pull/pulls}"
-API_URL="${API_URL}/files"
+API_URL="https://api.github.com/repos/AztecProtocol/aztec-packages/pulls/${PR_NUMBER}/files"
 
 echo "API URL: $API_URL"
 
@@ -24,8 +22,8 @@ fi
 
 # Regular deploy if the argument is not "master" and docs changed
 DEPLOY_OUTPUT=$(yarn netlify deploy --site aztec-docs-dev)
-UNIQUE_DEPLOY_URL=$(echo "$DEPLOY_OUTPUT" | grep -E "https://.*aztec-docs-dev.netlify.app" | awk '{print $4}')
-echo "Unique deploy URL: $UNIQUE_DEPLOY_URL"
+DOCS_PREVIEW_URL=$(echo "$DEPLOY_OUTPUT" | grep -E "https://.*aztec-docs-dev.netlify.app" | awk '{print $4}')
+echo "Unique deploy URL: $DOCS_PREVIEW_URL"
 
 cd ../yarn-project/scripts
-UNIQUE_DEPLOY_URL=$UNIQUE_DEPLOY_URL yarn docs-preview-comment
+AZTEC_BOT_COMMENTER_GITHUB_TOKEN=$AZTEC_BOT_COMMENTER_GITHUB_TOKEN PR_NUMBER=$PR_NUMBER DOCS_PREVIEW_URL=$DOCS_PREVIEW_URL yarn docs-preview-comment 
