e3cef7ef53fc4f6d58329326ccf80d635f88a65c
Alex Gherghisan <alexg@aztecprotocol.com>
2024-05-01 09:30:51 +0000

    feat: run noir-packages-test in Earthly

--- noir/scripts/bootstrap_packages.sh
+++ noir/scripts/bootstrap_packages.sh
@@ -9,7 +9,8 @@ cd $ROOT/noir-repo
 # Set build data manually.
 export SOURCE_DATE_EPOCH=$(date +%s)
 export GIT_DIRTY=false
-export GIT_COMMIT=${COMMIT_HASH:-$(git rev-parse --verify HEAD)}
+export COMMIT_HASH=${COMMIT_HASH:-$(git rev-parse --verify HEAD)}
+export GIT_COMMIT=${GIT_COMMIT:-$COMMIT_HASH}
 
 PROJECTS=(
   @noir-lang/acvm_js
