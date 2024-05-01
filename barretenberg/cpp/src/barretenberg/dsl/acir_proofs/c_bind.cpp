c39f6d5e6f6138342d3987695eba68229f0bc11e
lucasxia01 <lucasxia01@gmail.com>
2024-05-01 19:00:52 +0000

    fixed proof deserialization

--- barretenberg/cpp/src/barretenberg/dsl/acir_proofs/c_bind.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_proofs/c_bind.cpp
@@ -210,7 +210,7 @@ WASM_EXPORT void acir_verify_ultra_honk(uint8_t const* proof_buf, uint8_t const*
     using VerifierCommitmentKey = bb::VerifierCommitmentKey<curve::BN254>;
     using Verifier = UltraVerifier_<UltraFlavor>;
 
-    auto proof = from_buffer<std::vector<bb::fr>>(proof_buf);
+    auto proof = from_buffer<std::vector<bb::fr>>(from_buffer<std::vector<uint8_t>>(proof_buf));
     auto verification_key = std::make_shared<VerificationKey>(from_buffer<VerificationKey>(vk_buf));
     verification_key->pcs_verification_key = std::make_shared<VerifierCommitmentKey>();
 
