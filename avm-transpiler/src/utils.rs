de20d566eca9e74bb23010443307b0897c928970
vezenovm <mvezenov@gmail.com>
2024-05-01 21:21:01 +0000

    fixup avm-transpiler opcode checks

--- avm-transpiler/src/utils.rs
+++ avm-transpiler/src/utils.rs
@@ -15,17 +15,33 @@ pub fn extract_brillig_from_acir_program(program: &Program) -> &[BrilligOpcode] 
     assert_eq!(
         program.functions.len(),
         1,
-        "An AVM program should have only a single ACIR function flagged as 'BrilligCall'"
+        "An AVM program should have only a single ACIR function with a 'BrilligCall'"
     );
-    let opcodes = &program.functions[0].opcodes;
-    assert_eq!(
-        opcodes.len(),
-        1,
-        "An AVM program should have only a single ACIR function flagged as 'BrilligCall'"
-    );
+    let main_function = &program.functions[0];
+    let opcodes = &main_function.opcodes;
     match opcodes[0] {
-        Opcode::BrilligCall { .. } => {}
+        Opcode::BrilligCall { id, .. } => assert_eq!(id, 0, "The ID of the `BrilligCall` must be 0 as we have a single `Brillig` function"),
         _ => panic!("Tried to extract a Brillig program from its ACIR wrapper opcode, but the opcode doesn't contain Brillig!"),
+    }
+    // Noir currently defaults to making a witness for each return witness in order
+    // to match the return types serialization. The current way the compiler does this is by
+    // generating a constraint for each return witness, which is what is checked below.
+    // We should not expect any other opcodes for transpilation.
+    if opcodes.len() > 1 {
+        let remaining_opcodes = &opcodes[1..];
+        assert_eq!(remaining_opcodes.len(), main_function.return_values.0.len());
+        for (opcode, witness) in opcodes[1..]
+            .iter()
+            .zip(main_function.return_values.0.iter())
+        {
+            match opcode {
+                Opcode::AssertZero(expr) => {
+                    assert_eq!(expr.linear_combinations[1].1, *witness, "Expected the witness being set by the expression to match the return values");
+                }
+                // Reference this issue (https://github.com/noir-lang/noir/issues/4914) to understand what the Noir compiler is doing
+                _ => panic!("We only expect a simple arithmetic expressions to generate unique return witnesses"),
+            }
+        }
     }
     assert_eq!(
         program.unconstrained_functions.len(),
