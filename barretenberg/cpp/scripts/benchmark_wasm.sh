25336f789c1ad878692d0dd736d98c7fcbe0ac09
Cody Gunton <codygunton@gmail.com>
2024-05-02 16:15:41 +0000

    Align benchmark_wasm.sh interface with benchmark.sh

--- barretenberg/cpp/scripts/benchmark_wasm.sh
+++ barretenberg/cpp/scripts/benchmark_wasm.sh
@@ -2,17 +2,19 @@
 set -eu
 
 BENCHMARK=${1:-goblin_bench}
-COMMAND=${2:-./bin/$BENCHMARK}
+COMMAND=${2:-./$BENCHMARK}
+PRESET=${3:-wasm-threads}
+BUILD_DIR=${4:-build-wasm-threads}
 HARDWARE_CONCURRENCY=${HARDWARE_CONCURRENCY:-16}
 
 # Move above script dir.
 cd $(dirname $0)/..
 
 # Configure and build.
-cmake --preset wasm-threads
-cmake --build --preset wasm-threads --target $BENCHMARK
+cmake --preset $PRESET
+cmake --build --preset $PRESET --target $BENCHMARK
 
-cd build-wasm-threads
+cd $BUILD_DIR
 # Consistency with _wasm.sh targets / shorter $COMMAND.
 cp ./bin/$BENCHMARK .
 wasmtime run --env HARDWARE_CONCURRENCY=$HARDWARE_CONCURRENCY -Wthreads=y -Sthreads=y --dir=.. $COMMAND
