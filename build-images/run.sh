1270827b2c37d23b2078948bb320f6e0a21ff4ee
Charlie Lye <karl.lye@gmail.com>
2024-05-06 13:40:41 +0000

    docker works

--- build-images/run.sh
+++ build-images/run.sh
@@ -10,24 +10,24 @@ if [[ "$OSTYPE" == "linux"* ]]; then
   ID_ARGS="-e LOCAL_USER_ID=$(id -u) -e LOCAL_GROUP_ID=$(id -g)"
 fi
 
-arch=$(uname -m)
-case $arch in
-  x86_64)
-    arch="amd64"
-    ;;
-  aarch64)
-    arch="arm64"
-    ;;
-esac
-
-docker run \
-  -ti --rm \
-  --hostname devbox \
-  -e SSH_CONNECTION=' ' \
-  ${ID_ARGS:-} \
-  -w/workspaces/aztec-packages \
-  -v$PWD/..:/workspaces/aztec-packages \
-  -vdevbox-home:/home/aztec-dev \
-  -v$HOME/.ssh/id_rsa:/home/aztec-dev/.ssh/id_rsa:ro \
-  -v/var/run/docker.sock:/var/run/docker.sock \
-  aztecprotocol/devbox:1.0-$arch
+if docker ps -a --format '{{.Names}}' | grep -q '^aztec-devbox$'; then
+  # Container already exists. Exec into a new shell.
+  docker exec -ti --user aztec-dev aztec-devbox /bin/zsh
+else
+  # We override the docker config dir to ensure we don't conflict with vscodes dev-container.
+  # They share the same home dir, but vscode will add some credentials config that it needs to its docker config.
+  docker run \
+    -ti --rm \
+    --name aztec-devbox \
+    --hostname devbox \
+    -e SSH_CONNECTION=' ' \
+    -e DOCKER_CONFIG=/home/aztec-dev/.docker-devbox \
+    ${ID_ARGS:-} \
+    -w/workspaces/aztec-packages \
+    -v$PWD/..:/workspaces/aztec-packages \
+    -vdevbox-home:/home/aztec-dev \
+    -vdevbox-var-lib-docker:/var/lib/docker \
+    -v$HOME/.ssh/id_rsa:/home/aztec-dev/.ssh/id_rsa:ro \
+    --privileged \
+    aztecprotocol/devbox:1.0
+fi
