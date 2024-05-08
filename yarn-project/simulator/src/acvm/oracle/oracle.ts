f8f27a74a799945c257d18dfe4831c21cdd5bf7a
sklppy88 <esau@aztecprotocol.com>
2024-05-08 22:35:42 +0000

    fix

--- yarn-project/simulator/src/acvm/oracle/oracle.ts
+++ yarn-project/simulator/src/acvm/oracle/oracle.ts
@@ -1,5 +1,5 @@
 import { MerkleTreeId, UnencryptedL2Log } from '@aztec/circuit-types';
-import { type PartialAddress, acvmFieldMessageToString, oracleDebugCallToFormattedStr } from '@aztec/circuits.js';
+import { type PartialAddress, acvmFieldMessageToString, oracleDebugCallToFormattedStr, PublicKeys } from '@aztec/circuits.js';
 import { EventSelector, FunctionSelector } from '@aztec/foundation/abi';
 import { AztecAddress } from '@aztec/foundation/aztec-address';
 import { Fr, Point } from '@aztec/foundation/fields';
@@ -192,7 +192,7 @@ export class Oracle {
   }
 
   async getPublicKeysAndPartialAddress([address]: ACVMField[]): Promise<ACVMField[]> {
-    let publicKeys: Point[] | undefined;
+    let publicKeys: PublicKeys;
     let partialAddress: PartialAddress;
 
     // TODO #5834: This should be reworked to return the public keys as well
@@ -205,10 +205,20 @@ export class Oracle {
     try {
       publicKeys = await this.typedOracle.getPublicKeysForAddress(AztecAddress.fromField(fromACVMField(address)));
     } catch (err) {
-      publicKeys = Array(4).fill(Point.ZERO);
+      publicKeys = {
+        masterNullifierPublicKey: Point.ZERO,
+        masterIncomingViewingPublicKey: Point.ZERO,
+        masterOutgoingViewingPublicKey: Point.ZERO,
+        masterTaggingPublicKey: Point.ZERO,
+      };
     }
 
-    const acvmPublicKeys = publicKeys.flatMap(key => key.toFields());
+    const acvmPublicKeys = [
+      publicKeys.masterNullifierPublicKey.toFields(),
+      publicKeys.masterIncomingViewingPublicKey.toFields(),
+      publicKeys.masterOutgoingViewingPublicKey.toFields(),
+      publicKeys.masterTaggingPublicKey.toFields(),
+    ].flat();
 
     return [...acvmPublicKeys, partialAddress].map(toACVMField);
   }
