4bb42b821cc0bf980ea18446096949d07030d54d
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-05-08 18:40:26 +0000

    feat: add cpp constants gen to calculate kernel offsets

--- barretenberg/cpp/src/barretenberg/vm/avm_trace/constants.hpp
+++ barretenberg/cpp/src/barretenberg/vm/avm_trace/constants.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include "aztec_constants.hpp"
 #include <cstdint>
 
 // NOTE(MD): for now we will only include the public inputs that are included in call_context
 // With more being added in subsequent prs
-inline const std::size_t KERNEL_INPUTS_LENGTH = 18;
+// KERNEL_INPUTS_LENGTH = CALL_CONTEXT_LENGTH +
+inline const std::size_t KERNEL_INPUTS_LENGTH = PUBLIC_CONTEXT_INPUTS_LENGTH;
