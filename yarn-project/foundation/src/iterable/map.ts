42aa1d0d52e08a56e660236752d60315c35af16b
spypsy <spypsy@outlook.com>
2024-05-09 11:34:24 +0000

    add map doc

--- yarn-project/foundation/src/iterable/map.ts
+++ yarn-project/foundation/src/iterable/map.ts
@@ -3,13 +3,11 @@ import { peek } from './peek.js';
 
 /**
  * Takes an (async) iterable and returns one with each item mapped by the passed
- * function
+ * function.
+ * @param source - The iterable to run the map function on.
+ * @param func - The function to run over the iterable's items.
+ * @returns A generator of the mapped items.
  */
-function map<I, O>(
-  source: Iterable<I>,
-  func: (val: I, index: number) => Promise<O>,
-): AsyncGenerator<O, void, undefined>;
-function map<I, O>(source: Iterable<I>, func: (val: I, index: number) => O): Generator<O, void, undefined>;
 function map<I, O>(
   source: AsyncIterable<I> | Iterable<I>,
   func: (val: I, index: number) => O | Promise<O>,
