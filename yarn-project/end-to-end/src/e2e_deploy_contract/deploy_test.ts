28084d6a3cac70c06306170cfbe224311b48fc29
benesjan <janbenes1234@gmail.com>
2024-05-09 15:36:10 +0000

    cleanup

--- yarn-project/end-to-end/src/e2e_deploy_contract/deploy_test.ts
+++ yarn-project/end-to-end/src/e2e_deploy_contract/deploy_test.ts
@@ -7,7 +7,7 @@ import {
   type ContractArtifact,
   type ContractBase,
   type DebugLogger,
-  type Fr,
+  Fr,
   type PXE,
   type Wallet,
   createDebugLogger,
@@ -81,10 +81,8 @@ export class DeployTest {
   }
 
   async registerRandomAccount(): Promise<AztecAddress> {
-    const pxe = this.pxe;
-    const { completeAddress: owner, secretKey } = CompleteAddress.fromRandomSecretKey();
-    await pxe.registerAccount(secretKey, owner.partialAddress);
-    return owner.address;
+    const completeAddress = await this.pxe.registerAccount(Fr.random(), Fr.random());
+    return completeAddress.address;
   }
 }
 
