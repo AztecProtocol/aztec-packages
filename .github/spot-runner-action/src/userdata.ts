e6337d7540ba1c07635f2fd97206e167a6544cd2
ludamad <domuradical@gmail.com>
2024-05-09 17:45:06 +0000

    pass on workflow call

--- .github/spot-runner-action/src/userdata.ts
+++ .github/spot-runner-action/src/userdata.ts
@@ -15,6 +15,7 @@ export class UserData {
       `shutdown -P +${this.config.ec2InstanceTtl}`,
       `echo '{"default-address-pools":[{"base":"172.17.0.0/12","size":20}, {"base":"10.99.0.0/12","size":20}, {"base":"192.168.0.0/16","size":24}]}' > /etc/docker/daemon.json`,
       `service docker restart`,
+      "sudo apt install -y brotli",
       "sudo wget -q https://github.com/earthly/earthly/releases/latest/download/earthly-linux-$(dpkg --print-architecture) -O /usr/local/bin/earthly",
       "sudo chmod +x /usr/local/bin/earthly",
       "earthly bootstrap",
@@ -54,6 +55,7 @@ export class UserData {
       "sudo chmod +x /usr/local/bin/earthly",
       "earthly bootstrap",
       "cd /run",
+      "sudo apt install -y brotli",
       `mkdir -p shutdown-refcount`,
       // Shutdown rules:
       // - github actions job starts and ends always bump +ec2InstanceTtl minutes
