957c1f520b9bada1d3d9b1eb2897ea0b2fdb4139
ludamad <domuradical@gmail.com>
2024-05-10 22:32:54 +0000

    runners as ubuntu

--- .github/spot-runner-action/src/userdata.ts
+++ .github/spot-runner-action/src/userdata.ts
@@ -46,6 +46,32 @@ export class UserData {
     // Note, we dont make the runner ephemeral as we start fresh runners as needed
     // and delay shutdowns whenever jobs start
     // TODO could deregister runners right before shutdown starts
+    const setupRunnerCmds = [
+      // Shutdown rules:
+      // - github actions job starts and ends always bump +ec2InstanceTtl minutes
+      // - when the amount of started jobs (start_run_* files) equal the amount of finished jobs (end_run_* files), we shutdown in 5 minutes (with a reaper script installed later)
+      `echo "${bumpShutdown}" > ~/delay_shutdown.sh`,
+      "chmod +x ~/delay_shutdown.sh",
+      "export ACTIONS_RUNNER_HOOK_JOB_STARTED=~/delay_shutdown.sh",
+      "export ACTIONS_RUNNER_HOOK_JOB_COMPLETED=~/delay_shutdown.sh",
+      "mkdir -p actions-runner && cd actions-runner",
+      'echo "ACTIONS_RUNNER_HOOK_JOB_STARTED=~/delay_shutdown.sh" > .env',
+      'echo "ACTIONS_RUNNER_HOOK_JOB_COMPLETED=~/delay_shutdown.sh" > .env',
+      `GH_RUNNER_VERSION=${githubActionRunnerVersion}`,
+      'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
+      "curl -O -L https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-${RUNNER_ARCH}-${GH_RUNNER_VERSION}.tar.gz",
+      "tar xzf ./actions-runner-linux-${RUNNER_ARCH}-${GH_RUNNER_VERSION}.tar.gz",
+      "mv externals ..", // we share the big binaries between all the runner folders, symlink instead of copy them
+      // Note sharing bin doesn't work due to using it as a folder, and we don't bother splitting up sharing bin
+      "rm ./actions-runner-linux-${RUNNER_ARCH}-${GH_RUNNER_VERSION}.tar.gz", // cleanup as we will copy our runner folder
+      '[ -n "$(command -v yum)" ] && yum install libicu -y',
+      `TOKENS=(${tokensSpaceSep}) ; echo ${tokensSpaceSep} > ~/github-runner-tokens`, // for debugging failed attempts
+      `for i in {0..${this.config.githubActionRunnerConcurrency - 1}}; do`,
+      `  ( cp -r . ../${runnerNameBase}-$i && ln -s $(pwd)/../externals ../${runnerNameBase}-$i && cd ../${runnerNameBase}-$i && echo \${TOKENS[i]} > .runner-token && ./config.sh --unattended --url https://github.com/${github.context.repo.owner}/${github.context.repo.repo} --token \${TOKENS[i]} --labels ${this.config.githubActionRunnerLabel} --replace --name ${runnerNameBase}-$i ; ./run.sh ) &`,
+      "done",
+      "wait", // Wait for all background processes to finish
+    ];
+    const runners = Buffer.from(setupRunnerCmds.join("\n")).toString("base64");
     const cmds = [
       "#!/bin/bash",
       `exec 1>/run/log.out 2>&1`, // Log to /run/log.out
@@ -58,30 +84,7 @@ export class UserData {
       "sudo apt install -y brotli",
       'echo "MaxStartups 1000" >> /etc/ssh/sshd_config',
       "sudo service sshd restart",
-      // Shutdown rules:
-      // - github actions job starts and ends always bump +ec2InstanceTtl minutes
-      // - when the amount of started jobs (start_run_* files) equal the amount of finished jobs (end_run_* files), we shutdown in 5 minutes (with a reaper script installed later)
-      `echo "${bumpShutdown}" > /run/delay_shutdown.sh`,
-      "chmod +x /run/delay_shutdown.sh",
-      "export ACTIONS_RUNNER_HOOK_JOB_STARTED=/run/delay_shutdown.sh",
-      "export ACTIONS_RUNNER_HOOK_JOB_COMPLETED=/run/delay_shutdown.sh",
-      "mkdir -p actions-runner && cd actions-runner",
-      'echo "ACTIONS_RUNNER_HOOK_JOB_STARTED=/run/delay_shutdown.sh" > .env',
-      'echo "ACTIONS_RUNNER_HOOK_JOB_COMPLETED=/run/delay_shutdown.sh" > .env',
-      `GH_RUNNER_VERSION=${githubActionRunnerVersion}`,
-      'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
-      "curl -O -L https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-${RUNNER_ARCH}-${GH_RUNNER_VERSION}.tar.gz",
-      "tar xzf ./actions-runner-linux-${RUNNER_ARCH}-${GH_RUNNER_VERSION}.tar.gz",
-      "export RUNNER_ALLOW_RUNASROOT=1",
-      "mv externals ..", // we share the big binaries between all the runner folders, symlink instead of copy them
-      // Note sharing bin doesn't work due to using it as a folder, and we don't bother splitting up sharing bin
-      "rm ./actions-runner-linux-${RUNNER_ARCH}-${GH_RUNNER_VERSION}.tar.gz", // cleanup as we will copy our runner folder
-      '[ -n "$(command -v yum)" ] && yum install libicu -y',
-      `TOKENS=(${tokensSpaceSep}) ; echo ${tokensSpaceSep} > /run/github-runner-tokens`, // for debugging failed attempts
-      `for i in {0..${this.config.githubActionRunnerConcurrency - 1}}; do`,
-      `  ( cp -r . ../${runnerNameBase}-$i && ln -s $(pwd)/../externals ../${runnerNameBase}-$i && cd ../${runnerNameBase}-$i && echo \${TOKENS[i]} > .runner-token && ./config.sh --unattended --url https://github.com/${github.context.repo.owner}/${github.context.repo.repo} --token \${TOKENS[i]} --labels ${this.config.githubActionRunnerLabel} --replace --name ${runnerNameBase}-$i ; ./run.sh ) &`,
-      "done",
-      "wait", // Wait for all background processes to finish
+      `echo ${runners} | base64 --decode > /run/setup-runners.sh && sudo -u ubuntu bash /run/setup-runners.sh`,
     ];
     console.log(
       "Sending: ",
