9eba222f4d1423ce4bfb132598f2e8490c14d42f
ludamad <domuradical@gmail.com>
2024-05-11 00:45:11 +0000

    fix

--- .github/spot-runner-action/src/userdata.ts
+++ .github/spot-runner-action/src/userdata.ts
@@ -20,6 +20,7 @@ export class UserData {
       // NOTE also update versions below and in .github/ci-setup-action/action.yml
       "sudo wget -q https://github.com/earthly/earthly/releases/download/v0.8.9/earthly-linux-$(dpkg --print-architecture) -O /usr/local/bin/earthly",
       "sudo chmod +x /usr/local/bin/earthly",
+      "touch /home/ubuntu/.user-data-finished",
     ];
     console.log(
       "Sending: ",
@@ -50,6 +51,7 @@ export class UserData {
       // Shutdown rules:
       // - github actions job starts and ends always bump +ec2InstanceTtl minutes
       // - when the amount of started jobs (start_run_* files) equal the amount of finished jobs (end_run_* files), we shutdown in 5 minutes (with a reaper script installed later)
+      "touch /home/ubuntu/.user-data-started",
       `cd ~`,
       `echo "${bumpShutdown}" > /home/ubuntu/delay_shutdown.sh`,
       "chmod +x /home/ubuntu/delay_shutdown.sh",
@@ -75,6 +77,7 @@ export class UserData {
     const cmds = [
       "#!/bin/bash",
       `exec 1>/run/log.out 2>&1`, // Log to /run/log.out
+      "touch /home/ubuntu/.user-data-started",
       `shutdown -P +${this.config.ec2InstanceTtl}`,
       `echo '{"default-address-pools":[{"base":"172.17.0.0/12","size":20}, {"base":"10.99.0.0/12","size":20}, {"base":"192.168.0.0/16","size":24}]}' > /etc/docker/daemon.json`,
       `sudo service docker restart`,
@@ -86,6 +89,7 @@ export class UserData {
       "sudo service sshd restart",
       `echo ${runners} | base64 --decode > /run/setup-runners.sh`,
       `sudo -u ubuntu bash /run/setup-runners.sh`,
+      "touch /home/ubuntu/.user-data-finished",
     ];
     console.log(
       "Sending: ",
