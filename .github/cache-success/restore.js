21f20bbfc98185d9981fb8ce8ba83ee4af21febf
ludamad <domuradical@gmail.com>
2024-05-12 15:19:09 +0000

    try slim node

--- .github/cache-success/restore.js
+++ .github/cache-success/restore.js
@@ -2,25 +2,21 @@ const cache = require("@actions/cache");
 const fs = require("fs");
 
 async function main() {
-    try {
-      const cacheKey = await cache.restoreCache(["success.txt"], process.env.INPUT_SUCCESS_KEY);
+  const cacheKey = await cache.restoreCache(["success.txt"], process.env.INPUT_SUCCESS_KEY);
 
-      if (cacheKey) {
-        // Cache was found and restored
-        core.exportVariable("cache-success", "true");
-        core.info("Cache hit occurred, file restored.");
+  if (cacheKey) {
+    // Cache was found and restored
+    core.exportVariable("cache-success", "true");
+    core.info("Cache hit occurred, file restored.");
 
-        // Optionally, read and log the success file content
-        const successData = fs.readFileSync("success.txt", "utf8");
-        core.info(`NOTE: Skipping due to success from this run: ${successData}`);
-      } else {
-        // No cache found
-        core.exportVariable("cache-success", "false");
-        core.info("No cache hit occurred.");
-      }
-    } catch (error) {
-      core.setFailed(`An error occurred while restoring the cache: ${error}`);
-    }
+    // Optionally, read and log the success file content
+    const successData = fs.readFileSync("success.txt", "utf8");
+    core.info(`NOTE: Skipping due to success from this run: ${successData}`);
+  } else {
+    // No cache found
+    core.exportVariable("cache-success", "false");
+    core.info("No cache hit occurred.");
+  }
 }
 
 main();
