36b2124161d73ab79b8d99d1d3ebfcd3daa323c9
ludamad <domuradical@gmail.com>
2024-05-12 15:21:01 +0000

    fix save cache

--- .github/cache-success/save.js
+++ .github/cache-success/save.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 async function main() {
   const successData = `https://github.com/AztecProtocol/aztec-packages/actions/runs/${process.env.RUN_ID}`;
   fs.writeFileSync("success.txt", successData);
-  await cache.saveState(["success.txt"], process.env.INPUT_SUCCESS_KEY);
+  await cache.saveCache(["success.txt"], process.env.INPUT_SUCCESS_KEY);
 }
 
 main();
