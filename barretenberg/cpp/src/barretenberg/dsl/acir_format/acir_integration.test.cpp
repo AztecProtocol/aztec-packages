aaf1550c7be94cb0430451c1bd677d2549314703
ledwards2225 <l.edwards.d@gmail.com>
2024-05-20 22:07:33 +0000

    updates to inegration tests, client ivc test failing

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/acir_integration.test.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/acir_integration.test.cpp
@@ -4,6 +4,7 @@
 
 #include "acir_format.hpp"
 #include "barretenberg/bb/exec_pipe.hpp"
+#include "barretenberg/client_ivc/client_ivc.hpp"
 #include "barretenberg/common/streams.hpp"
 #include "barretenberg/dsl/acir_format/acir_to_constraint_buf.hpp"
 
@@ -47,7 +48,7 @@ class AcirIntegrationTests : public ::testing::Test {
         return program_stack.back();
     }
 
-    template <class Flavor> bool prove_and_verify_honk(Flavor::CircuitBuilder& builder)
+    template <class Flavor> bool prove_and_verify_honk(Flavor::CircuitBuilder builder)
     {
         using Prover = UltraProver_<Flavor>;
         using Verifier = UltraVerifier_<Flavor>;
@@ -67,8 +68,28 @@ class AcirIntegrationTests : public ::testing::Test {
         return verifier.verify_proof(proof);
     }
 
+    template <class Flavor> bool prove_and_verify_honk(std::shared_ptr<ProverInstance_<Flavor>> prover_instance)
+    {
+        using Prover = UltraProver_<Flavor>;
+        using Verifier = UltraVerifier_<Flavor>;
+        using VerificationKey = Flavor::VerificationKey;
+
+        Prover prover{ prover_instance };
+        auto proof = prover.construct_proof();
+
+        // Verify Honk proof
+        auto verification_key = std::make_shared<VerificationKey>(prover.instance->proving_key);
+        Verifier verifier{ verification_key };
+
+        return verifier.verify_proof(proof);
+    }
+
   protected:
-    static void SetUpTestSuite() { srs::init_crs_factory("../srs_db/ignition"); }
+    static void SetUpTestSuite()
+    {
+        srs::init_crs_factory("../srs_db/ignition");
+        srs::init_grumpkin_crs_factory("../srs_db/grumpkin");
+    }
 };
 
 TEST_F(AcirIntegrationTests, ProveAndVerifyProgram)
@@ -80,10 +101,10 @@ TEST_F(AcirIntegrationTests, ProveAndVerifyProgram)
     auto acir_program = get_program_data_from_test_file(test_name);
 
     // Construct a bberg circuit from the acir representation
-    auto builder = acir_format::create_circuit<Builder>(acir_program.constraints, 0, acir_program.witness);
+    auto circuit = acir_format::create_circuit<Builder>(acir_program.constraints, 0, acir_program.witness);
 
     // Construct and verify Honk proof
-    EXPECT_TRUE(prove_and_verify_honk<Flavor>(builder));
+    EXPECT_TRUE(prove_and_verify_honk<Flavor>(circuit));
 }
 
 TEST_F(AcirIntegrationTests, ProveAndVerifyProgramStack)
@@ -98,11 +119,52 @@ TEST_F(AcirIntegrationTests, ProveAndVerifyProgramStack)
         auto program = program_stack.back();
 
         // Construct a bberg circuit from the acir representation
-        auto builder = acir_format::create_circuit<Builder>(program.constraints, 0, program.witness);
+        auto circuit = acir_format::create_circuit<Builder>(program.constraints, 0, program.witness);
 
         // Construct and verify Honk proof for the individidual circuit
-        EXPECT_TRUE(prove_and_verify_honk<Flavor>(builder));
+        EXPECT_TRUE(prove_and_verify_honk<Flavor>(circuit));
 
         program_stack.pop_back();
     }
+}
+
+TEST_F(AcirIntegrationTests, FoldAndVerifyProgramStack)
+{
+    using Flavor = GoblinUltraFlavor;
+    using Builder = Flavor::CircuitBuilder;
+
+    std::string test_name = "fold_basic";
+    auto program_stack = get_program_stack_data_from_test_file(test_name);
+
+    ClientIVC ivc;
+    ivc.structured_flag = true;
+
+    while (!program_stack.empty()) {
+        info("Program:");
+        auto program = program_stack.back();
+
+        // Construct a bberg circuit from the acir representation
+        auto circuit =
+            acir_format::create_circuit<Builder>(program.constraints, 0, program.witness, false, ivc.goblin.op_queue);
+
+        ivc.accumulate(circuit);
+
+        info("Check");
+        CircuitChecker::check(circuit);
+
+        info("P & V");
+        EXPECT_TRUE(prove_and_verify_honk<Flavor>(ivc.prover_instance));
+
+        info("num gates = ", circuit.get_num_gates());
+        info("circuit size = ", ivc.prover_instance->proving_key.circuit_size);
+
+        program_stack.pop_back();
+    }
+
+    // EXPECT_TRUE(ivc.prove_and_verify());
+
+    // auto proof = ivc.prove();
+
+    // auto verifier_inst = std::make_shared<VerifierInstance>(ivc.instance_vk);
+    // return ivc.verify(proof, { ivc.verifier_accumulator, verifier_inst });
 }
