61977f61f78e2e4e53c6ac75a3531c665d270e8e
signorecello <github@zpedro.dev>
2024-05-20 15:26:28 +0000

    modifying the rest of the links, they should be all relative now

--- docs/scripts/rewrite_paths.js
+++ docs/scripts/rewrite_paths.js
@@ -10,27 +10,36 @@ function rewrite(markdown, fp) {
   // Get the absolute path of the file
   const filePath = path.resolve(fp);
   const fileDir = path.dirname(filePath);
-  const fileDirRelative = path.relative("docs", fileDir);
 
   console.log("------");
-  console.log("FILE: ", fileDirRelative);
+  console.log("FILE: ", fileDir);
 
   // Go through each link in the markdown file
   for (let l of absolutePaths) {
+    if (l.startsWith("](/img")) break;
     const originalLink = l.slice(2); // Remove the initial "]( /"
     const linkPath = path.resolve(path.join("docs", originalLink)); // Resolve to the docs directory
 
     console.log("Link: ", originalLink);
 
     // Calculate the relative path from fileDir to linkPath
-    const relativePath = path.relative(fileDir, linkPath);
+    let relativePath = path.relative(fileDir, linkPath);
+    if (
+      !fs.existsSync(linkPath + ".md") &&
+      !fs.existsSync(linkPath + ".mdx") &&
+      fs.statSync(linkPath).isDirectory()
+    ) {
+      relativePath += "/index.md";
+    } else {
+      relativePath += ".md";
+    }
 
     console.log("Transformed link: ", relativePath);
 
     // Replace the absolute path with the relative path in the markdown
     markdown = markdown.replace(`(${originalLink}`, `(${relativePath}`);
 
-    // fs.writeFileSync(fp, markdown);
+    fs.writeFileSync(fp, markdown);
   }
   console.log("\n");
 }
@@ -54,4 +63,4 @@ async function iterate(dir) {
   }
 }
 
-iterate("docs/aztec");
+iterate("docs");
