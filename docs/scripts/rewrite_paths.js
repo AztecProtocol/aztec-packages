bd79f3e609ab52d4f7ae910a595e3e528e1bbae3
signorecello <github@zpedro.dev>
2024-05-20 12:14:00 +0000

    re-wrote aztec/ links

--- docs/scripts/rewrite_paths.js
+++ docs/scripts/rewrite_paths.js
@@ -3,52 +3,52 @@ const path = require("path");
 
 function rewrite(markdown, fp) {
   const absolutePaths = markdown.match(
-    /]\(\/([a-zA-Z0-9_-]+\/)*[a-zA-Z0-9_-]+/g
+    /\]\(\/([a-zA-Z0-9_-]+\/)*[a-zA-Z0-9_-]+/g
   );
   if (!absolutePaths) return;
 
-  // this is the dissection of path of the file we're looking at
-  const filePath = fp.slice(0, -3).split("/").slice(1);
+  // Get the absolute path of the file
+  const filePath = path.resolve(fp);
+  const fileDir = path.dirname(filePath);
+  const fileDirRelative = path.relative("docs", fileDir);
+
   console.log("------");
-  console.log("FILE: ", filePath.join("/"));
+  console.log("FILE: ", fileDirRelative);
 
-  // we're going through each link in there
+  // Go through each link in the markdown file
   for (let l of absolutePaths) {
-    const originalLink = l.split("/").slice(1);
-    const link = l.split("/").slice(1);
-    console.log("Link: ", originalLink.join("/"));
+    const originalLink = l.slice(2); // Remove the initial "]( /"
+    const linkPath = path.resolve(path.join("docs", originalLink)); // Resolve to the docs directory
 
-    for (let i = 0; i < filePath.length; i++) {
-      if (filePath[i] === originalLink[i]) {
-        link.shift();
-      } else {
-        link.unshift("..");
-      }
-    }
+    console.log("Link: ", originalLink);
 
-    console.log("Transformed link:", link.join("/"));
-    // markdown = markdown.replace(l, `](${link.join("/")}`);
-    // console.log(markdown);
+    // Calculate the relative path from fileDir to linkPath
+    const relativePath = path.relative(fileDir, linkPath);
+
+    console.log("Transformed link: ", relativePath);
+
+    // Replace the absolute path with the relative path in the markdown
+    markdown = markdown.replace(`(${originalLink}`, `(${relativePath}`);
+
+    // fs.writeFileSync(fp, markdown);
   }
   console.log("\n");
 }
 
 async function iterate(dir) {
   const files = fs.readdirSync(dir);
-  const contentUpdates = [];
 
   for (const file of files) {
     const filepath = path.join(dir, file);
     const stat = fs.statSync(filepath);
 
     if (stat.isDirectory()) {
-      contentUpdates.push(iterate(filepath));
+      await iterate(filepath);
     } else if (
       stat.isFile() &&
       (file.endsWith(".md") || file.endsWith(".mdx"))
     ) {
       const markdownContent = fs.readFileSync(filepath, "utf-8");
-
       rewrite(markdownContent, filepath);
     }
   }
