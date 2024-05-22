8b0ac4826adc8a102bceb4a85f857b1de44aff14
ludamad <domuradical@gmail.com>
2024-05-22 23:39:12 +0000

    fix

--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/plookup_tables/plookup_tables.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/plookup_tables/plookup_tables.hpp
@@ -1,20 +1,6 @@
 #pragma once
 #include "barretenberg/common/throw_or_abort.hpp"
-
-#include "./fixed_base/fixed_base.hpp"
-#include "aes128.hpp"
-#include "blake2s.hpp"
-#include "dummy.hpp"
-#include "keccak/keccak_chi.hpp"
-#include "keccak/keccak_input.hpp"
-#include "keccak/keccak_output.hpp"
-#include "keccak/keccak_rho.hpp"
-#include "keccak/keccak_theta.hpp"
-#include "non_native_group_generator.hpp"
-#include "sha256.hpp"
-#include "sparse.hpp"
-#include "types.hpp"
-#include "uint.hpp"
+#include "barretenberg/stdlib_circuit_builders/plookup_tables/types.hpp"
 
 namespace bb::plookup {
 
@@ -25,5 +11,5 @@ ReadData<bb::fr> get_lookup_accumulators(MultiTableId id,
                                          const bb::fr& key_b = 0,
                                          bool is_2_to_1_lookup = false);
 
-BasicTable create_basic_table(const BasicTableId id, const size_t index);
+BasicTable create_basic_table(BasicTableId id, size_t index);
 } // namespace bb::plookup
