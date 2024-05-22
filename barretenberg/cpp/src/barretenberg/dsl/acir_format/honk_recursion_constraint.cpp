f3110849f89e0ee7128ca4ed327c670b42fcf901
lucasxia01 <lucasxia01@gmail.com>
2024-05-22 20:50:22 +0000

    aggregation, but failed (because biggroup elements being added can't have same x value?)

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/honk_recursion_constraint.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/honk_recursion_constraint.cpp
@@ -11,6 +11,23 @@ namespace acir_format {
 using namespace bb;
 using namespace bb::stdlib::recursion::honk;
 
+std::array<bn254::Group, 2> agg_points_from_witness_indicies(
+    Builder& builder, const std::array<uint32_t, HonkRecursionConstraint::AGGREGATION_OBJECT_SIZE>& obj_witness_indices)
+{
+    std::array<bn254::BaseField, 4> aggregation_elements;
+    for (size_t i = 0; i < 4; ++i) {
+        aggregation_elements[i] =
+            bn254::BaseField(field_ct::from_witness_index(&builder, obj_witness_indices[4 * i]),
+                             field_ct::from_witness_index(&builder, obj_witness_indices[4 * i + 1]),
+                             field_ct::from_witness_index(&builder, obj_witness_indices[4 * i + 2]),
+                             field_ct::from_witness_index(&builder, obj_witness_indices[4 * i + 3]));
+        aggregation_elements[i].assert_is_in_field();
+    }
+
+    return { bn254::Group(aggregation_elements[0], aggregation_elements[1]),
+             bn254::Group(aggregation_elements[2], aggregation_elements[3]) };
+}
+
 /**
  * @brief Add constraints required to recursively verify an UltraPlonk proof
  *
@@ -37,39 +54,40 @@ std::array<uint32_t, HonkRecursionConstraint::AGGREGATION_OBJECT_SIZE> create_ho
 
     static_cast<void>(has_valid_witness_assignments);
     static_cast<void>(nested_aggregation_object);
-    // const auto& nested_aggregation_indices = nested_aggregation_object;
-    // const bool inner_proof_contains_recursive_proof = true;
+    // std::array<bn254::Group, 2> nested_aggregation_points =
+    //     agg_points_from_witness_indicies(builder, nested_aggregation_object);
 
     // Construct an in-circuit representation of the verification key.
     // For now, the v-key is a circuit constant and is fixed for the circuit.
     // (We may need a separate recursion opcode for this to vary, or add more config witnesses to this opcode)
     const auto& aggregation_input = input_aggregation_object;
-    aggregation_state_ct previous_aggregation;
+    aggregation_state_ct cur_aggregation_object;
+    // cur_aggregation_object.P0 = nested_aggregation_points[0];
+    // cur_aggregation_object.P1 = nested_aggregation_points[1];
+    // cur_aggregation_object.has_data = true;
 
-    // If we have previously recursively verified proofs, `inner_aggregation_object_nonzero = true`
+    // If we have previously recursively verified proofs, `previous_aggregation_object_nonzero = true`
     // For now this is a complile-time constant i.e. whether this is true/false is fixed for the circuit!
-    bool inner_aggregation_indices_all_zero = true;
+    bool previous_aggregation_indices_all_zero = true;
     for (const auto& idx : aggregation_input) {
-        inner_aggregation_indices_all_zero &= (idx == 0);
+        previous_aggregation_indices_all_zero &= (idx == 0);
     }
 
-    if (!inner_aggregation_indices_all_zero) {
-        std::array<bn254::BaseField, 4> aggregation_elements;
-        for (size_t i = 0; i < 4; ++i) {
-            aggregation_elements[i] =
-                bn254::BaseField(field_ct::from_witness_index(&builder, aggregation_input[4 * i]),
-                                 field_ct::from_witness_index(&builder, aggregation_input[4 * i + 1]),
-                                 field_ct::from_witness_index(&builder, aggregation_input[4 * i + 2]),
-                                 field_ct::from_witness_index(&builder, aggregation_input[4 * i + 3]));
-            aggregation_elements[i].assert_is_in_field();
-        }
+    // Aggregate the aggregation object if it exists. It exists if we have previously verified proofs, i.e. if this is
+    // not the first recursion constraint.
+    if (!previous_aggregation_indices_all_zero) {
+        std::array<bn254::Group, 2> inner_agg_points = agg_points_from_witness_indicies(builder, aggregation_input);
         // If we have a previous aggregation object, assign it to `previous_aggregation` so that it is included
         // in stdlib::recursion::verify_proof
-        previous_aggregation.P0 = bn254::Group(aggregation_elements[0], aggregation_elements[1]);
-        previous_aggregation.P1 = bn254::Group(aggregation_elements[2], aggregation_elements[3]);
-        previous_aggregation.has_data = true;
+        // cur_aggregation_object.P0 += inner_agg_points[0]; // TODO: use a recursion separator
+        // cur_aggregation_object.P1 += inner_agg_points[1];
+        cur_aggregation_object.P0 = inner_agg_points[0];
+        cur_aggregation_object.P1 = inner_agg_points[1];
+        UltraFlavor::VerifierCommitmentKey pcs_verification_key;
+        ASSERT(pcs_verification_key.pairing_check(inner_agg_points[0].get_value(), inner_agg_points[1].get_value()));
+        cur_aggregation_object.has_data = true;
     } else {
-        previous_aggregation.has_data = false;
+        cur_aggregation_object.has_data = false;
     }
 
     std::vector<field_ct> key_fields;
@@ -96,27 +114,46 @@ std::array<uint32_t, HonkRecursionConstraint::AGGREGATION_OBJECT_SIZE> create_ho
     auto vkey = std::make_shared<RecursiveVerificationKey>(builder, key_fields);
     RecursiveVerifier verifier(&builder, vkey);
     std::array<typename Flavor::GroupElement, 2> pairing_points = verifier.verify_proof(proof_fields);
+    UltraFlavor::VerifierCommitmentKey pcs_verification_key;
+    ASSERT(pcs_verification_key.pairing_check(pairing_points[0].get_value(), pairing_points[1].get_value()));
+
+    // aggregate with the input aggregation object
+    if (cur_aggregation_object.has_data) {
+        info("agg points: ", cur_aggregation_object.P0.get_value(), cur_aggregation_object.P1.get_value());
+        info("pairing points: ", pairing_points[0].get_value(), pairing_points[1].get_value());
+        ASSERT(pcs_verification_key.pairing_check(cur_aggregation_object.P0.get_value(),
+                                                  cur_aggregation_object.P1.get_value()));
+        ASSERT(pcs_verification_key.pairing_check(pairing_points[0].get_value(), pairing_points[1].get_value()));
+        cur_aggregation_object.P0 += pairing_points[0];
+        cur_aggregation_object.P1 += pairing_points[1];
+        ASSERT(pcs_verification_key.pairing_check(cur_aggregation_object.P0.get_value(),
+                                                  cur_aggregation_object.P1.get_value()));
+    } else {
+        cur_aggregation_object.P0 = pairing_points[0];
+        cur_aggregation_object.P1 = pairing_points[1];
+        cur_aggregation_object.has_data = true;
+    }
 
     std::vector<uint32_t> proof_witness_indices = {
-        pairing_points[0].x.binary_basis_limbs[0].element.normalize().witness_index,
-        pairing_points[0].x.binary_basis_limbs[1].element.normalize().witness_index,
-        pairing_points[0].x.binary_basis_limbs[2].element.normalize().witness_index,
-        pairing_points[0].x.binary_basis_limbs[3].element.normalize().witness_index,
-        pairing_points[0].y.binary_basis_limbs[0].element.normalize().witness_index,
-        pairing_points[0].y.binary_basis_limbs[1].element.normalize().witness_index,
-        pairing_points[0].y.binary_basis_limbs[2].element.normalize().witness_index,
-        pairing_points[0].y.binary_basis_limbs[3].element.normalize().witness_index,
-        pairing_points[1].x.binary_basis_limbs[0].element.normalize().witness_index,
-        pairing_points[1].x.binary_basis_limbs[1].element.normalize().witness_index,
-        pairing_points[1].x.binary_basis_limbs[2].element.normalize().witness_index,
-        pairing_points[1].x.binary_basis_limbs[3].element.normalize().witness_index,
-        pairing_points[1].y.binary_basis_limbs[0].element.normalize().witness_index,
-        pairing_points[1].y.binary_basis_limbs[1].element.normalize().witness_index,
-        pairing_points[1].y.binary_basis_limbs[2].element.normalize().witness_index,
-        pairing_points[1].y.binary_basis_limbs[3].element.normalize().witness_index,
+        cur_aggregation_object.P0.x.binary_basis_limbs[0].element.normalize().witness_index,
+        cur_aggregation_object.P0.x.binary_basis_limbs[1].element.normalize().witness_index,
+        cur_aggregation_object.P0.x.binary_basis_limbs[2].element.normalize().witness_index,
+        cur_aggregation_object.P0.x.binary_basis_limbs[3].element.normalize().witness_index,
+        cur_aggregation_object.P0.y.binary_basis_limbs[0].element.normalize().witness_index,
+        cur_aggregation_object.P0.y.binary_basis_limbs[1].element.normalize().witness_index,
+        cur_aggregation_object.P0.y.binary_basis_limbs[2].element.normalize().witness_index,
+        cur_aggregation_object.P0.y.binary_basis_limbs[3].element.normalize().witness_index,
+        cur_aggregation_object.P1.x.binary_basis_limbs[0].element.normalize().witness_index,
+        cur_aggregation_object.P1.x.binary_basis_limbs[1].element.normalize().witness_index,
+        cur_aggregation_object.P1.x.binary_basis_limbs[2].element.normalize().witness_index,
+        cur_aggregation_object.P1.x.binary_basis_limbs[3].element.normalize().witness_index,
+        cur_aggregation_object.P1.y.binary_basis_limbs[0].element.normalize().witness_index,
+        cur_aggregation_object.P1.y.binary_basis_limbs[1].element.normalize().witness_index,
+        cur_aggregation_object.P1.y.binary_basis_limbs[2].element.normalize().witness_index,
+        cur_aggregation_object.P1.y.binary_basis_limbs[3].element.normalize().witness_index,
     };
-    auto result = aggregation_state_ct{ pairing_points[0], pairing_points[1], {}, proof_witness_indices, true };
-
+    auto result = cur_aggregation_object;
+    result.proof_witness_indices = proof_witness_indices;
     // ASSERT(result.public_inputs.size() == input.public_inputs.size());
 
     // Assign the `public_input` field to the public input of the inner proof
@@ -132,55 +169,6 @@ std::array<uint32_t, HonkRecursionConstraint::AGGREGATION_OBJECT_SIZE> create_ho
               resulting_output_aggregation_object.begin());
 
     return resulting_output_aggregation_object;
-}
-
-/**
- * @brief When recursively verifying proofs, we represent the verification key using field elements.
- *        This method exports the key formatted in the manner our recursive verifier expects.
- *        A dummy key is used when building a circuit without a valid witness assignment.
- *        We want the transcript to contain valid G1 points to prevent on-curve errors being thrown.
- *        We want a non-zero circuit size as this element will be inverted by the circuit
- *        and we do not want an "inverting 0" error thrown
- *
- * @return std::vector<bb::fr>
- */
-std::vector<bb::fr> export_dummy_honk_key_in_recursion_format(const PolynomialManifest& polynomial_manifest,
-                                                              const bool contains_recursive_proof)
-{
-    std::vector<fr> output;
-    output.emplace_back(1); // domain.domain (will be inverted)
-    output.emplace_back(1); // domain.root (will be inverted)
-    output.emplace_back(1); // domain.generator (will be inverted)
-
-    output.emplace_back(1); // circuit size
-    output.emplace_back(1); // num public inputs
-
-    output.emplace_back(contains_recursive_proof); // contains_recursive_proof
-    for (size_t i = 0; i < HonkRecursionConstraint::AGGREGATION_OBJECT_SIZE; ++i) {
-        output.emplace_back(0); // recursive_proof_public_input_indices
-    }
-
-    for (const auto& descriptor : polynomial_manifest.get()) {
-        if (descriptor.source == PolynomialSource::SELECTOR || descriptor.source == PolynomialSource::PERMUTATION) {
-            // the std::biggroup class creates unsatisfiable constraints when identical points are added/subtracted.
-            // (when verifying zk proofs this is acceptable as we make sure verification key points are not identical.
-            // And prover points should contain randomness for an honest Prover).
-            // This check can also trigger a runtime error due to causing 0 to be inverted.
-            // When creating dummy verification key points we must be mindful of the above and make sure that each
-            // transcript point is unique.
-            auto scalar = bb::fr::random_element();
-            const auto element = bb::g1::affine_element(bb::g1::one * scalar);
-            auto g1_as_fields = export_g1_affine_element_as_fields(element);
-            output.emplace_back(g1_as_fields.x_lo);
-            output.emplace_back(g1_as_fields.x_hi);
-            output.emplace_back(g1_as_fields.y_lo);
-            output.emplace_back(g1_as_fields.y_hi);
-        }
-    }
-
-    output.emplace_back(0); // key_hash
-
-    return output;
 }
 
 } // namespace acir_format
