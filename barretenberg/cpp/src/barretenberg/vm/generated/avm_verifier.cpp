b67d14f39f166b51a6e519a6980e31b638f86be7
jeanmon <jean@aztecprotocol.com>
2024-05-24 09:39:23 +0000

    6542: fix proving with public inputs

--- barretenberg/cpp/src/barretenberg/vm/generated/avm_verifier.cpp
+++ barretenberg/cpp/src/barretenberg/vm/generated/avm_verifier.cpp
@@ -36,7 +36,11 @@ using FF = AvmFlavor::FF;
     // TODO(https://github.com/AztecProtocol/aztec-packages/issues/6361): we pad the points to the circuit size in order
     // to get the correct evaluation. This is not efficient, and will not be valid in production.
     std::vector<FF> new_points(circuit_size, 0);
-    std::copy(points.begin(), points.end(), new_points.data());
+
+    // We need to shift the points by one to match the public inputs column in the circuit.
+    // Namely, the latter one is prepended with an extra first row to support shifted polynomials.
+    ASSERT(circuit_size > points.size());
+    std::copy(points.begin(), points.end(), new_points.data() + 1);
 
     Polynomial<FF> polynomial(new_points);
     return polynomial.evaluate_mle(challenges);
