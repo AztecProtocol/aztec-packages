47c624a9d74f938f9a3092d1681da927857588e6
codygunton <codygunton@gmail.com>
2024-05-28 22:37:28 +0000

    Test stubbed

--- barretenberg/cpp/src/barretenberg/stdlib/primitives/bigfield/bigfield.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib/primitives/bigfield/bigfield.hpp
@@ -245,6 +245,14 @@ template <typename Builder, typename T> class bigfield {
 
     bigfield sqr() const;
     bigfield sqradd(const std::vector<bigfield>& to_add) const;
+    /**
+     * @brief compute 'this ** exponent'
+     * @details The exponent is implicity range constrained to 32 bits.
+     *
+     * @param exponent A 32-bit witness
+     * @return bigfield
+     */
+    bigfield pow(const field_t<Builder>& exponent) const;
     bigfield madd(const bigfield& to_mul, const std::vector<bigfield>& to_add) const;
 
     static void perform_reductions_for_mult_madd(std::vector<bigfield>& mul_left,
