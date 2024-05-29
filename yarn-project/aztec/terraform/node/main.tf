98f57bc8f8a85d2998735846cea669205d67c90b
spypsy <spypsy@outlook.com>
2024-05-29 09:32:20 +0000

    use only subnet1 in node ecs service

--- yarn-project/aztec/terraform/node/main.tf
+++ yarn-project/aztec/terraform/node/main.tf
@@ -363,8 +363,7 @@ resource "aws_ecs_service" "aztec-node" {
 
   network_configuration {
     subnets = [
-      data.terraform_remote_state.setup_iac.outputs.subnet_az1_private_id,
-      data.terraform_remote_state.setup_iac.outputs.subnet_az2_private_id
+      data.terraform_remote_state.setup_iac.outputs.subnet_az1_private_id
     ]
     security_groups = [data.terraform_remote_state.aztec-network_iac.outputs.p2p_security_group_id, data.terraform_remote_state.setup_iac.outputs.security_group_private_id]
   }
