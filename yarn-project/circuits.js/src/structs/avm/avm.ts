f24b7670e9c4948df1fb02f8da34ea7a4fc6d97f
dbanks12 <david@aztecprotocol.com>
2024-05-31 17:01:52 +0000

    feat(avm): plumb execution hints from TS to AVM prover

--- yarn-project/circuits.js/src/structs/avm/avm.ts
+++ yarn-project/circuits.js/src/structs/avm/avm.ts
@@ -1,10 +1,85 @@
 import { type Fr } from '@aztec/foundation/fields';
-import { serializeToBuffer } from '@aztec/foundation/serialize';
+import { serializeArrayOfBufferableToVector, serializeToBuffer } from '@aztec/foundation/serialize';
 import { type FieldsOf } from '@aztec/foundation/types';
 
-// TODO(https://github.com/AztecProtocol/aztec-packages/issues/6774): add public inputs.
+import { type PublicCircuitPublicInputs } from '../public_circuit_public_inputs.js';
+
+export class AvmHint {
+  constructor(public readonly key: Fr, public readonly value: Fr) {}
+
+  /**
+   * Serializes the inputs to a buffer.
+   * @returns - The inputs serialized to a buffer.
+   */
+  toBuffer() {
+    return serializeToBuffer(...AvmHint.getFields(this));
+  }
+
+  /**
+   * Serializes the inputs to a hex string.
+   * @returns The instance serialized to a hex string.
+   */
+  toString() {
+    return this.toBuffer().toString('hex');
+  }
+
+  /**
+   * Extracts fields from an instance.
+   * @param fields - Fields to create the instance from.
+   * @returns An array of fields.
+   */
+  static getFields(fields: FieldsOf<AvmHint>) {
+    return [fields.key, fields.value] as const;
+  }
+}
+
+export class AvmExecutionHints {
+  constructor(
+    public readonly storageValues: AvmHint[],
+    public readonly noteHashExists: AvmHint[],
+    public readonly nullifierExists: AvmHint[],
+    public readonly l1ToL2MessageExists: AvmHint[],
+  ) {}
+
+  /**
+   * Serializes the inputs to a buffer.
+   * @returns - The inputs serialized to a buffer.
+   */
+  toBuffer() {
+    // Flatten to a single array before serializing
+    // Need to use serializeArrayOfBufferableToVector here to include array length in serialization
+    return serializeArrayOfBufferableToVector(AvmExecutionHints.getFields(this).flat());
+  }
+
+  /**
+   * Serializes the inputs to a hex string.
+   * @returns The instance serialized to a hex string.
+   */
+  toString() {
+    return this.toBuffer().toString('hex');
+  }
+
+  /**
+   * Extracts fields from an instance.
+   * @param fields - Fields to create the instance from.
+   * @returns An array of fields.
+   */
+  static getFields(fields: FieldsOf<AvmExecutionHints>) {
+    return [fields.storageValues, fields.noteHashExists, fields.nullifierExists, fields.l1ToL2MessageExists] as const;
+  }
+
+  static empty() {
+    return new AvmExecutionHints([], [], [], []);
+  }
+}
+
 export class AvmCircuitInputs {
-  constructor(public readonly bytecode: Buffer, public readonly calldata: Fr[]) {}
+  constructor(
+    public readonly bytecode: Buffer,
+    public readonly calldata: Fr[],
+    public readonly publicInputs: PublicCircuitPublicInputs,
+    public readonly avmHints: AvmExecutionHints,
+  ) {}
 
   /**
    * Serializes the inputs to a buffer.
@@ -37,6 +112,6 @@ export class AvmCircuitInputs {
    * @returns An array of fields.
    */
   static getFields(fields: FieldsOf<AvmCircuitInputs>) {
-    return [fields.bytecode, fields.calldata] as const;
+    return [fields.bytecode, fields.calldata, fields.publicInputs, fields.avmHints] as const;
   }
 }
