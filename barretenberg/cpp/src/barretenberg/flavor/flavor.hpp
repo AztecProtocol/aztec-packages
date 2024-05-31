ec8052078a09ad0e8355f092fea4f492ee92d909
lucasxia01 <lucasxia01@gmail.com>
2024-05-31 17:14:45 +0000

    fix build by initializing field

--- barretenberg/cpp/src/barretenberg/flavor/flavor.hpp
+++ barretenberg/cpp/src/barretenberg/flavor/flavor.hpp
@@ -184,7 +184,7 @@ class VerificationKey_ : public PrecomputedCommitments {
   public:
     std::shared_ptr<VerifierCommitmentKey> pcs_verification_key;
     bool contains_recursive_proof = false;
-    AggregationObjectPubInputIndices recursive_proof_public_input_indices;
+    AggregationObjectPubInputIndices recursive_proof_public_input_indices = {};
     uint64_t pub_inputs_offset = 0;
 
     VerificationKey_() = default;
