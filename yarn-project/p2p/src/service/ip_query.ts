9e8eff88c5cd261ee975e62fe14118543d112b4a
spypsy <spypsy@outlook.com>
2024-06-03 13:43:52 +0000

    use fetch

--- yarn-project/p2p/src/service/ip_query.ts
+++ yarn-project/p2p/src/service/ip_query.ts
@@ -1,21 +1,5 @@
-import * as https from 'https';
-
-export function getPublicIp(): Promise<string> {
-  return new Promise((resolve, reject) => {
-    https
-      .get('https://checkip.amazonaws.com', res => {
-        let data = '';
-
-        res.on('data', chunk => {
-          data += chunk;
-        });
-
-        res.on('end', () => {
-          resolve(data.trim());
-        });
-      })
-      .on('error', err => {
-        reject(`Error fetching public IP: ${err.message}`);
-      });
-  });
+export async function getPublicIp(): Promise<string> {
+  const resp = await fetch('http://checkip.amazonaws.com/');
+  const text = await resp.text();
+  return text.trim();
 }
