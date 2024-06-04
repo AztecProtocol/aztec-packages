c94d32eaa7f6aa00c77ff6567d49e16d3a6c8e7e
thunkar <gregojquiros@gmail.com>
2024-06-04 09:31:38 +0000

    more oracle calls

--- yarn-project/txe/src/txe_service/txe_service.ts
+++ yarn-project/txe/src/txe_service/txe_service.ts
@@ -9,20 +9,22 @@ import {
   type CompleteAddress,
   type Header,
   type KeyValidationRequest,
-  PUBLIC_DATA_TREE_HEIGHT,
+  type PUBLIC_DATA_TREE_HEIGHT,
   type PrivateCallStackItem,
   type PublicCallRequest,
-  PublicDataTreeLeafPreimage,
+  type PublicDataTreeLeafPreimage,
 } from '@aztec/circuits.js';
+import { Aes128 } from '@aztec/circuits.js/barretenberg';
 import { type FunctionSelector } from '@aztec/foundation/abi';
 import { AztecAddress } from '@aztec/foundation/aztec-address';
 import { Fr, type Point } from '@aztec/foundation/fields';
-import { type Logger } from '@aztec/foundation/log';
+import { type Logger, applyStringFormatting } from '@aztec/foundation/log';
 import { type AztecKVStore } from '@aztec/kv-store';
 import { openTmpStore } from '@aztec/kv-store/utils';
 import {
   type MessageLoadOracleInputs,
   type NoteData,
+  PackedValuesCache,
   type TypedOracle,
   WorldStateDB,
   WorldStatePublicDB,
@@ -37,45 +39,60 @@ import {
   fromSingle,
   toArray,
   toForeignCallResult,
+  toSingle,
 } from '../util/encoding.js';
 
 export class TXE implements TypedOracle {
   private worldStatePublicDB: WorldStatePublicDB;
   private worldStateDB: WorldStateDB;
 
-  constructor(private logger: Logger, private trees: MerkleTreeOperations, private contractAddress: AztecAddress) {
+  constructor(
+    private logger: Logger,
+    private trees: MerkleTreeOperations,
+    private packedValuesCache: PackedValuesCache,
+    private contractAddress: AztecAddress,
+  ) {
     this.worldStatePublicDB = new WorldStatePublicDB(this.trees);
     this.worldStateDB = new WorldStateDB(this.trees);
+    this.packedValuesCache = packedValuesCache;
   }
 
   getRandomField() {
     return Fr.random();
   }
 
-  packArgumentsArray(_args: Fr[]): Promise<Fr> {
-    throw new Error('Method not implemented.');
+  packArgumentsArray(args: Fr[]): Promise<Fr> {
+    return Promise.resolve(this.packedValuesCache.pack(args));
   }
-  packReturns(_returns: Fr[]): Promise<Fr> {
-    throw new Error('Method not implemented.');
+
+  packReturns(returns: Fr[]): Promise<Fr> {
+    return Promise.resolve(this.packedValuesCache.pack(returns));
   }
-  unpackReturns(_returnsHash: Fr): Promise<Fr[]> {
-    throw new Error('Method not implemented.');
+
+  unpackReturns(returnsHash: Fr): Promise<Fr[]> {
+    return Promise.resolve(this.packedValuesCache.unpack(returnsHash));
   }
+
   getKeyValidationRequest(_pkMHash: Fr): Promise<KeyValidationRequest> {
     throw new Error('Method not implemented.');
   }
+
   getContractInstance(_address: AztecAddress): Promise<ContractInstance> {
     throw new Error('Method not implemented.');
   }
+
   getMembershipWitness(_blockNumber: number, _treeId: MerkleTreeId, _leafValue: Fr): Promise<Fr[] | undefined> {
     throw new Error('Method not implemented.');
   }
+
   getSiblingPath(_blockNumber: number, _treeId: MerkleTreeId, _leafIndex: Fr): Promise<Fr[]> {
     throw new Error('Method not implemented.');
   }
+
   getNullifierMembershipWitness(_blockNumber: number, _nullifier: Fr): Promise<NullifierMembershipWitness | undefined> {
     throw new Error('Method not implemented.');
   }
+
   async getPublicDataTreeWitness(_blockNumber: number, leafSlot: Fr): Promise<PublicDataWitness | undefined> {
     const committedDb = this.trees;
     const lowLeafResult = await committedDb.getPreviousValueIndex(MerkleTreeId.PUBLIC_DATA_TREE, leafSlot.toBigInt());
@@ -93,24 +110,30 @@ export class TXE implements TypedOracle {
       return new PublicDataWitness(lowLeafResult.index, preimage, path);
     }
   }
+
   getLowNullifierMembershipWitness(
     _blockNumber: number,
     _nullifier: Fr,
   ): Promise<NullifierMembershipWitness | undefined> {
     throw new Error('Method not implemented.');
   }
+
   getHeader(_blockNumber: number): Promise<Header | undefined> {
     throw new Error('Method not implemented.');
   }
+
   getCompleteAddress(_account: AztecAddress): Promise<CompleteAddress> {
     throw new Error('Method not implemented.');
   }
+
   getAuthWitness(_messageHash: Fr): Promise<Fr[] | undefined> {
     throw new Error('Method not implemented.');
   }
+
   popCapsule(): Promise<Fr[]> {
     throw new Error('Method not implemented.');
   }
+
   getNotes(
     _storageSlot: Fr,
     _numSelects: number,
@@ -129,15 +152,19 @@ export class TXE implements TypedOracle {
   ): Promise<NoteData[]> {
     throw new Error('Method not implemented.');
   }
+
   notifyCreatedNote(_storageSlot: Fr, _noteTypeId: Fr, _note: Fr[], _innerNoteHash: Fr, _counter: number): void {
     throw new Error('Method not implemented.');
   }
+
   notifyNullifiedNote(_innerNullifier: Fr, _innerNoteHash: Fr, _counter: number): Promise<void> {
     throw new Error('Method not implemented.');
   }
+
   checkNullifierExists(_innerNullifier: Fr): Promise<boolean> {
     throw new Error('Method not implemented.');
   }
+
   getL1ToL2MembershipWitness(
     _contractAddress: AztecAddress,
     _messageHash: Fr,
@@ -145,6 +172,7 @@ export class TXE implements TypedOracle {
   ): Promise<MessageLoadOracleInputs<16>> {
     throw new Error('Method not implemented.');
   }
+
   async storageRead(startStorageSlot: Fr, numberOfElements: number): Promise<Fr[]> {
     const values = [];
     for (let i = 0n; i < numberOfElements; i++) {
@@ -166,12 +194,15 @@ export class TXE implements TypedOracle {
       }),
     );
   }
+
   emitEncryptedLog(_contractAddress: AztecAddress, _randomness: Fr, _encryptedNote: Buffer, _counter: number): void {
     throw new Error('Method not implemented.');
   }
+
   emitEncryptedNoteLog(_noteHashCounter: number, _encryptedNote: Buffer, _counter: number): void {
     throw new Error('Method not implemented.');
   }
+
   computeEncryptedLog(
     _contractAddress: AztecAddress,
     _storageSlot: Fr,
@@ -182,12 +213,15 @@ export class TXE implements TypedOracle {
   ): Buffer {
     throw new Error('Method not implemented.');
   }
+
   emitUnencryptedLog(_log: UnencryptedL2Log, _counter: number): void {
     throw new Error('Method not implemented.');
   }
+
   emitContractClassUnencryptedLog(_log: UnencryptedL2Log, _counter: number): Fr {
     throw new Error('Method not implemented.');
   }
+
   callPrivateFunction(
     _targetContractAddress: AztecAddress,
     _functionSelector: FunctionSelector,
@@ -198,6 +232,7 @@ export class TXE implements TypedOracle {
   ): Promise<PrivateCallStackItem> {
     throw new Error('Method not implemented.');
   }
+
   callPublicFunction(
     _targetContractAddress: AztecAddress,
     _functionSelector: FunctionSelector,
@@ -208,6 +243,7 @@ export class TXE implements TypedOracle {
   ): Promise<Fr[]> {
     throw new Error('Method not implemented.');
   }
+
   enqueuePublicFunctionCall(
     _targetContractAddress: AztecAddress,
     _functionSelector: FunctionSelector,
@@ -218,6 +254,7 @@ export class TXE implements TypedOracle {
   ): Promise<PublicCallRequest> {
     throw new Error('Method not implemented.');
   }
+
   setPublicTeardownFunctionCall(
     _targetContractAddress: AztecAddress,
     _functionSelector: FunctionSelector,
@@ -228,22 +265,31 @@ export class TXE implements TypedOracle {
   ): Promise<PublicCallRequest> {
     throw new Error('Method not implemented.');
   }
-  aes128Encrypt(_input: Buffer, _initializationVector: Buffer, _key: Buffer): Buffer {
-    throw new Error('Method not implemented.');
+
+  aes128Encrypt(input: Buffer, initializationVector: Buffer, key: Buffer): Buffer {
+    const aes128 = new Aes128();
+    return aes128.encryptBufferCBC(input, initializationVector, key);
   }
-  debugLog(_message: string, _fields: Fr[]): void {
-    throw new Error('Method not implemented.');
+
+  debugLog(message: string, fields: Fr[]): void {
+    this.logger.verbose(`debug_log ${applyStringFormatting(message, fields)}`);
   }
 }
 
 export class TXEService {
-  constructor(private typedOracle: TypedOracle, private store: AztecKVStore, private contractAddress: AztecAddress) {}
+  constructor(
+    private typedOracle: TypedOracle,
+    private store: AztecKVStore,
+    private packedValuesCache: PackedValuesCache,
+    private contractAddress: AztecAddress,
+  ) {}
 
   static async init(logger: Logger, contractAddress = AztecAddress.random()) {
     const store = openTmpStore(true);
     const trees = await MerkleTrees.new(store, logger);
-    const txe = new TXE(logger, trees.asLatest(), contractAddress);
-    return new TXEService(txe, store, contractAddress);
+    const packedValuesCache = new PackedValuesCache();
+    const txe = new TXE(logger, trees.asLatest(), packedValuesCache, contractAddress);
+    return new TXEService(txe, store, packedValuesCache, contractAddress);
   }
 
   setContractAddress(address = AztecAddress.random()): AztecAddress {
@@ -253,6 +299,38 @@ export class TXEService {
 
   async reset() {
     await this.store.clear();
+    this.packedValuesCache = new PackedValuesCache();
+    return toForeignCallResult([]);
+  }
+
+  async packArgumentsArray(args: ForeignCallArray) {
+    const packed = await this.typedOracle.packArgumentsArray(fromArray(args));
+    return toForeignCallResult([toSingle(packed)]);
+  }
+
+  async packArguments(_length: ForeignCallSingle, values: ForeignCallArray) {
+    const packed = await this.typedOracle.packArgumentsArray(fromArray(values));
+    return toForeignCallResult([toSingle(packed)]);
+  }
+
+  // Since the argument is a slice, noir automatically adds a length field to oracle call.
+  async packReturns(_length: ForeignCallSingle, values: ForeignCallArray) {
+    const packed = await this.typedOracle.packReturns(fromArray(values));
+    return toForeignCallResult([toSingle(packed)]);
+  }
+
+  async unpackReturns(returnsHash: ForeignCallSingle) {
+    const unpacked = await this.typedOracle.unpackReturns(fromSingle(returnsHash));
+    return toForeignCallResult([toArray(unpacked)]);
+  }
+
+  // Since the argument is a slice, noir automatically adds a length field to oracle call.
+  debugLog(message: ForeignCallArray, _length: ForeignCallSingle, fields: ForeignCallArray) {
+    const messageStr = fromArray(message)
+      .map(field => String.fromCharCode(field.toNumber()))
+      .join('');
+    const fieldsFr = fromArray(fields);
+    this.typedOracle.debugLog(messageStr, fieldsFr);
     return toForeignCallResult([]);
   }
 
