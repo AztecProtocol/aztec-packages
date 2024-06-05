b972339fe907c802f34fd871d4d021cc5af24b40
Sarkoxed <sarkoxed2013@yandex.ru>
2024-06-05 11:24:09 +0000

    Introducing base class for symbolic circuits + shl/shr/rot circuit optimizations

--- barretenberg/cpp/src/barretenberg/smt_verification/circuit/subcircuits.cpp
+++ barretenberg/cpp/src/barretenberg/smt_verification/circuit/subcircuits.cpp
@@ -5,12 +5,18 @@ namespace smt_subcircuits {
 CircuitProps get_standard_range_constraint_circuit(size_t n)
 {
     bb::StandardCircuitBuilder builder = bb::StandardCircuitBuilder();
-    uint32_t a_idx = builder.add_variable(bb::fr(0));
+    uint32_t a_idx = builder.add_variable(bb::fr(0xabbba));
     builder.set_variable_name(a_idx, "a");
 
     size_t start_gate = builder.get_num_gates();
-    builder.decompose_into_base4_accumulators(a_idx, n);
+    auto accumulators = builder.decompose_into_base4_accumulators(a_idx, n);
     size_t num_gates = builder.get_num_gates() - start_gate;
+
+    // give names to accumulators for tests
+    size_t num_accs = accumulators.size();
+    for (size_t i = 0; i < num_accs - 1; i++) {
+        builder.set_variable_name(accumulators[i], "acc_" + std::to_string(i));
+    }
 
     CircuitSchema exported = unpack_from_buffer(builder.export_circuit());
 
@@ -20,7 +26,16 @@ CircuitProps get_standard_range_constraint_circuit(size_t n)
     // number of the gate that contains lhs
     size_t gate_number = num_gates - 1;
 
-    return { exported, start_gate, num_gates, { lhs_position }, { gate_number } };
+    std::vector<uint32_t> idxs = { lhs_position };
+    std::vector<size_t> gate_idxs = { gate_number };
+
+    // same thing for accumulators
+    for (size_t i = 0; i < num_accs - 1; i++) {
+        idxs.push_back(0);
+        gate_idxs.push_back(gate_number - 4 * i);
+    }
+
+    return { exported, start_gate, num_gates, idxs, gate_idxs };
 }
 
 CircuitProps get_standard_logic_circuit(size_t n, bool is_xor)
