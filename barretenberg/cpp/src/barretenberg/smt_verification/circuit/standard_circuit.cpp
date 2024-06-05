0dee9cd081dde28cf40b9cd6537f5ecf20ab2ac0
Sarkoxed <sarkoxed2013@yandex.ru>
2024-06-05 14:48:21 +0000

    Fixed the master problem

--- barretenberg/cpp/src/barretenberg/smt_verification/circuit/standard_circuit.cpp
+++ barretenberg/cpp/src/barretenberg/smt_verification/circuit/standard_circuit.cpp
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
 #include "standard_circuit.hpp"
-=======
-#include "circuit.hpp"
->>>>>>> master
 
 namespace smt_circuit {
 
 /**
-<<<<<<< HEAD
  * @brief Construct a new StandardCircuit object
-=======
- * @brief Construct a new Circuit::Circuit object
->>>>>>> master
  *
  * @param circuit_info CircuitShema object
  * @param solver pointer to the global solver
  * @param tag tag of the circuit. Empty by default.
  */
-<<<<<<< HEAD
 StandardCircuit::StandardCircuit(
     CircuitSchema& circuit_info, Solver* solver, TermType type, const std::string& tag, bool optimizations)
     : CircuitBase(circuit_info.vars_of_interest,
@@ -38,46 +29,11 @@ StandardCircuit::StandardCircuit(
     optimized[1] = false;
 
     // Perform all relaxations for gates or
-=======
-Circuit::Circuit(CircuitSchema& circuit_info, Solver* solver, TermType type, const std::string& tag, bool optimizations)
-    : variables(circuit_info.variables)
-    , public_inps(circuit_info.public_inps)
-    , variable_names(circuit_info.vars_of_interest)
-    , selectors(circuit_info.selectors)
-    , wires_idxs(circuit_info.wires)
-    , real_variable_index(circuit_info.real_variable_index)
-    , optimizations(optimizations)
-    , solver(solver)
-    , type(type)
-    , tag(tag)
-{
-    if (!this->tag.empty()) {
-        if (this->tag[0] != '_') {
-            this->tag = "_" + this->tag;
-        }
-    }
-
-    for (auto& x : variable_names) {
-        variable_names_inverse.insert({ x.second, x.first });
-    }
-
-    variable_names.insert({ 0, "zero" });
-    variable_names.insert({ 1, "one" });
-    variable_names_inverse.insert({ "zero", 0 });
-    variable_names_inverse.insert({ "one", 1 });
-    optimized.insert({ 0, false });
-    optimized.insert({ 1, false });
-
-    this->init();
-
-    // Perform all relaxation for gates or
->>>>>>> master
     // add gate in its normal state to solver
     size_t i = 0;
     while (i < this->get_num_gates()) {
         i = this->prepare_gates(i);
     }
-<<<<<<< HEAD
 }
 
 /**
@@ -184,40 +140,6 @@ size_t StandardCircuit::prepare_gates(size_t cursor)
         eq == 0;
     }
     return cursor + 1;
-=======
-
-    for (const auto& i : this->public_inps) {
-        this->symbolic_vars[this->real_variable_index[i]] == this->variables[i];
-    }
-}
-
-/**
- * Creates all the needed symbolic variables and constants
- * which are used in circuit.
- *
- */
-void Circuit::init()
-{
-    size_t num_vars = variables.size();
-    symbolic_vars.insert({ 0, STerm::Var("zero" + this->tag, this->solver, this->type) });
-    symbolic_vars.insert({ 1, STerm::Var("one" + this->tag, this->solver, this->type) });
-
-    for (uint32_t i = 2; i < num_vars; i++) {
-        uint32_t real_idx = this->real_variable_index[i];
-        if (this->symbolic_vars.contains(real_idx)) {
-            continue;
-        }
-
-        std::string name = variable_names.contains(real_idx) ? variable_names[real_idx] : "var_" + std::to_string(i);
-        name += this->tag;
-        symbolic_vars.insert({ real_idx, STerm::Var(name, this->solver, this->type) });
-
-        optimized.insert({ real_idx, true });
-    }
-
-    symbolic_vars[0] == bb::fr(0);
-    symbolic_vars[1] == bb::fr(1);
->>>>>>> master
 }
 
 /**
@@ -232,12 +154,8 @@ void Circuit::init()
  * @param q_c constant
  * @param w   witness index
  */
-<<<<<<< HEAD
 void StandardCircuit::handle_univariate_constraint(
     bb::fr q_m, bb::fr q_1, bb::fr q_2, bb::fr q_3, bb::fr q_c, uint32_t w)
-=======
-void Circuit::handle_univariate_constraint(bb::fr q_m, bb::fr q_1, bb::fr q_2, bb::fr q_3, bb::fr q_c, uint32_t w)
->>>>>>> master
 {
     bb::fr b = q_1 + q_2 + q_3;
 
@@ -278,11 +196,7 @@ void Circuit::handle_univariate_constraint(bb::fr q_m, bb::fr q_1, bb::fr q_2, b
  * @param cursor current position
  * @return next position or -1
  */
-<<<<<<< HEAD
 size_t StandardCircuit::handle_logic_constraint(size_t cursor)
-=======
-size_t Circuit::handle_logic_constraint(size_t cursor)
->>>>>>> master
 {
     // Initialize binary search. Logic gate can only accept even bit lengths
     // So we need to find a match among [1, 127] and then multiply the result by 2
@@ -297,12 +211,6 @@ size_t Circuit::handle_logic_constraint(size_t cursor)
     bool and_flag = true;
     // Indicates the logic operation(true - XOR, false - AND) if the match is found.
     bool logic_flag = true;
-<<<<<<< HEAD
-=======
-    CircuitProps xor_props;
-    CircuitProps and_props;
-
->>>>>>> master
     bool stop_flag = false;
 
     while (beg <= end) {
@@ -322,21 +230,13 @@ size_t Circuit::handle_logic_constraint(size_t cursor)
             this->cached_subcircuits[SubcircuitType::XOR].insert(
                 { mid * 2, get_standard_logic_circuit(mid * 2, true) });
         }
-<<<<<<< HEAD
         CircuitProps xor_props = this->cached_subcircuits[SubcircuitType::XOR][mid * 2];
-=======
-        xor_props = this->cached_subcircuits[SubcircuitType::XOR][mid * 2];
->>>>>>> master
 
         if (!this->cached_subcircuits[SubcircuitType::AND].contains(mid * 2)) {
             this->cached_subcircuits[SubcircuitType::AND].insert(
                 { mid * 2, get_standard_logic_circuit(mid * 2, false) });
         }
-<<<<<<< HEAD
         CircuitProps and_props = this->cached_subcircuits[SubcircuitType::AND][mid * 2];
-=======
-        and_props = this->cached_subcircuits[SubcircuitType::AND][mid * 2];
->>>>>>> master
 
         CircuitSchema xor_circuit = xor_props.circuit;
         CircuitSchema and_circuit = and_props.circuit;
@@ -347,13 +247,8 @@ size_t Circuit::handle_logic_constraint(size_t cursor)
             for (size_t j = 0; j < xor_props.num_gates; j++) {
                 // It is possible for gates to be equal but wires to be not, but I think it's very
                 // unlikely to happen
-<<<<<<< HEAD
                 xor_flag &= xor_circuit.selectors[0][j + xor_props.start_gate] == this->selectors[cursor + j];
                 and_flag &= and_circuit.selectors[0][j + and_props.start_gate] == this->selectors[cursor + j];
-=======
-                xor_flag &= xor_circuit.selectors[j + xor_props.start_gate] == this->selectors[cursor + j];
-                and_flag &= and_circuit.selectors[j + and_props.start_gate] == this->selectors[cursor + j];
->>>>>>> master
 
                 if (!xor_flag && !and_flag) {
                     // Won't match at any bit length
@@ -381,13 +276,8 @@ size_t Circuit::handle_logic_constraint(size_t cursor)
     // TODO(alex): Figure out if I need to create range constraint here too or it'll be
     // created anyway in any circuit
     if (res != static_cast<size_t>(-1)) {
-<<<<<<< HEAD
         CircuitProps xor_props = get_standard_logic_circuit(res, true);
         CircuitProps and_props = get_standard_logic_circuit(res, false);
-=======
-        xor_props = get_standard_logic_circuit(res, true);
-        and_props = get_standard_logic_circuit(res, false);
->>>>>>> master
 
         info("Logic constraint optimization: ", std::to_string(res), " bits. is_xor: ", logic_flag);
         size_t left_gate = xor_props.gate_idxs[0];
@@ -436,21 +326,13 @@ size_t Circuit::handle_logic_constraint(size_t cursor)
  * @param cursor current position
  * @return next position or -1
  */
-<<<<<<< HEAD
 size_t StandardCircuit::handle_range_constraint(size_t cursor)
-=======
-size_t Circuit::handle_range_constraint(size_t cursor)
->>>>>>> master
 {
     // Indicates that current bit length is a match
     bool range_flag = true;
     size_t mid = 0;
     auto res = static_cast<size_t>(-1);
 
-<<<<<<< HEAD
-=======
-    CircuitProps range_props;
->>>>>>> master
     // Range constraints differ depending on oddness of bit_length
     // That's why we need to handle these cases separately
     for (size_t odd = 0; odd < 2; odd++) {
@@ -477,11 +359,7 @@ size_t Circuit::handle_range_constraint(size_t cursor)
                 this->cached_subcircuits[SubcircuitType::RANGE].insert(
                     { 2 * mid + odd, get_standard_range_constraint_circuit(2 * mid + odd) });
             }
-<<<<<<< HEAD
             CircuitProps range_props = this->cached_subcircuits[SubcircuitType::RANGE][2 * mid + odd];
-=======
-            range_props = this->cached_subcircuits[SubcircuitType::RANGE][2 * mid + odd];
->>>>>>> master
             CircuitSchema range_circuit = range_props.circuit;
 
             range_flag = cursor + range_props.num_gates <= this->get_num_gates();
@@ -489,11 +367,7 @@ size_t Circuit::handle_range_constraint(size_t cursor)
                 for (size_t j = 0; j < range_props.num_gates; j++) {
                     // It is possible for gates to be equal but wires to be not, but I think it's very
                     // unlikely to happen
-<<<<<<< HEAD
                     range_flag &= range_circuit.selectors[0][j + range_props.start_gate] == this->selectors[cursor + j];
-=======
-                    range_flag &= range_circuit.selectors[j + range_props.start_gate] == this->selectors[cursor + j];
->>>>>>> master
 
                     if (!range_flag) {
                         // Won't match at any bit length
@@ -524,18 +398,13 @@ size_t Circuit::handle_range_constraint(size_t cursor)
 
     if (range_flag) {
         info("Range constraint optimization: ", std::to_string(res), " bits");
-<<<<<<< HEAD
         CircuitProps range_props = get_standard_range_constraint_circuit(res);
-=======
-        range_props = get_standard_range_constraint_circuit(res);
->>>>>>> master
 
         size_t left_gate = range_props.gate_idxs[0];
         uint32_t left_gate_idx = range_props.idxs[0];
         uint32_t left_idx = this->real_variable_index[this->wires_idxs[cursor + left_gate][left_gate_idx]];
 
         STerm left = this->symbolic_vars[left_idx];
-<<<<<<< HEAD
 
         // preserving shifted values
         // we need this because even right shifts do not create
@@ -553,8 +422,6 @@ size_t Circuit::handle_range_constraint(size_t cursor)
             this->symbolic_vars[acc_idx] = (left >> static_cast<uint32_t>(2 * j));
         }
 
-=======
->>>>>>> master
         left <= (bb::fr(2).pow(res) - 1);
 
         // You have to mark these arguments so they won't be optimized out
@@ -565,7 +432,6 @@ size_t Circuit::handle_range_constraint(size_t cursor)
 }
 
 /**
-<<<<<<< HEAD
  * @brief Relaxes shr constraints.
  * @details This function is needed when we use bitwise compatible
  * symbolic terms.
@@ -888,105 +754,6 @@ size_t StandardCircuit::handle_ror_constraint(size_t cursor)
         return cursor + ror_props.num_gates;
     }
     return res;
-=======
- * @brief Adds all the gate constraints to the solver.
- * Relaxes constraint system for non-ff solver engines
- * via removing subcircuits that were already proved being correct.
- *
- */
-size_t Circuit::prepare_gates(size_t cursor)
-{
-    // TODO(alex): implement bitvector class and compute offsets
-    if (this->type == TermType::BVTerm && this->optimizations) {
-        size_t res = handle_logic_constraint(cursor);
-        if (res != static_cast<size_t>(-1)) {
-            return res;
-        }
-    }
-
-    if ((this->type == TermType::BVTerm || this->type == TermType::FFITerm) && this->optimizations) {
-        size_t res = handle_range_constraint(cursor);
-        if (res != static_cast<size_t>(-1)) {
-            return res;
-        }
-    }
-
-    bb::fr q_m = this->selectors[cursor][0];
-    bb::fr q_1 = this->selectors[cursor][1];
-    bb::fr q_2 = this->selectors[cursor][2];
-    bb::fr q_3 = this->selectors[cursor][3];
-    bb::fr q_c = this->selectors[cursor][4];
-
-    uint32_t w_l = this->wires_idxs[cursor][0];
-    uint32_t w_r = this->wires_idxs[cursor][1];
-    uint32_t w_o = this->wires_idxs[cursor][2];
-    optimized[w_l] = false;
-    optimized[w_r] = false;
-    optimized[w_o] = false;
-
-    // Handles the case when we have univariate polynomial as constraint
-    // by simply finding the roots via quadratic formula(or linear)
-    // There're 7 possibilities of that, which are present below
-    bool univariate_flag = false;
-    univariate_flag |= (w_l == w_r) && (w_r == w_o);
-    univariate_flag |= (w_l == w_r) && (q_3 == 0);
-    univariate_flag |= (w_l == w_o) && (q_2 == 0) && (q_m == 0);
-    univariate_flag |= (w_r == w_o) && (q_1 == 0) && (q_m == 0);
-    univariate_flag |= (q_m == 0) && (q_1 == 0) && (q_3 == 0);
-    univariate_flag |= (q_m == 0) && (q_2 == 0) && (q_3 == 0);
-    univariate_flag |= (q_m == 0) && (q_1 == 0) && (q_2 == 0);
-
-    // Univariate gate. Relaxes the solver. Or is it?
-    // TODO(alex): Test the effect of this relaxation after the tests are merged.
-    if (univariate_flag) {
-        if ((q_m == 1) && (q_1 == 0) && (q_2 == 0) && (q_3 == -1) && (q_c == 0)) {
-            (Bool(symbolic_vars[w_l]) == Bool(symbolic_vars[0]) | Bool(symbolic_vars[w_l]) == Bool(symbolic_vars[1]))
-                .assert_term();
-        } else {
-            this->handle_univariate_constraint(q_m, q_1, q_2, q_3, q_c, w_l);
-        }
-    } else {
-        STerm eq = symbolic_vars[0];
-
-        // mul selector
-        if (q_m != 0) {
-            eq += symbolic_vars[w_l] * symbolic_vars[w_r] * q_m;
-        }
-        // left selector
-        if (q_1 != 0) {
-            eq += symbolic_vars[w_l] * q_1;
-        }
-        // right selector
-        if (q_2 != 0) {
-            eq += symbolic_vars[w_r] * q_2;
-        }
-        // out selector
-        if (q_3 != 0) {
-            eq += symbolic_vars[w_o] * q_3;
-        }
-        // constant selector
-        if (q_c != 0) {
-            eq += q_c;
-        }
-        eq == symbolic_vars[0];
-    }
-    return cursor + 1;
-}
-
-/**
- * @brief Returns a previously named symbolic variable.
- *
- * @param name
- * @return STerm
- */
-STerm Circuit::operator[](const std::string& name)
-{
-    if (!this->variable_names_inverse.contains(name)) {
-        throw std::invalid_argument("No such an item `" + name + "` in vars or it vas not declared as interesting");
-    }
-    uint32_t idx = this->variable_names_inverse[name];
-    return this->symbolic_vars[idx];
->>>>>>> master
 }
 
 /**
@@ -997,11 +764,7 @@ STerm Circuit::operator[](const std::string& name)
  * @return true
  * @return false
  */
-<<<<<<< HEAD
 bool StandardCircuit::simulate_circuit_eval(std::vector<bb::fr>& witness) const
-=======
-bool Circuit::simulate_circuit_eval(std::vector<bb::fr>& witness) const
->>>>>>> master
 {
     if (witness.size() != this->get_num_vars()) {
         throw std::invalid_argument("Witness size should be " + std::to_string(this->get_num_vars()) + ", not " +
@@ -1039,7 +802,6 @@ bool Circuit::simulate_circuit_eval(std::vector<bb::fr>& witness) const
  * @param not_equal_at_the_same_time The list of variables, where at least one pair has to be distinct
  * @return std::pair<Circuit, Circuit>
  */
-<<<<<<< HEAD
 std::pair<StandardCircuit, StandardCircuit> StandardCircuit::unique_witness_ext(
     CircuitSchema& circuit_info,
     Solver* s,
@@ -1053,19 +815,6 @@ std::pair<StandardCircuit, StandardCircuit> StandardCircuit::unique_witness_ext(
     // TODO(alex): set optimizations to be true once they are confirmed
     StandardCircuit c1(circuit_info, s, type, "circuit1", optimizations);
     StandardCircuit c2(circuit_info, s, type, "circuit2", optimizations);
-=======
-std::pair<Circuit, Circuit> unique_witness_ext(CircuitSchema& circuit_info,
-                                               Solver* s,
-                                               TermType type,
-                                               const std::vector<std::string>& equal,
-                                               const std::vector<std::string>& not_equal,
-                                               const std::vector<std::string>& equal_at_the_same_time,
-                                               const std::vector<std::string>& not_equal_at_the_same_time)
-{
-    // TODO(alex): set optimizations to be true once they are confirmed
-    Circuit c1(circuit_info, s, type, "circuit1", false);
-    Circuit c2(circuit_info, s, type, "circuit2", false);
->>>>>>> master
 
     for (const auto& term : equal) {
         c1[term] == c2[term];
@@ -1113,23 +862,12 @@ std::pair<Circuit, Circuit> unique_witness_ext(CircuitSchema& circuit_info,
  * @param equal The list of names of variables which should be equal in both circuits(each is equal)
  * @return std::pair<Circuit, Circuit>
  */
-<<<<<<< HEAD
 std::pair<StandardCircuit, StandardCircuit> StandardCircuit::unique_witness(
     CircuitSchema& circuit_info, Solver* s, TermType type, const std::vector<std::string>& equal, bool optimizations)
 {
     // TODO(alex): set optimizations to be true once they are confirmed
     StandardCircuit c1(circuit_info, s, type, "circuit1", optimizations);
     StandardCircuit c2(circuit_info, s, type, "circuit2", optimizations);
-=======
-std::pair<Circuit, Circuit> unique_witness(CircuitSchema& circuit_info,
-                                           Solver* s,
-                                           TermType type,
-                                           const std::vector<std::string>& equal)
-{
-    // TODO(alex): set optimizations to be true once they are confirmed
-    Circuit c1(circuit_info, s, type, "circuit1", false);
-    Circuit c2(circuit_info, s, type, "circuit2", false);
->>>>>>> master
 
     for (const auto& term : equal) {
         c1[term] == c2[term];
