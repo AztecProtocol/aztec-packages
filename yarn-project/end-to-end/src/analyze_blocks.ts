694c6c5d71444d8293a69f8b168feda4e8890aad
benesjan <janbenes1234@gmail.com>
2024-06-06 09:18:56 +0000

    WIP

--- yarn-project/end-to-end/src/analyze_blocks.ts
+++ yarn-project/end-to-end/src/analyze_blocks.ts
@@ -21,30 +21,30 @@ export async function analyzeBlocks(node: AztecNode) {
     }
 }
 
-async function main() {
-  const AZTEC_NODE_URL = 'http://localhost:8080';
-  const node = createAztecNodeClient(AZTEC_NODE_URL);
-  const blocks = await node.getBlocks(1, 1000);
-  console.log('Num blocks: ', blocks.length);
-  const txEffects = blocks.flatMap(b => b.body.txEffects);
-  console.log('Num tx effects: ', txEffects.length);
+// async function main() {
+//   const AZTEC_NODE_URL = 'http://localhost:8080';
+//   const node = createAztecNodeClient(AZTEC_NODE_URL);
+//   const blocks = await node.getBlocks(1, 1000);
+//   console.log('Num blocks: ', blocks.length);
+//   const txEffects = blocks.flatMap(b => b.body.txEffects);
+//   console.log('Num tx effects: ', txEffects.length);
 
-  // key = {num_logs_func_0}_{log_length_func_0}-{num_logs_func_1}_{log_length_func_1}...
-  // value
-  for (const { noteEncryptedLogs } of txEffects) {
-    // const key = "";
-    console.log('=====================================');
-    for (const functionLog of noteEncryptedLogs.functionLogs) {
-        console.log('Function logs: ', functionLog.logs);
-        console.log(functionLog.logs.map(log => log.length));
-      // key += `${functionLog.logs.length}_${functionLog.logs[0].length}-`;
-    //   const lengths = functionLog.logs.map(log => log.length);
-    //   console.log(functionLog.logs.length, functionLog.logs[0].length, lengths);
-    }
-  }
-}
+//   // key = {num_logs_func_0}_{log_length_func_0}-{num_logs_func_1}_{log_length_func_1}...
+//   // value
+//   for (const { noteEncryptedLogs } of txEffects) {
+//     // const key = "";
+//     console.log('=====================================');
+//     for (const functionLog of noteEncryptedLogs.functionLogs) {
+//         console.log('Function logs: ', functionLog.logs);
+//         console.log(functionLog.logs.map(log => log.length));
+//       // key += `${functionLog.logs.length}_${functionLog.logs[0].length}-`;
+//     //   const lengths = functionLog.logs.map(log => log.length);
+//     //   console.log(functionLog.logs.length, functionLog.logs[0].length, lengths);
+//     }
+//   }
+// }
 
-main().catch(err => {
-  console.log('Error: ', err);
-  process.exit(1);
-});
+// main().catch(err => {
+//   console.log('Error: ', err);
+//   process.exit(1);
+// });
