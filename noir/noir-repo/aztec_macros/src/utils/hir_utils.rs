002eecce47ee547a8dec0ffd4670b76718a14b61
AztecBot <tech@aztecprotocol.com>
2024-06-06 13:40:18 +0000

    [1 changes] fix(elaborator): Lazily elaborate globals (https://github.com/noir-lang/noir/pull/5191) chore: schnorr signature verification in noir (https://github.com/noir-lang/noir/pull/5188) fix(experimental elaborator): Fix globals which use function calls (https://github.com/noir-lang/noir/pull/5172) feat!: restrict noir word size to u32 (https://github.com/noir-lang/noir/pull/5180) chore: break out helper methods for writing foreign call results (https://github.com/noir-lang/noir/pull/5181) fix: Fix panic in `get_global_let_statement` (https://github.com/noir-lang/noir/pull/5177) fix(elaborator): Invert unconstrained check (https://github.com/noir-lang/noir/pull/5176) chore: loosen trait bounds on impls depending on `AcirField` (https://github.com/noir-lang/noir/pull/5115) feat: support casting in globals (https://github.com/noir-lang/noir/pull/5164) chore(experimental elaborator): Handle `comptime` expressions in the elaborator (https://github.com/noir-lang/noir/pull/5169) chore: avoid manual creation of contract artifact in wasm (https://github.com/noir-lang/noir/pull/5117) chore: start moving lints into a separate linting directory (https://github.com/noir-lang/noir/pull/5165) chore: move acir docs to code declaration (https://github.com/noir-lang/noir/pull/5040) feat!: separate proving from `noir_js` (https://github.com/noir-lang/noir/pull/5072)

--- noir/noir-repo/aztec_macros/src/utils/hir_utils.rs
+++ noir/noir-repo/aztec_macros/src/utils/hir_utils.rs
@@ -1,3 +1,4 @@
+use acvm::acir::AcirField;
 use iter_extended::vecmap;
 use noirc_errors::Location;
 use noirc_frontend::ast;
@@ -319,7 +320,7 @@ pub fn get_serialized_length(
     trait_name: &str,
     typ: &Type,
     interner: &NodeInterner,
-) -> Result<u64, MacroError> {
+) -> Result<u32, MacroError> {
     let serialized_trait_impl_kind = traits
         .iter()
         .find_map(|&trait_id| {
