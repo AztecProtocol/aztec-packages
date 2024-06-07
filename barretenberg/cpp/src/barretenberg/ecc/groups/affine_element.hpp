561ec694d212de6aa10fd487d6b576f16de81547
ludamad <domuradical@gmail.com>
2024-06-07 13:18:07 +0000

    try to fix

--- barretenberg/cpp/src/barretenberg/ecc/groups/affine_element.hpp
+++ barretenberg/cpp/src/barretenberg/ecc/groups/affine_element.hpp
@@ -110,8 +110,9 @@ template <typename Fq_, typename Fr_, typename Params> class alignas(64) affine_
             // we only need this case because the below gets mangled converting from montgomery for infinity points
             memset(buffer, 255, sizeof(Fq) * 2);
         } else {
-            write(buffer, value.x);
+            // NOTE: for historic reasons we write 'y' first here but 'x' first below
             write(buffer, value.y);
+            write(buffer, value.x);
         }
     }
 
@@ -138,9 +139,10 @@ template <typename Fq_, typename Fr_, typename Params> class alignas(64) affine_
         if (is_point_at_infinity) {
             return affine_element::infinity();
         }
+        // NOTE: for historic reasons we read 'y' first here but 'x' first below
         affine_element result;
-        read(buffer, result.x);
         read(buffer, result.y);
+        read(buffer, result.x);
         return result;
     }
 
@@ -165,22 +167,36 @@ template <typename Fq_, typename Fr_, typename Params> class alignas(64) affine_
     Fq y;
 };
 
-template <typename B, typename Fq_, typename Fr_, typename Params>
-inline void read(B& it, group_elements::affine_element<Fq_, Fr_, Params>& element)
+template <typename Fq_, typename Fr_, typename Params>
+inline void read(const uint8_t* buffer, group_elements::affine_element<Fq_, Fr_, Params>& element)
 {
     using namespace serialize;
-    std::array<uint8_t, sizeof(element)> buffer;
-    read(it, buffer);
-    element = group_elements::affine_element<Fq_, Fr_, Params>::serialize_from_buffer(buffer.data());
+    // Does the buffer consist entirely of set bits? If so, we have a point at infinity
+    // Note that if it isn't, this loop should end early.
+    // We only need this case because the below gets mangled converting to montgomery for infinity points
+    bool is_point_at_infinity = std::all_of(buffer, buffer + sizeof(Fq_) * 2, [](uint8_t val) { return val == 255; });
+    if (is_point_at_infinity) {
+        element = group_elements::affine_element<Fq_, Fr_, Params>::infinity();
+        return;
+    }
+    // NOTE: for historic reasons we read 'x' first here but 'y' first above
+    read(buffer, element.x);
+    read(buffer, element.y);
 }
 
-template <typename B, typename Fq_, typename Fr_, typename Params>
-inline void write(B& it, group_elements::affine_element<Fq_, Fr_, Params> const& element)
+template <typename Fq_, typename Fr_, typename Params>
+inline void write(uint8_t* buffer, group_elements::affine_element<Fq_, Fr_, Params> const& element)
 {
     using namespace serialize;
-    std::array<uint8_t, sizeof(element)> buffer;
-    group_elements::affine_element<Fq_, Fr_, Params>::serialize_to_buffer(element, buffer.data());
-    write(it, buffer);
+    if (element.is_point_at_infinity()) {
+        // if we are infinity, just set all buffer bits to 1
+        // we only need this case because the below gets mangled converting from montgomery for infinity points
+        memset(buffer, 255, sizeof(Fq_) * 2);
+    } else {
+        // NOTE: for historic reasons we write 'x' first here but 'y' first above
+        write(buffer, element.x);
+        write(buffer, element.y);
+    }
 }
 } // namespace bb::group_elements
 
