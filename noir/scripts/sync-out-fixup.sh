0d06e2c0f0ad186638e432cb25d506891ede3406
Tom French <tom@tomfren.ch>
2024-06-13 19:05:39 +0100

    chore: fix comment

--- noir/scripts/sync-out-fixup.sh
+++ noir/scripts/sync-out-fixup.sh
@@ -15,6 +15,6 @@ jq --arg v $BB_VERSION '.dependencies."@aztec/bb.js" = $v' $BACKEND_BARRETENBERG
 # we then must turn this off in order to update yarn.lock. 
 YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
 
-# Remove requirement for `wasm-opt` to be installed
+# Add requirement for `wasm-opt` to be installed
 sed -i "s/^#require_command wasm-opt/require_command wasm-opt/" ./tooling/noirc_abi_wasm/build.sh
 sed -i "s/^#require_command wasm-opt/require_command wasm-opt/" ./acvm-repo/acvm_js/build.sh
