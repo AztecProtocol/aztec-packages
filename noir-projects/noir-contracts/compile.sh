4fdd046bbe432561b3092c3b839703500728d673
Nicolás Venturo <nicolas.venturo@gmail.com>
2024-06-13 18:20:17 +0000

    Make note_getter return BoundedVecs

---
+++ noir-projects/noir-contracts/compile.sh
@@ -0,0 +1,82 @@
+echo "app_subscription_contract"
+nargo compile --silence-warnings --package app_subscription_contract
+echo "auth_contract"
+nargo compile --silence-warnings --package auth_contract
+echo "avm_initializer_test_contract"
+nargo compile --silence-warnings --package avm_initializer_test_contract
+echo "avm_test_contract"
+nargo compile --silence-warnings --package avm_test_contract
+echo "benchmarking_contract"
+nargo compile --silence-warnings --package benchmarking_contract
+echo "card_game_contract"
+nargo compile --silence-warnings --package card_game_contract
+echo "child_contract"
+nargo compile --silence-warnings --package child_contract
+echo "claim_contract"
+nargo compile --silence-warnings --package claim_contract
+echo "contract_class_registerer_contract"
+nargo compile --silence-warnings --package contract_class_registerer_contract
+echo "contract_instance_deployer_contract"
+nargo compile --silence-warnings --package contract_instance_deployer_contract
+echo "counter_contract"
+nargo compile --silence-warnings --package counter_contract
+echo "crowdfunding_contract"
+nargo compile --silence-warnings --package crowdfunding_contract
+echo "delegated_on_contract"
+nargo compile --silence-warnings --package delegated_on_contract
+echo "delegator_contract"
+nargo compile --silence-warnings --package delegator_contract
+echo "docs_example_contract"
+nargo compile --silence-warnings --package docs_example_contract
+echo "easy_private_token_contract"
+nargo compile --silence-warnings --package easy_private_token_contract
+echo "easy_private_voting_contract"
+nargo compile --silence-warnings --package easy_private_voting_contract
+echo "ecdsa_account_contract"
+nargo compile --silence-warnings --package ecdsa_account_contract
+echo "escrow_contract"
+nargo compile --silence-warnings --package escrow_contract
+echo "fpc_contract"
+nargo compile --silence-warnings --package fpc_contract
+echo "gas_token_contract"
+nargo compile --silence-warnings --package gas_token_contract
+echo "import_test_contract"
+nargo compile --silence-warnings --package import_test_contract
+echo "inclusion_proofs_contract"
+nargo compile --silence-warnings --package inclusion_proofs_contract
+echo "key_registry_contract"
+nargo compile --silence-warnings --package key_registry_contract
+echo "lending_contract"
+nargo compile --silence-warnings --package lending_contract
+echo "multi_call_entrypoint_contract"
+nargo compile --silence-warnings --package multi_call_entrypoint_contract
+echo "parent_contract"
+nargo compile --silence-warnings --package parent_contract
+echo "pending_note_hashes_contract"
+nargo compile --silence-warnings --package pending_note_hashes_contract
+echo "price_feed_contract"
+nargo compile --silence-warnings --package price_feed_contract
+echo "schnorr_account_contract"
+nargo compile --silence-warnings --package schnorr_account_contract
+echo "schnorr_hardcoded_account_contract"
+nargo compile --silence-warnings --package schnorr_hardcoded_account_contract
+echo "schnorr_single_key_account_contract"
+nargo compile --silence-warnings --package schnorr_single_key_account_contract
+echo "stateful_test_contract"
+nargo compile --silence-warnings --package stateful_test_contract
+echo "static_child_contract"
+nargo compile --silence-warnings --package static_child_contract
+echo "static_parent_contract"
+nargo compile --silence-warnings --package static_parent_contract
+echo "test_contract"
+nargo compile --silence-warnings --package test_contract
+echo "test_log_contract"
+nargo compile --silence-warnings --package test_log_contract
+echo "token_blacklist_contract"
+nargo compile --silence-warnings --package token_blacklist_contract
+echo "token_bridge_contract"
+nargo compile --silence-warnings --package token_bridge_contract
+echo "token_contract"
+nargo compile --silence-warnings --package token_contract
+echo "uniswap_contract"
+nargo compile --silence-warnings --package uniswap_contract
