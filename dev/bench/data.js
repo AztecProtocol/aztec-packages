759126dfe8c0c1b44005f1606043362e74323861
github-action-benchmark <github@users.noreply.github.com>
2024-06-13 09:34:33 +0000

    add C++ Benchmark (googlecpp) benchmark result for 36f9b66d9ebd6d2a54fd356e7ef10038b8f5e1d3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718271113351,
+  "lastUpdate": 1718271273634,
   "repoUrl": "https://github.com/AztecProtocol/aztec-packages",
   "entries": {
     "C++ Benchmark": [
@@ -38508,6 +38508,74 @@ window.BENCHMARK_DATA = {
             "value": 163806961,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 163806961 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "AztecProtocol",
+            "username": "AztecProtocol"
+          },
+          "committer": {
+            "name": "AztecProtocol",
+            "username": "AztecProtocol"
+          },
+          "id": "36f9b66d9ebd6d2a54fd356e7ef10038b8f5e1d3",
+          "message": "feat: Add gate profiler for noir circuits",
+          "timestamp": "2024-06-13T09:01:46Z",
+          "url": "https://github.com/AztecProtocol/aztec-packages/pull/7004/commits/36f9b66d9ebd6d2a54fd356e7ef10038b8f5e1d3"
+        },
+        "date": 1718271271714,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "nativeClientIVCBench/Full/6",
+            "value": 15433.550148999984,
+            "unit": "ms/iter",
+            "extra": "iterations: 1\ncpu: 10731.618714000002 ms\nthreads: 1"
+          },
+          {
+            "name": "nativeconstruct_proof_ultrahonk_power_of_2/20",
+            "value": 5508.466339000009,
+            "unit": "ms/iter",
+            "extra": "iterations: 1\ncpu: 5160.4809749999995 ms\nthreads: 1"
+          },
+          {
+            "name": "wasmClientIVCBench/Full/6",
+            "value": 50121.306591,
+            "unit": "ms/iter",
+            "extra": "iterations: 1\ncpu: 50121308000 ms\nthreads: 1"
+          },
+          {
+            "name": "wasmconstruct_proof_ultrahonk_power_of_2/20",
+            "value": 16916.44375,
+            "unit": "ms/iter",
+            "extra": "iterations: 1\ncpu: 16916443000 ms\nthreads: 1"
+          },
+          {
+            "name": "commit(t)",
+            "value": 5138279002,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 5138279002 ns\nthreads: 1"
+          },
+          {
+            "name": "Goblin::merge(t)",
+            "value": 200039776,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 200039776 ns\nthreads: 1"
+          },
+          {
+            "name": "commit(t)",
+            "value": 4287845314,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 4287845314 ns\nthreads: 1"
+          },
+          {
+            "name": "Goblin::merge(t)",
+            "value": 163753573,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 163753573 ns\nthreads: 1"
           }
         ]
       }
