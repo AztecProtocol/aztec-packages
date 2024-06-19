ff05744840f974a2b4b91c19905da1c9bb42d89d
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-06-19 14:41:35 +0000

    chore: compile and rebuild script

--- barretenberg/cpp/scripts/compile_avm.sh
+++ barretenberg/cpp/scripts/compile_avm.sh
@@ -2,10 +2,3 @@
 
 # Compile
 ../../bb-pilcom/target/release/bb_pil pil/avm/main.pil --name Avm
-
-# Format generated folders
-git add **/generated/*
-./format.sh staged
-
-# Build vm tests
-cmake --build --preset clang16 --target vm_tests
