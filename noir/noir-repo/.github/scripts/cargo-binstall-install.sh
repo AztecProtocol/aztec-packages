f777a3d09fce50fb6309c09fb5494a0155663561
TomAFrench <tom@tomfren.ch>
2024-06-20 17:09:43 +0000

    chore: remove unwanted semicolon

--- noir/noir-repo/.github/scripts/cargo-binstall-install.sh
+++ noir/noir-repo/.github/scripts/cargo-binstall-install.sh
@@ -3,7 +3,7 @@ set -eu
 
 if [ command -v cargo-binstall  &> /dev/null ]; then
   cargo-binstall cargo-binstall -y
-else;
+else
   curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 fi
 
