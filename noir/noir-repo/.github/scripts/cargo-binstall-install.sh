b200f3dd9c541e78c4e64bd42009c8995ee9c8fe
TomAFrench <tom@tomfren.ch>
2024-06-20 17:00:14 +0000

    chore: update yarn.lock

--- noir/noir-repo/.github/scripts/cargo-binstall-install.sh
+++ noir/noir-repo/.github/scripts/cargo-binstall-install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+if [ command -v cargo-binstall  &> /dev/null ]; then
+  cargo-binstall cargo-binstall -y
+else;
+  curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+fi
+
