b4a8c9521d8e32c304b502748799823930cacd48
TomAFrench <tom@tomfren.ch>
2024-06-25 00:27:58 +0000

    chore: fmt

--- noir/noir-repo/aztec_macros/src/utils/ast_utils.rs
+++ noir/noir-repo/aztec_macros/src/utils/ast_utils.rs
@@ -52,10 +52,7 @@ pub fn method_call(
 }
 
 pub fn call(func: Expression, arguments: Vec<Expression>) -> Expression {
-    expression(ExpressionKind::Call(Box::new(CallExpression {
-        func: Box::new(func),
-        arguments,
-    })))
+    expression(ExpressionKind::Call(Box::new(CallExpression { func: Box::new(func), arguments })))
 }
 
 pub fn pattern(name: &str) -> Pattern {
