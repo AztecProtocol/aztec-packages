ea55803f9a2994a174f2e30530e1d8ae3cd73beb
esau <152162806+sklppy88@users.noreply.github.com>
2024-06-28 16:19:57 -0500

    Update transfer_private.test.ts

--- yarn-project/end-to-end/src/e2e_token_contract/transfer_private.test.ts
+++ yarn-project/end-to-end/src/e2e_token_contract/transfer_private.test.ts
@@ -37,12 +37,7 @@ describe('e2e_token_contract transfer private', () => {
     const tx = await asset.methods.transfer(accounts[1].address, amount).send().wait();
     tokenSim.transferPrivate(accounts[0].address, accounts[1].address, amount);
 
-    const events = await wallets[1].getEvents(
-      EventType.Encrypted,
-      TokenContract.events.Transfer,
-      tx.blockNumber!,
-      1,
-    );
+    const events = await wallets[1].getEvents(EventType.Encrypted, TokenContract.events.Transfer, tx.blockNumber!, 1);
 
     expect(events[0]).toEqual({
       from: accounts[0].address,
