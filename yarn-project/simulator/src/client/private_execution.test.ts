5b0e8a60c5e47dde1da84d99f0c1ea91851c70c1
LHerskind <lasse.herskind@gmail.com>
2024-07-01 12:21:51 +0000

    chore: add L2BlockNumber type

--- yarn-project/simulator/src/client/private_execution.test.ts
+++ yarn-project/simulator/src/client/private_execution.test.ts
@@ -2,6 +2,7 @@ import {
   type AztecNode,
   EncryptedNoteFunctionL2Logs,
   type L1ToL2Message,
+  type L2BlockNumber,
   Note,
   PackedValues,
   PublicDataWitness,
@@ -264,8 +265,8 @@ describe('Private Execution test suite', () => {
 
     node = mock<AztecNode>();
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    node.getPublicStorageAt.mockImplementation((address: Fr, storageSlot: Fr, blockNumber: number) => {
-      return Fr.ZERO;
+    node.getPublicStorageAt.mockImplementation((address: Fr, storageSlot: Fr, blockNumber: L2BlockNumber) => {
+      return Promise.resolve(Fr.ZERO);
     });
 
     acirSimulator = new AcirSimulator(oracle, node);
