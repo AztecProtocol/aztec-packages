4d76e6fd1b88d62da1b18880b2dcd4e1853450f4
sirasistant <sirasistant@gmail.com>
2024-07-03 08:53:40 +0000

    refactor vk gen

--- noir-projects/noir-protocol-circuits/bootstrap.sh
+++ noir-projects/noir-protocol-circuits/bootstrap.sh
@@ -22,4 +22,40 @@ echo "Compiling protocol circuits..."
 NARGO=${NARGO:-../../noir/noir-repo/target/release/nargo}
 $NARGO compile --silence-warnings --use-legacy
 
-node ./generate_vk_jsons.js
+mkdir -p "./target/keys"
+
+AVAILABLE_MEMORY=0
+
+case "$(uname)" in
+  Linux*)
+    # Check available memory on Linux
+    AVAILABLE_MEMORY=$(awk '/MemTotal/ { printf $2 }' /proc/meminfo)
+    ;;
+  *)
+    echo "Parallel vk generation not supported on this operating system"
+    ;;
+esac
+# This value may be too low.
+# If vk generation fail with an amount of free memory greater than this value then it should be increased.
+MIN_PARALLEL_VK_GENERATION_MEMORY=500000000
+
+if [[ AVAILABLE_MEMORY -lt MIN_PARALLEL_VK_GENERATION_MEMORY ]]; then
+  echo "System does not have enough memory for parallel vk generation, falling back to sequential"
+
+  for pathname in "./target"/*.json; do    
+      node ./generate_vk_json.js "$pathname" "./target/keys"
+  done
+
+else
+
+  echo "Generating vks in parallel..."
+  for pathname in "./target"/*.json; do    
+      node ./generate_vk_json.js "$pathname" "./target/keys" &
+  done
+
+  for job in $(jobs -p); do
+    wait $job || exit 1
+  done
+
+fi
+
