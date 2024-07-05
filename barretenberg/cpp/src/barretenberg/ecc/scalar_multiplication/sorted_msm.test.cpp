1772b64a3717b540e1e855b95e2c96f5d6b52793
ledwards2225 <l.edwards.d@gmail.com>
2024-07-05 22:32:52 +0000

    larger test

--- barretenberg/cpp/src/barretenberg/ecc/scalar_multiplication/sorted_msm.test.cpp
+++ barretenberg/cpp/src/barretenberg/ecc/scalar_multiplication/sorted_msm.test.cpp
@@ -30,8 +30,18 @@ template <typename Curve> class SortedMsmTests : public ::testing::Test {
         G1 msm_result;
     };
 
+    /**
+     * @brief Generate a set of random points and scalars based on an input sequence_counts
+     * @details E.g. given sequence counts {7, 2, 9}, generate a set of random points an scalars with only 3 unique
+     * scalar values repeated according to the sequence counts. Also compute the result of the corresponding MSM for
+     * test comparisons.
+     *
+     * @param sequence_counts
+     * @return TestData
+     */
     TestData generate_test_data_from_sequence_counts(std::span<uint64_t> sequence_counts)
     {
+        // Generate duplicate scalars corresponding to the sequence counts
         size_t num_points{ 0 };
         std::vector<Fr> scalars;
         for (auto& count : sequence_counts) {
@@ -42,11 +52,17 @@ template <typename Curve> class SortedMsmTests : public ::testing::Test {
             }
         }
 
+        // Randomly shuffle the scalars so duplicates are no longer grouped together
+        std::random_device rd;
+        std::shuffle(scalars.begin(), scalars.end(), std::default_random_engine(rd()));
+
+        // Randomly generate as many points as scalars
         std::vector<G1> points;
         for (size_t i = 0; i < num_points; ++i) {
             points.emplace_back(G1::random_element());
         }
 
+        // Compute the result of the MSM
         G1 msm_result = points[0] * scalars[0];
         for (size_t i = 1; i < num_points; ++i) {
             msm_result = msm_result + points[i] * scalars[i];
@@ -60,6 +76,7 @@ using Curves = ::testing::Types<curve::BN254, curve::Grumpkin>;
 
 TYPED_TEST_SUITE(SortedMsmTests, Curves);
 
+// Test method for a single affine addition with provided slope denominator
 TYPED_TEST(SortedMsmTests, AffineAddWithDenominator)
 {
     using Curve = TypeParam;
@@ -80,6 +97,7 @@ TYPED_TEST(SortedMsmTests, AffineAddWithDenominator)
     EXPECT_EQ(result, expected);
 }
 
+// Test method for batch computing slope denominators for a set of point addition sequences
 TYPED_TEST(SortedMsmTests, ComputePointAdditionDenominators)
 {
     using Curve = TypeParam;
@@ -110,30 +128,7 @@ TYPED_TEST(SortedMsmTests, ComputePointAdditionDenominators)
     }
 }
 
-TYPED_TEST(SortedMsmTests, BatchedAffineAddInPlaceSimple)
-{
-    using Curve = TypeParam;
-    using G1 = typename Curve::AffineElement;
-    using MsmManager = SortedMsmManager<Curve>;
-    using AdditionSequences = typename MsmManager::AdditionSequences;
-
-    // Generate random MSM inputs based on a set of sequence counts
-    std::array<uint64_t, 1> sequence_counts{ 2 };
-    auto [num_points, points, scalars, msm_result] =
-        TestFixture::generate_test_data_from_sequence_counts(sequence_counts);
-
-    AdditionSequences addition_sequences{ sequence_counts, points, {} };
-
-    std::array<G1, 1> expected_points{ points[0] + points[1] };
-
-    MsmManager msm_manager(num_points);
-    msm_manager.batched_affine_add_in_place(addition_sequences);
-
-    for (size_t idx = 0; idx < expected_points.size(); ++idx) {
-        EXPECT_EQ(expected_points[idx], points[idx]);
-    }
-}
-
+// Test method for batched addition of point addition sequences in place
 TYPED_TEST(SortedMsmTests, BatchedAffineAddInPlace)
 {
     using Curve = TypeParam;
@@ -166,6 +161,7 @@ TYPED_TEST(SortedMsmTests, BatchedAffineAddInPlace)
     }
 }
 
+// Test generation of point addition sequences from an arbitrary set of points and scalars
 TYPED_TEST(SortedMsmTests, GenerateAdditionSequences)
 {
     using Curve = TypeParam;
@@ -178,10 +174,6 @@ TYPED_TEST(SortedMsmTests, GenerateAdditionSequences)
     std::array<uint64_t, 3> sequence_counts{ 5, 2, 3 };
     auto [num_points, points, scalars, expected_msm_result] =
         TestFixture::generate_test_data_from_sequence_counts(sequence_counts);
-
-    // Randomly shuffle the scalars so duplicates are no longer grouped together
-    std::random_device rd;
-    std::shuffle(scalars.begin(), scalars.end(), std::default_random_engine(rd()));
 
     MsmManager msm_manager{ num_points };
     AdditionSequences result = msm_manager.construct_addition_sequences(scalars, points);
@@ -222,10 +214,6 @@ TYPED_TEST(SortedMsmTests, ReduceMsmInputsSimple)
     auto [num_points, points, scalars, expected_msm_result] =
         TestFixture::generate_test_data_from_sequence_counts(sequence_counts);
 
-    // Randomly shuffle the scalars so duplicates are no longer grouped together
-    std::random_device rd;
-    std::shuffle(scalars.begin(), scalars.end(), std::default_random_engine(rd()));
-
     MsmManager msm_manager{ num_points };
     auto [result_scalars, result_points] = msm_manager.reduce_msm_inputs(scalars, points);
 
@@ -237,57 +225,24 @@ TYPED_TEST(SortedMsmTests, ReduceMsmInputsSimple)
     EXPECT_EQ(msm_result, expected_msm_result);
 }
 
-// TYPED_TEST(ScalarMultiplicationTests, RemoveDuplicates)
-// {
-//     using Curve = TypeParam;
-//     using Element = typename Curve::Element;
-//     using AffineElement = typename Curve::AffineElement;
-//     using Fr = typename Curve::ScalarField;
+TYPED_TEST(SortedMsmTests, ReduceMsmInputs)
+{
+    using Curve = TypeParam;
+    using G1 = typename Curve::AffineElement;
+    using MsmManager = SortedMsmManager<Curve>;
 
-//     constexpr size_t num_points = 8192;
+    // Generate random MSM inputs based on a set of sequence counts
+    std::array<uint64_t, 5> sequence_counts{ 75, 1, 28, 382, 3 };
+    auto [num_points, points, scalars, expected_msm_result] =
+        TestFixture::generate_test_data_from_sequence_counts(sequence_counts);
 
-//     Fr* scalars = (Fr*)aligned_alloc(32, sizeof(Fr) * num_points);
+    MsmManager msm_manager{ num_points };
+    auto [result_scalars, result_points] = msm_manager.reduce_msm_inputs(scalars, points);
 
-//     AffineElement* points = (AffineElement*)aligned_alloc(32, sizeof(AffineElement) * (num_points * 2 + 1));
+    G1 msm_result = result_points[0] * result_scalars[0];
+    for (size_t i = 1; i < result_points.size(); ++i) {
+        msm_result = msm_result + result_points[i] * result_scalars[i];
+    }
 
-//     std::vector<AffineElement> expected;
-//     bool filling_scalars = true;
-//     size_t num_filled_scalars = 0;
-//     while (filling_scalars) {
-//         size_t num_identical_scalars = static_cast<size_t>(engine.get_random_uint64()) % 32 + 1;
-//         if (num_filled_scalars + num_identical_scalars > num_points) {
-//             num_identical_scalars = num_points - num_filled_scalars;
-//         }
-//         auto scalar = Fr::random_element();
-//         Element accumulator;
-//         accumulator.self_set_infinity();
-//         for (size_t i = 0; i < num_identical_scalars; ++i) {
-//             Element point = G1::random_element();
-//             scalars[num_filled_scalars + i] = scalar;
-//             points[num_filled_scalars + i] = point;
-//             accumulator += point;
-//         }
-//         expected.push_back(accumulator);
-//         num_filled_scalars += num_identical_scalars;
-//         if (num_filled_scalars == num_points) {
-//             filling_scalars = false;
-//         }
-//     }
-
-//     // for (size_t i = 0; i < num_points; ++i) {
-//     //     scalars[i] = Fr::random_element();
-//     //     points[i] = AffineElement(G1::random_element());
-//     // }
-
-//     scalar_multiplication::generate_pippenger_point_table<Curve>(points, points, num_points);
-//     scalar_multiplication::pippenger_runtime_state<Curve> state(num_points);
-
-//     scalar_multiplication::remove_duplicates<Curve>(
-//         std::span<Fr>(scalars, num_points), std::span<AffineElement>(points, num_points), state);
-
-//     AffineElement* result = state.point_pairs_2;
-
-//     for (size_t i = 0; i < expected.size(); ++i) {
-//         EXPECT_EQ(result[i], expected[i]);
-//     }
-// }
+    EXPECT_EQ(msm_result, expected_msm_result);
+}
