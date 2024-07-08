bd01afcf55096fa4a05e7e933fa249a10b485874
ludamad <domuradical@gmail.com>
2024-07-08 21:00:13 +0000

    p2p fixes

--- yarn-project/p2p/src/service/tx_messages.ts
+++ yarn-project/p2p/src/service/tx_messages.ts
@@ -113,8 +113,7 @@ export function fromTxMessage(buffer: Buffer): Tx {
   // this is the opposite of the 'toMessage' function
   // so the first 4 bytes is the complete length, skip it
   const publicInputs = toObject(buffer.subarray(4), PrivateKernelTailCircuitPublicInputs);
-  const proof = toObject(publicInputs.remainingData, Proof);
-  const clientIvcProof = toObject(proof.remainingData, ClientIvcProof);
+  const clientIvcProof = toObject(publicInputs.remainingData, ClientIvcProof);
 
   const noteEncryptedLogs = toObject(clientIvcProof.remainingData, EncryptedNoteTxL2Logs);
   if (!noteEncryptedLogs.obj) {
