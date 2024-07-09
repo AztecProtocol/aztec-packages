a888e254f1c75501319e139321c6eceb111fea7d
spypsy <spypsy@outlook.com>
2024-07-09 11:59:48 +0000

    fix mnemonic issue

--- iac/mainnet-fork/scripts/run_nginx_anvil.sh
+++ iac/mainnet-fork/scripts/run_nginx_anvil.sh
@@ -13,12 +13,14 @@ trap 'kill $(jobs -p)' SIGTERM
 HOST="0.0.0.0"
 PORT=8544
 ETHEREUM_HOST=$HOST:$PORT
+# Stripping double quotations from the mnemonic seed phrase
+MNEMONIC_STRIPPED=${MNEMONIC//\"/}
 
 # Data directory for anvil state
 mkdir -p /data
 
 # Run anvil silently
-.foundry/bin/anvil --silent --host $HOST -p $PORT -m "$MNEMONIC" -f=https://mainnet.infura.io/v3/$INFURA_API_KEY --chain-id=$CHAIN_ID --fork-block-number=15918000 --block-base-fee-per-gas=10 -s=$SNAPSHOT_FREQUENCY --state=./data/state --balance=1000000000000000000 >/dev/null &
+.foundry/bin/anvil --silent --host $HOST -p $PORT -m "$MNEMONIC_STRIPPED" -f=https://mainnet.infura.io/v3/$INFURA_API_KEY --chain-id=$CHAIN_ID --fork-block-number=15918000 --block-base-fee-per-gas=10 -s=$SNAPSHOT_FREQUENCY --state=./data/state --balance=1000000000000000000 >/dev/null &
 
 echo "Waiting for ethereum host at $ETHEREUM_HOST..."
 while ! curl -s $ETHEREUM_HOST >/dev/null; do sleep 1; done
