85da5a9aee00e465563f475943091e28cd1ed21d
MirandaWood <miranda@aztecprotocol.com>
2024-07-10 12:44:02 +0000

    feat: remove test code from nr contracts, add new ts tests for exploit

--- yarn-project/end-to-end/src/e2e_token_contract/token_contract_test.ts
+++ yarn-project/end-to-end/src/e2e_token_contract/token_contract_test.ts
@@ -1,7 +1,6 @@
 import { getSchnorrAccount } from '@aztec/accounts/schnorr';
 import {
   type AccountWallet,
-  type AztecNode,
   type CompleteAddress,
   type DebugLogger,
   ExtendedNote,
@@ -35,7 +34,6 @@ export class TokenContractTest {
   asset!: TokenContract;
   tokenSim!: TokenSimulator;
   badAccount!: DocsExampleContract;
-  aztecNode!: AztecNode;
 
   constructor(testName: string) {
     this.logger = createDebugLogger(`aztec:e2e_token_contract:${testName}`);
@@ -48,17 +46,12 @@ export class TokenContractTest {
    * 2. Publicly deploy accounts, deploy token contract and a "bad account".
    */
   async applyBaseSnapshots() {
-    await this.snapshotManager.snapshot(
-      '3_accounts',
-      addAccounts(3, this.logger),
-      async ({ accountKeys }, { pxe, aztecNode }) => {
-        const accountManagers = accountKeys.map(ak => getSchnorrAccount(pxe, ak[0], ak[1], 1));
-        this.wallets = await Promise.all(accountManagers.map(a => a.getWallet()));
-        this.accounts = await pxe.getRegisteredAccounts();
-        this.wallets.forEach((w, i) => this.logger.verbose(`Wallet ${i} address: ${w.getAddress()}`));
-        this.aztecNode = aztecNode;
-      },
-    );
+    await this.snapshotManager.snapshot('3_accounts', addAccounts(3, this.logger), async ({ accountKeys }, { pxe }) => {
+      const accountManagers = accountKeys.map(ak => getSchnorrAccount(pxe, ak[0], ak[1], 1));
+      this.wallets = await Promise.all(accountManagers.map(a => a.getWallet()));
+      this.accounts = await pxe.getRegisteredAccounts();
+      this.wallets.forEach((w, i) => this.logger.verbose(`Wallet ${i} address: ${w.getAddress()}`));
+    });
 
     await this.snapshotManager.snapshot(
       'e2e_token_contract',
