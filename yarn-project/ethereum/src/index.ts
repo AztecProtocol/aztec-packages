ee12c95c71d8834c1483fbd9e6d659abb2a3eaf1
spypsy <spypsy@outlook.com>
2024-07-10 10:14:59 +0000

    only use chainId chainInfo

--- yarn-project/ethereum/src/index.ts
+++ yarn-project/ethereum/src/index.ts
@@ -12,12 +12,23 @@ export * from './constants.js';
  * @param rpcUrl - The rpc url of the chain or a chain identifier (e.g. 'testnet')
  * @param apiKey - An optional API key for the chain client.
  */
-export function createEthereumChain(rpcUrl: string, chainId = 31337) {
-  return {
-    chainInfo: {
-      ...foundry,
-      id: chainId,
-    },
-    rpcUrl,
-  } as EthereumChain;
+export function createEthereumChain(rpcUrl: string, chainId?: number) {
+  if (chainId) {
+    return {
+      chainInfo: {
+        id: chainId,
+        nativeCurrency: {
+          decimals: 18,
+          name: 'Ether',
+          symbol: 'ETH',
+        },
+      },
+      rpcUrl,
+    } as EthereumChain;
+  } else {
+    return {
+      chainInfo: foundry,
+      rpcUrl,
+    } as EthereumChain;
+  }
 }
