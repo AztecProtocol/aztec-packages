744f1a70aa0961a1d3f8f23f441b0544e8e2dbd9
ledwards2225 <l.edwards.d@gmail.com>
2024-07-11 02:50:16 +0000

    expand commitment benchmarks

--- barretenberg/cpp/src/barretenberg/commitment_schemes/commit.bench.cpp
+++ barretenberg/cpp/src/barretenberg/commitment_schemes/commit.bench.cpp
@@ -7,16 +7,17 @@ namespace bb {
 
 template <typename Curve> std::shared_ptr<CommitmentKey<Curve>> create_commitment_key(const size_t num_points)
 {
+    bb::srs::init_crs_factory("../srs_db/ignition");
     std::string srs_path;
     return std::make_shared<CommitmentKey<Curve>>(num_points);
 }
 
-constexpr size_t MAX_LOG_NUM_POINTS = 24;
+constexpr size_t MAX_LOG_NUM_POINTS = 19;
 constexpr size_t MAX_NUM_POINTS = 1 << MAX_LOG_NUM_POINTS;
 
 auto key = create_commitment_key<curve::BN254>(MAX_NUM_POINTS);
 
-template <typename Curve> void bench_commit(::benchmark::State& state)
+template <typename Curve> void bench_commit_zero(::benchmark::State& state)
 {
     const size_t num_points = 1 << state.range(0);
     const auto polynomial = Polynomial<typename Curve::ScalarField>(num_points);
@@ -25,7 +26,47 @@ template <typename Curve> void bench_commit(::benchmark::State& state)
     }
 }
 
-BENCHMARK(bench_commit<curve::BN254>)->DenseRange(10, MAX_LOG_NUM_POINTS)->Unit(benchmark::kMillisecond);
+template <typename Curve> void bench_commit_sparse(::benchmark::State& state)
+{
+    using Fr = typename Curve::ScalarField;
+    const size_t num_points = 1 << state.range(0);
+    auto polynomial = Polynomial<Fr>(num_points);
+    polynomial[25] = 1;
+    polynomial[22] = 1;
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(key->commit(polynomial));
+    }
+}
+
+template <typename Curve> void bench_commit_sparse_random(::benchmark::State& state)
+{
+    using Fr = typename Curve::ScalarField;
+    const size_t num_points = 1 << state.range(0);
+    auto polynomial = Polynomial<Fr>(num_points);
+    polynomial[25] = Fr::random_element();
+    polynomial[22] = Fr::random_element();
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(key->commit(polynomial));
+    }
+}
+
+template <typename Curve> void bench_commit_random(::benchmark::State& state)
+{
+    using Fr = typename Curve::ScalarField;
+    const size_t num_points = 1 << state.range(0);
+    auto polynomial = Polynomial<Fr>(num_points);
+    for (auto& coeff : polynomial) {
+        coeff = Fr::random_element();
+    }
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(key->commit(polynomial));
+    }
+}
+
+BENCHMARK(bench_commit_zero<curve::BN254>)->DenseRange(14, MAX_LOG_NUM_POINTS)->Unit(benchmark::kMillisecond);
+BENCHMARK(bench_commit_sparse<curve::BN254>)->DenseRange(14, MAX_LOG_NUM_POINTS)->Unit(benchmark::kMillisecond);
+BENCHMARK(bench_commit_sparse_random<curve::BN254>)->DenseRange(14, MAX_LOG_NUM_POINTS)->Unit(benchmark::kMillisecond);
+BENCHMARK(bench_commit_random<curve::BN254>)->DenseRange(14, MAX_LOG_NUM_POINTS)->Unit(benchmark::kMillisecond);
 
 } // namespace bb
 
