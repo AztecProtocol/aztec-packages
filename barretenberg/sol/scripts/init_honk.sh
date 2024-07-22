ba206b77ee1b47d09846debc5ddc3fcb82eb5071
codygunton <codygunton@gmail.com>
2024-07-22 17:50:10 +0000

    Rever renaming for now.

--- barretenberg/sol/scripts/init_honk.sh
+++ barretenberg/sol/scripts/init_honk.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # TODO(https://github.com/AztecProtocol/barretenberg/issues/1057): Honk solidity verifier
-FLAVOUR="honk"
+PLONK_FLAVOUR="honk"
 SRS_PATH="../cpp/srs_db/ignition"
 OUTPUT_PATH="./src/honk"
 
 mkdir -p './src/honk/keys'
 
-../cpp/build/bin/honk_solidity_key_gen $FLAVOUR add2 $OUTPUT_PATH $SRS_PATH
-../cpp/build/bin/honk_solidity_key_gen $FLAVOUR blake $OUTPUT_PATH $SRS_PATH
-../cpp/build/bin/honk_solidity_key_gen $FLAVOUR ecdsa $OUTPUT_PATH $SRS_PATH
+../cpp/build/bin/honk_solidity_key_gen $PLONK_FLAVOUR add2 $OUTPUT_PATH $SRS_PATH
+../cpp/build/bin/honk_solidity_key_gen $PLONK_FLAVOUR blake $OUTPUT_PATH $SRS_PATH
+../cpp/build/bin/honk_solidity_key_gen $PLONK_FLAVOUR ecdsa $OUTPUT_PATH $SRS_PATH
