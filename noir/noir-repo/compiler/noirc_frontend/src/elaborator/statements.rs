ed49eabfb6fd5f3051a32ab8387d1e0f1554a137
AztecBot <tech@aztecprotocol.com>
2024-07-22 08:02:00 +0000

    [1 changes] fix(frontend): Disallow signed numeric generics (https://github.com/noir-lang/noir/pull/5572) chore: disable aztec-packages CI checks (https://github.com/noir-lang/noir/pull/5566) feat: LSP inlay parameter hints (https://github.com/noir-lang/noir/pull/5553) feat: Add `TraitDefinition::as_trait_constraint()` (https://github.com/noir-lang/noir/pull/5541) fix: Fix `uhashmap` test name (https://github.com/noir-lang/noir/pull/5563) fix: let unary traits work at comptime (https://github.com/noir-lang/noir/pull/5507) feat: Add a compile-time hash map type (https://github.com/noir-lang/noir/pull/5543) chore(docs): Docs for turbofish operator (https://github.com/noir-lang/noir/pull/5555) fix(frontend): Error for when impl is stricter than trait (https://github.com/noir-lang/noir/pull/5343) chore: filter warnings from elaborator in Aztec Macros (https://github.com/noir-lang/noir/pull/5556) fix: Don't panic when a macro fails to resolve (https://github.com/noir-lang/noir/pull/5537) fix(ssa): More robust array deduplication check (https://github.com/noir-lang/noir/pull/5547) fix: Fix occurs check (https://github.com/noir-lang/noir/pull/5535) fix: type_of for pointer types (https://github.com/noir-lang/noir/pull/5536) chore: Release Noir(0.32.0) (https://github.com/noir-lang/noir/pull/5268) fix: revert PR #5449 (https://github.com/noir-lang/noir/pull/5548) fix: never panic in LSP inlay hints (https://github.com/noir-lang/noir/pull/5534) feat: LSP document symbol (https://github.com/noir-lang/noir/pull/5532) feat: add comptime support for `modulus_*` compiler builtins (https://github.com/noir-lang/noir/pull/5530) chore: Remove unknown annotation warning (https://github.com/noir-lang/noir/pull/5531) feat: Add TraitConstraint type (https://github.com/noir-lang/noir/pull/5499) fix: Error on empty function bodies (https://github.com/noir-lang/noir/pull/5519) feat: LSP inlay hints for let and global (https://github.com/noir-lang/noir/pull/5510) chore: Remove dbg on find_func_with_name (https://github.com/noir-lang/noir/pull/5526) chore: Remove the remainder of legacy code (https://github.com/noir-lang/noir/pull/5525) chore: Remove `--use-legacy` and resolution code (https://github.com/noir-lang/noir/pull/5248) chore: switch to Noir Keccak implementation with variable size support (https://github.com/noir-lang/noir/pull/5508) chore: standardize experimental feature disclaimer across documentation (https://github.com/noir-lang/noir/pull/5367)

--- noir/noir-repo/compiler/noirc_frontend/src/elaborator/statements.rs
+++ noir/noir-repo/compiler/noirc_frontend/src/elaborator/statements.rs
@@ -3,7 +3,6 @@ use noirc_errors::{Location, Span};
 use crate::{
     ast::{AssignStatement, ConstrainStatement, LValue},
     hir::{
-        comptime::Interpreter,
         resolution::errors::ResolverError,
         type_check::{Source, TypeCheckError},
     },
@@ -16,7 +15,7 @@ use crate::{
     macros_api::{
         ForLoopStatement, ForRange, HirStatement, LetStatement, Path, Statement, StatementKind,
     },
-    node_interner::{DefinitionId, DefinitionKind, GlobalId, ReferenceId, StmtId},
+    node_interner::{DefinitionId, DefinitionKind, GlobalId, StmtId},
     Type,
 };
 
@@ -256,9 +255,8 @@ impl<'context> Elaborator<'context> {
                     typ.follow_bindings()
                 };
 
-                let referenced = ReferenceId::Local(ident.id);
-                let reference = ReferenceId::Reference(Location::new(span, self.file), false);
-                self.interner.add_reference(referenced, reference);
+                let reference_location = Location::new(span, self.file);
+                self.interner.add_local_reference(ident.id, reference_location);
 
                 (HirLValue::Ident(ident.clone(), typ.clone()), typ, mutable)
             }
@@ -381,6 +379,9 @@ impl<'context> Elaborator<'context> {
             Type::Struct(s, args) => {
                 let s = s.borrow();
                 if let Some((field, index)) = s.get_field(field_name, args) {
+                    let reference_location = Location::new(span, self.file);
+                    self.interner.add_struct_member_reference(s.id, index, reference_location);
+
                     return Some((field, index));
                 }
             }
@@ -444,11 +445,15 @@ impl<'context> Elaborator<'context> {
         let span = statement.span;
         let (hir_statement, _typ) = self.elaborate_statement(statement);
         self.check_and_pop_function_context();
-
-        let mut interpreter =
-            Interpreter::new(self.interner, &mut self.comptime_scopes, self.crate_id);
+        let mut interpreter_errors = vec![];
+        let mut interpreter = self.setup_interpreter(&mut interpreter_errors);
         let value = interpreter.evaluate_statement(hir_statement);
         let (expr, typ) = self.inline_comptime_value(value, span);
+        self.include_interpreter_errors(interpreter_errors);
+
+        let location = self.interner.id_location(hir_statement);
+        self.debug_comptime(location, |interner| expr.to_display_ast(interner).kind);
+
         (HirStatement::Expression(expr), typ)
     }
 }
