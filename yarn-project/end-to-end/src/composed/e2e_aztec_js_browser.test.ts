48cbe9ca4b5a4994a1058a43a2afec3a7d0594c2
benesjan <janbenes1234@gmail.com>
2024-07-24 14:13:03 +0000

    updated test comment

--- yarn-project/end-to-end/src/composed/e2e_aztec_js_browser.test.ts
+++ yarn-project/end-to-end/src/composed/e2e_aztec_js_browser.test.ts
@@ -26,7 +26,7 @@ const pageLogger = createDebugLogger('aztec:e2e_aztec_browser.js:web:page');
  *    3) start anvil: `anvil`,
  *    4) if you intend to use a remotely running environment then export the URL of your PXE e.g. `export PXE_URL='http://localhost:8080'`
  *    5) go to `yarn-project/end-to-end` and run the test: `yarn test aztec_js_browser`
- *    6) If you get dependency error run `apt install libssn3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libxdamage1 libxkbcommon0 libpango-1.0-0 libcairo2`.
+ *    6) If you get dependency error run `apt install libnss3 libatk1.0-0t64 libatk-bridge2.0-0t64 libcups2t64 libxdamage1 libxkbcommon0 libpango-1.0-0 libcairo2 libasound2t64`.
  *
  * NOTE 1: If you see the logs spammed with unexpected logs there is probably a chrome process with a webpage
  *       unexpectedly running in the background. Kill it with `killall chrome`
