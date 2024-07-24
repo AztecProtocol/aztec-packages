aae287f28d79229279c4c697aa92b36394540274
PhilWindle <philip.windle@gmail.com>
2024-07-24 17:17:42 +0000

    Grafana updates

--- metrics/grafana/terraform/main.tf
+++ metrics/grafana/terraform/main.tf
@@ -34,7 +34,7 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-resource "aws_service_discovery_service" "grafana" {
+resource "aws_service_discovery_service" "aztec_grafana" {
   name = "aztec-grafana"
 
   health_check_custom_config {
@@ -54,7 +54,7 @@ resource "aws_service_discovery_service" "grafana" {
 }
 
 # Configure an EFS filesystem.
-resource "aws_efs_file_system" "grafana_data_store" {
+resource "aws_efs_file_system" "aztec_grafana_data_store" {
   creation_token = "aztec-grafana-data-store"
 
   tags = {
@@ -71,19 +71,19 @@ resource "aws_efs_file_system" "grafana_data_store" {
 }
 
 resource "aws_efs_mount_target" "private_az1" {
-  file_system_id  = aws_efs_file_system.grafana_data_store.id
+  file_system_id  = aws_efs_file_system.aztec_grafana_data_store.id
   subnet_id       = data.terraform_remote_state.setup_iac.outputs.subnet_az1_private_id
   security_groups = [data.terraform_remote_state.setup_iac.outputs.security_group_private_id]
 }
 
 resource "aws_efs_mount_target" "private_az2" {
-  file_system_id  = aws_efs_file_system.grafana_data_store.id
+  file_system_id  = aws_efs_file_system.aztec_grafana_data_store.id
   subnet_id       = data.terraform_remote_state.setup_iac.outputs.subnet_az2_private_id
   security_groups = [data.terraform_remote_state.setup_iac.outputs.security_group_private_id]
 }
 
 # Define task definition and service.
-resource "aws_ecs_task_definition" "grafana" {
+resource "aws_ecs_task_definition" "aztec_grafana" {
   family                   = "aztec-grafana"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
@@ -94,7 +94,7 @@ resource "aws_ecs_task_definition" "grafana" {
   volume {
     name = "aztec-grafana-efs-data-store"
     efs_volume_configuration {
-      file_system_id = aws_efs_file_system.grafana_data_store.id
+      file_system_id = aws_efs_file_system.aztec_grafana_data_store.id
     }
   }
 
@@ -139,11 +139,11 @@ resource "aws_ecs_task_definition" "grafana" {
 DEFINITIONS
 }
 
-data "aws_ecs_task_definition" "grafana" {
-  task_definition = aws_ecs_task_definition.grafana.family
+data "aws_ecs_task_definition" "aztec_grafana" {
+  task_definition = aws_ecs_task_definition.aztec_grafana.family
 }
 
-resource "aws_ecs_service" "grafana" {
+resource "aws_ecs_service" "aztec_grafana" {
   name                               = "aztec-grafana"
   cluster                            = data.terraform_remote_state.setup_iac.outputs.ecs_cluster_id
   launch_type                        = "FARGATE"
@@ -160,76 +160,76 @@ resource "aws_ecs_service" "grafana" {
     security_groups = [data.terraform_remote_state.setup_iac.outputs.security_group_private_id]
   }
 
-  # load_balancer {
-  #   target_group_arn = aws_alb_target_group.grafana.arn
-  #   container_name   = "aztec-grafana"
-  #   container_port   = 80
-  # }
-
-  service_registries {
-    registry_arn = aws_service_discovery_service.grafana.arn
+  load_balancer {
+    target_group_arn = aws_alb_target_group.aztec_grafana.arn
+    container_name   = "aztec-grafana"
+    container_port   = 80
   }
 
-  task_definition = "${aws_ecs_task_definition.grafana.family}:${max(aws_ecs_task_definition.grafana.revision, data.aws_ecs_task_definition.grafana.revision)}"
+  service_registries {
+    registry_arn = aws_service_discovery_service.aztec_grafana.arn
+  }
+
+  task_definition = "${aws_ecs_task_definition.aztec_grafana.family}:${max(aws_ecs_task_definition.aztec_grafana.revision, data.aws_ecs_task_definition.aztec_grafana.revision)}"
 }
 
 # Logs
-resource "aws_cloudwatch_log_group" "grafana_logs" {
+resource "aws_cloudwatch_log_group" "aztec_grafana_logs" {
   name              = "/fargate/service/aztec-grafana"
   retention_in_days = "14"
 }
 
-# # Configure ALB to route metrics.aztec.network to grafana.
-# resource "aws_alb_target_group" "grafana" {
-#   name                 = "aztec-grafana"
-#   port                 = "80"
-#   protocol             = "HTTP"
-#   target_type          = "ip"
-#   vpc_id               = data.terraform_remote_state.setup_iac.outputs.vpc_id
-#   deregistration_delay = 5
+# Configure ALB to route metrics.aztec.network to grafana.
+resource "aws_alb_target_group" "aztec_grafana" {
+  name                 = "aztec-grafana"
+  port                 = "80"
+  protocol             = "HTTP"
+  target_type          = "ip"
+  vpc_id               = data.terraform_remote_state.setup_iac.outputs.vpc_id
+  deregistration_delay = 5
 
-#   health_check {
-#     path                = "/"
-#     matcher             = "302"
-#     interval            = 10
-#     healthy_threshold   = 2
-#     unhealthy_threshold = 5
-#     timeout             = 3
-#   }
+  health_check {
+    path                = "/"
+    matcher             = "302"
+    interval            = 10
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
+    timeout             = 3
+  }
 
-#   tags = {
-#     name = "grafana"
-#   }
-# }
+  tags = {
+    name = "aztec-grafana"
+  }
+}
 
-# resource "aws_lb_listener_rule" "api" {
-#   listener_arn = data.terraform_remote_state.aztec2_iac.outputs.alb_listener_arn
-#   priority     = 300
+resource "aws_lb_listener_rule" "api" {
+  listener_arn = data.terraform_remote_state.aztec2_iac.outputs.alb_listener_arn
+  priority     = 301
 
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_alb_target_group.grafana.arn
-#   }
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.aztec_grafana.arn
+  }
 
-#   condition {
-#     host_header {
-#       values = ["metrics.aztec.network"]
-#     }
-#   }
-# }
+  condition {
+    host_header {
+      values = ["grafana.aztec.network"]
+    }
+  }
+}
 
-# data "aws_alb" "aztec2" {
-#   arn = data.terraform_remote_state.aztec2_iac.outputs.alb_arn
-# }
+data "aws_alb" "aztec2" {
+  arn = data.terraform_remote_state.aztec2_iac.outputs.alb_arn
+}
 
-# # API metrics DNS entry.
-# resource "aws_route53_record" "metrics" {
-#   zone_id = data.terraform_remote_state.aztec2_iac.outputs.aws_route53_zone_id
-#   name    = "metrics"
-#   type    = "A"
-#   alias {
-#     name                   = data.aws_alb.aztec2.dns_name
-#     zone_id                = data.aws_alb.aztec2.zone_id
-#     evaluate_target_health = true
-#   }
-# }
+# API grafana DNS entry.
+resource "aws_route53_record" "metrics" {
+  zone_id = data.terraform_remote_state.aztec2_iac.outputs.aws_route53_zone_id
+  name    = "aztec-grafana"
+  type    = "A"
+  alias {
+    name                   = data.aws_alb.aztec2.dns_name
+    zone_id                = data.aws_alb.aztec2.zone_id
+    evaluate_target_health = true
+  }
+}
