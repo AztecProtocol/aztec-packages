ab331bb11b29f046e13111c5963a9bf992269e4e
PhilWindle <philip.windle@gmail.com>
2024-07-31 12:21:50 +0000

    Don't wait for transaction if configured not to do so.

--- yarn-project/bot/src/bot.ts
+++ yarn-project/bot/src/bot.ts
@@ -68,7 +68,14 @@ export class Bot {
     this.log.verbose(`Sending tx`, logCtx);
     const tx = batch.send(opts);
 
-    this.log.verbose(`Awaiting tx ${tx.getTxHash()} to be mined (timeout ${this.config.txMinedWaitSeconds}s)`, logCtx);
+    const txHash = tx.getTxHash();
+
+    if (this.config.txMinedWaitSeconds === 0) {
+      this.log.verbose(`Transaction ${txHash} sent, not waiting for it to be mined`);
+      return;
+    }
+
+    this.log.verbose(`Awaiting tx ${txHash} to be mined (timeout ${this.config.txMinedWaitSeconds}s)`, logCtx);
     const receipt = await tx.wait({ timeout: this.config.txMinedWaitSeconds });
 
     this.log.info(`Tx ${receipt.txHash} mined in block ${receipt.blockNumber}`, logCtx);
