7e6f5ec47a170677a9c7be8472837e59242c54c7
IlyasRidhuan <ilyasridhuan@gmail.com>
2024-07-31 12:31:51 +0000

    feat: ts pedersen commit with offset

--- yarn-project/foundation/src/crypto/pedersen/pedersen.wasm.ts
+++ yarn-project/foundation/src/crypto/pedersen/pedersen.wasm.ts
@@ -7,15 +7,18 @@ import { type Fieldable, serializeToFields } from '../../serialize/serialize.js'
  * Create a pedersen commitment (point) from an array of input fields.
  * Left pads any inputs less than 32 bytes.
  */
-export function pedersenCommit(input: Buffer[]) {
-  if (!input.every(i => i.length <= 32)) {
-    throw new Error('All Pedersen Commit input buffers must be <= 32 bytes.');
-  }
-  input = input.map(i => (i.length < 32 ? Buffer.concat([Buffer.alloc(32 - i.length, 0), i]) : i));
-  const point = BarretenbergSync.getSingleton().pedersenCommit(input.map(i => new FrBarretenberg(i)));
-  // toBuffer returns Uint8Arrays (browser/worker-boundary friendly).
-  // TODO: rename toTypedArray()?
-  return [Buffer.from(point.x.toBuffer()), Buffer.from(point.y.toBuffer())];
+export function pedersenCommit(input: Buffer[], offset = 0) {
+    if (!input.every(i => i.length <= 32)) {
+        throw new Error('All Pedersen Commit input buffers must be <= 32 bytes.');
+    }
+    input = input.map(i => (i.length < 32 ? Buffer.concat([Buffer.alloc(32 - i.length, 0), i]) : i));
+    const point = BarretenbergSync.getSingleton().pedersenCommit(
+        input.map(i => new FrBarretenberg(i)),
+        offset,
+    );
+    // toBuffer returns Uint8Arrays (browser/worker-boundary friendly).
+    // TODO: rename toTypedArray()?
+    return [Buffer.from(point.x.toBuffer()), Buffer.from(point.y.toBuffer())];
 }
 
 /**
@@ -25,22 +28,22 @@ export function pedersenCommit(input: Buffer[]) {
  * @returns The pedersen hash.
  */
 export function pedersenHash(input: Fieldable[], index = 0): Fr {
-  const inputFields = serializeToFields(input);
-  return Fr.fromBuffer(
-    Buffer.from(
-      BarretenbergSync.getSingleton()
-        .pedersenHash(
-          inputFields.map(i => new FrBarretenberg(i.toBuffer())), // TODO(#4189): remove this stupid conversion
-          index,
-        )
-        .toBuffer(),
-    ),
-  );
+    const inputFields = serializeToFields(input);
+    return Fr.fromBuffer(
+        Buffer.from(
+            BarretenbergSync.getSingleton()
+                .pedersenHash(
+                    inputFields.map(i => new FrBarretenberg(i.toBuffer())), // TODO(#4189): remove this stupid conversion
+                    index,
+                )
+                .toBuffer(),
+        ),
+    );
 }
 
 /**
  * Create a pedersen hash from an arbitrary length buffer.
  */
 export function pedersenHashBuffer(input: Buffer, index = 0) {
-  return Buffer.from(BarretenbergSync.getSingleton().pedersenHashBuffer(input, index).toBuffer());
+    return Buffer.from(BarretenbergSync.getSingleton().pedersenHashBuffer(input, index).toBuffer());
 }
