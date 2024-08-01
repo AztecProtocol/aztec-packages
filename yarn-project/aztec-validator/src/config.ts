78dbe1bd97cfa6f6f8e5ba5136d91181e5bd1236
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-08-01 21:32:42 +0000

    feat(vc): introduce validator client

---
+++ yarn-project/aztec-validator/src/config.ts
@@ -0,0 +1,8 @@
+
+/**
+ * The Validator Configuration
+ */
+export interface ValidatorConfig {
+    /** The private key of the validator participating in attestation duties */
+    validatorPrivateKey: string;
+}
---
+++ yarn-project/aztec-validator/src/validator.ts
@@ -0,0 +1,9 @@
+
+/** Validator
+ * 
+
+ * 
+ */
+export class Validator {
+
+}
