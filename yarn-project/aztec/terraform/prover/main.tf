cb7c8bf5b457733b8edfa775b1990ea9f4d1dcdf
Alex Gherghisan <alexg@aztecprotocol.com>
2024-08-02 14:27:18 +0000

    fix: set network id

--- yarn-project/aztec/terraform/prover/main.tf
+++ yarn-project/aztec/terraform/prover/main.tf
@@ -240,6 +240,10 @@ resource "aws_ecs_task_definition" "aztec-proving-agent" {
       {
         "name": "TEL_SERVICE_NAME",
         "value": "${var.DEPLOY_TAG}-aztec-proving-agent-group-${count.index + 1}"
+      },
+      {
+        name  = "TEL_NETWORK_ID",
+        value = "${var.DEPLOY_TAG}"
       }
     ],
     "logConfiguration": {
