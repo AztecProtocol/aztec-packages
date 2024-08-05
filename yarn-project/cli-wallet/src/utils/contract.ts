eb4132a3263406dcd09924b5c2c57e2adb4ad56f
thunkar <gregojquiros@gmail.com>
2024-08-05 12:01:09 +0200

    wip

---
+++ yarn-project/cli-wallet/src/utils/contract.ts
@@ -0,0 +1,23 @@
+import { getContractArtifact } from '@aztec/cli/utils';
+import { LogFn } from '@aztec/foundation/log';
+
+import { readdir, stat } from 'fs/promises';
+
+const TARGET_DIR = 'target';
+
+export async function contractArtifactFromWorkspace(log: LogFn, pkg?: string, contractName?: string) {
+  const cwd = process.cwd();
+  await stat(`${cwd}/Nargo.toml`);
+  const filesInTarget = await readdir(`${cwd}/${TARGET_DIR}`);
+  const bestMatch = filesInTarget.filter(
+    file => file.endsWith('.json') && file.includes(pkg || '') && file.includes(contractName || ''),
+  );
+  if (bestMatch.length === 0) {
+    throw new Error('No contract artifacts found in target directory with the specified criteria');
+  } else if (bestMatch.length > 1) {
+    throw new Error(
+      `Multiple contract artifacts found in target directory with the specified criteria ${bestMatch.join(', ')}`,
+    );
+  }
+  return getContractArtifact(`${cwd}/${TARGET_DIR}/${bestMatch[0]}`, log);
+}
