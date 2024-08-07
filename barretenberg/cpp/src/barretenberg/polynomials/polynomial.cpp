8f07c7dcc23aff01ea1cfb7ecbcdb72a49641c0f
ludamad <domuradical@gmail.com>
2024-08-07 19:44:24 +0000

    more impl

--- barretenberg/cpp/src/barretenberg/polynomials/polynomial.cpp
+++ barretenberg/cpp/src/barretenberg/polynomials/polynomial.cpp
@@ -402,6 +402,55 @@ template <typename Fr> Polynomial<Fr>& Polynomial<Fr>::operator*=(const Fr scali
     return *this;
 }
 
+// TODO(#723): This method is used for the transcript aggregation protocol. For convenience we currently enforce that
+// the shift is the same size as the input but this does not need to be the case. Revisit the logic/assertions in this
+// method when that issue is addressed.
+template <typename Fr> void Polynomial<Fr>::set_to_right_shifted(std::span<Fr> coeffs_in, size_t shift_size)
+{
+    // Ensure we're not trying to shift self
+    ASSERT(coefficients_ != coeffs_in.data());
+
+    auto size_in = coeffs_in.size();
+    ASSERT(size_in > 0);
+
+    // Ensure that the last shift_size-many input coefficients are zero to ensure no information is lost in the shift.
+    ASSERT(shift_size <= size_in);
+    for (size_t i = 0; i < shift_size; ++i) {
+        size_t idx = size_in - shift_size - 1;
+        ASSERT(coeffs_in[idx].is_zero());
+    }
+
+    // Set size of self equal to size of input and allocate memory
+    allocate_backing_memory(size_in);
+
+    // Zero out the first shift_size-many coefficients of self
+    memset(static_cast<void*>(coefficients_), 0, sizeof(Fr) * shift_size);
+
+    // Copy all but the last shift_size many input coeffs into self at the shift_size-th index.
+    std::size_t num_to_copy = size_ - shift_size;
+    memcpy(static_cast<void*>(coefficients_ + shift_size),
+           static_cast<void const*>(coeffs_in.data()),
+           sizeof(Fr) * num_to_copy);
+    zero_memory_beyond(size_);
+}
+
+template <typename Fr> void Polynomial<Fr>::add_scaled(std::span<const Fr> other, Fr scaling_factor)
+{
+    const size_t other_size = other.size();
+    ASSERT(in_place_operation_viable(other_size));
+
+    size_t num_threads = calculate_num_threads(other_size);
+    size_t range_per_thread = other_size / num_threads;
+    size_t leftovers = other_size - (range_per_thread * num_threads);
+    parallel_for(num_threads, [&](size_t j) {
+        size_t offset = j * range_per_thread;
+        size_t end = (j == num_threads - 1) ? offset + range_per_thread + leftovers : offset + range_per_thread;
+        for (size_t i = offset; i < end; ++i) {
+            coefficients_[i] += scaling_factor * other[i];
+        }
+    });
+}
+
 /**
  * @brief Returns a Polynomial the left-shift of self.
  *
