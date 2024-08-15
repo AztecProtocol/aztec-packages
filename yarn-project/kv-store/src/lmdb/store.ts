d5875cb73de8dd13b7bfab30b3adebce24a10e06
PhilWindle <philip.windle@gmail.com>
2024-08-15 10:18:25 +0000

    Hopefully fixed forking

--- yarn-project/kv-store/src/lmdb/store.ts
+++ yarn-project/kv-store/src/lmdb/store.ts
@@ -72,7 +72,7 @@ export class AztecLmdbStore implements AztecKVStore {
   async fork() {
     const baseDir = this.path ? dirname(this.path) : tmpdir();
     this.#log.debug(`Forking store with basedir ${baseDir}`);
-    const forkPath = (await mkdtemp(join(baseDir, 'aztec-store-fork-'))) + (this.isEphemeral ? '/data.mdb' : '');
+    const forkPath = (await mkdtemp(join(baseDir, 'aztec-store-fork-'))) + '/data.mdb';
     this.#log.verbose(`Forking store to ${forkPath}`);
     await this.#rootDb.backup(forkPath, false);
     const forkDb = open(forkPath, { noSync: this.isEphemeral });
