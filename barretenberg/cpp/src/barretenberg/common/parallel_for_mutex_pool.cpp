dd5e68727e412657da6d12c5868fb06790b0d195
ludamad <domuradical@gmail.com>
2024-08-19 22:24:19 +0000

    comment

--- barretenberg/cpp/src/barretenberg/common/parallel_for_mutex_pool.cpp
+++ barretenberg/cpp/src/barretenberg/common/parallel_for_mutex_pool.cpp
@@ -125,6 +125,8 @@ namespace bb {
 void parallel_for_mutex_pool(size_t num_iterations, const std::function<void(size_t)>& func)
 {
     static ThreadPool pool(get_num_cpus() - 1);
+    // Note that if this is used safely, we don't need the std::atomic_bool (can use bool), but if we are catching the
+    // mess up case of nesting parallel_for this should be atomic
     static std::atomic_bool nested = false;
     // Check if we are already in a nested parallel_for_mutex_pool call
     bool expected = false;
