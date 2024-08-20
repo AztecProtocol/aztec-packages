b4e14dbbcefbe353f1cec298e02a5c210769b8df
lucasxia01 <lucasxia01@gmail.com>
2024-08-20 17:44:48 +0000

    regenerate script that will regenerate things automatically and updated rebuild script that actually tells you what programs failed to rebuild

--- barretenberg/acir_tests/reset_acir_tests.sh
+++ barretenberg/acir_tests/reset_acir_tests.sh
@@ -1,12 +1,24 @@
+#!/usr/bin/env bash
+
 # Run from within barretenberg/acir_tests
 
-# clean and rebuild noir then compile the test programs
+# Optional argument that can be passed to rebuild.sh
+optional_arg=${1:-}
+
+# Clean and rebuild noir then compile the test programs
 cd ../../noir/noir-repo
 cargo clean
 noirup -p .
-cd test_programs && ./rebuild.sh
+cd test_programs
 
-# remove and repopulate the test artifacts in bberg
+# Check if the optional argument exists and pass it to rebuild.sh
+if [ -n "$optional_arg" ]; then
+    ./rebuild.sh "$optional_arg"
+else
+    ./rebuild.sh
+fi
+
+# Remove and repopulate the test artifacts in bberg
 cd ../../../barretenberg/acir_tests
 rm -rf acir_tests
 ./clone_test_vectors.sh
