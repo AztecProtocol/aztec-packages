0a8f7565c4c08afc5ae83c42857d259df2b9320b
thunkar <gregojquiros@gmail.com>
2024-08-21 13:30:22 +0200

    updated approach

--- yarn-project/cli-wallet/wallet-entrypoint.sh
+++ yarn-project/cli-wallet/wallet-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-socat UNIX-LISTEN:$HOME/ssh-agent.sock,fork TCP:host.docker.internal:12345 &
-SSH_AUTH_SOCK="$HOME/ssh-agent.sock" node --no-warnings /usr/src/yarn-project/cli-wallet/dest/bin/index.js
+socat UNIX-LISTEN:$HOME/ssh-agent.internal.sock,fork TCP:host.docker.internal:${SSH_AUTH_SOCK_SOCAT_PORT} &
+SSH_AUTH_SOCK="$HOME/ssh-agent.internal.sock" node --no-warnings /usr/src/yarn-project/cli-wallet/dest/bin/index.js $@
