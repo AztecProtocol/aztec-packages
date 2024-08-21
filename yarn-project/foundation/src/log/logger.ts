f27c854b3a325171fa136205b20508afa4591f1f
Santiago Palladino <santiago@aztecprotocol.com>
2024-08-20 23:16:45 -0300

    feat: JSON logging
    Adds a LOG_JSON env var that disables the foundation logger and replaces
    it with a winston json logger.

--- yarn-project/foundation/src/log/logger.ts
+++ yarn-project/foundation/src/log/logger.ts
@@ -11,7 +11,7 @@ const DefaultLogLevel = process.env.NODE_ENV === 'test' ? ('silent' as const) : 
 export type LogLevel = (typeof LogLevels)[number];
 
 const envLogLevel = process.env.LOG_LEVEL?.toLowerCase() as LogLevel;
-export const currentLevel = LogLevels.includes(envLogLevel) ? envLogLevel : DefaultLogLevel;
+export let currentLevel = LogLevels.includes(envLogLevel) ? envLogLevel : DefaultLogLevel;
 
 const namespaces = process.env.DEBUG ?? 'aztec:*';
 debug.enable(namespaces);
@@ -62,6 +62,11 @@ const logHandlers: LogHandler[] = [];
  */
 export function onLog(handler: LogHandler) {
   logHandlers.push(handler);
+}
+
+/** Overrides current log level. */
+export function setLevel(level: LogLevel) {
+  currentLevel = level;
 }
 
 /**
