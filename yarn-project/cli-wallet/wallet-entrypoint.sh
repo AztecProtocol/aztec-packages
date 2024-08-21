1ef23c7e7500ced64168d315117cbc8ef2b8e8f4
thunkar <gregojquiros@gmail.com>
2024-08-21 14:56:41 +0000

    handle no SSH_AUTH_SOCK

--- yarn-project/cli-wallet/wallet-entrypoint.sh
+++ yarn-project/cli-wallet/wallet-entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-SOCKET="$HOME/.aztec/aztec-wallet-$RANDOM.sock"
 
 cleanup() {
     kill -9 $SOCAT_PID
     rm -rf $SOCKET
 }
 
-socat UNIX-LISTEN:$SOCKET,fork TCP:host.docker.internal:${SSH_AUTH_SOCK_SOCAT_PORT} &
-SOCAT_PID=$!
-trap cleanup EXIT SIGKILL SIGTERM
-SSH_AUTH_SOCK="$SOCKET" node --no-warnings /usr/src/yarn-project/cli-wallet/dest/bin/index.js $@
+if [[ -n "${SSH_AUTH_SOCK_SOCAT_PORT:-}" ]]; then
+    SOCKET="$HOME/.aztec/aztec-wallet-$RANDOM.sock"
+    socat UNIX-LISTEN:$SOCKET,fork TCP:host.docker.internal:${SSH_AUTH_SOCK_SOCAT_PORT} &
+    SOCAT_PID=$!
+    trap cleanup EXIT SIGKILL SIGTERM
+fi
+
+SSH_AUTH_SOCK="${SOCKET:-}" node --no-warnings /usr/src/yarn-project/cli-wallet/dest/bin/index.js $@
