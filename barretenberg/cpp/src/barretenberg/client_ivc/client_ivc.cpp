692e1f1ed78b19099ee46636e8c8e1eeae1a3c88
lucasxia01 <lucasxia01@gmail.com>
2024-08-21 20:21:52 +0000

    missed subtle merge conflict

--- barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.cpp
+++ barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.cpp
@@ -68,8 +68,14 @@ void ClientIVC::accumulate(std::unique_ptr<ClientCircuit> circuit,
     // If a previous fold proof exists, add a recursive folding verification to the circuit
     if (!fold_output.proof.empty()) {
         BB_OP_COUNT_TIME_NAME("construct_circuits");
-        FoldingRecursiveVerifier verifier{ &(*circuit), { verifier_accumulator, { instance_vk } } };
-        auto verifier_accum = verifier.verify_folding_proof(fold_output.proof);
+
+        // Construct stdlib accumulator, vkey and proof
+        auto stdlib_verifier_accum = std::make_shared<RecursiveVerifierInstance>(&*circuit, verifier_accumulator);
+        auto stdlib_instance_vk = std::make_shared<RecursiveVerificationKey>(&*circuit, instance_vk);
+        auto stdlib_proof = bb::convert_proof_to_witness(&*circuit, fold_output.proof);
+
+        FoldingRecursiveVerifier verifier{ &*circuit, stdlib_verifier_accum, { stdlib_instance_vk } };
+        auto verifier_accum = verifier.verify_folding_proof(stdlib_proof);
         verifier_accumulator = std::make_shared<VerifierInstance>(verifier_accum->get_value());
     }
 
@@ -100,7 +106,7 @@ void ClientIVC::accumulate(std::unique_ptr<ClientCircuit> circuit,
         initialized = true;
     } else { // Otherwise, fold the new instance into the accumulator
         FoldingProver folding_prover({ fold_output.accumulator, prover_instance });
-        fold_output = folding_prover.fold_instances();
+        fold_output = folding_prover.prove();
     }
     // forget the prover_instance now?
     prover_instance = nullptr;
