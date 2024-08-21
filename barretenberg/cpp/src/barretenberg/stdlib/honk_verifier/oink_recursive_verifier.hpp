eb04728bdb3e19bdfe49a75f31075b9c9876afca
ledwards2225 <l.edwards.d@gmail.com>
2024-08-21 20:49:27 +0000

    use oink rec verifier in ultra rec verifier

---
+++ barretenberg/cpp/src/barretenberg/stdlib/honk_verifier/oink_recursive_verifier.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "barretenberg/stdlib/transcript/transcript.hpp"
+#include "barretenberg/stdlib_circuit_builders/mega_recursive_flavor.hpp"
+#include "barretenberg/stdlib_circuit_builders/ultra_recursive_flavor.hpp"
+
+namespace bb::stdlib::recursion::honk {
+
+template <typename Flavor> class OinkRecursiveVerifier_ {
+  public:
+    using FF = typename Flavor::FF;
+    using Commitment = typename Flavor::Commitment;
+    using GroupElement = typename Flavor::GroupElement;
+    using VerificationKey = typename Flavor::VerificationKey;
+    using Builder = typename Flavor::CircuitBuilder;
+    using RelationSeparator = typename Flavor::RelationSeparator;
+    using Transcript = bb::BaseTranscript<bb::stdlib::recursion::honk::StdlibTranscriptParams<Builder>>;
+
+    struct Output {
+        bb::RelationParameters<typename Flavor::FF> relation_parameters;
+        typename Flavor::WitnessCommitments commitments;
+        std::vector<typename Flavor::FF> public_inputs;
+        typename Flavor::RelationSeparator alphas;
+    };
+
+    explicit OinkRecursiveVerifier_(Builder* builder,
+                                    const std::shared_ptr<VerificationKey>& vkey,
+                                    std::shared_ptr<Transcript> transcript);
+
+    Output verify();
+
+    std::shared_ptr<VerificationKey> key;
+    Builder* builder;
+    std::shared_ptr<Transcript> transcript;
+};
+
+} // namespace bb::stdlib::recursion::honk
