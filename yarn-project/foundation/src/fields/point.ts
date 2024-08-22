168054b77928027d393e1c630112c5575023301b
benesjan <janbenes1234@gmail.com>
2024-08-22 07:14:45 +0000

    linking TODO

--- yarn-project/foundation/src/fields/point.ts
+++ yarn-project/foundation/src/fields/point.ts
@@ -226,7 +226,7 @@ export class Point {
     /* eslint-enable camelcase */
   }
 
-  // Used for IvpkM, OvpkM, NpkM and TpkM
+  // Used for IvpkM, OvpkM, NpkM and TpkM. TODO(#8124): Consider removing this method.
   toWrappedNoirStruct() {
     /* eslint-disable camelcase */
     return { inner: this.toNoirStruct() };
