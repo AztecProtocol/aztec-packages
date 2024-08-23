a3cdfb0d055510ea89061099695fc1b57c01120f
ledwards2225 <l.edwards.d@gmail.com>
2024-08-23 21:03:30 +0000

    instance in oink rec verifier

--- barretenberg/cpp/src/barretenberg/stdlib/honk_verifier/oink_recursive_verifier.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/honk_verifier/oink_recursive_verifier.cpp
@@ -9,10 +9,10 @@ namespace bb::stdlib::recursion::honk {
 
 template <typename Flavor>
 OinkRecursiveVerifier_<Flavor>::OinkRecursiveVerifier_(Builder* builder,
-                                                       const std::shared_ptr<VerificationKey>& vkey,
+                                                       const std::shared_ptr<Instance>& instance,
                                                        std::shared_ptr<Transcript> transcript,
                                                        std::string domain_separator)
-    : key(vkey)
+    : instance(instance)
     , builder(builder)
     , transcript(transcript)
     , domain_separator(std::move(domain_separator))
@@ -22,7 +22,7 @@ OinkRecursiveVerifier_<Flavor>::OinkRecursiveVerifier_(Builder* builder,
  * @brief This function constructs a recursive verifier circuit for a native Ultra Honk proof of a given flavor.
  * @return Output aggregation object
  */
-template <typename Flavor> OinkRecursiveVerifier_<Flavor>::Output OinkRecursiveVerifier_<Flavor>::verify()
+template <typename Flavor> void OinkRecursiveVerifier_<Flavor>::verify()
 {
     using CommitmentLabels = typename Flavor::CommitmentLabels;
     using RelationParams = ::bb::RelationParameters<FF>;
@@ -42,7 +42,7 @@ template <typename Flavor> OinkRecursiveVerifier_<Flavor>::Output OinkRecursiveV
     // ASSERT(static_cast<uint32_t>(pub_inputs_offset.get_value()) == key->pub_inputs_offset);
 
     std::vector<FF> public_inputs;
-    for (size_t i = 0; i < key->num_public_inputs; ++i) {
+    for (size_t i = 0; i < instance->verification_key->num_public_inputs; ++i) {
         public_inputs.emplace_back(
             transcript->template receive_from_prover<FF>(domain_separator + "public_input_" + std::to_string(i)));
     }
@@ -90,7 +90,7 @@ template <typename Flavor> OinkRecursiveVerifier_<Flavor>::Output OinkRecursiveV
     }
 
     const FF public_input_delta = compute_public_input_delta<Flavor>(
-        public_inputs, beta, gamma, circuit_size, static_cast<uint32_t>(key->pub_inputs_offset));
+        public_inputs, beta, gamma, circuit_size, static_cast<uint32_t>(instance->verification_key->pub_inputs_offset));
 
     relation_parameters = RelationParameters<FF>{ eta, eta_two, eta_three, beta, gamma, public_input_delta };
 
@@ -101,11 +101,6 @@ template <typename Flavor> OinkRecursiveVerifier_<Flavor>::Output OinkRecursiveV
     for (size_t idx = 0; idx < alphas.size(); idx++) {
         alphas[idx] = transcript->template get_challenge<FF>(domain_separator + "alpha_" + std::to_string(idx));
     }
-
-    return { .relation_parameters = relation_parameters,
-             .commitments = std::move(commitments),
-             .public_inputs = public_inputs,
-             .alphas = alphas };
 }
 
 template class OinkRecursiveVerifier_<bb::UltraRecursiveFlavor_<UltraCircuitBuilder>>;
