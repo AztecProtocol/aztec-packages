7724aeab31b3ff593606a7b234871130e98be629
AztecBot <tech@aztecprotocol.com>
2024-08-25 08:01:55 +0000

    [1 changes] chore: Add docs for each comptime method (https://github.com/noir-lang/noir/pull/5802) chore: Add comptime docs (https://github.com/noir-lang/noir/pull/5800) fix: Handle multiple entry points for Brillig call stack resolution after metadata deduplication (https://github.com/noir-lang/noir/pull/5788) fix(acir_gen): Nested dynamic array initialization (https://github.com/noir-lang/noir/pull/5810) fix: honor function visibility in LSP completion (https://github.com/noir-lang/noir/pull/5809) feat: LSP completion now works better in the middle of idents (https://github.com/noir-lang/noir/pull/5795) feat: Explicit Associated Types & Constants (https://github.com/noir-lang/noir/pull/5739) feat: add `Expr::as_cast` and `UnresolvedType::is_field` (https://github.com/noir-lang/noir/pull/5801) feat: add `Expr` methods: as_comptime, as_unsafe, is_break, is_continue (https://github.com/noir-lang/noir/pull/5799) fix: do not use predicate for index in array operation, when the index is safe (https://github.com/noir-lang/noir/pull/5779) chore: Toggle underconstrained check (https://github.com/noir-lang/noir/pull/5724) feat: Add `Expr::as_block` and `Expr::has_semicolon` (https://github.com/noir-lang/noir/pull/5784) feat: LSP hover and go-to-definition for crates (https://github.com/noir-lang/noir/pull/5786) fix(acvm): Clear ACIR call stack after successful circuit execution (https://github.com/noir-lang/noir/pull/5783) chore: sanitize url's to only allow github (https://github.com/noir-lang/noir/pull/5776) chore: enable constant inputs for more blackbox (https://github.com/noir-lang/noir/pull/5647) chore: move sha2 functions into the `hash` module (https://github.com/noir-lang/noir/pull/5768)

--- noir/noir-repo/tooling/debugger/src/context.rs
+++ noir/noir-repo/tooling/debugger/src/context.rs
@@ -442,16 +442,15 @@ impl<'a, B: BlackBoxFunctionSolver<FieldElement>> DebugContext<'a, B> {
         self.debug_artifact.debug_symbols[debug_location.circuit_id as usize]
             .opcode_location(&debug_location.opcode_location)
             .unwrap_or_else(|| {
-                if let Some(brillig_function_id) = debug_location.brillig_function_id {
+                if let (Some(brillig_function_id), Some(brillig_location)) = (
+                    debug_location.brillig_function_id,
+                    debug_location.opcode_location.to_brillig_location(),
+                ) {
                     let brillig_locations = self.debug_artifact.debug_symbols
                         [debug_location.circuit_id as usize]
                         .brillig_locations
                         .get(&brillig_function_id);
-                    brillig_locations
-                        .unwrap()
-                        .get(&debug_location.opcode_location)
-                        .cloned()
-                        .unwrap_or_default()
+                    brillig_locations.unwrap().get(&brillig_location).cloned().unwrap_or_default()
                 } else {
                     vec![]
                 }
@@ -566,7 +565,7 @@ impl<'a, B: BlackBoxFunctionSolver<FieldElement>> DebugContext<'a, B> {
         &mut self,
         call_info: AcirCallWaitInfo<FieldElement>,
     ) -> DebugCommandResult {
-        let callee_circuit = &self.circuits[call_info.id as usize];
+        let callee_circuit = &self.circuits[call_info.id.as_usize()];
         let callee_witness_map = call_info.initial_witness;
         let callee_acvm = ACVM::new(
             self.backend,
@@ -578,7 +577,7 @@ impl<'a, B: BlackBoxFunctionSolver<FieldElement>> DebugContext<'a, B> {
         let caller_acvm = std::mem::replace(&mut self.acvm, callee_acvm);
         self.acvm_stack
             .push(ExecutionFrame { circuit_id: self.current_circuit_id, acvm: caller_acvm });
-        self.current_circuit_id = call_info.id;
+        self.current_circuit_id = call_info.id.0;
 
         // Explicitly handling the new ACVM status here handles two edge cases:
         // 1. there is a breakpoint set at the beginning of a circuit
@@ -596,7 +595,7 @@ impl<'a, B: BlackBoxFunctionSolver<FieldElement>> DebugContext<'a, B> {
         let ACVMStatus::RequiresAcirCall(call_info) = self.acvm.get_status() else {
             unreachable!("Resolving an ACIR call, the caller is in an invalid state");
         };
-        let acir_to_call = &self.circuits[call_info.id as usize];
+        let acir_to_call = &self.circuits[call_info.id.as_usize()];
 
         let mut call_resolved_outputs = Vec::new();
         for return_witness_index in acir_to_call.return_values.indices() {
@@ -660,8 +659,9 @@ impl<'a, B: BlackBoxFunctionSolver<FieldElement>> DebugContext<'a, B> {
     fn get_current_acir_index(&self) -> Option<usize> {
         self.get_current_debug_location().map(|debug_location| {
             match debug_location.opcode_location {
-                OpcodeLocation::Acir(acir_index) => acir_index,
-                OpcodeLocation::Brillig { acir_index, .. } => acir_index,
+                OpcodeLocation::Acir(acir_index) | OpcodeLocation::Brillig { acir_index, .. } => {
+                    acir_index
+                }
             }
         })
     }
@@ -893,8 +893,19 @@ fn build_source_to_opcode_debug_mappings(
         );
 
         for (brillig_function_id, brillig_locations_map) in &debug_symbols.brillig_locations {
+            let brillig_locations_map = brillig_locations_map
+                .iter()
+                .map(|(key, val)| {
+                    (
+                        // TODO: this is a temporary placeholder until the debugger is updated to handle the new brillig debug locations.
+                        OpcodeLocation::Brillig { acir_index: 0, brillig_index: key.0 },
+                        val.clone(),
+                    )
+                })
+                .collect();
+
             add_opcode_locations_map(
-                brillig_locations_map,
+                &brillig_locations_map,
                 &mut result,
                 &simple_files,
                 circuit_id,
@@ -946,7 +957,7 @@ mod tests {
             brillig::IntegerBitSize,
             circuit::{
                 brillig::{BrilligFunctionId, BrilligInputs, BrilligOutputs},
-                opcodes::{BlockId, BlockType},
+                opcodes::{AcirFunctionId, BlockId, BlockType},
             },
             native_types::Expression,
             AcirField,
@@ -1210,7 +1221,12 @@ mod tests {
                     outputs: vec![],
                     predicate: None,
                 },
-                Opcode::Call { id: 1, inputs: vec![], outputs: vec![], predicate: None },
+                Opcode::Call {
+                    id: AcirFunctionId(1),
+                    inputs: vec![],
+                    outputs: vec![],
+                    predicate: None,
+                },
                 Opcode::AssertZero(Expression::default()),
             ],
             ..Circuit::default()
