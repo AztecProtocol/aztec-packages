b5e0656d8100f563ee2a3a136ddd5f4acb26e11f
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-08-27 17:12:30 +0000

    fix: test flakey

--- yarn-project/p2p/src/service/reqresp/p2p_client.integration.test.ts
+++ yarn-project/p2p/src/service/reqresp/p2p_client.integration.test.ts
@@ -5,7 +5,7 @@ import { sleep } from '@aztec/foundation/sleep';
 import { type AztecKVStore } from '@aztec/kv-store';
 import { openTmpStore } from '@aztec/kv-store/utils';
 
-import { afterEach, beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { describe, expect, it, jest } from '@jest/globals';
 import { generatePrivateKey } from 'viem/accounts';
 
 import { type AttestationPool } from '../../attestation_pool/attestation_pool.js';
@@ -25,7 +25,7 @@ type Mockify<T> = {
   [P in keyof T]: ReturnType<typeof jest.fn>;
 };
 
-const BEFORE_EACH_TIMEOUT = 60000;
+const TEST_TIMEOUT = 60000;
 
 const BOOT_NODE_UDP_PORT = 40400;
 async function createBootstrapNode(port: number) {
@@ -59,128 +59,133 @@ describe('Req Resp p2p client integration', () => {
   let attestationPool: Mockify<AttestationPool>;
   let blockSource: MockBlockSource;
   let kvStore: AztecKVStore;
-  const clients: P2PClient[] = [];
   const logger = createDebugLogger('p2p-client-integration-test');
 
-  let bootstrapNode: BootstrapNode;
+  const makeBootstrapNode = async (): Promise<[BootstrapNode, string]> => {
+    const bootstrapNode = await createBootstrapNode(BOOT_NODE_UDP_PORT);
+    const enr = bootstrapNode.getENR().encodeTxt();
+    return [bootstrapNode, enr];
+  };
 
-  // This test differs from the traditional p2p client test as we do not mock out
-  // the p2p client's functionality, as we need it to connect to peers and exchange
-  // information
-  beforeEach(async () => {
-    bootstrapNode = await createBootstrapNode(BOOT_NODE_UDP_PORT);
-    const bootstrapNodeEnr = bootstrapNode.getENR().encodeTxt();
-    logger.info(`Created bootstrap node with enr: ${bootstrapNodeEnr}`);
+  const createClients = async (numberOfPeers: number, bootstrapNodeEnr: string): Promise<P2PClient[]> => {
+    const clients: P2PClient[] = [];
+    const peerIdPrivateKeys = generatePeerIdPrivateKeys(numberOfPeers);
+    for (let i = 0; i < numberOfPeers; i++) {
+      // Note these bindings are important
+      const addr = `127.0.0.1:${i + 1 + BOOT_NODE_UDP_PORT}`;
+      const listenAddr = `0.0.0.0:${i + 1 + BOOT_NODE_UDP_PORT}`;
+      const config: P2PConfig = {
+        p2pEnabled: true,
+        peerIdPrivateKey: peerIdPrivateKeys[i],
+        tcpListenAddress: listenAddr, // run on port 0
+        udpListenAddress: listenAddr,
+        tcpAnnounceAddress: addr,
+        udpAnnounceAddress: addr,
+        l2QueueSize: 1,
+        bootstrapNodes: [bootstrapNodeEnr],
+        blockCheckIntervalMS: 1000,
+        peerCheckIntervalMS: 1000,
+        transactionProtocol: '',
+        minPeerCount: 1,
+        maxPeerCount: 10,
+        keepProvenTxsInPoolFor: 0,
+        queryForIp: false,
+      };
 
-    const createClients = async (numberOfPeers: number) => {
-      const peerIdPrivateKeys = generatePeerIdPrivateKeys(numberOfPeers);
-      for (let i = 0; i < numberOfPeers; i++) {
-        // Note these bindings are important
-        const addr = `127.0.0.1:${i + 1 + BOOT_NODE_UDP_PORT}`;
-        const listenAddr = `0.0.0.0:${i + 1 + BOOT_NODE_UDP_PORT}`;
-        const config: P2PConfig = {
-          p2pEnabled: true,
-          peerIdPrivateKey: peerIdPrivateKeys[i],
-          tcpListenAddress: listenAddr, // run on port 0
-          udpListenAddress: listenAddr,
-          tcpAnnounceAddress: addr,
-          udpAnnounceAddress: addr,
-          l2QueueSize: 1,
-          bootstrapNodes: [bootstrapNodeEnr],
-          blockCheckIntervalMS: 1000,
-          peerCheckIntervalMS: 1000,
-          transactionProtocol: '',
-          minPeerCount: 1,
-          maxPeerCount: 10,
-          keepProvenTxsInPoolFor: 0,
-          queryForIp: false,
-        };
+      txPool = {
+        addTxs: jest.fn(() => {}),
+        getTxByHash: jest.fn().mockReturnValue(undefined),
+        deleteTxs: jest.fn(),
+        getAllTxs: jest.fn().mockReturnValue([]),
+        getAllTxHashes: jest.fn().mockReturnValue([]),
+        getMinedTxHashes: jest.fn().mockReturnValue([]),
+        getPendingTxHashes: jest.fn().mockReturnValue([]),
+        getTxStatus: jest.fn().mockReturnValue(undefined),
+        markAsMined: jest.fn(),
+      };
 
-        txPool = {
-          addTxs: jest.fn(() => {}),
-          getTxByHash: jest.fn().mockReturnValue(undefined),
-          deleteTxs: jest.fn(),
-          getAllTxs: jest.fn().mockReturnValue([]),
-          getAllTxHashes: jest.fn().mockReturnValue([]),
-          getMinedTxHashes: jest.fn().mockReturnValue([]),
-          getPendingTxHashes: jest.fn().mockReturnValue([]),
-          getTxStatus: jest.fn().mockReturnValue(undefined),
-          markAsMined: jest.fn(),
-        };
+      attestationPool = {
+        addAttestations: jest.fn(),
+        deleteAttestations: jest.fn(),
+        deleteAttestationsForSlot: jest.fn(),
+        getAttestationsForSlot: jest.fn().mockReturnValue(undefined),
+      };
 
-        attestationPool = {
-          addAttestations: jest.fn(),
-          deleteAttestations: jest.fn(),
-          deleteAttestationsForSlot: jest.fn(),
-          getAttestationsForSlot: jest.fn().mockReturnValue(undefined),
-        };
+      blockSource = new MockBlockSource();
+      kvStore = openTmpStore();
+      const client = await createP2PClient(
+        config,
+        kvStore,
+        txPool as unknown as TxPool,
+        attestationPool as unknown as AttestationPool,
+        blockSource,
+      );
 
-        blockSource = new MockBlockSource();
-        kvStore = openTmpStore();
-        const client = await createP2PClient(
-          config,
-          kvStore,
-          txPool as unknown as TxPool,
-          attestationPool as unknown as AttestationPool,
-          blockSource,
-        );
+      await client.start();
+      clients.push(client);
 
-        await client.start();
-        clients.push(client);
+      logger.info(`Creating client ${i}`);
+    }
 
-        logger.info(`Creating client ${i}`);
-      }
+    logger.info(`Created ${NUMBER_OF_PEERS} clients`);
+    await Promise.all(clients.map(client => client.isReady()));
+    logger.info(`Clients ready`);
+    return clients;
+  };
 
-      logger.info(`Created ${NUMBER_OF_PEERS} clients`);
-    };
+  // Shutdown all test clients
+  const shutdown = async (clients: P2PClient[], bootnode: BootstrapNode) => {
+    await Promise.all([bootnode.stop(), ...clients.map(client => client.stop())]);
+    await sleep(1000);
+  };
 
-    await createClients(NUMBER_OF_PEERS);
-  }, BEFORE_EACH_TIMEOUT);
+  it(
+    'Returns undefined if unable to find a transaction from another peer',
+    async () => {
+      // We want to create a set of nodes and request transaction from them
+      // Not using a before each as a the wind down is not working as expected
+      const [bootstrapNode, bootstrapNodeEnr] = await makeBootstrapNode();
+      const clients = await createClients(NUMBER_OF_PEERS, bootstrapNodeEnr);
+      const [client1] = clients;
 
-  afterEach(async () => {
-    // Make sure all clients are stopped
-    await Promise.all(clients.map(client => client.stop()));
-    logger.info(`Stopped nodes`);
+      await sleep(2000);
 
-    await bootstrapNode.stop();
-    logger.info(`Stopped bootstrap node`);
+      // Perform a get tx request from client 1
+      const tx = mockTx();
+      const txHash = tx.getTxHash();
 
-    await sleep(5000);
-  });
+      const requestedTx = await client1.requestTxByHash(txHash);
+      expect(requestedTx).toBeUndefined();
 
-  it('Returns undefined if unable to find a transaction from another peer', async () => {
-    // We want to create a set of nodes and request transaction from them
-    const [client1] = clients;
+      await shutdown(clients, bootstrapNode);
+    },
+    TEST_TIMEOUT,
+  );
 
-    await sleep(2000);
+  it(
+    'Can request a transaction from another peer',
+    async () => {
+      // We want to create a set of nodes and request transaction from them
+      const [bootstrapNode, bootstrapNodeEnr] = await makeBootstrapNode();
+      const clients = await createClients(NUMBER_OF_PEERS, bootstrapNodeEnr);
+      const [client1] = clients;
 
-    // Perform a get tx request from client 1
-    const tx = mockTx();
-    const txHash = tx.getTxHash();
+      // Give the nodes time to discover each other
+      await sleep(6000);
 
-    const requestedTx = await client1.requestTxByHash(txHash);
-    expect(requestedTx).toBeUndefined();
-  });
+      // Perform a get tx request from client 1
+      const tx = mockTx();
+      const txHash = tx.getTxHash();
+      // Mock the tx pool to return the tx we are looking for
+      txPool.getTxByHash.mockImplementationOnce(() => tx);
 
-  it('Can request a transaction from another peer', async () => {
-    // We want to create a set of nodes and request transaction from them
-    const [client1] = clients;
+      const requestedTx = await client1.requestTxByHash(txHash);
 
-    // Doubly make sure we started
-    await Promise.all(clients.map(client => client.start()));
+      // Expect the tx to be the returned tx to be the same as the one we mocked
+      expect(requestedTx?.toBuffer()).toStrictEqual(tx.toBuffer());
 
-    // Give the nodes time to discover each other
-    await sleep(5000);
-
-    // Perform a get tx request from client 1
-    const tx = mockTx();
-    const txHash = tx.getTxHash();
-    // Mock the tx pool to return the tx we are looking for
-    txPool.getTxByHash.mockImplementationOnce(() => tx);
-
-    const requestedTx = await client1.requestTxByHash(txHash);
-
-    // Expect the tx to be the returned tx to be the same as the one we mocked
-    expect(requestedTx?.toBuffer()).toStrictEqual(tx.toBuffer());
-  });
+      await shutdown(clients, bootstrapNode);
+    },
+    TEST_TIMEOUT,
+  );
 });
