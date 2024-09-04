e1dc9878de06a1f3d4cde9bbcf652ac342951d52
PhilWindle <60546371+PhilWindle@users.noreply.github.com>
2024-09-04 19:30:29 +0100

    chore: Merge provernet to master (#8373)
    This PR brings the provernet changes back to master
    
    ---------
    
    Co-authored-by: Santiago Palladino <santiago@aztecprotocol.com>
    Co-authored-by: Alex Gherghisan <alexghr@users.noreply.github.com>
    Co-authored-by: spypsy <spypsy@users.noreply.github.com>

--- yarn-project/aztec/terraform/proof-verifier/main.tf
+++ yarn-project/aztec/terraform/proof-verifier/main.tf
@@ -34,13 +34,13 @@ data "terraform_remote_state" "setup_iac" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "aztec-bot-log-group" {
-  name              = "/fargate/service/${var.DEPLOY_TAG}/aztec-bot"
+resource "aws_cloudwatch_log_group" "aztec-proof-verifier-log-group" {
+  name              = "/fargate/service/${var.DEPLOY_TAG}/aztec-proof-verifier"
   retention_in_days = 14
 }
 
-resource "aws_service_discovery_service" "aztec-bot" {
-  name = "${var.DEPLOY_TAG}-aztec-bot"
+resource "aws_service_discovery_service" "aztec-proof-verifier" {
+  name = "${var.DEPLOY_TAG}-aztec-proof-verifier"
 
   health_check_custom_config {
     failure_threshold = 1
@@ -74,14 +74,14 @@ data "template_file" "user_data" {
   template = <<EOF
 #!/bin/bash
 echo ECS_CLUSTER=${data.terraform_remote_state.setup_iac.outputs.ecs_cluster_name} >> /etc/ecs/ecs.config
-echo 'ECS_INSTANCE_ATTRIBUTES={"group": "${var.DEPLOY_TAG}-bot"}' >> /etc/ecs/ecs.config
+echo 'ECS_INSTANCE_ATTRIBUTES={"group": "${var.DEPLOY_TAG}-proof-verifier"}' >> /etc/ecs/ecs.config
 EOF
 }
 
-resource "aws_launch_template" "bot_launch_template" {
-  name                   = "${var.DEPLOY_TAG}-launch-template"
+resource "aws_launch_template" "proof_verifier_launch_template" {
+  name                   = "${var.DEPLOY_TAG}-pf-launch-template"
   image_id               = "ami-0cd4858f2b923aa6b"
-  instance_type          = "c6a.2xlarge"
+  instance_type          = "m4.2xlarge" // 8 cores, 32 GB
   vpc_security_group_ids = [data.terraform_remote_state.setup_iac.outputs.security_group_private_id]
 
   iam_instance_profile {
@@ -95,17 +95,17 @@ resource "aws_launch_template" "bot_launch_template" {
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name       = "${var.DEPLOY_TAG}-bot"
+      Name       = "${var.DEPLOY_TAG}-proof-verifier"
       prometheus = ""
     }
   }
 }
 
-resource "aws_ec2_fleet" "bot_fleet" {
+resource "aws_ec2_fleet" "proof_verifier_fleet" {
   launch_template_config {
     launch_template_specification {
-      launch_template_id = aws_launch_template.bot_launch_template.id
-      version            = aws_launch_template.bot_launch_template.latest_version
+      launch_template_id = aws_launch_template.proof_verifier_launch_template.id
+      version            = aws_launch_template.proof_verifier_launch_template.latest_version
     }
 
     override {
@@ -123,21 +123,17 @@ resource "aws_ec2_fleet" "bot_fleet" {
 
   target_capacity_specification {
     default_target_capacity_type = "on-demand"
-    total_target_capacity        = var.BOT_COUNT
+    total_target_capacity        = 1
     spot_target_capacity         = 0
-    on_demand_target_capacity    = var.BOT_COUNT
+    on_demand_target_capacity    = 1
   }
 
   terminate_instances                 = true
   terminate_instances_with_expiration = true
 }
 
-locals {
-  api_prefix = "/${var.DEPLOY_TAG}/aztec-bot/${var.BOT_API_KEY}"
-}
-
-resource "aws_ecs_task_definition" "aztec-bot" {
-  family                   = "${var.DEPLOY_TAG}-aztec-bot"
+resource "aws_ecs_task_definition" "aztec-proof-verifier" {
+  family                   = "${var.DEPLOY_TAG}-aztec-proof-verifier"
   network_mode             = "awsvpc"
   requires_compatibilities = ["EC2"]
   execution_role_arn       = data.terraform_remote_state.setup_iac.outputs.ecs_task_execution_role_arn
@@ -145,43 +141,39 @@ resource "aws_ecs_task_definition" "aztec-bot" {
 
   container_definitions = jsonencode([
     {
-      name              = "${var.DEPLOY_TAG}-aztec-bot"
+      name              = "${var.DEPLOY_TAG}-aztec-proof-verifier"
       image             = "${var.DOCKERHUB_ACCOUNT}/aztec:${var.DEPLOY_TAG}"
-      command           = ["start", "--bot", "--pxe"]
+      command           = ["start", "--proof-verifier"]
       essential         = true
       cpu               = 8192
-      memoryReservation = 14336
+      memoryReservation = 30720
       portMappings = [
         {
           containerPort = 80
-          hostPort      = 80
         }
       ]
       environment = [
-        { name = "BOT_PRIVATE_KEY", value = var.BOT_PRIVATE_KEY },
-        { name = "BOT_NO_START", value = var.BOT_NO_START },
-        { name = "BOT_TX_INTERVAL_SECONDS", value = var.BOT_TX_INTERVAL_SECONDS },
+        { name = "PROOF_VERIFIER_L1_START_BLOCK", value = "15918000" },
+        { name = "PROOF_VERIFIER_POLL_INTERVAL_MS", value = tostring(var.PROOF_VERIFIER_POLL_INTERVAL_MS) },
+        { name = "ETHEREUM_HOST", value = var.ETHEREUM_HOST },
+        { name = "L1_CHAIN_ID", value = tostring(var.L1_CHAIN_ID) },
+        { name = "ROLLUP_CONTRACT_ADDRESS", value = var.ROLLUP_CONTRACT_ADDRESS },
+        {
+          name  = "OTEL_EXPORTER_OTLP_METRICS_ENDPOINT"
+          value = "http://aztec-otel.local:4318/v1/metrics"
+        },
+        {
+          name  = "OTEL_SERVICE_NAME"
+          value = "${var.DEPLOY_TAG}-aztec-proof-verifier"
+        },
         { name = "LOG_LEVEL", value = var.LOG_LEVEL },
-        { name = "AZTEC_PORT", value = "80" },
-        { name = "API_PREFIX", value = local.api_prefix },
-        { name = "BOT_PRIVATE_TRANSFERS_PER_TX", value = var.BOT_PRIVATE_TRANSFERS_PER_TX },
-        { name = "BOT_PUBLIC_TRANSFERS_PER_TX", value = var.BOT_PUBLIC_TRANSFERS_PER_TX },
-        { name = "BOT_TX_MINED_WAIT_SECONDS", value = var.BOT_TX_MINED_WAIT_SECONDS },
-        { name = "BOT_FOLLOW_CHAIN", value = var.BOT_FOLLOW_CHAIN },
-        { name = "AZTEC_NODE_URL", value = "http://${var.DEPLOY_TAG}-aztec-node-1.local/${var.DEPLOY_TAG}/aztec-node-1/${var.API_KEY}" },
-        { name = "PXE_PROVER_ENABLED", value = tostring(var.PROVING_ENABLED) },
         { name = "NETWORK", value = var.DEPLOY_TAG },
-        { name = "BOT_FLUSH_SETUP_TRANSACTIONS", value = tostring(var.BOT_FLUSH_SETUP_TRANSACTIONS) },
-        { name = "BOT_MAX_PENDING_TXS", value = tostring(var.BOT_MAX_PENDING_TXS) },
-        { name = "BOT_SKIP_PUBLIC_SIMULATION", value = tostring(var.BOT_SKIP_PUBLIC_SIMULATION) },
-        { name = "BOT_L2_GAS_LIMIT", value = var.BOT_L2_GAS_LIMIT },
-        { name = "BOT_DA_GAS_LIMIT", value = var.BOT_DA_GAS_LIMIT },
         { name = "LOG_JSON", value = "1" }
       ]
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          "awslogs-group"         = aws_cloudwatch_log_group.aztec-bot-log-group.name
+          "awslogs-group"         = aws_cloudwatch_log_group.aztec-proof-verifier-log-group.name
           "awslogs-region"        = "eu-west-2"
           "awslogs-stream-prefix" = "ecs"
         }
@@ -190,11 +182,11 @@ resource "aws_ecs_task_definition" "aztec-bot" {
   ])
 }
 
-resource "aws_ecs_service" "aztec-bot" {
-  name                               = "${var.DEPLOY_TAG}-aztec-bot"
+resource "aws_ecs_service" "aztec-proof-verifier" {
+  name                               = "${var.DEPLOY_TAG}-aztec-proof-verifier"
   cluster                            = data.terraform_remote_state.setup_iac.outputs.ecs_cluster_id
   launch_type                        = "EC2"
-  desired_count                      = var.BOT_COUNT
+  desired_count                      = 1
   deployment_maximum_percent         = 100
   deployment_minimum_healthy_percent = 0
   force_new_deployment               = true
@@ -210,58 +202,20 @@ resource "aws_ecs_service" "aztec-bot" {
 
   # load_balancer {
   #   target_group_arn = aws_alb_target_group.bot_http.arn
-  #   container_name   = "${var.DEPLOY_TAG}-aztec-bot"
+  #   container_name   = "${var.DEPLOY_TAG}-aztec-proof-verifier"
   #   container_port   = 80
   # }
 
   service_registries {
-    registry_arn   = aws_service_discovery_service.aztec-bot.arn
-    container_name = "${var.DEPLOY_TAG}-aztec-bot"
+    registry_arn   = aws_service_discovery_service.aztec-proof-verifier.arn
+    container_name = "${var.DEPLOY_TAG}-aztec-proof-verifier"
     container_port = 80
   }
 
   placement_constraints {
     type       = "memberOf"
-    expression = "attribute:group == ${var.DEPLOY_TAG}-bot"
+    expression = "attribute:group == ${var.DEPLOY_TAG}-proof-verifier"
   }
 
-  task_definition = aws_ecs_task_definition.aztec-bot.family
+  task_definition = aws_ecs_task_definition.aztec-proof-verifier.family
 }
-
-# resource "aws_alb_target_group" "bot_http" {
-#   name                 = "${var.DEPLOY_TAG}-bot-http"
-#   port                 = 80
-#   protocol             = "HTTP"
-#   target_type          = "ip"
-#   vpc_id               = data.terraform_remote_state.setup_iac.outputs.vpc_id
-#   deregistration_delay = 5
-
-#   health_check {
-#     path                = "${local.api_prefix}/status"
-#     matcher             = 200
-#     interval            = 10
-#     healthy_threshold   = 2
-#     unhealthy_threshold = 5
-#     timeout             = 5
-#   }
-
-#   tags = {
-#     name = "${var.DEPLOY_TAG}-bot-http"
-#   }
-# }
-
-# resource "aws_lb_listener_rule" "bot_api" {
-#   listener_arn = data.terraform_remote_state.aztec2_iac.outputs.alb_listener_arn
-#   priority     = 700
-
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_alb_target_group.bot_http.arn
-#   }
-
-#   condition {
-#     path_pattern {
-#       values = ["${local.api_prefix}*"]
-#     }
-#   }
-# }
