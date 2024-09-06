dd09b76f70420a3824bf406bb2044481f68cd741
David Banks <47112877+dbanks12@users.noreply.github.com>
2024-09-06 11:45:44 -0400

    chore!: remove coinbase and unimplemented block gas limit opcodes from AVM (#8408)

--- barretenberg/cpp/src/barretenberg/vm/avm/trace/opcode.cpp
+++ barretenberg/cpp/src/barretenberg/vm/avm/trace/opcode.cpp
@@ -79,16 +79,10 @@ std::string to_string(OpCode opcode)
         return "BLOCKNUMBER";
     case OpCode::TIMESTAMP:
         return "TIMESTAMP";
-    case OpCode::COINBASE:
-        return "COINBASE";
     case OpCode::FEEPERL2GAS:
         return "FEEPERL2GAS";
     case OpCode::FEEPERDAGAS:
         return "FEEPERDAGAS";
-    case OpCode::BLOCKL2GASLIMIT:
-        return "BLOCKL2GASLIMIT";
-    case OpCode::BLOCKDAGASLIMIT:
-        return "BLOCKDAGASLIMIT";
     // Execution Environment - Calldata
     case OpCode::CALLDATACOPY:
         return "CALLDATACOPY";
