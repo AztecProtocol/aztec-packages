6ff7fa55e6f9e63cf4469ba2ee4b4ba076357134
codygunton <codygunton@gmail.com>
2024-09-08 20:39:37 +0000

    Fixes post-rebase

--- barretenberg/cpp/src/barretenberg/ultra_honk/oink_prover.hpp
+++ barretenberg/cpp/src/barretenberg/ultra_honk/oink_prover.hpp
@@ -21,7 +21,7 @@
 
 #include "barretenberg/stdlib_circuit_builders/mega_flavor.hpp"
 #include "barretenberg/stdlib_circuit_builders/ultra_flavor.hpp"
-#include "barretenberg/stdlib_circuit_builders/ultra_keccak.hpp"
+#include "barretenberg/stdlib_circuit_builders/ultra_keccak_flavor.hpp"
 #include "barretenberg/transcript/transcript.hpp"
 #include "barretenberg/ultra_honk/decider_proving_key.hpp"
 
