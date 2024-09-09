bd332a97d5907e1fd18f6d34704db8497d705f41
fcarreiro <facundo@aztecprotocol.com>
2024-09-09 19:47:18 +0000

    fix sizes in wire format

--- barretenberg/cpp/src/barretenberg/vm/avm/trace/execution.cpp
+++ barretenberg/cpp/src/barretenberg/vm/avm/trace/execution.cpp
@@ -773,12 +773,12 @@ std::vector<Row> Execution::gen_trace(std::vector<Instruction> const& instructio
                                          std::get<uint32_t>(inst.operands.at(4)));
             break;
 
-            // Future Gadgets -- pending changes in noir
         case OpCode::SHA256COMPRESSION:
+            // TODO: sizes in wire format are ignored.
             trace_builder.op_sha256_compression(std::get<uint8_t>(inst.operands.at(0)),
                                                 std::get<uint32_t>(inst.operands.at(1)),
                                                 std::get<uint32_t>(inst.operands.at(2)),
-                                                std::get<uint32_t>(inst.operands.at(3)));
+                                                std::get<uint32_t>(inst.operands.at(4)));
             break;
 
         case OpCode::KECCAKF1600:
