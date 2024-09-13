37929f5a4104ae3bb4236f441d164e7e70748c58
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-09-13 10:13:07 +0000

    document secp256 helpers

--- yarn-project/foundation/src/crypto/secp256k1-signer/secp256k1_signer.ts
+++ yarn-project/foundation/src/crypto/secp256k1-signer/secp256k1_signer.ts
@@ -4,6 +4,13 @@ import { type Signature } from '@aztec/foundation/eth-signature';
 
 import { addressFromPrivateKey, signMessage } from './utils.js';
 
+/**
+ * Secp256k1Signer
+ *
+ * A class for signing messages using a secp256k1 private key.
+ * - This is a slim drop in replacement for an Ethereum signer, so it can be used in the same way.
+ * - See `utils.ts` for functions that enable recovering addresses and public keys from signatures.
+ */
 export class Secp256k1Signer {
   public readonly address: EthAddress;
 
--- yarn-project/foundation/src/crypto/secp256k1-signer/utils.ts
+++ yarn-project/foundation/src/crypto/secp256k1-signer/utils.ts
@@ -5,25 +5,52 @@ import { EthAddress } from '../../eth-address/index.js';
 import { Signature } from '../../eth-signature/eth_signature.js';
 import { keccak256 } from '../keccak/index.js';
 
+/**
+ * Converts a public key to an address.
+ * @param publicKey - The public key to convert.
+ * @returns The address.
+ */
 function publicKeyToAddress(publicKey: Buffer): EthAddress {
-  const hash = keccak256(publicKey.slice(1));
+  const hash = keccak256(publicKey.subarray(1));
   return new EthAddress(hash.subarray(12));
 }
 
+/**
+ * Converts a private key to a public key.
+ * @param privateKey - The private key to convert.
+ * @returns The public key.
+ */
 export function publicKeyFromPrivateKey(privateKey: Buffer): Buffer {
   return Buffer.from(secp256k1.getPublicKey(privateKey, false));
 }
 
+/**
+ * Converts a private key to an address.
+ * @param privateKey - The private key to convert.
+ * @returns The address.
+ */
 export function addressFromPrivateKey(privateKey: Buffer): EthAddress {
   const publicKey = publicKeyFromPrivateKey(privateKey);
   return publicKeyToAddress(publicKey);
 }
 
+/**
+ * Recovers an address from a hash and a signature.
+ * @param hash - The hash to recover the address from.
+ * @param signature - The signature to recover the address from.
+ * @returns The address.
+ */
 export function recoverAddress(hash: Buffer32, signature: Signature): EthAddress {
   const publicKey = recoverPublicKey(hash, signature);
   return publicKeyToAddress(publicKey);
 }
 
+/**
+ * @attribution - viem
+ * Converts a yParityOrV value to a recovery bit.
+ * @param yParityOrV - The yParityOrV value to convert.
+ * @returns The recovery bit.
+ */
 function toRecoveryBit(yParityOrV: number) {
   if (yParityOrV === 0 || yParityOrV === 1) {
     return yParityOrV;
@@ -37,19 +64,28 @@ function toRecoveryBit(yParityOrV: number) {
   throw new Error('Invalid yParityOrV value');
 }
 
+/**
+ * Signs a message using ecdsa over the secp256k1 curve.
+ * @param message - The message to sign.
+ * @param privateKey - The private key to sign the message with.
+ * @returns The signature.
+ */
 export function signMessage(message: Buffer32, privateKey: Buffer) {
   const { r, s, recovery } = secp256k1.sign(message.buffer, privateKey);
   return new Signature(Buffer32.fromBigInt(r), Buffer32.fromBigInt(s), recovery ? 28 : 27);
 }
 
+/**
+ * Recovers a public key from a hash and a signature.
+ * @param hash - The hash to recover the public key from.
+ * @param signature - The signature to recover the public key from.
+ * @returns The public key.
+ */
 export function recoverPublicKey(hash: Buffer32, signature: Signature): Buffer {
-  const signature_ = (() => {
-    // typeof signature: `Signature`
-    const { r, s, v } = signature;
-    const recoveryBit = toRecoveryBit(v);
-    return new secp256k1.Signature(r.toBigInt(), s.toBigInt()).addRecoveryBit(recoveryBit);
-  })();
+  const { r, s, v } = signature;
+  const recoveryBit = toRecoveryBit(v);
+  const sig = new secp256k1.Signature(r.toBigInt(), s.toBigInt()).addRecoveryBit(recoveryBit);
 
-  const publicKey = signature_.recoverPublicKey(hash.buffer).toHex(false);
+  const publicKey = sig.recoverPublicKey(hash.buffer).toHex(false);
   return Buffer.from(publicKey, 'hex');
 }
