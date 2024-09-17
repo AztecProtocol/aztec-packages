a8d0352001b040da8da27a2126595bb87918fddf
DanielKotov <katod.daniel@gmail.com>
2024-09-17 13:20:19 +0000

    fix for sha256 extend_witness function

--- barretenberg/cpp/src/barretenberg/stdlib/hash/sha256/sha256_plookup.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/hash/sha256/sha256_plookup.cpp
@@ -140,6 +140,11 @@ std::array<field_t<Builder>, 64> extend_witness(const std::array<field_t<Builder
             w_out = witness_t<Builder>(
                 ctx, fr(w_out_raw.get_value().from_montgomery_form().data[0] & (uint64_t)0xffffffffULL));
         }
+        constexpr fr inv_pow_two = fr(2).pow(32);
+        field_pt w_out_raw_inv_pow_two = w_out_raw * inv_pow_two;
+        field_pt w_out_inv_pow_two = w_out * inv_pow_two;
+        field_pt divisor = w_out_raw_inv_pow_two - w_out_inv_pow_two;
+        ctx->create_new_range_constraint(divisor.witness_index, 3);
         w_sparse[i] = sparse_witness_limbs(w_out);
     }
 
