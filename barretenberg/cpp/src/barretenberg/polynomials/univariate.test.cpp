2477aed33579e7d9e1a65431770bc6523f9d97e4
codygunton <codygunton@gmail.com>
2024-09-18 18:14:29 +0000

    Illustrate it works

--- barretenberg/cpp/src/barretenberg/polynomials/univariate.test.cpp
+++ barretenberg/cpp/src/barretenberg/polynomials/univariate.test.cpp
@@ -27,10 +27,10 @@ TYPED_TEST(UnivariateTest, Constructors)
 
 TYPED_TEST(UnivariateTest, Addition)
 {
-    Univariate<fr, 2> f1{ { 1, 2 } };
-    Univariate<fr, 2> f2{ { 3, 4 } };
+    Univariate<fr, 2> f1{ 1, 2 };
+    Univariate<fr, 2> f2{ 3, 4 };
     // output should be {4, 6}
-    Univariate<fr, 2> expected_result{ { 4, 6 } };
+    Univariate<fr, 2> expected_result{ 4, 6 };
     auto f1f2 = f1 + f2;
     EXPECT_EQ(f1f2, expected_result);
 }
@@ -38,10 +38,10 @@ TYPED_TEST(UnivariateTest, Addition)
 TYPED_TEST(UnivariateTest, Multiplication)
 {
 
-    Univariate<fr, 3> f1 = Univariate<fr, 2>{ { 1, 2 } }.template extend_to<3>();
-    Univariate<fr, 3> f2 = Univariate<fr, 2>{ { 3, 4 } }.template extend_to<3>();
+    Univariate<fr, 3> f1 = Univariate<fr, 2>{ 1, 2 }.template extend_to<3>();
+    Univariate<fr, 3> f2 = Univariate<fr, 2>{ 3, 4 }.template extend_to<3>();
     // output should be {3, 8, 15}
-    Univariate<fr, 3> expected_result{ { 3, 8, 15 } };
+    Univariate<fr, 3> expected_result{ 3, 8, 15 };
     Univariate<fr, 3> f1f2 = f1 * f2;
     EXPECT_EQ(f1f2, expected_result);
 }
@@ -49,7 +49,7 @@ TYPED_TEST(UnivariateTest, Multiplication)
 TYPED_TEST(UnivariateTest, ConstructUnivariateViewFromUnivariate)
 {
 
-    Univariate<fr, 3> f{ { 1, 2, 3 } };
+    Univariate<fr, 3> f{ 1, 2, 3 };
     UnivariateView<fr, 2> g(f);
     EXPECT_EQ(g.value_at(0), f.value_at(0));
     EXPECT_EQ(g.value_at(1), f.value_at(1));
@@ -58,7 +58,7 @@ TYPED_TEST(UnivariateTest, ConstructUnivariateViewFromUnivariate)
 TYPED_TEST(UnivariateTest, ConstructUnivariateFromUnivariateView)
 {
 
-    Univariate<fr, 3> f{ { 1, 2, 3 } };
+    Univariate<fr, 3> f{ 1, 2, 3 };
     UnivariateView<fr, 2> g(f);
     Univariate<fr, 2> h(g);
     EXPECT_EQ(h.value_at(0), g.value_at(0));
@@ -67,51 +67,51 @@ TYPED_TEST(UnivariateTest, ConstructUnivariateFromUnivariateView)
 
 TYPED_TEST(UnivariateTest, UnivariateViewAddition)
 {
-    Univariate<fr, 3> f1{ { 1, 2, 3 } };
-    Univariate<fr, 3> f2{ { 3, 4, 3 } };
+    Univariate<fr, 3> f1{ 1, 2, 3 };
+    Univariate<fr, 3> f2{ 3, 4, 3 };
 
     UnivariateView<fr, 2> g1(f1);
     UnivariateView<fr, 2> g2(f2);
 
-    Univariate<fr, 2> expected_result{ { 4, 6 } };
+    Univariate<fr, 2> expected_result{ 4, 6 };
     Univariate<fr, 2> result = g1 + g2;
     EXPECT_EQ(result, expected_result);
 
     Univariate<fr, 2> result2 = result + g1;
-    Univariate<fr, 2> expected_result2{ { 5, 8 } };
+    Univariate<fr, 2> expected_result2{ 5, 8 };
     EXPECT_EQ(result2, expected_result2);
 }
 TYPED_TEST(UnivariateTest, UnivariateViewSubtraction)
 {
-    Univariate<fr, 3> f1{ { 1, 2, 3 } };
-    Univariate<fr, 3> f2{ { 3, 4, 3 } };
+    Univariate<fr, 3> f1{ 1, 2, 3 };
+    Univariate<fr, 3> f2{ 3, 4, 3 };
 
     UnivariateView<fr, 2> g1(f1);
     UnivariateView<fr, 2> g2(f2);
 
-    Univariate<fr, 2> expected_result{ { -2, -2 } };
+    Univariate<fr, 2> expected_result{ -2, -2 };
     Univariate<fr, 2> result = g1 - g2;
     EXPECT_EQ(result, expected_result);
 
     Univariate<fr, 2> result2 = result - g1;
-    Univariate<fr, 2> expected_result2{ { -3, -4 } };
+    Univariate<fr, 2> expected_result2{ -3, -4 };
     EXPECT_EQ(result2, expected_result2);
 }
 
 TYPED_TEST(UnivariateTest, UnivariateViewMultiplication)
 {
-    Univariate<fr, 3> f1{ { 1, 2, 3 } };
-    Univariate<fr, 3> f2{ { 3, 4, 3 } };
+    Univariate<fr, 3> f1{ 1, 2, 3 };
+    Univariate<fr, 3> f2{ 3, 4, 3 };
 
     UnivariateView<fr, 2> g1(f1);
     UnivariateView<fr, 2> g2(f2);
 
-    Univariate<fr, 2> expected_result{ { 3, 8 } };
+    Univariate<fr, 2> expected_result{ 3, 8 };
     Univariate<fr, 2> result = g1 * g2;
     EXPECT_EQ(result, expected_result);
 
     Univariate<fr, 2> result2 = result * g1;
-    Univariate<fr, 2> expected_result2{ { 3, 16 } };
+    Univariate<fr, 2> expected_result2{ 3, 16 };
     EXPECT_EQ(result2, expected_result2);
 }
 
