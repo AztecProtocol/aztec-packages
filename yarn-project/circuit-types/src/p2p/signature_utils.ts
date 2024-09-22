60c22154169732039abb083ce36beefc087dc1a7
Mitch <mitchell@aztecprotocol.com>
2024-09-22 13:08:16 -0400

    update comment

--- yarn-project/circuit-types/src/p2p/signature_utils.ts
+++ yarn-project/circuit-types/src/p2p/signature_utils.ts
@@ -6,20 +6,18 @@ export interface Signable {
 }
 
 /**
- * Get the hashed payload for the signature of the block proposal
- * @param archive - The archive of the block
- * @param txs - The transactions in the block
- * @returns The hashed payload for the signature of the block proposal
+ * Get the hashed payload for the signature of the `Signable`
+ * @param s - The `Signable` to sign
+ * @returns The hashed payload for the signature of the `Signable`
  */
 export function getHashedSignaturePayload(s: Signable): Buffer32 {
   return Buffer32.fromBuffer(keccak256(s.getPayloadToSign()));
 }
 
 /**
- * Get the hashed payload for the signature of the block proposal as an Ethereum signed message EIP-712
- * @param archive - The archive of the block
- * @param txs - The transactions in the block
- * @returns The hashed payload for the signature of the block proposal as an Ethereum signed message
+ * Get the hashed payload for the signature of the `Signable` as an Ethereum signed message EIP-712
+ * @param s - the `Signable` to sign
+ * @returns The hashed payload for the signature of the `Signable` as an Ethereum signed message
  */
 export function getHashedSignaturePayloadEthSignedMessage(s: Signable): Buffer32 {
   const payload = getHashedSignaturePayload(s);
