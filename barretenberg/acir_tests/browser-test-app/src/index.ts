ab6e03c353e03da64e2fee5edc6f77edefb6b1d3
TomAFrench <tom@tomfren.ch>
2024-09-23 14:21:20 +0000

    .

--- barretenberg/acir_tests/browser-test-app/src/index.ts
+++ barretenberg/acir_tests/browser-test-app/src/index.ts
@@ -16,7 +16,8 @@ async function runTest(
   const proof = await backend.generateProof(witness);
 
   debug(`verifying...`);
-  const verified = await BarretenbergVerifier.verifyUltraplonkProof(proof);
+  const verifier = new BarretenbergVerifier({ threads });
+  const verified = await verifier.verifyUltraplonkProof(proof);
   debug(`verified: ${verified}`);
 
   await backend.destroy();
