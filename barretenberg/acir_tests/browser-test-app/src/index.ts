428e1181d0e0b170c98e1242a1e6339bbf3a2859
TomAFrench <tom@tomfren.ch>
2024-09-23 14:19:12 +0000

    chore: switch over tests to use high level API

--- barretenberg/acir_tests/browser-test-app/src/index.ts
+++ barretenberg/acir_tests/browser-test-app/src/index.ts
@@ -9,34 +9,17 @@ async function runTest(
   witness: Uint8Array,
   threads?: number
 ) {
-  const { Barretenberg, RawBuffer, Crs } = await import("@aztec/bb.js");
-  const CIRCUIT_SIZE = 2 ** 19;
+  const { UltraPlonkBackend, BarretenbergVerifier } = await import("@aztec/bb.js");
 
   debug("starting test...");
-  const api = await Barretenberg.new({ threads });
+  const backend = new UltraPlonkBackend(bytecode, { threads });
+  const proof = await backend.generateProof(witness);
 
-  // Important to init slab allocator as first thing, to ensure maximum memory efficiency.
-  await api.commonInitSlabAllocator(CIRCUIT_SIZE);
-
-  // Plus 1 needed!
-  const crs = await Crs.new(CIRCUIT_SIZE + 1);
-  await api.srsInitSrs(
-    new RawBuffer(crs.getG1Data()),
-    crs.numPoints,
-    new RawBuffer(crs.getG2Data())
-  );
-
-  const acirComposer = await api.acirNewAcirComposer(CIRCUIT_SIZE);
-  const proof = await api.acirCreateProof(
-    acirComposer,
-    bytecode,
-    witness,
-  );
   debug(`verifying...`);
-  const verified = await api.acirVerifyProof(acirComposer, proof);
+  const verified = await BarretenbergVerifier.verifyUltraplonkProof(proof);
   debug(`verified: ${verified}`);
 
-  await api.destroy();
+  await backend.destroy();
 
   debug("test complete.");
   return verified;
