62dd1e818ced2bd8544ad6bb39069dacd1214898
ludamad <domuradical@gmail.com>
2024-09-26 20:33:40 +0000

    simplify continued

---
+++ build-system/s3-cache-scripts/cache-download.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -eu
+
+# used by cache-download-, directhloads a named tar file and extracts it
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <tar.gz_file_to_download_and_extract>"
+    exit 1
+fi
+
+if [ "${AWS_ACCESS_KEY_ID}" == "" ] then
+  exit 1 # require a rebuild
+fi
+
+# Get the tar.gz file name from the argument
+TAR_FILE="$1"
+
+# check for the pattern emitted by compute-content-hash-if-git-clean.sh
+if [ "$TAR_FILE" = *uncommitted-changes* ] ; then
+  exit 1 # require a rebuild
+fi
+
+function on_exit() {
+  # Cleanup the temporary tar.gz file
+  rm -f "$TAR_FILE"
+}
+# Run on any exit
+trap on_exit EXIT
+
+# Attempt to download the cache file
+echo aws s3 cp "s3://aztec-ci-artifacts/build-cache/$TAR_FILE" "$TAR_FILE"
+
+# Extract the cache file
+tar -xzf "$TAR_FILE"
+
+echo "Cache download and extraction complete."
---
+++ build-system/s3-cache-scripts/cache-upload.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -eu
+
+# used by cache-update.sh, directly downloads a named tar file and extracts it
+
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <binary_paths_to_tar.gz_and_upload...> <name_without_tar.gz_extension>"
+  exit 1
+fi
+
+if [ "${S3_BUILD_CACHE_UPLOAD:-true}" = "false" ] || [ "${AWS_ACCESS_KEY_ID}" == "" ] then
+  # Silently do nothing
+  exit
+fi
+
+# check for the pattern emitted by compute-content-hash-if-git-clean.sh
+if [ "$TAR_FILE" = *uncommitted-changes* ] ; then
+  # Silently do nothing
+  exit
+fi
+
+# Extract the name without tar.gz extension
+NAME="${@: -1}"
+
+# Extract the binary paths to tar.gz and upload
+BIN_PATHS=("${@:1:$#-1}")
+
+TAR_DIR=$(mktemp -d)
+TAR_FILE="$TAR_DIR/${NAME}.tar.gz"
+
+function on_exit() {
+  # Cleanup the temporary folder
+  rm -rf "$TAR_DIR"
+}
+trap on_exit EXIT
+
+# Create the tar.gz file
+tar -czf "$TAR_FILE" "${BIN_PATHS[@]}"
+
+# flag to disable uploads
+S3_BUILD_CACHE_UPLOAD=${S3_BUILD_CACHE_UPLOAD:-false}
+
+echo aws s3 cp "$TAR_FILE" "s3://aztec-ci-artifacts/build-cache/$TAR_FILE"
