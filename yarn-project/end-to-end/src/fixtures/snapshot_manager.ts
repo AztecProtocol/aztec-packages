8d49205d432dc08a656d76d24af9509cdb7975c0
Mitch <mitchell@aztecprotocol.com>
2024-09-26 12:35:34 -0400

    do not assume proven on e2e_prover_node_test

--- yarn-project/end-to-end/src/fixtures/snapshot_manager.ts
+++ yarn-project/end-to-end/src/fixtures/snapshot_manager.ts
@@ -60,8 +60,15 @@ type SnapshotEntry = {
   snapshotPath: string;
 };
 
-export function createSnapshotManager(testName: string, dataPath?: string, config: Partial<AztecNodeConfig> = {}) {
-  return dataPath ? new SnapshotManager(testName, dataPath, config) : new MockSnapshotManager(testName, config);
+export function createSnapshotManager(
+  testName: string,
+  dataPath?: string,
+  config: Partial<AztecNodeConfig> = {},
+  deployL1ContractsArgs: Partial<DeployL1ContractsArgs> = { assumeProvenThrough: Number.MAX_SAFE_INTEGER },
+) {
+  return dataPath
+    ? new SnapshotManager(testName, dataPath, config, deployL1ContractsArgs)
+    : new MockSnapshotManager(testName, config, deployL1ContractsArgs);
 }
 
 export interface ISnapshotManager {
@@ -81,7 +88,11 @@ class MockSnapshotManager implements ISnapshotManager {
   private context?: SubsystemsContext;
   private logger: DebugLogger;
 
-  constructor(testName: string, private config: Partial<AztecNodeConfig> = {}) {
+  constructor(
+    testName: string,
+    private config: Partial<AztecNodeConfig> = {},
+    private deployL1ContractsArgs: Partial<DeployL1ContractsArgs> = { assumeProvenThrough: Number.MAX_SAFE_INTEGER },
+  ) {
     this.logger = createDebugLogger(`aztec:snapshot_manager:${testName}`);
     this.logger.warn(`No data path given, will not persist any snapshots.`);
   }
@@ -103,7 +114,7 @@ class MockSnapshotManager implements ISnapshotManager {
 
   public async setup() {
     if (!this.context) {
-      this.context = await setupFromFresh(undefined, this.logger, this.config);
+      this.context = await setupFromFresh(undefined, this.logger, this.config, this.deployL1ContractsArgs);
     }
     return this.context;
   }
@@ -124,7 +135,12 @@ class SnapshotManager implements ISnapshotManager {
   private livePath: string;
   private logger: DebugLogger;
 
-  constructor(testName: string, private dataPath: string, private config: Partial<AztecNodeConfig> = {}) {
+  constructor(
+    testName: string,
+    private dataPath: string,
+    private config: Partial<AztecNodeConfig> = {},
+    private deployL1ContractsArgs: Partial<DeployL1ContractsArgs> = { assumeProvenThrough: Number.MAX_SAFE_INTEGER },
+  ) {
     this.livePath = join(this.dataPath, 'live', testName);
     this.logger = createDebugLogger(`aztec:snapshot_manager:${testName}`);
   }
@@ -201,7 +217,7 @@ class SnapshotManager implements ISnapshotManager {
           this.logger.verbose(`Restoration of ${e.name} complete.`);
         });
       } else {
-        this.context = await setupFromFresh(this.livePath, this.logger, this.config);
+        this.context = await setupFromFresh(this.livePath, this.logger, this.config, this.deployL1ContractsArgs);
       }
     }
     return this.context;
