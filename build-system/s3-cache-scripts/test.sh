4d9b9a995ea8bfe99591e4f2da67f6276a87e2a6
ludamad <domuradical@gmail.com>
2024-09-27 00:00:45 +0000

    passing tests!

---
+++ build-system/s3-cache-scripts/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# test only minio caching
+set -eux
+function delete_test_cache() {
+  WS_SECRET_ACCESS_KEY=minioadmin AWS_ACCESS_KEY_ID=minioadmin \
+    aws --endpoint http://localhost:12000 \
+      s3 rm s3://aztec-ci-artifacts --recursive --exclude "*" --include "build-cache/barretenberg-preset-release-world-state-*.tar.gz"
+}
+function minio_cache_only() {
+  AWS_ACCESS_KEY_ID="" "$(git rev-parse --show-toplevel)/scripts/earthly-local" --no-cache $@
+}
+# if we have minio already running make sure the cache is deleted
+# otherwise it will be run by next earthly local command
+nc -zv 127.0.0.1 12000 && delete_test_cache
+# Our assertions (thanks to -e flag):
+# expect file to not exist at first
+! minio_cache_only ../../barretenberg/cpp/+test-cache-read 2>/dev/null
+minio_cache_only ../../barretenberg/cpp/+test-cache-write 2>/dev/null
+[ -f ~/.minio/data/*/*/test-cache*.tar.gz ]
+minio_cache_only ../../barretenberg/cpp/+test-cache-read 2>/dev/null
+echo "Success!"
+
+# aws ${S3_BUILD_CACHE_AWS_PARAMS:-} s3 cp "$TAR_FILE" "s3://aztec-ci-artifacts/build-cache/$NAME.tar.gz" --quiet
+# AWS_ACCESS_KEY_ID="" "$(git rev-parse --show-toplevel)/scripts/earthly-local" --no-cache ../../barretenberg/cpp/+test-cache
+# #S3_BUILD_CACHE_UPLOAD=true "$(git rev-parse --show-toplevel)/scripts/earthly-local" --no-cache ../../barretenberg/cpp/+test-cache
