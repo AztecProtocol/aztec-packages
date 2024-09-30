85763085c18320376b7b42384c6239d9e0899c7e
LHerskind <lasse.herskind@gmail.com>
2024-09-30 12:14:12 +0000

    chore: format

--- l1-contracts/src/core/libraries/TimeMath.sol
+++ l1-contracts/src/core/libraries/TimeMath.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2024 Aztec Labs.
-pragma solidity >=0.8.19;
+pragma solidity >=0.8.27;
 
 import {Constants} from "@aztec/core/libraries/ConstantsGen.sol";
 
