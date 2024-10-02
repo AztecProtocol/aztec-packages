8bd7988fe1b84838c0e40895e6db12b5f03e0723
ludamad <domuradical@gmail.com>
2024-10-02 21:22:03 +0000

    Better iterative workflow for changing e2e tests

---
+++ yarn-project/end-to-end/scripts/helm_deploy.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Usage: ./helm_deploy.sh
+# Required environment variables:
+#   NAMESPACE
+# Optional environment variables:
+#   VALUES_FILE (default: "default.yaml")
+#   CHAOS_VALUES (default: "")
+#   FRESH_INSTALL (default: "false")
+#   AZTEC_DOCKER_TAG (default: current git commit)
+
+set -eux
+
+# Default values for environment variables
+VALUES_FILE="${VALUES_FILE:-default.yaml}"
+CHAOS_VALUES="${CHAOS_VALUES:-}"
+FRESH_INSTALL="${FRESH_INSTALL:-false}"
+AZTEC_DOCKER_TAG=${AZTEC_DOCKER_TAG:-$(git rev-parse HEAD)}
+
+# Check required environment variable
+if [ -z "${NAMESPACE:-}" ]; then
+  echo "Environment variable NAMESPACE is required."
+  exit 1
+fi
+
+if ! docker image ls --format '{{.Repository}}:{{.Tag}}' | grep -q "aztecprotocol/aztec:$AZTEC_DOCKER_TAG"; then
+  echo "Docker images not found. They need to be built with 'earthly ./yarn-project/+export-aztec'."
+  exit 1
+fi
+
+# Load the Docker images into kind
+kind load docker-image aztecprotocol/aztec:$AZTEC_DOCKER_TAG
+
+# If FRESH_INSTALL is true, delete the namespace
+if [ "$FRESH_INSTALL" = "true" ]; then
+  kubectl delete namespace "$NAMESPACE" --ignore-not-found=true --wait=true --now --timeout=10m
+fi
+
+function show_status_until_pxe_ready() {
+  set +x # don't spam with our commands
+  sleep 15 # let helm upgrade start
+  for i in {1..100} ; do
+    if kubectl wait pod -l app==pxe --for=condition=Ready -n "transfer" --timeout=20s >/dev/null 2>/dev/null ; then
+      break # we are up, stop showing status
+    fi
+    # show startup status
+    kubectl get pods -n "$NAMESPACE"
+  done
+}
+
+show_status_until_pxe_ready &
+
+# Install the Helm chart
+helm upgrade --install spartan "$(git rev-parse --show-toplevel)/spartan/aztec-network/" \
+      --namespace "$NAMESPACE" \
+      --create-namespace \
+      --values "$(git rev-parse --show-toplevel)/spartan/aztec-network/values/$VALUES_FILE" \
+      --set images.aztec.image="aztecprotocol/aztec:$AZTEC_DOCKER_TAG" \
+      --wait \
+      --wait-for-jobs=true \
+      --timeout=30m
--- yarn-project/end-to-end/scripts/network_test.sh
+++ yarn-project/end-to-end/scripts/network_test.sh
@@ -1,73 +1,26 @@
 #!/bin/bash
 
 # Usage: ./network_test.sh <test>
-# Required environment variables:
-#   NAMESPACE
+# If <test> == "interactive" the 'docker run' drops into a shell where you can e.g. edit our test file
 # Optional environment variables:
+#   END_TO_END_DOCKER_TAG (default: AZTEC_DOCKER_TAG, if not set then current git commit)
+# Required environment variables passed to helm_deploy.sh:
+#   NAMESPACE
+# Optional environment variables passed to helm_deploy.sh:
 #   VALUES_FILE (default: "default.yaml")
 #   CHAOS_VALUES (default: "")
 #   FRESH_INSTALL (default: "false")
-#   AZTEC_DOCKER_TAG (default: current git commit)
 
 set -eux
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Main positional parameter
 TEST="$1"
 
-# Default values for environment variables
-VALUES_FILE="${VALUES_FILE:-default.yaml}"
-CHAOS_VALUES="${CHAOS_VALUES:-}"
-FRESH_INSTALL="${FRESH_INSTALL:-false}"
 AZTEC_DOCKER_TAG=${AZTEC_DOCKER_TAG:-$(git rev-parse HEAD)}
+END_TO_END_DOCKER_TAG=${END_TO_END_DOCKER_TAG:-$AZTEC_DOCKER_TAG}
 
-# Check required environment variable
-if [ -z "${NAMESPACE:-}" ]; then
-  echo "Environment variable NAMESPACE is required."
-  exit 1
-fi
-
-if ! docker image ls --format '{{.Repository}}:{{.Tag}}' | grep -q "aztecprotocol/aztec:$AZTEC_DOCKER_TAG" || \
-   ! docker image ls --format '{{.Repository}}:{{.Tag}}' | grep -q "aztecprotocol/end-to-end:$AZTEC_DOCKER_TAG"; then
-  echo "Docker images not found. They need to be built with 'earthly ./yarn-project/+export-e2e-test-images' or otherwise tagged with aztecprotocol/aztec:$AZTEC_DOCKER_TAG and aztecprotocol/end-to-end:$AZTEC_DOCKER_TAG."
-  exit 1
-fi
-
-# Load the Docker images into kind
-kind load docker-image aztecprotocol/end-to-end:$AZTEC_DOCKER_TAG
-kind load docker-image aztecprotocol/aztec:$AZTEC_DOCKER_TAG
-
-# If FRESH_INSTALL is true, delete the namespace
-if [ "$FRESH_INSTALL" = "true" ]; then
-  kubectl delete namespace "$NAMESPACE" --ignore-not-found=true --wait=true --now --timeout=10m
-fi
-
-function show_status_until_pxe_ready() {
-  set +x # don't spam with our commands
-  sleep 15 # let helm upgrade start
-  for i in {1..100} ; do
-    if kubectl wait pod -l app==pxe --for=condition=Ready -n "transfer" --timeout=20s >/dev/null 2>/dev/null ; then
-      break # we are up, stop showing status
-    fi
-    # show startup status
-    kubectl get pods -n "$NAMESPACE"
-  done
-}
-
-show_status_until_pxe_ready &
-
-# Install the Helm chart
-helm upgrade --install spartan "$(git rev-parse --show-toplevel)/spartan/aztec-network/" \
-      --namespace "$NAMESPACE" \
-      --create-namespace \
-      --values "$(git rev-parse --show-toplevel)/spartan/aztec-network/values/$VALUES_FILE" \
-      --set images.aztec.image="aztecprotocol/aztec:$AZTEC_DOCKER_TAG" \
-      --wait \
-      --wait-for-jobs=true \
-      --timeout=30m
-
-kubectl wait pod -l app==pxe --for=condition=Ready -n "$NAMESPACE" --timeout=10m
+# Ensure our aztec helm chart is deployed
+$(git rev-parse --show-toplevel)/yarn-project/end-to-end/scripts/helm_deploy.sh
 
 function k8s_pxe_port_forward() {
   # Clear any existing port forward
@@ -79,10 +32,21 @@ function k8s_pxe_port_forward() {
 k8s_pxe_port_forward 2>/dev/null &
 
 # run our test in the host network namespace (so we can access the above with localhost)
-docker run --rm --network=host \
-  -e SCENARIO=default \
-  -e PXE_URL=http://localhost:9080 \
-  -e DEBUG="aztec:*" \
-  -e LOG_LEVEL=debug \
-  -e LOG_JSON=1 \
-  aztecprotocol/end-to-end:$AZTEC_DOCKER_TAG $TEST
+if [ "$TEST" = interactive ] ; then
+  docker run --rm --network=host \
+    -e SCENARIO=default \
+    -e PXE_URL=http://localhost:9080 \
+    -e DEBUG="aztec:*" \
+    -e LOG_LEVEL=debug \
+    -e LOG_JSON=1 \
+    aztecprotocol/end-to-end:$END_TO_END_DOCKER_TAG $TEST
+else
+  docker run --rm -it --network=host \
+    -e SCENARIO=default \
+    -e PXE_URL=http://localhost:9080 \
+    -e DEBUG="aztec:*" \
+    -e LOG_LEVEL=debug \
+    -e LOG_JSON=1 \
+    --entrypoint /bin/sh \
+    aztecprotocol/end-to-end:$END_TO_END_DOCKER_TAG
+fi
