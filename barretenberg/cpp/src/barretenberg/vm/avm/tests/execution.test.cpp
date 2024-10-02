784d483b592cb80da143634c07d330ba2f2c9ab7
Ilyas Ridhuan <ilyas@aztecprotocol.com>
2024-10-02 08:56:03 +0100

    fix(avm): kernel out full proving fix (#8873)
    Please read [contributing guidelines](CONTRIBUTING.md) and remove this
    line.
    
    ---------
    
    Co-authored-by: ludamad <adam.domurad@gmail.com>

--- barretenberg/cpp/src/barretenberg/vm/avm/tests/execution.test.cpp
+++ barretenberg/cpp/src/barretenberg/vm/avm/tests/execution.test.cpp
@@ -1912,13 +1912,16 @@ TEST_F(AvmExecutionTests, kernelOutputEmitOpcodes)
     FF expected_hash = FF(std::string("0x006db65fd59fd356f6729140571b5bcd6bb3b83492a16e1bf0a3884442fc3c8a"));
     EXPECT_EQ(emit_log_row->main_ia, expected_hash);
     EXPECT_EQ(emit_log_row->main_side_effect_counter, 2);
+    // Value is 40 = 32 * log_length + 40 (and log_length is 0 in this case).
+    EXPECT_EQ(emit_log_row->main_ib, 40);
 
     uint32_t emit_log_out_offset = START_EMIT_UNENCRYPTED_LOG_WRITE_OFFSET;
     auto emit_log_kernel_out_row =
         std::ranges::find_if(trace.begin(), trace.end(), [&](Row r) { return r.main_clk == emit_log_out_offset; });
     EXPECT_EQ(emit_log_kernel_out_row->main_kernel_value_out, expected_hash);
     EXPECT_EQ(emit_log_kernel_out_row->main_kernel_side_effect_out, 2);
-    feed_output(emit_log_out_offset, 1, 2, 0);
+    EXPECT_EQ(emit_log_kernel_out_row->main_kernel_metadata_out, 40);
+    feed_output(emit_log_out_offset, expected_hash, 2, 40);
 
     // CHECK SEND L2 TO L1 MSG
     auto send_row =
