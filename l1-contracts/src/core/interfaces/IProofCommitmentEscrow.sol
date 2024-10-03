4ff9f8f9d42ebf2ae1e1e4bcaf88e91bfe59e858
ludamad <domuradical@gmail.com>
2024-10-03 18:40:47 +0000

    Reapply "feat: Prover node stakes to escrow contract (#8975)"
    This reverts commit ec356e210e777775b02c76843e63ec52869637ff.

--- l1-contracts/src/core/interfaces/IProofCommitmentEscrow.sol
+++ l1-contracts/src/core/interfaces/IProofCommitmentEscrow.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.8.27;
 
 import {Timestamp} from "@aztec/core/libraries/TimeMath.sol";
+import {IERC20} from "@oz/token/ERC20/IERC20.sol";
 
 interface IProofCommitmentEscrow {
   event Deposit(address indexed depositor, uint256 amount);
@@ -17,4 +18,6 @@ interface IProofCommitmentEscrow {
   function stakeBond(address _prover, uint256 _amount) external;
   function unstakeBond(address _prover, uint256 _amount) external;
   function minBalanceAtTime(Timestamp _timestamp, address _prover) external view returns (uint256);
+  function deposits(address) external view returns (uint256);
+  function token() external view returns (IERC20);
 }
