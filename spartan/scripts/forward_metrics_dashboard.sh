bd8439da15bc93ce6c8594a65d0d27244ddfbcb9
Mitch <mitchell@aztecprotocol.com>
2024-10-06 10:39:44 -0400

    clean up

--- spartan/scripts/forward_metrics_dashboard.sh
+++ spartan/scripts/forward_metrics_dashboard.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-echo "Run this script after the metrics pods all come up. After running this script, port forward 5601 locally"
-echo "Then navigate to https://localhost:5601 and login with user elastic password admin"
-kubectl port-forward -n metrics service/metrics-eck-kibana-kb-http 5601:5601
+echo "Run this script after the metrics pods all come up. After running this script, port forward 3000 locally"
+echo "Then navigate to https://localhost:3000 and login with user admin, and password as follows:"
+echo
+kubectl get secrets -n metrics metrics-grafana -o jsonpath='{.data.admin-password}' | base64 --decode
+# skip two empty lines
+echo
+echo
+kubectl port-forward -n metrics service/metrics-grafana 3000:80
--- spartan/scripts/setup_metrics_dashboard.sh
+++ spartan/scripts/setup_metrics_dashboard.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
-helm repo add elastic https://helm.elastic.co
-helm repo update
-helm dependency build "$(git rev-parse --show-toplevel)/spartan/metrics/"
-kubectl create -f https://download.elastic.co/downloads/eck/2.14.0/crds.yaml 2>/dev/null
-kubectl create secret generic elasticsearch-es-elastic-user --from-literal=elastic=admin -n metrics
-kubectl apply -f https://download.elastic.co/downloads/eck/2.14.0/operator.yaml
-helm upgrade --install metrics "$(git rev-parse --show-toplevel)/spartan/metrics/" --wait --namespace metrics --create-namespace --wait-for-jobs=true --timeout=30m
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm dependency update
+helm upgrade metrics ./metrics -n metrics --install --create-namespace --atomic
