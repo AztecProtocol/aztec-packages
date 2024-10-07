d6a40fecab1a88ad305447f2103d186090d3b4b4
ludamad <domuradical@gmail.com>
2024-10-07 16:40:22 +0000

    fix serialization issue with DeployProvenTx

--- yarn-project/aztec.js/src/contract/proven_tx.ts
+++ yarn-project/aztec.js/src/contract/proven_tx.ts
@@ -1,7 +1,10 @@
 import { type PXE, Tx } from '@aztec/circuit-types';
 
+
+
 import { type Wallet } from '../account/index.js';
 import { SentTx } from './sent_tx.js';
+
 
 /**
  * A proven transaction that can be sent to the network. Returned by the `prove` method of a contract interaction.
@@ -19,23 +22,25 @@ export class ProvenTx extends Tx {
     );
   }
 
+  // Clone the TX data to get a serializable object.
+  protected getPlainDataTx(): Tx {
+    return new Tx(
+      this.data,
+      this.clientIvcProof,
+      this.noteEncryptedLogs,
+      this.encryptedLogs,
+      this.unencryptedLogs,
+      this.enqueuedPublicFunctionCalls,
+      this.publicTeardownFunctionCall,
+    );
+  }
+
   /**
    * Sends the transaction to the network via the provided wallet.
    */
   public send(): SentTx {
     const promise = (() => {
-      // Clone the TX data to avoid serializing the ProvenTx object.
-      return this.wallet.sendTx(
-        new Tx(
-          this.data,
-          this.clientIvcProof,
-          this.noteEncryptedLogs,
-          this.encryptedLogs,
-          this.unencryptedLogs,
-          this.enqueuedPublicFunctionCalls,
-          this.publicTeardownFunctionCall,
-        ),
-      );
+      return this.wallet.sendTx(this.getPlainDataTx());
     })();
 
     return new SentTx(this.wallet, promise);
