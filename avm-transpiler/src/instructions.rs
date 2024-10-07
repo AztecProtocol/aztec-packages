52d1dcc91bc6b0ef4fcc4862f4220958258871ff
dbanks12 <david@aztecprotocol.com>
2024-10-07 18:52:16 +0000

    featBrillig and AVM default all uninitialized memory cells to Field 0

--- avm-transpiler/src/instructions.rs
+++ avm-transpiler/src/instructions.rs
@@ -93,7 +93,6 @@ impl Default for AvmInstruction {
 #[allow(clippy::upper_case_acronyms, dead_code)]
 #[derive(Copy, Clone, Debug)]
 pub enum AvmTypeTag {
-    UNINITIALIZED,
     UINT1,
     UINT8,
     UINT16,
