a34847d25262ecb367cc1b950a5c940b7b0d8260
Joe Andrews <joe@aztecprotocol.com>
2024-10-10 15:53:26 +0100

    add validator logging

--- yarn-project/foundation/src/log/logger.ts
+++ yarn-project/foundation/src/log/logger.ts
@@ -45,7 +45,9 @@ export type DebugLogger = Logger;
 
 export function createDebugLogger(name: string, fixedLogData?: LogData): DebugLogger {
   const debugLogger = debug(name);
-  if (currentLevel === 'debug') debugLogger.enabled = true;
+  if (currentLevel === 'debug') {
+    debugLogger.enabled = true;
+  }
 
   const attatchFixedLogData = (data?: LogData) => ({ ...fixedLogData, ...data });
 
