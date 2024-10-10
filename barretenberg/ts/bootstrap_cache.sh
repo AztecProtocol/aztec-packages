da8e1fd857b58df40ef7550cc1ca57361260f21a
Alex Gherghisan <alexg@aztecprotocol.com>
2024-10-10 15:55:25 +0000

    fix: cache scripts

--- barretenberg/ts/bootstrap_cache.sh
+++ barretenberg/ts/bootstrap_cache.sh
@@ -4,12 +4,14 @@ set -eu
 cd "$(dirname "$0")"
 source ../../build-system/scripts/setup_env '' '' mainframe_$USER > /dev/null
 
-echo -e "\033[1mRetrieving bb.js from remote cache...\033[0m"
-HASH=$(AZTEC_CACHE_REBUILD_PATTERNS="../cpp/.rebuild_patterns .rebuild_patterns" compute-content-hash.sh)
+CACHE_SCRIPTS=../../build-system/s3-cache-scripts
 
-cache-download.sh bb.js-esm-$HASH.tar.gz > /dev/null
-cache-download.sh bb.js-cjs-$HASH.tar.gz > /dev/null
-cache-download.sh bb.js-browser-$HASH.tar.gz > /dev/null
+echo -e "\033[1mRetrieving bb.js from remote cache...\033[0m"
+HASH=$(AZTEC_CACHE_REBUILD_PATTERNS="../cpp/.rebuild_patterns .rebuild_patterns" $CACHE_SCRIPTS/compute-content-hash.sh)
+
+$CACHE_SCRIPTS/cache-download.sh bb.js-esm-$HASH.tar.gz > /dev/null
+$CACHE_SCRIPTS/cache-download.sh bb.js-cjs-$HASH.tar.gz > /dev/null
+$CACHE_SCRIPTS/cache-download.sh bb.js-browser-$HASH.tar.gz > /dev/null
 
 # Annoyingly we still need to install modules, so they can be found as part of module resolution when portalled.
 yarn install
