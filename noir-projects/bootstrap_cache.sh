d9e6864eec7c8dd52ce7f2102463f554fae124c4
Alex Gherghisan <alexg@aztecprotocol.com>
2024-10-10 10:28:45 +0000

    fix: cache noir projects

--- noir-projects/bootstrap_cache.sh
+++ noir-projects/bootstrap_cache.sh
@@ -5,13 +5,16 @@ cd "$(dirname "$0")"
 source ../build-system/scripts/setup_env '' '' mainframe_$USER > /dev/null
 
 echo -e "\033[1mRetrieving noir projects from remote cache...\033[0m"
-extract_repo_if_working_copy_clean noir-projects \
-  /usr/src/noir-projects/noir-contracts/target ./noir-contracts \
-  /usr/src/noir-projects/noir-protocol-circuits/target ./noir-protocol-circuits
 
-remove_old_images noir-projects
+PROTOCOL_CIRCUITS_HASH=$(AZTEC_CACHE_REBUILD_PATTERNS="../noir/.rebuild_patterns_native ../barretenberg/cpp/.rebuild_patterns ./noir-protocol-circuits/.rebuild_patterns" compute-content-hash.sh)
+cache-download.sh noir-projects-noir-protocol-circuits-$PROTOCOL_CIRCUITS_HASH.tar.gz noir-protocol-circuits > /dev/null
+
+MOCK_CIRCUITS_HASH=$(AZTEC_CACHE_REBUILD_PATTERNS="../noir/.rebuild_patterns_native ../barretenberg/cpp/.rebuild_patterns ./mock-protocol-circuits/.rebuild_patterns" compute-content-hash.sh)
+cache-download.sh noir-projects-mock-protocol-circuits-$MOCK_CIRCUITS_HASH.tar.gz mock-protocol-circuits > /dev/null
+
+CONTRACTS_HASH=$(AZTEC_CACHE_REBUILD_PATTERNS="../noir/.rebuild_patterns_native ../avm-transpiler/.rebuild_patterns ../barretenberg/cpp/.rebuild_patterns noir-contracts/.rebuild_patterns" compute-content-hash.sh)
+cache-download.sh noir-projects-noir-contracts-$CONTRACTS_HASH.tar.gz noir-contracts > /dev/null
 
 yarn
 
-./mock-protocol-circuits/bootstrap.sh
 (cd ./noir-protocol-circuits && yarn && node ./scripts/generate_variants.js)
