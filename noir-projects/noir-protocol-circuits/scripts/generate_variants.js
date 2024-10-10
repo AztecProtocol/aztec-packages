5aa28826aeaab68fdf3833653237a243f8dbfee6
Alex Gherghisan <alexg@aztecprotocol.com>
2024-10-10 16:21:12 +0000

    chore: revert dimensions

--- noir-projects/noir-protocol-circuits/scripts/generate_variants.js
+++ noir-projects/noir-protocol-circuits/scripts/generate_variants.js
@@ -69,10 +69,8 @@ function generateDimensions(dimensionsList) {
 
   const fileContent = ["[", dimensionValuesStrings.join(",\n"), "]"].join("\n");
 
-  const outputFilePath = path.join(root, "target", "dimensions", "private_kernel_reset_dimensions.json");
-  fs.mkdirSync(path.dirname(outputFilePath), { recursive: true });
   fs.writeFileSync(
-    outputFilePath,
+    path.join(root, "private_kernel_reset_dimensions.json"),
     fileContent
   );
 }
