83b79e88abda206df1c5b8f8aea6b8ef3c6963b3
Cody Gunton <codygunton@gmail.com>
2024-10-11 16:57:25 +0100

    Clean up cmake stuff a bit

--- barretenberg/cpp/scripts/profile_tracy_capture_mainframe_view_local.sh
+++ barretenberg/cpp/scripts/profile_tracy_capture_mainframe_view_local.sh
@@ -15,7 +15,7 @@ BENCHMARK=${2:-client_ivc_bench}
 COMMAND=${3:-./bin/$BENCHMARK --benchmark_filter=ClientIVCBench/Full/6"\$"}
 HARDWARE_CONCURRENCY=${HARDWARE_CONCURRENCY:-16}
 # Can also set PRESET=tracy-gates env variable
-PRESET=${PRESET:-tracy-time-instrumented}
+PRESET=${PRESET:-tracy-memory}
 
 ssh $BOX "
 	set -eux ;
