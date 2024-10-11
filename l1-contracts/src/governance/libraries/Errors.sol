2da2fe2655ad57ab2bc19d589768b2b84ee8e393
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2024-10-11 14:04:48 +0100

    feat: sysstia (#8941)
    Fixes #8136.

--- l1-contracts/src/governance/libraries/Errors.sol
+++ l1-contracts/src/governance/libraries/Errors.sol
@@ -15,4 +15,6 @@ library Errors {
 
   error Registry__RollupAlreadyRegistered(address rollup); // 0x3c34eabf
   error Registry__RollupNotRegistered(address rollup); // 0xa1fee4cf
+
+  error Sysstia__InvalidCaller(address caller, address canonical); // 0xb95e39f6
 }
