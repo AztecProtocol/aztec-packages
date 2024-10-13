26edb4dd0b47df5d079fa8af7d20adef26da8ad7
just-mitch <68168980+just-mitch@users.noreply.github.com>
2024-10-13 10:48:51 +0100

    feat: externally accessible spartan deployment (#9171)
    Update the aztec-network helm chart to be accessible by external
    validators.
    
    Add notes and script for deploying the network and external validators
    in that capacity.

--- spartan/aztec-network/files/config/deploy-l1-contracts.sh
+++ spartan/aztec-network/files/config/deploy-l1-contracts.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
-set -eu
+set -exu
 
 alias aztec='node --no-warnings /usr/src/yarn-project/aztec/dest/bin/index.js'
 
-# Pass the bootnode url as an argument
-# Ask the bootnode for l1 contract addresses
-output=$(aztec get-node-info -u $1)
+# Run the deploy-l1-contracts command and capture the output
+output=""
+# if INIT_VALIDATORS is true, then we need to pass the validators flag to the deploy-l1-contracts command
+if [ "$INIT_VALIDATORS" = "true" ]; then
+  output=$(aztec deploy-l1-contracts --validators $1)
+else
+  output=$(aztec deploy-l1-contracts)
+fi
 
 echo "$output"
 
-boot_node_enr=$(echo "$output" | grep -oP 'Node ENR: \Kenr:[a-zA-Z0-9\-\_\.]+')
+# Extract contract addresses using grep and regex
 rollup_address=$(echo "$output" | grep -oP 'Rollup Address: \K0x[a-fA-F0-9]{40}')
 registry_address=$(echo "$output" | grep -oP 'Registry Address: \K0x[a-fA-F0-9]{40}')
 inbox_address=$(echo "$output" | grep -oP 'L1 -> L2 Inbox Address: \K0x[a-fA-F0-9]{40}')
@@ -17,10 +22,8 @@ outbox_address=$(echo "$output" | grep -oP 'L2 -> L1 Outbox Address: \K0x[a-fA-F
 fee_juice_address=$(echo "$output" | grep -oP 'Fee Juice Address: \K0x[a-fA-F0-9]{40}')
 fee_juice_portal_address=$(echo "$output" | grep -oP 'Fee Juice Portal Address: \K0x[a-fA-F0-9]{40}')
 
-
 # Write the addresses to a file in the shared volume
 cat <<EOF > /shared/contracts.env
-export BOOTSTRAP_NODES=$boot_node_enr
 export ROLLUP_CONTRACT_ADDRESS=$rollup_address
 export REGISTRY_CONTRACT_ADDRESS=$registry_address
 export INBOX_CONTRACT_ADDRESS=$inbox_address
