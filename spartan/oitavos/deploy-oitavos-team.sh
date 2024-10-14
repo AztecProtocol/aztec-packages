155b40b67616387f183dcb05d6ab08e9e4c3ab72
just-mitch <68168980+just-mitch@users.noreply.github.com>
2024-10-14 15:55:58 +0100

    chore: offsite network stuff (#9231)
    - turn down transaction bot
    - add ability to remove l1 validator from CLI
    - add ability to prune l1 rollup from CLI
    - add some helpful logging
    - touch up readme for oitavos net.
    - allow a config on the boot node to skip contract deployments to L1.
    - remove the persistence from the boot node yaml because it was
    improperly configured

--- spartan/oitavos/deploy-oitavos-team.sh
+++ spartan/oitavos/deploy-oitavos-team.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 set -eu
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# These need to be set in the environment
-# AZTEC_IMAGE=$1
-# ETHEREUM_HOST=$2
-# BOOT_NODE_URL=$3
-# VALIDATOR_PRIVATE_KEY=$4
-# VALIDATOR_ADDRESS=$5
-# PUBLIC_IP=$6
-# NODE_PORT=$7
-# P2P_TCP_PORT=$8
-# P2P_UDP_PORT=$9
+# These need to be set in the environment.
+# We'll echo them out now to make sure they're set:
+echo "AZTEC_IMAGE: $AZTEC_IMAGE"
+echo "ETHEREUM_HOST: $ETHEREUM_HOST"
+echo "BOOT_NODE_URL: $BOOT_NODE_URL"
+echo "PUBLIC_IP: $PUBLIC_IP"
+echo "VALIDATOR_PRIVATE_KEY: $VALIDATOR_PRIVATE_KEY"
+echo "VALIDATOR_ADDRESS: $VALIDATOR_ADDRESS"
+echo "NODE_PORT: $NODE_PORT"
+echo "P2P_TCP_PORT: $P2P_TCP_PORT"
+echo "P2P_UDP_PORT: $P2P_UDP_PORT"
 
-docker run $AZTEC_IMAGE get-node-info -u $BOOT_NODE_URL | tee $SCRIPT_DIR/node_info.txt
 
-boot_node_enr=$(cat $SCRIPT_DIR/node_info.txt | grep -oP 'Node ENR: \Kenr:[a-zA-Z0-9\-\_\.]+')
-rollup_address=$(cat $SCRIPT_DIR/node_info.txt | grep -oP 'Rollup Address: \K0x[a-fA-F0-9]{40}')
-registry_address=$(cat $SCRIPT_DIR/node_info.txt | grep -oP 'Registry Address: \K0x[a-fA-F0-9]{40}')
-inbox_address=$(cat $SCRIPT_DIR/node_info.txt | grep -oP 'L1 -> L2 Inbox Address: \K0x[a-fA-F0-9]{40}')
-outbox_address=$(cat $SCRIPT_DIR/node_info.txt | grep -oP 'L2 -> L1 Outbox Address: \K0x[a-fA-F0-9]{40}')
-fee_juice_address=$(cat $SCRIPT_DIR/node_info.txt | grep -oP 'Fee Juice Address: \K0x[a-fA-F0-9]{40}')
-fee_juice_portal_address=$(cat $SCRIPT_DIR/node_info.txt | grep -oP 'Fee Juice Portal Address: \K0x[a-fA-F0-9]{40}')
+
+docker run $AZTEC_IMAGE get-node-info -u $BOOT_NODE_URL | tee ./node_info.txt
+
+boot_node_enr=$(cat ./node_info.txt | grep -oP 'Node ENR: \Kenr:[a-zA-Z0-9\-\_\.]+')
+rollup_address=$(cat ./node_info.txt | grep -oP 'Rollup Address: \K0x[a-fA-F0-9]{40}')
+registry_address=$(cat ./node_info.txt | grep -oP 'Registry Address: \K0x[a-fA-F0-9]{40}')
+inbox_address=$(cat ./node_info.txt | grep -oP 'L1 -> L2 Inbox Address: \K0x[a-fA-F0-9]{40}')
+outbox_address=$(cat ./node_info.txt | grep -oP 'L2 -> L1 Outbox Address: \K0x[a-fA-F0-9]{40}')
+fee_juice_address=$(cat ./node_info.txt | grep -oP 'Fee Juice Address: \K0x[a-fA-F0-9]{40}')
+fee_juice_portal_address=$(cat ./node_info.txt | grep -oP 'Fee Juice Portal Address: \K0x[a-fA-F0-9]{40}')
 
 
 # Write the addresses to a file in the shared volume
-cat <<EOF > $SCRIPT_DIR/validator.env
+cat <<EOF > ./validator.env
 BOOTSTRAP_NODES=$boot_node_enr
 ROLLUP_CONTRACT_ADDRESS=$rollup_address
 REGISTRY_CONTRACT_ADDRESS=$registry_address
@@ -49,13 +51,14 @@ P2P_TCP_ANNOUNCE_ADDR=$PUBLIC_IP:$P2P_TCP_PORT
 P2P_UDP_ANNOUNCE_ADDR=$PUBLIC_IP:$P2P_UDP_PORT
 P2P_TCP_LISTEN_ADDR=0.0.0.0:$P2P_TCP_PORT
 P2P_UDP_LISTEN_ADDR=0.0.0.0:$P2P_UDP_PORT
+COINBASE=$VALIDATOR_ADDRESS
 EOF
 
-cat $SCRIPT_DIR/validator.env
+cat ./validator.env
 
 docker run $AZTEC_IMAGE add-l1-validator -u $ETHEREUM_HOST --validator $VALIDATOR_ADDRESS --rollup $rollup_address
 docker run --rm \
- --env-file $SCRIPT_DIR/validator.env \
+ --env-file ./validator.env \
  -p $NODE_PORT:$NODE_PORT \
  -p $P2P_TCP_PORT:$P2P_TCP_PORT \
  -p $P2P_UDP_PORT:$P2P_UDP_PORT/udp \
