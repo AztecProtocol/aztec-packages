d6b1ca248571ce221fbe8204c7e7547e5d5e1793
ludamad <domuradical@gmail.com>
2024-10-17 10:41:52 +0000

    .

--- docker_fast.sh
+++ docker_fast.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # TODO eventually rename this docker.sh when we've moved to it entirely
-set -eux
+set -eu
 
 function start_minio() {
   if nc -z 127.0.0.1 12000 2>/dev/null >/dev/null ; then
@@ -20,10 +20,12 @@ S3_BUILD_CACHE_MINIO_URL="http://$(hostname -I | awk '{print $1}'):12000"
 start_minio
 
 if ! git diff-index --quiet HEAD --; then
-  echo "Warning: You have unstaged changes. Disabling S3 caching and local minio caching for earthly to not accidentally pollute cache (which uses git data)." >&2
+  echo "Warning: You have unstaged changes. Disabling S3 caching and local minio caching to not accidentally pollute cache (which uses git data)." >&2
   S3_BUILD_CACHE_UPLOAD=false
   S3_BUILD_CACHE_DOWNLOAD=false
   S3_BUILD_CACHE_MINIO_URL=""
+  echo "Fatal: For now, this is a fatal error as it would defeat the purpose of 'fast'." >&2
+  exit 1
 elif [ ! -z "${AWS_ACCESS_KEY_ID:-}" ] ; then
   S3_BUILD_CACHE_DOWNLOAD=true
 elif [ -f ~/.aws/credentials ]; then
@@ -43,7 +45,6 @@ function on_exit() {
 }
 trap on_exit EXIT
 
-
 # Copy each secret environment variable into a separate file in $TMP_DIR
 echo "${AWS_ACCESS_KEY_ID:-}" > "$TMP/aws_access_key_id.txt"
 echo "${AWS_SECRET_ACCESS_KEY:-}" > "$TMP/aws_secret_access_key.txt"
@@ -51,13 +52,14 @@ echo "${S3_BUILD_CACHE_MINIO_URL:-}" > "$TMP/s3_build_cache_minio_url.txt"
 echo "${S3_BUILD_CACHE_UPLOAD:-}" > "$TMP/s3_build_cache_upload.txt"
 echo "${S3_BUILD_CACHE_DOWNLOAD:-}" > "$TMP/s3_build_cache_download.txt"
 
+# Create our .dockerignore from .gitignore
+npx gitignore-to-dockerignore
 cd $(git rev-parse --show-toplevel)
 # Secrets can be blank BUT will disable caching and do a slow build
 # note: currently the s3 build caches are unused
-DOCKER_BUILDKIT=1 docker build -t aztecprotocol/aztec -f Dockerfile.fast \
+DOCKER_BUILDKIT=1 docker build -t aztecprotocol/aztec -f Dockerfile.fast --progress=plain \
   --secret id=aws_access_key_id,src=$TMP/aws_access_key_id.txt \
   --secret id=aws_secret_access_key,src=$TMP/aws_secret_access_key.txt \
-  --secret id=aws_default_region,src=$TMP/aws_default_region.txt \
   --secret id=s3_build_cache_minio_url,src=$TMP/s3_build_cache_minio_url.txt \
   --secret id=s3_build_cache_upload,src=$TMP/s3_build_cache_upload.txt \
   --secret id=s3_build_cache_download,src=$TMP/s3_build_cache_download.txt \
