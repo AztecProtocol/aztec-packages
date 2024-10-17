d9a8fa2c00b52bcdb64113e7e94cffd1b09ae131
ludamad <domuradical@gmail.com>
2024-10-17 18:35:40 +0000

    .

--- docker_fast.sh
+++ docker_fast.sh
@@ -9,18 +9,18 @@ function start_minio() {
   fi
   docker run -d -p 12000:9000 -p 12001:12001 -v minio-data:/data \
     quay.io/minio/minio server /data --console-address ":12001"
-  # make our cache bucket
+  # Make our cache bucket
   AWS_ACCESS_KEY_ID="minioadmin" AWS_SECRET_ACCESS_KEY="minioadmin" aws --endpoint-url http://localhost:12000 s3 mb s3://aztec-ci-artifacts 2>/dev/null || true
 }
 
 S3_BUILD_CACHE_UPLOAD=${S3_BUILD_CACHE_UPLOAD:-false}
 S3_BUILD_CACHE_MINIO_URL="http://$(hostname -I | awk '{print $1}'):12000"
 
-# local file server for a quicker cache layer
+# Start local file server for a quicker cache layer
 start_minio
 
 if ! git diff-index --quiet HEAD --; then
-  echo "Warning: You have unstaged changes. Disabling S3 caching and local minio caching to not accidentally pollute cache (which uses git data)." >&2
+  echo "Warning: You have unstaged changes. Disabling S3 caching and local MinIO caching to avoid polluting cache (which uses Git data)." >&2
   S3_BUILD_CACHE_UPLOAD=false
   S3_BUILD_CACHE_DOWNLOAD=false
   S3_BUILD_CACHE_MINIO_URL=""
@@ -29,7 +29,7 @@ if ! git diff-index --quiet HEAD --; then
 elif [ ! -z "${AWS_ACCESS_KEY_ID:-}" ] ; then
   S3_BUILD_CACHE_DOWNLOAD=true
 elif [ -f ~/.aws/credentials ]; then
-  # make credentials avaialble to earthly
+  # Retrieve credentials if available in AWS config
   AWS_ACCESS_KEY_ID=$(aws configure get default.aws_access_key_id)
   AWS_SECRET_ACCESS_KEY=$(aws configure get default.aws_secret_access_key)
   S3_BUILD_CACHE_DOWNLOAD=true
@@ -40,24 +40,57 @@ fi
 
 TMP=$(mktemp -d)
 
-function on_exit() {
-  rm -rf "$TMP"
-}
-trap on_exit EXIT
+# function on_exit() {
+#   rm -rf "$TMP"
+# }
+# trap on_exit EXIT
 
-# Copy each secret environment variable into a separate file in $TMP_DIR
+# Save each secret environment variable into a separate file in $TMP directory
 echo "${AWS_ACCESS_KEY_ID:-}" > "$TMP/aws_access_key_id.txt"
 echo "${AWS_SECRET_ACCESS_KEY:-}" > "$TMP/aws_secret_access_key.txt"
 echo "${S3_BUILD_CACHE_MINIO_URL:-}" > "$TMP/s3_build_cache_minio_url.txt"
 echo "${S3_BUILD_CACHE_UPLOAD:-}" > "$TMP/s3_build_cache_upload.txt"
 echo "${S3_BUILD_CACHE_DOWNLOAD:-}" > "$TMP/s3_build_cache_download.txt"
 
-# Create our .dockerignore from .gitignore
+# Create a Dockerfile dynamically using git files list
+DOCKERFILE_PATH="$TMP/Dockerfile"
+cat <<EOF > $DOCKERFILE_PATH
+# Auto-generated Dockerfile based on current git files
+
+# Use an ARG to define the architecture, defaulting to amd64
+ARG ARCH=amd64
+
+# Conditionally set the FROM image based on the ARCH argument
+FROM aztecprotocol/build:1.0-\${ARCH}
+
+# Set working directory
+WORKDIR /usr/src
+
+# Copy all files from the git repository to the Docker image
+COPY $(git ls-files | tr '\n' ' ') .
+
+# Initialize git to use git commands if necessary
+RUN git init -b master
+
+# Set caching environment variable
+ENV USE_CACHE=1
+
+# Mount secrets and execute bootstrap script
+RUN --mount=type=secret,id=aws_access_key_id \\
+    --mount=type=secret,id=aws_secret_access_key \\
+    export AWS_ACCESS_KEY_ID=\$(cat /run/secrets/aws_access_key_id) && \\
+    export AWS_SECRET_ACCESS_KEY=\$(cat /run/secrets/aws_secret_access_key) && \\
+    cd barretenberg && ./bootstrap.sh
+EOF
+
+echo "$TMP/Dockerfile"
+
+# Generate .dockerignore from .gitignore
 npx gitignore-to-dockerignore
 cd $(git rev-parse --show-toplevel)
-# Secrets can be blank BUT will disable caching and do a slow build
-# note: currently the s3 build caches are unused
-DOCKER_BUILDKIT=1 docker build -t aztecprotocol/aztec -f Dockerfile.fast --progress=plain \
+
+# Run Docker build with secrets
+DOCKER_BUILDKIT=1 docker build -t aztecprotocol/aztec -f $DOCKERFILE_PATH --progress=plain \
   --secret id=aws_access_key_id,src=$TMP/aws_access_key_id.txt \
   --secret id=aws_secret_access_key,src=$TMP/aws_secret_access_key.txt \
   --secret id=s3_build_cache_minio_url,src=$TMP/s3_build_cache_minio_url.txt \
