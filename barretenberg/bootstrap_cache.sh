f89cc3422c5e3b6f69cfc43940c7a931c6713a82
ludamad <domuradical@gmail.com>
2024-10-20 18:07:12 +0000

    .

--- barretenberg/bootstrap_cache.sh
+++ barretenberg/bootstrap_cache.sh
@@ -3,6 +3,15 @@ set -eu
 
 cd "$(dirname "$0")"
 
-(cd cpp && ./bootstrap_cache.sh $@) &
-(cd ts && ./bootstrap_cache.sh $@) &
-wait
+# Run both tasks in the background
+(cd cpp && ./bootstrap_cache.sh "$@") &
+pid_cpp=$!
+(cd ts && ./bootstrap_cache.sh "$@") &
+pid_ts=$!
+
+# Wait for both processes and capture any non-zero exit codes
+wait $pid_cpp || exit_code=$?
+wait $pid_ts || exit_code=$?
+
+# Exit with the first non-zero exit code, if any
+exit ${exit_code:-0}
