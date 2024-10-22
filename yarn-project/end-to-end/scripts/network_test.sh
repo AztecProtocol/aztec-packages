4bde9cfe2c69a577d6fcb4c97caedec21f1d1e3c
ludamad <domuradical@gmail.com>
2024-10-22 00:36:04 +0000

    try fix

--- yarn-project/end-to-end/scripts/network_test.sh
+++ yarn-project/end-to-end/scripts/network_test.sh
@@ -65,7 +65,7 @@ show_status_until_pxe_ready &
 
 function cleanup() {
   # kill everything in our process group except our process
-  echo kill $(pgrep -g $$ | grep -v $$)
+  kill $(pgrep -g $$ | grep -v $$)
 }
 trap cleanup SIGINT SIGTERM EXIT
 # Install the Helm chart
