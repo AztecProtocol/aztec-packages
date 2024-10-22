d482a22156fb5f26ec398cb70a3f799f81239049
AztecBot <tech@aztecprotocol.com>
2024-10-22 08:02:27 +0000

    [1 changes] fix: Allow array map on empty arrays (https://github.com/noir-lang/noir/pull/6305) fix: Display function name and body when inlining recursion limit hit (https://github.com/noir-lang/noir/pull/6291) feat(interpreter): Comptime derive generators (https://github.com/noir-lang/noir/pull/6303) fix: enforce correctness of decompositions performed at compile time (https://github.com/noir-lang/noir/pull/6278) feat: Warn about private types leaking in public functions and struct fields (https://github.com/noir-lang/noir/pull/6296) chore(docs): refactoring guides and some other nits (https://github.com/noir-lang/noir/pull/6175) fix: Do not warn on unused self in traits (https://github.com/noir-lang/noir/pull/6298) fix: Reject invalid expression with in CLI parser (https://github.com/noir-lang/noir/pull/6287)

--- noir/noir-repo/compiler/noirc_driver/src/lib.rs
+++ noir/noir-repo/compiler/noirc_driver/src/lib.rs
@@ -5,6 +5,7 @@
 
 use abi_gen::{abi_type_from_hir_type, value_from_hir_expression};
 use acvm::acir::circuit::ExpressionWidth;
+use acvm::compiler::MIN_EXPRESSION_WIDTH;
 use clap::Args;
 use fm::{FileId, FileManager};
 use iter_extended::vecmap;
@@ -134,7 +135,11 @@ pub fn parse_expression_width(input: &str) -> Result<ExpressionWidth, std::io::E
 
     match width {
         0 => Ok(ExpressionWidth::Unbounded),
-        _ => Ok(ExpressionWidth::Bounded { width }),
+        w if w >= MIN_EXPRESSION_WIDTH => Ok(ExpressionWidth::Bounded { width }),
+        _ => Err(Error::new(
+            ErrorKind::InvalidInput,
+            format!("has to be 0 or at least {MIN_EXPRESSION_WIDTH}"),
+        )),
     }
 }
 
@@ -445,7 +450,7 @@ fn compile_contract_inner(
             .secondary
             .iter()
             .filter_map(|attr| {
-                if let SecondaryAttribute::Custom(attribute) = attr {
+                if let SecondaryAttribute::Tag(attribute) = attr {
                     Some(&attribute.contents)
                 } else {
                     None
