0db1443a3637306dd6fc251e69688088b251aa35
ludamad <domuradical@gmail.com>
2024-10-23 20:52:05 +0000

    fix

--- spartan/scripts/deploy_spartan.sh
+++ spartan/scripts/deploy_spartan.sh
@@ -2,13 +2,13 @@
 set -eux
 set -o pipefail
 
-IMAGE=$1
+TAG=$1
 VALUES=$2
 NAMESPACE=${3:-spartan}
 
 if [ -z "$IMAGE" ]; then
-  echo "Usage: $0 <fully qualified docker image> <values>"
-  echo "Example: $0 aztecprotocol/aztec@sha256:<exact image...> 48-validators"
+  echo "Usage: $0 <docker image tag> <values>"
+  echo "Example: $0 latest 48-validators"
   exit 1
 fi
 
@@ -39,6 +39,9 @@ function log_stern() {
 log_stern &
 
 function upgrade() {
+  # pull and resolve the image just to be absolutely sure k8s gets the latest image in the tag we want
+  docker pull aztecprotocol/aztec:$TAG
+  IMAGE=$(docker inspect --format='{{index .RepoDigests 0}}' aztecprotocol/aztec:$TAG)
   helm upgrade --install $NAMESPACE $SCRIPT_DIR/../aztec-network \
         --namespace $NAMESPACE \
         --create-namespace \
@@ -49,6 +52,7 @@ function upgrade() {
         --wait-for-jobs=true \
         --timeout=30m 2>&1
 }
+# docker inspect --format='{{index .RepoDigests 0}}' aztecprotocol/aztec:master
 if ! upgrade | tee "$SCRIPT_DIR/logs/$NAMESPACE-helm.log" ; then
   if grep 'cannot patch "'$NAMESPACE'-aztec-network-setup-l2-contracts"' "$SCRIPT_DIR/logs/$NAMESPACE-helm.log" ; then
     kubectl delete job $NAMESPACE-aztec-network-setup-l2-contracts -n $NAMESPACE
