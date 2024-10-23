f34742e5155eb1872378ce1cb3653a7ffa775c93
ludamad <domuradical@gmail.com>
2024-10-23 22:18:17 +0000

    allow native-network scripts to be called

--- yarn-project/end-to-end/scripts/native-network/test-4epochs.sh
+++ yarn-project/end-to-end/scripts/native-network/test-4epochs.sh
@@ -8,7 +8,7 @@ SCRIPT_NAME=$(basename "$0" .sh)
 # Redirect stdout and stderr to <script_name>.log while also printing to the console
 exec > >(tee -a "$(dirname $0)/logs/${SCRIPT_NAME}.log") 2> >(tee -a "$(dirname $0)/logs/${SCRIPT_NAME}.log" >&2)
 
-export BOOTNODE_URL=${BOOTNODE_URL:-http://127.0.0.1:8079}
+export BOOTNODE_URL=${BOOTNODE_URL:-http://127.0.0.1:8080}
 export PXE_URL=${PXE_URL:-http://127.0.0.1:8079}
 export ETHEREUM_HOST=${ETHEREUM_HOST:-http://127.0.0.1:8545}
 
