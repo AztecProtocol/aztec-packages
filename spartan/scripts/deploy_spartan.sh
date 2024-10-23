f7586abae23f7b1cd412d10493e0c64eee48f755
ludamad <domuradical@gmail.com>
2024-10-23 21:50:54 +0000

    update

--- spartan/scripts/deploy_spartan.sh
+++ spartan/scripts/deploy_spartan.sh
@@ -5,10 +5,11 @@ set -o pipefail
 TAG=$1
 VALUES=$2
 NAMESPACE=${3:-spartan}
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [ -z "$TAG" ]; then
-  echo "Usage: $0 <docker image tag> <values>"
-  echo "Example: $0 latest 48-validators"
+  echo "Usage: $0 <docker image tag> <values> (optional: <namespace>)"
+  echo "Example: $0 latest 48-validators devnet"
   exit 1
 fi
 
@@ -30,7 +31,6 @@ function show_status_until_pxe_ready() {
   done
 }
 show_status_until_pxe_ready &
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 function log_stern() {
   set +x
@@ -40,19 +40,28 @@ log_stern &
 
 function upgrade() {
   # pull and resolve the image just to be absolutely sure k8s gets the latest image in the tag we want
-  docker pull aztecprotocol/aztec:$TAG
+  docker pull --platform linux/amd64 aztecprotocol/aztec:$TAG
   IMAGE=$(docker inspect --format='{{index .RepoDigests 0}}' aztecprotocol/aztec:$TAG)
-  helm upgrade --install $NAMESPACE $SCRIPT_DIR/../aztec-network \
-        --namespace $NAMESPACE \
-        --create-namespace \
-        --values $SCRIPT_DIR/../aztec-network/values/$VALUES.yaml \
-        --set images.aztec.image="$IMAGE" \
-        --set network.public=true \
-        --wait \
-        --wait-for-jobs=true \
-        --timeout=30m 2>&1
+  if ! [ -z "${PRINT_ONLY:-}" ] ; then
+    helm template $NAMESPACE $SCRIPT_DIR/../aztec-network \
+          --namespace $NAMESPACE \
+          --create-namespace \
+          --values $SCRIPT_DIR/../aztec-network/values/$VALUES.yaml \
+          --set images.aztec.image="$IMAGE" \
+          --set network.public=true
+  else
+    helm upgrade --install $NAMESPACE $SCRIPT_DIR/../aztec-network \
+          --namespace $NAMESPACE \
+          --create-namespace \
+          --values $SCRIPT_DIR/../aztec-network/values/$VALUES.yaml \
+          --set images.aztec.image="$IMAGE" \
+          --set network.public=true \
+          --wait \
+          --wait-for-jobs=true \
+          --timeout=30m 2>&1
+  fi
 }
-# docker inspect --format='{{index .RepoDigests 0}}' aztecprotocol/aztec:master
+
 if ! upgrade | tee "$SCRIPT_DIR/logs/$NAMESPACE-helm.log" ; then
   if grep 'cannot patch "'$NAMESPACE'-aztec-network-setup-l2-contracts"' "$SCRIPT_DIR/logs/$NAMESPACE-helm.log" ; then
     kubectl delete job $NAMESPACE-aztec-network-setup-l2-contracts -n $NAMESPACE
--- yarn-project/end-to-end/scripts/native-network/test-4epochs.sh
+++ yarn-project/end-to-end/scripts/native-network/test-4epochs.sh
@@ -8,17 +8,21 @@ SCRIPT_NAME=$(basename "$0" .sh)
 # Redirect stdout and stderr to <script_name>.log while also printing to the console
 exec > >(tee -a "$(dirname $0)/logs/${SCRIPT_NAME}.log") 2> >(tee -a "$(dirname $0)/logs/${SCRIPT_NAME}.log" >&2)
 
+export BOOTNODE_URL=${BOOTNODE_URL:-http://127.0.0.1:8079}
+export PXE_URL=${PXE_URL:-http://127.0.0.1:8079}
+export ETHEREUM_HOST=${ETHEREUM_HOST:-http://127.0.0.1:8545}
+
 REPO=$(git rev-parse --show-toplevel)
 # Run our test assuming the port in pxe.sh
 # Wait for the Aztec Node to be ready
 echo "Waiting for Aztec Node..."
-until curl -s http://127.0.0.1:8080/status >/dev/null ; do
+until curl -s $BOOTNODE_URL/status >/dev/null ; do
   sleep 1
 done
 echo "Waiting for PXE service..."
 until curl -s -X POST -H 'content-type: application/json' \
   -d '{"jsonrpc":"2.0","method":"pxe_getNodeInfo","params":[],"id":67}' \
-  http://127.0.0.1:8079 | grep -q '"enr:-'; do
+  $PXE_URL | grep -q '"enr:-'; do
   sleep 1
 done
 echo "Waiting for l2 contracts to be deployed..."
@@ -29,7 +33,5 @@ echo "Done waiting."
 
 export DEBUG="aztec:*"
 export LOG_LEVEL=${LOG_LEVEL:-"debug"}
-export PXE_URL=http://localhost:8079
-export ETHEREUM_HOST=http://127.0.0.1:8545
 cd $(git rev-parse --show-toplevel)/yarn-project/end-to-end
 yarn test src/spartan/4epochs.test.ts
