34ecfc9df82ce2ac71e9f975c9389046e16a744c
ludamad <domuradical@gmail.com>
2024-10-23 20:30:40 +0000

    iterate on deploy spartan

--- spartan/scripts/deploy_spartan.sh
+++ spartan/scripts/deploy_spartan.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-set -eu
+set -eux
+set -o pipefail
 
-TAG=$1
+IMAGE=$1
 VALUES=$2
 NAMESPACE=${3:-spartan}
 
-if [ -z "$TAG" ]; then
+if [ -z "$IMAGE" ]; then
   echo "Usage: $0 <tag> <values>"
   echo "Example: $0 latest 48-validators"
   exit 1
 fi
 
 function cleanup() {
+  set +x
   # kill everything in our process group except our process
   trap - SIGTERM && kill $(pgrep -g $$ | grep -v $$) $(jobs -p) &>/dev/null || true
 }
 trap cleanup SIGINT SIGTERM EXIT
 
 function show_status_until_pxe_ready() {
+  set +x
   sleep 15 # let helm upgrade start
   kubectl get pods -n $NAMESPACE
   for i in {1..20} ; do
@@ -30,16 +33,25 @@ show_status_until_pxe_ready &
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 function log_stern() {
-  stern $NAMESPACE -n $NAMESPACE 2>&1 >> "$SCRIPT_DIR/$NAMESPACE-deploy.log"
+  set +x
+  stern $NAMESPACE -n $NAMESPACE 2>&1 > "$SCRIPT_DIR/logs/$NAMESPACE-deploy.log"
 }
 log_stern &
 
-helm upgrade --install $NAMESPACE $SCRIPT_DIR/../aztec-network \
-      --namespace $NAMESPACE \
-      --create-namespace \
-      --values $SCRIPT_DIR/../aztec-network/values/$VALUES.yaml \
-      --set images.aztec.image="aztecprotocol/aztec@sha256:5b6a419315169b7f9ff1d9d16b1ce208e5737067405fda3b924c5669934bac8e" \
-      --set network.public=true \
-      --wait \
-      --wait-for-jobs=true \
-      --timeout=30m
+function upgrade() {
+  helm upgrade --install $NAMESPACE $SCRIPT_DIR/../aztec-network \
+        --namespace $NAMESPACE \
+        --create-namespace \
+        --values $SCRIPT_DIR/../aztec-network/values/$VALUES.yaml \
+        --set images.aztec.image="$IMAGE" \
+        --set network.public=true \
+        --wait \
+        --wait-for-jobs=true \
+        --timeout=30m 2>&1
+}
+if ! upgrade | tee "$SCRIPT_DIR/logs/$NAMESPACE-helm.log" ; then
+  if grep 'cannot patch "'$NAMESPACE'-aztec-network-setup-l2-contracts"' "$SCRIPT_DIR/logs/$NAMESPACE-helm.log" ; then
+    kubectl delete job $NAMESPACE-aztec-network-setup-l2-contracts -n $NAMESPACE
+    upgrade
+  fi
+fi
