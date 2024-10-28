30ee02530f5d64372ddba4059e94d4e1c1f73a7d
spypsy <spypsy@outlook.com>
2024-10-28 10:29:51 +0000

    use select_runner for bench as well

--- scripts/ci/select_runner.sh
+++ scripts/ci/select_runner.sh
@@ -11,5 +11,9 @@ test_name=$1
 if requires_prover "$test_name"; then
   echo "64core-tester-x86"
 else
-  echo "8core-tester-x86"
+  if [[ $test_name == *"bench"* ]]; then
+    echo "16core-tester-x86"
+  else
+    echo "8core-tester-x86"
+  fi
 fi
