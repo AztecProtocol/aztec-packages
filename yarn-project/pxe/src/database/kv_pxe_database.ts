1c685b1dd3e7749e6f4570773ecc64ab245d8a0b
Gregorio Juliana <gregojquiros@gmail.com>
2024-10-29 10:23:09 +0100

    feat: added indexes and a way to store/retrieve tagged secrets (#9468)
    Closes: https://github.com/AztecProtocol/aztec-packages/issues/9365
    
    - Allows storing the current known index for a given tagging secret.
    - Returns the current index when calling `get_app_tagging_secret` so
    notes can be sent with the correct one.
    - Adds an oracle that returns the app tagging secrets for the whole
    addressbook, including their indexes.
    
    ---------
    
    Co-authored-by: esau <152162806+sklppy88@users.noreply.github.com>

--- yarn-project/pxe/src/database/kv_pxe_database.ts
+++ yarn-project/pxe/src/database/kv_pxe_database.ts
@@ -66,6 +66,8 @@ export class KVPxeDatabase implements PxeDatabase {
   #notesByTxHashAndScope: Map<string, AztecMultiMap<string, string>>;
   #notesByIvpkMAndScope: Map<string, AztecMultiMap<string, string>>;
 
+  #taggingSecretIndexes: AztecMap<string, number>;
+
   constructor(private db: AztecKVStore) {
     this.#db = db;
 
@@ -111,6 +113,8 @@ export class KVPxeDatabase implements PxeDatabase {
       this.#notesByTxHashAndScope.set(scope, db.openMultiMap(`${scope}:notes_by_tx_hash`));
       this.#notesByIvpkMAndScope.set(scope, db.openMultiMap(`${scope}:notes_by_ivpk_m`));
     }
+
+    this.#taggingSecretIndexes = db.openMap('tagging_secret_indices');
   }
 
   public async getContract(
@@ -571,5 +575,21 @@ export class KVPxeDatabase implements PxeDatabase {
     const treeRootsSize = Object.keys(MerkleTreeId).length * Fr.SIZE_IN_BYTES;
 
     return incomingNotesSize + outgoingNotesSize + treeRootsSize + authWitsSize + addressesSize;
+  }
+
+  async incrementTaggingSecretsIndexes(appTaggingSecrets: Fr[]): Promise<void> {
+    const indexes = await this.getTaggingSecretsIndexes(appTaggingSecrets);
+    await this.db.transaction(() => {
+      indexes.forEach(index => {
+        const nextIndex = index ? index + 1 : 1;
+        void this.#taggingSecretIndexes.set(appTaggingSecrets.toString(), nextIndex);
+      });
+    });
+  }
+
+  getTaggingSecretsIndexes(appTaggingSecrets: Fr[]): Promise<number[]> {
+    return this.db.transaction(() =>
+      appTaggingSecrets.map(secret => this.#taggingSecretIndexes.get(secret.toString()) ?? 0),
+    );
   }
 }
