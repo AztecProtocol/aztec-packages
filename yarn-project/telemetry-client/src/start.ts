6cb4b463d3f5474d1c831d42a03a3272d548a383
ludamad <domuradical@gmail.com>
2024-10-30 00:39:51 +0000

    fix metrics

--- yarn-project/telemetry-client/src/start.ts
+++ yarn-project/telemetry-client/src/start.ts
@@ -1,12 +1,9 @@
 import { createDebugLogger } from '@aztec/foundation/log';
 
-
-
 import { type TelemetryClientConfig } from './config.js';
 import { NoopTelemetryClient } from './noop.js';
 import { OpenTelemetryClient } from './otel.js';
 import { type TelemetryClient } from './telemetry.js';
-
 
 export * from './config.js';
 
@@ -14,7 +11,12 @@ export async function createAndStartTelemetryClient(config: TelemetryClientConfi
   const log = createDebugLogger('aztec:telemetry-client');
   if (config.metricsCollectorUrl) {
     log.info('Using OpenTelemetry client');
-    return await OpenTelemetryClient.createAndStart(config.metricsCollectorUrl, config.tracesCollectorUrl, config.logsCollectorUrl, log);
+    return await OpenTelemetryClient.createAndStart(
+      config.metricsCollectorUrl,
+      config.tracesCollectorUrl,
+      config.logsCollectorUrl,
+      log,
+    );
   } else {
     log.info('Using NoopTelemetryClient');
     return new NoopTelemetryClient();
