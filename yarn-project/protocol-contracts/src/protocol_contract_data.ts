d26cfee4dc32cd72a9fd46ba96c7ea6d9844e306
jeanmon <jean@aztecprotocol.com>
2024-10-31 09:55:18 +0000

    Update protocol contract data

--- yarn-project/protocol-contracts/src/protocol_contract_data.ts
+++ yarn-project/protocol-contracts/src/protocol_contract_data.ts
@@ -50,14 +50,14 @@ export const ProtocolContractAddress: Record<ProtocolContractName, AztecAddress>
 };
 
 export const ProtocolContractLeaf = {
-  AuthRegistry: Fr.fromString('0x2ace300b02ca5ab0a25052b1e852913a47292096997ca09f758c0e3624e84560'),
-  ContractInstanceDeployer: Fr.fromString('0x21e432f60f69ac5eb7582c26c03c6c7e4a3eb577720774bc8e1521561ca752a1'),
-  ContractClassRegisterer: Fr.fromString('0x1b6d5873cef5a35f681ab9468527f356c96e09b3c64603aef404ec2ad80aa3a9'),
-  MultiCallEntrypoint: Fr.fromString('0x0966ead8d11933bb3c72547bb997898971715f2275acd4c7d5d86fdf614ba1a2'),
-  FeeJuice: Fr.fromString('0x017f00981c9ac146ed746087a9ff9edc82b6f276e22840597ec2544f124ea4a0'),
-  Router: Fr.fromString('0x1d8f25db3e8faa6a96cb1ecf57876a2ee04581deb3c4f181488ccd817abcbdb0'),
+  AuthRegistry: Fr.fromString('0x1b6d9c59d70879bf40b7162dc09636b98b79c71de6da4f6220c3ce68299b32ff'),
+  ContractInstanceDeployer: Fr.fromString('0x04a661c9d4d295fc485a7e0f3de40c09b35366343bce8ad229106a8ef4076fe5'),
+  ContractClassRegisterer: Fr.fromString('0x147ba3294403576dbad10f86d3ffd4eb83fb230ffbcd5c8b153dd02942d0611f'),
+  MultiCallEntrypoint: Fr.fromString('0x154b701b41d6cf6da7204fef36b2ee9578b449d21b3792a9287bf45eba48fd26'),
+  FeeJuice: Fr.fromString('0x24f4432454069413e1fd66afb8948401568606bf58aeb177faeb6fa1986d594e'),
+  Router: Fr.fromString('0x04a71f51bcba229640326146613b63e0f18d5bc95825a6ccea1273825c49b973'),
 };
 
 export const protocolContractTreeRoot = Fr.fromString(
-  '0x1aa73a62d09c12a5430b14860f0830123b4fe37a3b61303319fe67448edbe3b6',
+  '0x25472984a7cd9688418bae074f9443884f00e467ea7ac5d458f3aca5e73f6d22',
 );
