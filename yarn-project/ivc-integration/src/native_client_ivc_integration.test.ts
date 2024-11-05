a9747560babb07fb27c05220721cd4cbf25e5b09
Cody <codygunton@gmail.com>
2024-11-05 01:17:55 +0000

    How yet again

--- yarn-project/ivc-integration/src/native_client_ivc_integration.test.ts
+++ yarn-project/ivc-integration/src/native_client_ivc_integration.test.ts
@@ -24,11 +24,7 @@ describe('Client IVC Integration', () => {
   beforeEach(async () => {
     // Create a temp working dir
     bbWorkingDirectory = await fs.mkdtemp(path.join(os.tmpdir(), 'bb-client-ivc-integration-'));
-    bbBinaryPath = path.join(
-      path.dirname(fileURLToPath(import.meta.url)),
-      '../../../barretenberg/cpp/build/bin',
-      'bb',
-    );
+    bbBinaryPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '../../../barretenberg/cpp/build/bin', 'bb');
   });
 
   async function createClientIvcProof(witnessStack: Uint8Array[], bytecodes: string[]): Promise<ClientIvcProof> {
