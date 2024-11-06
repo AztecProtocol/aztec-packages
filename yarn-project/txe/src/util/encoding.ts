cdfa8cb8051a2f67d560ee3c72ac11566f2605ee
Santiago Palladino <santiago@aztecprotocol.com>
2024-11-06 19:19:03 -0300

    Schema for txe dispatcher api

--- yarn-project/txe/src/util/encoding.ts
+++ yarn-project/txe/src/util/encoding.ts
@@ -1,8 +1,13 @@
+import { type ContractArtifact, ContractArtifactSchema } from '@aztec/foundation/abi';
 import { Fr } from '@aztec/foundation/fields';
+
+import { z } from 'zod';
 
 export type ForeignCallSingle = string;
 
 export type ForeignCallArray = string[];
+
+export type ForeignCallArgs = (ForeignCallSingle | ForeignCallArray | ContractArtifact)[];
 
 export type ForeignCallResult = {
   values: (ForeignCallSingle | ForeignCallArray)[];
@@ -27,3 +32,15 @@ export function toArray(objs: Fr[]) {
 export function toForeignCallResult(obj: (ForeignCallSingle | ForeignCallArray)[]) {
   return { values: obj };
 }
+
+export const ForeignCallSingleSchema = z.string();
+
+export const ForeignCallArraySchema = z.array(z.string());
+
+export const ForeignCallArgsSchema = z.array(
+  z.union([ForeignCallSingleSchema, ForeignCallArraySchema, ContractArtifactSchema]),
+);
+
+export const ForeignCallResultSchema = z.object({
+  values: z.array(z.union([ForeignCallSingleSchema, ForeignCallArraySchema])),
+});
