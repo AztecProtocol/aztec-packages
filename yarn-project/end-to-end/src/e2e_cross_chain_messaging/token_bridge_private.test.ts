1170debc74172e65e45c33bdc933c1388939a229
benesjan <janbenes1234@gmail.com>
2024-11-08 19:00:37 +0000

    WIP

--- yarn-project/end-to-end/src/e2e_cross_chain_messaging/token_bridge_private.test.ts
+++ yarn-project/end-to-end/src/e2e_cross_chain_messaging/token_bridge_private.test.ts
@@ -1,9 +1,7 @@
-import { Fr, L1Actor, L1ToL2Message, L2Actor } from '@aztec/aztec.js';
-import { sha256ToField } from '@aztec/foundation/crypto';
+import { Fr } from '@aztec/aztec.js';
 import { RollupAbi } from '@aztec/l1-artifacts';
 
 import { getContract } from 'viem';
-import { toFunctionSelector } from 'viem/utils';
 
 import { CrossChainMessagingTest } from './cross_chain_messaging_test.js';
 
@@ -103,7 +101,8 @@ describe('e2e_cross_chain_messaging token_bridge_private', () => {
   });
   // docs:end:e2e_private_cross_chain
 
-  it('Someone else can mint funds to me on my behalf (privately)', async () => {
+  // This test checks that it's enough to have the claim secret to claim the funds to whoever we want.
+  it('Claim secret is enough to consume the message', async () => {
     const l1TokenBalance = 1000000n;
     const bridgeAmount = 100n;
 
@@ -113,29 +112,6 @@ describe('e2e_cross_chain_messaging token_bridge_private', () => {
 
     // Wait for the message to be available for consumption
     await crossChainTestHarness.makeMessageConsumable(claim.messageHash);
-
-    // 3. Consume L1 -> L2 message and mint private tokens on L2
-    const wrongBridgeAmount = bridgeAmount + 1n;
-    const wrongMessageContent = sha256ToField([
-      Buffer.from(toFunctionSelector('mint_private(uint256)').substring(2), 'hex'),
-      new Fr(wrongBridgeAmount),
-    ]);
-
-    const wrongMessage = new L1ToL2Message(
-      new L1Actor(crossChainTestHarness.tokenPortalAddress, crossChainTestHarness.publicClient.chain.id),
-      new L2Actor(l2Bridge.address, 1),
-      wrongMessageContent,
-      claim.claimSecretHash,
-      new Fr(claim.messageLeafIndex),
-    );
-
-    // Sending wrong secret hashes should fail:
-    await expect(
-      l2Bridge
-        .withWallet(user2Wallet)
-        .methods.claim_private(ownerAddress, wrongBridgeAmount, claim.claimSecret, claim.messageLeafIndex)
-        .prove(),
-    ).rejects.toThrow(`No L1 to L2 message found for message hash ${wrongMessage.hash().toString()}`);
 
     // send the right one -
     await l2Bridge
