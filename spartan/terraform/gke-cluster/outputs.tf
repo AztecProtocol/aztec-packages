d2b27a1befc0ddf28a141234b42081c01e8d65d0
Steven Platt <mr.platt@gmail.com>
2024-11-12 17:29:08 -0500

    updated terraform outputs

--- spartan/terraform/gke-cluster/outputs.tf
+++ spartan/terraform/gke-cluster/outputs.tf
@@ -7,3 +7,13 @@ output "cluster_endpoint" {
 output "service_account_email" {
   value = google_service_account.gke_sa.email
 }
+
+output "region" {
+  description = "Google cloud region"
+  value       = var.region
+}
+
+output "kubernetes_cluster_name" {
+  description = "GKE Cluster Name"
+  value       = google_container_cluster.primary.name
+}
