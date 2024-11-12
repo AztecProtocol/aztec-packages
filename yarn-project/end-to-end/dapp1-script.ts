eb2146e33e80c4161186fbf47274d96a2bbb0e2b
David Banks <47112877+dbanks12@users.noreply.github.com>
2024-11-12 15:49:59 -0500

    chore: delete accidentally added file

--- yarn-project/end-to-end/dapp1-script.ts
+++
@@ -1,1 +0,0 @@
-Docker images not found. They need to be built with 'earthly ./yarn-project/+export-end-to-end' or otherwise tagged with aztecprotocol/end-to-end:7f000e4b57c12ce035c84b2e79034bfe9ab298f3.
