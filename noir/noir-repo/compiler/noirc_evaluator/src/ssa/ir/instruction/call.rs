458d661e141d50b83a65bf64f8a49c7df1271bd8
Tom French <tom@tomfren.ch>
2024-11-12 11:18:24 +0000

    chore!: remove SchnorrVerify opcode

--- noir/noir-repo/compiler/noirc_evaluator/src/ssa/ir/instruction/call.rs
+++ noir/noir-repo/compiler/noirc_evaluator/src/ssa/ir/instruction/call.rs
@@ -559,7 +559,6 @@ fn simplify_black_box_func(
 
         BlackBoxFunc::MultiScalarMul => SimplifyResult::None,
         BlackBoxFunc::EmbeddedCurveAdd => blackbox::simplify_ec_add(dfg, solver, arguments),
-        BlackBoxFunc::SchnorrVerify => blackbox::simplify_schnorr_verify(dfg, solver, arguments),
 
         BlackBoxFunc::BigIntAdd
         | BlackBoxFunc::BigIntSub
