af5efab7ef6e25778df239f3853ca82410eb5604
Steven Platt <mr.platt@gmail.com>
2024-11-12 17:36:05 -0500

    comment consistency

--- spartan/terraform/gke-cluster/outputs.tf
+++ spartan/terraform/gke-cluster/outputs.tf
@@ -1,9 +1,7 @@
-# Output the cluster endpoint
 output "cluster_endpoint" {
   value = google_container_cluster.primary.endpoint
 }
 
-# Output the service account email
 output "service_account_email" {
   value = google_service_account.gke_sa.email
 }
