d8a21ce21d34a950b660a75485e3f52a682df893
benesjan <janbenes1234@gmail.com>
2024-11-12 21:22:54 +0000

    WIP

--- yarn-project/cli-wallet/test/flows/token_utils/create_main_and_mint_private.sh
+++ yarn-project/cli-wallet/test/flows/token_utils/create_main_and_mint_private.sh
@@ -3,7 +3,7 @@ section "Deploying contracts and minting tokens privately"
 aztec-wallet create-account -a main
 aztec-wallet deploy token_contract@Token --args accounts:main Test TST 18 -f main -a token
 aztec-wallet create-secret -a shield
-aztec-wallet send mint_private -ca token --args $1 secrets:shield:hash -f main
+aztec-wallet send mint_to_private -ca token --args $1 secrets:shield:hash -f main
 aztec-wallet add-note TransparentNote pending_shields -ca token -t last -a main -b $1 secrets:shield:hash
 aztec-wallet send redeem_shield -ca token --args accounts:main $1 secrets:shield -f main
 
