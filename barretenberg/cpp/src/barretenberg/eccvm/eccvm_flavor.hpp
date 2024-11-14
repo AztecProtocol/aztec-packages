d144e50a9e3c0d8f7e502a3a053e11b4baa6db8a
lucasxia01 <lucasxia01@gmail.com>
2024-11-14 16:19:17 +0000

    split out IPA transcript in eccvm flavor

--- barretenberg/cpp/src/barretenberg/eccvm/eccvm_flavor.hpp
+++ barretenberg/cpp/src/barretenberg/eccvm/eccvm_flavor.hpp
@@ -1188,22 +1188,6 @@ class ECCVMFlavor {
                 NativeTranscript::template deserialize_from_buffer<FF>(NativeTranscript::proof_data, num_frs_read);
 
             shplonk_q2_comm = NativeTranscript::template deserialize_from_buffer<Commitment>(proof_data, num_frs_read);
-
-            // ipa_poly_degree = NativeTranscript::template
-            // deserialize_from_buffer<uint32_t>(NativeTranscript::proof_data,
-            //                                                                                num_frs_read);
-
-            // for (size_t i = 0; i < CONST_ECCVM_LOG_N; ++i) {
-            //     ipa_l_comms.emplace_back(NativeTranscript::template deserialize_from_buffer<Commitment>(
-            //         NativeTranscript::proof_data, num_frs_read));
-            //     ipa_r_comms.emplace_back(NativeTranscript::template deserialize_from_buffer<Commitment>(
-            //         NativeTranscript::proof_data, num_frs_read));
-            // }
-            // ipa_G_0_eval = NativeTranscript::template
-            // deserialize_from_buffer<Commitment>(NativeTranscript::proof_data,
-            //                                                                               num_frs_read);
-            // ipa_a_0_eval =
-            //     NativeTranscript::template deserialize_from_buffer<FF>(NativeTranscript::proof_data, num_frs_read);
         }
 
         void serialize_full_transcript()
@@ -1346,14 +1330,60 @@ class ECCVMFlavor {
 
             NativeTranscript::template serialize_to_buffer(shplonk_q2_comm, NativeTranscript::proof_data);
 
-            // NativeTranscript::template serialize_to_buffer(ipa_poly_degree, NativeTranscript::proof_data);
-            // for (size_t i = 0; i < CONST_ECCVM_LOG_N; ++i) {
-            //     NativeTranscript::template serialize_to_buffer(ipa_l_comms[i], NativeTranscript::proof_data);
-            //     NativeTranscript::template serialize_to_buffer(ipa_r_comms[i], NativeTranscript::proof_data);
-            // }
+            ASSERT(NativeTranscript::proof_data.size() == old_proof_length);
+        }
+    };
 
-            // serialize_to_buffer(ipa_G_0_eval, proof_data);
-            // serialize_to_buffer(ipa_a_0_eval, proof_data);
+    /**
+     * @brief Derived class that defines proof structure for ECCVM IPA proof, as well as supporting functions.
+     *
+     */
+    class IPATranscript : public NativeTranscript {
+      public:
+        uint32_t ipa_poly_degree;
+        std::vector<Commitment> ipa_l_comms;
+        std::vector<Commitment> ipa_r_comms;
+        Commitment ipa_G_0_eval;
+        FF ipa_a_0_eval;
+
+        IPATranscript() = default;
+
+        IPATranscript(const HonkProof& proof)
+            : NativeTranscript(proof)
+        {}
+
+        void deserialize_full_transcript()
+        {
+            // take current proof and put them into the struct
+            size_t num_frs_read = 0;
+            ipa_poly_degree = NativeTranscript::template deserialize_from_buffer<uint32_t>(NativeTranscript::proof_data,
+                                                                                           num_frs_read);
+
+            for (size_t i = 0; i < CONST_ECCVM_LOG_N; ++i) {
+                ipa_l_comms.emplace_back(NativeTranscript::template deserialize_from_buffer<Commitment>(
+                    NativeTranscript::proof_data, num_frs_read));
+                ipa_r_comms.emplace_back(NativeTranscript::template deserialize_from_buffer<Commitment>(
+                    NativeTranscript::proof_data, num_frs_read));
+            }
+            ipa_G_0_eval = NativeTranscript::template deserialize_from_buffer<Commitment>(NativeTranscript::proof_data,
+                                                                                          num_frs_read);
+            ipa_a_0_eval =
+                NativeTranscript::template deserialize_from_buffer<FF>(NativeTranscript::proof_data, num_frs_read);
+        }
+
+        void serialize_full_transcript()
+        {
+            size_t old_proof_length = NativeTranscript::proof_data.size();
+            NativeTranscript::proof_data.clear();
+
+            NativeTranscript::template serialize_to_buffer(ipa_poly_degree, NativeTranscript::proof_data);
+            for (size_t i = 0; i < CONST_ECCVM_LOG_N; ++i) {
+                NativeTranscript::template serialize_to_buffer(ipa_l_comms[i], NativeTranscript::proof_data);
+                NativeTranscript::template serialize_to_buffer(ipa_r_comms[i], NativeTranscript::proof_data);
+            }
+
+            serialize_to_buffer(ipa_G_0_eval, proof_data);
+            serialize_to_buffer(ipa_a_0_eval, proof_data);
 
             ASSERT(NativeTranscript::proof_data.size() == old_proof_length);
         }
