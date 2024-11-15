8f69116ec7967e1a49bf114a78733d41fafeae1b
jeanmon <jean@aztecprotocol.com>
2024-11-15 13:47:46 +0000

    Update some comments

--- barretenberg/cpp/src/barretenberg/vm/avm/tests/slice.test.cpp
+++ barretenberg/cpp/src/barretenberg/vm/avm/tests/slice.test.cpp
@@ -245,7 +245,7 @@ TEST_F(AvmSliceTests, indirectTwoCallsOverlap)
 
 TEST_F(AvmSliceTests, indirectFailedResolution)
 {
-    // TODO(#9131): Re-enable as part of #9131
+    // TODO(#9995): Re-enable as part of #9995
     GTEST_SKIP();
 
     gen_trace_builder({ 2, 3, 4, 5, 6 });
