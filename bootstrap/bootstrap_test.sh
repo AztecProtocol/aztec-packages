e71f21e3a935f70b3e07a19365363fcb2da36913
Dmitry <98899785+mdqst@users.noreply.github.com>
2024-11-16 21:27:11 +0300

    Typo fix Update bootstrap_test.sh
    Fix missing space in docker run volume mount argument

--- bootstrap/bootstrap_test.sh
+++ bootstrap/bootstrap_test.sh
@@ -13,7 +13,7 @@ FROM ubuntu:latest
 RUN apt update && apt install -y git rsync docker.io
 EOF
 
-docker run -ti --rm -v/run/user/$UID/docker.sock:/var/run/docker.sock -v$(git rev-parse --show-toplevel):/repo:ro bootstrap-build /bin/bash -c "
+docker run -ti --rm -v /run/user/$UID/docker.sock:/var/run/docker.sock -v$(git rev-parse --show-toplevel):/repo:ro bootstrap-build /bin/bash -c "
 # Checkout head.
 mkdir /project && cd /project
 git init
