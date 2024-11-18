cc71c305aca08537a0a13288a1e22a8f965db90a
jewelofchaos9 <jewelofchaos9@gmail.com>
2024-11-18 12:34:04 +0000

    save artifacts

--- noir/noir-repo/compiler/noirc_evaluator/ssa_test/src/main.rs
+++ noir/noir-repo/compiler/noirc_evaluator/ssa_test/src/main.rs
@@ -1,18 +1,25 @@
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 use::noirc_evaluator::acir_instruction_builder::{
     all_instructions, InstructionArtifacts
 };
 
+fn save_to_file(data: &[u8], filename: &str) -> Result<(), std::io::Error> {
+    let path = Path::new(filename);
+    let mut file = File::create(path)?;
+    file.write_all(data)?;
+    Ok(())
+}
 
 fn main() {
     let all_artifacts: Vec<InstructionArtifacts> = all_instructions();
 
     for artifacts in all_artifacts.iter() {
-        println!("============{}==============", artifacts.instruction_name);
-        println!("{:?}", artifacts.serialized_acir);
-        // println!("{}", artifacts.serialized_ssa);
-        // println!("Acir length: {:?}", artifacts.serialized_acir.len());
-        // let artifacts_serialized = serde_json::to_string(&artifacts).unwrap();
-        // println!("{}", artifacts_serialized);
+        let filename = format!("artifacts/{}{}", artifacts.instruction_name, ".acir");
+        match save_to_file(&artifacts.serialized_acir, &filename) {
+            Ok(_) => (),
+            Err(error) => println!("Error saving data: {}", error),
+        }
     }
 }
-
