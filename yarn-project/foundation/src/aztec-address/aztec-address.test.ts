868256346c5d05e30fac2be147cbf0027f827705
Nicolás Venturo <nicolas.venturo@gmail.com>
2024-11-20 20:01:03 +0000

    Add isValid and change random

---
+++ yarn-project/foundation/src/aztec-address/aztec-address.test.ts
@@ -0,0 +1,28 @@
+import { Fr } from "../fields/fields.js";
+import { AztecAddress } from "./index.js";
+
+describe('aztec-address', () => {
+    describe('isValid', () => {
+      it('returns true for a valid address', () => {
+        // The point (5, 21888242871839275195798879923479812031525119486506890092185616889232283231735) is on the
+        // Grumpkin curve.
+        const address = new AztecAddress(new Fr(5));
+        expect(address.isValid()).toEqual(true);
+      });
+
+      it('returns false for an invalid address', () => {
+        // No point on the Grumpkin curve has an x coordinate equal to 6.
+        const address = new AztecAddress(new Fr(6));
+        expect(address.isValid()).toEqual(false);
+      });
+    });
+
+    describe('random', () => {
+      it('alwways returns a valid address', () => {
+        for (let i = 0; i < 100; ++i) {
+            const address = AztecAddress.random();
+            expect(address.isValid()).toEqual(true);
+        }
+      });
+    });
+  });
--- yarn-project/foundation/src/aztec-address/index.ts
+++ yarn-project/foundation/src/aztec-address/index.ts
@@ -69,8 +69,17 @@ export class AztecAddress {
     return new AztecAddress(hexToBuffer(buf));
   }
 
+  /**
+   * @returns a random valid address (i.e. one that can be encryted to).
+   */
   static random() {
-    return new AztecAddress(Fr.random());
+    // About a third of random field elements result in invalid addresses, so we loop until we get a valid one.
+    while (true) {
+      const candidate = new AztecAddress(Fr.random());
+      if (candidate.isValid()) {
+        return candidate;
+      }
+    }
   }
 
   get size() {
@@ -83,6 +92,22 @@ export class AztecAddress {
 
   isZero() {
     return this.value.isZero();
+  }
+
+  /**
+   * @returns true if the address is valid. Invalid addresses cannot receive encrypted messages.
+   */
+  isValid() {
+    // An address is a field value (Fr), which for some purposes is assumed to be the x coordinate of a point in the
+    // Grumpkin curve (notably in order to encrypt to it). An address that is not the x coordinate of such a point is
+    // called an 'invalid' address.
+    //
+    // For Grumpkin, y^2 = x^3 − 17 . There exist values x ∈ Fr for which no y satisfies this equation. This means that
+    // given such an x and t = x^3 − 17, then sqrt(t) does not exist in Fr.
+
+    const cube = this.value.mul(this.value).mul(this.value);
+    const t = cube.sub(new Fr(17));
+    return t.sqrt() !== null;
   }
 
   toBuffer() {
