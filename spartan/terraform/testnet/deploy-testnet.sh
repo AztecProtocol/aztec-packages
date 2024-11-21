04239dfa432d8c38bf3330990ae0cb2ffafcbd01
Mitch <mitchell@aztecprotocol.com>
2024-11-20 19:56:34 -0500

    remove terraform changes

--- spartan/terraform/testnet/deploy-testnet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-TESTNET_NAME="testnet-2"
-terraform init -backend-config="key=deploy-network/${TESTNET_NAME}/terraform.tfstate"
-terraform apply -var-file="testnet.tfvars"
--- spartan/terraform/testnet/testnet.tfvars
+++
@@ -1,3 +0,0 @@
-testnet_name = "testnet-2"
-values_file = "testnet.yaml"
-aztec_docker_image = "iamjustmitch/aztec:701380ba5e48bfc286adb13a875e890754c4db32"
