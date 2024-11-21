65bcb8e131978bb87effa54e3dec4c8facb41e38
Charlie Lye <karl.lye@gmail.com>
2024-11-21 12:59:00 +0000

    wip

--- bootstrap_ci.sh
+++ bootstrap_ci.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+set -eu
+
+function on_exit {
+    set +e
+    if [ -n "$ip" ]; then
+        echo "Terminating spot instance..."
+        ssh ubuntu@$ip sudo halt -p > /dev/null 2>&1
+    fi
+}
+trap on_exit EXIT
 
 cd $(dirname $0)
 
-ip=$(./build-system/scripts/request_spot)
+ip=$(./build-system/scripts/request_spot charlie-ci3 128 x86_64)
 
 ssh ubuntu@$ip "
-  cd /root
-  git clone http://github.com/aztecprotocol/aztec-packages
-  cd aztec-packages
-  git checkout cl/ci3
-  CI=1 ./bootstrap.sh full
+  docker run --rm -t aztecprotocol/build:1.0 bash -c '
+    cd /root
+    git clone http://github.com/aztecprotocol/aztec-packages
+    cd aztec-packages
+    git checkout cl/ci3
+    CI=1 ./bootstrap.sh full
+  '
 "
--- noir/scripts/test_native.sh
+++ noir/scripts/test_native.sh
@@ -15,8 +15,8 @@ export GIT_COMMIT=${COMMIT_HASH:-$(git rev-parse --verify HEAD)}
 cargo fmt --all --check
 
 # Linting. If local use a separate build dir to not clobber incrementals. In CI we want to save space.
-[ "${CI:-0}" -eq 0 ] && clippy_args="--target-dir target/clippy"
-RUSTFLAGS=-Dwarnings cargo clippy $clippy_args --workspace --locked --release
+[ "${CI:-0}" -eq 0 ] && args="--target-dir target/clippy" || args=""
+RUSTFLAGS=-Dwarnings cargo clippy $args --workspace --locked --release
 
 # Install nextest.
 ./.github/scripts/cargo-binstall-install.sh
@@ -25,5 +25,5 @@ cargo-binstall cargo-nextest --version 0.9.67 -y --secure
 # Test.
 export RAYON_NUM_THREADS=1
 jobs=$(($(nproc) / RAYON_NUM_THREADS))
-[ "${CI:-0}" -eq 0 ] && test_args="--target-dir target/nextest"
-cargo nextest run -j$jobs $test_args --workspace --locked --release -E '!test(hello_world_example) & !test(simple_verifier_codegen)'
+[ "${CI:-0}" -eq 0 ] && args="--target-dir target/nextest" || args=""
+cargo nextest run -j$jobs $args --workspace --locked --release -E '!test(hello_world_example) & !test(simple_verifier_codegen)'
