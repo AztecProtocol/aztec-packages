f5e2436dce122f72453d7f40450c4e555b9e5313
Charlie Lye <karl.lye@gmail.com>
2024-11-23 10:48:21 +0000

    blerg

--- barretenberg/acir_tests/run_acir_tests_browser.sh
+++ barretenberg/acir_tests/run_acir_tests_browser.sh
@@ -2,11 +2,12 @@
 set -eum
 
 cleanup() {
-    BG_PIDS=$(jobs -p)
-    if [[ -n "$BG_PIDS" ]]; then
-        kill $BG_PIDS 2>/dev/null
-        wait $BG_PIDS 2>/dev/null
+    set +e
+    if [ -n "$pid" ]; then
+        kill $pid 2>/dev/null
+        wait $pid 2>/dev/null
     fi
+    exit $code
 }
 trap cleanup EXIT
 
@@ -19,5 +20,8 @@ THREAD_MODEL=${THREAD_MODEL:-mt}
 # TODO: Currently webkit doesn't seem to have shared memory so is a single threaded test regardless of THREAD_MODEL!
 echo "Testing thread model: $THREAD_MODEL"
 (cd browser-test-app && yarn serve:dest:$THREAD_MODEL) > /dev/null 2>&1 &
+pid=$!
 sleep 1
 VERBOSE=1 BIN=./headless-test/bb.js.browser ./run_acir_tests.sh $@
+code=$?
+exit $code
