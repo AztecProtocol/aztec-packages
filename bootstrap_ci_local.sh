9822a9a8dd86acc9d59e0e117c67923c85dc5de7
Charlie Lye <karl.lye@gmail.com>
2024-11-24 11:40:09 +0000

    lock

--- bootstrap_ci_local.sh
+++ bootstrap_ci_local.sh
@@ -5,11 +5,10 @@ docker run --name aztec_build -ti --rm \
   -v $PWD:/aztec-packages-host:ro \
   -v /var/run/docker.sock:/var/run/docker.sock \
   aztecprotocol/build:1.0 bash -c '
-  set -e
   git config --global --add safe.directory /aztec-packages-host/.git
   cd /root
   # Ensure we get a clean clone of the repo.
   git clone --depth 1 --branch cl/ci3 file:///aztec-packages-host aztec-packages
   cd aztec-packages
-  CI=1 ./bootstrap.sh fast
+  CI=1 ./bootstrap.sh fast || exec /bin/bash
 '
