f2d688deb6962ea87f88882e99e59ff02612398e
ludamad <domuradical@gmail.com>
2024-11-25 19:58:21 +0000

    proper git check

--- bootstrap_ci.sh
+++ bootstrap_ci.sh
@@ -5,12 +5,12 @@ NO_TERMINATE=${NO_TERMINATE:-0}
 
 function on_exit {
     set +e
-    if [ -n "$ip" ] && [ "$NO_TERMINATE" -eq 0 ]; then
+    if [ -n "${ip:-}" ] && [ "$NO_TERMINATE" -eq 0 ]; then
         echo "Terminating spot instance..."
         ssh ubuntu@$ip sudo halt -p > /dev/null 2>&1
         aws ec2 cancel-spot-instance-requests --spot-instance-request-ids $sir >/dev/null 2>&1 || true
     fi
-    if [ -n "$ip" ] && [ "$NO_TERMINATE" -ne 0 ]; then
+    if [ -n "${ip:-}" ] && [ "$NO_TERMINATE" -ne 0 ]; then
       echo "Remote machine not terminated, connect with: ssh -t ubuntu@$ip 'docker start aztec_build >/dev/null 2>&1 || true && docker exec -it aztec_build bash'"
     fi
 }
@@ -18,20 +18,20 @@ trap on_exit EXIT
 
 cd $(dirname $0)
 
+current_commit=$(git rev-parse HEAD)
+
+# Verify that the commit exists on the remote. It will be the remote tip of itself if so.
+if [ $(git fetch origin --negotiate-only --negotiation-tip=$current_commit) != $current_commit ] ; then
+  echo "Commit $current_commit is not pushed. The build instance needs this; exiting."
+  exit 1
+fi
+
 ip_sir=$(./build-system/scripts/request_spot ci3-$USER 64 x86_64)
 parts=(${ip_sir//:/ })
 ip="${parts[0]}"
 sir="${parts[1]}"
 
 [ "$NO_TERMINATE" -eq 0 ] && args="--rm" || args=""
-
-current_commit=$(git rev-parse HEAD)
-
-# Verify that the commit exists on the remote. It will be the remote tip of itself if so.
-if [ $(git fetch origin --negotiate-only --negotiation-tip=$current_commit) != $current_commit ] ; then
-  echo "Commit $current_commit does not exist."
-  exit 1
-fi
 
 # - Use ~/.ssh/build_instance_key to ssh into our requested instance (note, could be on-demand if spot fails)
 # - Pass our AWS cred through both ssh and docker
