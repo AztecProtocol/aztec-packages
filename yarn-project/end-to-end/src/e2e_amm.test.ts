c80f926c6b94aef8d24cec9a7c5cdf3c704dac53
Nicolás Venturo <nicolas.venturo@gmail.com>
2024-11-25 15:41:58 +0000

    Small readability edits

--- yarn-project/end-to-end/src/e2e_amm.test.ts
+++ yarn-project/end-to-end/src/e2e_amm.test.ts
@@ -2,14 +2,13 @@ import { type AccountWallet, type DebugLogger, Fr } from '@aztec/aztec.js';
 import { AMMContract, type TokenContract } from '@aztec/noir-contracts.js';
 
 import { jest } from '@jest/globals';
+import { describe } from 'node:test';
 
 import { deployToken, mintTokensToPrivate } from './fixtures/token_utils.js';
 import { setup } from './fixtures/utils.js';
 
 const TIMEOUT = 120_000;
 
-// This is a very simple test checking only the happy path. More complete tests might be done in an unimaginably far
-// future (and hence irrelevant) once I return from Patagonia.
 describe('AMM', () => {
   jest.setTimeout(TIMEOUT);
 
@@ -50,106 +49,121 @@ describe('AMM', () => {
     await mintTokensToPrivate(token0, adminWallet, swapper.getAddress(), swapperBalance0);
   });
 
-  afterAll(() => teardown());
+  afterAll(async () => await teardown());
 
-  it('full flow', async () => {
-    // ADDING LIQUIDITY
-    const amount0Desired = lpBalance0 / 2n;
-    const amount1Desired = lpBalance1 / 2n;
-    const amount0Min = lpBalance0 / 3n;
-    const amount1Min = lpBalance1 / 3n;
+  describe('full flow', () => {
+    // This is an integration test in which we perform an entire run of the happy path. Thorough unit testing is not
+    // included.
+    it.only('add liquidity', async () => {
+      const amount0Max = lpBalance0 / 2n;
+      const amount1Max = lpBalance1 / 2n;
+      const amount0Min = lpBalance0 / 3n;
+      const amount1Min = lpBalance1 / 3n;
 
-    // First we need to add authwits such that the AMM can transfer the tokens from the liquidity provider (LP).
-    // The only purpose of this nonce is to make the authwit unique (function args are part of authwit hash preimage)
-    const nonceForAuthwits = Fr.random();
+      // First we need to add authwits such that the AMM can transfer the tokens from the liquidity provider. These
+      // authwits are for the full amount, since the AMM will first transfer that to itself, and later refund any excess
+      // during public execution. Note that we need to tell the AMM of the nonce we used in the authwit.
+      const nonceForAuthwits = Fr.random();
 
-    await liquidityProvider.createAuthWit({
-      caller: amm.address,
-      action: token0.methods.transfer_to_public(
-        liquidityProvider.getAddress(),
-        amm.address,
-        amount0Desired,
-        nonceForAuthwits,
-      ),
-    });
-    await liquidityProvider.createAuthWit({
-      caller: amm.address,
-      action: token1.methods.transfer_to_public(
-        liquidityProvider.getAddress(),
-        amm.address,
-        amount1Desired,
-        nonceForAuthwits,
-      ),
-    });
+      await liquidityProvider.createAuthWit({
+        caller: amm.address,
+        action: token0.methods.transfer_to_public(
+          liquidityProvider.getAddress(),
+          amm.address,
+          amount0Max,
+          nonceForAuthwits,
+        ),
+      });
+      await liquidityProvider.createAuthWit({
+        caller: amm.address,
+        action: token1.methods.transfer_to_public(
+          liquidityProvider.getAddress(),
+          amm.address,
+          amount1Max,
+          nonceForAuthwits,
+        ),
+      });
 
-    await amm
-      .withWallet(liquidityProvider)
-      .methods.add_liquidity(amount0Desired, amount1Desired, amount0Min, amount1Min, nonceForAuthwits)
-      .send()
-      .wait();
+      await amm
+        .withWallet(liquidityProvider)
+        .methods.add_liquidity(amount0Max, amount1Max, amount0Min, amount1Min, nonceForAuthwits)
+        .send()
+        .wait();
 
-    // Since the LP was the first one to enter the pool, the desired amounts of tokens should have been deposited.
-    expect(await token0.methods.balance_of_private(liquidityProvider.getAddress()).simulate()).toEqual(
-      lpBalance0 - amount0Desired,
-    );
-    expect(await token1.methods.balance_of_private(liquidityProvider.getAddress()).simulate()).toEqual(
-      lpBalance1 - amount1Desired,
-    );
+      // Since the LP was the first one to enter the pool, the maximum amounts of tokens should have been deposited.
+      expect(await token0.methods.balance_of_private(amm.address).simulate()).toEqual(
+        amount0Max,
+      );
+      expect(await token0.methods.balance_of_private(liquidityProvider.getAddress()).simulate()).toEqual(
+        lpBalance0 - amount0Max,
+      );
+      expect(await token1.methods.balance_of_private(amm.address).simulate()).toEqual(
+        amount1Max,
+      );
+      expect(await token1.methods.balance_of_private(liquidityProvider.getAddress()).simulate()).toEqual(
+        lpBalance1 - amount1Max,
+      );
 
-    // The LP should now have liquidity token
-    expect(await liquidityToken.methods.balance_of_private(liquidityProvider.getAddress()).simulate()).toBeGreaterThan(
-      0n,
-    );
-
-    // SWAPPING EXACT TOKENS FOR TOKENS
-    // We try swapping half of swapper's token 0 balance for token 1
-    const amountIn = swapperBalance0 / 2n;
-
-    // We need to add authwit such that the AMM can transfer the tokens from the swapper
-    await swapper.createAuthWit({
-      caller: amm.address,
-      action: token0.methods.transfer_to_public(swapper.getAddress(), amm.address, amountIn, nonceForAuthwits),
-    });
-
-    // We don't care about the minimum amount of token 1 we get in this test as long as it's non-zero.
-    const amountOutMin = 1n;
-    await amm.methods
-      .swap_exact_tokens_for_tokens(token0.address, token1.address, amountIn, amountOutMin, nonceForAuthwits)
-      .send()
-      .wait();
-
-    // All the amountIn should have been swapped so LP balance0 should be decreased by that amount
-    const lpBalance0AfterSwap1 = await token0.methods.balance_of_private(liquidityProvider.getAddress()).simulate();
-    expect(lpBalance0AfterSwap1).toEqual(lpBalance0 - amountIn);
-
-    // At this point a user should have a non-zero balance of token 1
-    const lpBalance1AfterSwap1 = await token1.methods.balance_of_private(swapper.getAddress()).simulate();
-    expect(lpBalance1AfterSwap1).toBeGreaterThan(0n);
-
-    // SWAPPING TOKENS FOR EXACT TOKENS
-    const amount0Out = 1000n;
-    // We allow the AMM to take all our token1 balance (the difference will be refunded).
-    const amount1InMax = lpBalance1AfterSwap1;
-
-    // We need to add authwit such that the AMM can transfer the tokens from the swapper
-    await swapper.createAuthWit({
-      caller: amm.address,
-      action: token1.methods.transfer_to_public(swapper.getAddress(), amm.address, amount1InMax, nonceForAuthwits),
+      // Liquidity tokens should also be minted for the liquidity provider, as well as locked at the zero address.
+      expect(
+        await liquidityToken.methods.balance_of_private(liquidityProvider.getAddress()).simulate(),
+      ).toEqual(1000n);
+      expect(
+        await liquidityToken.methods.total_supply().simulate(),
+      ).toEqual(100000n);
     });
 
-    await amm.methods
-      .swap_tokens_for_exact_tokens(token1.address, token0.address, amount0Out, amount1InMax, nonceForAuthwits)
-      .send()
-      .wait();
+    it('the rest', async () => {
+      // SWAPPING EXACT TOKENS FOR TOKENS
+      // We try swapping half of swapper's token 0 balance for token 1
+      const amountIn = swapperBalance0 / 2n;
 
-    // We should have received the exact amount of token0
-    expect(await token0.methods.balance_of_private(swapper.getAddress()).simulate()).toEqual(
-      lpBalance0AfterSwap1 + amount0Out,
-    );
+      // We need to add authwit such that the AMM can transfer the tokens from the swapper
+      await swapper.createAuthWit({
+        caller: amm.address,
+        action: token0.methods.transfer_to_public(swapper.getAddress(), amm.address, amountIn, nonceForAuthwits),
+      });
 
-    // We should have received a refund of token 1 (meaning we should have more than "previous balance - amount1InMax")
-    expect(await token1.methods.balance_of_private(swapper.getAddress()).simulate()).toBeGreaterThan(
-      lpBalance1AfterSwap1 - amount1InMax,
-    );
+      // We don't care about the minimum amount of token 1 we get in this test as long as it's non-zero.
+      const amountOutMin = 1n;
+      await amm.methods
+        .swap_exact_tokens_for_tokens(token0.address, token1.address, amountIn, amountOutMin, nonceForAuthwits)
+        .send()
+        .wait();
+
+      // All the amountIn should have been swapped so LP balance0 should be decreased by that amount
+      const lpBalance0AfterSwap1 = await token0.methods.balance_of_private(liquidityProvider.getAddress()).simulate();
+      expect(lpBalance0AfterSwap1).toEqual(lpBalance0 - amountIn);
+
+      // At this point a user should have a non-zero balance of token 1
+      const lpBalance1AfterSwap1 = await token1.methods.balance_of_private(swapper.getAddress()).simulate();
+      expect(lpBalance1AfterSwap1).toBeGreaterThan(0n);
+
+      // SWAPPING TOKENS FOR EXACT TOKENS
+      const amount0Out = 1000n;
+      // We allow the AMM to take all our token1 balance (the difference will be refunded).
+      const amount1InMax = lpBalance1AfterSwap1;
+
+      // We need to add authwit such that the AMM can transfer the tokens from the swapper
+      await swapper.createAuthWit({
+        caller: amm.address,
+        action: token1.methods.transfer_to_public(swapper.getAddress(), amm.address, amount1InMax, nonceForAuthwits),
+      });
+
+      await amm.methods
+        .swap_tokens_for_exact_tokens(token1.address, token0.address, amount0Out, amount1InMax, nonceForAuthwits)
+        .send()
+        .wait();
+
+      // We should have received the exact amount of token0
+      expect(await token0.methods.balance_of_private(swapper.getAddress()).simulate()).toEqual(
+        lpBalance0AfterSwap1 + amount0Out,
+      );
+
+      // We should have received a refund of token 1 (meaning we should have more than "previous balance - amount1InMax")
+      expect(await token1.methods.balance_of_private(swapper.getAddress()).simulate()).toBeGreaterThan(
+        lpBalance1AfterSwap1 - amount1InMax,
+      );
+    });
   });
 });
