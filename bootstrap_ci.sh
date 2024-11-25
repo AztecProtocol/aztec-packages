650d28ad02912e5a3c61396b0dd33e97241c3c2e
ludamad <domuradical@gmail.com>
2024-11-25 19:00:38 +0000

    start ci

--- bootstrap_ci.sh
+++ bootstrap_ci.sh
@@ -25,14 +25,28 @@ sir="${parts[1]}"
 
 [ "$NO_TERMINATE" -eq 0 ] && args="--rm" || args=""
 
-ssh ubuntu@$ip "
-  docker run $args --name aztec_build -t -v /var/run/docker.sock:/var/run/docker.sock aztecprotocol/build:1.0 bash -c '
+# Get current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+function check_git() {
+  # Fetch remote branch
+  git fetch origin "$current_branch"
+
+  # Compare local and remote commits
+  local_commit=$(git rev-parse "$current_branch")
+  remote_commit=$(git rev-parse "origin/$current_branch")
+
+  test "$local_commit" = "$remote_commit"
+}
+check_git || (echo "Local branch $current_branch does not match remote branch origin/$current_branch. Not starting build instance." && exit 1)
+
+ssh -o SendEnv=AWS_ACCESS_KEY_ID -o SendEnv=AWS_SECRET_ACCESS_KEY ubuntu@$ip "
+  docker run $args -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY --name aztec_build -t -v /var/run/docker.sock:/var/run/docker.sock aztecprotocol/build:1.0 bash -c '
     set -e
     # When restarting the container, just hang around.
     while [ -f started ]; do sleep 999; done
     touch started
     cd /root
-    git clone --depth 1 --branch cl/ci3 http://github.com/aztecprotocol/aztec-packages
+    git clone --depth 1 --branch $current_branch http://github.com/aztecprotocol/aztec-packages
     cd aztec-packages
     CI=1 ./bootstrap.sh fast
   '
