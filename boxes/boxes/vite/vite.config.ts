36599c29800a6d4482ec29eda66e20dbecb62dc9
thunkar <gregojquiros@gmail.com>
2024-11-25 08:56:39 +0000

    working vite box

--- boxes/boxes/vite/vite.config.ts
+++ boxes/boxes/vite/vite.config.ts
@@ -1,13 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { PolyfillOptions, nodePolyfills } from "vite-plugin-node-polyfills";
 import topLevelAwait from "vite-plugin-top-level-await";
+
+// Unfortunate, but needed due to https://github.com/davidmyersdev/vite-plugin-node-polyfills/issues/81
+// Suspected to be because of the yarn workspace setup, but not sure
+const nodePolyfillsFix = (options?: PolyfillOptions | undefined): Plugin => {
+  return {
+    ...nodePolyfills(options),
+    /* @ts-ignore */
+    resolveId(source: string) {
+      const m =
+        /^vite-plugin-node-polyfills\/shims\/(buffer|global|process)$/.exec(
+          source,
+        );
+      if (m) {
+        return `../../node_modules/vite-plugin-node-polyfills/shims/${m[1]}/dist/index.cjs`;
+      }
+    },
+  };
+};
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills({
+    nodePolyfillsFix({
       overrides: {
         fs: "memfs",
       },
