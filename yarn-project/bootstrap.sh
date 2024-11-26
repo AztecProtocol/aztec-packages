7c68f174e06b8dbad453050cb4f7930d1d80e2a6
Charlie Lye <karl.lye@gmail.com>
2024-11-26 00:47:05 +0000

    more e2e tests. env var support

--- yarn-project/bootstrap.sh
+++ yarn-project/bootstrap.sh
@@ -43,15 +43,28 @@ function run_e2e_tests {
   # TODO: We want to avoid this time burden. Slim the image? Preload it in from host?
   docker pull aztecprotocol/build:1.0
 
-  # List every test individually. Do not put folders.
+  # List every test individually. Do not put folders. Ensures fair balancing of load and simplifies resource managment.
   # If a test flakes out, mark it as flake in your PR so it no longer runs, and post a message in slack about it.
   # If you can, try to find whoever is responsible for the test, and have them acknowledge they'll resolve it later.
   # DO NOT just re-run your PR and leave flakey tests running to impact on other engineers.
-  # If you've been tasked with resolving a flakey test, use e.g. 'while my_test_cmd; do sleep 0; done' to grind it.
-  PR_TESTS=(
+  # If you've been tasked with resolving a flakey test, grind on it using e.g.:
+  #    while ./scripts/test.sh simple e2e_2_pxes; do sleep 0; done
+  TESTS=(
     "simple e2e_2_pxes"
+    "simple e2e_account_contracts"
     "simple e2e_authwit"
     "simple e2e_avm_simulator"
+    "simple e2e_blacklist_token_contract/access_control"
+    "simple e2e_blacklist_token_contract/burn"
+    "simple e2e_blacklist_token_contract/minting"
+    "simple e2e_blacklist_token_contract/shielding"
+    "simple e2e_blacklist_token_contract/transfer_private"
+    "simple e2e_blacklist_token_contract/transfer_public"
+    "simple e2e_blacklist_token_contract/unshielding"
+    "flake e2e_block_building"
+    "simple e2e_bot"
+    "simple e2e_card_game"
+    "simple e2e_cheat_codes"
     "simple e2e_cross_chain_messaging/l1_to_l2"
     "simple e2e_cross_chain_messaging/l2_to_l1"
     "simple e2e_cross_chain_messaging/token_bridge_failure_cases"
@@ -62,25 +75,57 @@ function run_e2e_tests {
     "simple e2e_deploy_contract/deploy_method"
     "simple e2e_deploy_contract/legacy"
     "simple e2e_deploy_contract/private_initialization"
+    "simple e2e_escrow_contract"
+    "simple e2e_event_logs"
+    "simple e2e_fees/account_init"
     "simple e2e_fees/failures"
+    "simple e2e_fees/fee_juice_payments"
     "simple e2e_fees/gas_estimation"
     "simple e2e_fees/private_payments"
+    "simple e2e_keys"
+    "simple e2e_l1_with_wall_time"
     "simple e2e_lending_contract"
     "simple e2e_max_block_number"
+    "simple e2e_multiple_accounts_1_enc_key"
     "simple e2e_nested_contract/importer"
     "simple e2e_nested_contract/manual_private_call"
     "simple e2e_nested_contract/manual_private_enqueue"
     "simple e2e_nested_contract/manual_public"
+    "simple e2e_nft"
+    "simple e2e_non_contract_account"
+    "simple e2e_note_getter"
     "simple e2e_ordering"
-    "simple e2e_prover_coordination"
-    "simple e2e_static_calls"
-    "flake e2e_block_building"
-
+    "simple e2e_outbox"
     "simple e2e_p2p/gossip_network"
     "simple e2e_p2p/rediscovery"
     "simple e2e_p2p/reqresp"
     "flake e2e_p2p/upgrade_governance_proposer"
+    "simple e2e_private_voting_contract"
+    "simple e2e_prover/full FAKE_PROOFS=1"
+    "simple e2e_prover_coordination"
+    "simple e2e_public_testnet_transfer"
+    "simple e2e_state_vars"
+    "simple e2e_static_calls"
+    "simple e2e_synching"
+    "simple e2e_token_contract/access_control"
+    "simple e2e_token_contract/burn"
+    "simple e2e_token_contract/minting"
+    "simple e2e_token_contract/private_transfer_recursion"
+    "simple e2e_token_contract/reading_constants"
+    "simple e2e_token_contract/transfer_in_private"
+    "simple e2e_token_contract/transfer_in_public"
+    "simple e2e_token_contract/transfer_to_private"
+    "simple e2e_token_contract/transfer_to_public"
+    "simple e2e_token_contract/transfer.test"
+    "flake flakey_e2e_inclusion_proofs_contract"
 
+    "compose composed/docs_examples"
+    "compose composed/e2e_aztec_js_browser"
+    "compose composed/e2e_sandbox_example"
+    "compose composed/integration_l1_publisher"
+    "compose composed/pxe"
+    "compose sample-dapp/index"
+    "compose sample-dapp/ci/index"
     "compose guides/dapp_testing"
     "compose guides/up_quick_start"
     "compose guides/writing_an_account_contract"
@@ -88,20 +133,23 @@ function run_e2e_tests {
 
   commands=()
   tests=()
-  for pair in "${PR_TESTS[@]}"; do
-      read -r cmd test <<< "$pair"
-      commands+=("$cmd")
-      tests+=("$test")
+  for entry in "${TESTS[@]}"; do
+    cmd=$(echo "$entry" | awk '{print $1}')
+    test=$(echo "$entry" | awk '{print $2}')
+    env=$(echo "$entry" | cut -d' ' -f3-)
+    commands+=("$cmd")
+    tests+=("$test")
+    env_vars+=("$env")
   done
 
   rm -rf results
   set +e
-  parallel --timeout 15m --verbose --joblog joblog.txt --results results/{2}/ --halt now,fail=1 \
-      ./scripts/test.sh {1} {2} ::: ${commands[@]} :::+ ${tests[@]}
+  parallel --timeout 15m --verbose --joblog joblog.txt --results results/{2}-{#}/ --halt now,fail=1 \
+      {3} ./scripts/test.sh {1} {2} ::: ${commands[@]} :::+ ${tests[@]} :::+ "${env_vars[@]}"
   code=$?
   set -e
 
-  awk 'NR > 1 && $7 != 0 {print $NF}' joblog.txt | while read -r job; do
+  awk 'NR > 1 && $7 != 0 {print $NF "-" $1}' joblog.txt | while read -r job; do
     stdout_file="results/${job}/stdout"
     stderr_file="results/${job}/stderr"
     if [ -f "$stdout_file" ]; then
