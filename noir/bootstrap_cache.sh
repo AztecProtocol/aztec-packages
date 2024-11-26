fa751b2f10cfdcd616e439f9c1ece4292c3cc932
ludamad <domuradical@gmail.com>
2024-11-26 17:55:20 +0000

    .

--- noir/bootstrap_cache.sh
+++ noir/bootstrap_cache.sh
@@ -2,13 +2,12 @@
 set -eu
 
 cd "$(dirname "$0")"
-
-export PATH=$PWD/../build-system/s3-cache-scripts:$PATH
+ci3="$(git rev-parse --show-toplevel)/ci3"
 
 echo -e "\033[1mRetrieving noir packages from remote cache...\033[0m"
 export AZTEC_CACHE_REBUILD_PATTERNS=.rebuild_patterns_native
-cache-download.sh noir-nargo-$(compute-content-hash.sh).tar.gz
+$ci3/cache/download noir-nargo-$($ci3/cache/content_hash).tar.gz
 
 echo -e "\033[1mRetrieving nargo from remote cache...\033[0m"
 export AZTEC_CACHE_REBUILD_PATTERNS="../barretenberg/cpp/.rebuild_patterns ../barretenberg/ts/.rebuild_patterns .rebuild_patterns_packages"
-cache-download.sh noir-packages-$(compute-content-hash.sh).tar.gz
+$ci3/cache/download noir-packages-$($ci3/cache/content_hash).tar.gz
