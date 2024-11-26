332894f3b819da4f04ab5100165e36f28b57aabf
PhilWindle <philip.windle@gmail.com>
2024-11-26 13:51:31 +0000

    Review fixes

--- yarn-project/archiver/src/archiver/kv_archiver_store/kv_archiver_store.ts
+++ yarn-project/archiver/src/archiver/kv_archiver_store/kv_archiver_store.ts
@@ -64,6 +64,9 @@ export class KVArchiverDataStore implements ArchiverDataStore {
     return Promise.resolve(this.#contractArtifactStore.getContractArtifact(address));
   }
 
+  // TODO:  These function names are in memory only as they are for development/debugging. They require the full contract
+  //        artifact supplied to the node out of band. This should be reviewed and potentially removed as part of
+  //        the node api cleanup process.
   getContractFunctionName(address: AztecAddress, selector: FunctionSelector): Promise<string | undefined> {
     return Promise.resolve(this.functionNames.get(selector.toString()));
   }
