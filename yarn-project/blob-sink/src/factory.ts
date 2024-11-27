c3bb39643ca8c4c53e5ccf8e2d0aea3706c54e61
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-11-27 08:56:20 +0000

    fix: make simple run command

--- yarn-project/blob-sink/src/factory.ts
+++ yarn-project/blob-sink/src/factory.ts
@@ -7,10 +7,10 @@ import { BlobSinkServer } from './server.js';
 // If data store settings are provided, the store is created and returned.
 // Otherwise, undefined is returned and an in memory store will be used.
 async function getDataStoreConfig(config?: BlobSinkConfig): Promise<AztecKVStore | undefined> {
-  if (!config?.dataStoreConfig) {
+  if (!config?.dataDirectory) {
     return undefined;
   }
-  return await createStore('blob-sink', config.dataStoreConfig);
+  return await createStore('blob-sink', config);
 }
 
 // TOOD: telemetry client config too
