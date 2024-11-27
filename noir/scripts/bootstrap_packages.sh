3397fd714fcab78706cc43bc8fb5166fd9da9876
ludamad <domuradical@gmail.com>
2024-11-27 03:35:44 +0000

    install

--- noir/scripts/bootstrap_packages.sh
+++ noir/scripts/bootstrap_packages.sh
@@ -22,7 +22,7 @@ PROJECTS=(
 )
 INCLUDE=$(printf " --include %s" "${PROJECTS[@]}")
 
-$(git rev-parse --show-toplevel)/yarn/install
+$(git rev-parse --show-toplevel)/ci3/yarn/install
 
 yarn workspaces foreach --parallel --topological-dev --verbose $INCLUDE run build
 
