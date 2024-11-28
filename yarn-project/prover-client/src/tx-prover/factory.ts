6fd5fc18bd973b539fb9edfb372181fbe4617f75
Alex Gherghisan <alexghr@users.noreply.github.com>
2024-11-28 13:19:41 +0000

    feat: new proving broker (#10174)
    This PR integrates the new proving broker implementation added in #8495
    into the orchestrator and adds a new caching layer between the
    orchestrator and the job broker able to resolve jobs that have already
    been finished (in order to provide crash recovery for the prover-node).
    
    ---------
    
    Co-authored-by: Santiago Palladino <santiago@aztecprotocol.com>

--- yarn-project/prover-client/src/tx-prover/factory.ts
+++ yarn-project/prover-client/src/tx-prover/factory.ts
@@ -1,9 +1,14 @@
+import { type ProvingJobBroker } from '@aztec/circuit-types';
 import { type TelemetryClient } from '@aztec/telemetry-client';
 import { NoopTelemetryClient } from '@aztec/telemetry-client/noop';
 
 import { type ProverClientConfig } from '../config.js';
 import { TxProver } from './tx-prover.js';
 
-export function createProverClient(config: ProverClientConfig, telemetry: TelemetryClient = new NoopTelemetryClient()) {
-  return TxProver.new(config, telemetry);
+export function createProverClient(
+  config: ProverClientConfig,
+  broker: ProvingJobBroker,
+  telemetry: TelemetryClient = new NoopTelemetryClient(),
+) {
+  return TxProver.new(config, broker, telemetry);
 }
