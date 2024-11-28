3c0ab2c431d7f41369f6c78a316771dd989e9550
ludamad <domuradical@gmail.com>
2024-11-28 01:26:31 +0000

    noir projects scripts

---
+++ noir-projects/noir-contracts/bootstrap_circuits.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eu
+[ -n "${BUILD_SYSTEM_DEBUG:-}" ] && set -x # conditionally trace
+
+cd "$(dirname "$0")"
+
+CMD=${1:-}
+
+if [ -n "$CMD" ]; then
+  if [ "$CMD" = "clean" ]; then
+    git clean -fdx
+    exit 0
+  else
+    echo "Unknown command: $CMD"
+    exit 1
+  fi
+fi
+
+echo "Compiling contracts..."
+NARGO=${NARGO:-../../noir/noir-repo/target/release/nargo}
+$NARGO compile --silence-warnings --inliner-aggressiveness 0
+
+echo "Transpiling contracts..."
+scripts/transpile.sh
---
+++ noir-projects/noir-protocol-circuits/bootstrap_circuits.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eu
+[ -n "${BUILD_SYSTEM_DEBUG:-}" ] && set -x # conditionally trace
+# Use ci3 script base.
+cd $(dirname $0)
+
+CMD=${1:-}
+
+if [ -n "$CMD" ]; then
+  if [ "$CMD" = "clean" ]; then
+    git clean -fdx
+    exit 0
+  else
+    echo "Unknown command: $CMD"
+    exit 1
+  fi
+fi
+
+yarn
+node ./scripts/generate_variants.js
+
+export RAYON_NUM_THREADS=16
+export HARDWARE_CONCURRENCY=16
+
+echo "Compiling noir-protocol-circuits with $RAYON_NUM_THREADS threads..."
+NARGO=${NARGO:-../../noir/noir-repo/target/release/nargo}
+$NARGO compile --silence-warnings
