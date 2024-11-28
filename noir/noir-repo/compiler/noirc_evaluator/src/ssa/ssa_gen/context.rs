b561e6296b4afedafca09ec08db24db200cfb3c7
TomAFrench <tom@tomfren.ch>
2024-11-28 22:10:32 +0000

    .

--- noir/noir-repo/compiler/noirc_evaluator/src/ssa/ssa_gen/context.rs
+++ noir/noir-repo/compiler/noirc_evaluator/src/ssa/ssa_gen/context.rs
@@ -735,7 +735,7 @@ impl<'a> FunctionContext<'a> {
             // Reference counting in brillig relies on us incrementing reference
             // counts when arrays/slices are constructed or indexed.
             // Thus, if we dereference an lvalue which happens to be array/slice we should increment its reference counter.
-            self.builder.increment_array_reference_count(value_to_store);
+            self.builder.increment_array_reference_count(reference);
             self.builder.insert_load(reference, element_type).into()
         })
     }
