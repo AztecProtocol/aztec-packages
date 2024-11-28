366861a82f31e956a0c6a7eba548dc3bc60b720f
ludamad <domuradical@gmail.com>
2024-11-28 01:49:25 +0000

    chmod

--- noir-projects/bootstrap_cache_circuits.sh
+++ noir-projects/bootstrap_cache_circuits.sh
@@ -2,7 +2,7 @@
 # Use ci3 script base.
 source $(git rev-parse --show-toplevel)/ci3/base/source
 
-echo -e "\033[1mRetrieving noir projects vks from remote cache...\033[0m"
+echo -e "\033[1mRetrieving noir projects circuits from remote cache...\033[0m"
 
 export AZTEC_CACHE_REBUILD_PATTERNS={../noir,noir-protocol-circuits,mock-protocol-circuits,noir-contracts}/.rebuild_patterns
 $ci3/cache/download noir-projects-circuits-$($ci3/cache/content_hash).tar.gz
