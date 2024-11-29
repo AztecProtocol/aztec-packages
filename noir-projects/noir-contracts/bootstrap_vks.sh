57b5a1bcd72aebaad7803c7cfdba9b57f5e75d10
Charlie Lye <karl.lye@gmail.com>
2024-11-29 00:46:17 +0000

    hack around nargo race condition on dep dload

--- noir-projects/noir-contracts/bootstrap_vks.sh
+++ noir-projects/noir-contracts/bootstrap_vks.sh
@@ -9,6 +9,4 @@ echo Using BB hash $BB_HASH
 tempDir="./target/tmp"
 mkdir -p $tempDir
 
-for artifactPath in "./target"/*.json; do
-    BB_HASH=$BB_HASH node ./scripts/postprocess_contract.js "$artifactPath" "$tempDir"
-done
+parallel -v --line-buffer --tag --halt now,fail=1 BB_HASH=$BB_HASH node ./scripts/postprocess_contract.js {} $tempDir ::: ./target/*.json
