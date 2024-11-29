9d449a81bce13baa976fb69adbd2c5314108e104
ludamad <domuradical@gmail.com>
2024-11-29 14:30:17 +0000

    hack

--- noir-projects/noir-contracts/bootstrap_vks.sh
+++ noir-projects/noir-contracts/bootstrap_vks.sh
@@ -6,7 +6,12 @@ cd "$(dirname "$0")"
 echo "Postprocessing contracts..."
 BB_HASH=${BB_HASH:-$(cd ../../ && git ls-tree -r HEAD | grep 'barretenberg/cpp' | awk '{print $3}' | git hash-object --stdin)}
 echo Using BB hash $BB_HASH
-tempDir="./target/tmp"
-mkdir -p $tempDir
 
-parallel -v --line-buffer --tag --halt now,fail=1 BB_HASH=$BB_HASH node ./scripts/postprocess_contract.js {} $tempDir ::: ./target/*.json
+function process() {
+  tempDir=$(mktemp -d)
+  node ./scripts/postprocess_contract.js $1 $tempDir
+}
+export BB_HASH
+export -f process
+
+parallel -v --line-buffer --tag --halt now,fail=1 process {} ::: ./target/*.json
