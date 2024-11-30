0e599fb17e963b8e6f6aaafa900301cdc8340c92
ludamad <domuradical@gmail.com>
2024-11-30 01:26:50 +0000

    fix

--- yarn-project/bootstrap.sh
+++ yarn-project/bootstrap.sh
@@ -47,7 +47,7 @@ function build {
   fi
   $ci3/github/endgroup
 
-  if $ci3/cache/should_run "$HASH"; then
+  if $ci3/cache/should_run yarn-project-tests"$HASH"; then
     yarn test
     export ci3 YELLOW BLUE GREEN BOLD RESET CMD
     export -f run_e2e_tests
