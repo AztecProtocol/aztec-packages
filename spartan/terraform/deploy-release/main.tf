3efe99278d3f5fdc0cf7ece2a5b5ee3b0a9b6005
Alex Gherghisan <alexg@aztecprotocol.com>
2024-12-06 15:21:55 +0000

    fix: bump helm

--- spartan/terraform/deploy-release/main.tf
+++ spartan/terraform/deploy-release/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.12.1"
+      version = "~> 2.16.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
