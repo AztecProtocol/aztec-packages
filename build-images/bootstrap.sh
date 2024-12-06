234d765a44d7ee24e58950fad3c90e3f07f7e5b3
ludamad <domuradical@gmail.com>
2024-12-06 22:40:45 +0000

    fix CI flag

--- build-images/bootstrap.sh
+++ build-images/bootstrap.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # note: sets CI and USE_CACHE for us
-source $(git rev-parse --show-toplevel)/ci3/source_bootstrap
+source $(git rev-parse --show-toplevel)/ci3/source
 
 cmd=${1:-}
 hash=$(REBUILD_PATTERNS="^build-images/Earthfile" cache_content_hash)
 
 function build {
   github_group "build-images build"
+  export TEST=1 # for test_should_run
   if test_should_run build-images-$hash; then
     args=""
     if [ "${CI:-0}" = 1 ]; then
@@ -18,17 +19,4 @@ function build {
   github_endgroup
 }
 
-case "$cmd" in
-  ""|"fast"|"full")
-    build
-    ;;
-  "ci")
-    build
-    ;;
-  "hash")
-    echo $hash
-    ;;
-  *)
-    echo "Unknown command: $cmd"
-    exit 1
-esac
+build
