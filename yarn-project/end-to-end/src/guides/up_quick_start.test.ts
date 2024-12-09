a1d2ac575b4b11566bedaaadec10be275bad5e7c
thunkar <gregojquiros@gmail.com>
2024-12-09 07:29:19 +0000

    added waitForNode

--- yarn-project/end-to-end/src/guides/up_quick_start.test.ts
+++ yarn-project/end-to-end/src/guides/up_quick_start.test.ts
@@ -1,17 +1,20 @@
-import { createPXEClient, waitForPXE } from '@aztec/aztec.js';
+import { createAztecNodeClient, waitForNode } from '@aztec/aztec.js';
 
 import { execSync } from 'child_process';
 
-const { PXE_URL = '' } = process.env;
+const { AZTEC_NODE_URL = '' } = process.env;
 
 // Entrypoint for running the up-quick-start script on the CI
 describe('guides/up_quick_start', () => {
   // TODO: update to not use CLI
   it('works', async () => {
-    await waitForPXE(createPXEClient(PXE_URL));
-    execSync(`LOG_LEVEL=\${LOG_LEVEL:-verbose} ./src/guides/up_quick_start.sh`, {
-      shell: '/bin/bash',
-      stdio: 'inherit',
-    });
+    await waitForNode(createAztecNodeClient(AZTEC_NODE_URL));
+    execSync(
+      `LOG_LEVEL=\${LOG_LEVEL:-verbose} AZTEC_NODE_URL=\${AZTEC_NODE_URL:-http://localhost:8080} ./src/guides/up_quick_start.sh`,
+      {
+        shell: '/bin/bash',
+        stdio: 'inherit',
+      },
+    );
   });
 });
