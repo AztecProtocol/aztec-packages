2a30c35a76bd0f7d5a98bed916b028794117aa37
DanielKotov <katod.daniel@gmail.com>
2024-12-11 20:05:21 +0000

    add auxiliary block graph creation + resize method test for dynamic array

---
+++ barretenberg/cpp/src/barretenberg/boomerang_value_detection/graph_description_dynamic_array.test.cpp
@@ -0,0 +1,46 @@
+#include "barretenberg/boomerang_value_detection/graph.hpp"
+#include "barretenberg/common/test.hpp"
+#include "barretenberg/numeric/random/engine.hpp"
+#include "barretenberg/stdlib/primitives/bool/bool.hpp"
+#include "barretenberg/stdlib/primitives/circuit_builders/circuit_builders.hpp"
+#include "barretenberg/stdlib/primitives/memory/dynamic_array.hpp"
+
+using namespace bb;
+namespace {
+auto& engine = bb::numeric::get_debug_randomness();
+}
+
+// Defining ultra-specific types for local testing.
+using Builder = UltraCircuitBuilder;
+using bool_ct = stdlib::bool_t<Builder>;
+using field_ct = stdlib::field_t<Builder>;
+using witness_ct = stdlib::witness_t<Builder>;
+using DynamicArray_ct = stdlib::DynamicArray<Builder>;
+
+TEST(boomerang_stdlib_dynamic_array, graph_description_dynamic_array_method_resize_test)
+{
+
+    Builder builder;
+    const size_t max_size = 10;
+
+    DynamicArray_ct array(&builder, max_size);
+
+    field_ct next_size = field_ct(witness_ct(&builder, (uint256_t)(max_size - 1)));
+    for (size_t i = 0; i < max_size; ++i) {
+        array.push(field_ct::from_witness(&builder, i));
+    }
+
+    array.resize(next_size, 7);
+    Graph graph = Graph(builder);
+    auto connected_components = graph.find_connected_components();
+    auto variables_in_one_gate = graph.show_variables_in_one_gate(builder);
+    if (variables_in_one_gate.size() > 0) {
+        for (const auto& elem : variables_in_one_gate) {
+            info("elem = ", elem);
+        }
+    } else {
+        info("variables_in_one_gate is empty");
+    }
+    EXPECT_EQ(connected_components.size(), 1);
+    // EXPECT_EQ(variables_in_one_gate.size(), max_size * 2);
+}
