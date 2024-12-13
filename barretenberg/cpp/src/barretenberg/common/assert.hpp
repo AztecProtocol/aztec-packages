a8f564f0a8877c4457db612f9e7cf8856a086087
ludamad <domuradical@gmail.com>
2024-12-13 01:58:44 +0000

    try to turn make assertions warnings piecemeal

--- barretenberg/cpp/src/barretenberg/common/assert.hpp
+++ barretenberg/cpp/src/barretenberg/common/assert.hpp
@@ -2,7 +2,7 @@
 
 #ifdef NDEBUG
 // In NDEBUG mode, no assertion checks are performed.
-// Compiler should optimize this out without triggering unused variable warnings.
+// Compiler should optimize this out in release builds, without triggering an unused variable warning.
 #define DONT_EVALUATE(expression)                                                                                      \
     {                                                                                                                  \
         true ? static_cast<void>(0) : static_cast<void>((expression));                                                 \
@@ -12,5 +12,5 @@
 
 #else
 void bb_assert_fail(const char* assertion, const char* file, int line, const char* function);
-#define ASSERT(expr) (static_cast<bool>(expr) ? void(0) : bb_assert_fail(#expr, __FILE__, __LINE__, __func__))
+#define ASSERT(expr) (static_cast<bool>((expr)) ? void(0) : bb_assert_fail(#expr, __FILE__, __LINE__, __func__))
 #endif // NDEBUG
