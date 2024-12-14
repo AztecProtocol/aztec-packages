9cdbc3b49c4d03b82bd82e47b59ba0c3024cf317
Charlie Lye <karl.lye@gmail.com>
2024-12-14 13:14:57 +0000

    /tmp 1gb in-mem mount to spare disk io and start implementing limits

--- yarn-project/end-to-end/scripts/test.sh
+++ yarn-project/end-to-end/scripts/test.sh
@@ -4,6 +4,9 @@
 # A "simple" test is one that does not require docker-compose. They are still run within docker isolation however.
 # A "compose" test uses docker-compose to launch actual services.
 # A "skip" or "flake" test is simple skipped, the label is informational.
+#
+# To avoid thrashing the disk, we mount /tmp as a 1gb tmpfs.
+# We separate out jests temp dir for now, as it consumes a lot of space and we want to quota /tmp independently.
 set -eu
 
 TYPE=$1
@@ -18,6 +21,8 @@ case "$TYPE" in
       -v$PWD/../..:/root/aztec-packages \
       -v$HOME/.bb-crs:/root/.bb-crs \
       --mount type=tmpfs,target=/tmp,tmpfs-size=1g \
+      --mount type=tmpfs,target=/tmp-jest,tmpfs-size=512m \
+      -e JEST_CACHE_DIR=/tmp-jest \
       --workdir /root/aztec-packages/yarn-project/end-to-end \
       aztecprotocol/build:2.0 ./scripts/test_simple.sh $TEST
   ;;
