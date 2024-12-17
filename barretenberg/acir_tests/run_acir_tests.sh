3307d55c6825c0faeff4977cd328db02b0f72e45
ludamad <domuradical@gmail.com>
2024-12-17 19:07:08 +0000

    fix bench

--- barretenberg/acir_tests/run_acir_tests.sh
+++ barretenberg/acir_tests/run_acir_tests.sh
@@ -10,7 +10,24 @@ set -eu
 # Catch when running in parallel
 error_file="/tmp/error.$$"
 pids=()
-source ./bash_helpers/catch.sh
+
+# Handler for SIGCHLD, cleanup if child exit with error
+handle_sigchild() {
+    for pid in "${pids[@]}"; do
+        # If process is no longer running
+        if ! kill -0 "$pid" 2>/dev/null; then
+            # Wait for the process and get exit status
+            wait "$pid"
+            status=$?
+
+            # If exit status is error
+            if [ $status -ne 0 ]; then
+                # Create error file
+                touch "$error_file"
+            fi
+        fi
+    done
+}
 trap handle_sigchild SIGCHLD
 
 BIN=${BIN:-../cpp/build/bin/bb}
@@ -34,9 +51,6 @@ else
 fi
 
 export BIN CRS_PATH VERBOSE BRANCH RECURSIVE
-
-# copy the gzipped acir test data from noir/noir-repo/test_programs to barretenberg/acir_tests
-./clone_test_vectors.sh
 
 cd acir_tests
 
@@ -128,4 +142,9 @@ fi
 wait
 
 # Check for parallel errors
-check_error_file
+# If error file exists, exit with error
+if [ -f "$error_file" ]; then
+    rm "$error_file"
+    echo "Error occurred in one or more child processes. Exiting..."
+    exit 1
+fi
