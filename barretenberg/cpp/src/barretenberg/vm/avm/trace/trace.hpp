923826a9d1bbed6739527a82b34d5610600eca1b
Álvaro Rodríguez <sirasistant@gmail.com>
2024-12-17 17:05:14 +0100

    feat: Add tree equality assertions (#10756)
    Asserts in the public base that the end tree roots of the AVM equal the
    calculated tree roots in the public base, to check correctness of the
    AVM insertions.

--- barretenberg/cpp/src/barretenberg/vm/avm/trace/trace.hpp
+++ barretenberg/cpp/src/barretenberg/vm/avm/trace/trace.hpp
@@ -238,6 +238,7 @@ class AvmTraceBuilder {
     void insert_private_revertible_state(const std::vector<FF>& siloed_nullifiers,
                                          const std::vector<FF>& siloed_note_hashes);
     void pay_fee();
+    void pad_trees();
 
     // These are used for testing only.
     AvmTraceBuilder& set_range_check_required(bool required)
