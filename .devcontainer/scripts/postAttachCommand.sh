e891d731e98b123b99ca866839935efde693a758
Ae0h <dontlisten33@gmail.com>
2024-12-18 12:41:30 -0800

    feat:Cookbook Onboard integration

--- .devcontainer/scripts/postAttachCommand.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-apt update
-apt install gh
-gh codespace ports visibility 8080:public -c $CODESPACE_NAME
-
-aztec start --sandbox
--- noir/noir-repo/docs/src/theme/Root.js
+++ noir/noir-repo/docs/src/theme/Root.js
@@ -2,12 +2,17 @@ import React from 'react';
 import useMatomo from '@site/src/components/Matomo/matomo';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import useIsBrowser from '@docusaurus/useIsBrowser';
+import AskCookbook from '@cookbookdev/docsbot/react';
 
 function OptOutForm() {
   const banner = useMatomo();
 
   return <>{banner}</>;
 }
+
+/** It's a public API key, so it's safe to expose it here. */
+const COOKBOOK_PUBLIC_API_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NzVhMGVkODQ4MGI5OThmNzlhNjNhOTciLCJpYXQiOjE3MzM5NTUyODgsImV4cCI6MjA0OTUzMTI4OH0.NzQlH2sfhJkjvnYxoaRgSY6nRyNClxHg57n3-JueN9Q';
 
 export default function Root({ children }) {
   const useIsBrowserValue = useIsBrowser();
@@ -17,6 +22,7 @@ export default function Root({ children }) {
     <>
       {children}
       <BrowserOnly>{() => <OptOutForm />}</BrowserOnly>
+      <BrowserOnly>{() => <AskCookbook apiKey={COOKBOOK_PUBLIC_API_KEY} />}</BrowserOnly>
     </>
   );
 }
