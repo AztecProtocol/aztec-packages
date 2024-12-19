1bb627a56dfafa685a4d993657367004beca7b05
benesjan <janbenes1234@gmail.com>
2024-12-19 03:43:13 +0000

    WIP

---
+++ yarn-project/end-to-end/src/e2e_pxe_store.test.ts
@@ -0,0 +1,35 @@
+import {
+    type Wallet
+} from '@aztec/aztec.js';
+import { TestContract } from '@aztec/noir-contracts.js/Test';
+
+import { jest } from '@jest/globals';
+
+import { setup } from './fixtures/utils.js';
+
+const TIMEOUT = 120_000;
+
+// TODO(#10724): Nuke this once the linked issue is implemented. Made this ugly test to check it works when first
+// implementing this.
+describe('Keys', () => {
+  jest.setTimeout(TIMEOUT);
+
+  let teardown: () => Promise<void>;
+
+  let testContract: TestContract;
+
+  beforeAll(async () => {
+    let wallet: Wallet;
+    ({ teardown, wallet } = await setup(1));
+    testContract = await TestContract.deploy(wallet).send().deployed();
+  });
+
+  afterAll(() => teardown());
+
+  describe('using nsk_app to detect nullification', async () => {
+    const key = 6n;
+    const value = [268n, 862n, 268n ];
+    await testContract.methods.store_in_pxe_store(key, value).simulate();
+    expect(await testContract.methods.load_from_pxe_store(key).simulate()).toEqual(value);
+  });
+});
