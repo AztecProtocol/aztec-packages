8c179594332c7acd6403dde427097639295f7bdc
dbanks12 <david@aztecprotocol.com>
2024-12-23 21:56:05 +0000

    fix test

--- yarn-project/end-to-end/src/e2e_deploy_contract/deploy_method.test.ts
+++ yarn-project/end-to-end/src/e2e_deploy_contract/deploy_method.test.ts
@@ -32,27 +32,6 @@ describe('e2e_deploy_contract deploy method', () => {
 
   afterAll(() => t.teardown());
 
-  it('cannot skip contract class registration', async () => {
-    const owner = wallet.getAddress();
-    const opts = { skipClassRegistration: true };
-    await expect(StatefulTestContract.deploy(wallet, owner, owner, 42).send(opts)).rejects.toThrow(/blah/);
-    //await StatefulTestContract.deploy(wallet, owner, owner, 42).send(opts).wait();
-  });
-
-  it('cannot deploy a contract instance without registering its contract class', async () => {
-    const constructorArtifact = StatefulTestContract.artifact.functions.find(fn => fn.name == 'constructor');
-    if (!constructorArtifact) {
-      // If this gets thrown you've probably modified the StatefulTestContract to no longer include constructor.
-      // If that's the case you should update this test to use a private function which fits into the bytecode size limit.
-      throw new Error('No constructor found in the StatefulTestContract artifact. Does it still exist?');
-    }
-    const selector = FunctionSelector.fromNameAndParameters(constructorArtifact.name, constructorArtifact.parameters);
-    const txReceipt = await (await broadcastPrivateFunction(wallet, StatefulTestContract.artifact, selector))
-      .send()
-      .wait();
-    expect(txReceipt.status).toEqual(TxStatus.DROPPED);
-  });
-
   it('publicly deploys and initializes a contract', async () => {
     const owner = wallet.getAddress();
     logger.debug(`Deploying stateful test contract`);
@@ -118,6 +97,14 @@ describe('e2e_deploy_contract deploy method', () => {
     logger.debug(`Deploying contract with no constructor and skipping public deploy`);
     const opts = { skipPublicDeployment: true, skipClassRegistration: true };
     await expect(TestContract.deploy(wallet).prove(opts)).rejects.toThrow(/no function calls needed/i);
+  });
+
+  it('refused to deploy a contract whose contract class is not yet registered', async () => {
+    const owner = wallet.getAddress();
+    const opts = { skipClassRegistration: true };
+    await expect(StatefulTestContract.deploy(wallet, owner, owner, 42).send(opts).wait()).rejects.toThrow(
+      /Cannot find the leaf for nullifier/,
+    );
   });
 
   it.skip('publicly deploys and calls a public function in the same batched call', async () => {
