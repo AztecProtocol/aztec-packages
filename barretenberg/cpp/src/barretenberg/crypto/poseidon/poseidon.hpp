9fa447b3ad26ed25e493a77d26f57a6388540cc4
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2024-12-30 17:19:16 -0300

    Adds license and source notice

--- barretenberg/cpp/src/barretenberg/crypto/poseidon/poseidon.hpp
+++ barretenberg/cpp/src/barretenberg/crypto/poseidon/poseidon.hpp
@@ -1,3 +1,8 @@
+/* C implementation of Poseidon hash function.
+ * Extracted from: https://github.com/CryptoExperts/poseidon
+ * Licensed under the Apache License, Version 2.0.
+ */
+
 #pragma once
 
 #ifdef __cplusplus
