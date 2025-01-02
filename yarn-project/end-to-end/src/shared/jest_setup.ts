839e2fcda35f1ae33df6f8383b7ec5c07c9d52da
Santiago Palladino <santiago@aztecprotocol.com>
2025-01-02 10:02:05 -0300

    chore: Add afterEach hook to log when a test finishes
    We have a beforeEach hook in e2e tests that lets us know in the logs
    when a test starts, but we don't have an afterEach that points to when
    it finishes. Note that we cannot use the start of the next test as a
    pointer, since other setup hooks get in the way.

--- yarn-project/end-to-end/src/shared/jest_setup.ts
+++ yarn-project/end-to-end/src/shared/jest_setup.ts
@@ -1,13 +1,30 @@
 import { createLogger } from '@aztec/aztec.js';
 
-import { beforeEach, expect } from '@jest/globals';
+import { afterEach, beforeEach, expect } from '@jest/globals';
 import { basename } from 'path';
+
+function makeLogger(testPath: string) {
+  return createLogger(`e2e:${basename(testPath).replace('.test.ts', '')}`);
+}
 
 beforeEach(() => {
   const { testPath, currentTestName } = expect.getState();
   if (!testPath || !currentTestName) {
     return;
   }
-  const logger = createLogger(`e2e:${basename(testPath).replace('.test.ts', '')}`);
+  const logger = makeLogger(testPath);
   logger.info(`Running test: ${currentTestName}`);
+});
+
+afterEach(() => {
+  const { testPath, currentTestName, error } = expect.getState();
+  if (!testPath || !currentTestName) {
+    return;
+  }
+  const logger = makeLogger(testPath);
+  if (error) {
+    logger.error(`Failed test: ${currentTestName}`, error);
+  } else {
+    logger.info(`Finished test: ${currentTestName}`);
+  }
 });
