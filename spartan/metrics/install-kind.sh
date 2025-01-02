31e057f981a3ead8ab3e5cad63ec55237efea431
Mitch <mitchell@aztecprotocol.com>
2025-01-02 09:26:08 -0500

    chore: load in the big dashboard during metrics install

--- spartan/metrics/install-kind.sh
+++ spartan/metrics/install-kind.sh
@@ -10,6 +10,10 @@ if helm ls --namespace metrics | grep -q metrics; then
   exit 0
 fi
 
+# Inject the Aztec Networks dashboard into values.yaml
+DASHBOARD_JSON=$(jq -c '.' grafana_dashboards/aztec-dashboard-all-in-one.json)
+yq -Y --arg dashboard "$DASHBOARD_JSON" '.grafana.dashboards.default."aztec-networks".json = $dashboard' values.tmp.yaml > values.yaml
+
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
--- spartan/metrics/install-prod.sh
+++ spartan/metrics/install-prod.sh
@@ -3,4 +3,7 @@ set -eu
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+DASHBOARD_JSON=$(jq -c '.' grafana_dashboards/aztec-dashboard-all-in-one.json)
+yq -Y --arg dashboard "$DASHBOARD_JSON" '.grafana.dashboards.default."aztec-networks".json = $dashboard' values.tmp.yaml > values.yaml
+
 helm upgrade metrics . -n metrics --values "./values/prod.yaml" --install --create-namespace $@
