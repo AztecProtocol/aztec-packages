e39c48b5f6e1df772294eded34521ee2dc9ef879
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2025-01-03 13:24:37 +0000

    feat: move blob sink server into server folder

--- yarn-project/blob-sink/src/config.ts
+++
@@ -1,7 +0,0 @@
-import { type DataStoreConfig } from '@aztec/kv-store/config';
-
-export interface BlobSinkConfig {
-  port?: number;
-  dataStoreConfig?: DataStoreConfig;
-  otelMetricsCollectorUrl?: string;
-}
