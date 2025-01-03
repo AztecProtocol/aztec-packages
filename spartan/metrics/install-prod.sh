2ae8519e3136852ad87dfb8baedabfc2efbc324a
Mitch <mitchell@aztecprotocol.com>
2025-01-03 15:12:27 -0500

    fix: use same version of yq as in CI

--- spartan/metrics/install-prod.sh
+++ spartan/metrics/install-prod.sh
@@ -4,6 +4,6 @@ set -eu
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 DASHBOARD_JSON=$(jq -c '.' grafana_dashboards/aztec-dashboard-all-in-one.json)
-yq -Y --arg dashboard "$DASHBOARD_JSON" '.grafana.dashboards.default."aztec-networks".json = $dashboard' values.tmp.yaml > values.yaml
+DASHBOARD_JSON=$DASHBOARD_JSON ~/bin/yq e '.grafana.dashboards.default."aztec-networks".json = strenv(DASHBOARD_JSON)' values.tmp.yaml > values.yaml
 
 helm upgrade metrics . -n metrics --values "./values/prod.yaml" --install --create-namespace $@
