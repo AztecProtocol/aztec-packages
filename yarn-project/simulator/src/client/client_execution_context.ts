bbc5fc55c29b9f2f7d2c0d9606c7f91b7704da8d
Santiago Palladino <santiago@aztecprotocol.com>
2025-01-06 11:17:20 -0300

    Lint

--- yarn-project/simulator/src/client/client_execution_context.ts
+++ yarn-project/simulator/src/client/client_execution_context.ts
@@ -23,7 +23,7 @@ import { computeUniqueNoteHash, siloNoteHash } from '@aztec/circuits.js/hash';
 import { type FunctionAbi, type FunctionArtifact, type NoteSelector, countArgumentsSize } from '@aztec/foundation/abi';
 import { AztecAddress } from '@aztec/foundation/aztec-address';
 import { Fr } from '@aztec/foundation/fields';
-import { applyStringFormatting, createLogger } from '@aztec/foundation/log';
+import { createLogger } from '@aztec/foundation/log';
 
 import { type NoteData, toACVMWitness } from '../acvm/index.js';
 import { type PackedValuesCache } from '../common/packed_values_cache.js';
