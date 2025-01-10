b88f1e91e7745bcbb68bf5cc4dfd6c380815bdb2
Cody <codygunton@gmail.com>
2025-01-10 17:14:13 +0000

    Generating main circuit works

--- yarn-project/bb-bench/bench_native.sh
+++ yarn-project/bb-bench/bench_native.sh
@@ -1,3 +1,4 @@
 # WORKTODO: create proof dir if it doesn't exist
+CIRCUIT=recursion
 BB=../../../barretenberg/cpp/build/bin/bb
-cd recursion && BB_VERBOSE=1 $BB prove_and_verify_ultra_honk -b ./target/recursion.json -w ./target/recursion.gz -v
+cd $CIRCUIT && BB_VERBOSE=1 $BB prove_and_verify_ultra_honk -b ./target/$CIRCUIT.json -w ./target/$CIRCUIT.gz -v
