8d1d798044b3850f51cafd7434a2f24db86470fa
ludamad <adam.domurad@gmail.com>
2025-01-13 20:57:16 +0000

    comment

--- yarn-project/precommit.sh
+++ yarn-project/precommit.sh
@@ -2,8 +2,6 @@
 # Precommit hook for formatting staged files.
 # Formatting is slow. Linting is slow. That's not much fun in a precommit hook.
 # We only run the formatter over staged files, and we parallelize with chunks of 10 files per prettier.
-# Linting is run over everything, but we parallelize over projects directories, and use --cache to improve repeat runs.
-# Optional env var: HOOKS_NO_LINT, disables linting to bypass requiring a bootstrapped yarn-project.
 set -euo pipefail
 
 cd $(dirname $0)
@@ -13,6 +11,8 @@ export FORCE_COLOR=true
 staged_files_cmd="git diff-index --diff-filter=d --relative --cached --name-only HEAD"
 
 function lint {
+  # TODO(ci3) unused
+  # Linting is run over everything, but we parallelize over projects directories, and use --cache to improve repeat runs.
   ls -d ./*/src | xargs dirname | parallel 'cd {} && ../node_modules/.bin/eslint --cache ./src'
 }
 export -f lint
