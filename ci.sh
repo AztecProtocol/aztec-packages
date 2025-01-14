4597c7bab2b105f022a9c473c6caa88d120cf0c7
Charlie Lye <karl.lye@gmail.com>
2025-01-14 14:57:54 +0000

    wip

--- ci.sh
+++ ci.sh
@@ -10,33 +10,39 @@ ci3_workflow_id=128853861
 function echo_cmd {
   local name=$1
   shift
-  printf "${blue}${bold}%10s${reset}: %s\n" $name "$(echo $@ | sed 's/\. /.\n            /g')"
+  printf "${blue}${bold}%12s${reset}: %s\n" $name "$(echo $@ | sed 's/\.\\n/.\n             /g')"
 }
 
 function print_usage {
   echo "usage: $(basename $0) <cmd>"
   echo
-  echo_cmd "ec2"      "Launch an ec2 instance and bootstrap on it." \
-                      "Exactly what Github Action's does, but doesn't touch GA."
-  echo_cmd "local"    "Clone your last commit into the ci container and bootstrap on local hardware."
-  echo_cmd "trigger"  "Trigger the GA workflow on the PR associated with the current branch." \
-                      "Effectively the same as ec2, only the results will be tracked on your PR."
-  echo_cmd "log"      "Will tail the logs of the current GA run, or dump log if already completed."
-  echo_cmd "run"      "Same as calling trigger, then log."
-  echo_cmd "wt"       "Runs bootstrap in current working tree on local hardware."
-  echo_cmd "shell"    "Jump into a new shell on the current running build."
-  echo_cmd "attach"   "Attach to terminal of the current running build."
-  echo_cmd "ssh-host" "Connect to host instance of the current running build."
-  echo_cmd "draft"    "Mark current PR as draft (no automatic CI runs when pushing)."
-  echo_cmd "ready"    "Mark current PR as ready (enable automatic CI runs when pushing)."
+  echo_cmd "ec2"          "Launch an ec2 instance and './bootstrap.sh ci' on it.\n" \
+                          "Exactly what Github Action's does, but doesn't touch GA."
+  echo_cmd "ec2-no-cache" "Same as ec2, but perform a full build and test (disable build and test cache)."
+  echo_cmd "ec2-test"     "Same as ec2, but run all tests (disable test cache)."
+  echo_cmd "ec2-grind"    "Same as ec2-test, but run over N instances."
+  echo_cmd "ec2-shell"    "Launch an ec2 instance, clone the repo and drop into a shell."
+  echo_cmd "local"        "Clone your last commit into a fresh container and bootstrap on local hardware."
+  echo_cmd "run"          "Same as calling trigger, then log."
+  echo_cmd "shell"        "Jump into a new shell on the current running build instance.\n" \
+                          "Can provide a command to run instead of dropping into a shell, e.g. 'ci shell ls'."
+  echo_cmd "trigger"      "Trigger the GA workflow on the PR associated with the current branch.\n" \
+                          "Effectively the same as ec2, only the results will be tracked on your PR."
+  echo_cmd "rlog"         "Will tail the logs of the current GA run, or the given GA run id."
+  echo_cmd "ilog"         "Will tail the logs of the current running build instance."
+  echo_cmd "dlog"         "Display the log of the given denoise log it."
+  echo_cmd "tlog"         "Display the last log of the given test command."
+  echo_cmd "shell-host"   "Connect to host instance of the current running build."
+  echo_cmd "draft"        "Mark current PR as draft (no automatic CI runs when pushing)."
+  echo_cmd "ready"        "Mark current PR as ready (enable automatic CI runs when pushing)."
 }
 
 [ -n "$cmd" ] && shift
 
-instance_name=$(echo -n "$BRANCH" | tr -c 'a-zA-Z0-9-' '_')_$arch
+instance_name=$(echo -n "$BRANCH" | tr -c 'a-zA-Z0-9-' '_')_${arch}
+[ -n "${INSTANCE_POSTFIX:-}" ] && instance_name+="_$INSTANCE_POSTFIX"
 
 function get_ip_for_instance {
-  [ -n "${1:-}" ] && instance_name+="_$1"
   ip=$(aws ec2 describe-instances \
     --region us-east-2 \
     --filters "Name=tag:Name,Values=$instance_name" \
@@ -46,12 +52,6 @@ function get_ip_for_instance {
 
 function get_latest_run_id {
   gh run list --workflow $ci3_workflow_id -b $BRANCH --limit 1 --json databaseId -q .[0].databaseId
-}
-
-function remote_redis_cli {
-  ssh -o ControlMaster=auto -o ControlPath=/tmp/ssh_mux_%h_%p_%r -o ControlPersist=10m \
-    -F $ci3/aws/build_instance_ssh_config ci-bastion.aztecprotocol.com \
-    redis-cli -h ci-redis.lzka0i.0001.use2.cache.amazonaws.com --raw "$@"
 }
 
 function tail_live_instance {
@@ -65,26 +65,18 @@ function tail_live_instance {
 }
 
 case "$cmd" in
-  "init")
-    # Start a redis server we can use for caching logs etc.
-    if [ "$(docker ps -q -f name=ci-redis)" ]; then
-        echo "Container 'ci-redis' is already running."
-    else
-        docker run -d --rm --name ci-redis -p 6379:6379 redis:latest
-    fi
-    ;;
   "ec2")
     # Spin up ec2 instance and ci bootstrap with shell on failure.
     # You can override the bootstrap command with the first arg e.g: ci ec2 full
-    bootstrap_ec2 "./bootstrap.sh ${1:-ci}" ${2:-}
+    bootstrap_ec2 "./bootstrap.sh ${1:-ci}"
     ;;
   "ec2-no-cache")
     # Same as ec2, but disable the build and test cache.
-    bootstrap_ec2 "USE_CACHE=0 USE_TEST_CACHE=0 ./bootstrap.sh ${1:-ci}" ${2:-}
+    bootstrap_ec2 "USE_CACHE=0 USE_TEST_CACHE=0 ./bootstrap.sh ${1:-ci}"
     ;;
   "ec2-test")
     # Same as ec2, but don't use the test cache.
-    bootstrap_ec2 "USE_TEST_CACHE=0 ./bootstrap.sh ci" ${1:-}
+    bootstrap_ec2 "USE_TEST_CACHE=0 ./bootstrap.sh ci"
     ;;
   "ec2-shell")
     # Spin up ec2 instance, clone, and drop into shell.
@@ -106,9 +98,12 @@ case "$cmd" in
     $0 trigger
     $0 log
     ;;
-  "wt")
-    # Runs bootstrap in current working tree.
-    ./bootstrap.sh ci
+  "shell")
+    get_ip_for_instance
+    [ -z "$ip" ] && echo "No instance found: $instance_name" && exit 1
+    [ "$#" -eq 0 ] && set -- "zsh" || true
+    ssh -tq -F $ci3/aws/build_instance_ssh_config ubuntu@$ip \
+      "docker start aztec_build &>/dev/null || true && docker exec -it --user aztec-dev aztec_build $@"
     ;;
   "trigger")
     # Trigger workflow.
@@ -123,12 +118,12 @@ case "$cmd" in
     sleep 1
     gh pr edit "$pr_number" --remove-label "trigger-workflow" &> /dev/null
     run_id=$(get_latest_run_id)
-    echo "In progress..." | remote_redis_cli -x SETEX $run_id 3600 &> /dev/null
+    echo "In progress..." | redis_cli -x SETEX $run_id 3600 &> /dev/null
     echo -e "Triggered CI workflow for PR: $pr_number (${yellow}$run_id${reset})"
     ;;
-  "log")
+  "rlog")
     [ -z "${1:-}" ] && run_id=$(get_latest_run_id) || run_id=$1
-    output=$(remote_redis_cli GET $run_id)
+    output=$(redis_cli GET $run_id)
     if [ "$output" == "In progress..." ]; then
       # If we're in progress, tail live logs from launched instance.
       while ! tail_live_instance; do
@@ -139,45 +134,38 @@ case "$cmd" in
       echo "$output" | $PAGER
     fi
     ;;
-  "shell")
-    get_ip_for_instance ${INSTANCE_POSTFIX:-}
-    [ -z "$ip" ] && echo "No instance found: $instance_name" && exit 1
-    [ "$#" -eq 0 ] && set -- "zsh" || true
-    ssh -tq -F $ci3/aws/build_instance_ssh_config ubuntu@$ip "docker start aztec_build >/dev/null 2>&1 || true && docker exec -it --user aztec-dev aztec_build $@"
+  "ilog")
+    while ! tail_live_instance; do
+      echo "Waiting on instance with name: $instance_name"
+      sleep 10
+    done
     ;;
-  # "attach")
-  #   get_ip_for_instance ${1:-}
-  #   [ -z "$ip" ] && echo "No instance found: $instance_name" && exit 1
-  #   ssh -t -F $ci3/aws/build_instance_ssh_config ubuntu@$ip 'docker start aztec_build >/dev/null 2>&1 || true && docker attach aztec_build'
-  #  ;;
-  # "log")
-  #   get_ip_for_instance ${1:-}
-  #   [ -z "$ip" ] && echo "No instance found: $instance_name" && exit 1
-  #   ssh -t -F $ci3/aws/build_instance_ssh_config ubuntu@$ip 'docker logs -f aztec_build'
-  #   ;;
   "dlog")
+    if [ "$CI_REDIS_AVAILABLE" -ne 1 ]; then
+      echo "No redis available for log query."
+      exit 1
+    fi
     pager=${PAGER:-less}
     [ ! -t 0 ] && pager=cat
-    # TODO: Is this ok? We might show a local test log rather than a remote one as the hashes would collide.
-    if [ "$(redis-cli --raw EXISTS $1)" -eq 1 ]; then
-      redis-cli --raw GET $1 | $pager
-    else
-      remote_redis_cli GET $1 | $pager
-    fi
+    redis_cli GET $1 | $pager
     ;;
   "tlog")
+    if [ "$CI_REDIS_AVAILABLE" -ne 1 ]; then
+      echo "No redis available for test query."
+      exit 1
+    fi
     pager=${PAGER:-less}
     key=$(hash_str "$1")
-    log_key=$(remote_redis_cli --raw GET $key)
+    log_key=$(redis_cli --raw GET $key)
     if [ -n "$log_key" ]; then
-      remote_redis_cli GET $log_key | $pager
+      redis_cli GET $log_key | $pager
     else
       echo "No test log found for: $key"
       exit 1
     fi
     ;;
   "shell-host")
-    get_ip_for_instance ${1:-}
+    get_ip_for_instance
     [ -z "$ip" ] && echo "No instance found: $instance_name" && exit 1
     ssh -t -F $ci3/aws/build_instance_ssh_config ubuntu@$ip
     ;;
