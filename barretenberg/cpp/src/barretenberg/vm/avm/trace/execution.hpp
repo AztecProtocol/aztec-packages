29bc4bdd5b59ee1050951e0c143654ef3cdd25b0
David Banks <47112877+dbanks12@users.noreply.github.com>
2025-01-15 20:47:53 -0500

    fix: resolve misc bugs handling phases in avm witgen (#11218)
    * At the end of teardown, witgen needs to reset gas back to parent's end
    gas.
    * Make sure that order of enqueued calls is right in TX for bb-prover
    tests (should be a stack)
    * Add a test that reverts in teardown and can still be proven

--- barretenberg/cpp/src/barretenberg/vm/avm/trace/execution.hpp
+++ barretenberg/cpp/src/barretenberg/vm/avm/trace/execution.hpp
@@ -40,8 +40,10 @@ class Execution {
                                       ExecutionHints const& execution_hints,
                                       bool apply_e2e_assertions = false);
 
-    static AvmError execute_enqueued_call(AvmTraceBuilder& trace_builder,
+    static AvmError execute_enqueued_call(TxExecutionPhase& phase,
+                                          AvmTraceBuilder& trace_builder,
                                           AvmEnqueuedCallHint& enqueued_call_hint,
+                                          Gas const teardown_gas_limits,
                                           std::vector<FF>& returndata,
                                           bool check_bytecode_membership);
 
