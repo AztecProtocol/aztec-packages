bf87a933a16a55148be6f14e88ff2b2f7a64091a
ludamad <domuradical@gmail.com>
2025-01-20 18:46:03 +0000

    fix

--- noir-projects/noir-protocol-circuits/bootstrap.sh
+++ noir-projects/noir-protocol-circuits/bootstrap.sh
@@ -122,7 +122,7 @@ function compile {
       local verifier_path="$key_dir/${name}_verifier.sol"
       SECONDS=0
       # Generate solidity verifier for this contract.
-      echo "$vk" | $BB contract_ultra_honk -k - -o $verifier_path
+      echo "$vk" | xxd -r -p | $BB contract_ultra_honk -k - -o $verifier_path
       echo_stderr "VK output at: $verifier_path (${SECONDS}s)"
       # Include the verifier path if we create it.
       cache_upload vk-$hash.tar.gz $key_path $verifier_path &> /dev/null
