163886e63d2d8d01e828e75c4d61dcbc83893fa0
Cody <codygunton@gmail.com>
2025-01-21 20:04:33 +0000

    Remove _bb-bench for now

--- yarn-project/_bb-bench/bench_native.sh
+++
@@ -1,4 +0,0 @@
-# WORKTODO: create proof dir if it doesn't exist
-CIRCUIT=recursion
-BB=../../../barretenberg/cpp/build/bin/bb
-cd $CIRCUIT && BB_VERBOSE=1 $BB prove_and_verify_ultra_honk -b ./target/$CIRCUIT.json -w ./target/$CIRCUIT.gz -v
