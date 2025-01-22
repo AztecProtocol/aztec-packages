ab813b108cb02bac7b40a8b22fc471740d8dacf4
Cody <codygunton@gmail.com>
2025-01-22 00:41:47 +0000

    Revert unused changes

--- barretenberg/ts/src/barretenberg/backend.ts
+++ barretenberg/ts/src/barretenberg/backend.ts
@@ -315,11 +315,22 @@ export class UltraHonkBackend {
     return await this.api.acirHonkSolidityVerifier(this.acirUncompressedBytecode, new RawBuffer(vkBuf));
   }
 
+  // TODO(https://github.com/noir-lang/noir/issues/5661): Update this to handle Honk recursive aggregation in the browser once it is ready in the backend itself
   async generateRecursiveProofArtifacts(
-    proof: Uint8Array,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _proof: Uint8Array,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _numOfPublicInputs: number,
   ): Promise<{ proofAsFields: string[]; vkAsFields: string[]; vkHash: string }> {
     await this.instantiate();
-    const proofAsFrs = await this.api.acirProofAsFieldsUltraHonk(proof);
+    // TODO(https://github.com/noir-lang/noir/issues/5661): This needs to be updated to handle recursive aggregation.
+    // There is still a proofAsFields method but we could consider getting rid of it as the proof itself
+    // is a list of field elements.
+    // UltraHonk also does not have public inputs directly prepended to the proof and they are still instead
+    // inserted at an offset.
+    // const proof = reconstructProofWithPublicInputs(proofData);
+    // const proofAsFields = (await this.api.acirProofAsFieldsUltraHonk(proof)).slice(numOfPublicInputs);
+
     // TODO: perhaps we should put this in the init function. Need to benchmark
     // TODO how long it takes.
     const vkBuf = await this.api.acirWriteVkUltraHonk(this.acirUncompressedBytecode, this.circuitOptions.recursive);
@@ -327,7 +338,7 @@ export class UltraHonkBackend {
 
     return {
       // TODO(https://github.com/noir-lang/noir/issues/5661)
-      proofAsFields: proofAsFrs.map(proofAsFrs => proofAsFrs.toString()),
+      proofAsFields: [],
       vkAsFields: vk.map(vk => vk.toString()),
       // We use an empty string for the vk hash here as it is unneeded as part of the recursive artifacts
       // The user can be expected to hash the vk inside their circuit to check whether the vk is the circuit
