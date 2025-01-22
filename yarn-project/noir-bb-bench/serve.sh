80b0a5a215cd74139864168fce3579b0e1e917bf
Cody <codygunton@gmail.com>
2025-01-22 03:24:55 +0000

    remove unused serving logic

--- yarn-project/noir-bb-bench/serve.sh
+++ yarn-project/noir-bb-bench/serve.sh
@@ -3,21 +3,12 @@
 # Function to handle cleanup when the script is terminated
 cleanup() {
     echo "Initiating cleanup..."
-
     # Terminate the first yarn serve process (port 8080)
     if kill "$PID1" 2>/dev/null; then
         echo "Killed yarn serve on port 8080 (PID $PID1)."
     else
         echo "No active yarn serve process found on port 8080."
     fi
-
-    # # Terminate the second yarn serve process (port 8081)
-    # if kill "$PID2" 2>/dev/null; then
-    #     echo "Killed yarn serve on port 8081 (PID $PID2)."
-    # else
-    #     echo "No active yarn serve process found on port 8081."
-    # fi
-
     echo "Cleanup completed."
     exit 0
 }
@@ -30,11 +21,6 @@ yarn serve -n -L -p 8080 --cors -c ../serve.mt.json dest &
 PID1=$!
 echo "Started yarn serve on port 8080 with PID $PID1."
 
-# # Start the first yarn serve process on port 8081 in the background
-# yarn serve -n -L -p 8081 --cors -c serve.mt.json "$HOME/.bb-crs" &
-# PID2=$!
-# echo "Started yarn serve on port 8081 with PID $PID2."
-
 
 # Function to monitor background processes
 monitor_processes() {
@@ -44,12 +30,6 @@ monitor_processes() {
             echo "yarn serve on port 8080 (PID $PID1) has exited."
             cleanup
         fi
-
-        # if ! kill -0 "$PID2" 2>/dev/null; then
-        #     echo "yarn serve on port 8081 (PID $PID2) has exited."
-        #     cleanup
-        # fi
-
         sleep 1
     done
 }
