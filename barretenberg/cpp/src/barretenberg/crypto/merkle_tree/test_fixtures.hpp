7755f763db8321f3f91bab3b71357f55d8430836
PhilWindle <philip.windle@gmail.com>
2025-01-24 11:42:13 +0000

    WIP

--- barretenberg/cpp/src/barretenberg/crypto/merkle_tree/test_fixtures.hpp
+++ barretenberg/cpp/src/barretenberg/crypto/merkle_tree/test_fixtures.hpp
@@ -1,7 +1,6 @@
 
 #pragma once
 
-#include "barretenberg/common/test.hpp"
 #include "barretenberg/crypto/merkle_tree/indexed_tree/indexed_leaf.hpp"
 #include "barretenberg/crypto/merkle_tree/lmdb_store/lmdb_tree_store.hpp"
 #include "barretenberg/crypto/merkle_tree/response.hpp"
@@ -9,22 +8,80 @@
 #include "barretenberg/ecc/curves/bn254/fr.hpp"
 #include "barretenberg/numeric/random/engine.hpp"
 #include <cstdint>
+#include <gtest/gtest.h>
 #include <optional>
 
 namespace bb::crypto::merkle_tree {
 
-void check_block_and_root_data(LMDBTreeStore::SharedPtr db, block_number_t blockNumber, fr root, bool expectedSuccess);
+inline void check_block_and_root_data(LMDBTreeStore::SharedPtr db,
+                                      block_number_t blockNumber,
+                                      fr root,
+                                      bool expectedSuccess)
+{
+    BlockPayload blockData;
+    LMDBTreeStore::ReadTransaction::Ptr tx = db->create_read_transaction();
+    bool success = db->read_block_data(blockNumber, blockData, *tx);
+    EXPECT_EQ(success, expectedSuccess);
+    if (expectedSuccess) {
+        EXPECT_EQ(blockData.root, root);
+    }
+    NodePayload nodeData;
+    success = db->read_node(root, nodeData, *tx);
+    EXPECT_EQ(success, expectedSuccess);
+}
 
-void check_block_and_root_data(
-    LMDBTreeStore::SharedPtr db, block_number_t blockNumber, fr root, bool expectedSuccess, bool expectedRootSuccess);
+inline void check_block_and_root_data(
+    LMDBTreeStore::SharedPtr db, block_number_t blockNumber, fr root, bool expectedSuccess, bool expectedRootSuccess)
+{
+    BlockPayload blockData;
+    LMDBTreeStore::ReadTransaction::Ptr tx = db->create_read_transaction();
+    bool success = db->read_block_data(blockNumber, blockData, *tx);
+    EXPECT_EQ(success, expectedSuccess);
+    if (expectedSuccess) {
+        EXPECT_EQ(blockData.root, root);
+    }
+    NodePayload nodeData;
+    success = db->read_node(root, nodeData, *tx);
+    EXPECT_EQ(success, expectedRootSuccess);
+}
 
-void check_block_and_size_data(LMDBTreeStore::SharedPtr db,
-                               block_number_t blockNumber,
-                               index_t expectedSize,
-                               bool expectedSuccess);
+inline void check_block_and_size_data(LMDBTreeStore::SharedPtr db,
+                                      block_number_t blockNumber,
+                                      index_t expectedSize,
+                                      bool expectedSuccess)
+{
+    BlockPayload blockData;
+    LMDBTreeStore::ReadTransaction::Ptr tx = db->create_read_transaction();
+    bool success = db->read_block_data(blockNumber, blockData, *tx);
+    EXPECT_EQ(success, expectedSuccess);
+    if (expectedSuccess) {
+        EXPECT_EQ(blockData.size, expectedSize);
+    }
+}
 
-void check_indices_data(
-    LMDBTreeStore::SharedPtr db, fr leaf, index_t index, bool entryShouldBePresent, bool indexShouldBePresent);
+inline void check_indices_data(
+    LMDBTreeStore::SharedPtr db, fr leaf, index_t index, bool entryShouldBePresent, bool indexShouldBePresent)
+{
+    index_t retrieved = 0;
+    LMDBTreeStore::ReadTransaction::Ptr tx = db->create_read_transaction();
+    bool success = db->read_leaf_index(leaf, retrieved, *tx);
+    EXPECT_EQ(success, entryShouldBePresent);
+    if (entryShouldBePresent) {
+        EXPECT_EQ(index == retrieved, indexShouldBePresent);
+    }
+}
+
+inline void call_operation(std::function<void(std::function<void(const Response& response)>)> operation,
+                           bool expected_success = true)
+{
+    Signal signal;
+    auto completion = [&](const Response& response) -> void {
+        EXPECT_EQ(response.success, expected_success);
+        signal.signal_level();
+    };
+    operation(completion);
+    signal.wait_for_level();
+}
 
 template <typename LeafType, typename Hash>
 void check_leaf_by_hash(LMDBTreeStore::SharedPtr db, IndexedLeaf<LeafType> leaf, bool shouldBePresent)
@@ -194,9 +251,6 @@ fr_sibling_path get_sibling_path(TypeOfTree& tree,
     signal.wait_for_level();
     return h;
 }
-
-void call_operation(std::function<void(std::function<void(const Response& response)>)> operation,
-                    bool expected_success = true);
 
 template <typename TreeType> void rollback_tree(TreeType& tree)
 {
