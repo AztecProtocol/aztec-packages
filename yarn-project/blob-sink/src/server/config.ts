cab3d5f4b4a18cd7721461489e46aae089ea4af6
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2025-01-27 21:35:24 +0000

    fix: build

--- yarn-project/blob-sink/src/server/config.ts
+++ yarn-project/blob-sink/src/server/config.ts
@@ -25,27 +25,3 @@ export const blobSinkConfigMappings: ConfigMappingsType<BlobSinkConfig> = {
 export function getBlobSinkConfigFromEnv(): BlobSinkConfig {
   return getConfigFromMappings<BlobSinkConfig>(blobSinkConfigMappings);
 }
-
-export const blobSinkConfigMappings: ConfigMappingsType<BlobSinkConfig> = {
-  port: {
-    env: 'BLOB_SINK_PORT',
-    description: 'The port to run the blob sink server on',
-  },
-  dataStoreConfig: {
-    ...dataConfigMappings,
-    description: 'The configuration for the data store',
-  },
-  otelMetricsCollectorUrl: {
-    env: 'OTEL_EXPORTER_OTLP_METRICS_ENDPOINT',
-    description: 'The URL of the OTLP metrics collector',
-  },
-};
-
-/**
- * Returns the blob sink configuration from the environment variables.
- * Note: If an environment variable is not set, the default value is used.
- * @returns The blob sink configuration.
- */
-export function getBlobSinkConfigFromEnv(): BlobSinkConfig {
-  return getConfigFromMappings<BlobSinkConfig>(blobSinkConfigMappings);
-}
