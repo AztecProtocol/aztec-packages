91f7bc8cdad4e2a0fdcf9e1ab20977fe08c92ae9
ludamad <adam.domurad@gmail.com>
2025-01-27 16:11:57 +0000

    Update bootstrap.sh

--- spartan/bootstrap.sh
+++ spartan/bootstrap.sh
@@ -4,5 +4,5 @@ source $(git rev-parse --show-toplevel)/ci3/source_bootstrap
 cmd=${1:-}
 
 if [ "$cmd" = "hash" ]; then
-  echo $(hash_str $(cache_content_hash .rebuild_patterns) $(../yarn-project/bootstrap.sh hash))
+  hash_str $(cache_content_hash .rebuild_patterns) $(../yarn-project/bootstrap.sh hash)
 fi
