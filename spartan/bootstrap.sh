30c60f85f99e907766e105e719f6469064a82a46
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2025-01-27 16:07:12 +0000

    fix: spartan cache

--- spartan/bootstrap.sh
+++ spartan/bootstrap.sh
@@ -4,11 +4,8 @@ source $(git rev-parse --show-toplevel)/ci3/source_bootstrap
 cmd=${1:-}
 
 hash=$(cache_content_hash \
-  ../spartan/.rebuild_patterns)
+  ./.rebuild_patterns)
 
-function build {
-  github_group "spartan build"
-}
 
 if [ "$cmd" = "hash" ]; then
   echo $hash
