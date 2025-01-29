1c6da43e8f26e831928806e565a04e5823a693d3
ludamad <adam.domurad@gmail.com>
2025-01-29 12:40:21 +0000

    fixes

--- yarn-project/bb-prover/src/test/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './test_circuit_prover.js';
-export * from './test_verifier.js';
--- yarn-project/bb-prover/src/test/test_verifier.ts
+++
@@ -1,7 +0,0 @@
-import { type ClientProtocolCircuitVerifier, type Tx } from '@aztec/circuit-types';
-
-export class TestCircuitVerifier implements ClientProtocolCircuitVerifier {
-  verifyProof(_tx: Tx): Promise<boolean> {
-    return Promise.resolve(true);
-  }
-}
