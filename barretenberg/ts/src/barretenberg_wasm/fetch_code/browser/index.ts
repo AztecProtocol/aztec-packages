adb0e85a354f9fbb0dba54cb27be1247e7bd89f2
thunkar <gregojquiros@gmail.com>
2025-01-30 11:35:02 +0000

    simplified bb.js bundling

--- barretenberg/ts/src/barretenberg_wasm/fetch_code/browser/index.ts
+++ barretenberg/ts/src/barretenberg_wasm/fetch_code/browser/index.ts
@@ -1,9 +1,25 @@
 import barretenbergModule from '../../barretenberg.wasm.gz';
 import barretenbergThreadsModule from '../../barretenberg-threads.wasm.gz';
+import pako from 'pako';
 
 // Annoyingly the wasm declares if it's memory is shared or not. So now we need two wasms if we want to be
 // able to fallback on "non shared memory" situations.
 export async function fetchCode(multithreaded: boolean) {
-  const res = await fetch(multithreaded ? barretenbergThreadsModule : barretenbergModule);
-  return res.arrayBuffer();
+  let url = multithreaded ? barretenbergThreadsModule : barretenbergModule;
+  url = process.env.BB_WASM_URL ? process.env.BB_WASM_URL + `/${/[^/]+(?=\/$|$)/.exec(url)?.[0]}` : url;
+  const res = await fetch(url);
+  const maybeCompressedData = await res.arrayBuffer();
+  const buffer = new Uint8Array(maybeCompressedData);
+  const isGzip =
+    // Check magic number
+    buffer[0] === 0x1f &&
+    buffer[1] === 0x8b &&
+    // Check compression method:
+    buffer[2] === 0x08;
+  if (isGzip) {
+    const decompressedData = pako.ungzip(buffer);
+    return decompressedData.buffer;
+  } else {
+    return buffer;
+  }
 }
