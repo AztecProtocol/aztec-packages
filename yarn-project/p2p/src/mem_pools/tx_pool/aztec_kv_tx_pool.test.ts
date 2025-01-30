057da8c3561aa19085f279cc4bc84cc26b9257c8
Alex Gherghisan <alexg@aztecprotocol.com>
2025-01-30 10:49:48 +0000

    chore: acually commit conflict resolution

--- yarn-project/p2p/src/mem_pools/tx_pool/aztec_kv_tx_pool.test.ts
+++ yarn-project/p2p/src/mem_pools/tx_pool/aztec_kv_tx_pool.test.ts
@@ -24,42 +24,22 @@ describe('KV TX pool', () => {
     await txPool.addTxs([tx1, tx2, tx3, tx4, tx5]);
 
     // delete two txs and assert that they are properly archived
-<<<<<<< HEAD
-    await txPool.deleteTxs([tx1.getTxHash(), tx2.getTxHash()]);
-    expect(txPool.getArchivedTxByHash(tx1.getTxHash())).resolves.toEqual(tx1);
-    expect(txPool.getArchivedTxByHash(tx2.getTxHash())).resolves.toEqual(tx2);
-
-    // delete a single tx and assert that the first tx is purged and the new tx is archived
-    await txPool.deleteTxs([tx3.getTxHash()]);
-    expect(txPool.getArchivedTxByHash(tx1.getTxHash())).resolves.toBeUndefined();
-    expect(txPool.getArchivedTxByHash(tx2.getTxHash())).resolves.toEqual(tx2);
-    expect(txPool.getArchivedTxByHash(tx3.getTxHash())).resolves.toEqual(tx3);
-
-    // delete multiple txs and assert that the old txs are purged and the new txs are archived
-    await txPool.deleteTxs([tx4.getTxHash(), tx5.getTxHash()]);
-    expect(txPool.getArchivedTxByHash(tx1.getTxHash())).resolves.toBeUndefined();
-    expect(txPool.getArchivedTxByHash(tx2.getTxHash())).resolves.toBeUndefined();
-    expect(txPool.getArchivedTxByHash(tx3.getTxHash())).resolves.toBeUndefined();
-    expect(txPool.getArchivedTxByHash(tx4.getTxHash())).resolves.toEqual(tx4);
-    expect(txPool.getArchivedTxByHash(tx5.getTxHash())).resolves.toEqual(tx5);
-=======
     await txPool.deleteTxs([await tx1.getTxHash(), await tx2.getTxHash()]);
-    expect(txPool.getArchivedTxByHash(await tx1.getTxHash())).toEqual(tx1);
-    expect(txPool.getArchivedTxByHash(await tx2.getTxHash())).toEqual(tx2);
+    expect(txPool.getArchivedTxByHash(await tx1.getTxHash())).resolves.toEqual(tx1);
+    expect(txPool.getArchivedTxByHash(await tx2.getTxHash())).resolves.toEqual(tx2);
 
     // delete a single tx and assert that the first tx is purged and the new tx is archived
     await txPool.deleteTxs([await tx3.getTxHash()]);
-    expect(txPool.getArchivedTxByHash(await tx1.getTxHash())).toBeUndefined();
-    expect(txPool.getArchivedTxByHash(await tx2.getTxHash())).toEqual(tx2);
-    expect(txPool.getArchivedTxByHash(await tx3.getTxHash())).toEqual(tx3);
+    expect(txPool.getArchivedTxByHash(await tx1.getTxHash())).resolves.toBeUndefined();
+    expect(txPool.getArchivedTxByHash(await tx2.getTxHash())).resolves.toEqual(tx2);
+    expect(txPool.getArchivedTxByHash(await tx3.getTxHash())).resolves.toEqual(tx3);
 
     // delete multiple txs and assert that the old txs are purged and the new txs are archived
     await txPool.deleteTxs([await tx4.getTxHash(), await tx5.getTxHash()]);
-    expect(txPool.getArchivedTxByHash(await tx1.getTxHash())).toBeUndefined();
-    expect(txPool.getArchivedTxByHash(await tx2.getTxHash())).toBeUndefined();
-    expect(txPool.getArchivedTxByHash(await tx3.getTxHash())).toBeUndefined();
-    expect(txPool.getArchivedTxByHash(await tx4.getTxHash())).toEqual(tx4);
-    expect(txPool.getArchivedTxByHash(await tx5.getTxHash())).toEqual(tx5);
->>>>>>> origin/master
+    expect(txPool.getArchivedTxByHash(await tx1.getTxHash())).resolves.toBeUndefined();
+    expect(txPool.getArchivedTxByHash(await tx2.getTxHash())).resolves.toBeUndefined();
+    expect(txPool.getArchivedTxByHash(await tx3.getTxHash())).resolves.toBeUndefined();
+    expect(txPool.getArchivedTxByHash(await tx4.getTxHash())).resolves.toEqual(tx4);
+    expect(txPool.getArchivedTxByHash(await tx5.getTxHash())).resolves.toEqual(tx5);
   });
 });
