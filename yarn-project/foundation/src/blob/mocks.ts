3a8869fdcd74122c15fba250a6af8d4ae1fb6fa7
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2025-01-31 21:59:20 +0000

    fix: do not attempt to decode blob before filtering

--- yarn-project/foundation/src/blob/mocks.ts
+++ yarn-project/foundation/src/blob/mocks.ts
@@ -21,8 +21,26 @@ function encodeFirstField(length: number): Fr {
   );
 }
 
+/**
+ * Make an encoded blob with the given length
+ *
+ * This will fail deserialisation in the archiver
+ * @param length
+ * @returns
+ */
 export function makeEncodedBlob(length: number): Promise<Blob> {
   return Blob.fromFields([encodeFirstField(length + 1), ...Array.from({ length: length }, () => Fr.random())]);
+}
+
+/**
+ * Make an unencoded blob with the given length
+ *
+ * This will fail deserialisation in the archiver
+ * @param length
+ * @returns
+ */
+export function makeUnencodedBlob(length: number): Promise<Blob> {
+  return Blob.fromFields([...Array.from({ length: length }, () => Fr.random())]);
 }
 
 export function makeEncodedBlobFields(fields: Fr[]): Promise<Blob> {
