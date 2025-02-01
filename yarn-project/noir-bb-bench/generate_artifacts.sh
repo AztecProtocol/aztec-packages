ff6580bc894c081b6e70167f980acdee0e0af287
Cody <codygunton@gmail.com>
2025-02-01 03:11:23 +0000

    Fix vk gen in noir-bb-bench

--- yarn-project/noir-bb-bench/generate_artifacts.sh
+++ yarn-project/noir-bb-bench/generate_artifacts.sh
@@ -20,16 +20,15 @@ function compile {
   local filename="$name.json"
   mv $circuit_path/target/$filename artifacts/
 
-  # WORKTODO vk_as_fields
   local json_path="./artifacts/$filename"
-  local write_vk_cmd="write_vk --scheme -h 1"
-  local vk_as_fields_cmd="vk_as_fields_ultra_honk"
+  local write_vk_cmd="write_vk --scheme ultra_honk -h 1"
   local key_path="$key_dir/$name.vk.data.json"
   echo_stderr "Generating vk for circuit: $name..."
   SECONDS=0
-  local vk_cmd="jq -r '.bytecode' $json_path | base64 -d | gunzip | $BB $write_vk_cmd -b - -o - --recursive | xxd -p -c 0"
+  local _vk_cmd="jq -r '.bytecode' $json_path | base64 -d | gunzip | $BB $write_vk_cmd -b - -o - --recursive"
+    local vk_cmd="$_vk_cmd --output_type bytes | xxd -p -c 0"
   vk=$(dump_fail "$vk_cmd")
-  local vkf_cmd="echo '$vk' | xxd -r -p | $BB $vk_as_fields_cmd -k - -o -"
+  local vkf_cmd="$_vk_cmd --output_type fields"
   vk_fields=$(dump_fail "$vkf_cmd")
   jq -n --arg vk "$vk" --argjson vkf "$vk_fields" '{keyAsBytes: $vk, keyAsFields: $vkf}' > $key_path
   echo "Key output at: $key_path (${SECONDS}s)"
