53d5d1e5e7373d83f0978ec856840bd2ed9ddc75
maramihali <mara@aztecprotocol.com>
2025-02-01 19:08:34 +0000

    change file paths

--- barretenberg/acir_tests/flows/sol_honk_zk.sh
+++ barretenberg/acir_tests/flows/sol_honk_zk.sh
@@ -5,9 +5,9 @@ VFLAG=${VERBOSE:+-v}
 BFLAG="-b ./target/program.json"
 FLAGS="-c $CRS_PATH $VFLAG"
 
-export PROOF="$PWD/sol_honk_proof"
-export PROOF_AS_FIELDS="$PWD/sol_honk_proof_fields.json"
-export VK="$PWD/sol_honk_vk"
+export PROOF="$PWD/sol_honk_zk_proof"
+export PROOF_AS_FIELDS="$PWD/sol_honk_zk_proof_fields.json"
+export VK="$PWD/sol_honk_zk_vk"
 
 # Create a proof, write the solidity contract, write the proof as fields in order to extract the public inputs
 $BIN prove_ultra_keccak_honk_zk -o $PROOF $FLAGS $BFLAG
@@ -18,7 +18,7 @@ $BIN contract_ultra_honk_zk -k $VK $FLAGS -o Verifier.sol
 
 # Export the paths to the environment variables for the js test runner
 export VERIFIER_PATH="$PWD/Verifier.sol"
-export TEST_PATH=$(realpath "../../sol-test/HonkTest.sol")
+export TEST_PATH=$(realpath "../../sol-test/ZKHonkTest.sol")
 export TESTING_HONK="true"
 export HAS_ZK="true"
 
