fd91702729b51053ffc80ec6fe0c2ee7b6dd8a86
Charlie Lye <karl.lye@gmail.com>
2025-02-04 00:31:48 +0000

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -119,7 +119,7 @@ function test {
     existing_pid=$(lsof -ti :$((45730 + i)) || true)
     [ -n "$existing_pid" ] && kill -9 $existing_pid
     # TODO: I'd like to use dump_fail here, but TXE needs to exit 0 on receiving a SIGTERM.
-    (cd $root/yarn-project/txe && LOG_LEVEL=silent TXE_PORT=$((45730 + i)) yarn start) &>/dev/null &
+    (cd $root/yarn-project/txe && LOG_LEVEL=silent TXE_PORT=$((45730 + i)) yarn start) &
   done
   echo "Waiting for TXE's to start..."
   for i in $(seq 0 $((NUM_TXES-1))); do
