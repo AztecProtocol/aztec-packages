78206ec0e078522336db55510df070a264f668d9
Maxim Vezenov <mvezenov@gmail.com>
2025-02-05 11:27:56 +0000

    cleanup

--- noir/noir-repo/compiler/noirc_evaluator/src/brillig/brillig_gen/brillig_globals.rs
+++ noir/noir-repo/compiler/noirc_evaluator/src/brillig/brillig_gen/brillig_globals.rs
@@ -263,14 +263,7 @@ impl BrilligGlobals {
             for map in entry_point_allocations {
                 globals_allocations.extend(map);
             }
-        }
-        // else if let Some(globals) = self.entry_point_globals_map.get(&brillig_function_id) {
-        //     // If there is no mapping from an inner call to an entry point, that means `brillig_function_id`
-        //     // is itself an entry point and we can fetch the global allocations directly from `self.entry_point_globals_map`.
-        //     // vec![globals]
-        //     globals_allocations.extend(globals);
-        // }
-        else {
+        } else {
             unreachable!(
                 "ICE: Expected global allocation to be set for function {brillig_function_id}"
             );
