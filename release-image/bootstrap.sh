587d0beeaac0cb95af128fe5eef61ec4796c7679
ludamad <domuradical@gmail.com>
2025-02-05 18:57:33 +0000

    add bootstrap.sh to each rebuild pattern

--- release-image/bootstrap.sh
+++ release-image/bootstrap.sh
@@ -10,10 +10,13 @@ case "$cmd" in
     denoise "docker build -f release-image/Dockerfile -t aztecprotocol/aztec:$(git rev-parse HEAD) ."
     docker tag aztecprotocol/aztec:$(git rev-parse HEAD) aztecprotocol/aztec:latest
     ;;
+  "prerelease")
+    echo_header "release-image prerelease"
+    docker push aztecprotocol/aztec:$(git rev-parse HEAD)
+    ;;
   "release")
     echo_header "release-image release"
     docker tag aztecprotocol/aztec:$(git rev-parse HEAD) aztecprotocol/aztec:$REF_NAME
-    docker push aztecprotocol/aztec:$(git rev-parse HEAD)
     docker push aztecprotocol/aztec:$REF_NAME
     docker push aztecprotocol/aztec:latest
     ;;
@@ -21,3 +24,6 @@ case "$cmd" in
     echo "Unknown command: $cmd"
     exit 1
 esac
+# saleel merge
+# bb cpp version sed
+# l1 contracts release
