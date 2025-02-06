5fac8b83ac2eaf0d27ce1809f7a28ff3dd53f195
Santiago Palladino <santiago@aztecprotocol.com>
2025-02-06 11:38:58 +0000

    Remove unused transaction protocol topic name

--- yarn-project/aztec-node/src/aztec-node/server.ts
+++ yarn-project/aztec-node/src/aztec-node/server.ts
@@ -164,11 +164,6 @@ export class AztecNodeService implements AztecNode, Traceable {
 
     const archiver = await createArchiver(config, blobSinkClient, { blockUntilSync: true }, telemetry);
 
-    // we identify the P2P transaction protocol by using the rollup contract address.
-    // this may well change in future
-    const rollupAddress = config.l1Contracts.rollupAddress;
-    config.transactionProtocol = `/aztec/tx/${rollupAddress.toString()}`; // TODO! Seems not being used?
-
     // now create the merkle trees and the world state synchronizer
     const worldStateSynchronizer = await createWorldStateSynchronizer(config, archiver, telemetry);
     const proofVerifier = config.realProofs ? await BBCircuitVerifier.new(config) : new TestCircuitVerifier();
