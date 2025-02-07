e9835b5de6da00dc057699c4b320b66d05cf122b
Alex Gherghisan <alexg@aztecprotocol.com>
2025-02-07 16:24:12 +0000

    feat: add undici

---
+++ yarn-project/foundation/src/json-rpc/client/undici.ts
@@ -0,0 +1,60 @@
+import { Agent, type Dispatcher } from 'undici';
+
+import { createLogger } from '../../log/pino-logger.js';
+import { NoRetryError } from '../../retry/index.js';
+import { jsonStringify } from '../convert.js';
+
+const log = createLogger('json-rpc:json_rpc_client:undici');
+
+export { Agent };
+
+export function makeUndiciFetch(client = new Agent()) {
+  return async (
+    host: string,
+    rpcMethod: string,
+    body: any,
+    useApiEndpoints: boolean,
+    extraHeaders: Record<string, string> = {},
+    noRetry = false,
+  ) => {
+    log.trace(`JsonRpcClient.fetch: ${host} ${rpcMethod}`, { host, rpcMethod, body });
+    let resp: Dispatcher.ResponseData<string>;
+    try {
+      resp = await client.request<string>({
+        method: 'POST',
+        origin: new URL(host),
+        path: useApiEndpoints ? rpcMethod : '/',
+        body: jsonStringify(body),
+        headers: {
+          ...extraHeaders,
+          'content-type': 'application/json',
+        },
+      });
+    } catch (err) {
+      const errorMessage = `Error fetching from host ${host} with method ${rpcMethod}: ${String(err)}`;
+      throw new Error(errorMessage);
+    }
+
+    let responseJson: any;
+    const responseOk = resp.statusCode >= 200 && resp.statusCode <= 299;
+    try {
+      responseJson = await resp.body.json();
+    } catch (err) {
+      if (!responseOk) {
+        throw new Error('HTTP ' + resp.statusCode);
+      }
+      throw new Error(`Failed to parse body as JSON: ${await resp.body.text()}`);
+    }
+
+    if (!responseOk) {
+      const errorMessage = `Error ${resp.statusCode} response from server ${host} on ${rpcMethod}: ${responseJson.error.message}`;
+      if (noRetry || (resp.statusCode >= 400 && resp.statusCode < 500)) {
+        throw new NoRetryError(errorMessage);
+      } else {
+        throw new Error(errorMessage);
+      }
+    }
+
+    return responseJson;
+  };
+}
