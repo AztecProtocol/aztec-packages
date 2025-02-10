0bcdacbc1facfb036c277af26954208a40ce55a2
ludamad <adam.domurad@gmail.com>
2025-02-10 16:08:24 +0000

    fix

--- spartan/scripts/test_kind.sh
+++ spartan/scripts/test_kind.sh
@@ -45,6 +45,12 @@ if [ -z "$namespace" ]; then
   exit 1
 fi
 
+# Only check for end-to-end image if a test is specified
+if [ "$use_docker" = "true" ] && ! docker_has_image "aztecprotocol/end-to-end:$aztec_docker_tag"; then
+  echo "End-to-end Docker image not found. It needs to be built."
+  exit 1
+fi
+
 if [ "$install_metrics" = "true" ]; then
   ../bootstrap.sh metrics-kind
 fi
@@ -104,27 +110,65 @@ aztec_slot_duration=$(./read_value.sh "aztec.slotDuration" $value_yamls)
 aztec_epoch_duration=$(./read_value.sh "aztec.epochDuration" $value_yamls)
 aztec_epoch_proof_claim_window_in_l2_slots=$(./read_value.sh "aztec.epochProofClaimWindow" $value_yamls)
 
-echo "RUNNING TEST: $test"
-# Run test locally.
-export K8S="local"
-export INSTANCE_NAME="spartan"
-export SPARTAN_DIR="$(pwd)/.."
-export NAMESPACE="$namespace"
-export HOST_PXE_PORT="$forwarded_pxe_port"
-export CONTAINER_PXE_PORT="8081"
-export HOST_ETHEREUM_PORT="$forwarded_anvil_port"
-export CONTAINER_ETHEREUM_PORT="8545"
-export HOST_NODE_PORT="$forwarded_node_port"
-export CONTAINER_NODE_PORT="8080"
-export HOST_METRICS_PORT="$forwarded_metrics_port"
-export CONTAINER_METRICS_PORT="80"
-export GRAFANA_PASSWORD="$grafana_password"
-export DEBUG="${DEBUG:-""}"
-export LOG_JSON="1"
-export LOG_LEVEL="${LOG_LEVEL:-"debug; info: aztec:simulator, json-rpc"}"
-export ETHEREUM_SLOT_DURATION="$ethereum_slot_duration"
-export AZTEC_SLOT_DURATION="$aztec_slot_duration"
-export AZTEC_EPOCH_DURATION="$aztec_epoch_duration"
-export AZTEC_EPOCH_PROOF_CLAIM_WINDOW_IN_L2_SLOTS="$aztec_epoch_proof_claim_window_in_l2_slots"
+env_args=()
+if [ "$sepolia_run" = "true" ]; then
+  env_args+=(
+    -e ETHEREUM_HOST="$EXTERNAL_ETHEREUM_HOST"
+    -e SEPOLIA_RUN="true"
+  )
+fi
 
-yarn --cwd ../../yarn-project/end-to-end test --forceExit "$test"
+if [ "$use_docker" = "true" ]; then
+  echo "RUNNING TEST: $test (docker)"
+  # Run test in Docker.
+  # Note this will go away soon with the end-to-end image (which also means we deal with the duplication for now.)
+  docker run --rm --network=host \
+    -v ~/.kube:/root/.kube \
+    -e K8S=local \
+    -e INSTANCE_NAME="spartan" \
+    -e SPARTAN_DIR="/usr/src/spartan" \
+    -e NAMESPACE="$namespace" \
+    -e HOST_PXE_PORT=$forwarded_pxe_port \
+    -e CONTAINER_PXE_PORT=8081 \
+    -e HOST_ETHEREUM_PORT=$forwarded_anvil_port \
+    -e CONTAINER_ETHEREUM_PORT=8545 \
+    -e HOST_NODE_PORT=$forwarded_node_port \
+    -e CONTAINER_NODE_PORT=8080 \
+    -e HOST_METRICS_PORT=$forwarded_metrics_port \
+    -e CONTAINER_METRICS_PORT=80 \
+    -e GRAFANA_PASSWORD=$grafana_password \
+    -e DEBUG=${DEBUG:-""} \
+    -e LOG_JSON=1 \
+    -e LOG_LEVEL=${LOG_LEVEL:-"debug; info: aztec:simulator, json-rpc"} \
+    -e ETHEREUM_SLOT_DURATION=$ethereum_slot_duration \
+    -e AZTEC_SLOT_DURATION=$aztec_slot_duration \
+    -e AZTEC_EPOCH_DURATION=$aztec_epoch_duration \
+    -e AZTEC_EPOCH_PROOF_CLAIM_WINDOW_IN_L2_SLOTS=$aztec_epoch_proof_claim_window_in_l2_slots \
+    "${env_args[@]}" \
+    aztecprotocol/end-to-end:$aztec_docker_tag $test
+else
+  echo "RUNNING TEST: $test"
+  # Run test locally.
+  export K8S="local"
+  export INSTANCE_NAME="spartan"
+  export SPARTAN_DIR="$(pwd)/.."
+  export NAMESPACE="$namespace"
+  export HOST_PXE_PORT="$forwarded_pxe_port"
+  export CONTAINER_PXE_PORT="8081"
+  export HOST_ETHEREUM_PORT="$forwarded_anvil_port"
+  export CONTAINER_ETHEREUM_PORT="8545"
+  export HOST_NODE_PORT="$forwarded_node_port"
+  export CONTAINER_NODE_PORT="8080"
+  export HOST_METRICS_PORT="$forwarded_metrics_port"
+  export CONTAINER_METRICS_PORT="80"
+  export GRAFANA_PASSWORD="$grafana_password"
+  export DEBUG="${DEBUG:-""}"
+  export LOG_JSON="1"
+  export LOG_LEVEL="${LOG_LEVEL:-"debug; info: aztec:simulator, json-rpc"}"
+  export ETHEREUM_SLOT_DURATION="$ethereum_slot_duration"
+  export AZTEC_SLOT_DURATION="$aztec_slot_duration"
+  export AZTEC_EPOCH_DURATION="$aztec_epoch_duration"
+  export AZTEC_EPOCH_PROOF_CLAIM_WINDOW_IN_L2_SLOTS="$aztec_epoch_proof_claim_window_in_l2_slots"
+
+  yarn --cwd ../../yarn-project/end-to-end test --forceExit "$test"
+fi
