bc10f1790569971c18632f4d332dfb8fbe179532
esau <152162806+sklppy88@users.noreply.github.com>
2025-02-10 19:38:00 +0000

    chore: add blob lib to npm deploy (#11891)
    Adding this to the packages we deploy as it is a dep of other deployed
    packages.

--- yarn-project/publish_npm.sh
+++ yarn-project/publish_npm.sh
@@ -87,6 +87,7 @@ function deploy_package() {
 
 # New packages here should be added after the last package that they depend on
 deploy_package foundation
+deploy_package blob-lib
 deploy_package native
 deploy_package types
 deploy_package circuits.js
