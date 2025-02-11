1afddbd0712ad268bcc82931cf91bbb067766cbe
sergei iakovenko <105737703+iakovenkos@users.noreply.github.com>
2025-02-11 17:26:02 +0100

    chore: fixing the sizes of VMs in CIVC (#11793)
    The sizes of Translator and ECCVM circuits leak the amount of circuits
    that have been folded, so we need to fix them.
    
    `benchmark_client_ivc` after fixing the sizes (note that regression is
    expected, as the IPA prover is opening a dense polynomial of size 2^16
    instead of 2^15 + there is not much skipping in Sumcheck except for
    `SetRelation`)
    
    
    ECCVMProver(CircuitBuilder&)(t)          190     0.87%
    ECCVMProver::construct_proof(t)         2405    10.95%
    TranslatorProver::construct_proof(t)    1316     5.99%
    Goblin::merge(t)                         132     0.60%
    
    Total time accounted for: 21963ms/23040ms = 95.32%
    
    Benchmark on master: 
    
    ECCVMProver(CircuitBuilder&)(t)          182     0.85%
    ECCVMProver::construct_proof(t)         1612     7.51%
    TranslatorProver::construct_proof(t)    1650     7.69%
    Goblin::merge(t)                         132     0.62%
    
    Total time accounted for: 21468ms/22538ms = 95.25%

--- barretenberg/cpp/src/barretenberg/translator_vm/translator_prover.cpp
+++ barretenberg/cpp/src/barretenberg/translator_vm/translator_prover.cpp
@@ -45,11 +45,21 @@ void TranslatorProver::execute_preamble_round()
  */
 void TranslatorProver::execute_wire_and_sorted_constraints_commitments_round()
 {
-    // Commit to all wire polynomials and ordered range constraint polynomials
-    auto wire_polys = key->proving_key->polynomials.get_wires_and_ordered_range_constraints();
-    auto labels = commitment_labels.get_wires_and_ordered_range_constraints();
-    for (size_t idx = 0; idx < wire_polys.size(); ++idx) {
-        transcript->send_to_verifier(labels[idx], key->proving_key->commitment_key->commit(wire_polys[idx]));
+    // Commit to all wire polynomials, note that the wire polynomials have at most `mini_circuit_dyadic_size` non-zero
+    // values. Therefore we could commit to a subspan of that size.
+    for (const auto& [wire, label] :
+         zip_view(key->proving_key->polynomials.get_wires(), commitment_labels.get_wires())) {
+        // TODO(https://github.com/AztecProtocol/barretenberg/issues/1240) Structured Polynomials in
+        // ECCVM/Translator/MegaZK
+        PolynomialSpan<FF> wire_span = wire;
+        transcript->send_to_verifier(
+            label, key->proving_key->commitment_key->commit(wire_span.subspan(0, key->mini_circuit_dyadic_size)));
+    }
+
+    // The ordered range constraints are of full circuit size.
+    for (const auto& [ordered_range_constraint, label] : zip_view(
+             key->proving_key->polynomials.get_ordered_constraints(), commitment_labels.get_ordered_constraints())) {
+        transcript->send_to_verifier(label, key->proving_key->commitment_key->commit(ordered_range_constraint));
     }
 }
 
