a9333b87e0037d4de5af9c4ebbb76746dfa416b4
jeanmon <jean@aztecprotocol.com>
2025-02-12 11:08:38 +0000

    Adapt constraining unit tests

--- barretenberg/cpp/src/barretenberg/vm2/constraining/relations/bc_decomposition.test.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/constraining/relations/bc_decomposition.test.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "barretenberg/vm2/constraining/testing/check_relation.hpp"
+#include "barretenberg/vm2/generated/columns.hpp"
 #include "barretenberg/vm2/generated/flavor_settings.hpp"
 #include "barretenberg/vm2/generated/relations/bc_decomposition.hpp"
 #include "barretenberg/vm2/testing/macros.hpp"
