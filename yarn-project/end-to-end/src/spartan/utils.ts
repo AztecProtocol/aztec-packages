c47cd354d85e31975f98a4d9026b0185b738e9f6
Leila Wang <leizciw@gmail.com>
2025-02-13 17:40:10 +0000

    Update configs.
    Co-authored-by: Alex Gherghisan <alexghr@users.noreply.github.com>

--- yarn-project/end-to-end/src/spartan/utils.ts
+++ yarn-project/end-to-end/src/spartan/utils.ts
@@ -19,6 +19,8 @@ const k8sLocalConfigSchema = z.object({
   NAMESPACE: z.string().min(1, 'NAMESPACE env variable must be set'),
   HOST_NODE_PORT: z.coerce.number().min(1, 'HOST_NODE_PORT env variable must be set'),
   CONTAINER_NODE_PORT: z.coerce.number().default(8080),
+  HOST_SEQUENCER_PORT: z.coerce.number().min(1, 'HOST_SEQUENCER_PORT env variable must be set'),
+  CONTAINER_SEQUENCER_PORT: z.coerce.number().default(8080),
   HOST_PXE_PORT: z.coerce.number().min(1, 'HOST_PXE_PORT env variable must be set'),
   CONTAINER_PXE_PORT: z.coerce.number().default(8080),
   HOST_ETHEREUM_PORT: z.coerce.number().min(1, 'HOST_ETHEREUM_PORT env variable must be set'),
