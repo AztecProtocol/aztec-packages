9d818c42001010e1589866b806e345c642259361
ledwards2225 <l.edwards.d@gmail.com>
2025-02-14 21:13:52 +0000

    a simpler inheritence structure for both ultra and raw tables

--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/op_queue/ecc_op_queue.test.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/op_queue/ecc_op_queue.test.cpp
@@ -9,11 +9,14 @@
 using namespace bb;
 
 class ECCOpQueueTest : public ::testing::Test {
-    using scalar = fr;
+    using Curve = curve::BN254;
+    using Point = Curve::AffineElement;
+    using Scalar = fr;
+    using ECCVMOperation = bb::eccvm::VMOperation<Curve::Group>;
 
   public:
     struct MockUltraOpsTable {
-        std::array<std::vector<scalar>, 4> columns;
+        std::array<std::vector<Scalar>, 4> columns;
         void append(const UltraOp& op)
         {
             columns[0].push_back(op.op);
@@ -44,15 +47,38 @@ class ECCOpQueueTest : public ::testing::Test {
     {
         UltraOp op;
         op.op_code = NULL_OP;
-        op.op = scalar::random_element();
-        op.x_lo = scalar::random_element();
-        op.x_hi = scalar::random_element();
-        op.y_lo = scalar::random_element();
-        op.y_hi = scalar::random_element();
-        op.z_1 = scalar::random_element();
-        op.z_2 = scalar::random_element();
+        op.op = Scalar::random_element();
+        op.x_lo = Scalar::random_element();
+        op.x_hi = Scalar::random_element();
+        op.y_lo = Scalar::random_element();
+        op.y_hi = Scalar::random_element();
+        op.z_1 = Scalar::random_element();
+        op.z_2 = Scalar::random_element();
         op.return_is_infinity = false;
         return op;
+    }
+
+    struct MockRawOpsTable {
+        std::vector<ECCVMOperation> raw_ops;
+        void append(const ECCVMOperation& op) { raw_ops.push_back(op); }
+
+        MockRawOpsTable(const auto& subtable_ops)
+        {
+            for (auto& ops : std::ranges::reverse_view(subtable_ops)) {
+                for (const auto& op : ops) {
+                    append(op);
+                }
+            }
+        }
+    };
+
+    static ECCVMOperation random_raw_op()
+    {
+        return ECCVMOperation{ .mul = true,
+                               .base_point = curve::BN254::Group::affine_element::random_element(),
+                               .z1 = uint256_t(Scalar::random_element()),
+                               .z2 = uint256_t(Scalar::random_element()),
+                               .mul_scalar_full = Scalar::random_element() };
     }
 };
 
@@ -90,73 +116,7 @@ TEST(ECCOpQueueTest, InternalAccumulatorCorrectness)
     EXPECT_TRUE(op_queue.get_accumulator().is_point_at_infinity());
 }
 
-// TEST(ECCOpQueueTest, UltraOpsTableConstruction)
-// {
-//     using Fr = fr;
-
-//     constexpr size_t NUM_ROWS_PER_OP = 2; // Each ECC op is represented by two rows in the ultra ops table
-
-//     // Construct sets of ultra ops, each representing those added by a single circuit
-//     const size_t NUM_SUBTABLES = 3;
-//     std::array<std::vector<UltraOp>, NUM_SUBTABLES> subtable_ultra_ops;
-//     std::array<size_t, NUM_SUBTABLES> subtable_op_counts = { 4, 2, 7 };
-//     for (auto [subtable_ops, op_count] : zip_view(subtable_ultra_ops, subtable_op_counts)) {
-//         for (size_t i = 0; i < op_count; ++i) {
-//             subtable_ops.push_back(ECCOpQueueTest::random_ultra_op());
-//         }
-//     }
-
-//     // Construct the mock ultra ops table which contains the subtables ordered in reverse (as if prepended)
-//     ECCOpQueueTest::MockUltraOpsTable expected_ultra_ops_table(subtable_ultra_ops);
-
-//     // Construct the concatenated table internal to the op queue
-//     UltraOpsTable ultra_ops_table;
-//     for (const auto& subtable_ops : subtable_ultra_ops) {
-//         ultra_ops_table.create_new_subtable();
-//         for (const auto& op : subtable_ops) {
-//             ultra_ops_table.append_operation(op);
-//         }
-//     }
-
-//     // Compute the expected total table size as the sum of the subtable op counts times 2
-//     size_t expected_table_size = 0;
-//     for (const auto& count : subtable_op_counts) {
-//         expected_table_size += count * NUM_ROWS_PER_OP;
-//     }
-
-//     // Check that the ultra ops table internal to the op queue has the correct size
-//     EXPECT_EQ(ultra_ops_table.size(), expected_table_size);
-
-//     // expected_ultra_ops_table.columns[2][3] += 1;
-
-//     // Check that the ultra ops table constructed by the op queue matches the expected table using row iterator
-//     std::array<std::vector<Fr>, 4> columns;
-//     for (const auto& row : ultra_ops_table) {
-//         for (size_t i = 0; i < 4; ++i) {
-//             columns[i].push_back(row[i]);
-//         }
-//     }
-
-//     EXPECT_EQ(expected_ultra_ops_table.columns, columns);
-
-//     // Construct polynomials corresponding to the columns of the ultra ops table
-//     std::array<Polynomial<Fr>, 4> ultra_ops_table_polynomials;
-//     std::array<std::span<fr>, 4> column_spans;
-//     for (auto [column_span, column] : zip_view(column_spans, ultra_ops_table_polynomials)) {
-//         column = Polynomial<Fr>(expected_table_size);
-//         column_span = column.coeffs();
-//     }
-//     ultra_ops_table.populate_columns(column_spans);
-
-//     // Check that the ultra ops table constructed by the op queue matches the expected table
-//     for (auto [expected_column, poly] : zip_view(expected_ultra_ops_table.columns, ultra_ops_table_polynomials)) {
-//         for (auto [expected_value, value] : zip_view(expected_column, poly.coeffs())) {
-//             EXPECT_EQ(expected_value, value);
-//         }
-//     }
-// }
-
-TEST(ECCOpQueueTest, NewUltraOpsTableConstruction)
+TEST(ECCOpQueueTest, UltraOpsTableConstruction)
 {
     using Fr = fr;
 
@@ -205,5 +165,42 @@ TEST(ECCOpQueueTest, NewUltraOpsTableConstruction)
         for (auto [expected_value, value] : zip_view(expected_column, poly.coeffs())) {
             EXPECT_EQ(expected_value, value);
         }
+    }
+}
+
+TEST(ECCOpQueueTest, RawOpsTableConstruction)
+{
+    using ECCVMOperation = bb::eccvm::VMOperation<curve::BN254::Group>;
+
+    // Construct sets of ultra ops, each representing those added by a single circuit
+    const size_t NUM_SUBTABLES = 3;
+    std::array<std::vector<ECCVMOperation>, NUM_SUBTABLES> subtable_raw_ops;
+    std::array<size_t, NUM_SUBTABLES> subtable_op_counts = { 4, 2, 7 };
+    for (auto [subtable_ops, op_count] : zip_view(subtable_raw_ops, subtable_op_counts)) {
+        for (size_t i = 0; i < op_count; ++i) {
+            subtable_ops.push_back(ECCOpQueueTest::random_raw_op());
+        }
+    }
+
+    // Construct the mock ultra ops table which contains the subtables ordered in reverse (as if prepended)
+    ECCOpQueueTest::MockRawOpsTable expected_raw_ops_table(subtable_raw_ops);
+
+    // Construct the concatenated raw ops table
+    RawEccOpsTable raw_ops_table;
+    for (const auto& subtable_ops : subtable_raw_ops) {
+        raw_ops_table.create_new_subtable();
+        for (const auto& op : subtable_ops) {
+            raw_ops_table.push(op);
+        }
+    }
+
+    // Check that the table has the correct size
+    auto expected_num_ops = std::accumulate(subtable_op_counts.begin(), subtable_op_counts.end(), size_t(0));
+    EXPECT_EQ(raw_ops_table.size(), expected_num_ops);
+
+    // Check that the manually constructed mock table matches the genuine table
+    size_t idx = 0;
+    for (const auto& op : raw_ops_table) {
+        EXPECT_EQ(expected_raw_ops_table.raw_ops[idx++], op);
     }
 }
