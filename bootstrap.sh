130938e9cf4d24219dd2cea7cf1fca841966a32a
ludamad <domuradical@gmail.com>
2025-02-14 00:38:13 +0000

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -184,11 +184,11 @@ function release_github {
   # Add an easy link for comparing to previous release.
   local compare_link=""
   if gh release view "v$CURRENT_VERSION" &>/dev/null; then
-    compare_link=$(echo -e "See changes: https://github.com/$repo/compare/v${CURRENT_VERSION}...${COMMIT_HASH}")
+    compare_link=$(echo -e "See changes: https://github.com/AztecProtocol/aztec-packages/compare/v${CURRENT_VERSION}...${COMMIT_HASH}")
   fi
   # Legacy releases. TODO: Eventually remove.
   if gh release view "aztec-packages-v$CURRENT_VERSION" &>/dev/null; then
-    compare_link=$(echo -e "See changes: https://github.com/$repo/compare/aztec-packages-v${CURRENT_VERSION}...${COMMIT_HASH}")
+    compare_link=$(echo -e "See changes: https://github.com/AztecProtocol/aztec-packages/compare/aztec-packages-v${CURRENT_VERSION}...${COMMIT_HASH}")
   fi
   # Ensure we have a commit release.
   if ! gh release view "$REF_NAME" &>/dev/null; then
