9531bace14545b426519b339a5a5cf2cfcfd3754
benesjan <janbenes1234@gmail.com>
2025-02-19 13:15:29 +0000

    test

--- yarn-project/circuits.js/src/structs/shared_mutable/shared_mutable_values.test.ts
+++ yarn-project/circuits.js/src/structs/shared_mutable/shared_mutable_values.test.ts
@@ -1,4 +1,5 @@
 import { Fr } from '@aztec/foundation/fields';
+import { updateInlineTestData } from '@aztec/foundation/testing/files';
 
 import { ScheduledDelayChange } from './scheduled_delay_change.js';
 import { ScheduledValueChange } from './scheduled_value_change.js';
@@ -26,5 +27,22 @@ describe('SharedMutableValues', () => {
       new ScheduledDelayChange(5, 6, 7),
     );
     expect(values).toEqual(SharedMutableValues.fromFields(values.toFields()));
+  });
+
+  it('packed shared mutable values match Noir', () => {
+    const values = new SharedMutableValues(
+      new ScheduledValueChange([new Fr(1), new Fr(2)], [new Fr(3), new Fr(4)], 50),
+      new ScheduledDelayChange(1, 2, 50),
+    );
+
+    const packed = values.toFields();
+    const packedStr = `[${packed.map(f => f.toString()).join(',')}]`;
+
+    // Run with AZTEC_GENERATE_TEST_DATA=1 to update noir test data
+    updateInlineTestData(
+      'noir-projects/noir-protocol-circuits/crates/types/src/shared_mutable/shared_mutable_values/test.nr',
+      'packed_smv_from_typescript',
+      packedStr,
+    );
   });
 });
