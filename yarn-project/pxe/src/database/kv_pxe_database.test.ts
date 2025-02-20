b088422d7a1d04b7b666e34e631d783dac01612e
Alex Gherghisan <alexghr@users.noreply.github.com>
2025-02-20 10:52:10 +0000

    refactor: replace kv-store backend in pxe, key store and wallet (#12087)
    Fix #11658

--- yarn-project/pxe/src/database/kv_pxe_database.test.ts
+++ yarn-project/pxe/src/database/kv_pxe_database.test.ts
@@ -1,4 +1,4 @@
-import { openTmpStore } from '@aztec/kv-store/lmdb';
+import { openTmpStore } from '@aztec/kv-store/lmdb-v2';
 
 import { KVPxeDatabase } from './kv_pxe_database.js';
 import { describePxeDatabase } from './pxe_database_test_suite.js';
@@ -7,7 +7,7 @@ describe('KVPxeDatabase', () => {
   let database: KVPxeDatabase;
 
   beforeEach(async () => {
-    database = await KVPxeDatabase.create(openTmpStore());
+    database = await KVPxeDatabase.create(await openTmpStore('test'));
   });
 
   describePxeDatabase(() => database);
