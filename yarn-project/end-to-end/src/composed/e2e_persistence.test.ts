5cdb44fdc42511068fd12f4275a60b232a4da79b
thunkar <gregojquiros@gmail.com>
2025-02-21 18:21:24 +0000

    e2e

--- yarn-project/end-to-end/src/composed/e2e_persistence.test.ts
+++ yarn-project/end-to-end/src/composed/e2e_persistence.test.ts
@@ -1,9 +1,10 @@
 import { getSchnorrAccount, getSchnorrWallet } from '@aztec/accounts/schnorr';
 import { type InitialAccountData, deployFundedSchnorrAccount } from '@aztec/accounts/testing';
 import { type AccountWallet, type ContractInstanceWithAddress, type TxHash, computeSecretHash } from '@aztec/aztec.js';
-import { type AztecAddress, Fr } from '@aztec/circuits.js';
+import { type AztecAddress } from '@aztec/circuits.js/aztec-address';
 import { MAX_NOTE_HASHES_PER_TX } from '@aztec/constants';
 import { type DeployL1Contracts } from '@aztec/ethereum';
+import { Fr } from '@aztec/foundation/fields';
 // We use TokenBlacklist because we want to test the persistence of manually added notes and standard token no longer
 // implements TransparentNote shield flow.
 import { TokenBlacklistContract } from '@aztec/noir-contracts.js/TokenBlacklist';
