24ef1f851813fd656b3b3d7e5ffc5fed97d2c611
Amin Sammara <aminsammara1@gmail.com>
2025-02-23 06:22:02 -0500

    Deleted LOG_ELAPSED_TIME env variable

--- yarn-project/foundation/src/config/env_var.ts
+++ yarn-project/foundation/src/config/env_var.ts
@@ -59,7 +59,6 @@ export type EnvVar =
   | 'L1_CONSENSUS_HOST_API_KEY' // The API key for the L1 consensus client. Added end of URL as "?key=<api-key>" unless a header is defined
   | 'L1_CONSENSUS_HOST_API_KEY_HEADER' // The header name for the L1 consensus client API key, if needed. Added as "<api-key-header>: <api-key>
   | 'L1_PRIVATE_KEY' // Private key of account for publishing L1 contracts in aztec start. TODO: Change L1_PRIVATE_KEY in Sean's get-private-key.sh
-  | 'LOG_ELAPSED_TIME' // TODO: delete this env variable, it's unused.
   | 'LOG_JSON' // using pino-pretty for console logging if LOG_JSON is not set. vanilla stdio if it is. TODO: Check if JSON logging is implemented.
   | 'LOG_MULTILINE' //pino-pretty options: Whether to print each log on a single line or not. (errors will still be multi-line). TODO: Rename this to Single Line.
   | 'LOG_LEVEL' // controls the default log level. Use `LOG_LEVEL` with one of `silent`, `fatal`, `error`, `warn`, `info`, `verbose`, `debug`, or `trace`. To tweak log levels per module, add a list of module prefixes with their overridden level. For example, LOG_LEVEL="info; verbose: aztec:sequencer, aztec:archiver; debug: aztec:kv-store" sets `info` as the default log level, `verbose` for the sequencer and archiver, and `debug` for the kv-store. Module name match is done by prefix.
