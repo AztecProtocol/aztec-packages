4063f7a86ce30679de49da714995bdd5fff348d6
benesjan <janbenes1234@gmail.com>
2025-02-24 12:05:17 +0000

    fixes

--- yarn-project/aztec.js/src/fee/private_fee_payment_method.ts
+++ yarn-project/aztec.js/src/fee/private_fee_payment_method.ts
@@ -87,14 +87,14 @@ export class PrivateFeePaymentMethod implements FeePaymentMethod {
   async getFunctionCalls(gasSettings: GasSettings): Promise<FunctionCall[]> {
     // We assume 1:1 exchange rate between fee juice and token. But in reality you would need to convert feeLimit
     // (maxFee) to be in token denomination.
-    const maxFee = this.setMaxFeeToOne ? 1n : gasSettings.getFeeLimit().toBigInt();
+    const maxFee = this.setMaxFeeToOne ? Fr.ONE : gasSettings.getFeeLimit();
     const nonce = Fr.random();
 
     await this.wallet.createAuthWit({
       caller: this.paymentContract,
       action: {
         name: 'transfer_to_public',
-        args: [this.wallet.getAddress().toField(), this.paymentContract.toField(), new Fr(maxFee), nonce],
+        args: [this.wallet.getAddress().toField(), this.paymentContract.toField(), maxFee, nonce],
         selector: await FunctionSelector.fromSignature('transfer_to_public((Field),(Field),Field,Field)'),
         type: FunctionType.PRIVATE,
         isStatic: false,
@@ -110,7 +110,7 @@ export class PrivateFeePaymentMethod implements FeePaymentMethod {
         selector: await FunctionSelector.fromSignature('fee_entrypoint_private(Field,Field)'),
         type: FunctionType.PRIVATE,
         isStatic: false,
-        args: [new Fr(maxFee), nonce],
+        args: [maxFee, nonce],
         returnTypes: [],
       },
     ];
