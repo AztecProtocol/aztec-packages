3e21168a053edc69651a068b0f7e64526f42bbd0
github-action-benchmark <github@users.noreply.github.com>
2025-02-24 10:35:51 +0000

    add P2P Testbench (customSmallerIsBetter) benchmark result for d742834bb55548a1bc94a773b1eb40e8c9b397ae

---
+++ dev/p2p-bench/data.js
@@ -0,0 +1,103 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1740393351803,
+  "repoUrl": "https://github.com/AztecProtocol/aztec-packages",
+  "entries": {
+    "P2P Testbench": [
+      {
+        "commit": {
+          "author": {
+            "name": "AztecProtocol",
+            "username": "AztecProtocol"
+          },
+          "committer": {
+            "name": "AztecProtocol",
+            "username": "AztecProtocol"
+          },
+          "id": "d742834bb55548a1bc94a773b1eb40e8c9b397ae",
+          "message": "feat(p2p): gossipsub scoring adjustments + testbench in ci",
+          "timestamp": "2025-02-23T02:27:21Z",
+          "url": "https://github.com/AztecProtocol/aztec-packages/pull/12075/commits/d742834bb55548a1bc94a773b1eb40e8c9b397ae"
+        },
+        "date": 1740393350532,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "degree-1-strict - numberReceived",
+            "value": 4,
+            "unit": "ma"
+          },
+          {
+            "name": "degree-1-strict - minDelay",
+            "value": 48,
+            "unit": "ma"
+          },
+          {
+            "name": "degree-1-strict - maxDelay",
+            "value": 2474,
+            "unit": "ma"
+          },
+          {
+            "name": "degree-1-strict - averageDelay",
+            "value": 991.75,
+            "unit": "ma"
+          },
+          {
+            "name": "degree-1-strict - medianDelay",
+            "value": 1074,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-100-nodes - numberReceived",
+            "value": 16,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-100-nodes - minDelay",
+            "value": 61,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-100-nodes - maxDelay",
+            "value": 3997,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-100-nodes - averageDelay",
+            "value": 410,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-100-nodes - medianDelay",
+            "value": 120,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-50-nodes - numberReceived",
+            "value": 27,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-50-nodes - minDelay",
+            "value": 51,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-50-nodes - maxDelay",
+            "value": 7336,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-50-nodes - averageDelay",
+            "value": 4271.78,
+            "unit": "ma"
+          },
+          {
+            "name": "normal-degree-50-nodes - medianDelay",
+            "value": 4733,
+            "unit": "ma"
+          }
+        ]
+      }
+    ]
+  }
+}
