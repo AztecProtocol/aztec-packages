d82101f73ebb803b199990657cb42b8824114ce7
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2025-02-26 16:06:29 +0000

    refactor: getManaBaseFeeComponentsAt are zeros when not canonical (#12251)

--- l1-contracts/src/core/libraries/Errors.sol
+++ l1-contracts/src/core/libraries/Errors.sol
@@ -73,7 +73,6 @@ library Errors {
   error Rollup__TryingToProveNonExistingBlock(); // 0x34ef4954
   error Rollup__UnavailableTxs(bytes32 txsHash); // 0x414906c3
   error Rollup__NonZeroDaFee(); // 0xd9c75f52
-  error Rollup__NonZeroL2Fee(); // 0x7e728abc
   error Rollup__InvalidBasisPointFee(uint256 basisPointFee); // 0x4292d136
   error Rollup__InvalidManaBaseFee(uint256 expected, uint256 actual); // 0x73b6d896
   error Rollup__StartAndEndNotSameEpoch(Epoch start, Epoch end);
