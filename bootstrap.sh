7b09f49f7a1cb7cdb97b00722b58f58233a6514d
thunkar <gregojquiros@gmail.com>
2025-03-03 08:07:30 +0000

    ensure playground tests run

--- bootstrap.sh
+++ bootstrap.sh
@@ -107,7 +107,7 @@ function install_hooks {
 function test_cmds {
   if [ "$#" -eq 0 ]; then
     # Ordered with longest running first, to ensure they get scheduled earliest.
-    set -- spartan yarn-project/end-to-end aztec-up yarn-project noir-projects boxes barretenberg l1-contracts noir
+    set -- spartan yarn-project/end-to-end aztec-up yarn-project noir-projects boxes playground barretenberg l1-contracts noir
   fi
   parallel -k --line-buffer './{}/bootstrap.sh test_cmds 2>/dev/null' ::: $@ | filter_test_cmds
 }
@@ -271,6 +271,7 @@ function release_commit {
     yarn-project
     # Should publish at least one of our boxes to it's own repo.
     #boxes
+    playground
     docs
     release-image
   )
