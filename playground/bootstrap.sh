6f4f11438c9621a3a57b735cfa584d4d8803dde4
ludamad <domuradical@gmail.com>
2025-03-04 16:36:18 +0000

    fix

--- playground/bootstrap.sh
+++ playground/bootstrap.sh
@@ -32,7 +32,6 @@ function test_cmds {
 
 function release {
   echo_header "playground release"
-  yarn netlify build
   if [ $(dist_tag) != "latest" ]; then
     # TODO attach to github release
     do_or_dryrun yarn netlify deploy --site aztec-docs-dev --dir=dist
