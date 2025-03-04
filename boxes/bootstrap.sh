2126eb63cf95cd393d8e99d39ac4d44e3c737478
ludamad <domuradical@gmail.com>
2025-03-04 01:10:16 +0000

    idempotency refix

--- boxes/bootstrap.sh
+++ boxes/bootstrap.sh
@@ -87,9 +87,15 @@ function release_git_push {
     git checkout -b "$branch_name"
   fi
 
-  git add .
-  git commit -m "Release $tag_name." >/dev/null
-  git tag -a "$tag_name" -m "Release $tag_name."
+  if git rev-parse "$tag_name" >/dev/null 2>&1; then
+    echo "Tag $tag_name already exists. Skipping tag creation."
+  else
+    git add .
+    git commit -m "Release $tag_name." >/dev/null
+    git tag -a "$tag_name" -m "Release $tag_name."
+    do_or_dryrun git push origin "$branch_name" --quiet
+    do_or_dryrun git push origin --quiet --force "$tag_name" --tags
+  fi
 
   do_or_dryrun git push origin "$branch_name" --quiet
   do_or_dryrun git push origin --quiet --force "$tag_name" --tags
