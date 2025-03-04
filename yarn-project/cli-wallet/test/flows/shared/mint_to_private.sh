14b9df8850df3a0b8151a924532bf1a4862822e3
Leila Wang <leizciw@gmail.com>
2025-03-04 22:18:01 +0000

    fix: update cli payment method option (#12423)
    - Only allow to configure `feePayer` for the `--payment` option of
    `create-account` and `deploy-account`.
      - If `feePayer` is set for other commands, it will simply be ignored.
    - Add descriptions for the parameters of  the `--payment` option.

--- yarn-project/cli-wallet/test/flows/shared/mint_to_private.sh
+++ yarn-project/cli-wallet/test/flows/shared/mint_to_private.sh
@@ -1,7 +1,5 @@
-section "Deploying contracts and minting tokens privately"
+section "Minting tokens privately"
 
-aztec-wallet create-account -a main
-aztec-wallet deploy token_contract@Token --args accounts:main Test TST 18 -f main -a token
 aztec-wallet send mint_to_private -ca token --args accounts:main accounts:main $1 -f main
 
 RESULT_MAIN=$(aztec-wallet simulate balance_of_private -ca token --args accounts:main -f main | grep "Simulation result:" | awk '{print $3}')
