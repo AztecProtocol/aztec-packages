8f08bb3985135b02686c9155306f7ce4780c066f
benesjan <janbenes1234@gmail.com>
2025-03-06 15:31:32 +0000

    WIP

--- yarn-project/noir-contracts.js/scripts/copy-fee-contracts.sh
+++ yarn-project/noir-contracts.js/scripts/copy-fee-contracts.sh
@@ -5,14 +5,6 @@ set -eo pipefail
 mkdir -p ../aztec.js/src/fee/contracts/artifacts
 mkdir -p ../aztec.js/src/fee/contracts/typescript_artifacts
 
-# Format the JSON contract artifacts
-run -T prettier --write ./artifacts/token_contract-Token.json
-run -T prettier --write ./artifacts/fpc_contract-FPC.json
-
-# Format the TypeScript artifacts
-run -T prettier --write ./src/Token.ts
-run -T prettier --write ./src/FPC.ts
-
 # Copy contract artifacts
 cp ./artifacts/token_contract-Token.json ../aztec.js/src/fee/contracts/artifacts/
 cp ./artifacts/fpc_contract-FPC.json ../aztec.js/src/fee/contracts/artifacts/
