5d5f0fb4efe463e9fa9e5c37c51f8d15a0edc9bc
thunkar <gregojquiros@gmail.com>
2025-03-06 14:18:00 +0000

    fmt

--- yarn-project/txe/src/oracle/txe_oracle.ts
+++ yarn-project/txe/src/oracle/txe_oracle.ts
@@ -17,7 +17,7 @@ import { type Logger, applyStringFormatting } from '@aztec/foundation/log';
 import { Timer } from '@aztec/foundation/timer';
 import { KeyStore } from '@aztec/key-store';
 import type { AztecAsyncKVStore } from '@aztec/kv-store';
-import { type ProtocolContract, protocolContractNames } from '@aztec/protocol-contracts';
+import { type ProtocolContract } from '@aztec/protocol-contracts';
 import { BundledProtocolContractsProvider } from '@aztec/protocol-contracts/providers/bundle';
 import {
   AddressDataProvider,
@@ -185,7 +185,6 @@ export class TXE implements TypedOracle {
     const accountDataProvider = new TXEAccountDataProvider(store);
 
     // Register protocol contracts.
-    const provider = new BundledProtocolContractsProvider();
     for (const { contractClass, instance, artifact } of protocolContracts) {
       await contractDataProvider.addContractArtifact(contractClass.id, artifact);
       await contractDataProvider.addContractInstance(instance);
