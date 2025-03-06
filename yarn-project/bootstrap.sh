AztecProtocol/aztec-packages

commit cca368a7610994f4f7395c30dc346f957d0838e4

chore: Do not allow changes to package json during CI yarn install (#12125)
Running `yarn install` will format the package.json for all packages in
the workspace. This will cause `cache_content_hash` to fail on CI due to
changed files, which (before #12124) caused the build to terminate
abruptly (see
[here](https://github.com/AztecProtocol/aztec-packages/actions/runs/13422411604/job/37497960586?pr=12096#step:7:109)
for an example).

This PR adds all package.json files to yarn's `immutablePatterns`, so if
it detects that the install process changed them it, it will fail with
`The checksum for **/package.json has been modified by this install,
which is explicitly forbidden.` and a nice exit code, which should be
visible on the CI run. This only applies to CI runs.

yarn-project/.yarnrc.yml
@@ -7,3 +7,7 @@ logFilters:
     level: discard
 
 nodeLinker: node-modules
+
+# Do not allow 'yarn install' on CI to format package.json files,
+# otherwise it will change their hash and bust the cache
+immutablePatterns: ['**/package.json']

yarn-project/bootstrap.sh
@@ -93,8 +93,10 @@ function build {
   echo_header "yarn-project build"
   denoise "./bootstrap.sh clean-lite"
   if [ "${CI:-0}" = 1 ]; then
-    # If in CI mode, retry as bcrypto can sometimes fail mysteriously and we don't expect actual yarn install errors.
-    denoise "retry yarn install"
+    # If in CI mode, retry as bcrypto can sometimes fail mysteriously.
+    # We set immutable since we don't expect the yarn.lock to change. Note that we have also added all package.json
+    # files to yarn immutablePatterns, so if they are also changed, this step will fail.
+    denoise "retry yarn install --immutable"
   else
     denoise "yarn install"
   fi

commit 9f9d1a815d506f1d851286ac2511c3780f1677a2

chore: Fix yarn install immutable issues
We introduced two issues in #12125 when adding the immutable patterns to
yarn install:

- If `immutablePatterns` is set, yarn sets the `immutable` flag even
  outside CI (contradicting its documentation). This is fixed by
explicitly setting `--no-immutable` on the non-CI branch of install.

- The glob `**/package.json` inadvertently matched package.json files
  within `node_modules`, so installing them created the new files, which
means they were flagged as changed. This is fixed by having a less eager
glob.

Fixes #12538

yarn-project/.yarnrc.yml
@@ -10,4 +10,4 @@ nodeLinker: node-modules
 
 # Do not allow 'yarn install' on CI to format package.json files,
 # otherwise it will change their hash and bust the cache
-immutablePatterns: ['**/package.json']
+immutablePatterns: ['package.json', '*/package.json']

yarn-project/bootstrap.sh
@@ -92,7 +92,7 @@ function build {
     # files to yarn immutablePatterns, so if they are also changed, this step will fail.
     denoise "retry yarn install --immutable"
   else
-    denoise "yarn install"
+    denoise "yarn install --no-immutable"
   fi
   denoise "compile_all"
   echo -e "${green}Yarn project successfully built!${reset}"