2e599b43b28ab40200b8440488975cec2bf38b74
benesjan <janbenes1234@gmail.com>
2025-03-06 14:40:24 +0000

    fix

--- yarn-project/noir-contracts.js/scripts/copy-fee-contracts.sh
+++ yarn-project/noir-contracts.js/scripts/copy-fee-contracts.sh
@@ -2,13 +2,13 @@
 set -eo pipefail
 
 # Create destination directory if it doesn't exist
-mkdir -p ../../aztec.js/src/fee/contracts/artifacts
-mkdir -p ../../aztec.js/src/fee/contracts/typescript_artifacts
+mkdir -p ../aztec.js/src/fee/contracts/artifacts
+mkdir -p ../aztec.js/src/fee/contracts/typescript_artifacts
 
 # Copy TypeScript contract files
-cp ../src/Token.ts ../../aztec.js/src/fee/contracts/typescript_artifacts/
-cp ../src/FPC.ts ../../aztec.js/src/fee/contracts/typescript_artifacts/
+cp ./src/Token.ts ../aztec.js/src/fee/contracts/typescript_artifacts/
+cp ./src/FPC.ts ../aztec.js/src/fee/contracts/typescript_artifacts/
 
 # Copy contract artifacts
-cp ../artifacts/token_contract-Token.json ../../aztec.js/src/fee/contracts/artifacts/
-cp ../artifacts/fpc_contract-FPC.json ../../aztec.js/src/fee/contracts/artifacts/
+cp ./artifacts/token_contract-Token.json ../aztec.js/src/fee/contracts/artifacts/
+cp ./artifacts/fpc_contract-FPC.json ../aztec.js/src/fee/contracts/artifacts/
