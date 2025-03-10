8573344c8b3fd93bcda11bc5486e63a428417b34
Maddiaa <47148561+Maddiaa0@users.noreply.github.com>
2025-03-10 20:29:19 +0000

    Update yarn-project/p2p/src/services/discv5/discV5_service.ts

--- yarn-project/p2p/src/services/discv5/discV5_service.ts
+++ yarn-project/p2p/src/services/discv5/discV5_service.ts
@@ -117,7 +117,7 @@ export class DiscV5Service extends EventEmitter implements PeerDiscoveryService 
     // We want to update our tcp port to match the udp port
     const multiAddrTcp = multiaddr(convertToMultiaddr(m.nodeAddress().address, this.config.p2pPort, 'tcp'));
     this.enr.setLocationMultiaddr(multiAddrTcp);
-    this.logger.info('\n\n\n\nMultiaddr updated', { multiaddr: multiaddr.toString() });
+    this.logger.info('Multiaddr updated', { multiaddr: multiaddr.toString() });
   }
 
   public async start(): Promise<void> {
