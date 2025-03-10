0bf1001666c43b364ac30e9ec06089e5bf8c2118
sirasistant <sirasistant@gmail.com>
2025-03-10 09:34:08 +0000

    compute p_limbs_per_radix in comptime

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/to_radix_trace.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/to_radix_trace.cpp
@@ -1,11 +1,12 @@
-#include "barretenberg/numeric/uint256/uint256.hpp"
-#include "barretenberg/vm2/common/to_radix.hpp"
-#include "barretenberg/vm2/simulation/events/to_radix_event.hpp"
-
-#include "barretenberg/vm2/common/aztec_types.hpp"
-#include "barretenberg/vm2/simulation/events/event_emitter.hpp"
 #include "barretenberg/vm2/tracegen/to_radix_trace.hpp"
+
 #include <cassert>
+
+#include "barretenberg/numeric/uint256/uint256.hpp"
+#include "barretenberg/vm2/common/aztec_types.hpp"
+#include "barretenberg/vm2/common/to_radix.hpp"
+#include "barretenberg/vm2/simulation/events/event_emitter.hpp"
+#include "barretenberg/vm2/simulation/events/to_radix_event.hpp"
 
 namespace bb::avm2::tracegen {
 
