7d8a470dcac826ac513e77bc56dc467d7b7b2a95
aakoshh <akosh@aztecprotocol.com>
2025-03-12 12:34:35 +0000

    Native build seems to work

--- barretenberg/cpp/cmake/protobuf.cmake
+++ barretenberg/cpp/cmake/protobuf.cmake
@@ -4,8 +4,22 @@
 # See docs at https://cmake.org/cmake/help/latest/module/FindProtobuf.html
 # See source at https://fossies.org/linux/cmake/Modules/FindProtobuf.cmake or /usr/share/cmake-3.28/Modules/FindProtobuf.cmake
 
-include(FindProtobuf)
-find_package(Protobuf REQUIRED)
+# include(FindProtobuf)
+
+#list(APPEND CMAKE_PREFIX_PATH "/usr/local/lib/cmake/protobuf")
+set(Protobuf_DIR "/usr/local/lib/cmake/protobuf")
+
+# TODO: This errors saying it cannot find `Protobuf_INCLUDE_DIR`, unless we build protobuf from source.
+# find_package(Protobuf REQUIRED)
+find_package(Protobuf CONFIG REQUIRED)
+
+message("   --> PROTOBUF Found: ${Protobuf_FOUND}")
+message("   --> PROTOBUF VERSION: ${Protobuf_VERSION}")
+message("   --> PROTOBUF LIB: ${PROTOBUF_LIBRARIES}")
+message("   --> PROTOBUF INCLUDE: ${PROTOBUF_INCLUDE_DIRS}")
+
+# set(Protobuf_INCLUDE_DIR "/usr/include/google/protobuf")
+# set(Protobuf_INCLUDE_DIRS "/usr/include/google/protobuf")
 include_directories(${Protobuf_INCLUDE_DIRS})
 
 # Add this to what needs to use protobuf:
