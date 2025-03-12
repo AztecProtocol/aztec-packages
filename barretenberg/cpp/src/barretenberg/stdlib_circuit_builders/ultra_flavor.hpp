fc7d6db4b36ff24950ffda7724fb439073fa8ac0
jeanmon <jean@aztec-labs.com>
2025-03-12 13:26:15 +0000

    Addressing review feedback

--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/ultra_flavor.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/ultra_flavor.hpp
@@ -509,7 +509,7 @@ class UltraFlavor {
             PROFILE_THIS_NAME("PartiallyEvaluatedMultivariates constructor");
             for (auto [poly, full_poly] : zip_view(get_all(), full_polynomials.get_all())) {
                 // After the initial sumcheck round, the new size is CEIL(size/2).
-                size_t desired_size = std::min(full_poly.end_index() / 2 + full_poly.end_index() % 2, circuit_size / 2);
+                size_t desired_size = full_poly.end_index() / 2 + full_poly.end_index() % 2;
                 poly = Polynomial(desired_size, circuit_size / 2);
             }
         }
