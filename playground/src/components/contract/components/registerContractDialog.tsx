6345158022a55ea1e0ccdd874d12bbcb39d3c639
Miranda Wood <miranda@aztecprotocol.com>
2025-03-12 13:31:03 +0000

    feat: remove public bytecode from ts ContractArtifact (#12653)
    After discussions from #12081 this PR:
    
    - Removes public bytecode from the ts `ContractArtifact` class (it is
    not required and carries around sometimes very large bytecode)
    - Removes optional `aztecNrVersion`
    - Updates `artifact_hash` documentation

--- playground/src/components/contract/components/registerContractDialog.tsx
+++ playground/src/components/contract/components/registerContractDialog.tsx
@@ -41,13 +41,7 @@ export function RegisterContractDialog({
   const register = async () => {
     setRegistering(true);
 
-    const nodeInfo = await wallet.getNodeInfo();
-    const expectedAztecNrVersion = `v${nodeInfo.nodeVersion}`;
-    if (contractArtifact.aztecNrVersion && contractArtifact.aztecNrVersion !== expectedAztecNrVersion) {
-      throw new Error(
-        `Contract was compiled with a different version of Aztec.nr: ${contractArtifact.aztecNrVersion}. Consider updating Aztec.nr to ${expectedAztecNrVersion}`,
-      );
-    }
+    // TODO(#12081): Add contractArtifact.noirVersion and check here (via Noir.lock)?
 
     const contractInstance = await node.getContract(AztecAddress.fromString(address));
 
