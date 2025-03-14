899a67ad6abe3e61cc1f595ce9aee388c3ebb55d
ludamad <domuradical@gmail.com>
2025-03-14 04:33:20 +0000

    fix

--- yarn-project/ivc-integration/src/native_client_ivc_integration.test.ts
+++ yarn-project/ivc-integration/src/native_client_ivc_integration.test.ts
@@ -9,6 +9,7 @@ import { createLogger } from '@aztec/foundation/log';
 import type { ClientIvcProof } from '@aztec/stdlib/proofs';
 
 import { jest } from '@jest/globals';
+import { encode } from '@msgpack/msgpack';
 import { promises as fs } from 'fs';
 import os from 'os';
 import path from 'path';
