6e03c5c92a3d0acd30c75d4eca6b4fbf0bab52bf
aakoshh <akosh@aztecprotocol.com>
2025-03-18 10:18:40 +0000

    Change sync-in-fixup.sh to use Mac compatible option

--- noir/scripts/sync-out-fixup.sh
+++ noir/scripts/sync-out-fixup.sh
@@ -14,5 +14,5 @@ cd $(dirname $0)/../noir-repo
 # YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
 
 # Add requirement for `wasm-opt` to be installed
-sed -i "s/^#require_command wasm-opt/require_command wasm-opt/" ./tooling/noirc_abi_wasm/build.sh
-sed -i "s/^#require_command wasm-opt/require_command wasm-opt/" ./acvm-repo/acvm_js/build.sh
+sed -i.bak "s/^#require_command wasm-opt/require_command wasm-opt/" ./tooling/noirc_abi_wasm/build.sh
+sed -i.bak "s/^#require_command wasm-opt/require_command wasm-opt/" ./acvm-repo/acvm_js/build.sh
