417a5f3aee3817879bf604a44524cd766014bb16
github-action-benchmark <github@users.noreply.github.com>
2025-03-20 17:00:33 +0000

    add End-to-end Benchmark (customSmallerIsBetter) benchmark result for 13aa4f5fabadcd18daae30bac3955145eb44837e

--- dev/e2e-bench/data.js
+++ dev/e2e-bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742488142530,
+  "lastUpdate": 1742490033631,
   "repoUrl": "https://github.com/AztecProtocol/aztec-packages",
   "entries": {
     "End-to-end Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "santiago@aztecprotocol.com",
-            "name": "Santiago Palladino",
-            "username": "spalladino"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "946a418d138c1b2bee3a5dc14096616a902cc0b7",
-          "message": "chore!: Attestation collection times out based on sequencer timetable (#11248)\n\nRemoves the env var `VALIDATOR_ATTESTATIONS_WAIT_TIMEOUT_MS` and\r\nreplaces it with the deadline for publishing the block to L1. This\r\nensures the sequencer does not get stuck waiting for attestations where\r\nthe validators have given up due to a reexecution timeout.\r\n\r\nIf timetable is not enforced, this value defaults to one aztec slot.",
-          "timestamp": "2025-01-15T19:26:54Z",
-          "tree_id": "374758d87a6cc69d6359da31636d6c7f81b9340b",
-          "url": "https://github.com/AztecProtocol/aztec-packages/commit/946a418d138c1b2bee3a5dc14096616a902cc0b7"
-        },
-        "date": 1736970556715,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Sequencer/aztec.sequencer.block.build_duration",
-            "value": 4840,
-            "unit": "ms"
-          },
-          {
-            "name": "Sequencer/aztec.sequencer.block.time_per_mana",
-            "value": 3.6367339219993307,
-            "unit": "us/mana"
-          },
-          {
-            "name": "Sequencer/aztec.sequencer.block_builder_tree_insertion_duration",
-            "value": 713663,
-            "unit": "us"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -1949,6 +1910,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Sequencer/aztec.sequencer.block_builder_tree_insertion_duration",
             "value": 132237,
+            "unit": "us"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "60546371+PhilWindle@users.noreply.github.com",
+            "name": "PhilWindle",
+            "username": "PhilWindle"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "13aa4f5fabadcd18daae30bac3955145eb44837e",
+          "message": "chore: Cleanup and re-specify sequencer config in RC1 (#12898)\n\nThis PR just re-specifies sequencer config in RC-1 to ensure it doesn't\ninadvertently get overwritten. Also some cleanup of old env vars.",
+          "timestamp": "2025-03-20T16:33:38Z",
+          "tree_id": "b1c544f4493e8ac3bd80aa0f96d39e507b57d4b9",
+          "url": "https://github.com/AztecProtocol/aztec-packages/commit/13aa4f5fabadcd18daae30bac3955145eb44837e"
+        },
+        "date": 1742490033136,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sequencer/aztec.sequencer.block.build_duration",
+            "value": 9508,
+            "unit": "ms"
+          },
+          {
+            "name": "Sequencer/aztec.sequencer.block.time_per_mana",
+            "value": 0.23202232982902274,
+            "unit": "us/mana"
+          },
+          {
+            "name": "Sequencer/aztec.sequencer.block_builder_tree_insertion_duration",
+            "value": 138547,
             "unit": "us"
           }
         ]
