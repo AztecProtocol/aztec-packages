7e035338b2dc310029d5c22707ac238b83a4e1f8
benesjan <janbenes1234@gmail.com>
2025-03-20 23:02:54 +0000

    Reapply "feat: recording circuit inputs + oracles (#12148)"
    This reverts commit 61068dae2f702ce5dba74b36a50b68112ae05d38.

--- yarn-project/txe/src/oracle/txe_oracle.ts
+++ yarn-project/txe/src/oracle/txe_oracle.ts
@@ -826,12 +826,11 @@ export class TXE implements TypedOracle {
 
     const artifact = await this.contractDataProvider.getFunctionArtifact(targetContractAddress, functionSelector);
 
-    const acir = artifact.bytecode;
     const initialWitness = await this.getInitialWitness(artifact, argsHash, sideEffectCounter, isStaticCall);
     const acvmCallback = new Oracle(this);
     const timer = new Timer();
     const acirExecutionResult = await this.simulationProvider
-      .executeUserCircuit(acir, initialWitness, acvmCallback)
+      .executeUserCircuit(initialWitness, artifact, acvmCallback)
       .catch((err: Error) => {
         err.message = resolveAssertionMessageFromError(err, artifact);
 
