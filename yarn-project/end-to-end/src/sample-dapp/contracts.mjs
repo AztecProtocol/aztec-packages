82663cb4e5eb53472dded634374cbe5275f6bbb4
megabyte <megabyte0x@gmail.com>
2025-03-21 21:45:56 +0530

    fix: Contract class difference error

--- yarn-project/end-to-end/src/sample-dapp/contracts.mjs
+++ yarn-project/end-to-end/src/sample-dapp/contracts.mjs
@@ -1,14 +1,16 @@
 // docs:start:imports
-import { AztecAddress } from '@aztec/aztec.js';
-import { TokenContract } from '@aztec/noir-contracts.js/Token';
-
+import { AztecAddress, Contract, loadContractArtifact  } from '@aztec/aztec.js';
 import { readFileSync } from 'fs';
+
+import TokenContractJson from "./contracts/token/target/token-Token.json" assert { type: "json" };
 
 // docs:end:imports
 
 // docs:start:get-tokens
+const TokenContractArtifact = loadContractArtifact(TokenContractJson);
+
 export async function getToken(wallet) {
   const addresses = JSON.parse(readFileSync('addresses.json'));
-  return TokenContract.at(AztecAddress.fromString(addresses.token), wallet);
+  return Contract.at(AztecAddress.fromString(addresses.token), TokenContractArtifact, wallet);
 }
 // docs:end:get-tokens
