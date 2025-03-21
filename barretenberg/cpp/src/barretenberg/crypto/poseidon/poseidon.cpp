366560be461daba6ce4674d5d020dcbd60cb7ee9
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2025-03-21 16:18:22 -0300

    Inverts Poseidon state[0]/state[2] to reuse BB sponge implementation

--- barretenberg/cpp/src/barretenberg/crypto/poseidon/poseidon.cpp
+++ barretenberg/cpp/src/barretenberg/crypto/poseidon/poseidon.cpp
@@ -1,8 +1,68 @@
 #include "./poseidon.hpp"
 #include "./poseidon_params.hpp"
 #include "./poseidon_permutation.hpp"
+#include "../poseidon2/sponge/sponge.hpp"
 
 namespace bb::crypto {
+
+template <typename Params> class Poseidon {
+  public:
+    using FF = typename Params::FF;
+
+    using Sponge = FieldSponge<FF, Params::t - 1, 1, Params::t, PoseidonPermutation<Params>>;
+
+    static FF hash(const std::vector<FF>& input);
+
+    static FF hash_buffer(const std::vector<uint8_t>& input);
+};
+
+extern template class Poseidon<PoseidonStark252BaseFieldParams>;
+
+template <typename Params>
+typename Poseidon<Params>::FF Poseidon<Params>::hash(const std::vector<typename Poseidon<Params>::FF>& input)
+{
+    std::vector<FF> modified_input = { FF(1), FF(0) };
+    modified_input.insert(modified_input.end(), input.begin(), input.end());
+    FF iv = FF(std::string("0x0000000000000000000000000000000000000000537461726b6e6574486f6e6b")); // "StarknetHonk"
+    return Sponge::hash_internal(modified_input, iv);
+}
+
+template <typename Params>
+typename Poseidon<Params>::FF Poseidon<Params>::hash_buffer(const std::vector<uint8_t>& input)
+{
+    const size_t num_bytes = input.size();
+    const size_t bytes_per_element = 16;
+    size_t num_elements = static_cast<size_t>(num_bytes % bytes_per_element != 0) + (num_bytes / bytes_per_element);
+
+    const auto slice = [](const std::vector<uint8_t>& data, const size_t start, const size_t slice_size) {
+        uint256_t result(0);
+        for (size_t i = 0; i < slice_size; ++i) {
+            result = (result << uint256_t(8));
+            result += uint256_t(data[i + start]);
+        }
+        return FF(result);
+    };
+
+    std::vector<FF> converted;
+    for (size_t i = 0; i < num_elements - 1; ++i) {
+        size_t bytes_to_slice = bytes_per_element;
+        FF element = slice(input, i * bytes_per_element, bytes_to_slice);
+        converted.emplace_back(element);
+    }
+    size_t bytes_to_slice = num_bytes - ((num_elements - 1) * bytes_per_element);
+    FF element = slice(input, (num_elements - 1) * bytes_per_element, bytes_to_slice);
+    converted.emplace_back(element);
+
+    for (size_t i = 0; i < num_elements / 2; i += 2) {
+        auto t = converted[i + 1];
+        converted[i + 1] = converted[i];
+        converted[i] = t;
+    }
+
+    return hash(converted);
+}
+
+template class Poseidon<PoseidonStark252BaseFieldParams>;
 
 PoseidonHash poseidon_stark252(const std::vector<uint8_t>& input)
 {
@@ -11,6 +71,9 @@ PoseidonHash poseidon_stark252(const std::vector<uint8_t>& input)
     using Permutation = PoseidonPermutation<PoseidonStark252BaseFieldParams>;
     using State = Permutation::State;
     using FF = Permutation::FF;
+
+    //size_t elem_count = input.size() / 32;
+    //std::vector<FF> elems(2 * elem_count);
 
     State state = {
       FF(1),
@@ -32,13 +95,22 @@ PoseidonHash poseidon_stark252(const std::vector<uint8_t>& input)
         FF limb0 = from_buffer<FF>(limb_lo);
         FF limb1 = from_buffer<FF>(limb_hi);
 
-        state[2] += limb0;
+        //elems[2 * k + 0] = limb0;
+        //elems[2 * k + 1] = limb1;
+
+        state[0] += limb0;
         state[1] += limb1;
 
         state = Permutation::permutation(state);
     }
 
-    std::vector<uint8_t> digest = to_buffer(state[2]);
+    //FF output1 = Poseidon<PoseidonStark252BaseFieldParams>::hash_buffer(input);
+    //FF output2 = Poseidon<PoseidonStark252BaseFieldParams>::hash(elems);
+    //std::cout << output1 << std::endl;
+    //std::cout << output2 << std::endl;
+    //std::cout << state[0] << std::endl;
+
+    std::vector<uint8_t> digest = to_buffer(state[0]);
 
     PoseidonHash result;
     for (size_t i = 0; i < 32; ++i) {
