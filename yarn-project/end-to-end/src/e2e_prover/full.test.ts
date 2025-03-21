5b10e01c9e02cb964a52a9a987c53f6ecf8c4f43
ledwards2225 <l.edwards.d@gmail.com>
2025-03-21 18:36:12 +0000

    remove missed lines

--- yarn-project/end-to-end/src/e2e_prover/full.test.ts
+++ yarn-project/end-to-end/src/e2e_prover/full.test.ts
@@ -115,11 +115,6 @@ describe('full_prover', () => {
       await expect(t.circuitProofVerifier?.verifyProof(publicProvenTx)).resolves.not.toThrow();
       await expect(t.circuitProofVerifier?.verifyProof(privateProvenTx)).resolves.not.toThrow();
 
-      // Verify them
-      logger.info(`Verifying txs`);
-      await expect(t.circuitProofVerifier?.verifyProof(publicProvenTx)).resolves.not.toThrow();
-      await expect(t.circuitProofVerifier?.verifyProof(privateProvenTx)).resolves.not.toThrow();
-
       // Sends the txs to node and awaits them to be mined separately, so they land on different blocks,
       // and we have more than one block in the epoch we end up proving
       logger.info(`Sending private tx`);
