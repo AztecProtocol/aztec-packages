a5df874ebffdf061b93d76debc9d43dbfef61e44
thunkar <gregojquiros@gmail.com>
2025-03-24 12:14:56 +0000

    wip

--- yarn-project/cli-wallet/src/cmds/deploy_account.ts
+++ yarn-project/cli-wallet/src/cmds/deploy_account.ts
@@ -46,13 +46,12 @@ export async function deployAccount(
   };
 
   if (feeOpts.estimateOnly) {
-    if (feeOpts.estimateOnly) {
-      const fee =
-        !deployOpts?.deployWallet && deployOpts?.fee
-          ? { ...deployOpts.fee, paymentMethod: await account.getSelfPaymentMethod(deployOpts.fee.paymentMethod) }
-          : deployOpts?.fee;
-      const deployMethod = await account.getDeployMethod(deployOpts.deployWallet);
-      const gas = await deployMethod.estimateGas({ ...deployOpts, fee });
+    const fee =
+      !deployOpts?.deployWallet && deployOpts?.fee
+        ? { ...deployOpts.fee, paymentMethod: await account.getSelfPaymentMethod(deployOpts.fee.paymentMethod) }
+        : deployOpts?.fee;
+    const deployMethod = await account.getDeployMethod(deployOpts.deployWallet);
+    const gas = await deployMethod.estimateGas({ ...deployOpts, fee });
     if (json) {
       out.fee = {
         gasLimits: {
