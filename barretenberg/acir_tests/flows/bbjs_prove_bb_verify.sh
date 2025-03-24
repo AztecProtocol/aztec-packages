3448f64b2c6cd4e92fc146ae0b684847b87e52d7
lucasxia01 <lucasxia01@gmail.com>
2025-03-24 15:44:25 +0000

    update bbjs_prove_bb_verify to split public inputs

--- barretenberg/acir_tests/flows/bbjs_prove_bb_verify.sh
+++ barretenberg/acir_tests/flows/bbjs_prove_bb_verify.sh
@@ -13,7 +13,7 @@ output_dir=$artifact_dir/bb-bbjs-tmp
 mkdir -p $output_dir
 
 # Cleanup on exit
-trap "rm -rf $output_dir" EXIT
+# trap "rm -rf $output_dir" EXIT
 
 # Writes the proof, public inputs ./target; this also writes the VK
 node ../../bbjs-test prove \
@@ -26,9 +26,11 @@ node ../../bbjs-test prove \
 
 NUM_PUBLIC_INPUTS=$(cat $output_dir/public_inputs_fields.json | jq 'length')
 UH_PROOF_FIELDS_LENGTH=440
-PROOF_AND_PI_LENGTH_IN_FIELDS=$((NUM_PUBLIC_INPUTS + UH_PROOF_FIELDS_LENGTH))
+PROOF_LENGTH_IN_FIELDS=$((UH_PROOF_FIELDS_LENGTH))
+PI_LENGTH_IN_FIELDS=$((NUM_PUBLIC_INPUTS))
 # First 4 bytes is PROOF_AND_PI_LENGTH_IN_FIELDS
-proof_header=$(printf "%08x" $PROOF_AND_PI_LENGTH_IN_FIELDS)
+proof_header=$(printf "%08x" $PROOF_LENGTH_IN_FIELDS)
+pi_header=$(printf "%08x" $PI_LENGTH_IN_FIELDS)
 
 proof_bytes=$(cat $output_dir/proof | xxd -p)
 public_inputs=$(cat $output_dir/public_inputs_fields.json | jq -r '.[]')
@@ -38,11 +40,18 @@ for input in $public_inputs; do
   public_inputs_bytes+=$input
 done
 
-# Combine proof header, public inputs, and the proof to a single file
-echo -n $proof_header$public_inputs_bytes$proof_bytes | xxd -r -p > $output_dir/proof
+# Combine proof header and the proof to a single file
+echo -n $proof_header$proof_bytes | xxd -r -p > $output_dir/proof
+echo -n $pi_header$public_inputs_bytes | xxd -r -p > $output_dir/public_inputs
+echo "$BIN verify \
+  --scheme ultra_honk \
+  -k $output_dir/vk \
+  -p $output_dir/proof \
+  -i $output_dir/public_inputs"
 
 # Verify the proof with bb cli
 $BIN verify \
   --scheme ultra_honk \
   -k $output_dir/vk \
-  -p $output_dir/proof
+  -p $output_dir/proof \
+  -i $output_dir/public_inputs
