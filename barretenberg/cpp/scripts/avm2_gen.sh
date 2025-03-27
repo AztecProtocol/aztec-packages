dff8fbe7f4dad0962eecbc790c2cbf26f54ddcb9
jeanmon <jean@aztec-labs.com>
2025-03-27 17:03:38 +0000

    Addressing review feedback

--- barretenberg/cpp/scripts/avm2_gen.sh
+++ barretenberg/cpp/scripts/avm2_gen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 current_dir="$(dirname "$(readlink -f "$0")")"
-$current_dir/../../../bb-pilcom/target/release/bb_pil \
-    $current_dir/../pil/vm2/execution.pil --name Avm2 \
-    -y -o $current_dir/../src/barretenberg/vm2/generated \
-    && $current_dir/../format.sh changed \
+cd $current_dir/../
+../../bb-pilcom/target/release/bb_pil pil/vm2/execution.pil \
+    --name Avm2 -y -o src/barretenberg/vm2/generated \
+    && ./format.sh changed
