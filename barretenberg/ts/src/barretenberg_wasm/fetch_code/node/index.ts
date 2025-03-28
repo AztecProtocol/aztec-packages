8d9c2a3fd6d3384ba0f6a4886206a10c9d7c99eb
thunkar <gregojquiros@gmail.com>
2025-03-28 14:00:14 +0000

    allow pointing bb.js node to any bb wasm

--- barretenberg/ts/src/barretenberg_wasm/fetch_code/node/index.ts
+++ barretenberg/ts/src/barretenberg_wasm/fetch_code/node/index.ts
@@ -16,7 +16,18 @@ function getCurrentDir() {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function fetchCode(multithreaded: boolean, wasmPath?: string) {
   const path = wasmPath ?? getCurrentDir() + '/../../barretenberg-threads.wasm.gz';
-  const compressedData = await readFile(path);
-  const decompressedData = pako.ungzip(new Uint8Array(compressedData));
-  return decompressedData.buffer;
+  const maybeCompressedData = await readFile(path);
+  const buffer = new Uint8Array(maybeCompressedData);
+  const isGzip =
+    // Check magic number
+    buffer[0] === 0x1f &&
+    buffer[1] === 0x8b &&
+    // Check compression method:
+    buffer[2] === 0x08;
+  if (isGzip) {
+    const decompressedData = pako.ungzip(buffer);
+    return decompressedData.buffer;
+  } else {
+    return buffer;
+  }
 }
