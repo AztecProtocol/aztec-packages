64707712d28d17e013f652d8d67f4a375419714a
Nicolás Venturo <nicolas.venturo@gmail.com>
2025-03-28 20:51:19 +0000

    Fix extra import

--- yarn-project/end-to-end/src/bench/client_flows/bridging.test.ts
+++ yarn-project/end-to-end/src/bench/client_flows/bridging.test.ts
@@ -8,12 +8,7 @@ import { jest } from '@jest/globals';
 
 import { capturePrivateExecutionStepsIfEnvSet } from '../../shared/capture_private_execution_steps.js';
 import type { CrossChainTestHarness } from '../../shared/cross_chain_test_harness.js';
-import {
-  type AccountType,
-  type BenchmarkingFeePaymentMethod,
-  type ClientFlowConfig,
-  ClientFlowsBenchmark,
-} from './client_flows_benchmark.js';
+import { type AccountType, type BenchmarkingFeePaymentMethod, ClientFlowsBenchmark } from './client_flows_benchmark.js';
 
 jest.setTimeout(300_000);
 
