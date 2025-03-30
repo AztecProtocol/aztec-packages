9f2a6f42b2e111b424a4ffac8ee5d958f351564e
Adrian <hawkadrian10@gmail.com>
2025-03-30 17:01:04 +0300

    Update index.ts

--- yarn-project/txe/src/index.ts
+++ yarn-project/txe/src/index.ts
@@ -85,7 +85,7 @@ class TXEDispatcher {
     let instance;
 
     if (TXEArtifactsCache.has(cacheKey)) {
-      this.logger.debug('Using cached artifact for %s', cacheKey);
+      this.logger.debug({ cacheKey }, 'Using cached artifact');
       ({ artifact, instance } = TXEArtifactsCache.get(cacheKey)!);
     } else {
       let artifactPath = '';
@@ -99,24 +99,26 @@ class TXEDispatcher {
         if (pathStr.includes('@')) {
           const [workspace, pkg] = pathStr.split('@');
           const targetPath = join(rootPath, workspace, './target');
-          this.logger.debug('Looking for compiled artifact in workspace %s', targetPath);
+          this.logger.debug({ targetPath }, 'Looking for compiled artifact in workspace');
           artifactPath = join(targetPath, `${pkg}-${contractName}.json`);
         } else {
           // We're deploying a standalone contract
           // env.deploy("../path/to/contract/root", "contractName")
           const targetPath = join(rootPath, pathStr, './target');
-          this.logger.debug('Looking for compiled artifact in %s', targetPath);
+          this.logger.debug({ targetPath }, 'Looking for compiled artifact');
           [artifactPath] = (await readdir(targetPath)).filter(file => file.endsWith(`-${contractName}.json`));
         }
       }
-      this.logger.debug('Loading compiled artifact %s', artifactPath);
+      this.logger.debug({ artifactPath }, 'Loading compiled artifact');
       artifact = loadContractArtifact(JSON.parse(await readFile(artifactPath, 'utf-8')));
       this.logger.debug(
-        'Deploy %s with initializer %s(%s) and public keys hash %s',
-        artifact.name,
-        initializer,
-        decodedArgs.map(arg => arg.toString()).join('-'),
-        publicKeysHash.toString(),
+        {
+          name: artifact.name,
+          initializer,
+          args: decodedArgs.map(arg => arg.toString()).join('-'),
+          publicKeysHash: publicKeysHash.toString()
+        },
+        'Deploy contract'
       );
       instance = await getContractInstanceFromDeployParams(artifact, {
         constructorArgs: decodedArgs,
@@ -141,7 +143,7 @@ class TXEDispatcher {
     let instance;
 
     if (TXEArtifactsCache.has(cacheKey)) {
-      this.logger.debug(`Using cached artifact for ${cacheKey}`);
+      this.logger.debug({ cacheKey }, 'Using cached artifact');
       ({ artifact, instance } = TXEArtifactsCache.get(cacheKey)!);
     } else {
       const keys = await deriveKeys(secret);
@@ -164,10 +166,10 @@ class TXEDispatcher {
   // eslint-disable-next-line camelcase
   async resolve_foreign_call(callData: TXEForeignCallInput): Promise<ForeignCallResult> {
     const { session_id: sessionId, function: functionName, inputs } = callData;
-    this.logger.debug('Calling %s on session %s', functionName, sessionId);
+    this.logger.debug({ functionName, sessionId }, 'Calling function on session');
 
     if (!TXESessions.has(sessionId) && functionName != 'reset') {
-      this.logger.debug('Creating new session %s', sessionId);
+      this.logger.debug({ sessionId }, 'Creating new session');
       if (!this.protocolContracts) {
         this.protocolContracts = await Promise.all(
           protocolContractNames.map(name => new BundledProtocolContractsProvider().getProtocolContractArtifact(name)),
@@ -179,7 +181,7 @@ class TXEDispatcher {
     switch (functionName) {
       case 'reset': {
         TXESessions.delete(sessionId) &&
-          this.logger.debug('Called reset on session %s, yeeting it out of existence', sessionId);
+          this.logger.debug({ sessionId }, 'Called reset on session, yeeting it out of existence');
         return toForeignCallResult([]);
       }
       case 'deploy': {
