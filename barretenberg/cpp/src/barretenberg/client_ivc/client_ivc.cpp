04a079de454f658d9519e6a9242b82872067aac5
ledwards2225 <l.edwards.d@gmail.com>
2025-03-31 02:54:17 +0000

    use actual circuit content to size translator polynomials

--- barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.cpp
+++ barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.cpp
@@ -156,10 +156,9 @@ void ClientIVC::accumulate(ClientCircuit& circuit,
     // Construct the proving key for circuit
     std::shared_ptr<DeciderProvingKey> proving_key = std::make_shared<DeciderProvingKey>(circuit, trace_settings);
 
-    // The commitment key is initialised with the number of points determined by the trace_settings' dyadic size. If a
-    // circuit overflows past the dyadic size the commitment key will not have enough points so we need to increase it
-    // WORKTODO: we need to check that the size we set here is also enough to commit to the polynomials in translator
-    if (proving_key->proving_key.circuit_size > trace_settings.dyadic_size()) {
+    // If the current circuit overflows past the current size of the commitment key, reinitialize accordingly.
+    // WORKTODO: Make issue to set once and for all. Also, having to reset the goblin key is brittle.
+    if (proving_key->proving_key.circuit_size > bn254_commitment_key->dyadic_size) {
         bn254_commitment_key = std::make_shared<CommitmentKey<curve::BN254>>(proving_key->proving_key.circuit_size);
         goblin.commitment_key = bn254_commitment_key;
     }
