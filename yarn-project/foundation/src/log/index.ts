8223f620524c120aaa2cf913caef06a8b7156855
benesjan <janbenes1234@gmail.com>
2025-03-31 19:22:16 +0000

    nuking more stuff I don't understand

--- yarn-project/foundation/src/log/index.ts
+++ yarn-project/foundation/src/log/index.ts
@@ -1,6 +1,5 @@
 export * from './console.js';
-export * from './debug.js';
 export * from './pino-logger.js';
-export * from './log_history.js';
 export * from './log_fn.js';
 export * from './libp2p_logger.js';
+export * from './utils.js';
