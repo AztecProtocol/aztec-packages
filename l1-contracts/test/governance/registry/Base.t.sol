384f4e5a9b82768c0fc9f48116038986340fae1a
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2025-04-01 16:13:32 +0100

    refactor!: operation mouthwash (#13171)

--- l1-contracts/test/governance/registry/Base.t.sol
+++ l1-contracts/test/governance/registry/Base.t.sol
@@ -4,11 +4,19 @@ pragma solidity >=0.8.27;
 import {Test} from "forge-std/Test.sol";
 
 import {Registry} from "@aztec/governance/Registry.sol";
+import {TestERC20} from "@aztec/mock/TestERC20.sol";
+
+contract FakeRollup {
+  function getVersion() external view returns (uint256) {
+    return uint256(keccak256(abi.encodePacked(bytes("aztec_rollup"), block.chainid, address(this))));
+  }
+}
 
 contract RegistryBase is Test {
   Registry internal registry;
 
-  function setUp() public {
-    registry = new Registry(address(this));
+  function setUp() public virtual {
+    TestERC20 asset = new TestERC20("test", "TEST", address(this));
+    registry = new Registry(address(this), asset);
   }
 }
