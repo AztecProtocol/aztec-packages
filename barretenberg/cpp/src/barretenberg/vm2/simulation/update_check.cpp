4bd0dfcccd3f59ce2e59355b3257b2b9f160e2cc
sirasistant <sirasistant@gmail.com>
2025-04-03 16:58:04 +0000

    comments from peer review

--- barretenberg/cpp/src/barretenberg/vm2/simulation/update_check.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/simulation/update_check.cpp
@@ -21,11 +21,16 @@ FF unconstrained_read(const LowLevelMerkleDBInterface& merkle_db, const FF& leaf
 
 void UpdateCheck::check_current_class_id(const AztecAddress& address, const ContractInstance& instance)
 {
+    // Compute the public data tree slots
     FF shared_mutable_slot = poseidon2.hash({ UPDATED_CLASS_IDS_SLOT, address });
     FF shared_mutable_hash_slot = shared_mutable_slot + UPDATES_SHARED_MUTABLE_VALUES_LEN;
     FF shared_mutable_leaf_slot =
         poseidon2.hash({ GENERATOR_INDEX__PUBLIC_LEAF_INDEX, DEPLOYER_CONTRACT_ADDRESS, shared_mutable_hash_slot });
-    // Read the hash from the tree
+
+    // Read the hash from the tree. We do a trick with shared mutables (updates are shared mutables) where we store in
+    // one public data tree slot the hash of the whole structure. This is nice because in circuits you can receive the
+    // preimage as a hint and just read 1 storage slot instead of 3. We do that here, we will constrain the hash read
+    // but then read in unconstrained mode the preimage. The PIL for this gadget constrains the hash.
     FF hash = merkle_db.storage_read(shared_mutable_leaf_slot);
 
     uint256_t update_preimage_metadata = 0;
@@ -33,6 +38,7 @@ void UpdateCheck::check_current_class_id(const AztecAddress& address, const Cont
     FF update_preimage_post_class = 0;
 
     if (hash == 0) {
+        // If the shared mutable has never been written, then the contract was never updated. We short circuit early.
         if (instance.original_class_id != instance.current_class_id) {
             throw std::runtime_error("Current class id does not match expected class id");
         }
@@ -48,6 +54,8 @@ void UpdateCheck::check_current_class_id(const AztecAddress& address, const Cont
             update_preimage[i] = unconstrained_read(unconstrained_merkle_db, leaf_slot);
         }
 
+        // Double check that the unconstrained reads match the hash. This is just a sanity check, if slow, can be
+        // removed.
         FF reconstructed_hash = poseidon2.hash(update_preimage);
         if (hash != reconstructed_hash) {
             throw std::runtime_error("Stored hash does not match preimage hash");
@@ -57,11 +65,13 @@ void UpdateCheck::check_current_class_id(const AztecAddress& address, const Cont
         update_preimage_pre_class = update_preimage[1];
         update_preimage_post_class = update_preimage[2];
 
+        // Decompose the metadata: we want the least significant 32 bits since that's the block of change.
         uint128_t update_metadata_hi = static_cast<uint128_t>(update_preimage_metadata >> BLOCK_NUMBER_BIT_SIZE);
         uint32_t update_block_of_change = static_cast<uint32_t>(update_preimage_metadata & 0xffffffff);
         range_check.assert_range(update_metadata_hi, UPDATES_SHARED_MUTABLE_METADATA_BIT_SIZE - BLOCK_NUMBER_BIT_SIZE);
         range_check.assert_range(update_block_of_change, BLOCK_NUMBER_BIT_SIZE);
 
+        // pre and post can be zero, if they have never been touched. In that case we need to use the original class id.
         FF pre_class = update_preimage_pre_class == 0 ? instance.original_class_id : update_preimage_pre_class;
         FF post_class = update_preimage_post_class == 0 ? instance.original_class_id : update_preimage_post_class;
 
