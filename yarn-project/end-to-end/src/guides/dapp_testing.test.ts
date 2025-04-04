2c4ccb9b340c506f9f047f62bed65b7d7cde84d7
Santiago Palladino <santiago@aztec-labs.com>
2025-04-04 14:34:36 -0300

    Fix test

--- yarn-project/end-to-end/src/guides/dapp_testing.test.ts
+++ yarn-project/end-to-end/src/guides/dapp_testing.test.ts
@@ -143,7 +143,7 @@ describe('guides/dapp/testing', () => {
         const provenCall2 = await call2.prove();
 
         await provenCall1.send().wait();
-        await expect(provenCall2.send().wait()).rejects.toThrow(TX_ERROR_EXISTING_NULLIFIER);
+        await expect(provenCall2.send().wait()).rejects.toThrow(/dropped|nullifier/i);
         // docs:end:tx-dropped
       });
 
