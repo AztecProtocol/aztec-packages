16c6521291d2b2f34043d3c19bec541e0e3887e3
TomAFrench <tom@tomfren.ch>
2025-04-07 19:20:38 +0000

    .

--- noir-projects/scripts/format.sh
+++ noir-projects/scripts/format.sh
@@ -5,8 +5,11 @@ export NARGO=${NARGO:-../noir/noir-repo/target/release/nargo}
 
 cd $(dirname "$0")/../
 
+cd ./noir-protocol-circuits
+yarn && node ./scripts/generate_variants.js
+cd ..
+
 $NARGO fmt --program-dir ./aztec-nr
 $NARGO fmt --program-dir ./noir-contracts
-cd ./noir-protocol-circuits && yarn && node ./scripts/generate_variants.js
-$NARGO fmt --program-dir ./noir-protocol-circuits
 $NARGO fmt --program-dir ./mock-protocol-circuits
+$NARGO fmt --program-dir ./noir-protocol-circuits
