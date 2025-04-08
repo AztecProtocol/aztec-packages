11e848d999b7fe8556bd0f4fb5ce03e4617030a4
iakovenkos <sergey.s.yakovenko@gmail.com>
2025-04-08 12:13:38 +0000

    create file/folder

---
+++ barretenberg/cpp/src/barretenberg/stdlib/primitives/bit_by_bit_decomposition/bit_by_bit.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "../circuit_builders/circuit_builders_fwd.hpp"
+#include "../witness/witness.hpp"
+#include "barretenberg/transcript/origin_tag.hpp"
+
+using namespace bb;
+
+namespace bb::stdlib {}
