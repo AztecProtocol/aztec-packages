28e11487f3bd92ac6388979a3491328ffb43d6f9
sirasistant <sirasistant@gmail.com>
2025-04-09 07:00:33 +0000

    fix name

--- yarn-project/ivc-integration/src/avm_integration.test.ts
+++ yarn-project/ivc-integration/src/avm_integration.test.ts
@@ -105,7 +105,7 @@ describe('AVM Integration', () => {
     });
 
     await proveRollupHonk(
-      'MockRollupBasePrivateCircuit',
+      'MockRollupBasePublicCircuit',
       bbBinaryPath,
       bbWorkingDirectory,
       MockRollupBasePublicCircuit,
