73475e7ffd4cf2dde047deb475b5bda23ae26de3
signorecello <outgoing@zkpedro.dev>
2025-04-11 14:46:53 +0000

    adding back protocol sidebar

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -295,12 +295,6 @@ const config = {
                 label: "Roadmap",
                 className: "no-external-icon",
               },
-              // {
-              //   type: "docSidebar",
-              //   sidebarId: "protocolSpecSidebar",
-              //   label: "Protocol Specification",
-              //   className: "no-external-icon",
-              // },
               {
                 to: "https://noir-lang.org/docs",
                 label: "Noir docs",
@@ -431,5 +425,19 @@ const config = {
       },
     }),
 };
+
+if (process.env.PROTOCOL_SPECS) {
+  //@ts-ignore
+  const index = config.themeConfig.navbar.items.findIndex(
+    (e) => e.type == "dropdown"
+  );
+
+  //@ts-ignore
+  config.themeConfig.navbar.items.splice(index, 0, {
+    type: "docSidebar",
+    sidebarId: "protocolSpecSidebar",
+    label: "Protocol Specification",
+  });
+}
 
 module.exports = config;
