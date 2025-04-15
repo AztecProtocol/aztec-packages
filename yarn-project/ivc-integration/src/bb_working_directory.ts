cefc90de6258c47852ad7c47c317f1dbb8072749
ludamad <domuradical@gmail.com>
2025-04-15 00:19:33 +0000

    missing file

---
+++ yarn-project/ivc-integration/src/bb_working_directory.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import fs from "fs/promises";
+import os from "os";
+
+export async function getWorkingDirectory(prefix: string): Promise<string> {
+  const baseFolder = process.env.BB_WORKING_DIRECTORY || os.tmpdir();
+  await fs.mkdir(baseFolder, { recursive: true });
+  return await fs.mkdtemp(path.join(baseFolder, prefix));
+}
