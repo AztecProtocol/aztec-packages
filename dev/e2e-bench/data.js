55da9ab5f6870d7bba9f65a7b42fb9a6cc19be69
github-action-benchmark <github@users.noreply.github.com>
2025-04-19 17:44:57 +0000

    add End-to-end Benchmark (customSmallerIsBetter) benchmark result for c75e3652343db4b62fac08d9ac76a8bf1ea6943e

--- dev/e2e-bench/data.js
+++ dev/e2e-bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745077121726,
+  "lastUpdate": 1745084697150,
   "repoUrl": "https://github.com/AztecProtocol/aztec-packages",
   "entries": {
     "End-to-end Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "nicolas.venturo@gmail.com",
-            "name": "Nicolás Venturo",
-            "username": "nventuro"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "e09699ff628bb14bbb9fe0b07111fe80b1aa1c7c",
-          "message": "feat!: remove PXE starting block (#13504)\n\nAs of https://github.com/AztecProtocol/aztec-packages/pull/13475, PXE no\nlonger needs to use the `startingBlock` option, and hence we can also\nremove the environment variable used to configure it. The effects of\npassing no value are equivalent to a value of 1, which was the default\nif the envvar was unset.",
-          "timestamp": "2025-04-14T11:28:14Z",
-          "tree_id": "47902f8d544767fcaf01fbf0e9cbafa201a5aa74",
-          "url": "https://github.com/AztecProtocol/aztec-packages/commit/e09699ff628bb14bbb9fe0b07111fe80b1aa1c7c"
-        },
-        "date": 1744634270729,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Sequencer/aztec.sequencer.block.build_duration",
-            "value": 10077,
-            "unit": "ms"
-          },
-          {
-            "name": "Sequencer/aztec.sequencer.block.time_per_mana",
-            "value": 0.2707005378007584,
-            "unit": "us/mana"
-          },
-          {
-            "name": "Sequencer/aztec.sequencer.block_builder_tree_insertion_duration",
-            "value": 175551,
-            "unit": "us"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -1941,6 +1902,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Sequencer/aztec.sequencer.block_builder_tree_insertion_duration",
             "value": 153381,
+            "unit": "us"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "47112877+dbanks12@users.noreply.github.com",
+            "name": "David Banks",
+            "username": "dbanks12"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c75e3652343db4b62fac08d9ac76a8bf1ea6943e",
+          "message": "fix: dependency cycles in public simulator - part 1 (errors/revertReason) (#13679)\n\nBrings cycles down from 19 to 15 \n\n![image](https://github.com/user-attachments/assets/0321e7e9-3a60-42d9-82ed-1f6cb9f3b307)\n\nCo-authored-by: dbanks12 <david@aztecprotocol.com>",
+          "timestamp": "2025-04-19T16:53:01Z",
+          "tree_id": "c2bfadeab532664a6ee3e3c02d7615ec82512af1",
+          "url": "https://github.com/AztecProtocol/aztec-packages/commit/c75e3652343db4b62fac08d9ac76a8bf1ea6943e"
+        },
+        "date": 1745084688952,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sequencer/aztec.sequencer.block.build_duration",
+            "value": 9858,
+            "unit": "ms"
+          },
+          {
+            "name": "Sequencer/aztec.sequencer.block.time_per_mana",
+            "value": 0.2648834009757245,
+            "unit": "us/mana"
+          },
+          {
+            "name": "Sequencer/aztec.sequencer.block_builder_tree_insertion_duration",
+            "value": 148721,
             "unit": "us"
           }
         ]
