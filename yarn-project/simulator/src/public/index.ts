4f2bd5b78f550d8944d815529d449ba0ab32ac39
dbanks12 <david@aztecprotocol.com>
2025-04-20 16:45:35 +0000

    fix: no exports of simulator should depend on jest-mock-extended

--- yarn-project/simulator/src/public/index.ts
+++ yarn-project/simulator/src/public/index.ts
@@ -3,4 +3,3 @@ export { type PublicTxResult, PublicTxSimulator, TelemetryPublicTxSimulator } fr
 export { PublicProcessor, PublicProcessorFactory } from './public_processor/public_processor.js';
 export { PublicTxSimulationTester } from './fixtures/index.js';
 export { getCallRequestsWithCalldataByPhase } from './utils.js';
-export { SimpleContractDataSource } from './fixtures/simple_contract_data_source.js';
