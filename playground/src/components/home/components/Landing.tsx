4c3453bd064e4efa7a14073aacfd4ea399a01c12
saleel <saleel@aztecprotocol.com>
2025-04-22 10:59:12 +0400

    chore: playground improvements (#13588)
    - Add profiler to playground
    - Upload your own contract:
       - show which version of `aztec-up`
       - link to tutorial to push before to write their own contract
       - make instructions less transparent
    - Contract Interaction
        - show parameter type in the text box
        - authwit move description up and explain caller
        - Add tool tip for simulate, send, authwit, profiler
    - Connect to Sandbox
        - show what version to deploy on
    - Register contract
    - When selecting contract, load AbI per usual and then automatically
    load deploy pop up
        - register/deploy button
        - Rename button to - “click here to load this pre deployed contract”
        - rename to “deploy/load contract”
    - Select Contract/Account
        - when deployed, the menu item should show “Contract: TST (0x123…)
    - Add Account:
        - Add “recommended” next to r1
    - Add contact
        - show that contact is of aztec address type
    - Make log button is more obvious (it is a red button now like a chat)
    - Inspiration button“start building”
    - Set tx timeout to 2 mins
    
    ---------
    
    Co-authored-by: PhilWindle <60546371+PhilWindle@users.noreply.github.com>
    Co-authored-by: Alex Gherghisan <alexghr@users.noreply.github.com>
    Co-authored-by: thunkar <gregojquiros@gmail.com>

--- playground/src/components/home/components/Landing.tsx
+++ playground/src/components/home/components/Landing.tsx
@@ -1,5 +1,14 @@
 import { css } from '@emotion/react';
 import welcomeIconURL from '../../../assets/welcome_icon.svg';
+import { Fr } from '@aztec/aztec.js';
+import { Box, Button, CircularProgress } from '@mui/material';
+import { AztecContext } from '../../../aztecEnv';
+import { useContext, useState } from 'react';
+import { PREDEFINED_CONTRACTS } from '../../../constants';
+import { randomBytes } from '@aztec/foundation/crypto';
+import { loadContractArtifact } from '@aztec/aztec.js';
+import { getEcdsaRAccount } from '@aztec/accounts/ecdsa/lazy';
+import { useTransaction } from '../../../hooks/useTransaction';
 
 const landingPage = css({
   display: 'flex',
@@ -31,9 +40,9 @@ const featureCard = css({
   background: '#CDD1D5',
   borderRadius: '20px',
   padding: '25px',
-  height: '250px',
   display: 'flex',
   flexDirection: 'column',
+  justifyContent: 'space-between',
 });
 
 const cardIcon = css({
@@ -62,6 +71,8 @@ const cardDescription = css({
   lineHeight: '110%',
   letterSpacing: '0.01em',
   color: 'rgba(0, 0, 0, 0.8)',
+  paddingTop: '1rem',
+  paddingBottom: '2rem',
 });
 
 const contentFrame = css({
@@ -129,78 +140,10 @@ const welcomeText = css({
   maxWidth: '558px',
 });
 
-const mainContent = css({
-  display: 'flex',
-  flexDirection: 'column',
-  flex: 1,
-  minHeight: 0,
-  overflow: 'auto',
-  margin: '0 60px 0 24px',
-  '@media (max-width: 1200px)': {
-    minHeight: 'auto',
-    margin: '0 24px',
-  },
-});
-
-const getStartedButton = css({
+const featureCardButton = css({
   width: '205px',
   height: '56px',
-  backgroundColor: '#CDD1D5',
-  borderRadius: '12px',
   display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  fontFamily: 'Inter, sans-serif',
-  fontWeight: 600,
-  fontSize: '17px',
-  lineHeight: '16px',
-  margin: '20px auto 0',
-  cursor: 'pointer',
-  '&:hover': {
-    backgroundColor: '#BCC0C4',
-  },
-});
-
-const sidebarButton = css({
-  boxSizing: 'border-box',
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
-  padding: '15px 32px',
-  gap: '8px',
-  width: '230px',
-  height: '50px',
-  margin: '8px auto',
-  background: '#CDD1D5',
-  borderRadius: '12px',
-  fontFamily: 'Inter, sans-serif',
-  fontStyle: 'normal',
-  fontWeight: 600,
-  fontSize: '17px',
-  lineHeight: '16px',
-  color: '#000000',
-  cursor: 'pointer',
-  '&:hover': {
-    backgroundColor: '#BCC0C4',
-  },
-});
-
-const sidebarButtonActive = css({
-  background: '#9894FF',
-  color: '#FFFFFF',
-  '&:hover': {
-    backgroundColor: '#8C7EFF',
-  },
-});
-
-const flashAnimation = css({
-  animation: 'flash 1s ease-in-out',
-  '@keyframes flash': {
-    '0%': { backgroundColor: '#E9E9E9' },
-    '50%': { backgroundColor: '#9894FF' },
-    '100%': { backgroundColor: '#E9E9E9' },
-  },
 });
 
 // Account Abstraction icon
@@ -344,16 +287,99 @@ const PrivateTokensIcon = () => (
 );
 
 export function Landing() {
+  const {
+    setCurrentContractArtifact,
+    setShowContractInterface,
+    walletDB,
+    pxe,
+  } = useContext(AztecContext);
+
+  const [isCreatingAccount, setIsCreatingAccount] = useState(false);
+  const [isLoadingPrivateVoting, setIsLoadingPrivateVoting] = useState(false);
+  const [isLoadingPrivateTokens, setIsLoadingPrivateTokens] = useState(false);
+
+  const { sendTx } = useTransaction();
+
+  async function handleContractButtonClick(contractValue: string) {
+    let contractArtifactJSON;
+    switch (contractValue) {
+      case PREDEFINED_CONTRACTS.SIMPLE_VOTING:
+        ({ EasyPrivateVotingContractArtifact: contractArtifactJSON } = await import(
+          '@aztec/noir-contracts.js/EasyPrivateVoting'
+        ));
+        break;
+      case PREDEFINED_CONTRACTS.SIMPLE_TOKEN:
+        ({ SimpleTokenContractArtifact: contractArtifactJSON } = await import(
+          '@aztec/noir-contracts.js/SimpleToken'
+        ));
+        break;
+    }
+    const contractArtifact = await loadContractArtifact(contractArtifactJSON);
+    setCurrentContractArtifact(contractArtifact);
+    setShowContractInterface(true);
+  }
+
+  async function handleCreateAccountButtonClick() {
+    setIsCreatingAccount(true);
+    try {
+      const salt = Fr.random();
+      const secretKey = Fr.random();
+      const signingKey = randomBytes(32);
+      const accountManager = await getEcdsaRAccount(pxe, secretKey, signingKey, salt);
+      const accountWallet = await accountManager.getWallet();
+      await accountManager.register();
+      await walletDB.storeAccount(accountWallet.getAddress(), {
+        type: 'ecdsasecp256r1',
+        secretKey: accountWallet.getSecretKey(),
+        alias: 'My Account 1',
+        salt,
+        signingKey,
+      });
+
+      const { prepareForFeePayment } = await import('../../../utils/sponsoredFPC');
+      const feePaymentMethod = await prepareForFeePayment(pxe);
+
+      const deployMethod = await accountManager.getDeployMethod();
+      const opts = {
+        contractAddressSalt: salt,
+        fee: {
+          paymentMethod: await accountManager.getSelfPaymentMethod(feePaymentMethod),
+        },
+        universalDeploy: true,
+        skipClassRegistration: true,
+        skipPublicDeployment: true,
+      };
+      // onClose(accountWallet, publiclyDeploy, deployMethod, opts);
+
+      const deploymentResult = await sendTx(`Deployment of account`, deployMethod, accountWallet.getAddress(), opts);
+      if (deploymentResult) {
+        // setAccounts([
+        //   ...accounts,
+        //   { key: `accounts:${accountWallet.getAddress()}`, value: accountWallet.getAddress().toString() },
+        // ]);
+        // setWallet(accountWallet);
+      } else {
+        // Temporarily remove from accounts if deployment fails
+        await walletDB.deleteAccount(accountWallet.getAddress());
+      }
+    } catch (e) {
+      console.error(e);
+      setIsCreatingAccount(false);
+    } finally {
+      setIsCreatingAccount(false);
+    }
+  }
+
   return (
     <div css={landingPage}>
       <div css={contentFrame}>
         <div css={welcomeSection}>
           <div css={welcomeContent}>
-            <div css={welcomeTitle}>Welcome to the Playground</div>
+            <div css={welcomeTitle}>Deploy Privacy-Preserving Smart Contracts</div>
             <div css={welcomeText}>
-              Playground is a web-app for interacting with Aztec. Create an aztec account, try one of our default
-              contracts or upload your own and interact with it while creating client side proofs in the browser! It is
-              a minimalistic remix.ethereum.org but for Aztec
+              Get started deploying and interacting with smart contracts on Aztec.
+              Create an aztec account, try one of our default contracts or upload your own and interact with
+              public and private functions made possible by client-side ZK proofs created in your browser.
             </div>
           </div>
           <div style={{ width: '40%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -363,31 +389,69 @@ export function Landing() {
 
         <div css={cardsContainer}>
           <div css={featureCard}>
-            <div css={cardIcon}>
-              <AccountAbstractionIcon />
-            </div>
-            <div css={cardTitle}>Account Abstraction</div>
-            <div css={cardDescription}>Short description of what account abstraction is and how it's being used</div>
+            <Box>
+              <div css={cardIcon}>
+                <AccountAbstractionIcon />
+              </div>
+              <div css={cardTitle}>Account Abstraction</div>
+              <div css={cardDescription}>Aztec’s native account abstraction turns every account into a smart contract, enabling
+                highly flexible and programmable user identities that unlock features like gas sponsorship, nonce abstraction
+                (setting your own tx ordering), and the use of alternative signature schemes to control smart contracts with e.g. passkeys. </div>
+            </Box>
+            <Button variant="contained" css={featureCardButton} onClick={handleCreateAccountButtonClick}>
+              {isCreatingAccount ? <CircularProgress size={20} variant="determinate" /> : 'Create Account'}
+            </Button>
           </div>
 
           <div css={featureCard}>
-            <div css={cardIcon}>
-              <PrivateVotingIcon />
-            </div>
-            <div css={cardTitle}>Private Voting</div>
-            <div css={cardDescription}>Short description of how a user could setup private voting</div>
+            <Box>
+              <div css={cardIcon}>
+                <PrivateVotingIcon />
+              </div>
+              <div css={cardTitle}>Private Voting</div>
+              <div css={cardDescription}>Developers can seamlessly integrate public and private functions to unlock use cases like
+                private voting. Voters can hide their address and cast their votes privately through a private function, which internally
+                calls a public function to update the vote count transparently. </div>
+            </Box>
+
+            <Button variant="contained" css={featureCardButton} onClick={async () => {
+              setIsLoadingPrivateVoting(true);
+              await handleContractButtonClick(PREDEFINED_CONTRACTS.SIMPLE_VOTING);
+              setIsLoadingPrivateVoting(false);
+            }}>
+              {isLoadingPrivateVoting ? <CircularProgress /> : 'Check it out'}
+            </Button>
           </div>
 
           <div css={featureCard}>
-            <div css={cardIcon}>
-              <PrivateTokensIcon />
-            </div>
-            <div css={cardTitle}>Private Tokens</div>
-            <div css={cardDescription}>Short description of what is possible with private tokens</div>
+            <Box>
+              <div css={cardIcon}>
+                <PrivateTokensIcon />
+              </div>
+              <div css={cardTitle}>Private Tokens</div>
+              <div css={cardDescription}>Accounts, transactions, and execution on Aztec can be done privately using client-side proofs, enabling you
+                to private mint or transfer tokens, move public tokens into private domain or the reverse - transfer tokens from private to public,
+                all without revealing your address or even the amount and recipient (in case of private transfer), all the while maintaining the
+                total supply of tokens publicly.</div>
+            </Box>
+
+            <Button variant="contained" css={featureCardButton} onClick={async () => {
+              setIsLoadingPrivateTokens(true);
+              await handleContractButtonClick(PREDEFINED_CONTRACTS.SIMPLE_TOKEN);
+              setIsLoadingPrivateTokens(false);
+            }}>
+              {isLoadingPrivateTokens ? <CircularProgress /> : 'Check it out'}
+            </Button>
           </div>
         </div>
 
-        <div css={getStartedButton}>Get Started</div>
+        {/* <div css={{ marginTop: '3rem' }}>
+          <span>Go ahead and select a network and create an account.
+            If testnet is down, you can also download our sandbox </span>
+          <a href="https://docs.aztec.network/developers/getting_started" target="_blank" rel="noopener noreferrer">here</a>
+          <span> and connect playground to it</span>
+        </div>
+        <div css={getStartedButton}>Get Started</div> */}
       </div>
     </div>
   );
