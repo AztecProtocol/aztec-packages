466f9a5d85de3cc6e55d8005056000d7963bf7d8
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2025-04-24 19:47:28 +0000

    chore: prevent destroy

--- spartan/terraform/gke-cluster/alpha-testnet-static-ip.tf
+++ spartan/terraform/gke-cluster/alpha-testnet-static-ip.tf
@@ -1,5 +1,9 @@
 resource "google_compute_global_address" "alpha_testnet_full_node_ip" {
   name = "alpha-testnet-full-node-ip"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 output "alpha_testnet_full_node_ip" {
