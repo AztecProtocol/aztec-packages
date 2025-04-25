095b032ea6cea72cfdd78e05f8badff9158a78fd
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-04-25 22:40:34 +0100

    chore: provide hash function in `noir-protocol-circuits` (#13857)
    This fixes `./ci.sh gh-bench` as it's trying to call the `hash` function
    within `noir-protocol-circuits` which doesn't exist.
    
    ---------
    
    Co-authored-by: AztecBot <tech@aztecprotocol.com>

--- noir-projects/noir-protocol-circuits/bootstrap.sh
+++ noir-projects/noir-protocol-circuits/bootstrap.sh
@@ -205,7 +205,7 @@ function bench {
   # We assume that the caller has bb installed and all of the protocol circuit artifacts are in `./target`
 
   rm -rf bench-out && mkdir -p bench-out
-  # Here we just cache based off of the git commit
+   # We don't have a good global content hash for the protocol circuits so we use the git commit.
   local hash=$(git rev-list -n 1 ${AZTEC_CACHE_COMMIT:-HEAD})
   if cache_download noir-protocol-circuits-bench-results-$hash.tar.gz; then
     return
