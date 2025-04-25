82ea2df5ab2cc6c3de1dbe119dc34c37778c01ff
lucasxia01 <lucasxia01@gmail.com>
2025-04-25 19:34:22 +0000

    update normal assert to also take in error msg

--- barretenberg/cpp/src/barretenberg/common/assert.hpp
+++ barretenberg/cpp/src/barretenberg/common/assert.hpp
@@ -8,9 +8,8 @@
         true ? static_cast<void>(0) : static_cast<void>((expression));                                                 \
     }
 
-#define ASSERT(expression) DONT_EVALUATE((expression))
-
 // All assertion macros accept an optional message but do nothing in release.
+#define ASSERT(expression, ...) DONT_EVALUATE((expression))
 #define BB_ASSERT_EQ(actual, expected, ...) DONT_EVALUATE((actual) == (expected))
 #define BB_ASSERT_GT(left, right, ...) DONT_EVALUATE((left) > (right))
 #define BB_ASSERT_GTE(left, right, ...) DONT_EVALUATE((left) >= (right))
@@ -22,14 +21,21 @@
 #include <iostream>
 #include <string>
 
-#define ASSERT(expression) assert((expression))
+// Basic assert with optional error message
+#define ASSERT(expression, ...)                                                                                        \
+    do {                                                                                                               \
+        if (!(expression)) {                                                                                           \
+            info("Assertion failed: (" #expression ")" __VA_OPT__(": ", ) __VA_ARGS__);                                \
+            std::abort();                                                                                              \
+        }                                                                                                              \
+    } while (0)
 
 #define BB_ASSERT_EQ(actual, expected, ...)                                                                            \
     do {                                                                                                               \
         auto _actual = (actual);                                                                                       \
         auto _expected = (expected);                                                                                   \
         if (!(_actual == _expected)) {                                                                                 \
-            info("Assertion failed: (" #actual " == " #expected ")" __VA_OPT__(, ) __VA_ARGS__);                       \
+            info("Assertion failed: (" #actual " == " #expected ")" __VA_OPT__(": ", ) __VA_ARGS__);                   \
             info("  Actual  : ", _actual);                                                                             \
             info("  Expected: ", _expected);                                                                           \
             std::abort();                                                                                              \
@@ -41,7 +47,7 @@
         auto _left = (left);                                                                                           \
         auto _right = (right);                                                                                         \
         if (!(_left > _right)) {                                                                                       \
-            info("Assertion failed: (" #left " > " #right ")" __VA_OPT__(, ) __VA_ARGS__);                             \
+            info("Assertion failed: (" #left " > " #right ")" __VA_OPT__(": ", ) __VA_ARGS__);                         \
             info("  Left  : ", _left);                                                                                 \
             info("  Right : ", _right);                                                                                \
             std::abort();                                                                                              \
@@ -53,7 +59,7 @@
         auto _left = (left);                                                                                           \
         auto _right = (right);                                                                                         \
         if (!(_left >= _right)) {                                                                                      \
-            info("Assertion failed: (" #left " >= " #right ")" __VA_OPT__(, ) __VA_ARGS__);                            \
+            info("Assertion failed: (" #left " >= " #right ")" __VA_OPT__(": ", ) __VA_ARGS__);                        \
             info("  Left  : ", _left);                                                                                 \
             info("  Right : ", _right);                                                                                \
             std::abort();                                                                                              \
