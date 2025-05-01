75cb8b9fcf07766089ad4bb95a38c4469ecb0b20
lucasxia01 <lucasxia01@gmail.com>
2025-05-01 20:18:22 +0000

    change to testing MegaZKRecursiveFlavor_<UltraCircuitBuilder>

--- barretenberg/cpp/src/barretenberg/stdlib/honk_verifier/ultra_recursive_verifier.test.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/honk_verifier/ultra_recursive_verifier.test.cpp
@@ -255,8 +255,8 @@ template <typename RecursiveFlavor> class RecursiveVerifierTest : public testing
             }
         }
         // Check the size of the recursive verifier
-        if constexpr (std::same_as<Flavor, UltraRecursiveFlavor_<UltraCircuitBuilder>>) {
-            uint32_t NUM_GATES_EXPECTED = 730689;
+        if constexpr (std::same_as<Flavor, MegaZKRecursiveFlavor_<UltraCircuitBuilder>>) {
+            uint32_t NUM_GATES_EXPECTED = 937885;
             BB_ASSERT_EQ(static_cast<uint32_t>(outer_circuit.get_num_finalized_gates()),
                          NUM_GATES_EXPECTED,
                          "UltraHonk Recursive verifier built in Ultra changed in gate count! Update this value if you "
