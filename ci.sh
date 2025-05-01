50bc042988bd6786c84c1a9ec4b7b99e86579868
Charlie Lye <5764343+charlielye@users.noreply.github.com>
2025-05-01 08:17:46 +0000

    ga cmd

--- ci.sh
+++ ci.sh
@@ -98,6 +98,12 @@ case "$cmd" in
     seq 0 $((num - 1)) | parallel --tag --line-buffered \
       'INSTANCE_POSTFIX=${USER}_{} bootstrap_ec2 2>&1 | cache_log "Grind {}"'
     ;;
+  "ga")
+    parallel --tag --line-buffered --halt now,fail=1 denoise "{}" \
+      "JOB_ID=x1 ARCH=amd64 ./ci.sh ec2-test" \
+      "JOB_ID=x2 ARCH=amd64 ./ci.sh ec2-test" \
+      "JOB_ID=a1 ARCH=arm64 ./ci.sh ec2"
+    ;;
   "local")
     # Create container with clone of local repo and bootstrap.
     bootstrap_local "$@"
