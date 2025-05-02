2a4cd3c985f6ec6d38ce782a7525ca739620b8ed
spypsy <spypsy@users.noreply.github.com>
2025-05-02 18:24:37 +0300

    chore: use separate KV store for lip2p peers + logging missing txs  (#14026)

--- yarn-project/p2p/src/client/factory.ts
+++ yarn-project/p2p/src/client/factory.ts
@@ -42,9 +42,9 @@ export const createP2PClient = async <T extends P2PClientType>(
 ) => {
   let config = { ..._config, dataStoreMapSizeKB: _config.p2pStoreMapSizeKb ?? _config.dataStoreMapSizeKB };
   const logger = deps.logger ?? createLogger('p2p');
-  const store = deps.store ?? (await createStore(P2P_STORE_NAME, 1, config, createLogger('p2p:lmdb-v2')));
-  const archive = await createStore(P2P_ARCHIVE_STORE_NAME, 1, config, createLogger('p2p-archive:lmdb-v2'));
-  const peerStore = await createStore(P2P_PEER_STORE_NAME, 1, config, createLogger('p2p-peer:lmdb-v2'));
+  const store = deps.store ?? (await createStore('p2p', 1, config, createLogger('p2p:lmdb-v2')));
+  const archive = await createStore('p2p-archive', 1, config, createLogger('p2p-archive:lmdb-v2'));
+  const peerStore = await createStore('p2p-peers', 1, config, createLogger('p2p-peer:lmdb-v2'));
 
   const mempools: MemPools<T> = {
     txPool:
