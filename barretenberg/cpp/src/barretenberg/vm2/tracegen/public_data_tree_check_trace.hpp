1fb70a4a983da2fd775f833b45ec4d83f5a2b08b
Álvaro Rodríguez <sirasistant@gmail.com>
2025-05-02 22:26:33 +0900

    feat(avm): Evolve public data read to read/write (#13486)
    Evolves the public data read gadget to a checker gadget with both read
    and write. It's very similar to the nullifier one but handling the
    update case instead of failing in that case.
    
    ---------
    
    Co-authored-by: jeanmon <jean@aztec-labs.com>

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/public_data_tree_check_trace.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/public_data_tree_check_trace.hpp
@@ -4,14 +4,14 @@
 
 #include "barretenberg/vm2/generated/columns.hpp"
 #include "barretenberg/vm2/simulation/events/event_emitter.hpp"
-#include "barretenberg/vm2/simulation/events/public_data_tree_read_event.hpp"
+#include "barretenberg/vm2/simulation/events/public_data_tree_check_event.hpp"
 #include "barretenberg/vm2/tracegen/trace_container.hpp"
 
 namespace bb::avm2::tracegen {
 
-class PublicDataTreeReadTraceBuilder final {
+class PublicDataTreeCheckTraceBuilder final {
   public:
-    void process(const simulation::EventEmitterInterface<simulation::PublicDataTreeReadEvent>::Container& events,
+    void process(const simulation::EventEmitterInterface<simulation::PublicDataTreeCheckEvent>::Container& events,
                  TraceContainer& trace);
 
     static std::vector<std::unique_ptr<class InteractionBuilderInterface>> lookup_jobs();
