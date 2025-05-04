2953387517fa4cd0bb59522c293e58cc6e4f3acc
Maximilian Hubert <64627729+gap-editor@users.noreply.github.com>
2025-05-04 20:07:25 +0200

    Update download_grumpkin.sh

--- barretenberg/cpp/srs_db/download_grumpkin.sh
+++ barretenberg/cpp/srs_db/download_grumpkin.sh
@@ -2,4 +2,4 @@
 # TODO(https://github.com/AztecProtocol/barretenberg/issues/898): Grumpkin needs to match new layout.
 set -eu
 cd $(dirname $0)
-./download_srs.sh "TEST%20GRUMPKIN" grumpkin/monomial 1 $@
+./download_srs.sh "TEST%20GRUMPKIN" grumpkin/monomial 0 $@
