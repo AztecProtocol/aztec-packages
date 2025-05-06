74ea31e7955f11c90c4ed9c6a825259919aaf580
jeanmon <jean@aztec-labs.com>
2025-05-06 13:02:29 +0000

    TS - minor changes

--- yarn-project/prover-client/src/orchestrator/orchestrator.ts
+++ yarn-project/prover-client/src/orchestrator/orchestrator.ts
@@ -933,7 +933,7 @@ export class ProvingOrchestrator implements EpochProver {
       async (signal: AbortSignal) => {
         const inputs = txProvingState.getAvmInputs();
         try {
-          return await this.prover.getAvmProof(inputs, true, signal, provingState.epochNumber);
+          return await this.prover.getAvmProof(inputs, false, signal, provingState.epochNumber);
         } catch (err) {
           if (process.env.AVM_PROVING_STRICT) {
             logger.error(`Error thrown when proving AVM circuit with AVM_PROVING_STRICT on`, err);
