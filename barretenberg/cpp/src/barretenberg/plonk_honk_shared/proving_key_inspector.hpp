08184fbc13622a15f5bdea4f227dbe9d45685709
ledwards2225 <98505400+ledwards2225@users.noreply.github.com>
2025-05-07 13:22:59 -0700

    chore: civc debugging utils (#13900)
    Adds some debugging functionality that's been useful to me on a number
    of occasions including
    1. Two tests in AcirIntegrationTest for debugging CIVC from msgpack
    inputs (disabled like all of the others)
    2. A `compute_vk_hash` utility for debugging discrepancies between
    circuits that are expected to be equivalent
    3. Adds `Debug CIVC transaction` config to launch.json to allow quick
    debugging of CIVC w/ msgpack inputs

--- barretenberg/cpp/src/barretenberg/plonk_honk_shared/proving_key_inspector.hpp
+++ barretenberg/cpp/src/barretenberg/plonk_honk_shared/proving_key_inspector.hpp
@@ -8,8 +8,60 @@
 
 #include "barretenberg/common/assert.hpp"
 #include "barretenberg/common/log.hpp"
+#include "barretenberg/flavor/flavor.hpp"
+#include "barretenberg/plonk_honk_shared/execution_trace/mega_execution_trace.hpp"
+#include "barretenberg/ultra_honk/decider_proving_key.hpp"
 
 namespace bb::proving_key_inspector {
+
+// Helper for extracting a native Flavor from either a native or recursive flavor.
+template <typename Flavor, bool = IsRecursiveFlavor<Flavor>> struct NativeFlavorHelper {
+    using type = Flavor;
+};
+template <typename Flavor> struct NativeFlavorHelper<Flavor, true> {
+    using type = typename Flavor::NativeFlavor;
+};
+
+/**
+ * @brief Compute the hash of the verification key that results from constructing a proving key from the given circuit.
+ * @details This is useful for identifying the point of divergence for two circuits that are expected to be identical,
+ * for example, the circuit constructed from a given acir program with or without a genuine witness.
+ *
+ * @tparam Flavor Determines the type of PK and VK to be constructed.
+ * @tparam Builder The builder for the circuit in question.
+ */
+template <typename Flavor, typename Builder>
+uint256_t compute_vk_hash(const Builder& circuit_in,
+                          const TraceSettings& trace_settings = TraceSettings{ AZTEC_TRACE_STRUCTURE })
+    requires(IsMegaFlavor<Flavor> && IsMegaBuilder<Builder>)
+{
+    using NativeFlavor = typename NativeFlavorHelper<Flavor>::type;
+    using DeciderProvingKey = typename bb::DeciderProvingKey_<NativeFlavor>;
+    using VerificationKey = NativeFlavor::VerificationKey;
+
+    Builder circuit = circuit_in; // Copy the circuit to avoid modifying the original
+
+    // Avoid hitting debugging ASSERTs by correctly setting the pairing point accumulator public component key idx
+    uint32_t pairing_points_idx = static_cast<uint32_t>(circuit.public_inputs.size()) - PAIRING_POINTS_SIZE;
+    if (circuit.databus_propagation_data.is_kernel) {
+        pairing_points_idx -= PROPAGATED_DATABUS_COMMITMENTS_SIZE;
+    }
+    circuit.pairing_inputs_public_input_key.start_idx = pairing_points_idx;
+
+    DeciderProvingKey proving_key{ circuit, trace_settings };
+    VerificationKey verification_key{ proving_key.proving_key };
+
+    return verification_key.hash();
+}
+
+// A catch-all for Flavor/Builder combinations where the VK hash is not implemented.
+template <typename Flavor, typename Builder>
+uint256_t compute_vk_hash(const Builder&, const TraceSettings& = TraceSettings{ AZTEC_TRACE_STRUCTURE })
+    requires(!IsMegaFlavor<Flavor> || !IsMegaBuilder<Builder>)
+{
+    info("compute_vk_hash: Not implemented for this Flavor/Builder, returning 0.");
+    return 0;
+}
 
 // Determine whether a polynomial has at least one non-zero coefficient
 bool is_non_zero(auto& polynomial)
