63e51a077273f12585c8167ab19884322774399f
jeanmon <jean@aztec-labs.com>
2025-05-09 15:27:28 +0000

    Create a TS unit test for storing snapshot in a json file

--- yarn-project/simulator/src/public/public_tx_simulator/apps_tests/avm_minimal.test.ts
+++ yarn-project/simulator/src/public/public_tx_simulator/apps_tests/avm_minimal.test.ts
@@ -1,3 +1,7 @@
+import { jsonParseWithSchema, jsonStringify } from '@aztec/foundation/json-rpc';
+import { readTestData, writeTestData } from '@aztec/foundation/testing/files';
+import { AvmCircuitInputs } from '@aztec/stdlib/avm';
+
 import { createAvmMinimalPublicTx } from '../../fixtures/minimal_public_tx.js';
 
 describe('Public TX simulator apps tests: AvmMinimalTestContract', () => {
@@ -5,5 +9,20 @@ describe('Public TX simulator apps tests: AvmMinimalTestContract', () => {
     const result = await createAvmMinimalPublicTx();
     expect(result.revertCode.isOK()).toBe(true);
     expect(result.avmProvingRequest.inputs).toMatchSnapshot();
+  });
+
+  it('Minimal Tx avm inputs snapshot stored in Json file', async () => {
+    const result = await createAvmMinimalPublicTx();
+    expect(result.revertCode.isOK()).toBe(true);
+    const inputs = result.avmProvingRequest.inputs;
+    const json = jsonStringify(inputs);
+
+    // Run with AZTEC_GENERATE_TEST_DATA=1 to update test data
+    const path = 'yarn-project/simulator/src/public/public_tx_simulator/avm_minimal_inputs.json';
+    writeTestData(path, Buffer.from(json), /*raw=*/ true);
+
+    const expectedJson = readTestData(path);
+    const expectedAvmInputs = await jsonParseWithSchema(expectedJson.toString(), AvmCircuitInputs.schema);
+    expect(expectedAvmInputs).toEqual(inputs);
   });
 });
