52e33e8594e568ec647ab38bcdd0b06923abfdee
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2025-05-12 16:21:24 +0000

    chore: fix requested changes

--- l1-contracts/test/validator-selection/setupSampleSeed.t.sol
+++ l1-contracts/test/validator-selection/setupSampleSeed.t.sol
@@ -11,7 +11,7 @@ import {TestConstants} from "../harnesses/TestConstants.sol";
 contract SetupSampleSeedTest is ValidatorSelectionTestBase {
   function test_setupSampleSeed(uint16 _epochToTest) public setup(4) {
     // Check that the epoch is not set
-    bound(_epochToTest, 2, type(uint16).max);
+    _epochToTest = uint16(bound(_epochToTest, 2, type(uint16).max));
 
     uint256 timejump = _epochToTest * TestConstants.AZTEC_EPOCH_DURATION;
     uint256 originalSampleSeed = rollup.getSampleSeedAt(Timestamp.wrap(block.timestamp + timejump));
