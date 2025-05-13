21b8f90025ee4104b1ef8a6550691dfc48d7b54b
IlyasRidhuan <ilyasridhuan@gmail.com>
2025-05-13 08:57:02 +0000

    feat: tx trace

--- barretenberg/cpp/src/barretenberg/vm2/simulation/execution_components.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/simulation/execution_components.cpp
@@ -6,46 +6,6 @@
 
 namespace bb::avm2::simulation {
 
-std::unique_ptr<ContextInterface> ExecutionComponentsProvider::make_nested_context(AztecAddress address,
-                                                                                   AztecAddress msg_sender,
-                                                                                   ContextInterface& parent_context,
-                                                                                   MemoryAddress cd_offset_address,
-                                                                                   MemoryAddress cd_size_address,
-                                                                                   bool is_static,
-                                                                                   Gas gas_limit)
-{
-    uint32_t context_id = next_context_id++;
-    return std::make_unique<NestedContext>(context_id,
-                                           address,
-                                           msg_sender,
-                                           is_static,
-                                           gas_limit,
-                                           std::make_unique<BytecodeManager>(address, tx_bytecode_manager),
-                                           std::make_unique<Memory>(context_id, range_check, memory_events),
-                                           parent_context,
-                                           cd_offset_address,
-                                           cd_size_address);
-}
-
-std::unique_ptr<ContextInterface> ExecutionComponentsProvider::make_enqueued_context(AztecAddress address,
-                                                                                     AztecAddress msg_sender,
-                                                                                     std::span<const FF> calldata,
-                                                                                     bool is_static,
-                                                                                     Gas gas_limit,
-                                                                                     Gas gas_used)
-{
-    uint32_t context_id = next_context_id++;
-    return std::make_unique<EnqueuedCallContext>(context_id,
-                                                 address,
-                                                 msg_sender,
-                                                 is_static,
-                                                 gas_limit,
-                                                 gas_used,
-                                                 std::make_unique<BytecodeManager>(address, tx_bytecode_manager),
-                                                 std::make_unique<Memory>(context_id, range_check, memory_events),
-                                                 calldata);
-}
-
 std::unique_ptr<AddressingInterface> ExecutionComponentsProvider::make_addressing(AddressingEvent& event)
 {
     auto event_emitter = std::make_unique<OneShotEventEmitter<AddressingEvent>>(event);
