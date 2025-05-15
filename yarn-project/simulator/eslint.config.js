dc0f578d825a28d525a365016f3f6b23be300da0
thunkar <gregojquiros@gmail.com>
2025-05-15 08:14:00 +0000

    sim fix

--- yarn-project/simulator/eslint.config.js
+++ yarn-project/simulator/eslint.config.js
@@ -1,3 +1,11 @@
 import config from '@aztec/foundation/eslint';
 
-export default config;
+export default [
+  ...config,
+  {
+    files: ['src/public/avm/opcodes/*.ts'],
+    rules: {
+      'require-await': 'off',
+    },
+  },
+];
