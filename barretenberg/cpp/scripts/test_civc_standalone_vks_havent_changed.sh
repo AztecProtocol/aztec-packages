4a2361477f31c5bfbaea6e5521cb389a9ad2495d
lucasxia01 <lucasxia01@gmail.com>
2025-05-19 20:51:28 +0000

    ignore standalone vk test

--- barretenberg/cpp/scripts/test_civc_standalone_vks_havent_changed.sh
+++ barretenberg/cpp/scripts/test_civc_standalone_vks_havent_changed.sh
@@ -13,16 +13,16 @@ pinned_civc_inputs_url="https://aztec-ci-artifacts.s3.us-east-2.amazonaws.com/pr
 export inputs_tmp_dir=$(mktemp -d)
 trap 'rm -rf "$inputs_tmp_dir"' EXIT SIGINT
 
-curl -s -f "$pinned_civc_inputs_url" | tar -xzf - -C "$inputs_tmp_dir" &>/dev/null
+# curl -s -f "$pinned_civc_inputs_url" | tar -xzf - -C "$inputs_tmp_dir" &>/dev/null
 
-function check_circuit_vks {
-  set -eu
-  local flow_folder="$inputs_tmp_dir/$1"
-  ./build/bin/bb check --scheme client_ivc --ivc_inputs_path "$flow_folder/ivc-inputs.msgpack" || { echo_stderr "Error: Likely VK change detected in $flow_folder!"; exit 1; }
-}
+# function check_circuit_vks {
+#   set -eu
+#   local flow_folder="$inputs_tmp_dir/$1"
+#   ./build/bin/bb check --scheme client_ivc --ivc_inputs_path "$flow_folder/ivc-inputs.msgpack" || { echo_stderr "Error: Likely VK change detected in $flow_folder!"; exit 1; }
+# }
 
-export -f check_circuit_vks
+# export -f check_circuit_vks
 
-# Run on one public and one private input.
-ls "$inputs_tmp_dir"
-# parallel -v --line-buffer --tag check_circuit_vks {} ::: $(ls "$inputs_tmp_dir")
+# # Run on one public and one private input.
+# ls "$inputs_tmp_dir"
+# # parallel -v --line-buffer --tag check_circuit_vks {} ::: $(ls "$inputs_tmp_dir")
