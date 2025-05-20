f3b8a84529504718e3a37572ba1e5182a2c1ac6b
DanielKotov <katod.daniel@gmail.com>
2025-05-20 11:46:18 +0000

    removing false-cases for finalizing method

--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/circuit_builder_base_impl.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/circuit_builder_base_impl.hpp
@@ -92,7 +92,7 @@ template <typename FF_> uint32_t CircuitBuilderBase<FF_>::add_variable(const FF&
 {
     variables.emplace_back(in);
     const uint32_t index = static_cast<uint32_t>(variables.size()) - 1U;
-    if (index == 475151) {
+    if (index == 2548218) {
         info("AAAAAAAAAAAAAAAA");
     }
     real_variable_index.emplace_back(index);
