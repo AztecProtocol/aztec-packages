04dfa27b9728f2cb2b8d5f53227cd34b5fb80739
ledwards2225 <98505400+ledwards2225@users.noreply.github.com>
2025-05-20 10:40:08 -0700

    chore: simplify ivc mocking (#14197)
    Cleaning up a bit of leftover tech debt: To construct
    `ivc_recursion_constraints` in the absence of real witnesses we need to
    mock verification queue entries (PG/oink proofs, VKs). This was being
    done outside of `create_circuit` in a couple of places which was pattern
    breaking and unnecessary. It's now done inside
    `process_ivc_recursion_constraints`.

--- barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.cpp
+++ barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.cpp
@@ -6,6 +6,7 @@
 
 #include "barretenberg/client_ivc/client_ivc.hpp"
 #include "barretenberg/common/op_count.hpp"
+#include "barretenberg/honk/proving_key_inspector.hpp"
 #include "barretenberg/serialize/msgpack_impl.hpp"
 #include "barretenberg/ultra_honk/oink_prover.hpp"
 
