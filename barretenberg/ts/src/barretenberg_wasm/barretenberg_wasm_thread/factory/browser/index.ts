7ca8777acd5f11001b6440a712d32c60eb178c50
thunkar <gregojquiros@gmail.com>
2025-05-20 14:25:26 +0000

    whoops

--- barretenberg/ts/src/barretenberg_wasm/barretenberg_wasm_thread/factory/browser/index.ts
+++ barretenberg/ts/src/barretenberg_wasm/barretenberg_wasm_thread/factory/browser/index.ts
@@ -1,6 +1,8 @@
 import { readinessListener } from '../../../helpers/browser/index.js';
 
 export async function createThreadWorker() {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   const worker = new Worker(new URL('./thread.worker.js', import.meta.url));
   await new Promise<void>(resolve => readinessListener(worker, resolve));
   return worker;
