12dbd126b727a82bf77d3ebbd674e86e6dba51a5
thunkar <gregojquiros@gmail.com>
2025-05-21 09:50:45 +0000

    default level

--- barretenberg/ts/src/log.ts
+++ barretenberg/ts/src/log.ts
@@ -2,8 +2,9 @@ import { pino } from 'pino';
 
 export const logger = pino({
   name: 'bb.js',
+  useOnlyCustomLevels: false,
   customLevels: { verbose: 25 },
-  level: process.env.LOG_LEVEL,
+  level: process.env.LOG_LEVEL ?? 'info',
   browser: { asObject: false },
 });
 
