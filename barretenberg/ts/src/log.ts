f908b9207d0faacfd2b8b5a1244e424b2cbba6a1
thunkar <gregojquiros@gmail.com>
2025-05-21 09:36:41 +0000

    customlevels

--- barretenberg/ts/src/log.ts
+++ barretenberg/ts/src/log.ts
@@ -1,6 +1,11 @@
 import { pino } from 'pino';
 
-export const logger = pino({ name: 'bb.js', level: process.env.LOG_LEVEL, browser: { asObject: false } });
+export const logger = pino({
+  name: 'bb.js',
+  customLevels: { verbose: 25 },
+  level: process.env.LOG_LEVEL,
+  browser: { asObject: false },
+});
 
 export function createChildLogger(name: string) {
   const sublogger = logger.child({
