d19a446c4e0c21c4a2ff014e499272b79d274aad
thunkar <gregojquiros@gmail.com>
2025-05-22 18:11:20 +0000

    fixed logging

--- barretenberg/ts/src/log/node/index.ts
+++ barretenberg/ts/src/log/node/index.ts
@@ -1,4 +1,4 @@
-import { pino } from 'pino';
+import { Logger, pino } from 'pino';
 import { LogLevels, LogOptions } from '../types.js';
 
 const defaultOptions = {
@@ -9,12 +9,33 @@ const defaultOptions = {
 
 const defaultLevel = (process.env.LOG_LEVEL || 'info') as LogLevels;
 
+// Options must be exposed so they can be provided to threads upon creation
+// This way we ensure all loggers are spawned with the same options
+export let logOptions: LogOptions | undefined;
+
+let logger: Logger<'verbose'> | undefined;
+
 export function initLogger(
   { level = defaultLevel, useStdErr = false }: LogOptions = { level: defaultLevel, useStdErr: false },
 ) {
+  if (logger) {
+    return logger;
+  }
+  logOptions = { level, useStdErr };
   const transport = pino.transport({
     target: 'pino/file',
     options: { destination: useStdErr ? 2 : 1 },
   });
-  return { logger: pino({ ...defaultOptions, level }, transport), options: { level, useStdErr } };
+  logger = pino({ ...defaultOptions, level }, transport);
+}
+
+export function createDebugLogger(name: string) {
+  initLogger();
+
+  const sublogger = logger!.child({
+    name,
+  });
+  return (msg: string) => {
+    sublogger.debug(msg);
+  };
 }
