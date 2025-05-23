58595e35309b9db38c40f5a7d3a577ff947c59f6
thunkar <gregojquiros@gmail.com>
2025-05-23 13:24:18 +0000

    record oracles

--- yarn-project/simulator/src/client.ts
+++ yarn-project/simulator/src/client.ts
@@ -1,4 +1,6 @@
 export * from './private/index.js';
 export { WASMSimulator } from './private/providers/acvm_wasm.js';
+export { SimulationProviderRecorderWrapper } from './private/providers/circuit_recording/simulation_provider_recorder_wrapper.js';
+export { MemoryCircuitRecorder } from './private/providers/circuit_recording/memory_circuit_recorder.js';
 export { type SimulationProvider, type DecodedError } from './private/providers/simulation_provider.js';
 export * from './common/index.js';
