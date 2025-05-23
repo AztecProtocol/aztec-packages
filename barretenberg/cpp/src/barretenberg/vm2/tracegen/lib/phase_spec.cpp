6f5a3f36e40dd8d86d163d8603b19861a776612b
IlyasRidhuan <ilyasridhuan@gmail.com>
2025-05-23 14:46:37 +0000

    fix: comments

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/lib/phase_spec.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/lib/phase_spec.cpp
@@ -7,77 +7,71 @@
 #include <vector>
 
 namespace bb::avm2::tracegen {
-
 namespace {
-// TODO: check back after public inputs are done
+
 const std::unordered_map<TransactionPhase, TxPhaseOffsetsTable::Offsets> PHASE_PUB_INPUTS_OFFSETS = { {
     { TransactionPhase::SETUP,
       {
-          /*read_offset=*/AVM_PUBLIC_INPUTS_PUBLIC_SETUP_CALL_REQUESTS_ROW_IDX,
-          /*write_offset=*/0, // Write offset is 0 for public call requests
-          /*length_offset=*/AVM_PUBLIC_INPUTS_PUBLIC_CALL_REQUEST_ARRAY_LENGTHS_SETUP_CALLS_ROW_IDX,
+          .read_pi_offset = AVM_PUBLIC_INPUTS_PUBLIC_SETUP_CALL_REQUESTS_ROW_IDX,
+          .write_pi_offset = 0, // Write offset is 0 for public call requests
+          .read_pi_length_offset = AVM_PUBLIC_INPUTS_PUBLIC_CALL_REQUEST_ARRAY_LENGTHS_SETUP_CALLS_ROW_IDX,
       } },
     { TransactionPhase::APP_LOGIC,
       {
-          /*read_offset=*/AVM_PUBLIC_INPUTS_PUBLIC_APP_LOGIC_CALL_REQUESTS_ROW_IDX,
-          /*write_offset=*/0, // Write offset is 0 for public call requests
-          /*length_offset=*/AVM_PUBLIC_INPUTS_PUBLIC_CALL_REQUEST_ARRAY_LENGTHS_APP_LOGIC_CALLS_ROW_IDX,
+          .read_pi_offset = AVM_PUBLIC_INPUTS_PUBLIC_APP_LOGIC_CALL_REQUESTS_ROW_IDX,
+          .write_pi_offset = 0, // Write offset is 0 for public call requests
+          .read_pi_length_offset = AVM_PUBLIC_INPUTS_PUBLIC_CALL_REQUEST_ARRAY_LENGTHS_APP_LOGIC_CALLS_ROW_IDX,
       } },
     { TransactionPhase::TEARDOWN,
       {
-          /*read_offset=*/AVM_PUBLIC_INPUTS_PUBLIC_TEARDOWN_CALL_REQUEST_ROW_IDX,
-          /*write_offset=*/0, // Write offset is 0 for public call requests
-          /*length_offset=*/AVM_PUBLIC_INPUTS_PUBLIC_CALL_REQUEST_ARRAY_LENGTHS_TEARDOWN_CALL_ROW_IDX,
+          .read_pi_offset = AVM_PUBLIC_INPUTS_PUBLIC_TEARDOWN_CALL_REQUEST_ROW_IDX,
+          .write_pi_offset = 0, // Write offset is 0 for public call requests
+          .read_pi_length_offset = AVM_PUBLIC_INPUTS_PUBLIC_CALL_REQUEST_ARRAY_LENGTHS_TEARDOWN_CALL_ROW_IDX,
       } },
     { TransactionPhase::NR_NOTE_INSERTION,
       {
-          /*read_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX,
-          /*write_offset=*/AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX,
-          /*length_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
+          .read_pi_offset = AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX,
+          .write_pi_offset = AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX,
+          .read_pi_length_offset = AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
       } },
     { TransactionPhase::NR_NULLIFIER_INSERTION,
       {
-          /*read_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_NULLIFIERS_ROW_IDX,
-          /*write_offset=*/AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NULLIFIERS_ROW_IDX,
-          /*length_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
+          .read_pi_offset = AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_NULLIFIERS_ROW_IDX,
+          .write_pi_offset = AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NULLIFIERS_ROW_IDX,
+          .read_pi_length_offset = AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
       } },
     { TransactionPhase::R_NOTE_INSERTION,
       {
-          /*read_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX,
-          /*write_offset=*/AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX,
-          /*length_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
+          .read_pi_offset = AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX,
+          .write_pi_offset = AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX,
+          .read_pi_length_offset = AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
       } },
     { TransactionPhase::R_NULLIFIER_INSERTION,
       {
-          /*read_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_NULLIFIERS_ROW_IDX,
-          /*write_offset=*/AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NULLIFIERS_ROW_IDX,
-          /*length_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
+          .read_pi_offset = AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_NULLIFIERS_ROW_IDX,
+          .write_pi_offset = AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NULLIFIERS_ROW_IDX,
+          .read_pi_length_offset = AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
       } },
     { TransactionPhase::NR_L2_TO_L1_MESSAGE,
       {
-          /*read_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_L2_TO_L1_MSGS_ROW_IDX,
-          /*write_offset=*/AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_L2_TO_L1_MSGS_ROW_IDX,
-          /*length_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
+          .read_pi_offset = AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_L2_TO_L1_MSGS_ROW_IDX,
+          .write_pi_offset = AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_L2_TO_L1_MSGS_ROW_IDX,
+          .read_pi_length_offset = AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
       } },
     { TransactionPhase::R_L2_TO_L1_MESSAGE,
       {
-          /*read_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_L2_TO_L1_MSGS_ROW_IDX,
-          /*write_offset=*/AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_L2_TO_L1_MSGS_ROW_IDX,
-          /*length_offset=*/AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
+          .read_pi_offset = AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_L2_TO_L1_MSGS_ROW_IDX,
+          .write_pi_offset = AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_L2_TO_L1_MSGS_ROW_IDX,
+          .read_pi_length_offset = AVM_PUBLIC_INPUTS_PREVIOUS_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_ROW_IDX,
       } },
 
 } };
 } // namespace
 
-const TxPhaseOffsetsTable::Offsets& TxPhaseOffsetsTable::get_offsets(TransactionPhase phase) const
+const TxPhaseOffsetsTable::Offsets& TxPhaseOffsetsTable::get_offsets(TransactionPhase phase)
 {
+    assert(PHASE_PUB_INPUTS_OFFSETS.contains(phase));
     return PHASE_PUB_INPUTS_OFFSETS.at(phase);
-}
-
-const TxPhaseOffsetsTable& TxPhaseOffsetsTable::get()
-{
-    static const TxPhaseOffsetsTable table;
-    return table;
 }
 
 } // namespace bb::avm2::tracegen
