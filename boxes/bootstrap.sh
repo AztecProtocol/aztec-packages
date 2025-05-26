be96cb98adbc47dc15ee1dba52f8048ded4bc178
saleel <saleel@saleel.xyz>
2025-05-26 16:07:57 +0000

    update test setup

--- boxes/bootstrap.sh
+++ boxes/bootstrap.sh
@@ -32,10 +32,8 @@ function test {
 }
 
 function test_cmds {
-  for browser in chromium webkit firefox; do
-    for box in vanilla react vite; do
-      echo "$hash:ONLY_TERM_PARENT=1 BOX=$box BROWSER=$browser run_compose_test $box-$browser box boxes"
-    done
+  for box in vanilla react vite; do
+      echo "$hash:ONLY_TERM_PARENT=1 BOX=$box run_compose_test $box box boxes"
   done
 }
 
