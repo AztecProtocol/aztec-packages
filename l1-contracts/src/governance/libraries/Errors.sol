30bb7dcb0c6631d096ea479d0660f4fa89c9c92a
LHerskind <16536249+LHerskind@users.noreply.github.com>
2025-05-28 16:09:37 +0000

    feat!: GovernanceProposer look at GSE

--- l1-contracts/src/governance/libraries/Errors.sol
+++ l1-contracts/src/governance/libraries/Errors.sol
@@ -58,6 +58,7 @@ library Errors {
   error GovernanceProposer__ProposalHaveNoCode(IPayload proposal); // 0xb69440a1
   error GovernanceProposer__ProposalTooOld(uint256 roundNumber, uint256 currentRoundNumber); // 0xc3d7aa4f
   error GovernanceProposer__VoteAlreadyCastForSlot(Slot slot); // 0x3a6150ca
+  error GovernanceProposer__GSEPayloadInvalid();
 
   error CoinIssuer__InsufficientMintAvailable(uint256 available, uint256 needed); // 0xa1cc8799
 
--- l1-contracts/test/governance/governance-proposer/constructor.t.sol
+++ l1-contracts/test/governance/governance-proposer/constructor.t.sol
@@ -5,6 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {GovernanceProposer} from "@aztec/governance/proposer/GovernanceProposer.sol";
 import {Errors} from "@aztec/governance/libraries/Errors.sol";
 import {IRegistry} from "@aztec/governance/interfaces/IRegistry.sol";
+import {IGSE} from "@aztec/core/staking/GSE.sol";
 
 contract FakeRegistry {
   function getGovernance() external pure returns (address) {
@@ -18,6 +19,7 @@ contract FakeRegistry {
 
 contract ConstructorTest is Test {
   IRegistry internal REGISTRY = IRegistry(address(new FakeRegistry()));
+  IGSE internal GSE = IGSE(address(0x03));
 
   function test_WhenNIsLessThanOrEqualHalfOfM(uint256 _n, uint256 _m) external {
     // it revert
@@ -27,7 +29,7 @@ contract ConstructorTest is Test {
     vm.expectRevert(
       abi.encodeWithSelector(Errors.GovernanceProposer__InvalidNAndMValues.selector, n, _m)
     );
-    new GovernanceProposer(REGISTRY, n, _m);
+    new GovernanceProposer(REGISTRY, GSE, n, _m);
   }
 
   function test_WhenNLargerThanM(uint256 _n, uint256 _m) external {
@@ -38,7 +40,7 @@ contract ConstructorTest is Test {
     vm.expectRevert(
       abi.encodeWithSelector(Errors.GovernanceProposer__NCannotBeLargerTHanM.selector, n, m)
     );
-    new GovernanceProposer(REGISTRY, n, m);
+    new GovernanceProposer(REGISTRY, GSE, n, m);
   }
 
   function test_WhenNIsGreatherThanHalfOfM(uint256 _n, uint256 _m) external {
@@ -47,7 +49,7 @@ contract ConstructorTest is Test {
     uint256 m = bound(_m, 1, type(uint256).max);
     uint256 n = bound(_n, m / 2 + 1, m);
 
-    GovernanceProposer g = new GovernanceProposer(REGISTRY, n, m);
+    GovernanceProposer g = new GovernanceProposer(REGISTRY, GSE, n, m);
 
     assertEq(address(g.REGISTRY()), address(REGISTRY));
     assertEq(g.N(), n);
