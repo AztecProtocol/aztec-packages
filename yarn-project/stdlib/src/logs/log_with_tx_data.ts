4d1a1c6212f18639fa4acb5e62849c8dd463853a
benesjan <janbenes1234@gmail.com>
2025-05-29 12:08:35 +0000

    last touches

--- yarn-project/stdlib/src/logs/log_with_tx_data.ts
+++ yarn-project/stdlib/src/logs/log_with_tx_data.ts
@@ -22,6 +22,7 @@ class LogWithTxData<N extends number> {
   }
 }
 
+// This is used as a response for PXE's custom getPublicLogByTag oracle.
 export class PublicLogWithTxData extends LogWithTxData<typeof PUBLIC_LOG_PLAINTEXT_LEN> {
   constructor(logContent: Fr[], txHash: TxHash, uniqueNoteHashesInTx: Fr[], firstNullifierInTx: Fr) {
     super(logContent, txHash, uniqueNoteHashesInTx, firstNullifierInTx, PUBLIC_LOG_PLAINTEXT_LEN);
@@ -32,6 +33,7 @@ export class PublicLogWithTxData extends LogWithTxData<typeof PUBLIC_LOG_PLAINTE
   }
 }
 
+// This is used as a response for PXE's custom getPrivateLogByTag oracle.
 export class PrivateLogWithTxData extends LogWithTxData<typeof PRIVATE_LOG_CIPHERTEXT_LEN> {
   constructor(logContent: Fr[], txHash: TxHash, uniqueNoteHashesInTx: Fr[], firstNullifierInTx: Fr) {
     super(logContent, txHash, uniqueNoteHashesInTx, firstNullifierInTx, PRIVATE_LOG_CIPHERTEXT_LEN);
