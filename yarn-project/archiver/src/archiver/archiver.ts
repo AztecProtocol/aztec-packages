c511f727b44385e1e29e7fa87072ba76d88f23e3
Alex Gherghisan <alexghr@users.noreply.github.com>
2025-05-29 11:50:30 +0100

    test: fix flakey test (#14592)
    This PR attempts to fix the flakey `epochs_empty_blocks` test. There
    were a couple of problems with this test resulting from #13428:
    - the test assumes it will run for N consecutive epochs
    - but the test also waits for certain blocks to become
    proven/finalised/historical
    - in #13428 the meaning of 'finalised' was changed. This caused the test
    to _skip_ entire epochs (e.g. entire epochs would be proven in the
    background while it waited for historical blocks)
    - this PR replicates the 'finalised' logic from the archiver so that the
    epoch counter updates correctly

--- yarn-project/archiver/src/archiver/archiver.ts
+++ yarn-project/archiver/src/archiver/archiver.ts
@@ -1155,6 +1155,7 @@ export class Archiver extends (EventEmitter as new () => ArchiverEmitter) implem
 
     // TODO(#13569): Compute proper finalized block number based on L1 finalized block.
     // We just force it 2 epochs worth of proven data for now.
+    // NOTE: update end-to-end/src/e2e_epochs/epochs_empty_blocks.test.ts as that uses finalised blocks in computations
     const finalizedBlockNumber = Math.max(provenBlockNumber - this.l1constants.epochDuration * 2, 0);
 
     const [latestBlockHeader, provenBlockHeader, finalizedBlockHeader] = await Promise.all([
