2354117225e47a15c3dea3fc4ce69aac44693bb7
Gregorio Juliana <gregojquiros@gmail.com>
2025-05-31 12:15:21 +0200

    Update yarn-project/aztec.js/src/contract/interaction_options.ts
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>

--- yarn-project/aztec.js/src/contract/interaction_options.ts
+++ yarn-project/aztec.js/src/contract/interaction_options.ts
@@ -42,7 +42,8 @@ export type SimulateMethodOptions = Pick<
   skipTxValidation?: boolean;
   /** Whether to ensure the fee payer is not empty and has enough balance to pay for the fee. */
   skipFeeEnforcement?: boolean;
-  /** Whether to include statistics in the simulation result, instead of just the return of the function */
+  /** Whether to include performance statistics (e.g. timing information of the different circuits and oracles) in
+   * the simulation result, instead of just the return value of the function */
   includeStats?: boolean;
 };
 
