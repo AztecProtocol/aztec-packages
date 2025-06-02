fdf42cc944377907bd0f3e476219ffe3ece2dcdf
saleel <saleel@saleel.xyz>
2025-06-02 12:03:56 +0000

    disable firefox

--- boxes/boxes/vanilla/playwright.config.ts
+++ boxes/boxes/vanilla/playwright.config.ts
@@ -27,10 +27,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
   ],
 
   webServer: {
