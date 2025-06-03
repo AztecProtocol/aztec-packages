1a16d7a334da6249fdd669c1dd7d68af7f4b1f98
MirandaWood <miranda@aztecprotocol.com>
2025-06-03 12:02:54 +0000

    chore: revert fix to epochs test (no longer req)

--- yarn-project/end-to-end/src/e2e_epochs/epochs_empty_blocks.test.ts
+++ yarn-project/end-to-end/src/e2e_epochs/epochs_empty_blocks.test.ts
@@ -69,9 +69,6 @@ describe('e2e_epochs/epochs_empty_blocks', () => {
 
       // Verify the state syncs
       await test.waitForNodeToSync(provenBlockNumber, 'proven');
-      // TODO(MW): While waiting for the node to sync, the proven block number can overtake what we expect,
-      // hence updated below - TODO should rework timings to ensure this test covers all blocks?
-      provenBlockNumber = (await test.context.aztecNode.getWorldStateSyncStatus()).finalisedBlockNumber;
       await test.verifyHistoricBlock(provenBlockNumber, true);
 
       // right now finalisation means a block is two L2 epochs deep. If this rule changes then we need this test needs to be updated
