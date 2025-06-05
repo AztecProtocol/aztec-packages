51fe0f08218d3c89d224e5ac3cfdfcbe8d3fa85d
dbanks12 <david@aztec-labs.com>
2025-06-05 15:08:40 +0000

    new client prover test to run on arm instead of prover full

--- yarn-project/end-to-end/bootstrap.sh
+++ yarn-project/end-to-end/bootstrap.sh
@@ -16,7 +16,13 @@ function test_cmds {
 
   # Longest-running tests first
   # Can't run full prover tests on ARM because AVM is disabled.
-  if [ "${DISABLE_AZTEC_VM:-0}" -eq 0 ]; then
+  if [ "${DISABLE_AZTEC_VM:-0}" -eq 1 ]; then
+    if [ "$CI_FULL" -eq 1 ]; then
+      echo "$prefix:TIMEOUT=15m:CPUS=16:MEM=96g:NAME=e2e_prover_client_real $run_test_script simple e2e_prover/client"
+    else
+      echo "$prefix:NAME=e2e_prover_client_fake FAKE_PROOFS=1 $run_test_script simple e2e_prover/client"
+    fi
+  else
     if [ "$CI_FULL" -eq 1 ]; then
       echo "$prefix:TIMEOUT=15m:CPUS=16:MEM=96g:NAME=e2e_prover_full_real $run_test_script simple e2e_prover/full"
     else
