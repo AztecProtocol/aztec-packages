1a212297ace82f271df3e9d7f7853b83f3ffd34a
ludamad <domuradical@gmail.com>
2025-06-06 22:03:08 +0000

    hash separator

--- noir-projects/noir-protocol-circuits/bootstrap.sh
+++ noir-projects/noir-protocol-circuits/bootstrap.sh
@@ -94,7 +94,9 @@ function compile {
   # TODO: Change this to add verification_key to original json, like contracts does.
   # Will require changing TS code downstream.
   bytecode_hash=$(jq -r '.bytecode' $json_path | sha256sum | tr -d ' -')
-  hash=$(hash_str "$BB_HASH-$bytecode_hash-$proto")
+  # TODO(AD): If this helps, we have a cache bug...
+  cache_separator="post-14875"
+  hash=$(hash_str "$BB_HASH-$bytecode_hash-$proto-$cache_separator")
   if ! cache_download vk-$hash.tar.gz 1>&2; then
     local key_path="$key_dir/$name.vk.data.json"
     echo_stderr "Generating vk for function: $name..."
