df31742c6e7365954c1f1710c38655384bac04c0
sirasistant <sirasistant@gmail.com>
2025-06-06 14:40:33 +0000

    add some tests

--- barretenberg/cpp/src/barretenberg/serialize/msgpack_impl/uint_128_t_adaptor.hpp
+++ barretenberg/cpp/src/barretenberg/serialize/msgpack_impl/uint_128_t_adaptor.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #define MSGPACK_NO_BOOST
-#define MSGPACK_USE_STD_VARIANT_ADAPTOR
 #include <msgpack.hpp>
 
 #include "barretenberg/common/throw_or_abort.hpp"
@@ -15,12 +14,21 @@ template <> struct convert<uint128_t> {
         if (o.type == msgpack::type::POSITIVE_INTEGER) {
             v = static_cast<uint128_t>(o.via.u64);
         } else if (o.type == msgpack::type::STR) {
-            // When the bigint is too large to fit in a u64, msgpackr will serialize it as a string.
+            // When the bigint is too large to fit in a u64, msgpackr will serialize it as a digits string.
             // Configured on the TS side with largeBigIntToString: true.
             uint128_t result = 0;
+            // 2**128 is 39 digits long in base 10.
+            if (o.via.str.size > 39) {
+                throw_or_abort("uint128_t deserialization failed: string too long");
+            }
 
             for (size_t i = 0; i < o.via.str.size; ++i) {
-                result = result * 10 + (static_cast<uint128_t>(o.via.str.ptr[i] - '0'));
+                char c = o.via.str.ptr[i];
+                if (c < '0' || c > '9') {
+                    throw_or_abort("uint128_t deserialization failed: Non-digit character in input");
+                }
+
+                result = result * 10 + (static_cast<uint128_t>(c - '0'));
             }
 
             v = result;
