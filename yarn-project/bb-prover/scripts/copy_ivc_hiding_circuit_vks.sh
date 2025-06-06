fa801bb603d46025c36c88a28339003ebdd93661
ludamad <domuradical@gmail.com>
2025-06-06 18:20:56 +0000

    fix

--- yarn-project/bb-prover/scripts/copy_ivc_hiding_circuit_vks.sh
+++ yarn-project/bb-prover/scripts/copy_ivc_hiding_circuit_vks.sh
@@ -7,8 +7,8 @@ mkdir -p ../artifacts
 private_tail_vk=../../../noir-projects/noir-protocol-circuits/target/keys/private_kernel_tail.ivc.vk
 private_to_public_tail_vk=../../../noir-projects/noir-protocol-circuits/target/keys/private_kernel_tail_to_public.ivc.vk
 if [[ -f "$private_tail_vk" && -f "$private_to_public_tail_vk" ]]; then
-  cp "$private_tail_vk" artifacts/private-civc-vk
-  cp "$private_to_public_tail_vk" artifacts/public-civc-vk
+  cp "$private_tail_vk" ../artifacts/private-civc-vk
+  cp "$private_to_public_tail_vk" ../artifacts/public-civc-vk
 else
   echo_stderr "You may need to run ./bootstrap.sh in the noir-projects folder. Could not find the IVC VKs at $private_tail_vk and $private_to_public_tail_vk."
   exit 1
