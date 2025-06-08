15773f4701b5bc6a915c4ac77646b091c17395fb
sergei iakovenko <105737703+iakovenkos@users.noreply.github.com>
2025-06-08 11:25:01 +0200

    chore: stdlib field pre-audit pt.2 (#14604)
    * `accumulate()` docs + tests + fix a minor bug 
    * `fix_witness()` docs + test 
    * `is_zero()`:
    * + use `big_mul` gates instead of `poly` gates --> no need to
    `normalize()` the witness
    *   + slightly expanded the tests 
    *   + docs improved
    * `==` operator:
    * + found a bug in the implementation - missing `bool` constraint for
    the resulting `bool_t`
    *   + now `==` is simply re-using `.is_zero()` 
    * + note: this led to VK changes for various circuits where `==` is used
    *   + expanded test docs by explaining the expected gate counts

--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/ultra_circuit_builder.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/ultra_circuit_builder.cpp
@@ -308,7 +308,7 @@ void UltraCircuitBuilder_<ExecutionTrace>::create_big_mul_add_gate(const mul_qua
 
 /**
  * @brief Create a big addition gate, where in.a * in.a_scaling + in.b * in.b_scaling + in.c *
- * in.c_scaling + in.d * in.d_scaling + in.const_scaling = 0. If include_next_gate_w_4 is enabled, then thes sum also
+ * in.c_scaling + in.d * in.d_scaling + in.const_scaling = 0. If include_next_gate_w_4 is enabled, then the sum also
  * adds the value of the 4-th witness at the next index.
  *
  * @param in Structure with variable indexes and wire selector values
@@ -858,7 +858,7 @@ std::vector<uint32_t> UltraCircuitBuilder_<ExecutionTrace>::decompose_into_defau
 
     uint256_t val = (uint256_t)(this->get_variable(variable_index));
 
-    // If the value is out of range, set the composer error to the given msg.
+    // If the value is out of range, set the CircuitBuilder error to the given msg.
     if (val.get_msb() >= num_bits && !this->failed()) {
         this->failure(msg);
     }
