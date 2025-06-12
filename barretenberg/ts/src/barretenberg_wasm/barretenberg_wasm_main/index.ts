897ec02ddc25d409568156a2ed1cdbc3a9209496
ludamad <adam.domurad@gmail.com>
2025-06-12 06:18:00 -0400

    fix(bb.js): don't set maximum wasm memory >1GB on ios web
    This is the most we expect to be able to use anyway

--- barretenberg/ts/src/barretenberg_wasm/barretenberg_wasm_main/index.ts
+++ barretenberg/ts/src/barretenberg_wasm/barretenberg_wasm_main/index.ts
@@ -31,7 +31,7 @@ export class BarretenbergWasmMain extends BarretenbergWasmBase {
     threads = Math.min(getNumCpu(), BarretenbergWasmMain.MAX_THREADS),
     logger: (msg: string) => void = createDebugLogger('bb_wasm'),
     initial = 32,
-    maximum = 2 ** 16,
+    maximum = this.getDefaultMaximum(),
   ) {
     this.logger = logger;
 
@@ -61,6 +61,14 @@ export class BarretenbergWasmMain extends BarretenbergWasmBase {
       this.remoteWasms = await Promise.all(this.workers.map(getRemoteBarretenbergWasm<BarretenbergWasmThreadWorker>));
       await Promise.all(this.remoteWasms.map(w => w.initThread(module, this.memory)));
     }
+  }
+
+  private getDefaultMaximum(): number {
+    // iOS browser is very aggressive with memory. Check if running in browser and on iOS
+    if (typeof window !== 'undefined' && /iPhone/.test(navigator.userAgent)) {
+      return 2 ** 14;
+    }
+    return 2 ** 16;
   }
 
   /**
