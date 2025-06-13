832990ecdc0130098e95e9d4735782134beb5c80
Santiago Palladino <santiago@aztec-labs.com>
2025-06-13 19:57:16 -0300

    chore: Store tx effects individually
    We were storing tx effects in the archiver as part of the block body.
    This means that every time we had to get a tx receipt or effect, we had
    to deserialize the entire block to get just one tx.
    
    This changes the structure of the archiver db so we store each tx effect
    on its own, and the block just stores the list of block hashes it
    contains.

--- yarn-project/stdlib/src/tx/indexed_tx_effect.ts
+++ yarn-project/stdlib/src/tx/indexed_tx_effect.ts
@@ -1,4 +1,6 @@
+import { Buffer32 } from '@aztec/foundation/buffer';
 import { schemas } from '@aztec/foundation/schemas';
+import { BufferReader, serializeToBuffer } from '@aztec/foundation/serialize';
 
 import { type InBlock, inBlockSchemaFor, randomInBlock } from '../block/in_block.js';
 import { TxEffect } from './tx_effect.js';
@@ -13,5 +15,30 @@ export async function randomIndexedTxEffect(): Promise<IndexedTxEffect> {
   return {
     ...randomInBlock(await TxEffect.random()),
     txIndexInBlock: Math.floor(Math.random() * 1000),
+  };
+}
+
+export function serializeIndexedTxEffect(effect: IndexedTxEffect): Buffer {
+  return serializeToBuffer(
+    Buffer32.fromString(effect.l2BlockHash),
+    effect.l2BlockNumber,
+    effect.txIndexInBlock,
+    effect.data,
+  );
+}
+
+export function deserializeIndexedTxEffect(buffer: Buffer): IndexedTxEffect {
+  const reader = BufferReader.asReader(buffer);
+
+  const l2BlockHash = reader.readObject(Buffer32).toString();
+  const l2BlockNumber = reader.readNumber();
+  const txIndexInBlock = reader.readNumber();
+  const data = reader.readObject(TxEffect);
+
+  return {
+    l2BlockHash,
+    l2BlockNumber,
+    txIndexInBlock,
+    data,
   };
 }
