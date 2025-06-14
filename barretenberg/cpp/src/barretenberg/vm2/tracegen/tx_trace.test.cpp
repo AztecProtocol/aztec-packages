b80949775e5c27908a6055ad24592f7d98bdc45a
Facundo <fcarreiro@users.noreply.github.com>
2025-06-14 11:30:21 +0100

    refactor(avm): more execution temporality work (#14816)
    Please read [contributing guidelines](CONTRIBUTING.md) and remove this
    line.

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/tx_trace.test.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/tx_trace.test.cpp
@@ -17,14 +17,9 @@
 namespace bb::avm2::tracegen {
 namespace {
 
-using ::bb::avm2::testing::InstructionBuilder;
 using enum ::bb::avm2::WireOpCode;
 
 using ::testing::AllOf;
-using ::testing::Contains;
-using ::testing::Field;
-
-using R = TestTraceContainer::Row;
 
 TEST(TxTraceGenTest, EnqueuedCallEvent)
 {
@@ -62,40 +57,37 @@ TEST(TxTraceGenTest, EnqueuedCallEvent)
     builder.process({ startup_event, tx_event }, trace);
     auto rows = trace.as_rows();
     ASSERT_EQ(rows.size(), 11); // 10 tx trace rows + 1 precomputed row
-    EXPECT_THAT(
-        rows[static_cast<uint8_t>(TransactionPhase::NR_NOTE_INSERTION)],
-        AllOf(
-            ROW_FIELD_EQ(R, tx_sel, 1),
-            ROW_FIELD_EQ(R, tx_phase_value, static_cast<uint8_t>(TransactionPhase::NR_NOTE_INSERTION)),
-            ROW_FIELD_EQ(R, tx_is_padded, 1),
-            ROW_FIELD_EQ(R, tx_is_tree_insert_phase, 1),
-            ROW_FIELD_EQ(R, tx_sel_non_revertible_append_note_hash, 1),
-            ROW_FIELD_EQ(R,
-                         tx_read_pi_length_offset,
-                         AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_NOTE_HASHES_ROW_IDX),
-            ROW_FIELD_EQ(
-                R, tx_read_pi_offset, AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX),
-            ROW_FIELD_EQ(R, tx_write_pi_offset, AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX),
-            ROW_FIELD_EQ(R, tx_start_phase, 1),
-            ROW_FIELD_EQ(R, tx_end_phase, 1),
-            ROW_FIELD_EQ(R, tx_is_static, false)));
+    EXPECT_THAT(rows[static_cast<uint8_t>(TransactionPhase::NR_NOTE_INSERTION)],
+                AllOf(ROW_FIELD_EQ(tx_sel, 1),
+                      ROW_FIELD_EQ(tx_phase_value, static_cast<uint8_t>(TransactionPhase::NR_NOTE_INSERTION)),
+                      ROW_FIELD_EQ(tx_is_padded, 1),
+                      ROW_FIELD_EQ(tx_is_tree_insert_phase, 1),
+                      ROW_FIELD_EQ(tx_sel_non_revertible_append_note_hash, 1),
+                      ROW_FIELD_EQ(
+                          tx_read_pi_length_offset,
+                          AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_ARRAY_LENGTHS_NOTE_HASHES_ROW_IDX),
+                      ROW_FIELD_EQ(tx_read_pi_offset,
+                                   AVM_PUBLIC_INPUTS_PREVIOUS_NON_REVERTIBLE_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX),
+                      ROW_FIELD_EQ(tx_write_pi_offset, AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_NOTE_HASHES_ROW_IDX),
+                      ROW_FIELD_EQ(tx_start_phase, 1),
+                      ROW_FIELD_EQ(tx_end_phase, 1),
+                      ROW_FIELD_EQ(tx_is_static, false)));
 
     // Setup
     EXPECT_THAT(rows[static_cast<uint8_t>(TransactionPhase::SETUP)],
-                AllOf(ROW_FIELD_EQ(R, tx_sel, 1),
-                      ROW_FIELD_EQ(R, tx_phase_value, static_cast<uint8_t>(TransactionPhase::SETUP)),
-                      ROW_FIELD_EQ(R, tx_start_phase, 1),
-                      ROW_FIELD_EQ(R, tx_is_public_call_request, 1),
-                      ROW_FIELD_EQ(R,
-                                   tx_read_pi_length_offset,
+                AllOf(ROW_FIELD_EQ(tx_sel, 1),
+                      ROW_FIELD_EQ(tx_phase_value, static_cast<uint8_t>(TransactionPhase::SETUP)),
+                      ROW_FIELD_EQ(tx_start_phase, 1),
+                      ROW_FIELD_EQ(tx_is_public_call_request, 1),
+                      ROW_FIELD_EQ(tx_read_pi_length_offset,
                                    AVM_PUBLIC_INPUTS_PUBLIC_CALL_REQUEST_ARRAY_LENGTHS_SETUP_CALLS_ROW_IDX),
-                      ROW_FIELD_EQ(R, tx_read_pi_offset, AVM_PUBLIC_INPUTS_PUBLIC_SETUP_CALL_REQUESTS_ROW_IDX),
-                      ROW_FIELD_EQ(R, tx_remaining_phase_counter, 1),
-                      ROW_FIELD_EQ(R, tx_remaining_phase_inv, 1),
-                      ROW_FIELD_EQ(R, tx_msg_sender, msg_sender),
-                      ROW_FIELD_EQ(R, tx_contract_addr, contract_address),
-                      ROW_FIELD_EQ(R, tx_calldata_hash, calldata_hash),
-                      ROW_FIELD_EQ(R, tx_end_phase, 1)));
+                      ROW_FIELD_EQ(tx_read_pi_offset, AVM_PUBLIC_INPUTS_PUBLIC_SETUP_CALL_REQUESTS_ROW_IDX),
+                      ROW_FIELD_EQ(tx_remaining_phase_counter, 1),
+                      ROW_FIELD_EQ(tx_remaining_phase_inv, 1),
+                      ROW_FIELD_EQ(tx_msg_sender, msg_sender),
+                      ROW_FIELD_EQ(tx_contract_addr, contract_address),
+                      ROW_FIELD_EQ(tx_calldata_hash, calldata_hash),
+                      ROW_FIELD_EQ(tx_end_phase, 1)));
 };
 
 TEST(TxTraceGenTest, CollectFeeEvent)
@@ -136,30 +128,30 @@ TEST(TxTraceGenTest, CollectFeeEvent)
     ASSERT_EQ(rows.size(), 11); // 10 tx trace rows + 1 precomputed row
 
     EXPECT_THAT(rows[static_cast<uint8_t>(TransactionPhase::COLLECT_GAS_FEES)],
-                AllOf(ROW_FIELD_EQ(R, tx_sel, 1),
-                      ROW_FIELD_EQ(R, tx_phase_value, static_cast<uint8_t>(TransactionPhase::COLLECT_GAS_FEES)),
-                      ROW_FIELD_EQ(R, tx_is_padded, 0),
-                      ROW_FIELD_EQ(R, tx_start_phase, 1),
-                      ROW_FIELD_EQ(R, tx_sel_read_phase_length, 0),
-                      ROW_FIELD_EQ(R, tx_end_phase, 1),
-                      ROW_FIELD_EQ(R, tx_prev_da_gas_used, prev_da_gas_used),
-                      ROW_FIELD_EQ(R, tx_prev_l2_gas_used, prev_l2_gas_used),
-                      ROW_FIELD_EQ(R, tx_read_pi_offset, AVM_PUBLIC_INPUTS_EFFECTIVE_GAS_FEES_ROW_IDX),
-                      ROW_FIELD_EQ(R, tx_read_pi_length_offset, 0),
-                      ROW_FIELD_EQ(R, tx_write_pi_offset, AVM_PUBLIC_INPUTS_TRANSACTION_FEE_ROW_IDX),
-                      ROW_FIELD_EQ(R, tx_remaining_phase_counter, 1),
-                      ROW_FIELD_EQ(R, tx_remaining_phase_inv, FF(1).invert()),
-                      ROW_FIELD_EQ(R, tx_remaining_phase_minus_one_inv, 0),
-                      ROW_FIELD_EQ(R, tx_is_collect_fee, 1),
-                      ROW_FIELD_EQ(R, tx_effective_fee_per_da_gas, FF(effective_fee_per_da_gas)),
-                      ROW_FIELD_EQ(R, tx_effective_fee_per_l2_gas, FF(effective_fee_per_l2_gas)),
-                      ROW_FIELD_EQ(R, tx_fee_payer, fee_payer),
-                      ROW_FIELD_EQ(R, tx_fee_payer_pi_offset, AVM_PUBLIC_INPUTS_FEE_PAYER_ROW_IDX),
-                      ROW_FIELD_EQ(R, tx_fee, fee),
-                      ROW_FIELD_EQ(R, tx_fee_payer_balance, fee_payer_balance),
-                      ROW_FIELD_EQ(R, tx_end_gas_used_pi_offset, AVM_PUBLIC_INPUTS_END_GAS_USED_ROW_IDX),
-                      ROW_FIELD_EQ(R, tx_next_da_gas_used, prev_da_gas_used),
-                      ROW_FIELD_EQ(R, tx_next_l2_gas_used, prev_l2_gas_used)));
+                AllOf(ROW_FIELD_EQ(tx_sel, 1),
+                      ROW_FIELD_EQ(tx_phase_value, static_cast<uint8_t>(TransactionPhase::COLLECT_GAS_FEES)),
+                      ROW_FIELD_EQ(tx_is_padded, 0),
+                      ROW_FIELD_EQ(tx_start_phase, 1),
+                      ROW_FIELD_EQ(tx_sel_read_phase_length, 0),
+                      ROW_FIELD_EQ(tx_end_phase, 1),
+                      ROW_FIELD_EQ(tx_prev_da_gas_used, prev_da_gas_used),
+                      ROW_FIELD_EQ(tx_prev_l2_gas_used, prev_l2_gas_used),
+                      ROW_FIELD_EQ(tx_read_pi_offset, AVM_PUBLIC_INPUTS_EFFECTIVE_GAS_FEES_ROW_IDX),
+                      ROW_FIELD_EQ(tx_read_pi_length_offset, 0),
+                      ROW_FIELD_EQ(tx_write_pi_offset, AVM_PUBLIC_INPUTS_TRANSACTION_FEE_ROW_IDX),
+                      ROW_FIELD_EQ(tx_remaining_phase_counter, 1),
+                      ROW_FIELD_EQ(tx_remaining_phase_inv, FF(1).invert()),
+                      ROW_FIELD_EQ(tx_remaining_phase_minus_one_inv, 0),
+                      ROW_FIELD_EQ(tx_is_collect_fee, 1),
+                      ROW_FIELD_EQ(tx_effective_fee_per_da_gas, FF(effective_fee_per_da_gas)),
+                      ROW_FIELD_EQ(tx_effective_fee_per_l2_gas, FF(effective_fee_per_l2_gas)),
+                      ROW_FIELD_EQ(tx_fee_payer, fee_payer),
+                      ROW_FIELD_EQ(tx_fee_payer_pi_offset, AVM_PUBLIC_INPUTS_FEE_PAYER_ROW_IDX),
+                      ROW_FIELD_EQ(tx_fee, fee),
+                      ROW_FIELD_EQ(tx_fee_payer_balance, fee_payer_balance),
+                      ROW_FIELD_EQ(tx_end_gas_used_pi_offset, AVM_PUBLIC_INPUTS_END_GAS_USED_ROW_IDX),
+                      ROW_FIELD_EQ(tx_next_da_gas_used, prev_da_gas_used),
+                      ROW_FIELD_EQ(tx_next_l2_gas_used, prev_l2_gas_used)));
 };
 
 } // namespace
