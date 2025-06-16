15f6bed0559882bd1da678018f73d4b505b2ec3d
DanielKotov <katod.daniel@gmail.com>
2025-06-16 11:11:54 +0000

    test redesign for range constraints

--- barretenberg/cpp/src/barretenberg/boomerang_value_detection/graph_description.test.cpp
+++ barretenberg/cpp/src/barretenberg/boomerang_value_detection/graph_description.test.cpp
@@ -511,60 +511,63 @@ TEST(boomerang_ultra_circuit_constructor, test_variables_gates_counts_for_sorted
  */
 TEST(boomerang_ultra_circuit_constructor, test_variables_gates_counts_for_sorted_constraints_with_edges)
 {
-    fr a = fr::one();
-    fr b = fr(2);
-    fr c = fr(3);
-    fr d = fr(4);
-    fr e = fr(5);
-    fr f = fr(6);
-    fr g = fr(7);
-    fr h = fr(8);
-
     UltraCircuitBuilder circuit_constructor;
-    auto a_idx = circuit_constructor.add_variable(a);
-    auto b_idx = circuit_constructor.add_variable(b);
-    auto c_idx = circuit_constructor.add_variable(c);
-    auto d_idx = circuit_constructor.add_variable(d);
-    auto e_idx = circuit_constructor.add_variable(e);
-    auto f_idx = circuit_constructor.add_variable(f);
-    auto g_idx = circuit_constructor.add_variable(g);
-    auto h_idx = circuit_constructor.add_variable(h);
-    circuit_constructor.create_sort_constraint_with_edges(
-        { a_idx, b_idx, c_idx, d_idx, e_idx, f_idx, g_idx, h_idx }, a, h);
+    std::vector<fr> vars1 = { fr::one(), fr(2), fr(3), fr(4), fr(5), fr(6), fr(7), fr(8) };
+    std::vector<fr> vars2 = { fr(9), fr(10), fr(11), fr(12), fr(13), fr(14), fr(15), fr(16) };
+    auto var_idx1 = add_variables(circuit_constructor, vars1);
+    auto var_idx2 = add_variables(circuit_constructor, vars2);
 
-    fr a1 = fr(9);
-    fr b1 = fr(10);
-    fr c1 = fr(11);
-    fr d1 = fr(12);
-    fr e1 = fr(13);
-    fr f1 = fr(14);
-    fr g1 = fr(15);
-    fr h1 = fr(16);
+    circuit_constructor.create_sort_constraint_with_edges(var_idx1, vars1[0], vars1[vars1.size() - 1]);
 
-    auto a1_idx = circuit_constructor.add_variable(a1);
-    auto b1_idx = circuit_constructor.add_variable(b1);
-    auto c1_idx = circuit_constructor.add_variable(c1);
-    auto d1_idx = circuit_constructor.add_variable(d1);
-    auto e1_idx = circuit_constructor.add_variable(e1);
-    auto f1_idx = circuit_constructor.add_variable(f1);
-    auto g1_idx = circuit_constructor.add_variable(g1);
-    auto h1_idx = circuit_constructor.add_variable(h1);
-
-    circuit_constructor.create_sort_constraint_with_edges(
-        { a1_idx, b1_idx, c1_idx, d1_idx, e1_idx, f1_idx, g1_idx, h1_idx }, a1, h1);
+    circuit_constructor.create_sort_constraint_with_edges(var_idx2, vars2[0], vars2[vars2.size() - 1]);
     Graph graph = Graph(circuit_constructor);
     auto connected_components = graph.find_connected_components();
     auto variables_gate_counts = graph.get_variables_gate_counts();
+    EXPECT_EQ(connected_components.size(), 2);
     bool result = true;
-    for (size_t i = 0; i < connected_components[0].size(); i++) {
-        result = result && (variables_gate_counts[connected_components[0][i]] == 1);
+    for (const auto& [key, value] : variables_gate_counts) {
+        info("variable with index == ", key, " in ", value, " gates");
+    }
+    for (size_t i = 0; i < var_idx1.size(); i++) {
+        if (i % 4 == 1 && i > 1) {
+            result = variables_gate_counts[var_idx1[i]] == 2;
+        } else {
+            result = variables_gate_counts[var_idx1[i]] == 1;
+        }
     }
 
-    for (size_t i = 0; i < connected_components[1].size(); i++) {
-        result = result && (variables_gate_counts[connected_components[1][i]] == 1);
+    for (size_t i = 0; i < var_idx2.size(); i++) {
+        if (i % 4 == 1 && i > 1) {
+            result = variables_gate_counts[var_idx2[i]] == 2;
+        } else {
+            result = variables_gate_counts[var_idx1[i]] == 1;
+        }
     }
-    EXPECT_EQ(connected_components.size(), 2);
     EXPECT_EQ(result, true);
+}
+
+/**
+ * @brief Test graph description of circuit with range constraints
+ *
+ * @details This test verifies that:
+ * - All variables must be in one connected component
+ */
+
+TEST(boomerang_ultra_circuit_constructor, test_graph_for_range_constraints)
+{
+    UltraCircuitBuilder circuit_constructor = UltraCircuitBuilder();
+    auto indices = add_variables(circuit_constructor, { fr(1), fr(2), fr(3), fr(4) });
+    for (size_t i = 0; i < indices.size(); i++) {
+        circuit_constructor.create_new_range_constraint(indices[i], 5);
+    }
+    circuit_constructor.create_sort_constraint(indices);
+    Graph graph = Graph(circuit_constructor);
+    auto connected_components = graph.find_connected_components();
+    EXPECT_EQ(connected_components.size(), 1);
+    auto variables_gate_counts = graph.get_variables_gate_counts();
+    for (const auto& idx : indices) {
+        info("gate count for variable with index ", idx, " == ", variables_gate_counts[idx]);
+    }
 }
 
 /**
@@ -642,30 +645,6 @@ TEST(boomerang_ultra_circuit_constructor, test_variables_gates_counts_for_ecc_db
 
     EXPECT_EQ(connected_components.size(), 1);
     EXPECT_EQ(result, true);
-}
-
-/**
- * @brief Test graph description of circuit with range constraints
- *
- * @details This test verifies that:
- * - All variables must be in one connected component
- */
-
-TEST(boomerang_ultra_circuit_constructor, test_graph_for_range_constraints)
-{
-    UltraCircuitBuilder circuit_constructor = UltraCircuitBuilder();
-    auto indices = add_variables(circuit_constructor, { 1, 2, 3, 4 });
-    for (size_t i = 0; i < indices.size(); i++) {
-        circuit_constructor.create_new_range_constraint(indices[i], 5);
-    }
-    circuit_constructor.create_sort_constraint(indices);
-    Graph graph = Graph(circuit_constructor);
-    auto connected_components = graph.find_connected_components();
-    EXPECT_EQ(connected_components.size(), 1);
-    auto variables_gate_counts = graph.get_variables_gate_counts();
-    for (const auto& idx : indices) {
-        info("gate count for variable with index ", idx, " == ", variables_gate_counts[idx]);
-    }
 }
 
 /**
