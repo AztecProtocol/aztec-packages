ddd5ac2b3786bb1f370aa218a2eba9720020bb7c
DanielKotov <katod.daniel@gmail.com>
2025-06-17 18:00:26 +0000

    test redesign for goblin + fix for translator tests

--- barretenberg/cpp/src/barretenberg/boomerang_value_detection/graph_description_goblin.test.cpp
+++ barretenberg/cpp/src/barretenberg/boomerang_value_detection/graph_description_goblin.test.cpp
@@ -72,8 +72,8 @@ TEST_F(BoomerangGoblinRecursiveVerifierTests, graph_description_basic)
     // Construct and verify a proof for the Goblin Recursive Verifier circuit
     {
         auto proving_key = std::make_shared<OuterDeciderProvingKey>(builder);
-        OuterProver prover(proving_key);
         auto verification_key = std::make_shared<typename OuterFlavor::VerificationKey>(proving_key->proving_key);
+        OuterProver prover(proving_key, verification_key);
         OuterVerifier verifier(verification_key);
         auto proof = prover.construct_proof();
         bool verified = verifier.verify_proof(proof);
