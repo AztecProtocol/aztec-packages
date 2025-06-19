153a3364f037374eeecddcfa7f9160b0df48ee75
Alex Gherghisan <alexghr@users.noreply.github.com>
2025-06-19 13:02:46 +0000

    feat: add cluster of public open telemetry collectors

--- spartan/terraform/deploy-metrics/main.tf
+++ spartan/terraform/deploy-metrics/main.tf
@@ -43,6 +43,17 @@ resource "google_compute_address" "otel_collector_ip" {
   }
 }
 
+resource "google_compute_address" "public_otel_collector_ip" {
+  provider     = google
+  name         = "public-otel-ip-${var.RELEASE_NAME}"
+  address_type = "EXTERNAL"
+  region       = var.region
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 provider "kubernetes" {
   alias          = "gke-cluster"
   config_path    = "~/.kube/config"
@@ -128,4 +139,60 @@ resource "helm_release" "aztec-gke-cluster" {
   wait          = true
   wait_for_jobs = true
 
+}
+
+locals {
+  public_otel_ingress_host = "public-otel.alpha-testnet.aztec.network"
+}
+
+resource "kubernetes_manifest" "public_otel_ingress_certificate" {
+  manifest = {
+    "apiVersion" = "networking.gke.io/v1"
+    "kind"       = "ManagedCertificate"
+    "metadata" = {
+      "name" = "${var.RELEASE_NAME}-public-otelcol-ingres-cert"
+    }
+    "spec" = {
+      "domains" = [
+        local.public_otel_ingress_host
+      ]
+    }
+  }
+}
+
+resource "helm_release" "public_otel_collector" {
+  provider          = helm.gke-cluster
+  name              = "${var.RELEASE_NAME}-public-otelcol"
+  repository        = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  chart             = "opentelemetry-collector"
+  version           = "0.104.0"
+  namespace         = var.RELEASE_NAME
+  create_namespace  = true
+  upgrade_install   = true
+  dependency_update = true
+  force_update      = true
+  reuse_values      = true
+
+  # base values file
+  values = [file("./values/public-otel-collector.yaml")]
+
+  set {
+    # name  = "service.loadBalancerIP"
+    name  = "ingress.annotations.\"kubernetes.io/ingress.global-static-ip-name\""
+    value = google_compute_address.public_otel_collector_ip.address
+  }
+
+  set {
+    name  = "ingress.annotations.\"networking.gke.io/managed-certificates\""
+    value = "${var.RELEASE_NAME}-public-otelcol-ingres-cert"
+  }
+
+  set {
+    name  = "ingress.hosts[0].host"
+    value = local.public_otel_ingress_host
+  }
+
+  timeout       = 600
+  wait          = true
+  wait_for_jobs = true
 }
