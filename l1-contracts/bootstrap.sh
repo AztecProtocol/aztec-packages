d6718c3486005a47c81532a388bb391fd1134aaa
Charlie <5764343+charlielye@users.noreply.github.com>
2025-06-20 17:54:46 +0200

    fix: run shout tests on staging (#15188)
    * Run protocol check tests on staging.
    * Update protocol check tests with new hashes.
    * Fix noisy test log lines (we only want commit msg and author on the
    test tracking page)
    * Backport creates PRs with conventional commit title.

--- l1-contracts/bootstrap.sh
+++ l1-contracts/bootstrap.sh
@@ -56,7 +56,7 @@ function test_cmds {
   echo "$hash cd l1-contracts && solhint --config ./.solhint.json \"src/**/*.sol\""
   echo "$hash cd l1-contracts && forge fmt --check"
   echo "$hash cd l1-contracts && forge test"
-  if [ "$CI" -eq 0 ] || [[ "${TARGET_BRANCH:-}" == "master" ]]; then
+  if [ "$CI" -eq 0 ] || [[ "${TARGET_BRANCH:-}" == "master" || "${TARGET_BRANCH:-}" == "staging" ]]; then
     echo "$hash cd l1-contracts && forge test --no-match-contract UniswapPortalTest --match-contract ScreamAndShoutTest"
   fi
 }
