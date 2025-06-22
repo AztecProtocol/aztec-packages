9084982824b51d9aab4753c8cfd015b40d5bb428
suyash67 <suyashnbagad1997@gmail.com>
2025-06-22 20:10:33 +0000

    change TODO in sqr to NOTE.

--- barretenberg/cpp/src/barretenberg/stdlib/primitives/bigfield/bigfield.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib/primitives/bigfield/bigfield.hpp
@@ -498,7 +498,7 @@ template <typename Builder, typename T> class bigfield {
      * @return bigfield
      *
      * @details Costs the same as operator* as it just sets a = b.
-     * TODO(https://github.com/AztecProtocol/aztec-packages/issues/15089): can optimise this further.
+     * NOTE(https://github.com/AztecProtocol/aztec-packages/issues/15089): Can optimise this further to save a gate.
      */
     bigfield sqr() const;
 
