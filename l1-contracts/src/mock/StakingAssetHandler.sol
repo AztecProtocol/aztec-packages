2868628b6ae039db35b4267ab3975abc64a5d9b4
Maddiaa <47148561+Maddiaa0@users.noreply.github.com>
2025-06-23 20:29:22 +0800

    feat(staking_asset_handler): merkle check in next (#15179)
    ## Overview
    
    Adds merkle tree gating to staking asset handler in next

--- l1-contracts/src/mock/StakingAssetHandler.sol
+++ l1-contracts/src/mock/StakingAssetHandler.sol
@@ -5,6 +5,7 @@ import {IStaking} from "@aztec/core/interfaces/IStaking.sol";
 import {IRegistry} from "@aztec/governance/interfaces/IRegistry.sol";
 import {IMintableERC20} from "@aztec/shared/interfaces/IMintableERC20.sol";
 import {Ownable} from "@oz/access/Ownable.sol";
+import {MerkleProof} from "@oz/utils/cryptography/MerkleProof.sol";
 import {ZKPassportVerifier, ProofVerificationParams} from "@zkpassport/ZKPassportVerifier.sol";
 
 /**
@@ -37,6 +38,8 @@ interface IStakingAssetHandler {
   event ScopeUpdated(string newScope);
   event SubScopeUpdated(string newSubScope);
   event SkipBindCheckUpdated(bool _skipBindCheck);
+  event SkipMerkleCheckUpdated(bool _skipMerkleCheck);
+  event DepositMerkleRootUpdated(bytes32 _root);
 
   event UnhingedAdded(address indexed _address);
   event UnhingedRemoved(address indexed _address);
@@ -49,9 +52,14 @@ interface IStakingAssetHandler {
   error SybilDetected(bytes32 _nullifier);
   error AttesterDoesNotExist(address _attester);
   error NoNullifier();
+  error MerkleProofInvalid();
 
   // Add validator methods
-  function addValidator(address _attester, ProofVerificationParams memory _params) external;
+  function addValidator(
+    address _attester,
+    bytes32[] memory merkleProof,
+    ProofVerificationParams memory _params
+  ) external;
   function reenterExitedValidator(address _attester) external;
 
   // Admin methods
@@ -64,18 +72,38 @@ interface IStakingAssetHandler {
   function setScope(string memory _scope) external;
   function setSubscope(string memory _subscope) external;
   function setSkipBindCheck(bool _skipBindCheck) external;
+  function setSkipMerkleCheck(bool _skipMerkleCheck) external;
+  function setDepositMerkleRoot(bytes32 _root) external;
 
   // View
   function getRollup() external view returns (address);
 }
 
 contract StakingAssetHandler is IStakingAssetHandler, Ownable {
+  struct StakingAssetHandlerArgs {
+    address owner;
+    address stakingAsset;
+    IRegistry registry;
+    address withdrawer;
+    uint256 mintInterval;
+    uint256 depositsPerMint;
+    bytes32 depositMerkleRoot;
+    ZKPassportVerifier zkPassportVerifier;
+    address[] unhinged;
+    string scope;
+    string subscope;
+    bool skipBindCheck;
+    bool skipMerkleCheck;
+  }
+
   IMintableERC20 public immutable STAKING_ASSET;
   IRegistry public immutable REGISTRY;
 
   ZKPassportVerifier public zkPassportVerifier;
 
   bool internal skipBindCheck;
+  bool internal skipMerkleCheck;
+
   mapping(address attester => bool isUnhinged) public isUnhinged;
   mapping(bytes32 nullifier => bool exists) public nullifiers;
   mapping(address attester => bytes32 nullifier) public attesterToNullifier;
@@ -83,53 +111,46 @@ contract StakingAssetHandler is IStakingAssetHandler, Ownable {
   uint256 public lastMintTimestamp;
   uint256 public mintInterval;
   uint256 public depositsPerMint;
+  bytes32 public depositMerkleRoot;
 
   address public withdrawer;
 
   string public validScope;
   string public validSubscope;
 
-  constructor(
-    address _owner,
-    address _stakingAsset,
-    IRegistry _registry,
-    address _withdrawer,
-    uint256 _mintInterval,
-    uint256 _depositsPerMint,
-    ZKPassportVerifier _zkPassportVerifier,
-    address[] memory _unhinged,
-    string memory _scope,
-    string memory _subscope,
-    bool _skipBindCheck
-  ) Ownable(_owner) {
-    require(_depositsPerMint > 0, CannotMintZeroAmount());
+  constructor(StakingAssetHandlerArgs memory _args) Ownable(_args.owner) {
+    require(_args.depositsPerMint > 0, CannotMintZeroAmount());
 
-    STAKING_ASSET = IMintableERC20(_stakingAsset);
-    REGISTRY = _registry;
+    STAKING_ASSET = IMintableERC20(_args.stakingAsset);
+    REGISTRY = _args.registry;
 
-    withdrawer = _withdrawer;
-    emit WithdrawerUpdated(_withdrawer);
+    withdrawer = _args.withdrawer;
+    emit WithdrawerUpdated(_args.withdrawer);
 
-    mintInterval = _mintInterval;
-    emit IntervalUpdated(_mintInterval);
+    mintInterval = _args.mintInterval;
+    emit IntervalUpdated(_args.mintInterval);
 
-    depositsPerMint = _depositsPerMint;
-    emit DepositsPerMintUpdated(_depositsPerMint);
+    depositsPerMint = _args.depositsPerMint;
+    emit DepositsPerMintUpdated(_args.depositsPerMint);
 
-    for (uint256 i = 0; i < _unhinged.length; i++) {
-      isUnhinged[_unhinged[i]] = true;
-      emit UnhingedAdded(_unhinged[i]);
+    for (uint256 i = 0; i < _args.unhinged.length; i++) {
+      isUnhinged[_args.unhinged[i]] = true;
+      emit UnhingedAdded(_args.unhinged[i]);
     }
-    isUnhinged[_owner] = true;
-    emit UnhingedAdded(_owner);
+    isUnhinged[_args.owner] = true;
+    emit UnhingedAdded(_args.owner);
 
-    zkPassportVerifier = _zkPassportVerifier;
-    emit ZKPassportVerifierUpdated(address(_zkPassportVerifier));
+    zkPassportVerifier = _args.zkPassportVerifier;
+    emit ZKPassportVerifierUpdated(address(_args.zkPassportVerifier));
 
-    validScope = _scope;
-    validSubscope = _subscope;
+    depositMerkleRoot = _args.depositMerkleRoot;
+    emit DepositMerkleRootUpdated(_args.depositMerkleRoot);
 
-    skipBindCheck = _skipBindCheck;
+    validScope = _args.scope;
+    validSubscope = _args.subscope;
+
+    skipBindCheck = _args.skipBindCheck;
+    skipMerkleCheck = _args.skipMerkleCheck;
   }
 
   /**
@@ -137,10 +158,11 @@ contract StakingAssetHandler is IStakingAssetHandler, Ownable {
    *
    * @param _attester - the validator's attester address
    */
-  function addValidator(address _attester, ProofVerificationParams calldata _params)
-    external
-    override(IStakingAssetHandler)
-  {
+  function addValidator(
+    address _attester,
+    bytes32[] memory _merkleProof,
+    ProofVerificationParams calldata _params
+  ) external override(IStakingAssetHandler) {
     IStaking rollup = IStaking(address(REGISTRY.getCanonicalRollup()));
     uint256 depositAmount = rollup.getDepositAmount();
 
@@ -153,6 +175,8 @@ contract StakingAssetHandler is IStakingAssetHandler, Ownable {
     } else {
       _topUpIfRequired(depositAmount);
 
+      // Check attester has the guardian role (included in merkle tree)
+      _validateMerkleProof(_attester, _merkleProof);
       _validatePassportProof(_attester, _params);
 
       // If the attester is currently exiting, we finalize the exit for him.
@@ -233,6 +257,20 @@ contract StakingAssetHandler is IStakingAssetHandler, Ownable {
     emit SkipBindCheckUpdated(_skipBindCheck);
   }
 
+  function setSkipMerkleCheck(bool _skipMerkleCheck)
+    external
+    override(IStakingAssetHandler)
+    onlyOwner
+  {
+    skipMerkleCheck = _skipMerkleCheck;
+    emit SkipMerkleCheckUpdated(_skipMerkleCheck);
+  }
+
+  function setDepositMerkleRoot(bytes32 _root) external override(IStakingAssetHandler) onlyOwner {
+    depositMerkleRoot = _root;
+    emit DepositMerkleRootUpdated(_root);
+  }
+
   function getRollup() external view override(IStakingAssetHandler) returns (address) {
     return address(REGISTRY.getCanonicalRollup());
   }
@@ -306,5 +344,20 @@ contract StakingAssetHandler is IStakingAssetHandler, Ownable {
     STAKING_ASSET.approve(address(_rollup), _depositAmount);
     _rollup.deposit(_attester, withdrawer, true);
     emit ValidatorAdded(address(_rollup), _attester, withdrawer);
+  }
+
+  /**
+   * Validate Merkle Proof
+   *
+   * Check the provided merkle proof is correct for the given address
+   *
+   * @param _attester - the attester
+   * @param _merkleProof - a merkle proof for the attester
+   */
+  function _validateMerkleProof(address _attester, bytes32[] memory _merkleProof) internal view {
+    if (!skipMerkleCheck) {
+      bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(_attester))));
+      require(MerkleProof.verify(_merkleProof, depositMerkleRoot, leaf), MerkleProofInvalid());
+    }
   }
 }
