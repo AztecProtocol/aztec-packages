197d230c0ff910b599cac4f0c7336db41cd1145f
ludamad <adam.domurad@gmail.com>
2025-06-23 16:33:23 -0400

    feat(ci): docs-only CI

--- bootstrap.sh
+++ bootstrap.sh
@@ -416,6 +416,11 @@ case "$cmd" in
     build
     release
     ;;
+  "ci-docs")
+    export CI=1
+    export USE_TEST_CACHE=1
+    cd docs && ./bootstrap.sh && ./bootstrap.sh test
+    ;;
   test|test_cmds|build_bench|bench|bench_cmds|bench_merge|release|release_dryrun)
     $cmd "$@"
     ;;
