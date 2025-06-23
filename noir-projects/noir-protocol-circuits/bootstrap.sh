214e6d4da335c82702b943ffb1af03ae5b74f26d
benesjan <janbenes1234@gmail.com>
2025-06-23 11:24:59 +0000

    disabling 2 broken tests to see if the rest passes

--- noir-projects/noir-protocol-circuits/bootstrap.sh
+++ noir-projects/noir-protocol-circuits/bootstrap.sh
@@ -178,8 +178,6 @@ function test_cmds {
     private-kernel-reset
     private-kernel-tail-to-public
     private-kernel-tail
-    rollup-base-private
-    rollup-base-public
     rollup-block-root
     rollup-block-merge
     rollup-merge rollup-root
