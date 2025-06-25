10b2c95977d9f96c648b3d91389743613dd34b64
Jean M <132435771+jeanmon@users.noreply.github.com>
2025-06-25 18:17:41 +0200

    feat(avm)!: JUMPI opcode (#15279)
    This does not contain the tag U1 checking on conditional value in .cpp
    code.
    TS simulator does it though and unit tests are checking this.
    CPP tag enforcement will be done in #15278

--- barretenberg/cpp/src/barretenberg/vm2/constraining/relations/ctrl_flow.test.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/constraining/relations/ctrl_flow.test.cpp
@@ -37,5 +37,43 @@ TEST(CtrlFlowConstrainingTest, Jump)
                               "PC_NEXT_ROW_INT_CALL_JUMP");
 }
 
+TEST(CtrlFlowConstrainingTest, JumpiTrueCondition)
+{
+    TestTraceContainer trace({
+        { { C::precomputed_first_row, 1 } },
+        { { C::execution_sel, 1 },
+          { C::execution_sel_jumpi, 1 },
+          { C::execution_rop_1_, 120 },
+          { C::execution_next_pc, 220 },
+          { C::execution_register_0_, 1 } }, // True condition
+        { { C::execution_sel, 1 }, { C::execution_last, 1 }, { C::execution_pc, 120 } },
+    });
+
+    check_relation<execution>(trace, execution::SR_PC_NEXT_ROW_JUMPI);
+
+    // Negative test: pc on next row is incorrect
+    trace.set(C::execution_pc, 2, 220);
+    EXPECT_THROW_WITH_MESSAGE(check_relation<execution>(trace, execution::SR_PC_NEXT_ROW_JUMPI), "PC_NEXT_ROW_JUMPI");
+}
+
+TEST(CtrlFlowConstrainingTest, JumpiFalseCondition)
+{
+    TestTraceContainer trace({
+        { { C::precomputed_first_row, 1 } },
+        { { C::execution_sel, 1 },
+          { C::execution_sel_jumpi, 1 },
+          { C::execution_rop_1_, 120 },
+          { C::execution_next_pc, 220 },
+          { C::execution_register_0_, 0 } }, // False condition
+        { { C::execution_sel, 1 }, { C::execution_last, 1 }, { C::execution_pc, 220 } },
+    });
+
+    check_relation<execution>(trace, execution::SR_PC_NEXT_ROW_JUMPI);
+
+    // Negative test: pc on next row is incorrect
+    trace.set(C::execution_pc, 2, 120);
+    EXPECT_THROW_WITH_MESSAGE(check_relation<execution>(trace, execution::SR_PC_NEXT_ROW_JUMPI), "PC_NEXT_ROW_JUMPI");
+}
+
 } // namespace
 } // namespace bb::avm2::constraining
