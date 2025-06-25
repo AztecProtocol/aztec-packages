eee624f7089150104ab910b6cf9d9a2c020cb0a6
Alex Gherghisan <alexghr@users.noreply.github.com>
2025-06-25 09:29:24 +0000

    fix: shutdown with appropriate exit codes

--- yarn-project/aztec/src/cli/util.ts
+++ yarn-project/aztec/src/cli/util.ts
@@ -14,13 +14,50 @@ import type { Command } from 'commander';
 
 import { type AztecStartOption, aztecStartOptions } from './aztec_start_options.js';
 
+export const enum ExitCode {
+  SUCCESS = 0,
+  ERROR = 1,
+  ROLLUP_UPGRADE = 78, // EX_CONFIG from FreeBSD (https://man.freebsd.org/cgi/man.cgi?query=sysexits)
+  VERSION_UPGRADE = 79, // prev + 1 because there's nothing better
+  // 128 + int(SIGNAL)
+  SIGHUP = 129,
+  SIGINT = 130,
+  SIGQUIT = 131,
+  SIGTERM = 143,
+}
+
+let shutdownPromise: Promise<never> | undefined;
+export function shutdown(logFn: LogFn, exitCode: ExitCode, cb?: Array<() => Promise<void>>): Promise<never> {
+  if (shutdownPromise) {
+    logFn('Already shutting down.');
+    return shutdownPromise;
+  }
+
+  logFn('Shutting down...', { exitCode });
+  if (cb) {
+    shutdownPromise = Promise.allSettled(cb).then(() => process.exit(exitCode));
+  } else {
+    // synchronously shuts down the process
+    // no need to set shutdownPromise on this branch of the if statement because no more code will be executed
+    process.exit(exitCode);
+  }
+
+  return shutdownPromise;
+}
+
+export function isShuttingDown(): boolean {
+  return shutdownPromise !== undefined;
+}
+
 export const installSignalHandlers = (logFn: LogFn, cb?: Array<() => Promise<void>>) => {
-  process.removeAllListeners('SIGINT');
-  process.removeAllListeners('SIGTERM');
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  process.once('SIGINT', () => shutdown(logFn, cb));
+  process.once('SIGINT', () => shutdown(logFn, ExitCode.SIGINT, cb));
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  process.once('SIGTERM', () => shutdown(logFn, cb));
+  process.once('SIGTERM', () => shutdown(logFn, ExitCode.SIGTERM, cb));
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  process.once('SIGHUP', () => shutdown(logFn, ExitCode.SIGHUP, cb));
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  process.once('SIGQUIT', () => shutdown(logFn, ExitCode.SIGQUIT, cb));
 };
 
 /**
@@ -262,6 +299,10 @@ export async function setupUpdateMonitor(
 
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   checker.on('newRollupVersion', async ({ latestVersion, currentVersion }) => {
+    if (isShuttingDown()) {
+      return;
+    }
+
     // if node follows canonical rollup then this is equivalent to a config update
     if (!followsCanonicalRollup) {
       return;
@@ -269,7 +310,7 @@ export async function setupUpdateMonitor(
 
     if (autoUpdateMode === 'config' || autoUpdateMode === 'config-and-version') {
       logger.info(`New rollup version detected. Please restart the node`, { latestVersion, currentVersion });
-      await shutdown(logger.info, signalHandlers);
+      await shutdown(logger.info, ExitCode.ROLLUP_UPGRADE, signalHandlers);
     } else if (autoUpdateMode === 'notify') {
       logger.warn(`New rollup detected. Please restart the node`, { latestVersion, currentVersion });
     }
@@ -277,9 +318,12 @@ export async function setupUpdateMonitor(
 
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   checker.on('newNodeVersion', async ({ latestVersion, currentVersion }) => {
+    if (isShuttingDown()) {
+      return;
+    }
     if (autoUpdateMode === 'config-and-version') {
       logger.info(`New node version detected. Please update and restart the node`, { latestVersion, currentVersion });
-      await shutdown(logger.info, signalHandlers);
+      await shutdown(logger.info, ExitCode.VERSION_UPGRADE, signalHandlers);
     } else if (autoUpdateMode === 'notify') {
       logger.info(`New node version detected. Please update and restart the node`, { latestVersion, currentVersion });
     }
@@ -287,6 +331,10 @@ export async function setupUpdateMonitor(
 
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   checker.on('updateNodeConfig', async config => {
+    if (isShuttingDown()) {
+      return;
+    }
+
     if ((autoUpdateMode === 'config' || autoUpdateMode === 'config-and-version') && updateNodeConfig) {
       logger.warn(`Config change detected. Updating node`, config);
       try {
@@ -314,12 +362,4 @@ export async function setupUpdateMonitor(
   });
 
   checker.start();
-}
-
-export async function shutdown(logFn: LogFn, cb?: Array<() => Promise<void>>) {
-  logFn('Shutting down...');
-  if (cb) {
-    await Promise.all(cb);
-  }
-  process.exit(0);
 }
