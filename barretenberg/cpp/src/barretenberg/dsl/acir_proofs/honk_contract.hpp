132304224893de7fe287fdeec17dc6c23fcd0ff2
Maddiaa <47148561+Maddiaa0@users.noreply.github.com>
2025-06-26 19:04:46 +0800

    chore: correct pairing comments in honk verifier to reference G2 points (#15305)
    reapply https://github.com/AztecProtocol/aztec-packages/pull/14718 on
    next
    
    Co-authored-by: Michael Elliot <michael@digital.com.au>

--- barretenberg/cpp/src/barretenberg/dsl/acir_proofs/honk_contract.hpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_proofs/honk_contract.hpp
@@ -1455,14 +1455,14 @@ function pairing(Honk.G1Point memory rhs, Honk.G1Point memory lhs) view returns 
     bytes memory input = abi.encodePacked(
         rhs.x,
         rhs.y,
-        // Fixed G1 point
+        // Fixed G2 point
         uint256(0x198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c2),
         uint256(0x1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed),
         uint256(0x090689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b),
         uint256(0x12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa),
         lhs.x,
         lhs.y,
-        // G1 point from VK
+        // G2 point from VK
         uint256(0x260e01b251f6f1c7e7ff4e580791dee8ea51d87a358e038b4efe30fac09383c1),
         uint256(0x0118c4d5b837bcc2bc89b5b398b5974e9f5944073b32078b7e231fec938883b0),
         uint256(0x04fc6369f7110fe3d25156c1bb9a72859cf2a04641f99ba4ee413c80da6a5fe4),
