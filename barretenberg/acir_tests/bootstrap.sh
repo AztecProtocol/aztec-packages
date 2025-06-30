c9d734cf97e84449fb1f5ad6f42b13ac62143580
Tom French <15848336+TomAFrench@users.noreply.github.com>
2025-06-30 12:22:31 +0000

    .

--- barretenberg/acir_tests/bootstrap.sh
+++ barretenberg/acir_tests/bootstrap.sh
@@ -95,6 +95,8 @@ function build {
     cp -R ../../noir/noir-repo/test_programs/execution_success acir_tests
     # Running these requires extra gluecode so they're skipped.
     rm -rf acir_tests/{diamond_deps_0,workspace,workspace_default_member,regression_7323}
+
+    rm -rf acir_tests/{ecdsa_secp256k1_invalid_pub_key_in_inactive_branch, ecdsa_secp256r1_invalid_pub_key_in_inactive_branch}
     # These are breaking with:
     # Failed to solve program: 'Failed to solve blackbox function: embedded_curve_add, reason: Infinite input: embedded_curve_add(infinity, infinity)'
     rm -rf acir_tests/{regression_5045,regression_7744}
