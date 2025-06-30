4b470ba9e23e813bcb4c721a486cb8a874a8dfad
jeanmon <jean@aztec-labs.com>
2025-06-30 10:38:32 +0000

    Remove the constraining unit test

--- barretenberg/cpp/src/barretenberg/vm2/constraining/relations/ctrl_flow.test.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/constraining/relations/ctrl_flow.test.cpp
@@ -77,32 +77,5 @@ TEST(CtrlFlowConstrainingTest, JumpiFalseCondition)
     EXPECT_THROW_WITH_MESSAGE(check_relation<execution>(trace, execution::SR_PC_NEXT_ROW_JUMPI), "PC_NEXT_ROW_JUMPI");
 }
 
-TEST(CtrlFlowConstrainingTest, JumpiWrongTag)
-{
-    TestTraceContainer trace({
-        { { C::precomputed_first_row, 1 } },
-        { { C::execution_sel, 1 },
-          { C::execution_sel_jumpi, 1 },
-          { C::execution_sel_should_read_registers, 1 },
-          { C::execution_sel_tag_check_reg_0_, 1 },
-          { C::execution_mem_tag_reg_0_, 3 },
-          { C::execution_expected_tag_reg_0_, 0 },
-          { C::execution_sel_register_read_error, 1 },
-          { C::execution_batched_tags_diff_inv_reg, FF(3).invert() } },
-    });
-
-    check_relation<registers>(trace, registers::SR_REGISTER_READ_TAG_CHECK);
-
-    // Negative test: disable the error
-    trace.set(C::execution_sel_register_read_error, 1, 0);
-    EXPECT_THROW_WITH_MESSAGE(check_relation<registers>(trace, registers::SR_REGISTER_READ_TAG_CHECK),
-                              "REGISTER_READ_TAG_CHECK");
-
-    // Negative test: in addition attempts to put zero in execution_batched_tags_diff_inv_reg
-    trace.set(C::execution_batched_tags_diff_inv_reg, 1, 0);
-    EXPECT_THROW_WITH_MESSAGE(check_relation<registers>(trace, registers::SR_REGISTER_READ_TAG_CHECK),
-                              "REGISTER_READ_TAG_CHECK");
-}
-
 } // namespace
 } // namespace bb::avm2::constraining
