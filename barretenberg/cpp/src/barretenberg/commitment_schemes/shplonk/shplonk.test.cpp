ca75e777db9d48d02081d812a30ea9c99804c1ab
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2025-07-03 14:13:58 +0000

    New test structure

--- barretenberg/cpp/src/barretenberg/commitment_schemes/shplonk/shplonk.test.cpp
+++ barretenberg/cpp/src/barretenberg/commitment_schemes/shplonk/shplonk.test.cpp
@@ -12,67 +12,6 @@ template <class Params> class ShplonkTest : public CommitmentTest<Params> {
   public:
     size_t log_n = 4;
     size_t n = 1UL << log_n;
-
-    using Fr = typename Params::ScalarField;
-    using ProverOpeningClaim = ProverOpeningClaim<Params>;
-    using OpeningClaim = OpeningClaim<Params>;
-    using Commitment = Params::AffineElement;
-
-    class TestData {
-      public:
-        std::vector<ProverOpeningClaim> prover_opening_claims;
-        std::vector<OpeningClaim> verifier_opening_claims;
-
-        void add_poly_evaluation(const CommitmentKey<Params>& ck, const Polynomial<Fr>& poly)
-        {
-            auto r = Fr::random_element();
-            auto eval = poly.evaluate(r);
-            auto commit = ck.commit(poly);
-            this->prover_opening_claims.emplace_back(ProverOpeningClaim(poly, { r, eval }));
-            this->verifier_opening_claims.emplace_back(OpeningClaim({ r, eval }, commit));
-        }
-
-        std::pair<std::vector<Fr>, Polynomial<Fr>> linear_combination(const std::vector<size_t>& indices,
-                                                                      const size_t& max_poly_degree)
-        {
-            std::vector<Fr> coefficients;
-            Polynomial<Fr> poly(max_poly_degree);
-            for (const auto& index : indices) {
-                auto tmp = Fr::random_element();
-                coefficients.emplace_back(tmp);
-                poly.add_scaled(this->prover_opening_claims[index].polynomial, tmp);
-            }
-
-            return std::make_pair(coefficients, poly);
-        }
-
-        std::vector<Commitment> extract_commitments()
-        {
-            std::vector<Commitment> commitments;
-            commitments.reserve(verifier_opening_claims.size());
-            for (const auto& claim : verifier_opening_claims) {
-                commitments.emplace_back(claim.commitment);
-            }
-
-            return commitments;
-        }
-    };
-
-    TestData generate_test_data(const size_t& num_polys, const std::vector<size_t>& poly_degrees)
-    {
-        TestData setup;
-
-        for (size_t idx = 0; idx < num_polys; idx++) {
-            auto tmp_poly = Polynomial<Fr>::random(poly_degrees[idx]);
-            auto tmp_commit = this->commit(tmp_poly);
-            auto tmp_r = Fr::random_element();
-            auto tmp_eval = tmp_poly.evaluate(tmp_r);
-            setup.prover_opening_claims.emplace_back(ProverOpeningClaim(tmp_poly, { tmp_r, tmp_eval }));
-            setup.verifier_opening_claims.emplace_back(OpeningClaim({ tmp_r, tmp_eval }, tmp_commit));
-        }
-
-        return setup;
-    }
 };
 
 using CurveTypes = ::testing::Types<curve::BN254, curve::Grumpkin>;
@@ -81,6 +20,7 @@ TYPED_TEST_SUITE(ShplonkTest, CurveTypes);
 // Test of Shplonk prover/verifier for two polynomials of different sizes, each opened at a single (different) point
 TYPED_TEST(ShplonkTest, ShplonkSimple)
 {
+    using ClaimData = UnivariateClaimData<TypeParam>;
     using ShplonkProver = ShplonkProver_<TypeParam>;
     using ShplonkVerifier = ShplonkVerifier_<TypeParam>;
 
@@ -88,10 +28,11 @@ TYPED_TEST(ShplonkTest, ShplonkSimple)
 
     // Generate two random (unrelated) polynomials of two different sizes, as well as their evaluations at a (single
     // but different) random point and their commitments.
-    auto setup = this->generate_test_data(2, { this->n, this->n / 2 });
+    auto setup = this->generate_claim_data({ this->n, this->n / 2 });
 
     // Execute the shplonk prover functionality
-    const auto batched_opening_claim = ShplonkProver::prove(this->ck(), setup.prover_opening_claims, prover_transcript);
+    auto prover_opening_claims = ClaimData::prover_opening_claims(setup);
+    const auto batched_opening_claim = ShplonkProver::prove(this->ck(), prover_opening_claims, prover_transcript);
     // An intermediate check to confirm the opening of the shplonk prover witness Q
     this->verify_opening_pair(batched_opening_claim.opening_pair, batched_opening_claim.polynomial);
 
@@ -100,7 +41,7 @@ TYPED_TEST(ShplonkTest, ShplonkSimple)
 
     // Execute the shplonk verifier functionality
     const auto batched_verifier_claim = ShplonkVerifier::reduce_verification(
-        this->vk().get_g1_identity(), setup.verifier_opening_claims, verifier_transcript);
+        this->vk().get_g1_identity(), ClaimData::verifier_opening_claims(setup), verifier_transcript);
 
     this->verify_opening_claim(batched_verifier_claim, batched_opening_claim.polynomial);
 }
@@ -108,28 +49,25 @@ TYPED_TEST(ShplonkTest, ShplonkSimple)
 // Test of Shplonk prover/verifier for polynomials that are linearly dependent
 TYPED_TEST(ShplonkTest, ShplonkLinearlyDependent)
 {
+    using ClaimData = UnivariateClaimData<TypeParam>;
     using ShplonkProver = ShplonkProver_<TypeParam>;
     using ShplonkVerifier = ShplonkVerifier_<TypeParam>;
     using Fr = typename TypeParam::ScalarField;
-    using Commitment = typename ShplonkTest<TypeParam>::Commitment;
 
     auto prover_transcript = NativeTranscript::prover_init_empty();
 
     // Generate two random (unrelated) polynomials of two different sizes and a random linear combinations
-    auto setup = this->generate_test_data(2, { this->n, this->n / 2 });
+    auto setup = this->generate_claim_data({ this->n, this->n / 2 });
 
     // Extract the commitments to be used in the Shplonk verifier
-    std::vector<Commitment> commitments = setup.extract_commitments();
+    auto commitments = ClaimData::polynomial_commitments(setup);
 
-    // Linearly combine the polynomials
-    auto [coefficients, poly] = setup.linear_combination({ 0, 1 }, this->n);
-
-    // Add evaluation of linear combination
-    setup.add_poly_evaluation(this->ck(), poly);
+    // Linearly combine the polynomials and evalu
+    auto [coefficients, evals] = this->combine_claims(setup);
 
     // Execute the shplonk prover functionality
-    auto opening_claims = setup.prover_opening_claims; // Need to copy them because `prove` modifies the opening claims
-    const auto batched_opening_claim = ShplonkProver::prove(this->ck(), opening_claims, prover_transcript);
+    auto prover_opening_claims = ClaimData::prover_opening_claims(setup);
+    const auto batched_opening_claim = ShplonkProver::prove(this->ck(), prover_opening_claims, prover_transcript);
     // An intermediate check to confirm the opening of the shplonk prover witness Q
     this->verify_opening_pair(batched_opening_claim.opening_pair, batched_opening_claim.polynomial);
 
@@ -138,22 +76,18 @@ TYPED_TEST(ShplonkTest, ShplonkLinearlyDependent)
     ShplonkVerifier verifier(commitments, verifier_transcript);
 
     // Update internal state for poly1(r1) = eval1
+    auto verifier_opening_claims = ClaimData::verifier_opening_claims(setup);
     verifier.update({ 0 },
                     { Fr(1) },
-                    { setup.verifier_opening_claims[0].opening_pair.evaluation },
-                    setup.verifier_opening_claims[0].opening_pair.challenge);
+                    { verifier_opening_claims[0].opening_pair.evaluation },
+                    verifier_opening_claims[0].opening_pair.challenge);
     // Update internal state for poly2(r2) = eval2
     verifier.update({ 1 },
                     { Fr(1) },
-                    { setup.verifier_opening_claims[1].opening_pair.evaluation },
-                    setup.verifier_opening_claims[1].opening_pair.challenge);
+                    { verifier_opening_claims[1].opening_pair.evaluation },
+                    verifier_opening_claims[1].opening_pair.challenge);
     // Update internal state for poly3(r3) = eval3
-    auto r = setup.verifier_opening_claims[2].opening_pair.challenge;
-    verifier.update({ 0, 1 },
-                    coefficients,
-                    { setup.prover_opening_claims[0].polynomial.evaluate(r),
-                      setup.prover_opening_claims[1].polynomial.evaluate(r) },
-                    r);
+    verifier.update({ 0, 1 }, coefficients, evals, verifier_opening_claims[2].opening_pair.challenge);
 
     // Execute the shplonk verifier functionality
     const auto batched_verifier_claim = verifier.finalize(this->vk().get_g1_identity());
@@ -166,16 +100,18 @@ using ShplonkBN254 = ShplonkTest<curve::BN254>;
 
 TEST_F(ShplonkBN254, ExtractStateAndVerify)
 {
+    using ClaimData = UnivariateClaimData<curve::BN254>;
     using ShplonkProver = ShplonkProver_<curve::BN254>;
     using ShplonkVerifier = ShplonkVerifier_<curve::BN254>;
 
     auto prover_transcript = NativeTranscript::prover_init_empty();
 
     // Generate two random (unrelated) polynomials of two different sizes and a random linear combinations
-    auto setup = this->generate_test_data(2, { ShplonkTest<curve::BN254>::n, ShplonkTest<curve::BN254>::n / 2 });
+    auto setup = this->generate_claim_data({ this->n, this->n / 2 });
 
     // Execute the shplonk prover functionality
-    const auto batched_opening_claim = ShplonkProver::prove(this->ck(), setup.prover_opening_claims, prover_transcript);
+    auto prover_opening_claims = ClaimData::prover_opening_claims(setup);
+    const auto batched_opening_claim = ShplonkProver::prove(this->ck(), prover_opening_claims, prover_transcript);
     // An intermediate check to confirm the opening of the shplonk prover witness Q
     this->verify_opening_pair(batched_opening_claim.opening_pair, batched_opening_claim.polynomial);
     // Compute KZG proof
@@ -183,11 +119,10 @@ TEST_F(ShplonkBN254, ExtractStateAndVerify)
 
     // Shplonk verification
     auto verifier_transcript = NativeTranscript::verifier_init_empty(prover_transcript);
-    std::vector<Commitment> commitments = setup.extract_commitments();
 
     // Execute the shplonk verifier functionality
-    auto verifier =
-        ShplonkVerifier::reduce_verification_no_finalize(setup.verifier_opening_claims, verifier_transcript);
+    auto verifier_opening_claims = ClaimData::verifier_opening_claims(setup);
+    auto verifier = ShplonkVerifier::reduce_verification_no_finalize(verifier_opening_claims, verifier_transcript);
 
     // Export state
     const auto batched_verifier_claim = verifier.export_state(this->vk().get_g1_identity());
