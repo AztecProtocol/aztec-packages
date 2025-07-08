08e3cf9dea533c00159ae144dd10643170b4240c
spypsy <spypsy@outlook.com>
2025-07-08 11:36:49 +0000

    update values

--- spartan/aztec-network/eth-devnet/entrypoints/eth-execution.sh
+++ spartan/aztec-network/eth-devnet/entrypoints/eth-execution.sh
@@ -1,25 +1,20 @@
 #! /bin/bash
 
-reth node \
-    --http \
-    --http.port=${HTTP_PORT} \
-    --http.addr="0.0.0.0" \
-    --http.api="admin,net,eth,web3,debug,trace" \
-    --http.corsdomain="*" \
-    --ws \
-    --ws.addr="0.0.0.0" \
-    --ws.port=${WS_PORT} \
-    --ws.api="admin,net,eth,web3,debug,trace" \
-    --ws.origins="*" \
-    --txpool.max-tx-input-bytes=${MAX_TX_INPUT_SIZE_BYTES} \
-    --max-outbound-peers=0 \
-    --max-inbound-peers=0 \
-    --ipcdisable \
-    --disable-discovery \
-    --authrpc.addr="0.0.0.0" \
-    --authrpc.port=8551 \
-    --authrpc.jwtsecret="/genesis/jwt-secret.hex" \
-    --chain="/genesis/genesis.json" \
-    --datadir="/data" \
-    --log.stdout.format=json \
-    -vvv
+# Get the execution client from the first argument
+execution_client=${1:-"reth"}
+
+case "$execution_client" in
+"reth")
+  /eth-execution-reth.sh
+  ;;
+"geth")
+  /eth-execution-geth.sh
+  ;;
+"nethermind")
+  /eth-execution-nethermind.sh
+  ;;
+*)
+  echo "Unknown execution client: $execution_client"
+  exit 1
+  ;;
+esac
