f2ec06ef821239e7e9b92b3f80fca441e79f2ee0
Facundo <fcarreiro@users.noreply.github.com>
2025-07-08 13:11:51 +0100

    feat(avm)!: debuglog prover side (#15566)
    Please read [contributing guidelines](CONTRIBUTING.md) and remove this
    line.
    
    For audit-related pull requests, please use the [audit PR
    template](?expand=1&template=audit.md).

--- barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
@@ -84,10 +84,10 @@ namespace bb::avm2 {
 
 struct AvmFlavorVariables {
     static constexpr size_t NUM_PRECOMPUTED_ENTITIES = 127;
-    static constexpr size_t NUM_WITNESS_ENTITIES = 2226;
+    static constexpr size_t NUM_WITNESS_ENTITIES = 2227;
     static constexpr size_t NUM_SHIFTED_ENTITIES = 248;
     static constexpr size_t NUM_WIRES = NUM_WITNESS_ENTITIES + NUM_PRECOMPUTED_ENTITIES;
-    static constexpr size_t NUM_ALL_ENTITIES = 2601;
+    static constexpr size_t NUM_ALL_ENTITIES = 2602;
 
     // Need to be templated for recursive verifier
     template <typename FF_>
