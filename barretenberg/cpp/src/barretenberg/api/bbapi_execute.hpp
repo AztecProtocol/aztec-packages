f936e125e0a912bb1c7686c144d552da57fb5b12
ludamad <domuradical@gmail.com>
2025-07-09 14:25:25 +0000

    work on && refs

--- barretenberg/cpp/src/barretenberg/api/bbapi_execute.hpp
+++ barretenberg/cpp/src/barretenberg/api/bbapi_execute.hpp
@@ -7,17 +7,6 @@
 namespace bb::bbapi {
 
 /**
- * @brief Convert a vector of field elements to JSON array format
- */
-inline std::string field_elements_to_json(const std::vector<bb::fr>& fields)
-{
-    if (fields.empty()) {
-        return "[]";
-    }
-    return format("[", join(transform::map(fields, [](auto fr) { return format("\"", fr, "\""); })), "]");
-}
-
-/**
  * @brief Executes a command by visiting a variant of all possible commands.
  *
  * @param command The command to execute, consumed by this function.
@@ -28,11 +17,7 @@ inline CommandResponse execute(BBApiRequest& request, Command&& command)
 {
     return std::move(command).visit([&request](auto&& cmd) -> CommandResponse {
         using CmdType = std::decay_t<decltype(cmd)>;
-        if constexpr (RequiresBBApiRequest<CmdType>) {
-            return cmd.execute(request);
-        } else {
-            return cmd.execute(request);
-        }
+        return std::forward<CmdType>(cmd).execute(request);
     });
 }
 
@@ -42,12 +27,7 @@ inline std::vector<CommandResponse> execute_request(BBApiRequest&& request, std:
     std::vector<CommandResponse> responses;
     responses.reserve(commands.size());
     for (Command& command : commands) {
-        try {
-            responses.push_back(execute(request, std::move(command)));
-        } catch (const std::exception& e) {
-            // If there was an error, we stop processing further commands.
-            throw_or_abort(e.what());
-        }
+        responses.push_back(execute(request, std::move(command)));
     }
     return responses;
 }
