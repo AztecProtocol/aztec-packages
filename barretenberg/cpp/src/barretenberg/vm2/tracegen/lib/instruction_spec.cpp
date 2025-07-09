cd9cd0195a421e8e0751895b8cda80ecdd50f9d6
sirasistant <sirasistant@gmail.com>
2025-07-09 12:46:30 +0000

    feat(avm)!: Implement SLOAD opcode

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/lib/instruction_spec.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/lib/instruction_spec.cpp
@@ -72,6 +72,8 @@ const std::unordered_map<ExecutionOpCode, SubtraceInfo> SUBTRACE_INFO_MAP = {
       { .subtrace_selector = SubtraceSel::EXECUTION, .subtrace_operation_id = AVM_EXEC_OP_ID_DEBUGLOG } },
     // KeccakF1600
     { ExecutionOpCode::KECCAKF1600, { .subtrace_selector = SubtraceSel::KECCAKF1600, .subtrace_operation_id = 0 } },
+    { ExecutionOpCode::SLOAD,
+      { .subtrace_selector = SubtraceSel::EXECUTION, .subtrace_operation_id = AVM_EXEC_OP_ID_SLOAD } },
 };
 
 } // namespace bb::avm2::tracegen
