1911ac9796fabefff8f30d2ecceb9829187e93c4
Lucas Xia <lucasxia01@gmail.com>
2025-07-10 12:42:46 -0400

    feat: fiat-shamir VK in ECCVM and Translator (#15640)
    FS the VKs in the ECCVM and Translator.
    
    Closes https://github.com/AztecProtocol/barretenberg/issues/1466.

--- barretenberg/cpp/src/barretenberg/eccvm/eccvm_flavor.hpp
+++ barretenberg/cpp/src/barretenberg/eccvm/eccvm_flavor.hpp
@@ -814,8 +814,10 @@ class ECCVMFlavor {
         fr add_hash_to_transcript([[maybe_unused]] const std::string& domain_separator,
                                   [[maybe_unused]] Transcript& transcript) const override
         {
-            // TODO(https://github.com/AztecProtocol/barretenberg/issues/1466): Implement this function.
-            throw_or_abort("Not implemented yet!");
+            for (const Commitment& commitment : this->get_all()) {
+                transcript.add_to_independent_hash_buffer(domain_separator + "vk_commitment", commitment);
+            }
+            return transcript.hash_independent_buffer(domain_separator + "vk_hash");
         }
 
         // TODO(https://github.com/AztecProtocol/barretenberg/issues/1324): Remove `circuit_size` and `log_circuit_size`
