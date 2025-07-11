93f7f0b723be9f021728e8bb82251673665913b8
sirasistant <sirasistant@gmail.com>
2025-07-11 15:47:41 +0000

    remove from minimal

--- yarn-project/simulator/src/public/fixtures/minimal_public_tx.ts
+++ yarn-project/simulator/src/public/fixtures/minimal_public_tx.ts
@@ -4,7 +4,7 @@ import { AztecAddress } from '@aztec/stdlib/aztec-address';
 
 import avmMinimalCircuitInputsJson from '../../../artifacts/avm_minimal_inputs.json' with { type: 'json' };
 import { TypeTag } from '../avm/avm_memory_types.js';
-import { Add, Return, SLoad, SStore, Set } from '../avm/opcodes/index.js';
+import { Add, Return, Set } from '../avm/opcodes/index.js';
 import { encodeToBytecode } from '../avm/serialization/bytecode_serialization.js';
 import { Opcode } from '../avm/serialization/instruction_serialization.js';
 import type { PublicTxResult } from '../public_tx_simulator/public_tx_simulator.js';
@@ -19,10 +19,6 @@ export async function createAvmMinimalPublicTx(): Promise<PublicTxResult> {
     new Set(/*indirect*/ 0, /*dstOffset*/ 0, TypeTag.UINT32, /*value*/ 1).as(Opcode.SET_8, Set.wireFormat8),
     new Set(/*indirect*/ 0, /*dstOffset*/ 1, TypeTag.UINT32, /*value*/ 2).as(Opcode.SET_8, Set.wireFormat8),
     new Add(/*indirect=*/ 0, /*aOffset=*/ 0, /*bOffset=*/ 1, /*dstOffset=*/ 2).as(Opcode.ADD_8, Add.wireFormat8),
-    new Set(/*indirect*/ 0, /*dstOffset*/ 3, TypeTag.FIELD, /*value*/ 27n).as(Opcode.SET_FF, Set.wireFormatFF),
-    new Set(/*indirect*/ 0, /*dstOffset*/ 4, TypeTag.FIELD, /*value*/ 42n).as(Opcode.SET_FF, Set.wireFormatFF),
-    new SStore(/*indirect*/ 0, /*value*/ 4, /*slot*/ 3).as(Opcode.SSTORE, SStore.wireFormat),
-    new SLoad(/*indirect*/ 0, /*slot*/ 3, /*dstOffset*/ 5).as(Opcode.SLOAD, SLoad.wireFormat),
     new Return(/*indirect=*/ 0, /*copySizeOffset=*/ 0, /*returnOffset=*/ 2),
   ]);
 
