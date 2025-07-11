537cc14c34fa9ce1c03d4b00617f1a7a9002a127
Alex Gherghisan <alexghr@users.noreply.github.com>
2025-07-11 13:15:49 +0100

    refactor: split staging deployment (#15668)
    More refactoring for staging

---
+++ spartan/terraform/deploy-staging-testnet/variables.tf
@@ -0,0 +1,53 @@
+variable "GCP_PROJECT" {
+  description = "GCP project id"
+  type        = string
+  default     = "testnet-440309"
+}
+
+variable "GCP_REGION" {
+  default = "us-west1"
+  type    = string
+}
+
+variable "GKE_CLUSTER_CONTEXT" {
+  description = "GKE cluster context"
+  type        = string
+  default     = "gke_testnet-440309_us-west1-a_aztec-gke-public"
+}
+
+variable "RELEASE_PREFIX" {
+  description = "The prefix to use for the helm installs"
+  type        = string
+  default     = "staging-testnet"
+}
+
+variable "NAMESPACE" {
+  description = "The namespace to install into"
+  type        = string
+  default     = "staging-testnet"
+}
+
+variable "AZTEC_DOCKER_IMAGE" {
+  description = "Docker image to use for the aztec network"
+  type        = string
+  default     = "aztecprotocol/aztec:staging"
+}
+
+variable "METRICS_NAMESPACE" {
+  description = "Namespace to deploy the metrics to"
+  type        = string
+  default     = "metrics"
+}
+
+variable "VALIDATOR_VALUES" {
+  description = "The values file to apply"
+  type        = string
+  default     = "staging-testnet-validator.yaml"
+}
+
+variable "PROVER_VALUES" {
+  description = "The values file to apply"
+  type        = string
+  default     = "staging-testnet-prover.yaml"
+}
+
