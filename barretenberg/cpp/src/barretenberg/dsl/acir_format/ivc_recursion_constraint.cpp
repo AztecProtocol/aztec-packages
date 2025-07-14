642b57c121619c9d590fd0d803a2ea862057d701
maramihali <mara@aztecprotocol.com>
2025-07-14 22:33:15 +0100

    chore: specify number of circuits in CIVC (#15694)
    Specify the number of circuits accumulated during an iteration of CIVC
    (i.e. how many `accumulate` calls we expect in total).

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/ivc_recursion_constraint.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/ivc_recursion_constraint.cpp
@@ -157,7 +157,7 @@ std::shared_ptr<ClientIVC::DeciderVerificationKey> create_mock_decider_vk()
 std::shared_ptr<ClientIVC> create_mock_ivc_from_constraints(const std::vector<RecursionConstraint>& constraints,
                                                             const TraceSettings& trace_settings)
 {
-    auto ivc = std::make_shared<ClientIVC>(trace_settings);
+    auto ivc = std::make_shared<ClientIVC>(constraints.size(), trace_settings);
 
     uint32_t oink_type = static_cast<uint32_t>(PROOF_TYPE::OINK);
     uint32_t pg_type = static_cast<uint32_t>(PROOF_TYPE::PG);
@@ -242,7 +242,7 @@ void mock_ivc_accumulation(const std::shared_ptr<ClientIVC>& ivc, ClientIVC::QUE
         acir_format::create_mock_verification_queue_entry(type, ivc->trace_settings, is_kernel);
     ivc->verification_queue.emplace_back(entry);
     ivc->goblin.merge_verification_queue.emplace_back(acir_format::create_dummy_merge_proof());
-    ivc->initialized = true;
+    ivc->num_circuits_accumulated++;
 }
 
 /**
