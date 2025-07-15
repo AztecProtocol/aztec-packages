2582abd1bcee0aef1e1a0e804cf674305fbc6e9b
Alex Gherghisan <alexghr@users.noreply.github.com>
2025-07-15 23:54:27 +0100

    fix: use cached VK for public base rollup (#15748)
    We're back

--- noir-projects/noir-protocol-circuits/bootstrap.sh
+++ noir-projects/noir-protocol-circuits/bootstrap.sh
@@ -10,6 +10,7 @@ export PLATFORM_TAG=any
 export BB=${BB:-../../barretenberg/cpp/build/bin/bb}
 export NARGO=${NARGO:-../../noir/noir-repo/target/release/nargo}
 export BB_HASH=$(../../barretenberg/cpp/bootstrap.sh hash)
+export BB_HASH_FOR_PUBLIC_BASE_ROLLUP=$(PLATFORM_TAG="x86_64" ../../barretenberg/cpp/bootstrap.sh hash)
 export NOIR_HASH=${NOIR_HASH:-$(../../noir/bootstrap.sh hash)}
 
 export key_dir=./target/keys
@@ -95,10 +96,22 @@ function compile {
   # TODO: Change this to add verification_key to original json, like contracts does.
   # Will require changing TS code downstream.
   bytecode_hash=$(jq -r '.bytecode' $json_path | sha256sum | tr -d ' -')
-  hash=$(hash_str "$BB_HASH-$bytecode_hash-$proto")
+  if echo "$name" | grep -qE "rollup_base_public"; then
+    hash=$(hash_str "$BB_HASH_FOR_PUBLIC_BASE_ROLLUP-$bytecode_hash-$proto")
+  else
+    hash=$(hash_str "$BB_HASH-$bytecode_hash-$proto")
+  fi
+
   if [ "${USE_CIRCUITS_CACHE:-0}" -eq 0 ] || ! cache_download vk-$hash.tar.gz 1>&2; then
     local key_path="$key_dir/$name.vk.data.json"
     echo_stderr "Generating vk for function: $name..."
+
+    # on ARM builds the AVM is disabled which leads to a different VK. Avoid this issue by reading the VK from the cache for x86
+    if echo "$name" | grep -qE "rollup_base_public" && [ $(uname -m) != "x86_64" ]; then
+      echo_stderr "Refusing to generate VK for $name on $(uname -m)"
+      exit 1
+    fi
+
     SECONDS=0
     outdir=$(mktemp -d)
     trap "rm -rf $outdir" EXIT
