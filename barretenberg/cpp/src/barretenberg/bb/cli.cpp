aa0f43c84fa54d03702a1434794acdf3906718fa
Jonathan Hao <jonathan@aztec-labs.com>
2025-07-15 14:04:35 +0200

    feat: mmap backed polynomials (#15531)
    This makes `SharedShiftedVirtualZeroesArray` holds an abstract
    BackingMemory. When `BB_SLOW_LOW_MEMORY=1` or when the
    `--slow_low_memory` runtime flag is enabled, `BackingMemory` will be
    `FileBackedMemory`. The memory is `mmap`ed to physical files so in
    memory-constrained environment, it won't be OOM. Otherwise,
    `BackingMemory` is `AlignedMemory` and the behavior is unchanged.
    
    See https://github.com/AztecProtocol/barretenberg/issues/1456 for more
    contexts.

--- barretenberg/cpp/src/barretenberg/bb/cli.cpp
+++ barretenberg/cpp/src/barretenberg/bb/cli.cpp
@@ -286,6 +286,11 @@ int parse_and_run_cli_command(int argc, char* argv[])
                                     "Include gates_per_opcode in the output of the gates command.");
     };
 
+    const auto add_slow_low_memory_flag = [&](CLI::App* subcommand) {
+        return subcommand->add_flag(
+            "--slow_low_memory", flags.slow_low_memory, "Enable low memory mode (can be 2x slower or more).");
+    };
+
     /***************************************************************************************************************
      * Top-level flags
      ***************************************************************************************************************/
@@ -348,6 +353,7 @@ int parse_and_run_cli_command(int argc, char* argv[])
     add_ipa_accumulation_flag(prove);
     add_recursive_flag(prove);
     add_honk_recursion_option(prove);
+    add_slow_low_memory_flag(prove);
 
     prove->add_flag("--verify", "Verify the proof natively, resulting in a boolean output. Useful for testing.");
 
@@ -578,6 +584,7 @@ int parse_and_run_cli_command(int argc, char* argv[])
     }
     debug_logging = flags.debug;
     verbose_logging = debug_logging || flags.verbose;
+    slow_low_memory = flags.slow_low_memory;
 
     print_active_subcommands(app);
     info("Scheme is: ", flags.scheme, ", num threads: ", get_num_cpus());
