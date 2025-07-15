1f43a27d7c443bf46672202d67bd5a6219585585
Santiago Palladino <santiago@aztec-labs.com>
2025-07-15 16:09:00 -0300

    Fix slasher key indices

--- yarn-project/end-to-end/src/fixtures/setup_p2p_test.ts
+++ yarn-project/end-to-end/src/fixtures/setup_p2p_test.ts
@@ -21,6 +21,8 @@ import { getEndToEndTestTelemetryClient } from './with_telemetry_utils.js';
 // Setup snapshots will create a node with index 0, and run extra bootstrap with
 // index 1, so all of our loops here need to start from 2 to avoid running validators with the same key
 export const ATTESTER_PRIVATE_KEYS_START_INDEX = 2;
+// We use slasher keys after the first 10 anvil keys, note that this requires anvil to be started with more than the default 10 accounts
+export const SLASHER_PRIVATE_KEYS_START_INDEX = 12;
 
 export interface NodeContext {
   node: AztecNodeService;
@@ -150,9 +152,8 @@ export async function createValidatorConfig(
 ) {
   port = port ?? (await getPort());
 
-  const index = ATTESTER_PRIVATE_KEYS_START_INDEX + addressIndex * 2;
-  const attesterPrivateKey = bufferToHex(getPrivateKeyFromIndex(index)!);
-  const slasherPrivateKey = bufferToHex(getPrivateKeyFromIndex(index + 1)!);
+  const attesterPrivateKey = bufferToHex(getPrivateKeyFromIndex(ATTESTER_PRIVATE_KEYS_START_INDEX + addressIndex)!);
+  const slasherPrivateKey = bufferToHex(getPrivateKeyFromIndex(SLASHER_PRIVATE_KEYS_START_INDEX + addressIndex)!);
 
   config.validatorPrivateKeys = new SecretValue([attesterPrivateKey]);
   config.publisherPrivateKey = new SecretValue(attesterPrivateKey);
