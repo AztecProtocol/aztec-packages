4c8a315e0d0a050d738fab8b1efe96f20e1c863e
Ilyas Ridhuan <ilyas@aztecprotocol.com>
2025-07-16 12:20:55 +0100

    feat(avm): comparison operators in TaggedValue (#15575)
    This PR adds the comparator operands to the `TaggedValue` class. If the
    tags do not match, these operators will return false.
    
    This PR also updates the way errors are handled and used in the ALU and
    bitwise classes. Given that the TaggedValue class is already checking
    the tag of it's inputs, re-checking them when handling the opcodes is
    unnecessary. Instead we propagate the errors up from the TaggedValue
    class
    
    ---------
    
    Co-authored-by: MirandaWood <miranda@aztecprotocol.com>

--- barretenberg/cpp/src/barretenberg/vm2/common/tagged_value.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/common/tagged_value.hpp
@@ -12,6 +12,20 @@
 
 namespace bb::avm2 {
 
+class TagMismatchException : public std::runtime_error {
+  public:
+    TagMismatchException()
+        : std::runtime_error("Mismatched tags")
+    {}
+};
+
+class InvalidOperationTag : public std::runtime_error {
+  public:
+    InvalidOperationTag(const std::string& msg)
+        : std::runtime_error("InvalidOperationTag: " + msg)
+    {}
+};
+
 enum class ValueTag {
     FF = MEM_TAG_FF,
     U1 = MEM_TAG_U1,
@@ -78,9 +92,11 @@ class TaggedValue {
     // Shift operations not valid for FF. They will throw.
     TaggedValue operator<<(const TaggedValue& other) const;
     TaggedValue operator>>(const TaggedValue& other) const;
-
-    bool operator==(const TaggedValue& other) const = default;
-    bool operator!=(const TaggedValue& other) const = default;
+    // Comparison operators. If the tags are not the same, false is returned
+    bool operator<(const TaggedValue& other) const;
+    bool operator<=(const TaggedValue& other) const;
+    bool operator==(const TaggedValue& other) const;
+    bool operator!=(const TaggedValue& other) const;
 
     // Converts any type to FF.
     FF as_ff() const;
