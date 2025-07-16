e4aae870fb0dd5ed95adf5749d9723ca749f1cb9
DanielKotov <katod.daniel@gmail.com>
2025-07-16 22:32:13 +0000

    new tool's design to work with Mega and Ultra

--- barretenberg/cpp/src/barretenberg/boomerang_value_detection/graph_description.test.cpp
+++ barretenberg/cpp/src/barretenberg/boomerang_value_detection/graph_description.test.cpp
@@ -41,7 +41,7 @@ TEST(boomerang_ultra_circuit_constructor, test_graph_for_arithmetic_gates)
 
     StaticAnalyzer graph = StaticAnalyzer(circuit_constructor);
     auto connected_components = graph.find_connected_components();
-    auto variables_in_one_gate = graph.show_variables_in_one_gate(circuit_constructor);
+    auto variables_in_one_gate = graph.get_variables_in_one_gate();
     EXPECT_EQ(variables_in_one_gate.size(), 1024);
     EXPECT_EQ(connected_components.size(), 256);
 }
@@ -99,7 +99,7 @@ TEST(boomerang_ultra_circuit_constructor, test_graph_for_boolean_gates)
     StaticAnalyzer graph = StaticAnalyzer(circuit_constructor);
     auto connected_components = graph.find_connected_components();
     auto num_connected_components = connected_components.size();
-    auto variables_in_one_gate = graph.show_variables_in_one_gate(circuit_constructor);
+    auto variables_in_one_gate = graph.get_variables_in_one_gate();
     bool result = num_connected_components == 0;
     EXPECT_EQ(result, true);
     EXPECT_EQ(variables_in_one_gate.size(), 20);
--- barretenberg/cpp/src/barretenberg/boomerang_value_detection/graph_description_blake3s.test.cpp
+++ barretenberg/cpp/src/barretenberg/boomerang_value_detection/graph_description_blake3s.test.cpp
@@ -39,7 +39,7 @@ TEST(boomerang_stdlib_blake3s, test_single_block_plookup)
     StaticAnalyzer graph = StaticAnalyzer(builder);
     auto connected_components = graph.find_connected_components();
     EXPECT_EQ(connected_components.size(), 1);
-    auto variables_in_one_gate = graph.show_variables_in_one_gate(builder);
+    auto variables_in_one_gate = graph.get_variables_in_one_gate();
     EXPECT_EQ(variables_in_one_gate.size(), 0);
 }
 
@@ -63,6 +63,6 @@ TEST(boomerang_stdlib_blake3s, test_double_block_plookup)
     StaticAnalyzer graph = StaticAnalyzer(builder);
     auto connected_components = graph.find_connected_components();
     EXPECT_EQ(connected_components.size(), 1);
-    auto variables_in_one_gate = graph.show_variables_in_one_gate(builder);
+    auto variables_in_one_gate = graph.get_variables_in_one_gate();
     EXPECT_EQ(variables_in_one_gate.size(), 0);
 }
