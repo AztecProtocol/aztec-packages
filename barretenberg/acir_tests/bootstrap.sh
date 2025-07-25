14b5c23692d43c089c7b71c6052eb0ed2fe42d56
saleel <saleel@saleel.xyz>
2025-07-25 13:06:13 +0000

    uncomment tests

--- barretenberg/acir_tests/bootstrap.sh
+++ barretenberg/acir_tests/bootstrap.sh
@@ -149,8 +149,8 @@ function test_cmds {
   echo "$prefix FLOW=sol_honk USE_OPTIMIZED_CONTRACT=true $run_test assert_statement"
   echo "$prefix FLOW=sol_honk USE_OPTIMIZED_CONTRACT=true $run_test a_1_mul"
   echo "$prefix FLOW=sol_honk USE_OPTIMIZED_CONTRACT=true $run_test slices"
-  # echo "$prefix FLOW=sol_honk USE_OPTIMIZED_CONTRACT=true $run_test verify_honk_proof"
-  # echo "$prefix FLOW=sol_honk USE_OPTIMIZED_CONTRACT=true $run_test double_verify_honk_proof"
+  echo "$prefix FLOW=sol_honk USE_OPTIMIZED_CONTRACT=true $run_test verify_honk_proof"
+  echo "$prefix FLOW=sol_honk USE_OPTIMIZED_CONTRACT=true $run_test double_verify_honk_proof"
 
   # bb.js browser tests. Isolate because server.
   local prefix="$tests_hash:ISOLATE=1:NET=1:CPUS=8"
