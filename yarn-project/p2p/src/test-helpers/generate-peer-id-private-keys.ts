46737cbdb6ca5e80810ac1c8834146523f374123
mralj <nikola.mratinic@gmail.com>
2025-07-29 17:41:57 +0200

    chore: Revert upgraded libp2p (#15911)" (#16071)
    This reverts commit 10f2c5f121b9c0b634f3d711f2ec7d5785bfa54a.
    Due to a massive increase in e2e flakes
    
    ---------
    
    Co-authored-by: Santiago Palladino <spalladino@gmail.com>

--- yarn-project/p2p/src/test-helpers/generate-peer-id-private-keys.ts
+++ yarn-project/p2p/src/test-helpers/generate-peer-id-private-keys.ts
@@ -1,18 +1,15 @@
-import type { PrivateKey } from '@libp2p/interface';
 import { generatePrivateKey } from 'viem/accounts';
-
-import { privateKeyFromHex } from '../util.js';
 
 /**
  * Generate a list of peer id private keys
  * @param numberOfPeers - The number of peer id private keys to generate
  * @returns A list of peer id private keys
  */
-export function generatePeerIdPrivateKeys(numberOfPeers: number): PrivateKey[] {
+export function generatePeerIdPrivateKeys(numberOfPeers: number): string[] {
   const peerIdPrivateKeys: string[] = [];
   for (let i = 0; i < numberOfPeers; i++) {
     // magic number is multiaddr prefix: https://multiformats.io/multiaddr/
     peerIdPrivateKeys.push('08021220' + generatePrivateKey().slice(2, 68));
   }
-  return peerIdPrivateKeys.map(privateKeyFromHex);
+  return peerIdPrivateKeys;
 }
