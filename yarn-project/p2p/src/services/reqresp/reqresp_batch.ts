08f50177a74a7fb34ccde06f44aca4bd2a6ab629
mralj <nikola.mratinic@gmail.com>
2025-07-29 14:01:33 +0000

    More improvements and cleanups

--- yarn-project/p2p/src/services/reqresp/reqresp_batch.ts
+++ yarn-project/p2p/src/services/reqresp/reqresp_batch.ts
@@ -1,8 +1,8 @@
 import { chunk } from '@aztec/foundation/collection';
 import { createLogger } from '@aztec/foundation/log';
-import { promiseWithResolvers } from '@aztec/foundation/promise';
+import { Semaphore } from '@aztec/foundation/queue';
 import type { BlockProposal } from '@aztec/stdlib/p2p';
-import type { Tx, TxArray, TxHash } from '@aztec/stdlib/tx';
+import { type Tx, type TxArray, TxHash } from '@aztec/stdlib/tx';
 
 import type { PeerId } from '@libp2p/interface';
 import { peerIdFromString } from '@libp2p/peer-id';
@@ -13,7 +13,8 @@ import { BlockTxsRequest, BlockTxsResponse } from './protocols/index.js';
 import { ReqRespStatus } from './status.js';
 
 const TX_BATCH_SIZE = 8;
-const PEERS_TO_QUERY_IN_PARALLEL = 10;
+const SMART_PEERS_TO_QUERY_IN_PARALLEL = 10;
+const DUMB_PEERS_TO_QUERY_IN_PARALLEL = 10;
 
 export class BatchTxRequester {
   private readonly peers: PeerId[];
@@ -24,11 +25,11 @@ export class BatchTxRequester {
 
   private readonly deadline;
 
-  private startSmartRequester: ((v: void) => void) | undefined = undefined;
+  private readonly smartRequesterSemaphore = new Semaphore(0);
 
   constructor(
+    missingTxs: TxHash[],
     private readonly blockProposal: BlockProposal,
-    private readonly missingTxs: TxHash[],
     private readonly pinnedPeer: PeerId | undefined,
     private readonly timeoutMs: number,
     private readonly reqresp: ReqRespInterface,
@@ -36,7 +37,7 @@ export class BatchTxRequester {
     private logger = createLogger('p2p:reqresp_batch'),
   ) {
     this.txsMetadata = new MissingTxMetadataCollection(
-      this.missingTxs.map(txHash => [txHash.toString(), new MissingTxMetadata(txHash)]),
+      missingTxs.map(txHash => [txHash.toString(), new MissingTxMetadata(txHash)]),
     );
     this.peers = this.connectionSampler.getPeerListSortedByConnectionCountAsc();
     if (this.pinnedPeer) {
@@ -47,30 +48,45 @@ export class BatchTxRequester {
   }
 
   public async run() {
-    if (this.missingTxs.length === 0) {
+    if (this.txsMetadata.getMissingTxHashes().size === 0) {
       this.logger.debug('No missing txs to request');
       return;
     }
 
-    const { promise, resolve } = promiseWithResolvers<void>();
-    this.startSmartRequester = resolve;
-
     //TODO: executeTimeout?
-    await Promise.allSettled([this.smartRequester(promise), this.dumbRequester()]);
+    await Promise.allSettled([this.smartRequester(), this.dumbRequester(), this.pinnedPeerRequester()]);
 
     //TODO: handle this via async iter
     return this.txsMetadata.getFetchedTxs();
   }
 
-  private async smartRequester(start: Promise<void>) {
+  //TODO: handle pinned peer properly
+  private async pinnedPeerRequester() {
+    while (!this.shouldStop()) {
+      if (!this.pinnedPeer) {
+        this.logger.debug('No pinned peer to request from');
+        return;
+      }
+
+      const txsToRequest = this.txsMetadata.getTxsToRequestFromThePeer(this.pinnedPeer).slice(0, TX_BATCH_SIZE);
+      if (txsToRequest.length === 0) {
+        this.logger.debug(`Pinned peer ${this.pinnedPeer.toString()} has no txs to request`);
+        return;
+      }
+
+      txsToRequest.forEach(tx => this.txsMetadata.markRequested(tx));
+
+      const request = BlockTxsRequest.fromBlockProposalAndMissingTxs(this.blockProposal, txsToRequest);
+      if (!request) {
+        return;
+      }
+      await this.requestTxBatch(this.pinnedPeer, request);
+    }
+  }
+
+  private async smartRequester() {
     const nextPeerIndex = this.makeRoundRobinIndexer(() => getPeers().length);
     const getPeers = () => Array.from(this.smartPeers.difference(this.badPeers));
-
-    // if we don't have a pinned peer we wait to start smart requester
-    // otherwise we start immediately with the pinned peer
-    if (!this.pinnedPeer) {
-      await start;
-    }
 
     const nextPeer = () => {
       const idx = nextPeerIndex();
@@ -78,15 +94,20 @@ export class BatchTxRequester {
     };
 
     const makeRequest = (pid: PeerId) => {
-      const txsToRequest = this.txsMetadata.getTxsToRequestFromThePeer(pid).slice(0, TX_BATCH_SIZE);
-      txsToRequest.forEach(tx => this.txsMetadata.markRequested(tx));
+      const txs = this.txsMetadata.getTxsToRequestFromThePeer(pid).slice(0, TX_BATCH_SIZE);
 
-      return BlockTxsRequest.fromBlockProposalAndMissingTxs(this.blockProposal, txsToRequest);
+      txs.forEach(tx => {
+        this.txsMetadata.markRequested(tx);
+        this.txsMetadata.markInFlightBySmartPeer(tx);
+      });
+
+      return { blockRequest: BlockTxsRequest.fromBlockProposalAndMissingTxs(this.blockProposal, txs), txs };
     };
 
-    const workers = Array.from({ length: Math.min(PEERS_TO_QUERY_IN_PARALLEL, getPeers().length) }, () =>
+    const workers = Array.from({ length: SMART_PEERS_TO_QUERY_IN_PARALLEL }, () =>
       this.workerLoop(nextPeer, makeRequest, 'smart'),
     );
+
     await Promise.allSettled(workers);
   }
 
@@ -97,8 +118,10 @@ export class BatchTxRequester {
     const getPeers = () => Array.from(peers.difference(this.smartPeers.union(this.badPeers)));
 
     const txChunks = () =>
-      chunk<TxHash>(
-        this.missingTxs.filter(t => !this.txsMetadata.isFetched(t)),
+      chunk<string>(
+        this.txsMetadata
+          .getSortedByRequestedCountThenByInFlightCountAsc(Array.from(this.txsMetadata.getMissingTxHashes()))
+          .map(t => t.txHash.toString()),
         TX_BATCH_SIZE,
       );
 
@@ -108,9 +131,9 @@ export class BatchTxRequester {
         return undefined;
       }
 
-      const txs = txChunks()[idx];
+      const txs = txChunks()[idx].map(t => TxHash.fromString(t));
       txs.forEach(tx => this.txsMetadata.markRequested(tx));
-      return BlockTxsRequest.fromBlockProposalAndMissingTxs(this.blockProposal, txs);
+      return { blockRequest: BlockTxsRequest.fromBlockProposalAndMissingTxs(this.blockProposal, txs), txs };
     };
 
     const nextPeer = () => {
@@ -118,17 +141,23 @@ export class BatchTxRequester {
       return idx === undefined ? undefined : peerIdFromString(Array.from(getPeers())[idx]);
     };
 
-    const workers = Array.from({ length: Math.min(PEERS_TO_QUERY_IN_PARALLEL, getPeers().length) }, () =>
+    const workers = Array.from({ length: Math.min(DUMB_PEERS_TO_QUERY_IN_PARALLEL, getPeers().length) }, () =>
       this.workerLoop(nextPeer, makeRequest, 'dumb'),
     );
     await Promise.allSettled(workers);
   }
 
+  //TODO: cleanup the typeing here
+  // splitting this in workerLoopSmart and dumb probably makes sense
   private async workerLoop(
     pickNextPeer: () => PeerId | undefined,
-    request: (pid: PeerId) => BlockTxsRequest | undefined,
+    request: (pid: PeerId) => { blockRequest: BlockTxsRequest | undefined; txs: TxHash[] } | undefined,
     type: 'smart' | 'dumb',
   ) {
+    if (type === 'smart') {
+      await this.smartRequesterSemaphore.acquire();
+    }
+
     while (!this.shouldStop()) {
       const peerId = pickNextPeer();
       const weRanOutOfPeersToQuery = peerId === undefined;
@@ -137,7 +166,6 @@ export class BatchTxRequester {
         return;
       }
 
-      // TODO: think about this a bit more what should we do in this case?
       const nextBatchTxRequest = request(peerId);
       if (!nextBatchTxRequest) {
         this.logger.warn(`Worker loop: ${type}: Could not create next batch request`);
@@ -145,7 +173,18 @@ export class BatchTxRequester {
         continue;
       }
 
-      await this.requestTxBatch(peerId, nextBatchTxRequest);
+      const { blockRequest, txs } = nextBatchTxRequest;
+      if (blockRequest === undefined) {
+        return;
+      }
+
+      await this.requestTxBatch(peerId, blockRequest);
+
+      if (type === 'smart') {
+        txs.forEach(tx => {
+          this.txsMetadata.markNotInFlightBySmartPeer(tx);
+        });
+      }
     }
   }
 
@@ -156,8 +195,8 @@ export class BatchTxRequester {
         return;
       }
 
-      const block_response = BlockTxsResponse.fromBuffer(response.data);
-      this.handleSuccessResponseFromPeer(peerId, block_response);
+      const blockResponse = BlockTxsResponse.fromBuffer(response.data);
+      this.handleSuccessResponseFromPeer(peerId, blockResponse);
     } catch (err: any) {
       this.logger.error(`Failed to deserialize response from peer ${peerId.toString()}: ${err.message}`, {
         peerId,
@@ -174,16 +213,19 @@ export class BatchTxRequester {
       return;
     }
 
-    this.smartPeers.add(peerId.toString());
-
-    this.markTxsPeerHas(peerId, response);
     this.handleReceivedTxs(peerId, response.txs);
 
-    //TODO: maybe wait for at least couple of peers so that we don't spam single one?
-    if (this.startSmartRequester !== undefined) {
-      this.startSmartRequester();
-      // We use "undefined" here as marker that startSmartRequester has been called
-      this.startSmartRequester = undefined;
+    // We mark peer as "smart" only if they have some txs we are missing
+    // Otherwise we keep them as "dumb" in hope they'll receive some new txs we are missing in the future
+    if (!this.peerHasSomeTxsWeAreMissing(peerId, response)) {
+      return;
+    }
+
+    this.markTxsPeerHas(peerId, response);
+    this.smartPeers.add(peerId.toString());
+
+    if (this.smartPeers.size <= SMART_PEERS_TO_QUERY_IN_PARALLEL) {
+      this.smartRequesterSemaphore.release();
     }
   }
 
@@ -192,6 +234,11 @@ export class BatchTxRequester {
     const blockIdsMatch = this.blockProposal.archive === response.blockHash;
     const peerHasSomeTxsFromProposal = !response.txIndices.isEmpty();
     return blockIdsMatch && peerHasSomeTxsFromProposal;
+  }
+
+  private peerHasSomeTxsWeAreMissing(peerId: PeerId, response: BlockTxsResponse): boolean {
+    const txsPeerHas = new Set(this.extractHashesPeerHasFromResponse(response).map(h => h.toString()));
+    return this.txsMetadata.getMissingTxHashes().intersection(txsPeerHas).size > 0;
   }
 
   private handleReceivedTxs(peerId: PeerId, txs: TxArray) {
@@ -252,13 +299,26 @@ class MissingTxMetadata {
   constructor(
     public readonly txHash: TxHash,
     public fetched = false,
-    public requestedTimes = 0,
+    public requestedCount = 0,
+    public inFlightCount = 0,
     public tx: Tx | undefined = undefined,
     public readonly peers = new Set<string>(),
   ) {}
 
   public markAsRequested() {
-    this.requestedTimes++;
+    this.requestedCount++;
+  }
+
+  public markInFlight() {
+    this.inFlightCount++;
+  }
+
+  public markNotInFlight() {
+    this.inFlightCount = Math.max(--this.inFlightCount, 0);
+  }
+
+  public isInFlight(): boolean {
+    return this.inFlightCount > 0;
   }
 
   public markAsFetched(peerId: PeerId, tx: Tx) {
@@ -278,8 +338,27 @@ class MissingTxMetadata {
  * This could be better optimized but given expected count of missing txs (N < 100)
  * At the moment there is no need for it. And benefit is that we have everything in single store*/
 class MissingTxMetadataCollection extends Map<string, MissingTxMetadata> {
-  public getSortedByRequestedTimesAsc(): MissingTxMetadata[] {
-    return Array.from(this.values()).sort((a, b) => a.requestedTimes - b.requestedTimes);
+  public getSortedByRequestedCountAsc(txs: string[]): MissingTxMetadata[] {
+    return Array.from(this.values().filter(txMeta => txs.includes(txMeta.txHash.toString()))).sort(
+      (a, b) => a.requestedCount - b.requestedCount,
+    );
+  }
+
+  public getSortedByRequestedCountThenByInFlightCountAsc(txs: string[]): MissingTxMetadata[] {
+    const filtered = Array.from(this.values()).filter(txMeta => txs.includes(txMeta.txHash.toString()));
+
+    const [notInFlight, inFlight] = filtered.reduce<[MissingTxMetadata[], MissingTxMetadata[]]>(
+      (buckets, tx) => {
+        tx.isInFlight() ? buckets[1].push(tx) : buckets[0].push(tx);
+        return buckets;
+      },
+      [[], []],
+    );
+
+    notInFlight.sort((a, b) => a.requestedCount - b.requestedCount);
+    inFlight.sort((a, b) => a.inFlightCount - b.inFlightCount);
+
+    return [...notInFlight, ...inFlight];
   }
 
   public isFetched(txHash: TxHash): boolean {
@@ -288,11 +367,27 @@ class MissingTxMetadataCollection extends Map<string, MissingTxMetadata> {
     return this.get(txHash.toString())?.fetched ?? true;
   }
 
-  public getFetchedTxHashes(): Set<TxHash> {
+  public getFetchedTxHashes(): Set<string> {
     return new Set(
       this.values()
         .filter(t => t.fetched)
-        .map(t => t.txHash),
+        .map(t => t.txHash.toString()),
+    );
+  }
+
+  public getMissingTxHashes(): Set<string> {
+    return new Set(
+      this.values()
+        .filter(t => !t.fetched)
+        .map(t => t.txHash.toString()),
+    );
+  }
+
+  public getInFlightTxHashes(): Set<string> {
+    return new Set(
+      this.values()
+        .filter(t => t.isInFlight())
+        .map(t => t.txHash.toString()),
     );
   }
 
@@ -304,29 +399,59 @@ class MissingTxMetadataCollection extends Map<string, MissingTxMetadata> {
     );
   }
 
-  public getTxsPeerHas(peer: PeerId): Set<TxHash> {
+  public getTxsPeerHas(peer: PeerId): Set<string> {
     const peerIdStr = peer.toString();
-    const txsPeerHas = new Set<TxHash>();
+    const txsPeerHas = new Set<string>();
 
     this.values().forEach(txMeta => {
       if (txMeta.peers.has(peerIdStr)) {
-        txsPeerHas.add(txMeta.txHash);
+        txsPeerHas.add(txMeta.txHash.toString());
       }
     });
 
     return txsPeerHas;
   }
 
-  //TODO: sort by least requested
   public getTxsToRequestFromThePeer(peer: PeerId): TxHash[] {
     const txsPeerHas = this.getTxsPeerHas(peer);
     const fetchedTxs = this.getFetchedTxHashes();
 
-    return Array.from(txsPeerHas.difference(fetchedTxs));
+    const txsToRequest = txsPeerHas.difference(fetchedTxs);
+    if (txsToRequest.size >= TX_BATCH_SIZE) {
+      return this.getSortedByRequestedCountThenByInFlightCountAsc(Array.from(txsToRequest)).map(t => t.txHash);
+    }
+
+    // Otherwise fill the txs to request till TX_BATCH_SIZE with random txs we are missing
+    // Who knows, maybe we get lucky and peer received these txs in the meantime
+
+    const countToFill = TX_BATCH_SIZE - txsToRequest.size;
+    const txsToFill = this.getSortedByRequestedCountThenByInFlightCountAsc(
+      Array.from(this.getMissingTxHashes().difference(txsToRequest)),
+    )
+      .slice(0, countToFill)
+      .map(t => t.txHash);
+
+    return [...Array.from(txsToRequest).map(t => TxHash.fromString(t)), ...txsToFill];
   }
 
   public markRequested(txHash: TxHash) {
     this.get(txHash.toString())?.markAsRequested();
+  }
+
+  /*
+   * This should be called only when requesting tx from smart peer
+   * Because the smart peer should return this tx, whereas
+   * "dumb" peer might return it, or might not - we don't know*/
+  public markInFlightBySmartPeer(txHash: TxHash) {
+    this.get(txHash.toString())?.markInFlight();
+  }
+
+  /*
+   * This should be called only when requesting tx from smart peer
+   * Because the smart peer should return this tx, whereas
+   * "dumb" peer might return it, or might not - we don't know*/
+  public markNotInFlightBySmartPeer(txHash: TxHash) {
+    this.get(txHash.toString())?.markNotInFlight();
   }
 
   public markFetched(peerId: PeerId, tx: Tx) {
@@ -342,8 +467,6 @@ class MissingTxMetadataCollection extends Map<string, MissingTxMetadata> {
     }
 
     txMeta.markAsFetched(peerId, tx);
-
-    txMeta.fetched = true;
   }
 
   public markPeerHas(peerId: PeerId, txHash: TxHash[]) {
