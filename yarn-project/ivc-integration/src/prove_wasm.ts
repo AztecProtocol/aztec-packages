ce963c846fad41be2111ecf5caa2d702eff7d89e
maramihali <mara@aztecprotocol.com>
2025-08-01 10:32:04 +0000

    remove unused function

--- yarn-project/ivc-integration/src/prove_wasm.ts
+++ yarn-project/ivc-integration/src/prove_wasm.ts
@@ -10,8 +10,6 @@ function base64ToUint8Array(base64: string): Uint8Array {
   return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
 }
 
-const hexToUint8Array = (hex: string): Uint8Array => new Uint8Array(Buffer.from(hex, 'hex'));
-
 export async function proveClientIVC(
   bytecodes: string[],
   witnessStack: Uint8Array[],
