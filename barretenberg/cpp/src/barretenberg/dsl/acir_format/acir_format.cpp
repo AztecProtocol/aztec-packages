c31438087c61b7ebe26716313393bacfb4e542a3
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2025-08-07 10:10:19 +0000

    Fix

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/acir_format.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/acir_format.cpp
@@ -434,7 +434,7 @@ process_honk_recursion_constraints(Builder& builder,
             auto honk_recursion_constraint = create_honk_recursion_constraints<UltraRecursiveFlavor_<Builder>>(
                 builder, constraint, has_valid_witness_assignments);
             if (output.points_accumulator.has_data) {
-                output.points_accumulator.aggregate(honk_recursion_constraintpairing_points);
+                output.points_accumulator.aggregate(honk_recursion_constraint.pairing_points);
             } else {
                 output.points_accumulator = honk_recursion_constraint.pairing_points;
             }
