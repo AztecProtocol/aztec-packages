1c6ec47d8ca7e33e97fb64da0a16093b42ce7167
Suyash Bagad <suyash@aztecprotocol.com>
2025-08-12 17:49:57 +0530

    chore: remove `HasPlookup` from remaining places (#16322)
    Removed last few traces of `HasPlookup` as we now only support `Ultra`
    and `Mega` circuit builders in stdlib and beyond.

--- barretenberg/cpp/src/barretenberg/stdlib/primitives/memory/ram_table.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/primitives/memory/ram_table.cpp
@@ -22,7 +22,6 @@ namespace bb::stdlib {
  */
 template <typename Builder> ram_table<Builder>::ram_table(Builder* builder, const size_t table_size)
 {
-    static_assert(HasPlookup<Builder>);
     _context = builder;
     _length = table_size;
     _index_initialized.resize(table_size);
@@ -44,7 +43,6 @@ template <typename Builder> ram_table<Builder>::ram_table(Builder* builder, cons
  */
 template <typename Builder> ram_table<Builder>::ram_table(const std::vector<field_pt>& table_entries)
 {
-    static_assert(HasPlookup<Builder>);
     // get the builder _context
     for (const auto& entry : table_entries) {
         if (entry.get_context() != nullptr) {
