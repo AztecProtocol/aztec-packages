6f4933314bbfe0bbbfa5b3c326a9b08398890915
ludamad <domuradical@gmail.com>
2025-08-14 01:50:21 +0000

    let's go fighting

--- barretenberg/acir_tests/scripts/browser_prove.sh
+++ barretenberg/acir_tests/scripts/browser_prove.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 source $(git rev-parse --show-toplevel)/ci3/source
-
 cd ../acir_tests/$1
-export HARDWARE_CONCURRENCY=8
-
 export BROWSER=$2
 
 # Launch browser server
@@ -12,28 +9,4 @@ dump_fail \
 while ! nc -z localhost 8080 &>/dev/null; do sleep 1; done;
 
 # Use the browser binary for the test
-browser_bin="../../headless-test/bb.js.browser"
-
-# Build base flags for ultra_honk
-flags="${VERBOSE:+-v} --scheme ultra_honk --oracle_hash ${HASH:-poseidon2}"
-
-# Add any additional arguments (skip first two - test name and browser type)
-shift 2
-for arg in "$@"; do
-    flags+=" $arg"
-done
-
-mkdir -p output-$$
-trap "rm -rf output-$$" EXIT
-
-# Generate VK
-$browser_bin write_vk $flags -b target/program.json -o output-$$
-
-# Prove
-$browser_bin prove $flags -b target/program.json -k output-$$/vk -o output-$$
-
-# Verify
-$browser_bin verify $flags \
-    -k output-$$/vk \
-    -p output-$$/proof \
-    -i output-$$/public_inputs
+../../headless-test/bb.js.browser prove_and_verify -b target/program.json ${VERBOSE:+-v}}
