264af54a094ec52d11a2d8a037cdf555d5423585
iakovenkos <sergey.s.yakovenko@gmail.com>
2025-08-17 19:40:11 +0000

    bump const_pg_log_n to match avm mega circuit size

--- barretenberg/cpp/src/barretenberg/constants.hpp
+++ barretenberg/cpp/src/barretenberg/constants.hpp
@@ -15,7 +15,7 @@ static constexpr uint32_t CONST_PROOF_SIZE_LOG_N = 28;
 // The log of the max circuit size of circuits being folded. This size is assumed by the PG prover and verifier in order
 // to ensure a constant PG proof size and a PG recursive verifier circuit that is independent of the size of the
 // circuits being folded.
-static constexpr uint32_t CONST_PG_LOG_N = 20;
+static constexpr uint32_t CONST_PG_LOG_N = 21;
 
 static constexpr uint32_t CONST_ECCVM_LOG_N = 16;
 
