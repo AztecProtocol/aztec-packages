5e18e2de0c1e1db4609800cb3e931189b68fb34d
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2025-08-21 14:03:29 +0200

    chore: Removed unused functions from ecdsa stdlib (#16494)
    Audit part 1: remove unused functions from ecdsa stdlib

--- barretenberg/cpp/src/barretenberg/stdlib/encryption/ecdsa/ecdsa.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib/encryption/ecdsa/ecdsa.hpp
@@ -23,30 +23,9 @@ bool_t<Builder> ecdsa_verify_signature(const stdlib::byte_array<Builder>& messag
                                        const ecdsa_signature<Builder>& sig);
 
 template <typename Builder, typename Curve, typename Fq, typename Fr, typename G1>
-bool_t<Builder> ecdsa_verify_signature_noassert(const stdlib::byte_array<Builder>& message,
-                                                const G1& public_key,
-                                                const ecdsa_signature<Builder>& sig);
-
-template <typename Builder, typename Curve, typename Fq, typename Fr, typename G1>
 bool_t<Builder> ecdsa_verify_signature_prehashed_message_noassert(const stdlib::byte_array<Builder>& hashed_message,
                                                                   const G1& public_key,
                                                                   const ecdsa_signature<Builder>& sig);
-
-template <typename Builder>
-static ecdsa_signature<Builder> ecdsa_from_witness(Builder* ctx, const crypto::ecdsa_signature& input)
-{
-    std::vector<uint8_t> r_vec(std::begin(input.r), std::end(input.r));
-    std::vector<uint8_t> s_vec(std::begin(input.s), std::end(input.s));
-    std::vector<uint8_t> v_vec = { input.v }; // Create single-element vector for v
-    stdlib::byte_array<Builder> r(ctx, r_vec);
-    stdlib::byte_array<Builder> s(ctx, s_vec);
-    stdlib::byte_array<Builder> v(ctx, v_vec); // v is now a byte_array with size 1
-    ecdsa_signature<Builder> out;
-    out.r = r;
-    out.s = s;
-    out.v = v;
-    return out;
-}
 
 template <typename Builder> void generate_ecdsa_verification_test_circuit(Builder& builder, size_t num_iterations);
 
