af380fe45f1420164f4c8ae5c03fe80ed6132701
PhilWindle <philip.windle@gmail.com>
2025-08-22 15:01:20 +0000

    Test fixes

--- yarn-project/end-to-end/src/e2e_multi_validator/e2e_multi_validator_node_key_store.test.ts
+++ yarn-project/end-to-end/src/e2e_multi_validator/e2e_multi_validator_node_key_store.test.ts
@@ -1,28 +1,39 @@
 import type { AztecNodeConfig } from '@aztec/aztec-node';
-import { AztecAddress, ContractDeployer, EthAddress, Fr, type Wallet, retryUntil } from '@aztec/aztec.js';
+import {
+  AztecAddress,
+  type AztecNode,
+  ContractDeployer,
+  EthAddress,
+  Fr,
+  type Wallet,
+  retryUntil,
+  waitForProven,
+} from '@aztec/aztec.js';
 import {
   type DeployL1ContractsReturnType,
   RollupContract,
-  createExtendedL1Client,
   getAddressFromPrivateKey,
   getL1ContractsConfigEnvVars,
 } from '@aztec/ethereum';
 import { SecretValue } from '@aztec/foundation/config';
 import { type EthPrivateKey, KeystoreManager, loadKeystores, mergeKeystores } from '@aztec/node-keystore';
 import { StatefulTestContractArtifact } from '@aztec/noir-test-contracts.js/StatefulTest';
-import { NodeKeystoreAdapter } from '@aztec/validator-client';
+import type { Sequencer, SequencerClient, SequencerPublisherFactory } from '@aztec/sequencer-client';
+import type { TestSequencer, TestSequencerClient } from '@aztec/sequencer-client/test';
+import type { BlockProposalOptions } from '@aztec/stdlib/p2p';
+import type { ProposedBlockHeader, StateReference, Tx } from '@aztec/stdlib/tx';
+import { NodeKeystoreAdapter, ValidatorClient } from '@aztec/validator-client';
 
+import { jest } from '@jest/globals';
 import { mkdtemp, rmdir } from 'fs/promises';
 import { createServer } from 'http';
 import { tmpdir } from 'os';
 import { join } from 'path';
 import { privateKeyToAccount } from 'viem/accounts';
-import { foundry } from 'viem/chains';
 
 import { MNEMONIC } from '../fixtures/fixtures.js';
 import { getPrivateKeyFromIndex, setup } from '../fixtures/utils.js';
 import {
-  type KeyAndWallet,
   addressForPrivateKey,
   createJSONRPCSigner,
   createKeyFile1,
@@ -30,17 +41,28 @@ import {
   createKeyFile3,
   createKeyFile4,
   createKeyFile5,
+  createKeyFile6,
 } from './utils.js';
 
-// Import KZG from viem
-
-const VALIDATOR_COUNT = 5;
+const VALIDATOR_COUNT = 7;
 const COMMITTEE_SIZE = VALIDATOR_COUNT;
 const PUBLISHER_COUNT = 7;
 const VALIDATOR_KEY_START_INDEX = 0;
 const PUBLISHER_KEY_START_INDEX = VALIDATOR_COUNT + VALIDATOR_KEY_START_INDEX;
-const SIGNER_URL = 'http://localhost:15000';
+const SIGNER_URL_PORT = 15000;
+const SIGNER_URL = `http://localhost:${SIGNER_URL_PORT}`;
 const PROVER_PUBLISHER_INDEX = PUBLISHER_KEY_START_INDEX + PUBLISHER_COUNT;
+const BLOCK_COUNT = 20;
+
+const publishers = Array.from({ length: PUBLISHER_COUNT }, (_, i) => ({
+  index: i + PUBLISHER_KEY_START_INDEX,
+  key: `0x${getPrivateKeyFromIndex(i + PUBLISHER_KEY_START_INDEX)!.toString('hex')}` as EthPrivateKey,
+}));
+
+const validators = Array.from(
+  { length: VALIDATOR_COUNT },
+  (_, i) => `0x${getPrivateKeyFromIndex(i + VALIDATOR_KEY_START_INDEX)!.toString('hex')}` as EthPrivateKey,
+);
 
 async function createKeyFiles() {
   const directory = await mkdtemp(join(tmpdir(), 'foo-'));
@@ -49,21 +71,16 @@ async function createKeyFiles() {
   const file3 = join(directory, 'keyfile3.json');
   const file4 = join(directory, 'keyfile4.json');
   const file5 = join(directory, 'keyfile5.json');
-
-  const publishers = Array.from({ length: PUBLISHER_COUNT }, (_, i) => ({
-    index: i + PUBLISHER_KEY_START_INDEX,
-    key: `0x${getPrivateKeyFromIndex(i + PUBLISHER_KEY_START_INDEX)!.toString('hex')}` as EthPrivateKey,
-  }));
-
-  const validators = Array.from(
-    { length: VALIDATOR_COUNT },
-    (_, i) => `0x${getPrivateKeyFromIndex(i + VALIDATOR_KEY_START_INDEX)!.toString('hex')}` as EthPrivateKey,
-  );
+  const file6 = join(directory, 'keyfile6.json');
 
   const proverPrivateKey = `0x${getPrivateKeyFromIndex(PROVER_PUBLISHER_INDEX)!.toString('hex')}` as EthPrivateKey;
 
   const coinbaseAddresses = Array.from({ length: VALIDATOR_COUNT }, (_, i) => {
     return EthAddress.fromNumber(i + 1);
+  });
+
+  const feeRecipientAddresses = Array.from({ length: VALIDATOR_COUNT }, (_, i) => {
+    return AztecAddress.fromNumber(i + 1);
   });
 
   await createKeyFile1(
@@ -73,8 +90,16 @@ async function createKeyFiles() {
     publishers[0].key,
     publishers[1].key,
     coinbaseAddresses[0],
+    feeRecipientAddresses[0],
   );
-  await createKeyFile2(file2, validators[1], publishers[0].key, publishers[1].key, coinbaseAddresses[1]);
+  await createKeyFile2(
+    file2,
+    validators[1],
+    MNEMONIC,
+    PUBLISHER_KEY_START_INDEX,
+    coinbaseAddresses[1],
+    feeRecipientAddresses[1],
+  );
   await createKeyFile3(
     file3,
     addressForPrivateKey(validators[2]),
@@ -82,6 +107,7 @@ async function createKeyFiles() {
     publishers[3].key,
     coinbaseAddresses[1],
     SIGNER_URL,
+    feeRecipientAddresses[2],
   );
   await createKeyFile4(
     file4,
@@ -91,11 +117,14 @@ async function createKeyFiles() {
     publishers[5].key,
     MNEMONIC,
     publishers[6].key,
-    coinbaseAddresses[2],
     coinbaseAddresses[3],
+    coinbaseAddresses[4],
     SIGNER_URL,
+    feeRecipientAddresses[3],
+    feeRecipientAddresses[4],
   );
   await createKeyFile5(file5, addressForPrivateKey(proverPrivateKey), SIGNER_URL);
+  await createKeyFile6(file6, MNEMONIC, 5, coinbaseAddresses[5], feeRecipientAddresses[5]);
   return directory;
 }
 
@@ -103,7 +132,9 @@ function verifyKeyStore(directory: string) {
   const keyStores = loadKeystores(directory);
   const keyStore = mergeKeystores(keyStores);
   expect(keyStore.validators).toBeDefined();
-  expect(keyStore.validators!.length).toBeGreaterThanOrEqual(VALIDATOR_COUNT);
+
+  // This is the count of validator blocks. There is one validator block consisting of multiple keys so we expect 1 less
+  expect(keyStore.validators!.length).toBeGreaterThanOrEqual(VALIDATOR_COUNT - 1);
 
   const keyStoreManager = new KeystoreManager(keyStore);
   const validatorAdapter = NodeKeystoreAdapter.fromKeyStoreManager(keyStoreManager);
@@ -121,9 +152,19 @@ describe('e2e_multi_validator_node', () => {
   let deployL1ContractsValues: DeployL1ContractsReturnType;
   let rollup: RollupContract;
   let keyStoreDirectory: string;
+  let aztecNode: AztecNode;
+  let sequencer: Sequencer | undefined;
+  let sequencerClient: SequencerClient | undefined;
+  let publisherFactory: SequencerPublisherFactory;
+  let validatorClient: ValidatorClient;
   let jsonRpcServer: ReturnType<typeof createServer> | null = null;
   const artifact = StatefulTestContractArtifact;
-  const addressToPrivateKey = new Map<string, KeyAndWallet>();
+  const addressToPrivateKey = new Map<string, EthPrivateKey>();
+  const remoteSignerStats = new Map<string, number>();
+  const expectedRemoteSigners = new Set<string>();
+  const expectedCoinbaseAddresses = new Map<string, string>();
+  const expectedFeeRecipientAddresses = new Map<string, string>();
+  const expectedPublishers = new Map<string, string[]>();
 
   beforeEach(async () => {
     initialValidatorPrivateKeys = Array.from(
@@ -144,21 +185,94 @@ describe('e2e_multi_validator_node', () => {
       };
     });
 
+    // These validators have remote signing configured
+    expectedRemoteSigners.add(validatorAddresses[2].toLowerCase());
+    expectedRemoteSigners.add(validatorAddresses[3].toLowerCase());
+    expectedRemoteSigners.add(validatorAddresses[4].toLowerCase());
+
+    // Setup expected coinbase and fee recipient values per validator
+    validatorAddresses.forEach((validatorAddress, i) => {
+      const coinbase = EthAddress.fromNumber(i + 1)
+        .toString()
+        .toLowerCase();
+      expectedCoinbaseAddresses.set(validatorAddress.toLowerCase(), coinbase);
+      const feeRecipient = AztecAddress.fromNumber(i + 1)
+        .toString()
+        .toLowerCase();
+      expectedFeeRecipientAddresses.set(validatorAddress.toLowerCase(), feeRecipient);
+    });
+
+    // We need to modify the expected coinbase addresses. Some validators use the same
+    expectedCoinbaseAddresses.set(
+      validatorAddresses[2].toLowerCase(),
+      expectedCoinbaseAddresses.get(validatorAddresses[1].toLowerCase())!,
+    );
+    expectedCoinbaseAddresses.set(
+      validatorAddresses[6].toLowerCase(),
+      expectedCoinbaseAddresses.get(validatorAddresses[5].toLowerCase())!,
+    );
+
+    // We need to do similar to fee recipients
+    expectedFeeRecipientAddresses.set(
+      validatorAddresses[6].toLowerCase(),
+      expectedFeeRecipientAddresses.get(validatorAddresses[5].toLowerCase())!,
+    );
+
+    // Now collect the sets of expected publishers
+    const publisherAddresses = publishers.map(x => getAddressFromPrivateKey(x.key));
+
+    expectedPublishers.set(validatorAddresses[0].toLowerCase(), [
+      publisherAddresses[0].toLowerCase(),
+      publisherAddresses[1].toLowerCase(),
+    ]);
+
+    // Validator 2 uses the same publishers as 1
+    expectedPublishers.set(validatorAddresses[1].toLowerCase(), [
+      publisherAddresses[0].toLowerCase(),
+      publisherAddresses[1].toLowerCase(),
+    ]);
+
+    expectedPublishers.set(validatorAddresses[2].toLowerCase(), [
+      publisherAddresses[2].toLowerCase(),
+      publisherAddresses[3].toLowerCase(),
+    ]);
+
+    expectedPublishers.set(validatorAddresses[3].toLowerCase(), [
+      publisherAddresses[4].toLowerCase(),
+      publisherAddresses[5].toLowerCase(),
+    ]);
+
+    expectedPublishers.set(validatorAddresses[4].toLowerCase(), [
+      publisherAddresses[5].toLowerCase(),
+      publisherAddresses[6].toLowerCase(),
+    ]);
+
+    // Validators 6 and 7 use their own and each other's addresses
+    expectedPublishers.set(validatorAddresses[5].toLowerCase(), [
+      validatorAddresses[5].toLowerCase(),
+      validatorAddresses[6].toLowerCase(),
+    ]);
+
+    expectedPublishers.set(validatorAddresses[6].toLowerCase(), [
+      validatorAddresses[5].toLowerCase(),
+      validatorAddresses[6].toLowerCase(),
+    ]);
+
     keyStoreDirectory = await createKeyFiles();
     verifyKeyStore(keyStoreDirectory);
 
+    // Create keys and wallets for signing using our remote signer
     for (let i = 0; i < VALIDATOR_COUNT + PUBLISHER_COUNT + 1; i++) {
-      const pk = `0x${getPrivateKeyFromIndex(i + VALIDATOR_KEY_START_INDEX)!.toString('hex')}` as `0x${string}`;
+      const pk = `0x${getPrivateKeyFromIndex(i + VALIDATOR_KEY_START_INDEX)!.toString('hex')}` as EthPrivateKey;
       const account = getAddressFromPrivateKey(pk);
-      const wallet = createExtendedL1Client(['http://127.0.0.1:8545'], pk, foundry);
-      addressToPrivateKey.set(account.toLowerCase(), { key: pk as EthPrivateKey, wallet });
+      addressToPrivateKey.set(account.toLowerCase(), pk);
     }
 
     // Create JSON RPC server for signing transactions
-    jsonRpcServer = createJSONRPCSigner(addressToPrivateKey);
-    // Start server on port 15000 (SIGNER_URL port)
+    jsonRpcServer = createJSONRPCSigner(addressToPrivateKey, remoteSignerStats);
+    // Start server on the SIGNER_URL port
     await new Promise<void>(resolve => {
-      jsonRpcServer!.listen(15000, resolve);
+      jsonRpcServer!.listen(SIGNER_URL_PORT, resolve);
     });
 
     const { aztecSlotDuration: _aztecSlotDuration } = getL1ContractsConfigEnvVars();
@@ -169,18 +283,24 @@ describe('e2e_multi_validator_node', () => {
       accounts: [ownerAddress],
       config,
       deployL1ContractsValues,
+      aztecNode,
+      sequencer: sequencerClient,
     } = await setup(1, {
       initialValidators,
       aztecTargetCommitteeSize: COMMITTEE_SIZE,
       keyStoreDirectory,
       minTxsPerBlock: 1,
+      maxTxsPerBlock: 1,
       archiverPollingIntervalMS: 200,
       transactionPollingIntervalMS: 200,
       worldStateBlockCheckIntervalMS: 200,
       blockCheckIntervalMS: 200,
       startProverNode: true,
-      maxTxsPerBlock: 1,
     }));
+
+    sequencer = (sequencerClient! as TestSequencerClient).getSequencer();
+    publisherFactory = (sequencer as TestSequencer).publisherFactory;
+    validatorClient = (sequencer as TestSequencer).validatorClient;
 
     rollup = new RollupContract(
       deployL1ContractsValues.l1Client,
@@ -208,7 +328,6 @@ describe('e2e_multi_validator_node', () => {
       await new Promise<void>(resolve => {
         jsonRpcServer!.close(() => resolve());
       });
-      jsonRpcServer = null;
     }
   });
 
@@ -224,13 +343,102 @@ describe('e2e_multi_validator_node', () => {
   };
 
   it('should build blocks & attest with multiple validator keys', async () => {
-    const sentTransactionPromises = Array.from({ length: 10 }, (_, i) => {
+    const requestedCoinbaseAddresses = new Map<string, string>();
+    const requestedFeeRecipientAddresses = new Map<string, string>();
+
+    type PublisherRequest = {
+      proposer: EthAddress;
+      publisher: EthAddress;
+    };
+    const publisherRequests: PublisherRequest[] = [];
+
+    // We substitute 2 critical functions within the sequencer.
+    // We don't modify the behavior, merely capture the arguments for assertion later.
+    // The first is the request for a publisher. This tells us which publisher has been selected for the proposer.
+    // The second is the request to create a block proposal, this tells us the selected values for coinbase and fee recipient.
+
+    const originalCreate = publisherFactory.create.bind(publisherFactory);
+    const createPublisher = async (proposer?: EthAddress) => {
+      const received = await originalCreate(proposer);
+      const request: PublisherRequest = {
+        proposer: received.attestorAddress,
+        publisher: received.publisher.getSenderAddress(),
+      };
+      publisherRequests.push(request);
+      return received;
+    };
+    publisherFactory.create = jest.fn(createPublisher);
+
+    const originalCreateProposal = validatorClient.createBlockProposal.bind(validatorClient);
+    const createBlockProposal = (
+      blockNumber: number,
+      header: ProposedBlockHeader,
+      archive: Fr,
+      stateReference: StateReference,
+      txs: Tx[],
+      proposerAddress: EthAddress | undefined,
+      options: BlockProposalOptions,
+    ) => {
+      if (proposerAddress) {
+        requestedCoinbaseAddresses.set(
+          proposerAddress.toString().toLowerCase(),
+          header.coinbase.toString().toLowerCase(),
+        );
+        requestedFeeRecipientAddresses.set(
+          proposerAddress.toString().toLowerCase(),
+          header.feeRecipient.toString().toLowerCase(),
+        );
+      }
+
+      return originalCreateProposal(blockNumber, header, archive, stateReference, txs, proposerAddress, options);
+    };
+    validatorClient.createBlockProposal = jest.fn(createBlockProposal);
+
+    // Now we just submit some transactions to create some blocks. 1 tx per block.
+    // Then we check the results captured above
+    const sentTransactionPromises = Array.from({ length: BLOCK_COUNT }, (_, i) => {
       const contractAddressSalt = new Fr(i + 1);
       return sendTx(ownerAddress, contractAddressSalt);
     });
 
-    const sentTransactions = await Promise.all(sentTransactionPromises);
+    const settledTransactions = await Promise.all(sentTransactionPromises.map(async tx => (await tx).wait()));
 
-    await Promise.all(sentTransactions.map(tx => tx.wait()));
+    await Promise.all(
+      settledTransactions.map(tx => {
+        return waitForProven(aztecNode, tx, {
+          provenTimeout: (config.aztecProofSubmissionEpochs + 1) * config.aztecEpochDuration * config.aztecSlotDuration,
+        });
+      }),
+    );
+
+    const currentBlockNumber = await aztecNode.getBlockNumber();
+
+    for (const expectedRemoteSigner of expectedRemoteSigners) {
+      const remoteSigner = remoteSignerStats.get(expectedRemoteSigner);
+      expect(remoteSigner).toBeDefined();
+      expect(remoteSigner).toBeGreaterThanOrEqual(currentBlockNumber);
+    }
+
+    for (const [proposer, coinbase] of requestedCoinbaseAddresses) {
+      const expectedCoinbase = expectedCoinbaseAddresses.get(proposer);
+      expect(expectedCoinbase).toBeDefined();
+      expect(coinbase).toEqual(expectedCoinbase);
+    }
+
+    for (const [proposer, feeRecipient] of requestedFeeRecipientAddresses) {
+      const expectedFeeRecipient = expectedFeeRecipientAddresses.get(proposer);
+      expect(expectedFeeRecipient).toBeDefined();
+      expect(feeRecipient).toEqual(expectedFeeRecipient);
+    }
+
+    // Now verify all of the publisher requests
+    for (const publisherRequest of publisherRequests) {
+      const proposer = publisherRequest.proposer.toString().toLowerCase();
+      const receivedPublisher = publisherRequest.publisher.toString().toLowerCase();
+      const expectedPublisher = expectedPublishers.get(proposer);
+      expect(expectedPublisher).toBeDefined();
+      const found = expectedPublisher!.includes(receivedPublisher);
+      expect(found).toBeTrue();
+    }
   });
 });
--- yarn-project/sequencer-client/src/test/index.ts
+++ yarn-project/sequencer-client/src/test/index.ts
@@ -1,8 +1,10 @@
 import type { PublisherManager } from '@aztec/ethereum';
 import type { L1TxUtilsWithBlobs } from '@aztec/ethereum/l1-tx-utils-with-blobs';
 import type { PublicProcessorFactory } from '@aztec/simulator/server';
+import type { ValidatorClient } from '@aztec/validator-client';
 
 import { SequencerClient } from '../client/sequencer-client.js';
+import type { SequencerPublisherFactory } from '../publisher/sequencer-publisher-factory.js';
 import type { SequencerPublisher } from '../publisher/sequencer-publisher.js';
 import { Sequencer } from '../sequencer/sequencer.js';
 import type { SequencerTimetable } from '../sequencer/timetable.js';
@@ -11,6 +13,8 @@ class TestSequencer_ extends Sequencer {
   declare public publicProcessorFactory: PublicProcessorFactory;
   declare public timetable: SequencerTimetable;
   declare public publisher: SequencerPublisher;
+  declare public publisherFactory: SequencerPublisherFactory;
+  declare public validatorClient: ValidatorClient;
 }
 
 export type TestSequencer = TestSequencer_;
