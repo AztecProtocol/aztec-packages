8bdcb6018b4972c7c520757fb547984322f15ed5
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2025-08-22 12:06:26 +0000

    Reintroduce self reduce; move input validation at the beginning of the function

--- barretenberg/cpp/src/barretenberg/stdlib/encryption/ecdsa/ecdsa.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib/encryption/ecdsa/ecdsa.hpp
@@ -41,8 +41,7 @@ bool_t<Builder> ecdsa_verify_signature(const stdlib::byte_array<Builder>& hashed
 template <typename Builder, typename Curve, typename Fq, typename Fr, typename G1>
 void validate_inputs(const stdlib::byte_array<Builder>& hashed_message,
                      const G1& public_key,
-                     const ecdsa_signature<Builder>& sig,
-                     const G1& scalar_mul_result);
+                     const ecdsa_signature<Builder>& sig);
 
 template <typename Builder> void generate_ecdsa_verification_test_circuit(Builder& builder, size_t num_iterations);
 
