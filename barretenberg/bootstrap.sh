85143181820d7997b45cca671c084dc5ddba0de9
ludamad <domuradical@gmail.com>
2025-08-26 16:38:14 +0000

    remove hack

--- barretenberg/bootstrap.sh
+++ barretenberg/bootstrap.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 source $(git rev-parse --show-toplevel)/ci3/source
 
-
 function bootstrap_all {
   # To run bb we need a crs.
   # Download ignition up front to ensure no race conditions at runtime.
@@ -32,20 +31,6 @@ case "$cmd" in
   "release-preview")
     ./docs/bootstrap.sh release-preview
     ;;
-  bootstrap_e2e_hack)
-    echo "WARNING: This assumes your PR only changes barretenberg and the rest of the repository is unchanged from master."
-    echo "WARNING: This is only sound if you have not changed VK generation! (or noir-projects VKs will be incorrect)."
-    echo "WARNING: It builds up until yarn-project and allows end-to-end tests (not boxes/playground/release image etc)."
-    merge_base=$(git merge-base HEAD origin/master)
-    for project in noir barretenberg avm-transpiler noir-projects l1-contracts yarn-project ; do
-      if [ $project == barretenberg ]; then
-        ../$project/bootstrap.sh # i.e. this script
-      else
-        AZTEC_CACHE_COMMIT=$merge_base ../$project/bootstrap.sh
-      fi
-    done
-    ;;
-
   *)
     echo "Unknown command: $cmd"
     exit 1
