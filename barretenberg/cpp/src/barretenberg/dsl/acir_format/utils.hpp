dff5f6e6a6e00dfc3d67da6ec33a6bea47403dc5
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2025-08-27 15:38:00 +0000

    Define utilities

---
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/utils.hpp
@@ -0,0 +1,43 @@
+// === AUDIT STATUS ===
+// internal:    { status: not started, auditors: [], date: YYYY-MM-DD }
+// external_1:  { status: not started, auditors: [], date: YYYY-MM-DD }
+// external_2:  { status: not started, auditors: [], date: YYYY-MM-DD }
+// =====================
+
+#pragma once
+
+#include "barretenberg/dsl/acir_format/acir_format.hpp"
+#include "barretenberg/stdlib/primitives/field/field.hpp"
+#include <vector>
+
+namespace acir_format {
+
+using namespace bb;
+
+template <typename Builder>
+static std::vector<stdlib::field_t<Builder>> fields_from_witnesses(Builder& builder,
+                                                                   std::span<const uint32_t> witness_indices)
+{
+    std::vector<stdlib::field_t<Builder>> result;
+    result.reserve(witness_indices.size());
+    for (const auto& idx : witness_indices) {
+        result.emplace_back(stdlib::field_t<Builder>::from_witness_index(&builder, idx));
+    }
+    return result;
+}
+
+// Helper to append some values to the witness vector and return their corresponding indices
+template <typename T>
+std::vector<uint32_t> add_to_witness_and_track_indices(WitnessVector& witness, std::span<const T> input)
+{
+    std::vector<uint32_t> indices;
+    indices.reserve(input.size());
+    auto witness_idx = static_cast<uint32_t>(witness.size());
+    for (const auto& value : input) {
+        witness.push_back(bb::fr(value));
+        indices.push_back(witness_idx++);
+    }
+    return indices;
+};
+
+} // namespace acir_format
