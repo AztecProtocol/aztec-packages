8caf4cee7afd5df1fac6929bf9c3f06ba6f87e0a
ledwards2225 <98505400+ledwards2225@users.noreply.github.com>
2025-08-27 16:25:44 -0700

    chore: civc tidy 2 (#16600)
    Another round of CIVC cleanup. Primarily introduces new single-purpose
    methods to improve readability of core methods (accumulation, kernel
    completion) and reduce duplication.

--- barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.hpp
+++ barretenberg/cpp/src/barretenberg/client_ivc/client_ivc.hpp
@@ -172,13 +172,14 @@ class ClientIVC {
         MSGPACK_FIELDS(mega, eccvm, translator);
     };
 
+    // Specifies proof type or equivalently the type of recursive verification to be performed on a given proof
     enum class QUEUE_TYPE {
         OINK,
         PG,
         PG_FINAL, // the final PG verification, used in hiding kernel
         PG_TAIL,  // used in tail to indicate special handling of merge for ZK
         MEGA
-    }; // for specifying type of proof in the verification queue
+    };
 
     // An entry in the native verification queue
     struct VerifierInputs {
@@ -216,7 +217,6 @@ class ClientIVC {
 
     ProverFoldOutput fold_output; // prover accumulator and fold proof
     HonkProof decider_proof;      // decider proof to be verified in the hiding circuit
-    HonkProof mega_proof;         // proof of the hiding circuit
 
     std::shared_ptr<DeciderVerificationKey>
         recursive_verifier_native_accum; // native verifier accumulator used in recursive folding
@@ -251,7 +251,7 @@ class ClientIVC {
         perform_recursive_verification_and_databus_consistency_checks(
             ClientCircuit& circuit,
             const StdlibVerifierInputs& verifier_inputs,
-            const std::shared_ptr<RecursiveDeciderVerificationKey>& input_stdlib_verifier_accumulator,
+            const std::shared_ptr<RecursiveDeciderVerificationKey>& input_verifier_accumulator,
             const TableCommitments& T_prev_commitments,
             const std::shared_ptr<RecursiveTranscript>& accumulation_recursive_transcript);
 
@@ -270,10 +270,8 @@ class ClientIVC {
 
     Proof prove();
 
-    std::shared_ptr<ClientIVC::DeciderZKProvingKey> construct_hiding_circuit_key(ClientCircuit& circuit);
-    std::shared_ptr<ClientIVC::DeciderZKProvingKey> compute_hiding_circuit_proving_key(ClientCircuit& circuit);
     static void hide_op_queue_accumulation_result(ClientCircuit& circuit);
-    HonkProof prove_hiding_circuit(ClientCircuit& circuit);
+    HonkProof construct_mega_proof_for_hiding_kernel(ClientCircuit& circuit);
 
     static bool verify(const Proof& proof, const VerificationKey& vk);
 
@@ -281,19 +279,45 @@ class ClientIVC {
 
     bool prove_and_verify();
 
-    HonkProof decider_prove();
+    HonkProof construct_decider_proof();
 
     VerificationKey get_vk() const;
 
   private:
     /**
-     * @brief Update the native verifier accumulator based on the provided queue entry and transcript.
+     * @brief Runs either Oink or PG native verifier to update the native verifier accumulator
      *
      * @param queue_entry The verifier inputs from the queue.
      * @param verifier_transcript Verifier transcript corresponding to the prover transcript.
      */
     void update_native_verifier_accumulator(const VerifierInputs& queue_entry,
                                             const std::shared_ptr<Transcript>& verifier_transcript);
+
+    HonkProof construct_oink_proof(const std::shared_ptr<DeciderProvingKey>& proving_key,
+                                   const std::shared_ptr<MegaVerificationKey>& honk_vk,
+                                   const std::shared_ptr<Transcript>& transcript);
+
+    HonkProof construct_pg_proof(const std::shared_ptr<DeciderProvingKey>& proving_key,
+                                 const std::shared_ptr<MegaVerificationKey>& honk_vk,
+                                 const std::shared_ptr<Transcript>& transcript,
+                                 bool is_kernel);
+
+    QUEUE_TYPE get_queue_type() const;
+
+    static std::shared_ptr<RecursiveDeciderVerificationKey> perform_oink_recursive_verification(
+        ClientCircuit& circuit,
+        const std::shared_ptr<RecursiveDeciderVerificationKey>& verifier_instance,
+        const std::shared_ptr<RecursiveTranscript>& transcript,
+        const StdlibProof& proof);
+
+    static std::shared_ptr<RecursiveDeciderVerificationKey> perform_pg_recursive_verification(
+        ClientCircuit& circuit,
+        const std::shared_ptr<RecursiveDeciderVerificationKey>& verifier_accumulator,
+        const std::shared_ptr<RecursiveDeciderVerificationKey>& verifier_instance,
+        const std::shared_ptr<RecursiveTranscript>& transcript,
+        const StdlibProof& proof,
+        std::optional<StdlibFF>& prev_accum_hash,
+        bool is_kernel);
 };
 
 } // namespace bb
