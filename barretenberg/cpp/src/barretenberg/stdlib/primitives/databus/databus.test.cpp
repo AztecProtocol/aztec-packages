e9d0d5a9fb3b31f3a9011459461f05225fd52a59
Khashayar Barooti <khashayar@aztecprotocol.com>
2025-09-03 10:53:44 +0000

    small test change

--- barretenberg/cpp/src/barretenberg/stdlib/primitives/databus/databus.test.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/primitives/databus/databus.test.cpp
@@ -105,25 +105,26 @@ TEST(Databus, UnnormalizedEntryAccess)
     Builder builder;
     databus_ct databus;
     std::array<fr, 3> raw_calldata_entries = { 3, 2, 1 };
-    std::array<fr, 3> raw_returndata_entries = { 6, 4, 2 };
+    std::array<fr, 3> raw_returndata_entries = { 3, 2, 1 };
     std::vector<field_ct> calldata_entries;
     for (fr entry : raw_calldata_entries) {
+        calldata_entries.emplace_back(witness_ct(&builder, entry));
         field_ct entry_witness = witness_ct(&builder, entry);
-        // add the value to itself to make it unnormalized
-        calldata_entries.emplace_back(entry_witness + entry_witness);
     }
     std::vector<field_ct> returndata_entries;
     for (fr entry : raw_returndata_entries) {
-        returndata_entries.emplace_back(witness_ct(&builder, entry));
+        field_ct entry_witness = witness_ct(&builder, entry);
+        // add the value to itself to make it unnormalized
+        returndata_entries.emplace_back(entry_witness + entry_witness);
     }
     databus.calldata.set_values(calldata_entries);
     databus.return_data.set_values(returndata_entries);
     field_ct idx_0 = witness_ct(&builder, 0);
     field_ct idx_1 = witness_ct(&builder, 1);
     field_ct idx_2 = witness_ct(&builder, 2);
-    databus.return_data[idx_0].assert_equal(databus.calldata[idx_0]);
-    databus.return_data[idx_1].assert_equal(databus.calldata[idx_1]);
-    databus.return_data[idx_2].assert_equal(databus.calldata[idx_2]);
+    databus.return_data[idx_0].assert_equal(databus.calldata[idx_0] + databus.calldata[idx_0]);
+    databus.return_data[idx_1].assert_equal(databus.calldata[idx_1] + databus.calldata[idx_1]);
+    databus.return_data[idx_2].assert_equal(databus.calldata[idx_2] + databus.calldata[idx_2]);
     EXPECT_TRUE(CircuitChecker::check(builder));
 }
 
