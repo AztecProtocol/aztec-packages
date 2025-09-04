94dbc5fbb8af765a851d473e772904af8380d004
ludamad <domuradical@gmail.com>
2025-09-04 16:53:01 +0000

    Fix

--- yarn-project/end-to-end/bootstrap.sh
+++ yarn-project/end-to-end/bootstrap.sh
@@ -26,9 +26,10 @@ function test_cmds {
       echo "$prefix:NAME=e2e_prover_ddos_prevention_real $run_test_script simple e2e_prover/ddos_prevention"
     else
       echo "$prefix:NAME=e2e_prover_full_fake FAKE_PROOFS=1 $run_test_script simple e2e_prover/full"
-      echo "$prefix:NAME=e2e_prover_generate_sample_toml_fake FAKE_PROOFS=1 $run_test_script simple e2e_prover/generate_sample_toml"
-      echo "$prefix:NAME=e2e_prover_invalid_proofs_fake FAKE_PROOFS=1 $run_test_script simple e2e_prover/invalid_proofs"
-      echo "$prefix:NAME=e2e_prover_ddos_prevention_fake FAKE_PROOFS=1 $run_test_script simple e2e_prover/ddos_prevention"
+      # fake proof versions of these are a no-op
+      # echo "$prefix:NAME=e2e_prover_generate_sample_toml_fake FAKE_PROOFS=1 $run_test_script simple e2e_prover/generate_sample_toml"
+      # echo "$prefix:NAME=e2e_prover_invalid_proofs_fake FAKE_PROOFS=1 $run_test_script simple e2e_prover/invalid_proofs"
+      # echo "$prefix:NAME=e2e_prover_ddos_prevention_fake FAKE_PROOFS=1 $run_test_script simple e2e_prover/ddos_prevention"
     fi
   fi
   echo "$prefix:TIMEOUT=15m:NAME=e2e_block_building $run_test_script simple e2e_block_building"
