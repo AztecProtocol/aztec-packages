5bf8f10a819f1c94fd7d8e7416fad713e37e4e58
ludamad <adam.domurad@gmail.com>
2025-09-04 20:45:27 +0000

    fix(ci): post-fix for noir projects cache bug (#16793)

--- noir-projects/noir-protocol-circuits/bootstrap.sh
+++ noir-projects/noir-protocol-circuits/bootstrap.sh
@@ -104,7 +104,7 @@ function compile {
   # TODO: Change this to add verification_key to original json, like contracts does.
   # Will require changing TS code downstream.
   bytecode_hash=$(jq -r '.bytecode' $json_path | sha256sum | tr -d ' -')
-  hash=$(hash_str "$BB_HASH-$bytecode_hash-$proto")
+  hash=$(hash_str "$BB_HASH-$bytecode_hash-$proto-$(cache_content_hash "^noir-projects/noir-protocol-circuits/bootstrap.sh")")
   if ! cache_download vk-$hash.tar.gz 1>&2; then
     local key_path="$key_dir/$name.vk.data.json"
     echo_stderr "Generating vk for function: $name..."
