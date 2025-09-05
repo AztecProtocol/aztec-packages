2c80eb7347ed7a141e8b3009f8ffed21442192de
fcarreiro <facundo@aztecprotocol.com>
2025-09-05 12:12:48 +0000

    feat(avm)!: use View in accumulate

---
+++ barretenberg/cpp/src/barretenberg/vm2/constraining/relations/relation_macros.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+template <typename View, typename FF_> constexpr auto avm_relations_maybe_cast_(auto&& v)
+{
+    if constexpr (std::is_same_v<std::decay_t<decltype(v)>, FF_>) {
+        return std::forward<decltype(v)>(v);
+    } else {
+        return static_cast<View>(std::forward<decltype(v)>(v));
+    }
+};
+
+#define CView(v) avm_relations_maybe_cast_<View, FF_>(v)
