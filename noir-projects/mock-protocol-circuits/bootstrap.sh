0c30d286b4f247928e8e502eeb8831b68f06f1f2
ludamad <adam.domurad@gmail.com>
2025-09-05 18:09:56 -0400

    Discard changes to noir-projects/mock-protocol-circuits/bootstrap.sh

--- noir-projects/mock-protocol-circuits/bootstrap.sh
+++ noir-projects/mock-protocol-circuits/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source $(git rev-parse --show-toplevel)/ci3/source
-export NOIR_PROTOCOL_CIRCUITS_WORKING_DIR="$(pwd)"
-../noir-protocol-circuits/bootstrap.sh "$@"
+
+../noir-protocol-circuits/bootstrap.sh "${1:-}" $(pwd)
