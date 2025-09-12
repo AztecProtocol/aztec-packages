7c2a997ed7667133d0a971a40c03083e01c0b5f6
jewelofchaos9 <jewelofchaos9@gmail.com>
2025-09-12 07:33:29 +0000

    fix overcleaning

--- yarn-project/simulator/src/public/avm/avm_simulator.test.ts
+++ yarn-project/simulator/src/public/avm/avm_simulator.test.ts
@@ -212,6 +212,16 @@ describe('AVM simulator: transpiled Noir contracts', () => {
     expect(results.output).toEqual([new Fr(0)]);
   });
 
+  it('Should handle calldata oracle', async () => {
+    const calldata: Fr[] = [new Fr(1), new Fr(2), new Fr(3)];
+    const context = initContext({ env: initExecutionEnvironment({ calldata }) });
+
+    const bytecode = getAvmTestContractBytecode('assert_calldata_copy');
+    const results = await new AvmSimulator(context).executeBytecode(bytecode);
+
+    expect(results.reverted).toBe(false);
+  });
+
   it('Should handle return oracle', async () => {
     const context = initContext();
 
