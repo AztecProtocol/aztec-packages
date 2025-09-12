c3e390d111e71f946b8de0d5ee89341f1cebeedc
ludamad <adam.domurad@gmail.com>
2025-09-12 13:23:21 -0400

    Revert "feat: Include SMT build into CI and create container-build for smt"

--- barretenberg/cpp/bootstrap.sh
+++ barretenberg/cpp/bootstrap.sh
@@ -147,18 +147,6 @@ function build_fuzzing_syntax_check_only {
   cache_upload barretenberg-fuzzing-$hash.zst build-fuzzing/syntax-check-success.flag
 }
 
-# Do basic tests that the smt preset still compiles and runs
-function build_smt_verification {
-  set -eu
-  if cache_download barretenberg-smt-$hash.zst; then
-    return
-  fi
-  sudo apt update && sudo apt install -y python3-pip python3-venv m4
-  cmake --preset smt-verification
-  cmake --build build-smt --target smt_verification_tests
-  cache_upload barretenberg-smt-$hash.zst build-smt/bin
-}
-
 function build_release {
   local arch=$(arch)
   rm -rf build-release
@@ -177,7 +165,7 @@ function build_release {
   fi
 }
 
-export -f build_preset build_native build_asan_fast build_darwin build_nodejs_module build_wasm build_wasm_threads build_gcc_syntax_check_only build_fuzzing_syntax_check_only build_smt_verification
+export -f build_preset build_native build_asan_fast build_darwin build_nodejs_module build_wasm build_wasm_threads build_gcc_syntax_check_only build_fuzzing_syntax_check_only
 
 function build {
   echo_header "bb cpp build"
@@ -188,7 +176,7 @@ function build {
     build_wasm_threads
   )
   if [ "$(arch)" == "amd64" ] && [ "$CI" -eq 1 ]; then
-    builds+=(build_gcc_syntax_check_only build_fuzzing_syntax_check_only build_smt_verification build_asan_fast)
+    builds+=(build_gcc_syntax_check_only build_fuzzing_syntax_check_only build_asan_fast)
   fi
   if [ "$CI_FULL" -eq 1 ]; then
     builds+=(build_darwin)
@@ -237,13 +225,6 @@ function test_cmds {
       echo -e "$prefix barretenberg/cpp/build-asan-fast/bin/$bin_name --gtest_filter=$filter"
     done
   fi
-
-  # Run the SMT compatibility tests
-  if [ "$(arch)" == "amd64" ] &&  [ "$CI" -eq 1 ]; then
-    local prefix="$hash:CPUS=4:MEM=8g"
-    echo -e "$prefix barretenberg/cpp/build-smt/bin/smt_verification_tests"
-  fi
-
   echo "$hash barretenberg/cpp/scripts/test_civc_standalone_vks_havent_changed.sh"
 }
 
@@ -275,7 +256,7 @@ function bench_cmds {
   echo "$prefix barretenberg/cpp/scripts/run_bench.sh native bb-micro-bench/native/client_ivc_verify build/bin/client_ivc_bench VerificationOnly$"
 }
 
-# Runs benchmarks sharded over mache cores.
+# Runs benchmarks sharded over machine cores.
 function bench {
   echo_header "bb bench"
   rm -rf bench-out && mkdir -p bench-out
@@ -353,7 +334,7 @@ case "$cmd" in
   "hash")
     echo $hash
     ;;
-  test|test_cmds|bench|bench_cmds|build_bench|release|build_native|build_nodejs_module|build_asan_fast|build_wasm|build_wasm_threads|build_gcc_syntax_check_only|build_fuzzing_syntax_check_only|build_darwin|build_release|build_smt_verification|inject_version)
+  test|test_cmds|bench|bench_cmds|build_bench|release|build_native|build_nodejs_module|build_asan_fast|build_wasm|build_wasm_threads|build_gcc_syntax_check_only|build_fuzzing_syntax_check_only|build_darwin|build_release|inject_version)
     $cmd "$@"
     ;;
   *)
--- container-builds/smt-container/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-image_name="smt-build-static"
-COMMIT="f3ff6161155ba5e07d14ea1c52a218494eb8bb96"
-docker build -t "$image_name" --target reuse --build-arg COMMIT="$COMMIT" src
