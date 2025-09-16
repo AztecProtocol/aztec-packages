1b4ee38d691fbbae8921d68702c0482f717c72f3
Josh Crites <jc@joshcrites.com>
2025-09-16 15:26:18 +0000

    fix(docs): fix nightly docs builds (#17073)

--- barretenberg/docs/docusaurus.config.ts
+++ barretenberg/docs/docusaurus.config.ts
@@ -89,7 +89,7 @@ const config: Config = {
           lastVersion: versions[1],
           versions: {
             [versions[0]]: {
-              path: "nightly",
+              ...(versions[0].includes("nightly") && { path: "nightly" }),
             },
             ...(process.env.ENV === "dev" && {
               current: {
