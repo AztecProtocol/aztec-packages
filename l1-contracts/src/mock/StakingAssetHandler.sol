93c64e47a69b67304ae76fedc20795ed47fe6a68
saleel <saleel@saleel.xyz>
2025-09-16 12:03:36 +0000

    chore: update zkpassport version in StakingAssetHandler (#17062)

--- l1-contracts/src/mock/StakingAssetHandler.sol
+++ l1-contracts/src/mock/StakingAssetHandler.sol
@@ -7,7 +7,7 @@ import {IMintableERC20} from "@aztec/shared/interfaces/IMintableERC20.sol";
 import {G1Point, G2Point} from "@aztec/shared/libraries/BN254Lib.sol";
 import {Ownable} from "@oz/access/Ownable.sol";
 import {MerkleProof} from "@oz/utils/cryptography/MerkleProof.sol";
-import {ZKPassportVerifier, ProofVerificationParams} from "@zkpassport/ZKPassportVerifier.sol";
+import {ZKPassportVerifier, ProofVerificationParams, ProofType} from "@zkpassport/ZKPassportVerifier.sol";
 
 /**
  * @title StakingAssetHandler
@@ -50,8 +50,13 @@ interface IStakingAssetHandler {
   error InvalidProof();
   error InvalidScope();
   error InvalidDomain();
-  error ProofNotBoundToAddress(address _expected, address _received);
-  error ProofNotBoundToChainId(uint256 _expected, uint256 _received);
+  error InvalidBoundAddress(address _expected, address _received);
+  error InvalidChainId(uint256 _expected, uint256 _received);
+  error InvalidAge();
+  error InvalidCountry();
+  error InvalidCurrentDate();
+  error InvalidValidityPeriod();
+  error ExtraDiscloseDataNonZero();
   error SybilDetected(bytes32 _nullifier);
   error AttesterDoesNotExist(address _attester);
   error NoNullifier();
@@ -126,8 +131,18 @@ contract StakingAssetHandler is IStakingAssetHandler, Ownable {
 
   address public withdrawer;
 
+  // ZKPassport constraints
   string public validDomain;
   string public validScope;
+  uint256 public validValidityPeriodInSeconds = 7 days;
+  uint256 public validMinAge = 18;
+  uint256 public validMaxAge = 0;
+
+  // ZKPassport - Excluded counties
+  bytes32 internal pkr = keccak256(bytes("PRK"));
+  bytes32 internal ukr = keccak256(bytes("UKR"));
+  bytes32 internal irn = keccak256(bytes("IRN"));
+  bytes32 internal cub = keccak256(bytes("CUB"));
 
   constructor(StakingAssetHandlerArgs memory _args) Ownable(_args.owner) {
     require(_args.depositsPerMint > 0, CannotMintZeroAmount());
@@ -303,13 +318,34 @@ contract StakingAssetHandler is IStakingAssetHandler, Ownable {
 
     if (!skipBindCheck) {
       bytes memory data = zkPassportVerifier.getBindProofInputs(_params.committedInputs, _params.committedInputCounts);
-      // Use the getBoundData function to get the formatted data
-      // which includes the user's address, chainId and any custom data
-      (address boundAddress, uint256 chainId,) = zkPassportVerifier.getBoundData(data);
+
+      (address boundAddress, uint256 chainId, string memory customData) = zkPassportVerifier.getBoundData(data);
       // Make sure the bound user address is the same as the _attester
-      require(boundAddress == _attester, ProofNotBoundToAddress(boundAddress, _attester));
+      require(boundAddress == _attester, InvalidBoundAddress(boundAddress, _attester));
       // Make sure the chainId is the same as the current chainId
-      require(chainId == block.chainid, ProofNotBoundToChainId(chainId, block.chainid));
+      require(chainId == block.chainid, InvalidChainId(chainId, block.chainid));
+      // Make sure the custom data is empty
+      require(bytes(customData).length == 0, ExtraDiscloseDataNonZero());
+
+      // Validity period check
+      require(validValidityPeriodInSeconds == _params.validityPeriodInSeconds, InvalidValidityPeriod());
+
+      // Age check
+      (uint256 currentDate, uint8 minAge, uint8 maxAge) =
+        zkPassportVerifier.getAgeProofInputs(_params.committedInputs, _params.committedInputCounts);
+      require(block.timestamp >= currentDate, InvalidCurrentDate());
+      require(validMinAge == minAge && validMaxAge == maxAge, InvalidAge());
+
+      // Country exclusion check
+      string[] memory exclusionCountryList = zkPassportVerifier.getCountryProofInputs(
+        _params.committedInputs, _params.committedInputCounts, ProofType.NATIONALITY_EXCLUSION
+      );
+      require(keccak256(bytes(exclusionCountryList[0])) == cub, InvalidCountry());
+      require(keccak256(bytes(exclusionCountryList[1])) == irn, InvalidCountry());
+      require(keccak256(bytes(exclusionCountryList[2])) == pkr, InvalidCountry());
+      require(keccak256(bytes(exclusionCountryList[3])) == ukr, InvalidCountry());
+
+      zkPassportVerifier.enforceSanctionsRoot(_params.committedInputs, _params.committedInputCounts);
     }
 
     // Set nullifier to consumed
