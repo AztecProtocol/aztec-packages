1c0c331a9c37bc9b9a48b9ea3741bd6fe3f45b36
Suyash Bagad <suyash@aztecprotocol.com>
2025-09-19 02:16:35 +0530

    chore: expanded bigfield tests (veridise report) (#16802)
    As per the suggestion of adding more tests and including several edge
    cases, we have added multiple more test cases with critical edge cases
    and multiple witness-constant configurations.

--- barretenberg/cpp/src/barretenberg/stdlib/primitives/field/field.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib/primitives/field/field.hpp
@@ -373,8 +373,16 @@ template <typename Builder_> class field_t {
                                                const bool_t<Builder>& t1,
                                                const bool_t<Builder>& t0);
 
-    static void evaluate_linear_identity(const field_t& a, const field_t& b, const field_t& c, const field_t& d);
-    static void evaluate_polynomial_identity(const field_t& a, const field_t& b, const field_t& c, const field_t& d);
+    static void evaluate_linear_identity(const field_t& a,
+                                         const field_t& b,
+                                         const field_t& c,
+                                         const field_t& d,
+                                         const std::string& msg = "field_t::evaluate_linear_identity");
+    static void evaluate_polynomial_identity(const field_t& a,
+                                             const field_t& b,
+                                             const field_t& c,
+                                             const field_t& d,
+                                             const std::string& msg = "field_t::evaluate_polynomial_identity");
 
     static field_t accumulate(const std::vector<field_t>& input);
 
