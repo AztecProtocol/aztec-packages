770fa73581441dde3099d9553a03ac18fa1c1f65
Suyash Bagad <suyash@aztecprotocol.com>
2025-09-23 11:02:16 +0200

    fix: use correct range constraints in non-native field multiplication tests (in ultra builder) (#17197)
    Fixes the rare test failures seen in `UltraCircuitBuilder` tests in
    non-native field multiplication.
    
    tldr: we were range constraining the output carries `hi, lo` to be 70
    bit numbers each, but in some cases, these could be 71 or 72-bit
    numbers.
    
    ### The Issue
    
    In the `UltraCircuitBuilder.NonNativeFieldMultiplication` test, we
    range-constrained the lo and hi carries to be 70-bit field elements:
    
    
    https://github.com/AztecProtocol/aztec-packages/blob/41110d306c1e5bb616c6eadb4a9f86556f9bddf8/barretenberg/cpp/src/barretenberg/circuit_checker/ultra_circuit_builder.test.cpp#L627-L628
    
    The function `range_constrain_two_limbs` uses custom NNF gates to range
    constrain the inputs by breaking them up in 5 parts each of 14 bits. In
    other words, it allows number of bits ≤ 70.
    
    In our test, since we choose random inputs $a, b \in \mathbb{F}_q$ for
    non-native field multiplication, the carries lo and hi could have more
    than 70 bits. One such example is:
    
    ```cpp
    uint256_t a_u256 = uint256_t("0x00ab1504deacff852326adf4a01099e9340f232e2a631042852fce3c4eb8a51b");
    uint256_t b_u256 = uint256_t("0x1be457323502cfcd85f8cfa54c8c4fea146b9db2a7d86b29d966d61b714ee249");
    uint256_t q_u256 = uint256_t("0x00629b9d576dfc6b5c28a4a254d5e8e3384124f6a898858e95265254a01414d5");
    uint256_t r_u256 = uint256_t("0x2c1590eb70a48dce72f7686bbf79b59bf7926c99bc16aba92e474c65a04ea2a0");
    ```
    
    In this example, the hi carry is a 71-bit number and hence it (silently)
    fails the range-constraint by the function `range_constrain_two_limbs`.
    
    ### Fix
    
    I've modified the test(s) to check if the lo or hi contain more than 70
    bits and if so, use `decompose_into_default_range` which allows for
    larger ranges. I set the range in `decompose_into_default_range` to be
    72 bits. See below for why this is sufficient. I've also modified the
    original `NonNativeFieldMultiplication` test to run for 50 iterations,
    and added a regression test to show that the fix works for the edge case
    example. I've also added error logging in usage of
    `range_constrain_two_limbs`.
    
    ### Appendix
    
    To understand why 72 bits is a sufficient range for the edge cases,
    recall that the max values $(\textcolor{orange}{M_{\textsf{lo}}},
    \textcolor{orange}{M_{\textsf{hi}}})$ of the low and high outputs
    respectively must be:
    
    $$\textsf{max}(\textcolor{orange}{M_{\textsf{lo}}},
    \textcolor{orange}{M_{\textsf{hi}}}) < 2^{2Q + L + 3}$$
    
    where $Q$ is the bit-size of the limbs. Refer to the README in bigfield
    to understand how we get this bound. In the test cases, we know that $Q
    = L = 68$. Thus, the max values will always be less than $(3L + 3)$. The
    carry is computed by dividing the max values by $2^{2L}$ and hence the
    max carry values will always be less than
    
    $$\frac{2^{(3L + 3)}}{2^{2L}} = 2^{L +3} = 2^{71}$$ 
    
    i.e., 71 bit range constraint should suffice. 
    
    In bigfield computations, we allow the limb sizes to grow to $Q = 78$,
    in which case this bound would translate to:
    
    $$\frac{2^{(2Q + L + 3)}}{2^{2L}} = 2^{91}$$

--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/ultra_circuit_builder.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/ultra_circuit_builder.hpp
@@ -794,7 +794,8 @@ class UltraCircuitBuilder_ : public CircuitBuilderBase<typename ExecutionTrace_:
     void range_constrain_two_limbs(const uint32_t lo_idx,
                                    const uint32_t hi_idx,
                                    const size_t lo_limb_bits = DEFAULT_NON_NATIVE_FIELD_LIMB_BITS,
-                                   const size_t hi_limb_bits = DEFAULT_NON_NATIVE_FIELD_LIMB_BITS);
+                                   const size_t hi_limb_bits = DEFAULT_NON_NATIVE_FIELD_LIMB_BITS,
+                                   std::string const& msg = "range_constrain_two_limbs");
     std::array<uint32_t, 2> decompose_non_native_field_double_width_limb(
         const uint32_t limb_idx, const size_t num_limb_bits = (2 * DEFAULT_NON_NATIVE_FIELD_LIMB_BITS));
     std::array<uint32_t, 2> evaluate_non_native_field_multiplication(
