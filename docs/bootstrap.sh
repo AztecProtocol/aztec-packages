ca4a065d10c1b7eb9089b11a66c42d23fb998dab
sklppy <152162806+sklppy88@users.noreply.github.com>
2025-09-25 10:47:21 +0000

    fix

--- docs/bootstrap.sh
+++ docs/bootstrap.sh
@@ -3,6 +3,28 @@ source $(git rev-parse --show-toplevel)/ci3/source_bootstrap
 
 cmd=${1:-}
 
+export RAYON_NUM_THREADS=${RAYON_NUM_THREADS:-16}
+export HARDWARE_CONCURRENCY=${HARDWARE_CONCURRENCY:-16}
+export PLATFORM_TAG=any
+
+export BB=${BB:-../barretenberg/cpp/build/bin/bb}
+export NARGO=${NARGO:-../noir/noir-repo/target/release/nargo}
+export TRANSPILER=${TRANSPILER:-../avm-transpiler/target/release/avm-transpiler}
+export BB_HASH=$(../barretenberg/cpp/bootstrap.sh hash)
+export NOIR_HASH=${NOIR_HASH:-$(../noir/bootstrap.sh hash)}
+
+export tmp_dir=./target/tmp
+
+# Remove our tmp dir from last run.
+# Note: This can use BASH 'trap' for better cleanliness, but the script has been hitting edge-cases so is (temporarily?) simplified.
+rm -rf $tmp_dir
+mkdir -p $tmp_dir
+
+# Set common flags for parallel.
+export PARALLEL_FLAGS="-j${PARALLELISM:-16} --halt now,fail=1 --memsuspend $(memsuspend_limit)"
+
+cmd=${1:-}
+
 # We search the docs/*.md files to find included code, and use those as our rebuild dependencies.
 # We prefix the results with ^ to make them "not a file", otherwise they'd be interpreted as pattern files.
 hash=$(
@@ -47,6 +69,102 @@ function test {
   test_cmds | parallelize
 }
 
+
+# This computes a vk and adds it to the input function json if it's private, else returns same input.
+# stdin has the function json.
+# stdout receives the function json with the vk added (if private).
+# The function is exported and called by a sub-shell in parallel, so we must "set -eu" etc..
+# If debugging, a set -x at the start can help.
+function process_function {
+  set -euo pipefail
+  local func name bytecode_b64 hash vk
+
+  contract_hash=$1
+  # Read the function json.
+  func="$(cat)"
+  name=$(echo "$func" | jq -r '.name')
+  echo_stderr "Processing function: $name..."
+
+  # Check if the function is neither public nor unconstrained.
+  # TODO: Why do we need to gen keys for functions that are not marked private?
+  # We allow the jq call to error (set +e) because it returns an error code if the result is false.
+  # We then differentiate between a real error, and the result being false.
+  set +e
+  make_vk=$(echo "$func" | jq -e '(.custom_attributes | index("public") == null) and (.is_unconstrained == false)')
+  if [ $? -ne 0 ] && [ "$make_vk" != "false" ]; then
+    echo_stderr "Failed to check function $name is neither public nor unconstrained."
+    exit 1
+  fi
+  set -e
+
+  if [ "$make_vk" == "true" ]; then
+    # It's a private function.
+    # Build hash, check if in cache.
+    # If it's in the cache it's extracted to $tmp_dir/$hash
+    bytecode_b64=$(echo "$func" | jq -r '.bytecode')
+    hash=$((echo "$BB_HASH"; echo "$bytecode_b64") | sha256sum | tr -d ' -')
+
+    if ! cache_download vk-$contract_hash-$hash.tar.gz >&2; then
+      # It's not in the cache. Generate the vk file and upload it to the cache.
+      echo_stderr "Generating vk for function: $name..."
+
+      local outdir=$(mktemp -d -p $tmp_dir)
+      echo "$bytecode_b64" | base64 -d | gunzip | $BB write_vk --scheme client_ivc --verifier_type standalone -b - -o $outdir -v
+      mv $outdir/vk $tmp_dir/$contract_hash/$hash
+
+      cache_upload vk-$contract_hash-$hash.tar.gz $tmp_dir/$contract_hash/$hash
+    fi
+
+    # Return (echo) json containing the base64 encoded verification key.
+    vk=$(cat $tmp_dir/$contract_hash/$hash | base64 -w 0)
+    echo "$func" | jq -c --arg vk "$vk" '. + {verification_key: $vk}'
+  else
+    echo_stderr "Function $name is neither public nor unconstrained, skipping."
+    # Not a private function. Return the original function json.
+    echo "$func"
+  fi
+}
+export -f process_function
+
+# Compute hash for a given contract.
+function get_contract_hash {
+  local contract_path=$(get_contract_path "$1")
+
+  hash_str \
+    $NOIR_HASH \
+    $(cache_content_hash \
+      ../avm-transpiler/.rebuild_patterns \
+      "^docs/examples/$contract_path/" \
+      "^noir-projects/aztec-nr/" \
+      "^noir-projects/noir-protocol-circuits/crates/types/")
+}
+export -f get_contract_hash
+
+# Extract contract path from Nargo.toml based on argument
+# Handle both formats: full path relative to examples/ or just contract name
+# E.g. for both "ecdsa_k_account_contract" and "account/ecdsa_k_account_contractor" returns
+# "account/ecdsa_k_account_contractor"
+#
+# This is done to ensure that both paths can be provided as inputs to the script.
+function get_contract_path {
+  local input=$1
+  local contract_path
+  if [[ $input == *"/"* ]]; then
+    # Full path provided (e.g. account/ecdsa_k_account_contract)
+    contract_path=$input
+  else
+    # Just contract name provided (e.g. ecdsa_k_account_contract)
+    contract_path=$(grep -oP "(?<=examples/)[^\"]+/$input" Nargo.toml)
+    if [ -z "$contract_path" ]; then
+      echo "Contract $input not found in Nargo.toml" >&2
+      exit 1
+    fi
+  fi
+  echo "$contract_path"
+}
+export -f get_contract_path
+
+
 # This compiles a noir contract, transpile's public functions, and generates vk's for private functions.
 # $1 is the input package name, and on exit it's fully processed json artifact is in the target dir.
 # The function is exported and called by a sub-shell in parallel, so we must "set -eu" etc..
@@ -57,7 +175,7 @@ function compile {
   local contract_path=$(get_contract_path "$1")
   local contract=${contract_path#*/}
   # Calculate filename because nargo...
-  contract_name=$(cat contracts/$contract_path/src/main.nr | awk '/^contract / { print $2 } /^pub contract / { print $3 }')
+  contract_name=$(cat examples/$contract_path/src/main.nr | awk '/^contract / { print $2 } /^pub contract / { print $3 }')
   local filename="$contract-$contract_name.json"
   local json_path="./target/$filename"
   contract_hash=$(get_contract_hash $1)
@@ -91,7 +209,7 @@ function build {
   if [ "$#" -eq 0 ]; then
     rm -rf target
     mkdir -p $tmp_dir
-    local contracts=$(grep -oP '(?<=contracts/)[^"]+' Nargo.toml)
+    local contracts=$(grep -oP '(?<=examples/)[^"]+' Nargo.toml)
   else
     local contracts="$@"
   fi
