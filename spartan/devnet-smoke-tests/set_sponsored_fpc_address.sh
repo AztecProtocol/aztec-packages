2fb4bafffe32cbeb75fbd82e3b88704038c18603
Alex Gherghisan <alexghr@users.noreply.github.com>
2025-09-26 10:26:52 +0000

    chore: spartan cleanup (#17271)

--- spartan/devnet-smoke-tests/set_sponsored_fpc_address.sh
+++
@@ -1,4 +0,0 @@
-SPONSORED_FPC_ADDRESS=$(aztec \
-  get-canonical-sponsored-fpc-address \
-  | awk '{print $NF}')
-SPONSORED_FPC_PAYMENT_METHOD="--payment method=fpc-sponsored,fpc=$SPONSORED_FPC_ADDRESS"
