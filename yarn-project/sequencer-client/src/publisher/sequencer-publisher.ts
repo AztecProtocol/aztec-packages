fe1cfe442d768983dd907a73f42375de1ad55816
Alex Gherghisan <alexghr@users.noreply.github.com>
2025-09-26 11:16:34 +0000

    feat: optionally disable publishing proofs on L1

--- yarn-project/sequencer-client/src/publisher/sequencer-publisher.ts
+++ yarn-project/sequencer-client/src/publisher/sequencer-publisher.ts
@@ -102,7 +102,6 @@ interface RequestWithExpiry {
 }
 
 export class SequencerPublisher {
-  private enabled: boolean;
   private interrupted = false;
   private metrics: SequencerPublisherMetrics;
   public epochCache: EpochCache;
@@ -152,7 +151,6 @@ export class SequencerPublisher {
       log?: Logger;
     },
   ) {
-    this.enabled = config.publisherEnabled ?? true;
     this.log = deps.log ?? createLogger('sequencer:publisher');
     this.ethereumSlotDuration = BigInt(config.ethereumSlotDuration);
     this.epochCache = deps.epochCache;
@@ -202,14 +200,6 @@ export class SequencerPublisher {
    * - undefined if no valid requests are found OR the tx failed to send.
    */
   public async sendRequests() {
-    if (!this.enabled) {
-      this.log.warn(`Sending L1 txs is disabled`, {
-        requestsDiscarded: this.requests.map(r => r.action),
-      });
-      this.requests = [];
-      return undefined;
-    }
-
     const requestsToProcess = [...this.requests];
     this.requests = [];
     if (this.interrupted) {
