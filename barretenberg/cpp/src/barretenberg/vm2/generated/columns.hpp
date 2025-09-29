51a181116d81e3aa1c5d5265e4a0b69f59442055
fcarreiro <facundo@aztecprotocol.com>
2025-09-29 12:21:49 +0000

    rebase

--- barretenberg/cpp/src/barretenberg/vm2/generated/columns.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/generated/columns.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <optional>
 
 #include "barretenberg/common/std_string.hpp"
 
@@ -39,29 +40,19 @@ enum class ColumnAndShifts {
 constexpr auto NUM_COLUMNS_WITH_SHIFTS = 3561;
 constexpr auto NUM_COLUMNS_WITHOUT_SHIFTS = 3225;
 constexpr auto NUM_PRECOMPUTED_ENTITIES = 133;
-<<<<<<< HEAD
 constexpr auto NUM_WIRE_ENTITIES = 2620;
 constexpr auto NUM_DERIVED_ENTITIES = 472;
 constexpr auto NUM_SHIFTED_ENTITIES = 336;
 constexpr auto NUM_ALL_ENTITIES = 3561;
-=======
-constexpr auto NUM_WIRE_ENTITIES = 2603;
-constexpr auto NUM_DERIVED_ENTITIES = 467;
-constexpr auto NUM_WITNESS_ENTITIES = NUM_WIRE_ENTITIES + NUM_DERIVED_ENTITIES;
-constexpr auto NUM_WIRES_TO_BE_SHIFTED = 330;
-constexpr auto NUM_SHIFTED_ENTITIES = 330;
-constexpr auto NUM_UNSHIFTED_ENTITIES = NUM_COLUMNS_WITHOUT_SHIFTS;
-constexpr auto NUM_ALL_ENTITIES = 3533;
->>>>>>> 4417996b05 (fixes)
 
 /*
  * Layout for all entities is:
  *
- * precomputed | wire(not shifted) : wire(to be shifted) | derived | shifted
- *             \___________________________________________________/
- *                              witness
- * \_______________________________________________________________/
- *                             unshifted
+ * precomputed | wire | derived | shifted
+ *             \________________/
+ *                  witness
+ * \____________________________/
+ *           unshifted
  * The following end indices are exclusive.
  * That is, a section is [start_idx, end_idx).
  */
@@ -69,8 +60,6 @@ constexpr auto PRECOMPUTED_START_IDX = 0;
 constexpr auto PRECOMPUTED_END_IDX = NUM_PRECOMPUTED_ENTITIES;
 constexpr auto WIRE_START_IDX = PRECOMPUTED_END_IDX;
 constexpr auto WIRE_END_IDX = WIRE_START_IDX + NUM_WIRE_ENTITIES;
-constexpr auto WIRES_TO_BE_SHIFTED_START_IDX = WIRE_END_IDX - NUM_WIRES_TO_BE_SHIFTED;
-constexpr auto WIRES_TO_BE_SHIFTED_END_IDX = WIRE_END_IDX;
 constexpr auto DERIVED_START_IDX = WIRE_END_IDX;
 constexpr auto DERIVED_END_IDX = DERIVED_START_IDX + NUM_DERIVED_ENTITIES;
 constexpr auto SHIFTED_START_IDX = DERIVED_END_IDX;
