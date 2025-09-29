71b4d0c729eec67953b98001d0de9f0873ef3197
ledwards2225 <l.edwards.d@gmail.com>
2025-09-29 23:18:10 +0000

    remove todo

--- barretenberg/cpp/src/barretenberg/stdlib/primitives/group/cycle_group.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/primitives/group/cycle_group.cpp
@@ -55,9 +55,7 @@ cycle_group<Builder>::cycle_group(field_t _x, field_t _y, bool_t is_infinity)
         *this = constant_infinity(this->context);
     }
 
-    // TODO(https://github.com/AztecProtocol/barretenberg/issues/1067): This ASSERT is missing in the constructor but
-    // causes schnorr acir test to fail due to a bad input (a public key that has x and y coordinate set to 0).
-    // Investigate this to be able to enable the test.
+    // Note: Points are not constrained in-circuit to be on the curve when using this constructor
     ASSERT(get_value().on_curve());
 }
 
@@ -344,9 +342,16 @@ cycle_group<Builder> cycle_group<Builder>::dbl(const std::optional<AffineElement
         return *this;
     }
 
+    info("x: ", x.get_value());
+    info("y: ", y.get_value());
+    info("is_infinity: ", is_point_at_infinity().get_value());
+
     // To support the point at infinity, we conditionally modify y to be 1 to avoid division by zero in the
     // doubling formula
     auto modified_y = field_t::conditional_assign(is_point_at_infinity(), 1, y).normalize();
+
+    info("x: ", x.get_value());
+    info("modified_y: ", modified_y.get_value());
 
     // Compute the doubled point coordinates (either from hint or by native calculation)
     bb::fr x3;
@@ -563,6 +568,11 @@ template <typename Builder> cycle_group<Builder> cycle_group<Builder>::operator+
     const field_t y1 = y;
     const field_t x2 = other.x;
     const field_t y2 = other.y;
+
+    info("x1: ", x1.get_value());
+    info("y1: ", y1.get_value());
+    info("x2: ", x2.get_value());
+    info("y2: ", y2.get_value());
 
     // Execute point addition with modified lambda = (y2 - y1)/(x2 - x1 + x_coordinates_match) to avoid the possibility
     // of division by zero.
