439f0caee661977bd9335bcfc36470f18180b5f6
Charlie Lye <5764343+charlielye@users.noreply.github.com>
2025-10-01 21:55:44 +0000

    fix

--- barretenberg/cpp/bootstrap.sh
+++ barretenberg/cpp/bootstrap.sh
@@ -207,7 +207,7 @@ function build_release {
   fi
 }
 
-export -f build_preset build_native build_asan_fast build_darwin build_nodejs_module build_wasm build_wasm_threads build_gcc_syntax_check_only build_fuzzing_syntax_check_only build_smt_verification
+export -f ensure_zig build_preset build_native build_asan_fast build_darwin build_nodejs_module build_wasm build_wasm_threads build_gcc_syntax_check_only build_fuzzing_syntax_check_only build_smt_verification
 
 function build {
   echo_header "bb cpp build"
