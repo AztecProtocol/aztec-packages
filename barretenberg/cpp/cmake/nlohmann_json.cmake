b308071d280a1b901ace061a735e9cdbcfaeb306
ludamad <domuradical@gmail.com>
2025-10-02 23:37:07 +0000

    Replace popen with C++ utilities
    - Add nlohmann/json and httplib dependencies
    - Add base64 encoding/decoding
    - Add get_bytecode for reading contract artifacts
    - Add http_download for fetching CRS over HTTP

---
+++ barretenberg/cpp/cmake/nlohmann_json.cmake
@@ -0,0 +1,22 @@
+include(FetchContent)
+
+set(NLOHMANN_JSON_INCLUDE "${CMAKE_BINARY_DIR}/_deps/nlohmann_json-src/include")
+
+FetchContent_Declare(
+    nlohmann_json
+    GIT_REPOSITORY https://github.com/nlohmann/json.git
+    GIT_TAG v3.11.3
+    FIND_PACKAGE_ARGS
+)
+
+set(JSON_BuildTests OFF CACHE INTERNAL "")
+set(JSON_Install OFF CACHE INTERNAL "")
+
+FetchContent_MakeAvailable(nlohmann_json)
+
+if(NOT nlohmann_json_FOUND)
+    # FetchContent_MakeAvailable calls FetchContent_Populate if `find_package` is unsuccessful
+    # so these variables will be available if we reach this case
+    set_property(DIRECTORY ${nlohmann_json_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL)
+    set_property(DIRECTORY ${nlohmann_json_BINARY_DIR} PROPERTY EXCLUDE_FROM_ALL)
+endif()
