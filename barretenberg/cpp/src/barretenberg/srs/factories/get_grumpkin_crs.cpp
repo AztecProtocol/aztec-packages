df64c436771f771e1d658231e4e64a6304a5be11
ludamad <domuradical@gmail.com>
2025-10-02 02:32:32 +0000

    fix threading

--- barretenberg/cpp/src/barretenberg/srs/factories/get_grumpkin_crs.cpp
+++ barretenberg/cpp/src/barretenberg/srs/factories/get_grumpkin_crs.cpp
@@ -1,5 +1,6 @@
 #include "get_grumpkin_crs.hpp"
 #include "barretenberg/api/file_io.hpp"
+#include "barretenberg/common/flock.hpp"
 #include "barretenberg/common/throw_or_abort.hpp"
 #include "barretenberg/common/try_catch_shim.hpp"
 #include "barretenberg/ecc/curves/grumpkin/grumpkin.hpp"
@@ -10,11 +11,15 @@ std::vector<curve::Grumpkin::AffineElement> get_grumpkin_g1_data(const std::file
                                                                  size_t num_points,
                                                                  bool allow_download)
 {
-    // TODO(AD): per Charlie this should just download and replace the flat file portion atomically so we have no race
-    // condition
     std::filesystem::create_directories(path);
+
     auto g1_path = path / "grumpkin_g1.flat.dat";
+    auto lock_path = path / "crs.lock";
+    // Acquire exclusive lock to prevent simultaneous generation/writes
+    FileLockGuard lock(lock_path.string());
+
     size_t g1_downloaded_points = get_file_size(g1_path) / sizeof(curve::Grumpkin::AffineElement);
+
     if (g1_downloaded_points >= num_points) {
         vinfo("using cached grumpkin crs with num points ", g1_downloaded_points, " at: ", g1_path);
         auto data = read_file(g1_path, num_points * sizeof(curve::Grumpkin::AffineElement));
@@ -26,6 +31,7 @@ std::vector<curve::Grumpkin::AffineElement> get_grumpkin_g1_data(const std::file
             return points;
         }
     }
+
     if (!allow_download && g1_downloaded_points == 0) {
         throw_or_abort("grumpkin g1 data not found and generation not allowed in this context");
     } else if (!allow_download) {
@@ -35,9 +41,10 @@ std::vector<curve::Grumpkin::AffineElement> get_grumpkin_g1_data(const std::file
                               num_points,
                               " were requested but generation not allowed in this context"));
     }
+
     vinfo("generating grumpkin crs...");
     auto points = srs::generate_grumpkin_srs(num_points);
-    write_file(path / "grumpkin_g1.flat.dat", to_buffer(points));
+    write_file(g1_path, to_buffer(points));
     return points;
 }
 } // namespace bb
