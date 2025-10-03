0f21937cd995048bb13f76c2f2c6dd2b8446cdc8
ludamad <domuradical@gmail.com>
2025-10-03 15:17:44 +0000

    Remove version files and duplicate grumpkin_srs_gen.hpp

--- barretenberg/cpp/src/barretenberg/common/version.cpp
+++
@@ -1,7 +0,0 @@
-#include "version.hpp"
-
-namespace bb {
-// This is updated in-place by bootstrap.sh during the release process. This prevents
-// the version string from needing to be present at build-time, simplifying e.g. caching.
-const char* const BB_VERSION_PLACEHOLDER = "00000000.00000000.00000000";
-} // namespace bb
