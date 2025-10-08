56c8b80c2dc848f66b11f6478b9debde922962e5
sirasistant <sirasistant@gmail.com>
2025-10-08 13:24:21 +0000

    add fdiv spam

--- yarn-project/simulator/src/public/fixtures/red_contract.test.ts
+++ yarn-project/simulator/src/public/fixtures/red_contract.test.ts
@@ -5,7 +5,18 @@ import { AztecAddress } from '@aztec/stdlib/aztec-address';
 import { writeFile } from 'fs/promises';
 
 import { TypeTag } from '../avm/avm_memory_types.js';
-import { Add, CalldataCopy, Cast, Instruction, JumpI, Lt, Poseidon2, Return, Set } from '../avm/opcodes/index.js';
+import {
+  Add,
+  CalldataCopy,
+  Cast,
+  FieldDiv,
+  Instruction,
+  JumpI,
+  Lt,
+  Poseidon2,
+  Return,
+  Set,
+} from '../avm/opcodes/index.js';
 import { encodeToBytecode } from '../avm/serialization/bytecode_serialization.js';
 import { Opcode } from '../avm/serialization/instruction_serialization.js';
 import { PublicTxSimulationTester } from './public_tx_simulation_tester.js';
@@ -71,6 +82,56 @@ function createPoseidonSpamContractNoLoop() {
   return buffer;
 }
 
+function createFDivSpamContract() {
+  const preambleInstructions = [
+    // SET at address 1 an 1_u32 constant.
+    new Set(/*indirect*/ 0, /*dstOffset*/ 1, TypeTag.UINT32, /* value */ 1).as(Opcode.SET_8, Set.wireFormat8),
+    // COPY from calldata starting at offset 1, size 1, to address 2.
+    new CalldataCopy(/*indirect*/ 0, /*copySize*/ 1, /* cdStartOffset */ 1, /*dstOffset*/ 2).as(
+      Opcode.CALLDATACOPY,
+      CalldataCopy.wireFormat,
+    ),
+    // CAST address 2 to a u32.
+    new Cast(/*indirect*/ 0, /*srcOffset*/ 2, /*dstOffset*/ 2, /*dstTag*/ TypeTag.UINT32).as(
+      Opcode.CAST_8,
+      Cast.wireFormat8,
+    ),
+    // SET the starting value of the iterator at address 3
+    new Set(/*indirect*/ 0, /*dstOffset*/ 3, TypeTag.UINT32, /*value*/ 0).as(Opcode.SET_8, Set.wireFormat8),
+    // SET a FF at address 42
+    new Set(/*indirect*/ 0, /*dstOffset*/ 42, TypeTag.FIELD, /*value*/ Fr.random().toBigInt()).as(
+      Opcode.SET_FF,
+      Set.wireFormatFF,
+    ),
+    // SET a FF at address 43
+    new Set(/*indirect*/ 0, /*dstOffset*/ 43, TypeTag.FIELD, /*value*/ Fr.random().toBigInt()).as(
+      Opcode.SET_FF,
+      Set.wireFormatFF,
+    ),
+  ];
+
+  const preambleSize = encodeToBytecode(preambleInstructions).length;
+
+  const loopBodyInstructions = new Array(2600).fill(
+    new FieldDiv(/*indirect*/ 0, /*aOffset*/ 43, /*bOffset*/ 42, /*dstOffset*/ 201).as(
+      Opcode.FDIV_8,
+      FieldDiv.wireFormat8,
+    ),
+  );
+
+  const buffer = encodeToBytecode([
+    ...preambleInstructions,
+    ...loopBodyInstructions,
+    new Add(/*indirect*/ 0, /*aOffset*/ 3, /*bOffset*/ 1, /*dstOffset*/ 3).as(Opcode.ADD_8, Add.wireFormat8),
+    new Lt(/*indirect*/ 0, /*aOffset*/ 3, /*bOffset*/ 2, /*dstOffset*/ 4).as(Opcode.LT_8, Lt.wireFormat8),
+    new JumpI(/*indirect*/ 0, /*condOffset*/ 4, /*loc*/ preambleSize).as(Opcode.JUMPI_32, JumpI.wireFormat),
+    new Set(/*indirect*/ 0, /*dstOffset*/ 42, TypeTag.UINT32, /* value */ 0).as(Opcode.SET_8, Set.wireFormat8),
+    new Return(/*indirect*/ 0, /*returnSizeAddress*/ 42, /*returnOffset*/ 42).as(Opcode.RETURN, Return.wireFormat),
+  ]);
+
+  return buffer;
+}
+
 it('generates poseidon spam no loop', async () => {
   const base64 = createPoseidonSpamContractNoLoop().toString('base64');
   await writeFile('poseidon_spam_no_loop.bytecode', base64);
@@ -119,6 +180,59 @@ it('executes poseidon spam with loop', async () => {
         address: minimalTestContract.address,
         fnName: 'public_dispatch',
         args: [new Fr(249)],
+      },
+    ],
+    /*teardownCall=*/ undefined,
+    /*feePayer=*/ deployer,
+  );
+  if (!result.revertCode.isOK()) {
+    throw new Error(`Contract execution has reverted: ${result.revertReason?.getMessage()}`);
+  }
+});
+
+it('generates fdiv spam with loop', async () => {
+  const base64 = createFDivSpamContract().toString('base64');
+  await writeFile('fdiv_spam.bytecode', base64);
+});
+
+it('executes fdiv spam with loop', async () => {
+  const deployer = AztecAddress.fromNumber(42);
+
+  const simTester = await PublicTxSimulationTester.create();
+  const spamBytecode = createFDivSpamContract();
+
+  const minimalContractArtifact = emptyContractArtifact();
+  minimalContractArtifact.name = 'MinimalContract';
+  minimalContractArtifact.functions = [emptyFunctionArtifact()];
+  minimalContractArtifact.functions[0].name = 'public_dispatch';
+  minimalContractArtifact.functions[0].functionType = FunctionType.PUBLIC;
+  minimalContractArtifact.functions[0].bytecode = spamBytecode;
+  minimalContractArtifact.functions[0].parameters = [
+    {
+      name: 'n',
+      type: {
+        kind: 'integer',
+        sign: 'unsigned',
+        width: 32,
+      },
+      visibility: 'public',
+    },
+  ];
+
+  const minimalTestContract = await simTester.registerAndDeployContract(
+    /*constructorArgs=*/ [],
+    deployer,
+    /*contractArtifact=*/ minimalContractArtifact,
+  );
+
+  const result = await simTester.simulateTx(
+    /*sender=*/ deployer,
+    /*setupCalls=*/ [],
+    /*appCalls=*/ [
+      {
+        address: minimalTestContract.address,
+        fnName: 'public_dispatch',
+        args: [new Fr(250)],
       },
     ],
     /*teardownCall=*/ undefined,
