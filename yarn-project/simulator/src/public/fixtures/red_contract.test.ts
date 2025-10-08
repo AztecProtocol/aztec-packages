544092bb01e43f9123f2038a66db57da8c57dbe7
sirasistant <sirasistant@gmail.com>
2025-10-08 12:49:50 +0000

    theoretical max poseidons (almost)

--- yarn-project/simulator/src/public/fixtures/red_contract.test.ts
+++ yarn-project/simulator/src/public/fixtures/red_contract.test.ts
@@ -1,9 +1,14 @@
+import { Fr } from '@aztec/foundation/fields';
+import { FunctionType, emptyContractArtifact, emptyFunctionArtifact } from '@aztec/stdlib/abi';
+import { AztecAddress } from '@aztec/stdlib/aztec-address';
+
 import { writeFile } from 'fs/promises';
 
 import { TypeTag } from '../avm/avm_memory_types.js';
 import { Add, CalldataCopy, Cast, Instruction, JumpI, Lt, Poseidon2, Return, Set } from '../avm/opcodes/index.js';
 import { encodeToBytecode } from '../avm/serialization/bytecode_serialization.js';
 import { Opcode } from '../avm/serialization/instruction_serialization.js';
+import { PublicTxSimulationTester } from './public_tx_simulation_tester.js';
 
 function createPoseidonSpamContract() {
   const preambleInstructions = [
@@ -25,12 +30,16 @@ function createPoseidonSpamContract() {
 
   const preambleSize = encodeToBytecode(preambleInstructions).length;
 
-  const buffer = encodeToBytecode([
-    ...preambleInstructions,
+  const loopBodyInstructions = new Array(1000).fill(
     new Poseidon2(/*indirect*/ 0, /*inputStateOffset*/ 100, /*outputStateOffset*/ 100).as(
       Opcode.POSEIDON2,
       Poseidon2.wireFormat,
     ),
+  );
+
+  const buffer = encodeToBytecode([
+    ...preambleInstructions,
+    ...loopBodyInstructions,
     new Add(/*indirect*/ 0, /*aOffset*/ 3, /*bOffset*/ 1, /*dstOffset*/ 3).as(Opcode.ADD_8, Add.wireFormat8),
     new Lt(/*indirect*/ 0, /*aOffset*/ 3, /*bOffset*/ 2, /*dstOffset*/ 4).as(Opcode.LT_8, Lt.wireFormat8),
     new JumpI(/*indirect*/ 0, /*condOffset*/ 4, /*loc*/ preambleSize).as(Opcode.JUMPI_32, JumpI.wireFormat),
@@ -38,8 +47,7 @@ function createPoseidonSpamContract() {
     new Return(/*indirect*/ 0, /*returnSizeAddress*/ 42, /*returnOffset*/ 42).as(Opcode.RETURN, Return.wireFormat),
   ]);
 
-  const base64 = buffer.toString('base64');
-  return base64;
+  return buffer;
 }
 
 function createPoseidonSpamContractNoLoop() {
@@ -60,17 +68,63 @@ function createPoseidonSpamContractNoLoop() {
   );
 
   const buffer = encodeToBytecode(instructions);
-  // Convert buffer to base64
-  const base64 = buffer.toString('base64');
-  return base64;
+  return buffer;
 }
 
 it('generates poseidon spam no loop', async () => {
-  const base64 = createPoseidonSpamContractNoLoop();
+  const base64 = createPoseidonSpamContractNoLoop().toString('base64');
   await writeFile('poseidon_spam_no_loop.bytecode', base64);
 });
 
 it('generates poseidon spam with loop', async () => {
-  const base64 = createPoseidonSpamContract();
+  const base64 = createPoseidonSpamContract().toString('base64');
   await writeFile('poseidon_spam.bytecode', base64);
+});
+
+it('executes poseidon spam with loop', async () => {
+  const deployer = AztecAddress.fromNumber(42);
+
+  const simTester = await PublicTxSimulationTester.create();
+  const spamBytecode = createPoseidonSpamContract();
+
+  const minimalContractArtifact = emptyContractArtifact();
+  minimalContractArtifact.name = 'MinimalContract';
+  minimalContractArtifact.functions = [emptyFunctionArtifact()];
+  minimalContractArtifact.functions[0].name = 'public_dispatch';
+  minimalContractArtifact.functions[0].functionType = FunctionType.PUBLIC;
+  minimalContractArtifact.functions[0].bytecode = spamBytecode;
+  minimalContractArtifact.functions[0].parameters = [
+    {
+      name: 'n',
+      type: {
+        kind: 'integer',
+        sign: 'unsigned',
+        width: 32,
+      },
+      visibility: 'public',
+    },
+  ];
+
+  const minimalTestContract = await simTester.registerAndDeployContract(
+    /*constructorArgs=*/ [],
+    deployer,
+    /*contractArtifact=*/ minimalContractArtifact,
+  );
+
+  const result = await simTester.simulateTx(
+    /*sender=*/ deployer,
+    /*setupCalls=*/ [],
+    /*appCalls=*/ [
+      {
+        address: minimalTestContract.address,
+        fnName: 'public_dispatch',
+        args: [new Fr(249)],
+      },
+    ],
+    /*teardownCall=*/ undefined,
+    /*feePayer=*/ deployer,
+  );
+  if (!result.revertCode.isOK()) {
+    throw new Error(`Contract execution has reverted: ${result.revertReason?.getMessage()}`);
+  }
 });
