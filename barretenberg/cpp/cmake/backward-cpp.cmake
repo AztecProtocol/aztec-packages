88ecc0a09a9517b1542179df7fb16e0a88f51bf2
ludamad <adam.domurad@gmail.com>
2025-10-14 16:16:43 +0100

    chore: enable shallow clones for CMake external dependencies (#17620)

--- barretenberg/cpp/cmake/backward-cpp.cmake
+++ barretenberg/cpp/cmake/backward-cpp.cmake
@@ -1,11 +1,27 @@
 if(ENABLE_STACKTRACES)
-    include(FetchContent)
+    include(ExternalProject)
+
+    set(BACKWARD_PREFIX "${CMAKE_BINARY_DIR}/_deps/backward")
+    set(BACKWARD_INCLUDE "${BACKWARD_PREFIX}/src/backward")
+
+    # Create directory structure for ExternalProject
+    file(MAKE_DIRECTORY ${BACKWARD_INCLUDE})
 
     # Also requires one of: libbfd (gnu binutils), libdwarf, libdw (elfutils)
-    FetchContent_Declare(backward
-        GIT_REPOSITORY https://github.com/bombela/backward-cpp
-        GIT_TAG 51f0700452cf71c57d43c2d028277b24cde32502
-        SYSTEM          # optional, the Backward include directory will be treated as system directory
+    ExternalProject_Add(
+        backward
+        PREFIX ${BACKWARD_PREFIX}
+        # We need to go through some hoops to do a shallow clone of a fixed commit (as opposed to a tag).
+        DOWNLOAD_COMMAND
+            sh -c "mkdir -p ${BACKWARD_PREFIX}/src/backward && cd ${BACKWARD_PREFIX}/src/backward && git init . && (git remote add origin https://github.com/bombela/backward-cpp || true) && git fetch --depth 1 origin 51f0700452cf71c57d43c2d028277b24cde32502 && git checkout FETCH_HEAD"
+        SOURCE_DIR ${BACKWARD_PREFIX}/src/backward
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        UPDATE_COMMAND ""
     )
-    FetchContent_MakeAvailable(backward)
+
+    add_library(Backward::Interface INTERFACE IMPORTED)
+    add_dependencies(Backward::Interface backward)
+    target_include_directories(Backward::Interface SYSTEM INTERFACE ${BACKWARD_INCLUDE})
 endif()
