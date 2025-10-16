9aaf1f0280f368515e15a8b642a58b26b1985fad
thunkar <gregojquiros@gmail.com>
2025-10-16 18:32:31 +0000

    exported missing types

--- yarn-project/aztec.js/src/api/wallet.ts
+++ yarn-project/aztec.js/src/api/wallet.ts
@@ -10,6 +10,10 @@ export {
   type SendOptions,
   type ProfileOptions,
   type ContractInstanceAndArtifact,
+  type BatchedMethod,
+  type BatchableMethods,
+  type BatchedMethodResult,
+  type BatchResults,
 } from '../wallet/index.js';
 
 export { type DeployAccountOptions, DeployAccountMethod } from '../wallet/deploy_account_method.js';
