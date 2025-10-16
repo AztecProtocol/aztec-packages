863ed5b59a1dd09b34ab3777a83bf0dd048e26b0
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2025-10-16 08:14:01 +0000

    Clean up auto

--- barretenberg/cpp/src/barretenberg/multilinear_batching/multilinear_batching_claims.hpp
+++ barretenberg/cpp/src/barretenberg/multilinear_batching/multilinear_batching_claims.hpp
@@ -41,22 +41,42 @@ template <typename Curve> struct MultilinearBatchingVerifierClaim {
         , shifted_commitment(shifted_commitment)
     {}
 
-    MultilinearBatchingVerifierClaim(auto* builder, const auto& native_claim)
+    /**
+     * @brief Constructor for instantiating a recursive claim from a native one
+     *
+     * @tparam RecursiveCurve
+     * @param builder
+     * @param native_claim
+     * @return MultilinearBatchingVerifierClaim
+     */
+    template <typename RecursiveCurve>
+    static MultilinearBatchingVerifierClaim stdlib_from_native(
+        typename RecursiveCurve::Builder* builder,
+        const MultilinearBatchingVerifierClaim<typename RecursiveCurve::NativeCurve>& native_claim)
         requires Curve::is_stdlib_type
-        : shifted_evaluation(FF::from_witness(builder, native_claim.shifted_evaluation))
-        , non_shifted_evaluation(FF::from_witness(builder, native_claim.non_shifted_evaluation))
-        , non_shifted_commitment(Commitment::from_witness(builder, native_claim.non_shifted_commitment))
-        , shifted_commitment(Commitment::from_witness(builder, native_claim.shifted_commitment))
     {
+        MultilinearBatchingVerifierClaim<RecursiveCurve> result;
+
         for (auto& element : native_claim.challenge) {
-            challenge.emplace_back(FF::from_witness(builder, element));
+            result.challenge.emplace_back(FF::from_witness(builder, element));
         }
+
+        result.shifted_evaluation = FF::from_witness(builder, native_claim.shifted_evaluation);
+        result.non_shifted_evaluation = FF::from_witness(builder, native_claim.non_shifted_evaluation);
+        result.non_shifted_commitment = Commitment::from_witness(builder, native_claim.non_shifted_commitment);
+        result.shifted_commitment = Commitment::from_witness(builder, native_claim.shifted_commitment);
+
+        return result;
     }
 
-    auto get_value()
+    /**
+     * @brief Return the native claim underlying the recursive one
+     */
+    template <typename T>
+    T get_value()
         requires Curve::is_stdlib_type
     {
-        MultilinearBatchingVerifierClaim<typename Curve::NativeCurve> native_claim;
+        T native_claim;
         native_claim.challenge.reserve(challenge.size());
 
         for (auto& recursive_challenge : challenge) {
@@ -70,7 +90,10 @@ template <typename Curve> struct MultilinearBatchingVerifierClaim {
         return native_claim;
     }
 
-    FF hash_through_transcript(const std::string& domain_separator, auto& transcript) const
+    /**
+     * @brief Hash the claim via the transcript mechanism
+     */
+    template <typename T> FF hash_through_transcript(const std::string& domain_separator, T& transcript) const
     {
         for (size_t idx = 0; auto& element : challenge) {
             transcript.add_to_independent_hash_buffer(domain_separator + "challenge_" + std::to_string(idx), element);
