f25f3e5f6882e3a6a52f7a692bde874d19b95885
Charlie Lye <5764343+charlielye@users.noreply.github.com>
2025-10-18 16:26:29 +0000

    fields should use sync api

--- yarn-project/foundation/src/fields/fields.ts
+++ yarn-project/foundation/src/fields/fields.ts
@@ -1,4 +1,4 @@
-import { Barretenberg } from '@aztec/bb.js';
+import { BarretenbergSync } from '@aztec/bb.js';
 
 import { inspect } from 'util';
 
@@ -321,8 +321,9 @@ export class Fr extends BaseField {
    * @returns A square root of the field element (null if it does not exist).
    */
   async sqrt(): Promise<Fr | null> {
-    const api = await Barretenberg.initSingleton();
-    const response = await api.bn254FrSqrt({ input: this.toBuffer() });
+    await BarretenbergSync.initSingleton();
+    const api = BarretenbergSync.getSingleton();
+    const response = api.bn254FrSqrt({ input: this.toBuffer() });
     if (!response.isSquareRoot) {
       // Field element is not a quadratic residue mod p so it has no square root.
       return null;
