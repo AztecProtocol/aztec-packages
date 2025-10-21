6f5d33619c3e317aae15cfcd15ff4a4a8abf4f39
LHerskind <16536249+LHerskind@users.noreply.github.com>
2025-10-21 08:47:01 +0000

    feat: coin issuer uses percentage of total supply
    (cherry picked from commit 124fa94caed6e667f5422b6fc3ee3af6052243d3)

--- l1-contracts/src/governance/interfaces/ICoinIssuer.sol
+++ l1-contracts/src/governance/interfaces/ICoinIssuer.sol
@@ -3,6 +3,8 @@
 pragma solidity >=0.8.27;
 
 interface ICoinIssuer {
+  event BudgetReset(uint256 indexed newYear, uint256 newBudget);
+
   function mint(address _to, uint256 _amount) external;
   function acceptTokenOwnership() external;
   function mintAvailable() external view returns (uint256);
