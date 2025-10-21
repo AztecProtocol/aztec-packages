46835e2aae1f36c9f4129c9967531aa4516f0508
Charlie Lye <5764343+charlielye@users.noreply.github.com>
2025-10-21 23:31:01 +0000

    fix

--- yarn-project/native/scripts/copy-modules.sh
+++ yarn-project/native/scripts/copy-modules.sh
@@ -6,5 +6,5 @@ rm -rf build
 
 for variant in amd64-linux arm64-linux amd64-macos arm64-macos; do
   mkdir -p "build/$variant"
-  cp -v "$BB_ROOT/build-zig-node-$variant/lib/nodejs_module.node" "build/$variant/" 2>/dev/null || echo "Warning: $variant not found"
+  cp -v "$BB_ROOT/build-zig-$variant/lib/nodejs_module.node" "build/$variant/" 2>/dev/null || echo "Warning: $variant not found"
 done
