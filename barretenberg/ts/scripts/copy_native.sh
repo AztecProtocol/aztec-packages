ae8c6ad4867852a7d47edca39418b6d49bcc8573
Charlie Lye <5764343+charlielye@users.noreply.github.com>
2025-10-21 19:22:21 +0000

    wip

--- barretenberg/ts/scripts/copy_native.sh
+++ barretenberg/ts/scripts/copy_native.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # Copies native bb binary and napi module to dest.
 set -e
+NO_CD=1 source $(git rev-parse --show-toplevel)/ci3/source
 
 cd $(dirname $0)/..
 
@@ -15,7 +16,7 @@ if semver check "$REF_NAME" && [[ "$(arch)" == "amd64" ]]; then
   for arch in arm64-linux amd64-macos arm64-macos; do
     mkdir -p ./build/$arch
     cp ../cpp/build-zig-$arch/bin/bb ./build/$arch
-    cp ../cpp/build-zig-node-$arch/lib/nodejs_module.node ./build/$arch
+    cp ../cpp/build-zig-$arch/lib/nodejs_module.node ./build/$arch
   done
 
   llvm-strip-20 ./build/*/*
