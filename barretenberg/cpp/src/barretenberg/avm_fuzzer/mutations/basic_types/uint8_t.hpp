58dab6eee12b0ba3e89342892f4da13e37c18a0e
defkit <84741533+defkit@users.noreply.github.com>
2025-10-22 12:06:46 +0300

    feat(AVM_fuzzing): build for avm fuzzer + smol formatting fix (#17861)
    Temporary (i hope) solution so that it could at least be built somehow

--- barretenberg/cpp/src/barretenberg/avm_fuzzer/mutations/basic_types/uint8_t.hpp
+++ barretenberg/cpp/src/barretenberg/avm_fuzzer/mutations/basic_types/uint8_t.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
-#include <cstdint>
-#include <random>
 #include "barretenberg/avm_fuzzer/mutations/configuration.hpp"
+#include <random>
 
 uint8_t generate_random_uint8(std::mt19937_64& rng);
 void mutate_uint8_t(uint8_t& value, std::mt19937_64& rng, const Uint8MutationConfig& config);
