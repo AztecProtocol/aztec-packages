ced51db8a1134afe5f5ac54730a89389cb586131
Charlie Lye <5764343+charlielye@users.noreply.github.com>
2025-10-22 08:23:10 +0000

    fix

--- barretenberg/ts/scripts/copy_native.sh
+++ barretenberg/ts/scripts/copy_native.sh
@@ -5,7 +5,7 @@ NO_CD=1 source $(git rev-parse --show-toplevel)/ci3/source
 
 cd $(dirname $0)/..
 
-if semver check "$REF_NAME" && [[ "$(arch)" == "amd64" ]]; then
+if semver check "${REF_NAME:-}" && [[ "$(arch)" == "amd64" ]]; then
   # We're building a release.
   # We take host build for amd64-linux.
   mkdir -p ./build/amd64-linux
