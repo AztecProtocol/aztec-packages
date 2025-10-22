15095594c938b7d1dc700571c8361d7bfe4d28b6
DanielKotov <katod.daniel@gmail.com>
2025-10-22 16:30:54 +0300

    issues with tests

--- barretenberg/cpp/src/barretenberg/noir_programs_boomerang_values/graph_description_noir.test.cpp
+++ barretenberg/cpp/src/barretenberg/noir_programs_boomerang_values/graph_description_noir.test.cpp
@@ -12,14 +12,19 @@ using namespace cdg;
 static std::string common_preffix =
     "/home/dkidot/aztec/aztec-packages/barretenberg/cpp/src/barretenberg/noir_programs_boomerang_values/noir_programs/";
 
-
-void test_acir(std::vector<uint8_t>& bytecode) {
-    auto tool = StaticAnalyzerAcir(bytecode);
-    auto [variables_in_one_gate, unconstrained_vars] = tool.analyze_acir();
-    EXPECT_EQ(variables_in_one_gate.size(), 0);
-    if (variables_in_one_gate.size() > 0) {
-        info("print variables_in_one_gate");
-        for (const auto& elem: variables_in_one_gate) {
+void test_acir_circuit_builder(std::vector<uint8_t>& acir_program_buf)
+{
+    auto constraint_systems = acir_format::program_buf_to_acir_format(std::move(acir_program_buf));
+    const acir_format::AcirFormat constraint_system = constraint_systems.at(0);
+    acir_format::AcirProgram program{ constraint_system };
+    bb::UltraCircuitBuilder builder = acir_format::create_circuit(program);
+    auto tool = StaticAnalyzer(builder);
+    auto test_result = tool.analyze_circuit();
+    EXPECT_EQ(test_result.first.size(), 1);
+    EXPECT_EQ(test_result.second.size(), 0);
+    auto variables_in_one_gate = test_result.second;
+    if (test_result.second.size() > 0) {
+        for (const auto& elem : test_result.second) {
             info("elem == ", elem);
         }
     }
@@ -31,9 +36,7 @@ void test_acir(std::vector<uint8_t>& bytecode) {
     }
 }
 
-
-TEST(BoomerangAcirCircuitBuilder, InitCase)
-{
+TEST(BoomerangAcirCircuitBuilder, InitCase) {
     std::string init_bytecode_path = "init/target/init.json";
     std::string bytecode_file = common_preffix + init_bytecode_path;
     std::vector<uint8_t> vector_bytecode = get_bytecode_from_json(bytecode_file);
