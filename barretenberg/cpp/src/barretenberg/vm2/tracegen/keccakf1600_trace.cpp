9ac076e327aad3731734f852352e99f02d9c9fb9
jeanmon <jean@aztec-labs.com>
2025-10-23 12:18:12 +0000

    Precomputed inverses should be stored in a static const variable

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/keccakf1600_trace.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/keccakf1600_trace.cpp
@@ -390,12 +390,14 @@ std::array<FF, AVM_KECCAKF1600_STATE_SIZE + 1> generate_precomputed_inverses()
     return precomputed_inverses;
 }
 
-} // namespace
-
-KeccakF1600TraceBuilder::KeccakF1600TraceBuilder()
+// Get inverse for a given index using static precomputed inverses.
+const FF& get_precomputed_inverse(size_t index)
 {
-    precomputed_inverses = generate_precomputed_inverses();
+    static const std::array<FF, AVM_KECCAKF1600_STATE_SIZE + 1> precomputed_inverses = generate_precomputed_inverses();
+    return precomputed_inverses.at(index);
 }
+
+} // namespace
 
 // Populate a memory slice read or write operation for the Keccak permutation.
 void KeccakF1600TraceBuilder::process_single_slice(const simulation::KeccakF1600Event& event,
@@ -450,9 +452,9 @@ void KeccakF1600TraceBuilder::process_single_slice(const simulation::KeccakF1600
                 { C::keccak_memory_sel, 1 },
                 { C::keccak_memory_clk, event.execution_clk },
                 { C::keccak_memory_ctr, i + 1 },
-                { C::keccak_memory_ctr_inv, precomputed_inverses.at(i + 1) },
+                { C::keccak_memory_ctr_inv, get_precomputed_inverse(i + 1) },
                 { C::keccak_memory_state_size_min_ctr_inv,
-                  precomputed_inverses.at(AVM_KECCAKF1600_STATE_SIZE - i - 1) },
+                  get_precomputed_inverse(AVM_KECCAKF1600_STATE_SIZE - i - 1) },
                 { C::keccak_memory_start_read, (i == 0 && !write) ? 1 : 0 },
                 { C::keccak_memory_start_write, (i == 0 && write) ? 1 : 0 },
                 { C::keccak_memory_ctr_end, i == AVM_KECCAKF1600_STATE_SIZE - 1 ? 1 : 0 },
@@ -555,7 +557,7 @@ void KeccakF1600TraceBuilder::process_permutation(
                           { C::keccakf1600_dst_addr, event.dst_addr },
                           { C::keccakf1600_sel_no_error, error ? 0 : 1 },
                           { C::keccakf1600_space_id, event.space_id },
-                          { C::keccakf1600_round_inv, precomputed_inverses.at(round_idx + 1) },
+                          { C::keccakf1600_round_inv, get_precomputed_inverse(round_idx + 1) },
                       } });
 
             // When no out-of-range value occured but a tag value error, we
