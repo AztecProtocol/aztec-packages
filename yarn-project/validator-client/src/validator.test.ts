258020f2edca4c0447ad0f9499e5aaa8954adfbc
Santiago Palladino <santiago@aztecprotocol.com>
2025-10-27 09:59:54 -0300

    fix: Do not allow txs in proposals if disableTransactions is set (#17882) (#17947)
    Reuses the `disableTransactions` flag used in p2p to also reject block
    proposals that have non-empty tx hashes.
    
    Backport of #17882

--- yarn-project/validator-client/src/validator.test.ts
+++ yarn-project/validator-client/src/validator.test.ts
@@ -39,7 +39,8 @@ import { type ValidatorClientConfig, validatorClientConfigMappings } from './con
 import { ValidatorClient } from './validator.js';
 
 describe('ValidatorClient', () => {
-  let config: ValidatorClientConfig & Pick<SlasherConfig, 'slashBroadcastedInvalidBlockPenalty'>;
+  let config: ValidatorClientConfig &
+    Pick<SlasherConfig, 'slashBroadcastedInvalidBlockPenalty'> & { disableTransactions: boolean };
   let validatorClient: ValidatorClient;
   let p2pClient: MockProxy<P2P>;
   let blockSource: MockProxy<L2BlockSource>;
@@ -76,6 +77,7 @@ describe('ValidatorClient', () => {
       validatorReexecute: false,
       validatorReexecuteDeadlineMs: 6000,
       slashBroadcastedInvalidBlockPenalty: 1n,
+      disableTransactions: false,
     };
 
     const keyStore: KeyStore = {
