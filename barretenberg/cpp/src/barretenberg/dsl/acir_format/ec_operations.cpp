bad1c0e8742d2d790183674d3f3d881976d78e43
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2025-10-29 12:02:16 +0000

    Refactoring

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/ec_operations.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/ec_operations.cpp
@@ -16,16 +16,12 @@ namespace acir_format {
 /**
  * @brief Create constraints for addition of two points on the Grumpkin curve.
  *
- * @details We proceed in 5 steps:
+ * @details We proceed in 2 steps:
  * 1. We reconstruct the Grumpkin points input1, input2 and input_result for which we must check input1 + input2 =
- *    input_result. NOTE: This step does not enforce that input1 and input2 are valid points on Grumpkin.
- * 2. If we are in write_vk mode (the builder was constructed without a valid assignment of a witness vector), we
- *    populate the fields of input1, input2 and input_result with dummy data.
- * 3. If the predicate is not constant, we conditionally assign the values of input1 and input2 to avoid failures when
- *    the constraint appears in an inactive branch (predicate is witness false). When the predicate is witness false, we
- *    set input1 = input2 equal to the generator of the Grumpkin curve.
- * 4. We check that input1 and input2 are valid points on the Grumpkin curve.
- * 5. We compute input1 + input2 and check that it agrees with input_result.
+ *    input_result. The reconstruction handles all cases: has_valid_witness_assignments equal to false (write_vk
+ *    scenario) and a witness predicate. If either has_valid_witness_assignments if false or the predicate is witness
+ *    false, we set input1 and input2 to be the generator of Grumpkin.
+ * 2. We compute input1 + input2 and check that it agrees with input_result.
  *
  * @tparam Builder
  * @param builder
@@ -42,40 +38,19 @@ void create_ec_add_constraint(Builder& builder, const EcAdd& input, bool has_val
     // Step 1.
     field_ct input_result_x = field_ct::from_witness_index(&builder, input.result_x);
     field_ct input_result_y = field_ct::from_witness_index(&builder, input.result_y);
-    bool_ct input_result_infinite = static_cast<bool_ct>(field_ct::from_witness_index(&builder, input.result_infinite));
-    bool_ct predicate; // To be instantiated in Step 3 if needed.
+    bool_ct input_result_infinite = bool_ct(field_ct::from_witness_index(&builder, input.result_infinite));
+    bool_ct predicate = bool_ct(to_field_ct(input.predicate, builder));
 
-    cycle_group_ct input1 = to_grumpkin_point_unsafe(builder, input.input1_x, input.input1_y, input.input1_infinite);
-    cycle_group_ct input2 = to_grumpkin_point_unsafe(builder, input.input2_x, input.input2_y, input.input2_infinite);
+    cycle_group_ct input1_point = to_grumpkin_point(
+        input.input1_x, input.input1_y, input.input1_infinite, has_valid_witness_assignments, predicate, builder);
+    cycle_group_ct input2_point = to_grumpkin_point(
+        input.input2_x, input.input2_y, input.input2_infinite, has_valid_witness_assignments, predicate, builder);
     cycle_group_ct input_result(input_result_x, input_result_y, input_result_infinite, /*assert_on_curve=*/true);
 
     // Step 2.
-    if (!has_valid_witness_assignments) {
-        create_dummy_ec_add_constraint(builder, input1, input2, input_result);
-    }
+    cycle_group_ct result = input1_point + input2_point;
 
-    // Step 3.
-    if (!input.predicate.is_constant) {
-        predicate = static_cast<bool_ct>(to_field_ct(input.predicate, builder));
-
-        // Note that we do not need to assign input_result because for an honest user it passed by Noir and is always a
-        // point on the curve.
-        cycle_group_ct affine_one(bb::grumpkin::g1::affine_one);
-        input1 = cycle_group_ct::conditional_assign(predicate, input1, affine_one);
-        input2 = cycle_group_ct::conditional_assign(predicate, input2, affine_one);
-    } else {
-        BB_ASSERT(input.predicate.value, "Creating EcAdd constraints with a constant predicate equal to false.");
-    }
-
-    // Step 4.
-    // AUDITTODO: Do we want also check that the coordinates are smaller than the field modulus?
-    input1.validate_on_curve();
-    input2.validate_on_curve();
-
-    // Step 5.
-    cycle_group_ct result = input1 + input2;
-
-    if (!input.predicate.is_constant) {
+    if (!predicate.is_constant()) {
         cycle_group_ct to_be_asserted_equal = cycle_group_ct::conditional_assign(predicate, input_result, result);
         result.assert_equal(to_be_asserted_equal);
     } else {
@@ -88,40 +63,11 @@ void create_ec_add_constraint(Builder& builder, const EcAdd& input, bool has_val
     }
 }
 
-template <typename Builder>
-void create_dummy_ec_add_constraint(Builder& builder,
-                                    const bb::stdlib::cycle_group<Builder>& input1,
-                                    const bb::stdlib::cycle_group<Builder>& input2,
-                                    const bb::stdlib::cycle_group<Builder>& input_result)
-{
-    auto affine_one = bb::grumpkin::g1::affine_one;
-
-    for (auto const& input : { input1, input2, input_result }) {
-        if (!input.is_constant()) {
-            builder.set_variable(input.x().get_witness_index(), affine_one.x);
-            builder.set_variable(input.y().get_witness_index(), affine_one.y);
-            builder.set_variable(input.is_point_at_infinity().get_witness_index(), false);
-        }
-    }
-}
-
 template void create_ec_add_constraint<bb::UltraCircuitBuilder>(bb::UltraCircuitBuilder& builder,
                                                                 const EcAdd& input,
                                                                 bool has_valid_witness_assignments);
 template void create_ec_add_constraint<bb::MegaCircuitBuilder>(bb::MegaCircuitBuilder& builder,
                                                                const EcAdd& input,
                                                                bool has_valid_witness_assignments);
-
-template void create_dummy_ec_add_constraint<bb::UltraCircuitBuilder>(
-    bb::UltraCircuitBuilder& builder,
-    const bb::stdlib::cycle_group<bb::UltraCircuitBuilder>& input1,
-    const bb::stdlib::cycle_group<bb::UltraCircuitBuilder>& input2,
-    const bb::stdlib::cycle_group<bb::UltraCircuitBuilder>& input_result);
-
-template void create_dummy_ec_add_constraint<bb::MegaCircuitBuilder>(
-    bb::MegaCircuitBuilder& builder,
-    const bb::stdlib::cycle_group<bb::MegaCircuitBuilder>& input1,
-    const bb::stdlib::cycle_group<bb::MegaCircuitBuilder>& input2,
-    const bb::stdlib::cycle_group<bb::MegaCircuitBuilder>& input_result);
 
 } // namespace acir_format
