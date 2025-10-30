cb98333fbf322099d4148d1374f2ebfd1264d326
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2025-10-30 12:55:20 +0000

    Add remaining tests, define class for input constancy

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/ec_operations.test.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/ec_operations.test.cpp
@@ -11,11 +11,18 @@
 
 using namespace ::acir_format;
 
+enum class InputConstancy : uint8_t { None, Input1, Input2, Both };
+
 /**
- * @brief Testing functions to generate the EcOperationTest test suite. WitnessOrConstants specifies which inputs to the
- * constraints should be constants: 0 : None, 1 : input1, 2 : input2, 3 : both inputs.
+ * @brief Testing functions to generate the EcOperationTest test suite. Constancy specifies which inputs to the
+ * constraints should be constant.
+ *
+ * @details Edge cases for point addition on Grumpkin are tested in cycle_group. Here we test that:
+ * 1. If input1 + input2 != result, then the circuit fails (TamperingMode::Result)
+ * 2. If the inputs are not valid points on Grumpkin, or input1 + input2 != result, but the predicate is witness false,
+ *    then the circuit is satisfied.
  */
-template <typename Builder_, uint8_t WitnessOrConstants> class EcOperationsTestingFunctions {
+template <typename Builder_, InputConstancy Constancy> class EcOperationsTestingFunctions {
   public:
     using Builder = Builder_;
     using AcirConstraint = EcAdd;
@@ -49,7 +56,7 @@ template <typename Builder_, uint8_t WitnessOrConstants> class EcOperationsTesti
         GrumpkinPoint result = input1 + input2;
         BB_ASSERT(result != GrumpkinPoint::one()); // Ensure that tampering works correctly
 
-        if constexpr (WitnessOrConstants == 0) {
+        if constexpr (Constancy == InputConstancy::None) {
             std::vector<uint32_t> input1_indices = add_to_witness_and_track_indices(witness_values, input1);
             std::vector<uint32_t> input2_indices = add_to_witness_and_track_indices(witness_values, input2);
             std::vector<uint32_t> result_indices = add_to_witness_and_track_indices(witness_values, result);
@@ -69,7 +76,7 @@ template <typename Builder_, uint8_t WitnessOrConstants> class EcOperationsTesti
                 .result_y = result_indices[1],
                 .result_infinite = result_indices[2],
             };
-        } else if constexpr (WitnessOrConstants == 1) {
+        } else if constexpr (Constancy == InputConstancy::Input1) {
             std::vector<uint32_t> input2_indices = add_to_witness_and_track_indices(witness_values, input2);
             std::vector<uint32_t> result_indices = add_to_witness_and_track_indices(witness_values, result);
 
@@ -88,7 +95,7 @@ template <typename Builder_, uint8_t WitnessOrConstants> class EcOperationsTesti
                 .result_y = result_indices[1],
                 .result_infinite = result_indices[2],
             };
-        } else if constexpr (WitnessOrConstants == 2) {
+        } else if constexpr (Constancy == InputConstancy::Input2) {
             std::vector<uint32_t> input1_indices = add_to_witness_and_track_indices(witness_values, input1);
             std::vector<uint32_t> result_indices = add_to_witness_and_track_indices(witness_values, result);
 
@@ -107,7 +114,7 @@ template <typename Builder_, uint8_t WitnessOrConstants> class EcOperationsTesti
                 .result_y = result_indices[1],
                 .result_infinite = result_indices[2],
             };
-        } else if constexpr (WitnessOrConstants == 3) {
+        } else if constexpr (Constancy == InputConstancy::Both) {
             std::vector<uint32_t> result_indices = add_to_witness_and_track_indices(witness_values, result);
 
             uint32_t predicate_index = static_cast<uint32_t>(witness_values.size());
@@ -135,7 +142,7 @@ template <typename Builder_, uint8_t WitnessOrConstants> class EcOperationsTesti
         switch (witness_override) {
         case WitnessOverride::Case::Input1: {
             // Invalidate the first input by adding 1 to x coordinate
-            if constexpr (WitnessOrConstants == 0 || WitnessOrConstants == 2) {
+            if constexpr (Constancy == InputConstancy::None || Constancy == InputConstancy::Input2) {
                 witness_values[constraint.input1_x.index] += bb::fr(1);
             } else {
                 constraint.input1_x = WitnessOrConstant<FF>::from_constant(constraint.input1_x.value + bb::fr(1));
@@ -144,7 +151,7 @@ template <typename Builder_, uint8_t WitnessOrConstants> class EcOperationsTesti
         }
         case WitnessOverride::Case::Input2: {
             // Invalidate the second input by adding 1 to x coordinate
-            if constexpr (WitnessOrConstants == 0 || WitnessOrConstants == 1) {
+            if constexpr (Constancy == InputConstancy::None || Constancy == InputConstancy::Input1) {
                 witness_values[constraint.input2_x.index] += bb::fr(1);
             } else {
                 constraint.input2_x = WitnessOrConstant<FF>::from_constant(constraint.input2_x.value + bb::fr(1));
@@ -177,29 +184,33 @@ template <typename Builder_, uint8_t WitnessOrConstants> class EcOperationsTesti
 };
 
 template <typename Builder>
-class EcOperationsTestsNoneConstant : public ::testing::Test,
-                                      public TestClassWithPredicate<EcOperationsTestingFunctions<Builder, 0>> {
+class EcOperationsTestsNoneConstant
+    : public ::testing::Test,
+      public TestClassWithPredicate<EcOperationsTestingFunctions<Builder, InputConstancy::None>> {
   protected:
     static void SetUpTestSuite() { bb::srs::init_file_crs_factory(bb::srs::bb_crs_path()); }
 };
 
 template <typename Builder>
-class EcOperationsTestsInput1Constant : public ::testing::Test,
-                                        public TestClassWithPredicate<EcOperationsTestingFunctions<Builder, 1>> {
+class EcOperationsTestsInput1Constant
+    : public ::testing::Test,
+      public TestClassWithPredicate<EcOperationsTestingFunctions<Builder, InputConstancy::Input1>> {
   protected:
     static void SetUpTestSuite() { bb::srs::init_file_crs_factory(bb::srs::bb_crs_path()); }
 };
 
 template <typename Builder>
-class EcOperationsTestsInput2Constant : public ::testing::Test,
-                                        public TestClassWithPredicate<EcOperationsTestingFunctions<Builder, 2>> {
+class EcOperationsTestsInput2Constant
+    : public ::testing::Test,
+      public TestClassWithPredicate<EcOperationsTestingFunctions<Builder, InputConstancy::Input2>> {
   protected:
     static void SetUpTestSuite() { bb::srs::init_file_crs_factory(bb::srs::bb_crs_path()); }
 };
 
 template <typename Builder>
-class EcOperationsTestsBothConstant : public ::testing::Test,
-                                      public TestClassWithPredicate<EcOperationsTestingFunctions<Builder, 3>> {
+class EcOperationsTestsBothConstant
+    : public ::testing::Test,
+      public TestClassWithPredicate<EcOperationsTestingFunctions<Builder, InputConstancy::Both>> {
   protected:
     static void SetUpTestSuite() { bb::srs::init_file_crs_factory(bb::srs::bb_crs_path()); }
 };
@@ -229,10 +240,10 @@ TYPED_TEST(EcOperationsTestsNoneConstant, WitnessTrue)
     TestFixture::test_witness_true(TestFixture::TamperingMode::Result);
 }
 
-TYPED_TEST(EcOperationsTestsNoneConstant, WitnessFalse)
+TYPED_TEST(EcOperationsTestsNoneConstant, WitnessFalseSlow)
 {
     BB_DISABLE_ASSERTS();
-    TestFixture::test_witness_false(TestFixture::TamperingMode::Result);
+    TestFixture::test_witness_false_slow(TestFixture::TamperingMode::Result);
 }
 
 TYPED_TEST(EcOperationsTestsInput1Constant, GenerateVKFromConstraints)
@@ -253,8 +264,56 @@ TYPED_TEST(EcOperationsTestsInput1Constant, WitnessTrue)
     TestFixture::test_witness_true(TestFixture::TamperingMode::Result);
 }
 
-TYPED_TEST(EcOperationsTestsInput1Constant, WitnessFalse)
+TYPED_TEST(EcOperationsTestsInput1Constant, WitnessFalseSlow)
 {
     BB_DISABLE_ASSERTS();
-    TestFixture::test_witness_false(TestFixture::TamperingMode::Result);
+    TestFixture::test_witness_false_slow(TestFixture::TamperingMode::Result);
+}
+
+TYPED_TEST(EcOperationsTestsInput2Constant, GenerateVKFromConstraints)
+{
+    using Flavor = std::conditional_t<std::is_same_v<TypeParam, UltraCircuitBuilder>, UltraFlavor, MegaFlavor>;
+    TestFixture::template test_vk_independence<Flavor>();
+}
+
+TYPED_TEST(EcOperationsTestsInput2Constant, ConstantTrue)
+{
+    BB_DISABLE_ASSERTS();
+    TestFixture::test_constant_true(TestFixture::TamperingMode::Result);
+}
+
+TYPED_TEST(EcOperationsTestsInput2Constant, WitnessTrue)
+{
+    BB_DISABLE_ASSERTS();
+    TestFixture::test_witness_true(TestFixture::TamperingMode::Result);
+}
+
+TYPED_TEST(EcOperationsTestsInput2Constant, WitnessFalseSlow)
+{
+    BB_DISABLE_ASSERTS();
+    TestFixture::test_witness_false_slow(TestFixture::TamperingMode::Result);
+}
+
+TYPED_TEST(EcOperationsTestsBothConstant, GenerateVKFromConstraints)
+{
+    using Flavor = std::conditional_t<std::is_same_v<TypeParam, UltraCircuitBuilder>, UltraFlavor, MegaFlavor>;
+    TestFixture::template test_vk_independence<Flavor>();
+}
+
+TYPED_TEST(EcOperationsTestsBothConstant, ConstantTrue)
+{
+    BB_DISABLE_ASSERTS();
+    TestFixture::test_constant_true(TestFixture::TamperingMode::Result);
+}
+
+TYPED_TEST(EcOperationsTestsBothConstant, WitnessTrue)
+{
+    BB_DISABLE_ASSERTS();
+    TestFixture::test_witness_true(TestFixture::TamperingMode::Result);
+}
+
+TYPED_TEST(EcOperationsTestsBothConstant, WitnessFalseSlow)
+{
+    BB_DISABLE_ASSERTS();
+    TestFixture::test_witness_false_slow(TestFixture::TamperingMode::Result);
 }
