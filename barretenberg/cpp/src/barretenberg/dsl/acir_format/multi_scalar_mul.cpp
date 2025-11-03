32290e8a341ae3f5b7cfa6b5dbece46d96f0ebba
ledwards2225 <l.edwards.d@gmail.com>
2025-11-03 15:14:42 +0000

    comments in msm

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/multi_scalar_mul.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/multi_scalar_mul.cpp
@@ -23,6 +23,28 @@ template <typename Builder> struct MsmInputs {
     std::vector<typename bb::stdlib::cycle_group<Builder>::cycle_scalar> scalars;
 };
 
+/**
+ * @brief Create constraints for multi-scalar multiplication on the Grumpkin curve.
+ *
+ * @details We proceed in 2 steps:
+ * 1. We reconstruct the Grumpkin points, scalars, and input_result for which we must check
+ *    sum(scalars[i] * points[i]) = input_result. The reconstruction handles all cases: has_valid_witness_assignments
+ *    equal to false (write_vk scenario) and a witness predicate. If:
+ *      - has_valid_witness_assignments is false, then we set all points, scalars, and input_result to dummy values
+ *        (generator point and scalar 1)
+ *      - the predicate is witness false, we set all input points and scalars to dummy values.
+ * 2. We compute the multi-scalar multiplication and check that it agrees with input_result.
+ *
+ * @note We do not need to enforce in-circuit that input_result is on the curve because we check that input_result is
+ * equal to result, which we know is on the curve as it is the result of batch_mul on valid curve points. In the case
+ * of predicate equal to witness false, the constraint is supposed to be inactive, so even if input_result is not
+ * checked to be on the curve in this case, it is OK.
+ *
+ * @tparam Builder
+ * @param builder
+ * @param constraint_input The MSM constraint containing witness indices and constants for points and scalars
+ * @param has_valid_witness_assignments Whether valid witnesses are provided (false during VK generation)
+ */
 template <typename Builder>
 void create_multi_scalar_mul_constraint(Builder& builder,
                                         const MultiScalarMul& constraint_input,
@@ -33,7 +55,7 @@ void create_multi_scalar_mul_constraint(Builder& builder,
     // Step 1: Reconstruct inputs
     MsmInputs input = reconstruct_msm_inputs(builder, constraint_input, has_valid_witness_assignments);
 
-    // Step 2: Compute and check result
+    // Step 2: Compute result and connect it to the expected result reconstructed from inputs
     auto result = cycle_group_ct::batch_mul(input.points, input.scalars).get_standard_form();
     cycle_group_ct to_be_asserted_equal = cycle_group_ct::conditional_assign(input.predicate, input.result, result);
     result.assert_equal(to_be_asserted_equal);
@@ -67,6 +89,7 @@ static MsmInputs<Builder> reconstruct_msm_inputs(Builder& builder,
     field_ct input_result_y = field_ct::from_witness_index(&builder, input.out_point_y);
     bool_ct input_result_infinite = bool_ct(field_ct::from_witness_index(&builder, input.out_point_is_infinite));
 
+    // If no valid witness assignments, set result to generator point to avoid errors during circuit construction
     if (!has_valid_witness_assignments) {
         builder.set_variable(input_result_x.get_witness_index(), bb::grumpkin::g1::affine_one.x);
         builder.set_variable(input_result_y.get_witness_index(), bb::grumpkin::g1::affine_one.y);
@@ -80,6 +103,9 @@ static MsmInputs<Builder> reconstruct_msm_inputs(Builder& builder,
     // Reconstruct points and scalars
     std::vector<cycle_group_ct> points;
     std::vector<cycle_scalar_ct> scalars;
+
+    // Ensure that the number of points and scalars are consistent (3 field elements per point, 2 per scalar)
+    BB_ASSERT(input.points.size() * 2 == input.scalars.size() * 3, "MultiScalarMul input size mismatch");
 
     for (size_t i = 0; i < input.points.size(); i += 3) {
         cycle_group_ct input_point = to_grumpkin_point(input.points[i],
