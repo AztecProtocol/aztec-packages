03f0543bd2a39d1281d8ee341d66164cef6162ee
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2025-11-04 18:45:31 +0000

    fox

--- barretenberg/sol/src/honk/optimised/blake-opt.sol
+++ barretenberg/sol/src/honk/optimised/blake-opt.sol
@@ -11,7 +11,7 @@ uint256 constant NUMBER_UNSHIFTED = 36;
 uint256 constant NUMBER_TO_BE_SHIFTED = 5;
 uint256 constant PAIRING_POINTS_SIZE = 16;
 
-uint256 constant VK_HASH = 0x1981d022ae23f25c02cf445175f26defa26ed4917a721cf063a7478617894a41;
+uint256 constant VK_HASH = 0x23470b638ec3a004e3bb8a747e81abd8fdb31481b03622275a57470672d2d66a;
 uint256 constant CIRCUIT_SIZE = 32768;
 uint256 constant LOG_N = 15;
 uint256 constant NUMBER_PUBLIC_INPUTS = 20;
@@ -1208,32 +1208,16 @@ contract BlakeOptHonkVerifier is IVerifier {
 
                 prev_challenge := mod(keccak256(0x00, 0xa0), p)
                 mstore(0x00, prev_challenge)
-                let alpha_0 := and(prev_challenge, LOWER_128_MASK)
-                let alpha_1 := shr(128, prev_challenge)
-                mstore(ALPHA_CHALLENGE_0, alpha_0)
-                mstore(ALPHA_CHALLENGE_1, alpha_1)
+                let alpha := and(prev_challenge, LOWER_128_MASK)
+                mstore(ALPHA_CHALLENGE_0, alpha)
 
-                // For number of alphas / 2 ( 26 /2 )
-                let alpha_off_set := ALPHA_CHALLENGE_2
-                for {} lt(alpha_off_set, ALPHA_CHALLENGE_26) {} {
-                    prev_challenge := mod(keccak256(0x00, 0x20), p)
-                    mstore(0x00, prev_challenge)
-
-                    let alpha_even := and(prev_challenge, LOWER_128_MASK)
-                    let alpha_odd := shr(128, prev_challenge)
-
-                    mstore(alpha_off_set, alpha_even)
-                    mstore(add(alpha_off_set, 0x20), alpha_odd)
-
-                    alpha_off_set := add(alpha_off_set, 0x40)
+                // Compute powers of alpha: alpha^2, alpha^3, ..., alpha^26
+                let alpha_off_set := ALPHA_CHALLENGE_1
+                for {} lt(alpha_off_set, add(ALPHA_CHALLENGE_26, 0x20)) {} {
+                    let prev_alpha := mload(sub(alpha_off_set, 0x20))
+                    mstore(alpha_off_set, mulmod(prev_alpha, alpha, p))
+                    alpha_off_set := add(alpha_off_set, 0x20)
                 }
-
-                // The final alpha challenge
-                prev_challenge := mod(keccak256(0x00, 0x20), p)
-                mstore(0x00, prev_challenge)
-
-                let alpha_26 := and(prev_challenge, LOWER_128_MASK)
-                mstore(ALPHA_CHALLENGE_26, alpha_26)
 
                 /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
                 /*                       GATE CHALLENGES                      */
