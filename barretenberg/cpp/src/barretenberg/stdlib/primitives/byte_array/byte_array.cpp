110524f1c9a03868c3a3063902358814086d70ec
iakovenkos <sergey.s.yakovenko@gmail.com>
2025-11-06 09:52:12 +0000

    fix byte array asserts (addressed in post-audit pr already)

--- barretenberg/cpp/src/barretenberg/stdlib/primitives/byte_array/byte_array.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/primitives/byte_array/byte_array.cpp
@@ -277,7 +277,7 @@ template <typename Builder> byte_array<Builder>& byte_array<Builder>::write_at(b
  */
 template <typename Builder> byte_array<Builder> byte_array<Builder>::slice(size_t offset) const
 {
-    BB_ASSERT_DEBUG(offset < values.size());
+    BB_ASSERT_LTE(offset, values.size());
     return byte_array(context, bytes_t(values.begin() + static_cast<ptrdiff_t>(offset), values.end()));
 }
 
@@ -287,7 +287,7 @@ template <typename Builder> byte_array<Builder> byte_array<Builder>::slice(size_
  **/
 template <typename Builder> byte_array<Builder> byte_array<Builder>::slice(size_t offset, size_t length) const
 {
-    BB_ASSERT_DEBUG(offset < values.size());
+    BB_ASSERT_LTE(offset, values.size());
     // it's <= cause vector constructor doesn't include end point
     BB_ASSERT_DEBUG(length <= values.size() - offset);
     auto start = values.begin() + static_cast<ptrdiff_t>(offset);
