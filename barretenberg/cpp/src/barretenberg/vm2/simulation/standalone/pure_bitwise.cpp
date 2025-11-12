62f8d479c785734c6a0d2b06eb0603b82edc6fb4
Ilyas Ridhuan <ilyas@aztecprotocol.com>
2025-11-12 13:11:17 +0000

    fix(avm): pure bitwise try_catch (#18358)
    Need to handle tag mismatches gracefully in the standalone simulator

--- barretenberg/cpp/src/barretenberg/vm2/simulation/standalone/pure_bitwise.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/simulation/standalone/pure_bitwise.cpp
@@ -1,23 +1,34 @@
 #include "barretenberg/vm2/simulation/standalone/pure_bitwise.hpp"
 
-#include "barretenberg/numeric/uint128/uint128.hpp"
-#include "barretenberg/numeric/uint256/uint256.hpp"
-#include "barretenberg/vm2/common/field.hpp"
+#include "barretenberg/vm2/common/memory_types.hpp"
 #include "barretenberg/vm2/common/tagged_value.hpp"
+#include "barretenberg/vm2/simulation/interfaces/bitwise.hpp"
 
 namespace bb::avm2::simulation {
 
 MemoryValue PureBitwise::and_op(const MemoryValue& a, const MemoryValue& b)
 {
-    return a & b;
+    try {
+        return a & b;
+    } catch (const std::exception& e) {
+        throw BitwiseException("AND, " + std::string(e.what()));
+    }
 }
 MemoryValue PureBitwise::or_op(const MemoryValue& a, const MemoryValue& b)
 {
-    return a | b;
+    try {
+        return a | b;
+    } catch (const std::exception& e) {
+        throw BitwiseException("OR, " + std::string(e.what()));
+    }
 }
 MemoryValue PureBitwise::xor_op(const MemoryValue& a, const MemoryValue& b)
 {
-    return a ^ b;
+    try {
+        return a ^ b;
+    } catch (const std::exception& e) {
+        throw BitwiseException("XOR, " + std::string(e.what()));
+    }
 }
 
 } // namespace bb::avm2::simulation
