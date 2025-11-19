fa4c38fe2ba30b67df3fac4ea04c9932960cce04
jeanmon <jean@aztec-labs.com>
2025-11-19 20:21:14 +0000

    Flatten the slice memory columns

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/keccakf1600_trace.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/keccakf1600_trace.cpp
@@ -369,13 +369,13 @@ constexpr std::array<std::array<C, 5>, 5> STATE_CHI_COLS = {
 // Mapping 1-dimensional array indices of read/write memory slice values to columns.
 constexpr std::array<C, AVM_KECCAKF1600_STATE_SIZE> MEM_VAL_COLS = {
     {
-        C::keccak_memory_val00, C::keccak_memory_val01, C::keccak_memory_val02, C::keccak_memory_val03,
-        C::keccak_memory_val04, C::keccak_memory_val10, C::keccak_memory_val11, C::keccak_memory_val12,
-        C::keccak_memory_val13, C::keccak_memory_val14, C::keccak_memory_val20, C::keccak_memory_val21,
-        C::keccak_memory_val22, C::keccak_memory_val23, C::keccak_memory_val24, C::keccak_memory_val30,
-        C::keccak_memory_val31, C::keccak_memory_val32, C::keccak_memory_val33, C::keccak_memory_val34,
-        C::keccak_memory_val40, C::keccak_memory_val41, C::keccak_memory_val42, C::keccak_memory_val43,
-        C::keccak_memory_val44,
+        C::keccak_memory_val_0_,  C::keccak_memory_val_1_,  C::keccak_memory_val_2_,  C::keccak_memory_val_3_,
+        C::keccak_memory_val_4_,  C::keccak_memory_val_5_,  C::keccak_memory_val_6_,  C::keccak_memory_val_7_,
+        C::keccak_memory_val_8_,  C::keccak_memory_val_9_,  C::keccak_memory_val_10_, C::keccak_memory_val_11_,
+        C::keccak_memory_val_12_, C::keccak_memory_val_13_, C::keccak_memory_val_14_, C::keccak_memory_val_15_,
+        C::keccak_memory_val_16_, C::keccak_memory_val_17_, C::keccak_memory_val_18_, C::keccak_memory_val_19_,
+        C::keccak_memory_val_20_, C::keccak_memory_val_21_, C::keccak_memory_val_22_, C::keccak_memory_val_23_,
+        C::keccak_memory_val_24_,
     },
 };
 
@@ -412,23 +412,20 @@ void KeccakF1600TraceBuilder::process_single_slice(const simulation::KeccakF1600
     size_t num_rows = AVM_KECCAKF1600_STATE_SIZE;
 
     // The relevant state and read/write memory values depending on read/write boolean.
-    std::array<std::array<FF, 5>, 5> state_ff;
+    std::array<FF, AVM_KECCAKF1600_STATE_SIZE> slice_ff;
     if (write) {
         for (size_t i = 0; i < 5; i++) {
             for (size_t j = 0; j < 5; j++) {
-                state_ff[i][j] = event.rounds[AVM_KECCAKF1600_NUM_ROUNDS - 1].state_chi[i][j];
+                // Standard Keccak layout: memory[(5 * j) + i] = A[i][j].
+                slice_ff[(5 * j) + i] = event.rounds[AVM_KECCAKF1600_NUM_ROUNDS - 1].state_chi[i][j];
             }
         }
-        state_ff[0][0] = event.rounds[AVM_KECCAKF1600_NUM_ROUNDS - 1].state_iota_00;
+        slice_ff[0] = event.rounds[AVM_KECCAKF1600_NUM_ROUNDS - 1].state_iota_00;
     } else {
         // While reading we need to check the tag for each slice value.
-        // Standard Keccak layout: memory[k] = A[k%5][k/5]
-        // Standard Keccak layout: memory[(j * 5) + i] = A[i][j], so linear index k maps to (i=k%5, j=k/5)
         for (size_t k = 0; k < AVM_KECCAKF1600_STATE_SIZE; k++) {
             const auto& mem_val = event.src_mem_values[k];
-            const size_t i = k % 5; // x coordinate (column)
-            const size_t j = k / 5; // y coordinate (row)
-            state_ff[i][j] = mem_val.as_ff();
+            slice_ff[k] = mem_val.as_ff();
             tags[k] = mem_val.get_tag();
             if (tags[k] != MemoryTag::U64) {
                 single_tag_errors.at(k) = true;
@@ -471,10 +468,9 @@ void KeccakF1600TraceBuilder::process_single_slice(const simulation::KeccakF1600
                 { C::keccak_memory_num_rounds, AVM_KECCAKF1600_NUM_ROUNDS },
             } });
 
-        // We get a "triangle" when shifting values to their columns from val00 bottom-up.
-        // Standard Keccak layout: memory[(y * 5) + x] = A[x][y], so linear index j maps to (x=j%5, y=j/5)
+        // We get a "triangle" when shifting values to their columns from val_0_ bottom-up.
         for (size_t j = i; j < num_rows; j++) {
-            trace.set(MEM_VAL_COLS.at(j - i), row, state_ff[j % 5][j / 5]);
+            trace.set(MEM_VAL_COLS.at(j - i), row, slice_ff[j]);
         }
 
         row++;
