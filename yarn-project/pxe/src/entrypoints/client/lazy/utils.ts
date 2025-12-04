5e6bd817d47df1f3b387ae40eef6553dbacd1bae
Porco <porcorossoj89@gmail.com>
2025-12-04 18:11:34 +0700

    feat(pxe): make CircuitSimulator an optional parameter in PXE creation

--- yarn-project/pxe/src/entrypoints/client/lazy/utils.ts
+++ yarn-project/pxe/src/entrypoints/client/lazy/utils.ts
@@ -43,7 +43,7 @@ export async function createPXE(
 
   const store = options.store ?? (await createStore('pxe_data', configWithContracts, storeLogger));
 
-  const simulator = new WASMSimulator();
+  const simulator = options.simulator ?? new WASMSimulator();
   const proverLogger = loggers.prover
     ? loggers.prover
     : createLogger('pxe:bb:wasm:bundle' + (logSuffix ? `:${logSuffix}` : ''));
--- yarn-project/pxe/src/entrypoints/pxe_creation_options.ts
+++ yarn-project/pxe/src/entrypoints/pxe_creation_options.ts
@@ -1,5 +1,6 @@
 import type { Logger } from '@aztec/foundation/log';
 import type { AztecAsyncKVStore } from '@aztec/kv-store';
+import type { CircuitSimulator } from '@aztec/simulator';
 import type { PrivateKernelProver } from '@aztec/stdlib/interfaces/client';
 
 export type PXECreationOptions = {
@@ -7,4 +8,5 @@ export type PXECreationOptions = {
   useLogSuffix?: boolean | string;
   prover?: PrivateKernelProver;
   store?: AztecAsyncKVStore;
+  simulator?: CircuitSimulator;
 };
