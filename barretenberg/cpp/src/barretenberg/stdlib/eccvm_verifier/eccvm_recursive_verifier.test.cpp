59ac0775ae349804dce42cfe8994f841081897cc
sergei iakovenko <105737703+iakovenkos@users.noreply.github.com>
2025-12-05 22:04:07 +0100

    fix: `accumulated_result` fully random (#18736)
    The `accumulated_result` used to establish Translator <-> ECCVM
    consistency was only partially masked by a 128-bit scalar. Turned it
    into a fully random Grumpkin scalar.

--- barretenberg/cpp/src/barretenberg/stdlib/eccvm_verifier/eccvm_recursive_verifier.test.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/eccvm_verifier/eccvm_recursive_verifier.test.cpp
@@ -53,6 +53,7 @@ class ECCVMRecursiveTests : public ::testing::Test {
         using Curve = curve::BN254;
         using G1 = Curve::Element;
         using Fr = Curve::ScalarField;
+        using Fq = curve::Grumpkin::ScalarField;
 
         std::shared_ptr<ECCOpQueue> op_queue = std::make_shared<ECCOpQueue>();
         G1 a = G1::random_element(engine);
@@ -77,6 +78,8 @@ class ECCVMRecursiveTests : public ::testing::Test {
             op_queue->mul_accumulate(c, x);
             op_queue->merge();
         }
+        // Set hiding op for ECCVM ZK (required before ECCVMCircuitBuilder construction)
+        op_queue->append_hiding_op(Fq::random_element(engine), Fq::random_element(engine));
         InnerBuilder builder{ op_queue };
         return builder;
     }
@@ -156,7 +159,7 @@ class ECCVMRecursiveTests : public ::testing::Test {
         }
 
         // Check that the size of the recursive verifier is consistent with historical expectation
-        uint32_t NUM_GATES_EXPECTED = 214043;
+        uint32_t NUM_GATES_EXPECTED = 215193;
         ASSERT_EQ(static_cast<uint32_t>(outer_circuit.get_num_finalized_gates()), NUM_GATES_EXPECTED)
             << "Ultra-arithmetized ECCVM Recursive verifier gate count changed! Update this value if you are sure this "
                "is expected.";
