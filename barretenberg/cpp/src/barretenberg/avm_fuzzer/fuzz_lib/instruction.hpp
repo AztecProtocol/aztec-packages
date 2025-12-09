5bfb72f7582d911ff28edc205e0924e4d822f036
jewelofchaos9 <jewelofchaos9@gmail.com>
2025-12-09 06:08:46 +0000

    forgor to add pointer_address to msgpack

--- barretenberg/cpp/src/barretenberg/avm_fuzzer/fuzz_lib/instruction.hpp
+++ barretenberg/cpp/src/barretenberg/avm_fuzzer/fuzz_lib/instruction.hpp
@@ -77,7 +77,7 @@ struct AddressRef {
     uint32_t base_offset = 0;
     AddressingMode mode = AddressingMode::Direct;
 
-    MSGPACK_FIELDS(tag, index, base_offset, mode);
+    MSGPACK_FIELDS(tag, index, pointer_address, base_offset, mode);
 };
 
 struct ResultAddressRef {
@@ -89,7 +89,7 @@ struct ResultAddressRef {
     /// @brief Base offset used for Relative/IndirectRelative modes only
     uint32_t base_offset = 0;
     AddressingMode mode = AddressingMode::Direct;
-    MSGPACK_FIELDS(address, mode);
+    MSGPACK_FIELDS(address, pointer_address, base_offset, mode);
 };
 
 /// @brief mem[result_offset] = mem[a_address] + mem[b_address]
