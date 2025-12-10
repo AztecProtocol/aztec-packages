33753d6f357529918009431106d1ec98757bab0b
iakovenkos <sergey.s.yakovenko@gmail.com>
2025-12-10 15:41:41 +0000

    fix gcc

--- barretenberg/cpp/src/barretenberg/goblin/goblin.cpp
+++ barretenberg/cpp/src/barretenberg/goblin/goblin.cpp
@@ -43,7 +43,7 @@ void Goblin::prove_eccvm()
 
     // Compute IPA proof for the opening claim
     auto ipa_transcript = std::make_shared<NativeTranscript>();
-    IPA::compute_opening_proof(eccvm_prover.key->commitment_key, opening_claim, ipa_transcript);
+    IPA_PCS::compute_opening_proof(eccvm_prover.key->commitment_key, opening_claim, ipa_transcript);
     goblin_proof.ipa_proof = ipa_transcript->export_proof();
 
     translation_batching_challenge_v = eccvm_prover.batching_challenge_v;
