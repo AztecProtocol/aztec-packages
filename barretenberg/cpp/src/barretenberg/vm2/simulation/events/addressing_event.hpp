869ffc69681cd557164c5248f73ca7b2745046a9
jeanmon <jean@aztec-labs.com>
2025-12-10 09:09:29 +0000

    addressing event comments and linter warning

--- barretenberg/cpp/src/barretenberg/vm2/simulation/events/addressing_event.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/simulation/events/addressing_event.hpp
@@ -11,12 +11,14 @@
 
 namespace bb::avm2::simulation {
 
-enum class AddressingEventError {
-    // The base address (mem[0]) is not a valid address.
+enum class AddressingEventError : uint8_t {
+    // The base address (mem[0]) points to a MemoryValue not of the
+    // right tag (not MemoryAddressTag).
     BASE_ADDRESS_INVALID,
     // The relative computation overflowed.
     RELATIVE_COMPUTATION_OOB,
-    // The address obtained after applying indirection is not a valid address.
+    // The address obtained after applying indirection points to
+    // a MemoryValue not of the right tag (not MemoryAddressTag).
     INVALID_ADDRESS_AFTER_INDIRECTION,
 };
 
@@ -41,18 +43,22 @@ struct AddressingException : public std::runtime_error {
     {}
 };
 
+// This represents the resolution of a single operand.
+// The operand can be either an immediate or an address.
+// If it is an address, we can apply relative addressing and indirection to it.
+// We will store the result of the resolution in the resolved_operand field.
+// An immediate operand will never have an error and both resolved_operand
+// and after_relative will be the same as the original operand.
 struct OperandResolutionInfo {
     Operand after_relative;
     Operand resolved_operand;
     std::optional<AddressingEventError> error;
 };
 
-// See https://docs.google.com/document/d/1EgFj0OQYZCWufjzLgoAAiVL9jV0-fUAaCCIVlvRc8bY/ for circuit details.
-// - The activation mask can be derived from spec.num_addresses.
 struct AddressingEvent {
-    Instruction instruction;
-    MemoryValue base_address;
-    std::vector<OperandResolutionInfo> resolution_info; // One per operand (including immediates).
+    Instruction instruction;                            // The instruction being resolved.
+    MemoryValue base_address;                           // mem[0] (for relative addressing).
+    std::vector<OperandResolutionInfo> resolution_info; // One entry per operand (including immediates).
 };
 
 } // namespace bb::avm2::simulation
