9dd744b496e504289f2ecc35302457d0cd51e87e
jeanmon <jean@aztec-labs.com>
2025-12-11 10:13:44 +0000

    Be explicit for error in padded resolution_info

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/execution_trace.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/execution_trace.cpp
@@ -849,6 +849,7 @@ void ExecutionTraceBuilder::process_addressing(const simulation::AddressingEvent
                                    // This is the default we want: both tag and value 0.
                                    .after_relative = FF::zero(),
                                    .resolved_operand = Operand::from_tag(static_cast<ValueTag>(0), 0),
+                                   .error = std::nullopt,
                                });
 
     std::array<bool, AVM_MAX_OPERANDS> should_apply_indirection{};
