cc6a9e71556aed6edf0ad8f36d7bd3fc35dcce28
fcarreiro <facundo@aztecprotocol.com>
2025-12-11 11:14:12 +0000

    done

--- yarn-project/bb-prover/src/avm_proving_tests/avm_proving_tester.ts
+++ yarn-project/bb-prover/src/avm_proving_tests/avm_proving_tester.ts
@@ -222,6 +222,7 @@ export class AvmProvingTester extends PublicTxSimulationTester {
     txLabel: string = 'unlabeledTx',
     disableRevertCheck: boolean = false,
   ): Promise<PublicTxResult> {
+    const simTimer = new Timer();
     const simRes = await this.simulateTx(
       sender,
       setupCalls,
@@ -231,6 +232,8 @@ export class AvmProvingTester extends PublicTxSimulationTester {
       privateInsertions,
       txLabel,
     );
+    const simDuration = simTimer.ms();
+    this.logger.info(`Simulation took ${simDuration} ms for tx ${txLabel}`);
 
     if (!disableRevertCheck) {
       expect(simRes.revertCode.isOK()).toBe(expectRevert ? false : true);
