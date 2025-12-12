2879cf1892c96bf130256073edee90b47168b0a8
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2025-12-12 16:15:03 +0000

    Remove constructor

--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/mega_circuit_builder.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/mega_circuit_builder.hpp
@@ -64,10 +64,6 @@ template <typename FF> class MegaCircuitBuilder_ : public UltraCircuitBuilder_<M
         set_goblin_ecc_op_code_constant_variables();
     };
 
-    MegaCircuitBuilder_(std::shared_ptr<ECCOpQueue> op_queue_in)
-        : MegaCircuitBuilder_(0, op_queue_in)
-    {}
-
     /**
      * @brief Constructor from data generated from ACIR
      *
