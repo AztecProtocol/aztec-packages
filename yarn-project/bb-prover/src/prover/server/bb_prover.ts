b8ae746625935f4e294de06ede7054fcadabdfe9
ludamad <adam.domurad@gmail.com>
2025-12-11 22:27:50 -0500

    Discard changes to yarn-project/bb-prover/src/prover/server/bb_prover.ts

--- yarn-project/bb-prover/src/prover/server/bb_prover.ts
+++ yarn-project/bb-prover/src/prover/server/bb_prover.ts
@@ -507,7 +507,6 @@ export class BBNativeRollupProver implements ServerCircuitProver {
       outputWitnessFile,
       getUltraHonkFlavorForCircuit(circuitType),
       logger,
-      this.config.bbMaxMemory,
     );
 
     if (provingResult.status === BB_RESULT.FAILURE) {
@@ -524,14 +523,7 @@ export class BBNativeRollupProver implements ServerCircuitProver {
   private async generateAvmProofWithBB(input: AvmCircuitInputs, workingDirectory: string): Promise<BBSuccess> {
     logger.info(`Proving avm-circuit for TX ${input.hints.tx.hash}...`);
 
-    const provingResult = await generateAvmProof(
-      this.config.bbBinaryPath,
-      workingDirectory,
-      input,
-      logger,
-      false,
-      this.config.bbMaxMemory,
-    );
+    const provingResult = await generateAvmProof(this.config.bbBinaryPath, workingDirectory, input, logger);
 
     if (provingResult.status === BB_RESULT.FAILURE) {
       logger.error(`Failed to generate AVM proof for TX ${input.hints.tx.hash}: ${provingResult.reason}`);
