fbca9c64e344f642c6cef920b5a88666bd708393
ludamad <domuradical@gmail.com>
2025-12-16 01:04:51 +0000

    state fixup: before PR f96984ce34

--- yarn-project/test-wallet/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { TestWallet } from './wallet/bundle.js';
-export { type AccountData } from './wallet/test_wallet.js';
-export { deployFundedSchnorrAccounts, registerInitialSandboxAccountsInWallet } from './utils.js';
