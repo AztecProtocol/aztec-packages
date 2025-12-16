658faa946db845045f00b04bbad046104f36624b
sirasistant <sirasistant@gmail.com>
2025-12-16 17:49:21 +0000

    add todo

--- barretenberg/cpp/src/barretenberg/avm_fuzzer/mutations/fuzzer_data.cpp
+++ barretenberg/cpp/src/barretenberg/avm_fuzzer/mutations/fuzzer_data.cpp
@@ -52,6 +52,7 @@ void add_default_instruction_block_if_empty(FuzzerData& fuzzer_data, std::mt1993
         instruction_block.reserve(num_tags);
         // Add one set per memory tag type
         for (uint32_t i = 0; i < num_tags; i++) {
+            // TODO: Randomize address, value. Keep address < 255 so it can be used anywhere.
             auto tag = static_cast<ValueTag>(i);
             instruction_block.push_back(SET_8_Instruction{
                 .value_tag = tag,
