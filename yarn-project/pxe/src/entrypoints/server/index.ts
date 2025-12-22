c3d269b494faeed499ced7bb29ce32e6eb5f6b11
mverzilli <martin@aztec-labs.com>
2025-12-22 12:37:57 +0000

    went too far in the shaking

--- yarn-project/pxe/src/entrypoints/server/index.ts
+++ yarn-project/pxe/src/entrypoints/server/index.ts
@@ -3,5 +3,6 @@ export * from '../../config/index.js';
 export * from '../../error_enriching.js';
 export * from '../../storage/index.js';
 export * from './utils.js';
+export { NoteService } from '../../notes/note_service.js';
 export { ORACLE_VERSION } from '../../oracle_version.js';
 export { type PXECreationOptions } from '../pxe_creation_options.js';
