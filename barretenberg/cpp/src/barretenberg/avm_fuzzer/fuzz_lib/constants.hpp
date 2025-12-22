2e48e76fe03ddb41b55c556adaab96c3c78147ba
defkit <84741533+defkit@users.noreply.github.com>
2025-12-22 14:45:06 +0300

    feat(fuzzing_avm): successcopy + getcontractinstance (#19183)
    ### Added opcodes
    1) SUCCESSCOPY
    2) GETCONTRACTINSTANCE
    
    ### Changed
    predefined function so that they do not collide with tests

--- barretenberg/cpp/src/barretenberg/avm_fuzzer/fuzz_lib/constants.hpp
+++ barretenberg/cpp/src/barretenberg/avm_fuzzer/fuzz_lib/constants.hpp
@@ -51,8 +51,19 @@ inline void fuzz_info_(std::function<std::string()> func)
 #define fuzz_info(...) fuzz_info_([&]() { return format(__VA_ARGS__); })
 
 // =========== Pre defined functions ===========
-// Taken from ADD_8 test in fuzz.test.cpp
-const std::vector<uint8_t> ADD_8_BYTECODE = { 39, 0,  0, 2, 5, 39, 0, 1, 2,  2, 0, 0, 0, 1,
+// SET[0] = 1, SET[1] = 1, ADD[0, 1, 2]
+const std::vector<uint8_t> ADD_8_BYTECODE = { 39, 0,  0, 2, 1, 39, 0, 1, 2,  1, 0, 0, 0, 1,
                                               2,  40, 0, 0, 5, 4,  0, 1, 59, 0, 0, 5, 0, 2 };
 
-const std::vector<std::vector<uint8_t>> PREDEFINED_FUNCTIONS = { ADD_8_BYTECODE };
+// SET[0] = 0, DIV[0, 0]
+const std::vector<uint8_t> ZERO_DIVISION_BYTECODE = { 39, 0, 0, 2, 0, 6,  0, 0, 0, 2, 40, 0,
+                                                      0,  5, 4, 0, 1, 59, 0, 0, 5, 0, 0 };
+
+// SSTORE[10] = 200
+const std::vector<uint8_t> SSTORE_BYTECODE = { 39, 0, 10, 0,  200, 44, 0,  0, 10, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 0,
+                                               0,  0, 0,  0,  0,   0,  0,  0, 0,  0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0, 10,
+                                               48, 0, 0,  10, 0,   10, 40, 0, 0,  5, 4, 0, 1, 59, 0, 0, 5, 0, 10 };
+
+const std::vector<std::vector<uint8_t>> PREDEFINED_FUNCTIONS = { ADD_8_BYTECODE,
+                                                                 ZERO_DIVISION_BYTECODE,
+                                                                 SSTORE_BYTECODE };
