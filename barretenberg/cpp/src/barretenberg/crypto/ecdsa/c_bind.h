eca4e8ca785a22dd98afb22c1059aac9324d3fb3
ledwards2225 <l.edwards.d@gmail.com>
2026-01-05 23:20:46 +0000

    remove audit headers from remaining cbinds

--- barretenberg/cpp/src/barretenberg/crypto/ecdsa/c_bind.h
+++ barretenberg/cpp/src/barretenberg/crypto/ecdsa/c_bind.h
@@ -1,9 +1,3 @@
-// === AUDIT STATUS ===
-// internal:    { status: not started, auditors: [], commit: }
-// external_1:  { status: not started, auditors: [], commit: }
-// external_2:  { status: not started, auditors: [], commit: }
-// =====================
-
 #include "barretenberg/common/wasm_export.hpp"
 #include <ecc/curves/secp256k1/secp256k1.hpp>
 #include <ecc/curves/secp256r1/secp256r1.hpp>
