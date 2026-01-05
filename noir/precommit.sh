ac31bd51e469f6bdcfb5175123f5c27e15605506
charlielye <5764343+charlielye@users.noreply.github.com>
2026-01-05 16:34:16 +0000

    fix: CI Updates
    * Brings the ci dashboard code from `iac` repo to `ci3/dashboard`.
    * Attempt to fix cleanup on makefile build path. We used setsid but think correct approach is to do a background subshell `() &`
    * Fix PR's not getting RUN_ID's.
    * We explicitly name the dashboard CI runs are shown on with CI_DASHBOARD.
      * `<merge queue target>`
      * `prs`
      * `nightly`
      * `network`
    *  Add `ci dash` command so you can see your CI runs direct in terminal.

--- noir/precommit.sh
+++ noir/precommit.sh
@@ -7,16 +7,21 @@ if git diff --cached --submodule=short noir/noir-repo | grep -q "^-Subproject co
   echo "⚠️  WARNING: You are about to change the noir/noir-repo submodule hash"
   echo ""
 
-  # Ask for confirmation
-  read -p "Do you really want to commit this submodule change? (y/N): " -n 1 -r
-  echo ""
+  if [ -t 0 ]; then
+    # Ask for confirmation
+    read -p "Do you really want to commit this submodule change? (y/N): " -n 1 -r
+    echo ""
 
-  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    echo "Commit aborted."
-    echo ""
-    echo "Maybe you want to re-pull submodules with:"
-    echo "  git submodule update --init --recursive"
-    echo ""
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      echo "Commit aborted."
+      echo ""
+      echo "Maybe you want to re-pull submodules with:"
+      echo "  git submodule update --init --recursive"
+      echo ""
+      exit 1
+    fi
+  else
+    echo "You can git commit --no-verify if you're sure."
     exit 1
   fi
 fi
