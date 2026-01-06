8476e8f084762e76b59f5506cdb02e34043b7400
ledwards2225 <l.edwards.d@gmail.com>
2026-01-06 17:12:18 +0000

    avm rec verifier scope

--- barretenberg/cpp/src/barretenberg/vm2/constraining/recursion/recursive_flavor_settings.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/constraining/recursion/recursive_flavor_settings.hpp
@@ -1,3 +1,9 @@
+// === AUDIT STATUS ===
+// internal:    { status: Planned, auditors: [], commit: }
+// external_1:  { status: not started, auditors: [], commit: }
+// external_2:  { status: not started, auditors: [], commit: }
+// =====================
+
 #pragma once
 
 #include "barretenberg/commitment_schemes/kzg/kzg.hpp"
