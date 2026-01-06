2c0a4c3c4560271ccdbec75234fbac23a2afbcf2
nishatkoti <nishatkoti@gmail.com>
2026-01-06 09:04:56 +0000

    add num_bytes assert in blake3s

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/blake3_constraint.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/blake3_constraint.cpp
@@ -26,6 +26,7 @@ template <typename Builder> void create_blake3_constraints(Builder& builder, con
 
         // XXX: The implementation requires us to truncate the element to the nearest byte and not bit
         auto num_bytes = round_to_nearest_byte(num_bits);
+        BB_ASSERT_LTE(num_bytes, 32U, "Input num_bytes exceeds 32 per element in blake3s");
         field_ct element = to_field_ct(witness_index, builder);
 
         // byte_array_ct(field, num_bytes) constructor adds range constraints for each byte
