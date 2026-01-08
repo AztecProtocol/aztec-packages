cd46dd2559704d0b774a8788381f3e35568a976e
jeanmon <jean@aztec-labs.com>
2026-01-08 07:17:21 +0000

    Tracegen - migrate boolean to FF implicit conversion

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/context_stack_trace.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/context_stack_trace.cpp
@@ -30,7 +30,7 @@ void ContextStackTraceBuilder::process(
                       { C::context_stack_msg_sender, event.msg_sender },
                       { C::context_stack_contract_address, event.contract_addr },
                       { C::context_stack_bytecode_id, event.bytecode_id },
-                      { C::context_stack_is_static, event.is_static },
+                      { C::context_stack_is_static, event.is_static ? 1 : 0 },
                       { C::context_stack_parent_calldata_addr, event.parent_cd_addr },
                       { C::context_stack_parent_calldata_size, event.parent_cd_size },
                       { C::context_stack_parent_l2_gas_limit, event.parent_gas_limit.l2_gas },
