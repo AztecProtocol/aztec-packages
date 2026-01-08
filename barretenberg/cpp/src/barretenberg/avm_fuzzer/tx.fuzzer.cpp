8be6690e93289490c913707ef5d5434ea9dae5f2
sirasistant <sirasistant@gmail.com>
2026-01-08 14:11:10 +0000

    add comment

--- barretenberg/cpp/src/barretenberg/avm_fuzzer/tx.fuzzer.cpp
+++ barretenberg/cpp/src/barretenberg/avm_fuzzer/tx.fuzzer.cpp
@@ -27,6 +27,8 @@ extern "C" size_t LLVMFuzzerCustomMutator(uint8_t* serialized_fuzzer_data,
                                           size_t max_size,
                                           unsigned int seed)
 {
+    // Haven't thought much about the lifecycle of this in the tx fuzzer. Maybe we want it in the serialized data?
+    // Or we can regenerate from the serialized data.
     FuzzerContext context;
     return mutate_tx_data(context, serialized_fuzzer_data, serialized_fuzzer_data_size, max_size, seed);
 }
