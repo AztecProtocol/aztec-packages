30c2097ecd0da60841137f2f04163861eb71f3c9
iakovenkos <sergey.s.yakovenko@gmail.com>
2026-01-09 15:15:35 +0000

    updating headers

--- barretenberg/cpp/src/barretenberg/stdlib/hash/poseidon2/poseidon2.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib/hash/poseidon2/poseidon2.hpp
@@ -1,6 +1,6 @@
 // === AUDIT STATUS ===
 // internal:    { status: Complete, auditors: [Sergei], commit: 777717f6af324188ecd6bb68c3c86ee7befef94d}
-// external_1:  { status: not started, auditors: [], commit: }
+// external_1:  { status: Complete, auditors: [@ed25519 (Spearbit)], commit: }
 // external_2:  { status: not started, auditors: [], commit: }
 // =====================
 
