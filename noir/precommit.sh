93265007780e57687c4d1c06f689a5f08490cd9a
Charlie <5764343+charlielye@users.noreply.github.com>
2026-01-09 14:56:37 +0000

    check we're on a noir tag

--- noir/precommit.sh
+++ noir/precommit.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+cd $(git rev-parse --show-toplevel)
+
+if ! git -C noir/noir-repo describe --tags --exact-match HEAD &>/dev/null; then
+  echo >&2 "noir/noir-repo submodule should be pointing at an official release tag."
+  exit 1
+fi
+
 # Check if the noir-repo submodule hash is being modified in the staged changes
 if git diff --cached --submodule=short noir/noir-repo | grep -q "^-Subproject commit\|^+Subproject commit"; then
   echo ""
