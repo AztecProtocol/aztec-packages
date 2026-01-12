1c3a2f2d9a771ce7812ea8c13e5cd351b7bc8b5e
iakovenkos <sergey.s.yakovenko@gmail.com>
2026-01-12 11:41:16 +0000

    upd docs

--- barretenberg/cpp/src/barretenberg/flavor/multilinear_batching_flavor.cpp
+++ barretenberg/cpp/src/barretenberg/flavor/multilinear_batching_flavor.cpp
@@ -19,24 +19,29 @@ MultilinearBatchingFlavor::ProvingKey::ProvingKey(MultilinearBatchingProverClaim
     circuit_size = std::max(accumulator_claim.dyadic_size, instance_claim.dyadic_size);
     size_t virtual_circuit_size = 1 << MultilinearBatchingFlavor::VIRTUAL_LOG_N;
 
-    // Move batched polynomials (the "P" in evaluation claims P(r) = v)
+    // Move unshifted polynomials (the "P" in evaluation claims P(r) = v)
     polynomials.batched_unshifted_accumulator = std::move(accumulator_claim.non_shifted_polynomial);
-    polynomials.batched_shifted_accumulator = accumulator_claim.shifted_polynomial.shifted();
     polynomials.batched_unshifted_instance = std::move(instance_claim.non_shifted_polynomial);
-    polynomials.batched_shifted_instance = instance_claim.shifted_polynomial.shifted();
 
-    // Construct eq polynomials from challenges (eq must be built before challenges are moved)
+    // Initialize shiftable polynomials that are required to create the output claim's shifted polynomial
+    preshifted_accumulator = std::move(accumulator_claim.shifted_polynomial);
+    preshifted_instance = std::move(instance_claim.shifted_polynomial);
+
+    // Create shifted views for sumcheck. These share the underlying memory buffer with preshifted_*
+    polynomials.batched_shifted_accumulator = preshifted_accumulator.shifted();
+    polynomials.batched_shifted_instance = preshifted_instance.shifted();
+
+    // Construct `eq` polynomials from challenges
     polynomials.eq_accumulator =
         ProverEqPolynomial<FF>::construct(accumulator_claim.challenge, bb::numeric::get_msb(circuit_size));
     polynomials.eq_instance =
         ProverEqPolynomial<FF>::construct(instance_claim.challenge, bb::numeric::get_msb(circuit_size));
     polynomials.increase_polynomials_virtual_size(virtual_circuit_size);
 
-    // Move challenges (evaluation points r_acc and r_inst)
+    // Move incoming challenges  and copy commitments with corresponding evaluations
     accumulator_challenge = std::move(accumulator_claim.challenge);
     instance_challenge = std::move(instance_claim.challenge);
 
-    // Copy evaluations and commitments
     accumulator_evaluations = { accumulator_claim.non_shifted_evaluation, accumulator_claim.shifted_evaluation };
     instance_evaluations = { instance_claim.non_shifted_evaluation, instance_claim.shifted_evaluation };
 
@@ -44,10 +49,6 @@ MultilinearBatchingFlavor::ProvingKey::ProvingKey(MultilinearBatchingProverClaim
     shifted_accumulator_commitment = accumulator_claim.shifted_commitment;
     non_shifted_instance_commitment = instance_claim.non_shifted_commitment;
     shifted_instance_commitment = instance_claim.shifted_commitment;
-
-    // Move preshifted polynomials (needed for new claim computation)
-    preshifted_accumulator = std::move(accumulator_claim.shifted_polynomial);
-    preshifted_instance = std::move(instance_claim.shifted_polynomial);
 }
 
 } // namespace bb
