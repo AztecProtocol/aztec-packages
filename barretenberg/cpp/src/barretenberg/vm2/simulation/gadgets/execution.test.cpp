0d4b48dab49257d2864595a261effb042a319c3d
fcarreiro <facundo@aztecprotocol.com>
2026-01-12 15:05:11 +0000

    fix tests

--- barretenberg/cpp/src/barretenberg/vm2/simulation/gadgets/execution.test.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/simulation/gadgets/execution.test.cpp
@@ -518,7 +518,7 @@ TEST_F(ExecutionSimulationTest, GetEnvVarAddress)
     EXPECT_CALL(memory, set(1, MemoryValue::from<FF>(addr)));
     EXPECT_CALL(gas_tracker, consume_gas(Gas{ 0, 0 }));
 
-    execution.get_env_var(context, 1, static_cast<uint8_t>(EnvironmentVariable::ADDRESS));
+    execution.get_env_var(context, 1, EnvironmentVariable::ADDRESS);
 }
 
 TEST_F(ExecutionSimulationTest, GetEnvVarChainId)
@@ -530,7 +530,7 @@ TEST_F(ExecutionSimulationTest, GetEnvVarChainId)
     EXPECT_CALL(memory, set(1, MemoryValue::from<FF>(1)));
     EXPECT_CALL(gas_tracker, consume_gas(Gas{ 0, 0 }));
 
-    execution.get_env_var(context, 1, static_cast<uint8_t>(EnvironmentVariable::CHAINID));
+    execution.get_env_var(context, 1, EnvironmentVariable::CHAINID);
 }
 
 TEST_F(ExecutionSimulationTest, GetEnvVarIsStaticCall)
@@ -540,7 +540,7 @@ TEST_F(ExecutionSimulationTest, GetEnvVarIsStaticCall)
     EXPECT_CALL(memory, set(1, MemoryValue::from<uint1_t>(1)));
     EXPECT_CALL(gas_tracker, consume_gas(Gas{ 0, 0 }));
 
-    execution.get_env_var(context, 1, static_cast<uint8_t>(EnvironmentVariable::ISSTATICCALL));
+    execution.get_env_var(context, 1, EnvironmentVariable::ISSTATICCALL);
 }
 
 TEST_F(ExecutionSimulationTest, GetEnvVarInvalidEnum)
@@ -548,7 +548,7 @@ TEST_F(ExecutionSimulationTest, GetEnvVarInvalidEnum)
     EXPECT_CALL(context, get_memory());
     EXPECT_CALL(gas_tracker, consume_gas(Gas{ 0, 0 }));
 
-    EXPECT_THROW(execution.get_env_var(context, 1, 255), std::runtime_error);
+    EXPECT_THROW(execution.get_env_var(context, 1, static_cast<EnvironmentVariable>(255)), std::runtime_error);
 }
 
 // Trivial test at the moment.
@@ -982,7 +982,7 @@ TEST_F(ExecutionSimulationTest, EmitNullifierCollision)
 TEST_F(ExecutionSimulationTest, Set)
 {
     MemoryAddress dst_addr = 10;
-    uint8_t dst_tag = static_cast<uint8_t>(MemoryTag::U8);
+    MemoryTag dst_tag = MemoryTag::U8;
     FF value = 7;
 
     EXPECT_CALL(context, get_memory());
@@ -997,7 +997,7 @@ TEST_F(ExecutionSimulationTest, Cast)
 {
     MemoryAddress src_addr = 9;
     MemoryAddress dst_addr = 10;
-    uint8_t dst_tag = static_cast<uint8_t>(MemoryTag::U1);
+    MemoryTag dst_tag = MemoryTag::U1;
     MemoryValue value = MemoryValue::from<uint64_t>(7);
 
     EXPECT_CALL(context, get_memory()).WillOnce(ReturnRef(memory));
