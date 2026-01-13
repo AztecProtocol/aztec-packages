d8345bf988fd5fe32d7ad46eb9851d3b394321b9
jewelofchaos9 <jewelofchaos9@gmail.com>
2026-01-13 07:11:40 +0000

    move fix to the test

--- barretenberg/cpp/src/barretenberg/vm2/simulation/gadgets/update_check.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/simulation/gadgets/update_check.hpp
@@ -15,13 +15,13 @@ class UpdateCheck : public UpdateCheckInterface {
                 GreaterThanInterface& gt,
                 HighLevelMerkleDBInterface& merkle_db,
                 EventEmitterInterface<UpdateCheckEvent>& read_event_emitter,
-                const GlobalVariables& globals_)
+                const GlobalVariables& globals)
         : update_check_events(read_event_emitter)
         , poseidon2(poseidon2)
         , range_check(range_check)
         , gt(gt)
         , merkle_db(merkle_db)
-        , globals(globals_)
+        , globals(globals)
     {}
 
     void check_current_class_id(const AztecAddress& address, const ContractInstance& instance) override;
@@ -32,7 +32,7 @@ class UpdateCheck : public UpdateCheckInterface {
     RangeCheckInterface& range_check;
     GreaterThanInterface& gt;
     HighLevelMerkleDBInterface& merkle_db;
-    GlobalVariables globals;
+    const GlobalVariables& globals;
 };
 
 } // namespace bb::avm2::simulation
