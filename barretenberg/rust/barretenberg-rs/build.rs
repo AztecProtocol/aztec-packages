1afc0540f2dc578500bb23bd01d76004d4bf9c37
Jonathan Hao <jonathan@aztec-labs.com>
2026-01-13 15:48:25 +0000

    feat: create libbb-external.a for barretenberg-rs
    Create a single static library that bundles everything barretenberg-rs needs:
    - All objects from libbarretenberg.a
    - env module (logstr, throw_or_abort_impl, env_hardware_concurrency)
    - vm2_stub (provides stub for create_avm2_recursion_constraints_goblin)
    
    This simplifies linking in barretenberg-rs to just one library and
    removes the need for --allow-multiple-definition and --start-group/--end-group.

--- barretenberg/rust/barretenberg-rs/build.rs
+++ barretenberg/rust/barretenberg-rs/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    // Only for ffi feature - link libbarretenberg from cpp build
+    // Only for ffi feature - link libbb-external from cpp build
     #[cfg(feature = "ffi")]
     {
         // Find the cpp build lib directory relative to this crate
@@ -11,17 +11,8 @@ fn main() {
 
         println!("cargo:rustc-link-search=native={}", lib_dir.display());
 
-        // Use --start-group/--end-group to handle circular dependencies between static libraries.
-        // libbarretenberg.a is a mega-library containing most symbols.
-        // env provides logstr, throw_or_abort_impl, env_hardware_concurrency
-        // vm2_stub provides create_avm2_recursion_constraints_goblin stub (throws at runtime)
-        // --allow-multiple-definition is needed because some objects are partially in libbarretenberg.a
-        println!("cargo:rustc-link-arg=-Wl,--allow-multiple-definition");
-        println!("cargo:rustc-link-arg=-Wl,--start-group");
-        println!("cargo:rustc-link-lib=static=barretenberg");
-        println!("cargo:rustc-link-lib=static=env");
-        println!("cargo:rustc-link-lib=static=vm2_stub");
-        println!("cargo:rustc-link-arg=-Wl,--end-group");
+        // libbb-external.a contains everything needed: barretenberg + env + vm2_stub
+        println!("cargo:rustc-link-lib=static=bb-external");
         println!("cargo:rustc-link-lib=dylib=stdc++");
     }
 }
