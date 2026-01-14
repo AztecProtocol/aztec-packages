354b7c0dd33576a2e7df0e594add4a3ad4271904
Santiago Palladino <santiago@aztec-labs.com>
2026-01-14 19:01:46 -0300

    feat(mbps): enable pushing proposed blocks to the archiver

--- yarn-project/aztec-node/src/aztec-node/server.ts
+++ yarn-project/aztec-node/src/aztec-node/server.ts
@@ -770,11 +770,10 @@ export class AztecNodeService implements AztecNode, AztecNodeAdmin, Traceable {
   }
 
   public async getTxReceipt(txHash: TxHash): Promise<TxReceipt> {
-    // We first check if the tx is in pending (instead of first checking if it is mined) because if we first check
-    // for mined and then for pending there could be a race condition where the tx is mined between the two checks
-    // and we would incorrectly return a TxReceipt with status DROPPED
-    const txStatus = await this.p2pClient.getTxStatus(txHash);
-    const isKnownToPool = txStatus === 'pending' || txStatus === 'mined';
+    // Check the tx pool status first. If the tx is known to the pool (pending or mined), we'll use that
+    // as a fallback if we don't find a settled receipt in the archiver.
+    const txPoolStatus = await this.p2pClient.getTxStatus(txHash);
+    const isKnownToPool = txPoolStatus === 'pending' || txPoolStatus === 'mined';
 
     // Then get the actual tx from the archiver, which tracks every tx in a mined block.
     const settledTxReceipt = await this.blockSource.getSettledTxReceipt(txHash);
