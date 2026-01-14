0e9eb6436fd1433b884f17ecc0d78d74e19aff1d
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2026-01-14 11:44:47 +0000

    Update gate count constants

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
@@ -159,7 +159,7 @@ inline constexpr size_t ECCVM_RECURSIVE_VERIFIER_GATE_COUNT = 214908;
 // Goblin AVM Recursive Verifier Constants
 // ========================================
 
-inline constexpr size_t GOBLIN_AVM_GATE_COUNT = 3316428;
-inline constexpr size_t FINALIZED_GOBLIN_AVM_GATE_COUNT = 3316444;
+inline constexpr size_t GOBLIN_AVM_GATE_COUNT = 3316531;
+inline constexpr size_t FINALIZED_GOBLIN_AVM_GATE_COUNT = 3316547;
 
 } // namespace acir_format
