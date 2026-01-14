87cbf1e8056b2d7de0e49fa82d69348016e9ba28
Ilyas Ridhuan <ilyas@aztecprotocol.com>
2026-01-14 13:40:32 +0000

    feat(avm): contract class mutation (#19498)
    Adds mutation for contract classses. This requires changing from simple
    `ContractClass` to `ContractClassWithCommitment` within the fuzzer.
    
    We need the commitment so that when the contract class is mutated, we
    can re-compute: the class id and the contract address.

--- barretenberg/cpp/src/barretenberg/avm_fuzzer/mutations/bytecode.cpp
+++ barretenberg/cpp/src/barretenberg/avm_fuzzer/mutations/bytecode.cpp
@@ -1,15 +1,17 @@
 #include "barretenberg/avm_fuzzer/mutations/bytecode.hpp"
 
 #include "barretenberg/avm_fuzzer/fuzz_lib/constants.hpp"
+#include "barretenberg/avm_fuzzer/mutations/basic_types/field.hpp"
 #include "barretenberg/crypto/poseidon2/poseidon2.hpp"
 #include "barretenberg/vm2/common/aztec_constants.hpp"
+#include "barretenberg/vm2/common/aztec_types.hpp"
 #include "barretenberg/vm2/simulation/lib/contract_crypto.hpp"
 
 extern "C" size_t LLVMFuzzerMutate(uint8_t* Data, size_t Size, size_t MaxSize);
 
 namespace bb::avm2::fuzzer {
 
-void mutate_bytecode(std::vector<ContractClass>& contract_classes,
+void mutate_bytecode(std::vector<ContractClassWithCommitment>& contract_classes,
                      std::vector<ContractInstance>& contract_instances,
                      const std::vector<AztecAddress>& contract_addresses,
                      std::vector<bb::crypto::merkle_tree::PublicDataLeafValue>& public_data_writes,
@@ -25,7 +27,7 @@ void mutate_bytecode(std::vector<ContractClass>& contract_classes,
     // Select a random contract
     size_t idx = std::uniform_int_distribution<size_t>(0, contract_classes.size() - 1)(rng);
 
-    ContractClass& klass = contract_classes[idx];
+    ContractClassWithCommitment& klass = contract_classes[idx];
     ContractInstance& instance = contract_instances[idx];
     const AztecAddress& address = contract_addresses[idx];
 
@@ -51,9 +53,10 @@ void mutate_bytecode(std::vector<ContractClass>& contract_classes,
 
     // Copy into NEW contract class with updated bytecode and id, we don't modify the existing one in case
     // other instances refer to it
-    ContractClass new_class = klass;
+    ContractClassWithCommitment new_class = klass;
     new_class.id = new_class_id;
     new_class.packed_bytecode = std::move(bytecode);
+    new_class.public_bytecode_commitment = new_bytecode_commitment;
 
     // Update instance's current class ID to point to the newly upgraded-to class
     fuzz_info("Contract at address ", address, ", upgraded from ", original_class_id, " -> ", new_class_id);
@@ -77,6 +80,66 @@ void mutate_bytecode(std::vector<ContractClass>& contract_classes,
             { FF(DOM_SEP__PUBLIC_LEAF_INDEX), FF(CONTRACT_INSTANCE_REGISTRY_CONTRACT_ADDRESS), storage_slot });
         public_data_writes.push_back(bb::crypto::merkle_tree::PublicDataLeafValue{ leaf_slot, values[i] });
     }
+}
+
+void mutate_contract_classes(std::vector<ContractClassWithCommitment>& contract_classes,
+                             std::vector<ContractInstance>& contract_instances,
+                             std::vector<AztecAddress>& contract_addresses,
+                             std::mt19937_64& rng)
+{
+    // Skip if no contracts to mutate
+    if (contract_classes.empty()) {
+        return;
+    }
+
+    // Select a random contract
+    size_t idx = std::uniform_int_distribution<size_t>(0, contract_classes.size() - 1)(rng);
+
+    ContractClassWithCommitment& klass = contract_classes[idx];
+
+    // We don't mutate the packed bytecode here, only some metadata fields
+    // We also do not mutate the class id directly such that it may fail.
+    // The fuzzer (and AVM) asserts that class IDs are correctly derived in class_id_derivation.cpp
+    auto choice = std::uniform_int_distribution<int>(0, 1)(rng);
+    switch (choice) {
+    case 0:
+        // Mutate artifact hash
+        mutate_field(klass.artifact_hash, rng, BASIC_FIELD_MUTATION_CONFIGURATION);
+        break;
+    case 1:
+        // Mutate private functions root
+        mutate_field(klass.private_functions_root, rng, BASIC_FIELD_MUTATION_CONFIGURATION);
+        break;
+    default:
+        break;
+    }
+
+    auto new_class_id = simulation::compute_contract_class_id(
+        klass.artifact_hash, klass.private_functions_root, klass.public_bytecode_commitment);
+
+    for (size_t i = 0; i < contract_instances.size(); i++) {
+        // Check if an original instance (i.e. it has address nullifier) refers to this class
+        if (contract_instances[i].original_contract_class_id == klass.id) {
+            fuzz_info("Mutated original contract class ", klass.id, " at address ", contract_addresses[i]);
+            // Since this is an original instance, we also need to update the address nullifier
+            auto original_address = simulation::compute_contract_address(contract_instances[i]);
+            // Update instance's current class ID to point to the mutated class
+            contract_instances[i].current_contract_class_id = new_class_id;
+            contract_instances[i].original_contract_class_id = new_class_id;
+            auto contract_addr = std::ranges::find_if(
+                contract_addresses, [&](const AztecAddress& addr) { return addr == original_address; });
+            *contract_addr = simulation::compute_contract_address(contract_instances[i]);
+        }
+        // Check if an upgraded instance refers to this class
+        if (contract_instances[i].current_contract_class_id == klass.id) {
+            fuzz_info("Mutated current contract class ", klass.id, " at address ", contract_addresses[i]);
+            // Update instance's current class ID to point to the mutated class
+            contract_instances[i].current_contract_class_id = new_class_id;
+            // No need to update address as it depends on original class ID only
+        }
+    }
+    // Update class ID
+    klass.id = new_class_id;
 }
 
 } // namespace bb::avm2::fuzzer
--- barretenberg/cpp/src/barretenberg/avm_fuzzer/mutations/bytecode.hpp
+++ barretenberg/cpp/src/barretenberg/avm_fuzzer/mutations/bytecode.hpp
@@ -7,10 +7,15 @@
 
 namespace bb::avm2::fuzzer {
 
-void mutate_bytecode(std::vector<ContractClass>& contract_classes,
+void mutate_bytecode(std::vector<ContractClassWithCommitment>& contract_classes,
                      std::vector<ContractInstance>& contract_instances,
                      const std::vector<AztecAddress>& contract_addresses,
                      std::vector<bb::crypto::merkle_tree::PublicDataLeafValue>& public_data_writes,
                      std::mt19937_64& rng);
+
+void mutate_contract_classes(std::vector<ContractClassWithCommitment>& contract_classes,
+                             std::vector<ContractInstance>& contract_instances,
+                             std::vector<AztecAddress>& contract_addresses,
+                             std::mt19937_64& rng);
 
 } // namespace bb::avm2::fuzzer
