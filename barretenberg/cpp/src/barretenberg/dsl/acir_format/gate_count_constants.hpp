a32458295ca0033474fec784f6ad95bf2ed625a7
suyash67 <suyashnbagad1997@gmail.com>
2026-01-15 10:16:15 +0000

    update avm recursive gate count.

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
@@ -159,7 +159,7 @@ inline constexpr size_t ECCVM_RECURSIVE_VERIFIER_GATE_COUNT = 214907;
 // Goblin AVM Recursive Verifier Constants
 // ========================================
 
-inline constexpr size_t GOBLIN_AVM_GATE_COUNT = 3316531;
-inline constexpr size_t FINALIZED_GOBLIN_AVM_GATE_COUNT = 3316547;
+inline constexpr size_t GOBLIN_AVM_GATE_COUNT = 3316528;
+inline constexpr size_t FINALIZED_GOBLIN_AVM_GATE_COUNT = 3316544;
 
 } // namespace acir_format
