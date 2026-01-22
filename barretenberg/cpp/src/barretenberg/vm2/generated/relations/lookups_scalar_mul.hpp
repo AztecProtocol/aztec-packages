2121212cd1e77e2e7d9d49ab50bfd28896b649e6
MirandaWood <miranda@aztecprotocol.com>
2026-01-22 12:56:20 +0000

    chore: reduce lookup tuple for double in scalarmul

--- barretenberg/cpp/src/barretenberg/vm2/generated/relations/lookups_scalar_mul.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/generated/relations/lookups_scalar_mul.hpp
@@ -44,22 +44,21 @@ using lookup_scalar_mul_to_radix_relation = lookup_relation_base<FF_, lookup_sca
 struct lookup_scalar_mul_double_settings_ {
     static constexpr std::string_view NAME = "LOOKUP_SCALAR_MUL_DOUBLE";
     static constexpr std::string_view RELATION_NAME = "scalar_mul";
-    static constexpr size_t LOOKUP_TUPLE_SIZE = 9;
+    static constexpr size_t LOOKUP_TUPLE_SIZE = 7;
     static constexpr Column SRC_SELECTOR = Column::scalar_mul_sel_not_end;
     static constexpr Column DST_SELECTOR = Column::ecc_sel;
     static constexpr Column COUNTS = Column::lookup_scalar_mul_double_counts;
     static constexpr Column INVERSES = Column::lookup_scalar_mul_double_inv;
     static constexpr std::array<ColumnAndShifts, LOOKUP_TUPLE_SIZE> SRC_COLUMNS = {
-        ColumnAndShifts::scalar_mul_temp_x,        ColumnAndShifts::scalar_mul_temp_y,
-        ColumnAndShifts::scalar_mul_temp_inf,      ColumnAndShifts::scalar_mul_temp_x_shift,
-        ColumnAndShifts::scalar_mul_temp_y_shift,  ColumnAndShifts::scalar_mul_temp_inf_shift,
-        ColumnAndShifts::scalar_mul_temp_x_shift,  ColumnAndShifts::scalar_mul_temp_y_shift,
-        ColumnAndShifts::scalar_mul_temp_inf_shift
+        ColumnAndShifts::scalar_mul_temp_x,       ColumnAndShifts::scalar_mul_temp_y,
+        ColumnAndShifts::scalar_mul_temp_inf,     ColumnAndShifts::scalar_mul_temp_x_shift,
+        ColumnAndShifts::scalar_mul_temp_y_shift, ColumnAndShifts::scalar_mul_temp_inf_shift,
+        ColumnAndShifts::scalar_mul_sel_not_end
     };
     static constexpr std::array<ColumnAndShifts, LOOKUP_TUPLE_SIZE> DST_COLUMNS = {
-        ColumnAndShifts::ecc_r_x, ColumnAndShifts::ecc_r_y, ColumnAndShifts::ecc_r_is_inf,
-        ColumnAndShifts::ecc_p_x, ColumnAndShifts::ecc_p_y, ColumnAndShifts::ecc_p_is_inf,
-        ColumnAndShifts::ecc_q_x, ColumnAndShifts::ecc_q_y, ColumnAndShifts::ecc_q_is_inf
+        ColumnAndShifts::ecc_r_x,      ColumnAndShifts::ecc_r_y, ColumnAndShifts::ecc_r_is_inf,
+        ColumnAndShifts::ecc_p_x,      ColumnAndShifts::ecc_p_y, ColumnAndShifts::ecc_p_is_inf,
+        ColumnAndShifts::ecc_double_op
     };
 };
 
