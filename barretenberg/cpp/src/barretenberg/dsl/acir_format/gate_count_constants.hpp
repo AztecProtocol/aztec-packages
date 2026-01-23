eb35701be6da2dcc15a329a469caa7a62a9c68eb
iakovenkos <sergey.s.yakovenko@gmail.com>
2026-01-23 12:45:18 +0000

    fix gate counts

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
@@ -75,7 +75,7 @@ constexpr std::tuple<size_t, size_t> HONK_RECURSION_CONSTANTS(
     } else if constexpr (std::is_same_v<RecursiveFlavor, bb::UltraZKRecursiveFlavor_<UltraCircuitBuilder>>) {
         switch (mode) {
         case PredicateTestCase::ConstantTrue:
-            return std::make_tuple(767417, 0);
+            return std::make_tuple(766164, 0);
         case PredicateTestCase::WitnessTrue:
         case PredicateTestCase::WitnessFalse:
             return std::make_tuple(767417, 0);
