6ef5cf505cc41555c99de5852dd0489815b507b1
alexghr <3816165+alexghr@users.noreply.github.com>
2026-01-23 10:18:35 +0000

    chore: cleanup RPC ingresses
    Adds a nextnet ingress and removes unused ones.

--- spartan/terraform/gke-cluster/network-ingress.tf
+++ spartan/terraform/gke-cluster/network-ingress.tf
@@ -1,25 +1,3 @@
-resource "google_compute_global_address" "staging_public_rpc_ip" {
-  name        = "staging-public-rpc-ip"
-  description = "Static IP for staging-public network RPC ingress"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "google_compute_managed_ssl_certificate" "staging_public_rpc_cert" {
-  name        = "staging-public-rpc-cert"
-  description = "Managed SSL certificate for staging-public RPC ingress"
-
-  managed {
-    domains = ["staging.alpha-testnet.aztec-labs.com"]
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 resource "google_compute_global_address" "testnet_rpc_ip" {
   name        = "testnet-rpc-ip"
   description = "Static IP for testnet RPC ingress"
@@ -35,6 +13,28 @@ resource "google_compute_managed_ssl_certificate" "testnet_rpc_cert" {
 
   managed {
     domains = ["rpc.testnet.aztec-labs.com"]
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "google_compute_global_address" "nextnet_rpc_ip" {
+  name        = "nextnet-rpc-ip"
+  description = "Static IP for nextnet RPC ingress"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "google_compute_managed_ssl_certificate" "nextnet_rpc_cert" {
+  name        = "nextnet-rpc-cert"
+  description = "Managed SSL certificate for nextnet RPC ingress"
+
+  managed {
+    domains = ["nextnet.aztec-labs.com"]
   }
 
   lifecycle {
