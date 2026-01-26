8ddfe34f13a439c72a059ce642b4643c2f6b6997
iakovenkos <sergey.s.yakovenko@gmail.com>
2026-01-26 19:15:04 +0000

    don't use get_value to compute_num_public inputs

--- barretenberg/cpp/src/barretenberg/ultra_honk/oink_verifier.cpp
+++ barretenberg/cpp/src/barretenberg/ultra_honk/oink_verifier.cpp
@@ -69,10 +69,6 @@ template <typename Flavor> void OinkVerifier<Flavor>::execute_preamble_round()
         vk->num_public_inputs.assert_equal(FF(num_public_inputs), "OinkVerifier: num_public_inputs mismatch with VK");
     } else {
         BB_ASSERT_EQ(verifier_instance->vk_and_hash->hash, vk_hash, "Native Ultra Verifier: VK Hash Mismatch");
-        // Assert that the provided num_public_inputs matches VK's value
-        BB_ASSERT_EQ(num_public_inputs,
-                     static_cast<size_t>(vk->num_public_inputs),
-                     "OinkVerifier: num_public_inputs mismatch with VK");
     };
 
     std::vector<FF> public_inputs;
