5866e7eeef8cd2033f99af97d722cf78f4ab033d
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2026-01-26 17:39:50 +0000

    Update script and inputs

--- barretenberg/cpp/scripts/test_chonk_standalone_vks_havent_changed.sh
+++ barretenberg/cpp/scripts/test_chonk_standalone_vks_havent_changed.sh
@@ -13,7 +13,7 @@ cd ..
 # - Generate a hash for versioning: sha256sum bb-chonk-inputs.tar.gz
 # - Upload the compressed results: aws s3 cp bb-chonk-inputs.tar.gz s3://aztec-ci-artifacts/protocol/bb-chonk-inputs-[hash(0:8)].tar.gz
 # Note: In case of the "Test suite failed to run ... Unexpected token 'with' " error, need to run: docker pull aztecprotocol/build:3.0
-pinned_short_hash="2b022426"
+pinned_short_hash="77e58648"
 pinned_chonk_inputs_url="https://aztec-ci-artifacts.s3.us-east-2.amazonaws.com/protocol/bb-chonk-inputs-${pinned_short_hash}.tar.gz"
 
 script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")/scripts" && pwd)/$(basename "${BASH_SOURCE[0]}")"
@@ -152,7 +152,7 @@ elif [[ "${1:-}" == "--update_inputs" ]]; then
     compress_and_upload ../../yarn-project/end-to-end/example-app-ivc-inputs-out
 
     prove_exit_code=0
-    parallel -v --line-buffer --tag prove_and_verify_inputs {} ::: $(ls "$inputs_tmp_dir") || prove_exit_code=$?
+    parallel -v --line-buffer --tag prove_and_verify_inputs {} ::: $(ls ../../yarn-project/end-to-end/example-app-ivc-inputs-out) || prove_exit_code=$?
 
     if [[ $prove_exit_code -eq 1 ]]; then
       echo "One or more flows failed the proof test after updating inputs. Please investigate."
