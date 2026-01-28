b211fe06e2622586d8ce6210ee427a71630d5a38
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2026-01-28 17:16:16 +0000

    Docs in logderivative library for lookup accumulation

--- barretenberg/cpp/src/barretenberg/honk/proof_system/logderivative_library.hpp
+++ barretenberg/cpp/src/barretenberg/honk/proof_system/logderivative_library.hpp
@@ -91,20 +91,21 @@ void compute_logderivative_inverse(Polynomials& polynomials, auto& relation_para
 
 /**
  * @brief Compute generic log-derivative lookup subrelation accumulation
- * @details The generic log-derivative lookup relation consistes of two subrelations. The first demonstrates that the
- * inverse polynomial I, defined via I_i =  1/[(read_term_i) * (write_term_i)], has been computed correctly. The second
- * establishes the correctness of the lookups themselves based on the log-derivative lookup argument. Note that the
- * latter subrelation is "linearly dependent" in the sense that it establishes that a sum across all rows of the
+ * @details The generic log-derivative lookup relation consists of two subrelations. The first demonstrates that the
+ * inverse polynomial I, defined via I_i =  1/[(lookup_term_i) * (table_term_i)], has been computed correctly. The
+ * second establishes the correctness of the lookups themselves based on the log-derivative lookup argument. Note that
+ * the latter subrelation is "linearly dependent" in the sense that it establishes that a sum across all rows of the
  * exectution trace is zero, rather than that some expression holds independently at each row. Accordingly, this
  * subrelation is not multiplied by a scaling factor at each accumulation step. The subrelation expressions are
  * respectively:
  *
- *  I_i * (read_term_i) * (write_term_i) - 1 = 0
+ *  I_i * (lookup_term_i) * (table_term_i) - 1 = 0
  *
- * \sum_{i=0}^{n-1} [q_{logderiv_lookup} * I_i * write_term_i + read_count_i * I_i * read_term_i] = 0
+ * \sum_{i=0}^{n-1} [lookup_term_predicate_i * 1 /lookup_term_i
+ *               + lookup_count_i * table_term_predicate_i * 1 / table_term_i] = 0
  *
- * The explicit expressions for read_term and write_term are dependent upon the particular structure of the lookup being
- * performed and methods for computing them must be defined in the corresponding relation class.
+ * The explicit expressions for lookup_term and table_term are dependent upon the particular structure of the lookup
+ * being performed and methods for computing them must be defined in the corresponding relation class.
  *
  * @tparam FF
  * @tparam Relation
@@ -140,42 +141,50 @@ void accumulate_logderivative_lookup_subrelation_contributions(ContainerOverSubr
     // The purpose of this next section is to derive individual inverse terms using `lookup_inverses`
     // i.e. (1 / read_term[i]) = lookup_inverse * \prod_{j /ne i} (read_term[j]) * \prod_k (write_term[k])
     //      (1 / write_term[i]) = lookup_inverse * \prod_j (read_term[j]) * \prod_{k ne i} (write_term[k])
+
+    // The inverse polynomial gives us the produce of all the inverses, i.e.
+    // lookup_inverse = \prod_j (1 / lookup_term[j]) * \prod_k (1 / table_term[k])
+    // To obtain the inverses 1 / lookup_term[i], 1 / table_term[i], we multiply lookup_inverse by the product of all
+    // terms except the one we want to invert. We perform this calculation via the following algorithm:
+    // 1) Compute the successive products of all lookup terms and table terms
+    // 2) Check that lookup_inverse is the inverse of the full product
+    // 3) Iteratively compute the inverses as follows:
+    //      (lookup_term_1 * .. * lookup_term_(i-1)) * lookup_inverse * (lookup_term_(i+1) * .. * table_term_m)
+
+    // Collect all the terms in a single vector, first the lookup terms, then the table terms
     bb::constexpr_for<0, NUM_LOOKUP_TERMS, 1>(
         [&]<size_t i>() { lookup_terms[i] = Relation::template compute_lookup_term<Accumulator, i>(in, params); });
     bb::constexpr_for<0, NUM_TABLE_TERMS, 1>([&]<size_t i>() {
         lookup_terms[i + NUM_LOOKUP_TERMS] = Relation::template compute_table_term<Accumulator, i>(in, params);
     });
 
+    // 1) Construct the successive products
     bb::constexpr_for<0, NUM_TOTAL_TERMS, 1>([&]<size_t i>() { denominator_accumulator[i] = lookup_terms[i]; });
-
     bb::constexpr_for<0, NUM_TOTAL_TERMS - 1, 1>(
         [&]<size_t i>() { denominator_accumulator[i + 1] *= denominator_accumulator[i]; });
 
-    auto inverse_accumulator = Accumulator(lookup_inverses); // denominator_accumulator[NUM_TOTAL_TERMS - 1];
-
+    // 2) First subrelation: check that lookup_inverse is the inverse of the cumulative product if inverse_exists = 1
+    auto inverse_accumulator = Accumulator(lookup_inverses);
     const auto inverse_exists = Relation::template compute_inverse_exists<Accumulator>(in);
 
-    // Note: the lookup_inverses are computed so that the value is 0 if !inverse_exists
     std::get<0>(accumulator) +=
         (denominator_accumulator[NUM_TOTAL_TERMS - 1] * lookup_inverses - inverse_exists) * scaling_factor;
 
-    // After this algo, total degree of denominator_accumulator = NUM_TOTAL_TERMS
-    for (size_t i = 0; i < NUM_TOTAL_TERMS - 1; ++i) {
-        denominator_accumulator[NUM_TOTAL_TERMS - 1 - i] =
-            denominator_accumulator[NUM_TOTAL_TERMS - 2 - i] * inverse_accumulator;
-        inverse_accumulator = inverse_accumulator * lookup_terms[NUM_TOTAL_TERMS - 1 - i];
+    // 3) Iteratively compute the single inverses
+    for (size_t i = NUM_TOTAL_TERMS - 1; i > 0; --i) {
+        // Take the cumulative product up to the previous index and multiply by the current inverse accumulator
+        denominator_accumulator[i] = denominator_accumulator[i - 1] * inverse_accumulator;
+        // Multiply the inverse accumulator by the current term to "remove" it from the product of the inverses
+        inverse_accumulator = inverse_accumulator * lookup_terms[i];
     }
     denominator_accumulator[0] = inverse_accumulator;
 
-    // each predicate is degree-1
-    // degree of relation at this point = NUM_TOTAL_TERMS + 1
+    // Second subrelation
     bb::constexpr_for<0, NUM_LOOKUP_TERMS, 1>([&]<size_t i>() {
         std::get<1>(accumulator) +=
             Relation::template get_lookup_term_predicate<Accumulator, i>(in) * denominator_accumulator[i];
     });
 
-    // each predicate is degree-1, `lookup_read_counts` is degree-1
-    // degree of relation = NUM_TOTAL_TERMS + 2
     bb::constexpr_for<0, NUM_TABLE_TERMS, 1>([&]<size_t i>() {
         const auto p = Relation::template get_table_term_predicate<Accumulator, i>(in);
         const auto lookup_read_count = Relation::template lookup_read_counts<Accumulator, i>(in);
