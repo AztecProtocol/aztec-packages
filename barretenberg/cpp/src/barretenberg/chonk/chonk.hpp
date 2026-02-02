135b523f7cfdcd6823fa4ef8287facd1b0676db5
Jonathan Hao <jonathan@aztec-labs.com>
2026-02-02 17:03:56 +0000

    fix: add explicit constructor to StdlibVerifierInputs for iOS libc++ compatibility

--- barretenberg/cpp/src/barretenberg/chonk/chonk.hpp
+++ barretenberg/cpp/src/barretenberg/chonk/chonk.hpp
@@ -119,6 +119,17 @@ class Chonk : public IVCBase {
         std::shared_ptr<RecursiveVKAndHash> honk_vk_and_hash;
         QUEUE_TYPE type;
         bool is_kernel = false;
+
+        // Explicit constructor needed for older libc++ (iOS SDK) compatibility with std::deque::emplace_back
+        StdlibVerifierInputs(StdlibProof proof_,
+                             std::shared_ptr<RecursiveVKAndHash> honk_vk_and_hash_,
+                             QUEUE_TYPE type_,
+                             bool is_kernel_)
+            : proof(std::move(proof_))
+            , honk_vk_and_hash(std::move(honk_vk_and_hash_))
+            , type(type_)
+            , is_kernel(is_kernel_)
+        {}
     };
     using StdlibVerificationQueue = std::deque<StdlibVerifierInputs>;
 
