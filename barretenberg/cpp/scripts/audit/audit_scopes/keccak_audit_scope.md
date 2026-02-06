635353e98dbf22d3be02e257b545319b83af98d5
nishatkoti <nishatkoti@gmail.com>
2026-02-06 17:08:19 +0530

    chore: update commit hash in blake, keccak and pedersen scoped files (#20121)
    Update commit hash in blake, keccak, pedersen scoped files.

--- barretenberg/cpp/scripts/audit/audit_scopes/keccak_audit_scope.md
+++ barretenberg/cpp/scripts/audit/audit_scopes/keccak_audit_scope.md
@@ -1,7 +1,7 @@
 # External Audit Scope: Keccak
 
 Repository: https://github.com/AztecProtocol/aztec-packages
-Commit hash: 89a12920681072efff1eed881589aad16347e0d6
+Commit hash: 5be53b6f75bac06d6d0132220044b28777021f0f
 
 ### Files to audit
 
--- barretenberg/cpp/src/barretenberg/crypto/pedersen_hash/pedersen.hpp
+++ barretenberg/cpp/src/barretenberg/crypto/pedersen_hash/pedersen.hpp
@@ -1,5 +1,5 @@
 // === AUDIT STATUS ===
-// internal:    { status: Planned, auditors: [], commit: }
+// internal:    { status: Complete, auditors: [Nishat], commit: 8c1bc925461f1ed6f3f53824646c6e971b8c6af6 }
 // external_1:  { status: not started, auditors: [], commit: }
 // external_2:  { status: not started, auditors: [], commit: }
 // =====================
