147d8d29ac639120b978906d55cb794fb0552954
suyash67 <suyashnbagad1997@gmail.com>
2026-02-06 06:50:06 +0000

    use wit inf.

--- barretenberg/cpp/src/barretenberg/stdlib/primitives/field/field_conversion.test.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/primitives/field/field_conversion.test.cpp
@@ -151,8 +151,8 @@ TYPED_TEST(stdlib_field_conversion, FieldConversionBN254AffineElement)
     { // Serialize and deserialize the point at infinity
         Builder builder;
 
-        // Use constant_infinity() for infinity points (from_witness rejects infinity)
-        bn254_element<Builder> group_element = bn254_element<Builder>::constant_infinity(&builder);
+        bn254_element<Builder> group_element =
+            bn254_element<Builder>::from_witness(&builder, curve::BN254::AffineElement::infinity());
         // Point at infinity is now consistently represented as (0, 0) across all builders
         this->check_conversion(group_element);
     }
