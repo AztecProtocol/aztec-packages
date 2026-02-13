4fbf826e60982d91907e9298916a13c99ddab345
ludamad <domuradical@gmail.com>
2026-02-13 16:35:48 +0000

    fix: update test_cmds port base in aztec-nr to match new TXE range
    The test_cmds function was still generating port 45730+ for the
    oracle-resolver URL while TXE servers now start on 14730+.

--- noir-projects/aztec-nr/bootstrap.sh
+++ noir-projects/aztec-nr/bootstrap.sh
@@ -20,7 +20,7 @@ function test_cmds {
   i=0
   $NARGO test --list-tests --silence-warnings | sort | while read -r package test; do
     # We assume there are 8 txe's running.
-    port=$((45730 + (i++ % ${NUM_TXES:-1})))
+    port=$((14730 + (i++ % ${NUM_TXES:-1})))
     echo "$hash noir-projects/scripts/run_test.sh aztec-nr $package $test $port"
   done
 }
