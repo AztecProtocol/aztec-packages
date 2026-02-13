0628a8fbb6f9289e17ebabcdfa54bf7c294747db
Ilyas Ridhuan <ilyas@aztecprotocol.com>
2026-02-13 16:30:50 +0000

    fix(avm)!: emit public log pre-audit (#20417)
    Pre-audit for the public logs opcode.
    
    Mostly verbose documentation and general cleanup. Major changes were 
    - Simplification of some constraints, 
    - Reduction of some relation degrees (which enabled the removal of 1
    column) and
    - Removing some implied boolean constraints
    - Some negative tests

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/opcodes/emit_public_log_trace.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/opcodes/emit_public_log_trace.cpp
@@ -1,11 +1,8 @@
 #include "barretenberg/vm2/tracegen/opcodes/emit_public_log_trace.hpp"
 
-#include <cstddef>
 #include <cstdint>
 
 #include "barretenberg/vm2/common/aztec_constants.hpp"
-#include "barretenberg/vm2/common/aztec_types.hpp"
-#include "barretenberg/vm2/common/constants.hpp"
 #include "barretenberg/vm2/common/memory_types.hpp"
 #include "barretenberg/vm2/common/tagged_value.hpp"
 #include "barretenberg/vm2/generated/columns.hpp"
@@ -19,16 +16,20 @@ namespace bb::avm2::tracegen {
 
 using C = Column;
 
+/**
+ * @brief Process emit-public-log events into trace rows.
+ *
+ * @param events The container of emit-public-log simulation events.
+ * @param trace The trace container to populate with constrained columns.
+ */
 void EmitPublicLogTraceBuilder::process(
     const simulation::EventEmitterInterface<simulation::EmitPublicLogEvent>::Container& events, TraceContainer& trace)
 {
     uint32_t row = 1;
     process_with_discard(events, [&](const simulation::EmitPublicLogWriteEvent& event, bool discard) {
-        // error = error_too_large | error_out_of_bounds | error_too_many_logs | error_tag_mismatch | is_static
-        // we split the above computation in 2 to reduce the degree of the full relation
-        bool error_too_many_logs_wrong_tag_is_static =
-            event.error_too_many_log_fields || event.error_tag_mismatch || event.is_static;
-        bool error = event.error_memory_out_of_bounds || error_too_many_logs_wrong_tag_is_static;
+        // error = error_out_of_bounds | error_too_many_log_fields | error_tag_mismatch | is_static
+        bool error = event.error_memory_out_of_bounds || event.error_too_many_log_fields || event.error_tag_mismatch ||
+                     event.is_static;
 
         FF log_address = FF(event.log_address);
         bool seen_wrong_tag = false;
@@ -68,47 +69,44 @@ void EmitPublicLogTraceBuilder::process(
                 public_inputs_value = value;
             }
 
-            trace.set(
-                row,
-                { {
-                    { C::emit_public_log_sel, 1 },
-                    { C::emit_public_log_execution_clk, event.execution_clk },
-                    { C::emit_public_log_space_id, event.space_id },
-                    { C::emit_public_log_log_address, log_address },
-                    { C::emit_public_log_log_size, event.log_size },
-                    { C::emit_public_log_contract_address, event.contract_address },
-                    { C::emit_public_log_prev_num_public_log_fields, event.prev_num_public_log_fields },
-                    { C::emit_public_log_next_num_public_log_fields, event.next_num_public_log_fields },
-                    { C::emit_public_log_is_static, event.is_static },
-                    { C::emit_public_log_error, error },
-                    { C::emit_public_log_discard, discard },
-                    { C::emit_public_log_start, i == 0 },
-                    { C::emit_public_log_end, i == event.log_size + PUBLIC_LOG_HEADER_LENGTH - 1 },
-                    { C::emit_public_log_remaining_rows, remaining_rows },
-                    { C::emit_public_log_remaining_rows_inv, remaining_rows }, // Will be inverted in batch later
-                    { C::emit_public_log_error_out_of_bounds, event.error_memory_out_of_bounds },
-                    { C::emit_public_log_max_mem_size, static_cast<uint64_t>(AVM_MEMORY_SIZE) },
-                    { C::emit_public_log_end_log_address_upper_bound, log_address + event.log_size },
-                    { C::emit_public_log_error_too_many_log_fields, event.error_too_many_log_fields },
-                    { C::emit_public_log_expected_next_log_fields, expected_next_log_fields },
-                    { C::emit_public_log_public_logs_payload_length, FLAT_PUBLIC_LOGS_PAYLOAD_LENGTH },
-                    { C::emit_public_log_error_tag_mismatch, event.error_tag_mismatch },
-                    { C::emit_public_log_seen_wrong_tag, seen_wrong_tag },
-                    { C::emit_public_log_error_too_many_logs_wrong_tag_is_static,
-                      error_too_many_logs_wrong_tag_is_static },
-                    { C::emit_public_log_sel_should_write_to_public_inputs, !error && !discard },
-                    { C::emit_public_log_is_write_contract_address, is_contract_address_row },
-                    { C::emit_public_log_is_write_memory_value, is_value_row },
-                    { C::emit_public_log_sel_should_read_memory, is_value_row && !event.error_memory_out_of_bounds },
-                    { C::emit_public_log_value, value },
-                    { C::emit_public_log_tag, static_cast<uint8_t>(tag) },
-                    { C::emit_public_log_correct_tag, correct_tag },
-                    { C::emit_public_log_tag_inv, static_cast<uint8_t>(tag) }, // Will be inverted in batch later
-                    { C::emit_public_log_public_inputs_index,
-                      AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_PUBLIC_LOGS_ROW_IDX + FLAT_PUBLIC_LOGS_HEADER_LENGTH +
-                          event.prev_num_public_log_fields + i },
-                    { C::emit_public_log_public_inputs_value, public_inputs_value },
-                } });
+            trace.set(row,
+                      { {
+                          { C::emit_public_log_sel, 1 },
+                          { C::emit_public_log_execution_clk, event.execution_clk },
+                          { C::emit_public_log_space_id, event.space_id },
+                          { C::emit_public_log_log_address, log_address },
+                          { C::emit_public_log_log_size, event.log_size },
+                          { C::emit_public_log_contract_address, event.contract_address },
+                          { C::emit_public_log_prev_num_public_log_fields, event.prev_num_public_log_fields },
+                          { C::emit_public_log_next_num_public_log_fields, event.next_num_public_log_fields },
+                          { C::emit_public_log_is_static, event.is_static },
+                          { C::emit_public_log_error, error },
+                          { C::emit_public_log_discard, discard },
+                          { C::emit_public_log_start, i == 0 },
+                          { C::emit_public_log_end, i == event.log_size + PUBLIC_LOG_HEADER_LENGTH - 1 },
+                          { C::emit_public_log_remaining_rows, remaining_rows },
+                          { C::emit_public_log_remaining_rows_inv, remaining_rows }, // Will be inverted in batch later
+                          { C::emit_public_log_error_out_of_bounds, event.error_memory_out_of_bounds },
+                          { C::emit_public_log_max_mem_size, static_cast<uint64_t>(AVM_MEMORY_SIZE) },
+                          { C::emit_public_log_end_log_address_upper_bound, log_address + event.log_size },
+                          { C::emit_public_log_error_too_many_log_fields, event.error_too_many_log_fields },
+                          { C::emit_public_log_expected_next_log_fields, expected_next_log_fields },
+                          { C::emit_public_log_max_public_logs_payload_length, FLAT_PUBLIC_LOGS_PAYLOAD_LENGTH },
+                          { C::emit_public_log_error_tag_mismatch, event.error_tag_mismatch },
+                          { C::emit_public_log_seen_wrong_tag, seen_wrong_tag },
+                          { C::emit_public_log_sel_write_to_public_inputs, !error && !discard },
+                          { C::emit_public_log_is_write_contract_address, is_contract_address_row },
+                          { C::emit_public_log_is_write_memory_value, is_value_row },
+                          { C::emit_public_log_sel_read_memory, is_value_row && !event.error_memory_out_of_bounds },
+                          { C::emit_public_log_value, value },
+                          { C::emit_public_log_tag, static_cast<uint8_t>(tag) },
+                          { C::emit_public_log_correct_tag, correct_tag },
+                          { C::emit_public_log_tag_inv, static_cast<uint8_t>(tag) }, // Will be inverted in batch later
+                          { C::emit_public_log_public_inputs_index,
+                            AVM_PUBLIC_INPUTS_AVM_ACCUMULATED_DATA_PUBLIC_LOGS_ROW_IDX +
+                                FLAT_PUBLIC_LOGS_HEADER_LENGTH + event.prev_num_public_log_fields + i },
+                          { C::emit_public_log_public_inputs_value, public_inputs_value },
+                      } });
 
             row++;
             if (is_value_row) {
