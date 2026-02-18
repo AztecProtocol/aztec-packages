57542a55b7e1db76279abd93361e2a3571b24876
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2026-02-18 15:24:25 +0000

    Auditing

--- barretenberg/cpp/src/barretenberg/crypto/ecdsa/ecdsa.hpp
+++ barretenberg/cpp/src/barretenberg/crypto/ecdsa/ecdsa.hpp
@@ -15,9 +15,13 @@
 #include <string>
 
 namespace bb::crypto {
+
+static constexpr int ECDSA_RECOVERY_ID_OFFSET = 27;
+
 template <typename Fr, typename G1> struct ecdsa_key_pair {
     Fr private_key;
-    typename G1::affine_element public_key;
+    G1::affine_element public_key;
+
     // For serialization, update with any new fields
     MSGPACK_FIELDS(private_key, public_key);
 };
@@ -26,21 +30,54 @@ struct ecdsa_signature {
     std::array<uint8_t, 32> r;
     std::array<uint8_t, 32> s;
     uint8_t v;
+
     // For serialization, update with any new fields
     MSGPACK_FIELDS(r, s, v);
 };
 
+/**
+ * @brief Generate the ECDSA for the message using the provided account key pair and hash function
+ *
+ */
 template <typename Hash, typename Fq, typename Fr, typename G1>
 ecdsa_signature ecdsa_construct_signature(const std::string& message, const ecdsa_key_pair<Fr, G1>& account);
 
+/**
+ * @brief Given a message and a valid signature with recovery_id, recover the public key whose private key was used to
+ * sign the message
+ *
+ * @details Given \f$(m, (r, s))\f$ a valid signature for message m, we can recover up to four public keys for which
+ * this is a valid signature: a public key P is valid if \f$H(m) \cdot s^{-1} G + r \cdot s^{-1} P = \pm R\f$, where R
+ * is the point on the curve with x-coordinate equal to r. Given r, there can be two points R with that x-coordinate.
+ * Solving the equation for \f$P\f$, gives \f$P = r^{-1} \cdot s \cdot (- H(m) \cdot s^{-1} G + \pm R)$, which means we
+ * have up to four possibilities for \f$P\f$. We use a recovery_id \f$v\f$ to select which of the four public keys to
+ * get. The mapping is: recovery_id = offset (=27) +
+ *  y is even  &&  x < |Fr| --> 0
+ *  y is odd   &&  x < |Fr| --> 1
+ *  y is even  &&  |Fr| <= x < |Fq| --> 2
+ *  y is odd   &&  |Fr| <= x < |Fq| --> 3
+ *
+ */
 template <typename Hash, typename Fq, typename Fr, typename G1>
 typename G1::affine_element ecdsa_recover_public_key(const std::string& message, const ecdsa_signature& sig);
 
-// TODO(https://github.com/AztecProtocol/barretenberg/issues/659)
+/**
+ * @brief Verify the ECDSA signature for the message using the provided public key and hash function
+ *
+ */
 template <typename Hash, typename Fq, typename Fr, typename G1>
 bool ecdsa_verify_signature(const std::string& message,
                             const typename G1::affine_element& public_key,
                             const ecdsa_signature& signature);
+
+/**
+ * @brief Hash the message and trim the hash output if required
+ *
+ * @details Following https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-5.pdf, if the length of the hash output is
+ * longer than the bit length of the modulus of the scalar field, we trim the hash output.
+ *
+ */
+template <typename Hash, typename Fr> Fr ecdsa_hash_message(const std::string& message);
 
 inline bool operator==(ecdsa_signature const& lhs, ecdsa_signature const& rhs)
 {
@@ -55,4 +92,4 @@ inline std::ostream& operator<<(std::ostream& os, ecdsa_signature const& sig)
 
 } // namespace bb::crypto
 
-#include "./ecdsa_impl.hpp"
+#include "ecdsa_impl.hpp"
--- barretenberg/cpp/src/barretenberg/crypto/ecdsa/ecdsa_impl.hpp
+++ barretenberg/cpp/src/barretenberg/crypto/ecdsa/ecdsa_impl.hpp
@@ -6,9 +6,12 @@
 
 #pragma once
 
+#include <algorithm>
+
 #include "../hmac/hmac.hpp"
 #include "barretenberg/common/serialize.hpp"
 #include "barretenberg/numeric/uint256/uint256.hpp"
+#include "ecdsa.hpp"
 
 namespace bb::crypto {
 
@@ -17,43 +20,44 @@ ecdsa_signature ecdsa_construct_signature(const std::string& message, const ecds
 {
     ecdsa_signature sig;
 
-    // use HMAC in PRF mode to derive 32-byte secret `k`
+    // Hash the message and convert it to an element of Fr
+    Fr z = ecdsa_hash_message<Hash, Fr>(message);
+
+    // Derived secret `k` using deterministic derivation according to RFC6979
     std::vector<uint8_t> pkey_buffer;
     write(pkey_buffer, account.private_key);
-    Fr k = crypto::get_unbiased_field_from_hmac<Hash, Fr>(message, pkey_buffer);
+    Fr k = crypto::deterministic_nonce_rfc6979<Hash, Fr>(message, pkey_buffer);
 
+    // Compute R = k * G
     typename G1::affine_element R(G1::one * k);
-    Fq::serialize_to_buffer(R.x, &sig.r[0]);
 
-    std::vector<uint8_t> message_buffer;
-    std::copy(message.begin(), message.end(), std::back_inserter(message_buffer));
-    auto ev = Hash::hash(message_buffer);
-
-    Fr z = Fr::serialize_from_buffer(&ev[0]);
-    Fr r_fr = Fr::serialize_from_buffer(&sig.r[0]);
+    // Compute the signature
+    Fr r_fr = Fr(R.x);
     Fr s_fr = (z + r_fr * account.private_key) / k;
 
     // Ensure that the value of s is "low", i.e. s := min{ s_fr, (|Fr| - s_fr) }
-    const bool is_s_low = (uint256_t(s_fr) < (uint256_t(Fr::modulus) / 2));
+    const bool is_s_low = (uint256_t(s_fr) < (uint256_t(Fr::modulus) + 1) / 2);
     uint256_t s_uint256 = is_s_low ? uint256_t(s_fr) : (uint256_t(Fr::modulus) - uint256_t(s_fr));
 
+    Fr::serialize_to_buffer(r_fr, &sig.r[0]);
     Fr::serialize_to_buffer(Fr(s_uint256), &sig.s[0]);
 
-    // compute recovery_id: given R = (x, y)
+    // Compute recovery_id: given R = (x, y)
     //   0: y is even  &&  x < |Fr|
     //   1: y is odd   &&  x < |Fr|
     //   2: y is even  &&  |Fr| <= x < |Fq|
     //   3: y is odd   &&  |Fr| <= x < |Fq|
     // v = offset + recovery_id
-    Fq r_fq = Fq(R.x);
-    bool is_r_finite = (uint256_t(r_fq) == uint256_t(r_fr));
+    bool is_r_finite = (uint256_t(R.x) == uint256_t(r_fr));
     bool y_parity = uint256_t(R.y).get_bit(0);
-    bool recovery_bit = y_parity ^ is_s_low;
-    constexpr uint8_t offset = 27;
+    // When s is negated (low-s normalization), the effective nonce point becomes -R,
+    // which has the opposite y-parity. Flip y_parity accordingly.
+    bool recovery_id = is_s_low ? y_parity : !y_parity;
 
-    int value = offset + recovery_bit + static_cast<uint8_t>(2) * !is_r_finite;
+    int value = is_r_finite ? ECDSA_RECOVERY_ID_OFFSET + recovery_id : ECDSA_RECOVERY_ID_OFFSET + recovery_id + 2;
     BB_ASSERT_LTE(value, UINT8_MAX);
     sig.v = static_cast<uint8_t>(value);
+
     return sig;
 }
 
@@ -61,7 +65,9 @@ template <typename Hash, typename Fq, typename Fr, typename G1>
 typename G1::affine_element ecdsa_recover_public_key(const std::string& message, const ecdsa_signature& sig)
 {
     using serialize::read;
-    using Point = G1::affine_element;
+    using AffineElement = G1::affine_element;
+
+    // Read the signature components
     uint256_t r_uint;
     uint256_t s_uint;
     uint8_t v_uint;
@@ -73,17 +79,17 @@ typename G1::affine_element ecdsa_recover_public_key(const std::string& message,
     read(r_buf, r_uint);
     read(s_buf, s_uint);
     read(v_buf, v_uint);
-
-    // We need to check that r and s are in Field according to specification
-    BB_ASSERT((r_uint < mod) && (s_uint < mod), "r or s value exceeds the modulus");
-    BB_ASSERT((r_uint != 0) && (s_uint != 0), "r or s value is zero");
-
-    // Check that the s value is less than |Fr| / 2
-    BB_ASSERT_LTE(s_uint * 2, mod, "s value is not less than curve order by 2");
-
-    // Check that v must either be in {27, 28, 29, 30}
     Fr r = Fr(r_uint);
     Fr s = Fr(s_uint);
+
+    // Validate signature components according to specification
+    BB_ASSERT_LT(r_uint, mod, "r value exceeds the modulus");
+    BB_ASSERT_LT(s_uint, mod, "s value exceeds the modulus");
+    BB_ASSERT_LT(s_uint, (mod + 1) / 2, "s value is not less than curve order by 2");
+    BB_ASSERT(r_uint != 0, "r value is zero");
+    BB_ASSERT(s_uint != 0, "s value is zero");
+
+    // Check that v is in {27, 28, 29, 30} and then bring it back to the range {0, 1, 2, 3}
     Fq r_fq = Fq(r_uint);
     bool is_r_finite = true;
 
@@ -95,32 +101,32 @@ typename G1::affine_element ecdsa_recover_public_key(const std::string& message,
     } else {
         bb::assert_failure("v value is not in {27, 28, 29, 30}");
     }
+    v_uint -= ECDSA_RECOVERY_ID_OFFSET;
 
     // Decompress the x-coordinate r_uint to get two possible R points
     // The first uncompressed R point is selected when r < |Fr|
     // The second uncompressed R point is selected when |Fr| <= r < |Fq|
-    // Note that the second condition can occur with probability 1/2^128 so its highly unlikely.
-    auto uncompressed_points = Point::from_compressed_unsafe(r_uint);
-    Point point_R = uncompressed_points[!is_r_finite];
+    // Note that the second condition occurs with very low probability, so it's highly unlikely.
+    auto uncompressed_points = AffineElement::from_compressed_unsafe(r_uint);
+    AffineElement point_R = uncompressed_points[is_r_finite ? 0 : 1];
 
     // Negate the y-coordinate point of R based on the parity of v
     bool y_parity_R = uint256_t(point_R.y).get_bit(0);
-    if ((v_uint & 1) ^ y_parity_R) {
+    bool v_parity = v_uint & 1;
+    // Flip the sign of R.y if either v is even and R.y is odd, or v is odd and R.y is even
+    if ((v_parity && !y_parity_R) || (!v_parity && y_parity_R)) {
         point_R.y = -point_R.y;
     }
 
     // Start key recovery algorithm
-    std::vector<uint8_t> message_buffer;
-    std::copy(message.begin(), message.end(), std::back_inserter(message_buffer));
-    auto ev = Hash::hash(message_buffer);
-    Fr z = Fr::serialize_from_buffer(&ev[0]);
+    Fr z = ecdsa_hash_message<Hash, Fr>(message);
 
     Fr r_inv = r.invert();
 
     Fr u1 = -(z * r_inv);
     Fr u2 = s * r_inv;
 
-    Point recovered_public_key(Point(point_R) * u2 + G1::one * u1);
+    AffineElement recovered_public_key(point_R * u2 + G1::one * u1);
     return recovered_public_key;
 }
 
@@ -130,45 +136,81 @@ bool ecdsa_verify_signature(const std::string& message,
                             const ecdsa_signature& sig)
 {
     using serialize::read;
-    uint256_t r_uint;
-    uint256_t s_uint;
-    uint256_t mod = uint256_t(Fr::modulus);
+    using Element = G1::element;
+    using AffineElement = G1::affine_element;
+
+    // Validate that the public key is on the curve and not the point at infinity
     if ((!public_key.on_curve()) || (public_key.is_point_at_infinity())) {
         return false;
     }
+
+    // Deserialize the signature and validate the components
+    uint256_t r_uint;
+    uint256_t s_uint;
+    uint256_t mod = uint256_t(Fr::modulus);
+
     const auto* r_buf = &sig.r[0];
     const auto* s_buf = &sig.s[0];
     read(r_buf, r_uint);
     read(s_buf, s_uint);
+
     // We need to check that r and s are in Field according to specification
-    if ((r_uint >= mod) || (s_uint >= mod)) {
+    if (r_uint >= mod) {
+        vinfo("The r component of the signature exceeds the modulus of the scalar field of the curve.");
         return false;
     }
-    if ((r_uint == 0) || (s_uint == 0)) {
+    if (s_uint >= (mod + 1) / 2) {
+        vinfo("The s component of the signature exceeds (modulus + 1) / 2.");
+        return false;
+    }
+    if (r_uint == 0) {
+        vinfo("The r component of the signature is zero.");
+        return false;
+    }
+    if (s_uint == 0) {
+        vinfo("The s component of the signature is zero.");
         return false;
     }
 
-    // Check that the s value is less than |Fr| / 2
-    BB_ASSERT_LT(s_uint, (mod + 1) / 2, "s value is not less than curve order by 2");
+    // Hash the message
+    Fr z = ecdsa_hash_message<Hash, Fr>(message);
 
+    // Validate the signature
     Fr r = Fr(r_uint);
     Fr s = Fr(s_uint);
-
-    std::vector<uint8_t> message_buffer;
-    std::copy(message.begin(), message.end(), std::back_inserter(message_buffer));
-    auto ev = Hash::hash(message_buffer);
-    Fr z = Fr::serialize_from_buffer(&ev[0]);
 
     Fr s_inv = s.invert();
 
     Fr u1 = z * s_inv;
     Fr u2 = r * s_inv;
 
-    typename G1::affine_element R((typename G1::element(public_key) * u2) + (G1::one * u1));
-    BB_ASSERT_EQ(R.is_point_at_infinity(), false, "Result of the scalar multiplication is the point at infinity.");
+    AffineElement R = (G1::one * u1) + (Element(public_key) * u2);
+    if (R.is_point_at_infinity()) {
+        vinfo("Result of the scalar multiplication is the point at infinity.");
+        return false;
+    }
 
     uint256_t Rx(R.x);
     Fr result(Rx);
     return result == r;
+}
+
+template <typename Hash, typename Fr> Fr ecdsa_hash_message(const std::string& message)
+{
+    constexpr size_t MODULUS_BIT_LENGTH = Fr::modulus.get_msb() + 1;
+
+    std::vector<uint8_t> message_buffer;
+    std::ranges::copy(message, std::back_inserter(message_buffer));
+    auto ev_array = Hash::hash(message_buffer);
+    std::vector<uint8_t> ev(ev_array.begin(), ev_array.end());
+
+    if (Hash::OUTPUT_SIZE * 8 > MODULUS_BIT_LENGTH) {
+        // Trim the hash output
+        size_t remainder = MODULUS_BIT_LENGTH % 8;
+        ev.resize((MODULUS_BIT_LENGTH + 7) / 8);
+        ev.back() &= (1 << remainder) - 1;
+    }
+
+    return Fr::serialize_from_buffer(&ev[0]);
 }
 } // namespace bb::crypto
