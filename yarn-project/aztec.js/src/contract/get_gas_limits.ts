c36a4ab4818b3842be179cee4315580643eee91a
sirasistant <5372114+sirasistant@users.noreply.github.com>
2026-02-20 13:16:02 +0000

    fix!: Tx overhead and private emission L2 gas based on simulation time
    This PR:
     - Splits AVM_MAX_PROCESSABLE_GAS (6M) from MAX_PROCESSABLE_GAS (6M+PUBLIC_TX_OVERHEAD)
     - Splits Public tx overhead and private tx overhead
     - Plugs in numbers from simulation metrics to tx overheads
     - Plugs in numbers from simulation metrics to side effects
     - TODO: Figure out contract class logs: we are having trouble measuring them.

--- yarn-project/aztec.js/src/contract/get_gas_limits.ts
+++ yarn-project/aztec.js/src/contract/get_gas_limits.ts
@@ -1,4 +1,4 @@
-import { AVM_MAX_PROCESSABLE_L2_GAS } from '@aztec/constants';
+import { MAX_PROCESSABLE_L2_GAS } from '@aztec/constants';
 import { Gas } from '@aztec/stdlib/gas';
 import type { TxSimulationResult } from '@aztec/stdlib/tx';
 
@@ -23,8 +23,8 @@ export function getGasLimits(
   const gasLimits = simulationResult.gasUsed.totalGas.mul(1 + pad);
   const teardownGasLimits = simulationResult.gasUsed.teardownGas.mul(1 + pad);
 
-  if (gasLimits.l2Gas > AVM_MAX_PROCESSABLE_L2_GAS) {
-    throw new Error('Transaction consumes more gas than the AVM maximum processable gas');
+  if (gasLimits.l2Gas > MAX_PROCESSABLE_L2_GAS) {
+    throw new Error('Transaction consumes more l2 gas than the maximum processable gas');
   }
   return {
     gasLimits,
