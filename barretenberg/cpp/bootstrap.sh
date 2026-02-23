a824a397bf539854fc67256df3f8c5054c0e34ab
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2026-02-23 09:22:18 +0000

    Inject version to correct binary

--- barretenberg/cpp/bootstrap.sh
+++ barretenberg/cpp/bootstrap.sh
@@ -71,7 +71,11 @@ function build_native {
     cache_upload barretenberg-$native_preset-$hash.zst build/{bin,lib}
   fi
   # Always inject version (even for cached binaries) to ensure correct version on release
-  inject_version build/bin/bb
+  if [ "${NATIVE_PRESET:-clang20}" == "clang20" ]; then
+    inject_version build/bin/bb
+  elif [ "${NATIVE_PRESET}" == "debug" ]; then
+    inject_version build-debug/bin/bb
+  fi
   if [ -f build/bin/bb-avm ]; then
     inject_version build/bin/bb-avm
   fi
