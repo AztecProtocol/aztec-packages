dcc46717ba35701501505306c2b56894039b9715
jeanmon <jean@aztec-labs.com>
2026-02-25 11:11:10 +0000

    Documentation

--- barretenberg/cpp/src/barretenberg/vm2/tracegen/to_radix_trace.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/tracegen/to_radix_trace.cpp
@@ -256,6 +256,7 @@ void ToRadixTraceBuilder::process_with_memory(
             bool last = i == (num_limbs - 1);
 
             // Note that the following columns at i == 0 were already set above but code is simpler this way:
+            // - C::to_radix_mem_sel
             // - C::to_radix_mem_execution_clk
             // - C::to_radix_mem_space_id
             // - C::to_radix_mem_dst_addr
