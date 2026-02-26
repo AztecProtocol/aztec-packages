e3e0fb65fd8e1873093094cdfc9e58f36b94d2f0
Alex Gherghisan <alexghr@users.noreply.github.com>
2026-02-26 09:20:39 +0000

    chore: skip cross_copy on non-x86 builds

--- barretenberg/ts/scripts/copy_cross.sh
+++ barretenberg/ts/scripts/copy_cross.sh
@@ -26,6 +26,5 @@ elif semver check "${REF_NAME:-}" && [[ "$(arch)" == "amd64" ]]; then
 
   llvm-strip-20 ./build/*/*
 else
-  echo "This task is expected to be run in an x86 release context."
-  exit 1
+  echo "Skipping: cross_copy is only needed on amd64 for releases."
 fi
