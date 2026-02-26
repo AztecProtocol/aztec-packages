76e348ba5c549b19259bb3176d7d17fbeb75cd71
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2026-02-26 17:26:12 +0000

    Move some testing to general field testing; standardise fq/fr testing; mark from_montgomery_form as const in fq12

--- barretenberg/cpp/src/barretenberg/ecc/curves/bn254/fr.test.cpp
+++ barretenberg/cpp/src/barretenberg/ecc/curves/bn254/fr.test.cpp
@@ -89,6 +89,9 @@ TEST(BN254Fr, SplitIntoEndomorphismScalars)
     k1.self_to_montgomery_form();
     k2.self_to_montgomery_form();
 
+    EXPECT_LT(uint256_t(k1).get_msb(), 128);
+    EXPECT_LT(uint256_t(k2).get_msb(), 128);
+
     fr lambda = fr::cube_root_of_unity();
     result = k2 * lambda;
     result = k1 - result;
@@ -109,6 +112,9 @@ TEST(BN254Fr, SplitIntoEndomorphismScalarsSimple)
     fr result{ 0, 0, 0, 0 };
     k1.self_to_montgomery_form_reduced();
     k2.self_to_montgomery_form_reduced();
+
+    EXPECT_LT(uint256_t(k1).get_msb(), 128);
+    EXPECT_LT(uint256_t(k2).get_msb(), 128);
 
     fr lambda = fr::cube_root_of_unity();
     result = k2 * lambda;
