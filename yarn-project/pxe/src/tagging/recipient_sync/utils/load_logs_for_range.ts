28eedee316013d470a1af94f3b0c1400309c551d
benesjan <benesjan@users.noreply.github.com>
2026-02-26 08:56:44 +0000

    refactor: bundling app into type with directional app tagging secret (#20796)

--- yarn-project/pxe/src/tagging/recipient_sync/utils/load_logs_for_range.ts
+++ yarn-project/pxe/src/tagging/recipient_sync/utils/load_logs_for_range.ts
@@ -1,20 +1,18 @@
 import type { BlockNumber } from '@aztec/foundation/branded-types';
-import type { AztecAddress } from '@aztec/stdlib/aztec-address';
 import type { BlockHash } from '@aztec/stdlib/block';
 import type { AztecNode } from '@aztec/stdlib/interfaces/client';
-import type { DirectionalAppTaggingSecret, PreTag, TxScopedL2Log } from '@aztec/stdlib/logs';
+import type { ExtendedDirectionalAppTaggingSecret, PreTag, TxScopedL2Log } from '@aztec/stdlib/logs';
 import { SiloedTag, Tag } from '@aztec/stdlib/logs';
 
 import { getAllPrivateLogsByTags } from '../../get_all_logs_by_tags.js';
 
 /**
- * Gets private logs with their corresponding block timestamps and tagging indexes for the given index range, `app` and
- * `secret`. At most load logs from blocks up to and including `anchorBlockNumber`. `start` is inclusive and `end` is
- * exclusive.
+ * Gets private logs with their corresponding block timestamps and tagging indexes for the given index range and
+ * `extendedSecret`. At most load logs from blocks up to and including `anchorBlockNumber`. `start` is inclusive and
+ * `end` is exclusive.
  */
 export async function loadLogsForRange(
-  secret: DirectionalAppTaggingSecret,
-  app: AztecAddress,
+  extendedSecret: ExtendedDirectionalAppTaggingSecret,
   aztecNode: AztecNode,
   start: number,
   end: number,
@@ -24,9 +22,9 @@ export async function loadLogsForRange(
   // Derive tags for the window
   const preTags: PreTag[] = Array(end - start)
     .fill(0)
-    .map((_, i) => ({ secret, index: start + i }));
+    .map((_, i) => ({ extendedSecret, index: start + i }));
   const siloedTags = await Promise.all(preTags.map(preTag => Tag.compute(preTag))).then(tags =>
-    Promise.all(tags.map(tag => SiloedTag.compute(tag, app))),
+    Promise.all(tags.map(tag => SiloedTag.compute(tag, extendedSecret.app))),
   );
 
   // We use the utility function below to retrieve all logs for the tags across all pages, so we don't need to handle
