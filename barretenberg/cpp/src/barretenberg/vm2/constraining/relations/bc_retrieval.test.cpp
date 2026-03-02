35bfa24436019792be59d9815a9709549cfddb2a
Miranda Wood <miranda@aztecprotocol.com>
2026-03-02 11:07:01 +0000

    chore(avm): Class ID Derivation pre-audit docs (#20853)
    Class ID  pre-audit PR including:
    
    - New comments/documentation
    - Rearranging code
    
    This is just a small 'destination' subtrace which computes a hash, so no
    significant findings!
    Closes
    [AVM-56](https://linear.app/aztec-labs/issue/AVM-56/bytecode-class-id-deriv)
    
    TODOs/Notes: ~Only TODO is to remove bytecode from the event (unused)~
    None!

--- barretenberg/cpp/src/barretenberg/vm2/constraining/relations/bc_retrieval.test.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/constraining/relations/bc_retrieval.test.cpp
@@ -126,14 +126,11 @@ TEST_F(BytecodeRetrievalConstrainingTest, SuccessfulRetrieval)
                                                     .exists = true,
                                                 } },
                                                 trace);
-    ContractClassWithCommitment klass_with_commitment = {
-        .id = instance.current_contract_class_id,
-        .artifact_hash = klass.artifact_hash,
-        .private_functions_root = klass.private_functions_root,
-        .packed_bytecode = klass.packed_bytecode,
-        .public_bytecode_commitment = bytecode_commitment,
-    };
-    class_id_builder.process({ { .klass = klass_with_commitment } }, trace);
+    class_id_builder.process({ { .class_id = instance.current_contract_class_id,
+                                 .artifact_hash = klass.artifact_hash,
+                                 .private_functions_root = klass.private_functions_root,
+                                 .public_bytecode_commitment = bytecode_commitment } },
+                             trace);
 
     AppendOnlyTreeSnapshot snapshot_before = AppendOnlyTreeSnapshot{
         .root = FF(AVM_RETRIEVED_BYTECODES_TREE_INITIAL_ROOT),
@@ -347,6 +344,7 @@ TEST_F(BytecodeRetrievalConstrainingTestFewerMocks, SuccessfulRetrievalFewerMock
     FF class_id = poseidon2.hash(
         { DOM_SEP__CONTRACT_CLASS_ID, klass.artifact_hash, klass.private_functions_root, bytecode_commitment });
     instance.current_contract_class_id = class_id;
+    klass.id = class_id;
     contract_instance_retrieval_builder.process({ {
                                                     .address = instance.deployer,
                                                     .contract_instance = { instance },
@@ -356,14 +354,11 @@ TEST_F(BytecodeRetrievalConstrainingTestFewerMocks, SuccessfulRetrievalFewerMock
                                                 } },
                                                 trace);
 
-    ContractClassWithCommitment klass_with_commitment = {
-        .id = instance.current_contract_class_id,
-        .artifact_hash = klass.artifact_hash,
-        .private_functions_root = klass.private_functions_root,
-        .packed_bytecode = klass.packed_bytecode,
-        .public_bytecode_commitment = bytecode_commitment,
-    };
-    class_id_builder.process({ { .klass = klass_with_commitment } }, trace);
+    class_id_builder.process({ { .class_id = klass.id,
+                                 .artifact_hash = klass.artifact_hash,
+                                 .private_functions_root = klass.private_functions_root,
+                                 .public_bytecode_commitment = bytecode_commitment } },
+                             trace);
 
     AppendOnlyTreeSnapshot snapshot_before = retrieved_bytecodes_tree_check.get_snapshot();
 
@@ -434,6 +429,7 @@ TEST_F(BytecodeRetrievalConstrainingTestFewerMocks, SuccessfulRepeatedRetrievalF
     FF class_id = poseidon2.hash(
         { DOM_SEP__CONTRACT_CLASS_ID, klass.artifact_hash, klass.private_functions_root, bytecode_commitment });
     instance.current_contract_class_id = class_id;
+    klass.id = class_id;
     contract_instance_retrieval_builder.process({ {
                                                     .address = instance.deployer,
                                                     .contract_instance = { instance },
@@ -442,14 +438,11 @@ TEST_F(BytecodeRetrievalConstrainingTestFewerMocks, SuccessfulRepeatedRetrievalF
                                                     .exists = true,
                                                 } },
                                                 trace);
-    ContractClassWithCommitment klass_with_commitment = {
-        .id = instance.current_contract_class_id,
-        .artifact_hash = klass.artifact_hash,
-        .private_functions_root = klass.private_functions_root,
-        .packed_bytecode = klass.packed_bytecode,
-        .public_bytecode_commitment = bytecode_commitment,
-    };
-    class_id_builder.process({ { .klass = klass_with_commitment } }, trace);
+    class_id_builder.process({ { .class_id = klass.id,
+                                 .artifact_hash = klass.artifact_hash,
+                                 .private_functions_root = klass.private_functions_root,
+                                 .public_bytecode_commitment = bytecode_commitment } },
+                             trace);
 
     AppendOnlyTreeSnapshot snapshot_before = retrieved_bytecodes_tree_check.get_snapshot();
 
