f00153d0eb3a8be0c0edbc9f9e578558f518d78d
mverzilli <martin@aztec-labs.com>
2026-03-04 09:18:54 +0000

    remove unused import

--- yarn-project/aztec/src/cli/cmds/start_node.ts
+++ yarn-project/aztec/src/cli/cmds/start_node.ts
@@ -10,7 +10,6 @@ import { Agent, makeUndiciFetch } from '@aztec/foundation/json-rpc/undici';
 import type { LogFn } from '@aztec/foundation/log';
 import { ProvingJobConsumerSchema, createProvingJobBrokerClient } from '@aztec/prover-client/broker';
 import { type CliPXEOptions, type PXEConfig, allPxeConfigMappings } from '@aztec/pxe/config';
-import { AztecAddress } from '@aztec/stdlib/aztec-address';
 import { AztecNodeAdminApiSchema, AztecNodeApiSchema } from '@aztec/stdlib/interfaces/client';
 import { P2PApiSchema, ProverNodeApiSchema, type ProvingJobBroker } from '@aztec/stdlib/interfaces/server';
 import {
