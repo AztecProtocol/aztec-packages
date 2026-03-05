922b33e385b3b1cadfed63e8072670d8d19b00e9
Michal Rzeszutko <michal.rzeszutko@gmail.com>
2026-03-05 13:09:54 +0100

    Update yarn-project/aztec.js/src/fee/private_fee_payment_method.ts
    Co-authored-by: Santiago Palladino <santiago@aztecprotocol.com>

--- yarn-project/aztec.js/src/fee/private_fee_payment_method.ts
+++ yarn-project/aztec.js/src/fee/private_fee_payment_method.ts
@@ -10,7 +10,7 @@ import type { FeePaymentMethod } from './fee_payment_method.js';
 
 /**
  * Holds information about how the fee for a transaction is to be paid.
- * @deprecated Will not work on mainnet alpha. Use {@link FeeJuicePaymentMethodWithClaim} or `SponsoredFeePaymentMethod` instead.
+ * @deprecated Is not supported on mainnet. Use {@link FeeJuicePaymentMethodWithClaim} or `SponsoredFeePaymentMethod` instead.
  */
 export class PrivateFeePaymentMethod implements FeePaymentMethod {
   private assetPromise: Promise<AztecAddress> | null = null;
