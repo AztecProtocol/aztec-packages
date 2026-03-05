8ca9f96866d61ec19b449de078ec1d26d36d54a9
Jan Beneš <janbenes1234@gmail.com>
2026-03-05 07:18:47 +0100

    Update yarn-project/aztec/scripts/aztec.sh
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>

--- yarn-project/aztec/scripts/aztec.sh
+++ yarn-project/aztec/scripts/aztec.sh
@@ -20,6 +20,7 @@ function aztec {
 
 case $cmd in
   test)
+    # Attempt to compile, no-op if there are no changes
     node --no-warnings "$script_dir/../dest/bin/index.js" compile
 
     export LOG_LEVEL="${LOG_LEVEL:-"error;trace:contract_log"}"
