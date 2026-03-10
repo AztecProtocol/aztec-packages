3c609114509e1e5361462f34f84d582d6a7f40d6
mverzilli <martin@aztec-labs.com>
2026-03-10 13:03:31 +0000

    bump oracle version

--- yarn-project/pxe/src/oracle_version.ts
+++ yarn-project/pxe/src/oracle_version.ts
@@ -4,9 +4,9 @@
 ///
 /// @dev Whenever a contract function or Noir test is run, the `aztec_utl_assertCompatibleOracleVersion` oracle is called
 /// and if the oracle version is incompatible an error is thrown.
-export const ORACLE_VERSION = 15;
+export const ORACLE_VERSION = 16;
 
 /// This hash is computed as by hashing the Oracle interface and it is used to detect when the Oracle interface changes,
 /// which in turn implies that you need to update the ORACLE_VERSION constant in this file and in
 /// `noir-projects/aztec-nr/aztec/src/oracle/version.nr`.
-export const ORACLE_INTERFACE_HASH = '34238ab576c377b4ffc1ddbf557560f07a66baf879920191897bc784fa5a97ee';
+export const ORACLE_INTERFACE_HASH = '74f288878a8010aa4b99ee42bb7c3456446e942ba77f74484084e886c35a1545';
