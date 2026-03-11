99c30d95d10b1eb981721680fc7fbe839b959486
Martin Verzilli <martin@aztec-labs.com>
2026-03-11 13:30:42 +0100

    cherry-pick: feat: offchain reception (#20893)
    Cherry-pick of b1ada99eef onto v4-next.
    This commit contains conflict markers for reviewer visibility.

--- yarn-project/pxe/src/oracle_version.ts
+++ yarn-project/pxe/src/oracle_version.ts
@@ -4,9 +4,9 @@
 ///
 /// @dev Whenever a contract function or Noir test is run, the `aztec_utl_assertCompatibleOracleVersion` oracle is called
 /// and if the oracle version is incompatible an error is thrown.
-export const ORACLE_VERSION = 15;
+export const ORACLE_VERSION = 16;
 
 /// This hash is computed as by hashing the Oracle interface and it is used to detect when the Oracle interface changes,
 /// which in turn implies that you need to update the ORACLE_VERSION constant in this file and in
 /// `noir-projects/aztec-nr/aztec/src/oracle/version.nr`.
-export const ORACLE_INTERFACE_HASH = '32b3efa9e08765a5fad5fba732df263f61ca13e7979e30c1843215914b76b97e';
+export const ORACLE_INTERFACE_HASH = '73ccb2a24bc9fe7514108be9ff98d7ca8734bc316fb7c1ec4329d1d32f412a55';
