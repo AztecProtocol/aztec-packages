344d93e6eb035c3bf0353f87cb5e0cec8f7c544e
notnotraju <raju@aztec-labs.com>
2026-03-12 12:30:42 +0000

    Update CHONK_RECURSION_GATES constant to 1587413

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
@@ -113,7 +113,7 @@ constexpr std::tuple<size_t, size_t> HONK_RECURSION_CONSTANTS(
 // ========================================
 
 // Gate count for Chonk recursive verification (Ultra with RollupIO)
-inline constexpr size_t CHONK_RECURSION_GATES = 1587459;
+inline constexpr size_t CHONK_RECURSION_GATES = 1587413;
 
 // ========================================
 // Hypernova Recursion Constants
