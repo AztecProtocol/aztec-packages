04c95fd428cd101f0f1ddc538f8855b2634a30b3
MirandaWood <miranda@aztecprotocol.com>
2026-03-12 11:16:58 +0000

    chore: more docs, tests, document poss completeness issue

--- barretenberg/cpp/src/barretenberg/vm2/simulation/gadgets/address_derivation.cpp
+++ barretenberg/cpp/src/barretenberg/vm2/simulation/gadgets/address_derivation.cpp
@@ -32,6 +32,11 @@ void AddressDerivation::assert_derivation(const AztecAddress& address, const Con
 {
     // Check if we've already derived this address.
     if (cached_derivations.contains(address)) {
+        // TODO(MW): This ignores the input instance, so if we have derived addr_a for instance A and call this
+        // again with addr_a but instance B, it will not fail and simply return. However calling this with addr_a and
+        // instance B /first/ will throw a runtime error. Neither case emits an event but only case throws -
+        // completeness issue?
+
         // Already processed this address - cache hit, don't emit event.
         return;
     }
