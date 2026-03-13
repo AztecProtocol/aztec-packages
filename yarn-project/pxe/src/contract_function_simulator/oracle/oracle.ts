dfed5a3902df44110e6a51890bed1d2e7abe65da
Nicolas Chamo <nicolas@chamo.com.ar>
2026-03-13 13:01:09 -0300

    fix: address PR review feedback and sync with merge-train/fairies

--- yarn-project/pxe/src/contract_function_simulator/oracle/oracle.ts
+++ yarn-project/pxe/src/contract_function_simulator/oracle/oracle.ts
@@ -604,6 +604,7 @@ export class Oracle {
     return [];
   }
 
+  // TODO(F-452): Return Option and wrap in try/catch so BB exceptions don't crash PXE.
   // eslint-disable-next-line camelcase
   async aztec_utl_aes128Decrypt(
     ciphertextBVecStorage: ACVMField[],
