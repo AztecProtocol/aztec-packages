ebc74dd8ac8e980111af11dae97bb2b5df6d2e8c
Ilyas Ridhuan <ilyas@aztecprotocol.com>
2026-03-13 13:52:35 +0000

    fix(avm)!: remove unused dc selector (#21314)
    `sel_op_dc_17` was unused since #13971 and can be safely removed.

--- barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
@@ -140,11 +140,11 @@
 namespace bb::avm2 {
 
 struct AvmFlavorVariables {
-    static constexpr size_t NUM_PRECOMPUTED_ENTITIES = 123;
+    static constexpr size_t NUM_PRECOMPUTED_ENTITIES = 122;
     static constexpr size_t NUM_WITNESS_ENTITIES = 3012;
     static constexpr size_t NUM_SHIFTED_ENTITIES = 363;
     static constexpr size_t NUM_WIRES = 2559;
-    static constexpr size_t NUM_ALL_ENTITIES = 3498;
+    static constexpr size_t NUM_ALL_ENTITIES = 3497;
 
     // Need to be templated for recursive verifier
     template <typename FF_>
