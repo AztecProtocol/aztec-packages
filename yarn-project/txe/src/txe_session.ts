9806851aee00223632dffcc7dc58b769a4b14718
mverzilli <martin@aztec-labs.com>
2026-03-17 11:45:18 +0000

    use keystore to provide scopes from txe

--- yarn-project/txe/src/txe_session.ts
+++ yarn-project/txe/src/txe_session.ts
@@ -177,7 +177,7 @@ export class TXESession implements TXESessionStateHandler {
 
     const archiver = new TXEArchiver(store);
     const anchorBlockStore = new AnchorBlockStore(store);
-    const stateMachine = await TXEStateMachine.create(archiver, anchorBlockStore, contractStore, noteStore);
+    const stateMachine = await TXEStateMachine.create(archiver, anchorBlockStore, contractStore, noteStore, keyStore);
 
     const nextBlockTimestamp = BigInt(Math.floor(new Date().getTime() / 1000));
     const version = new Fr(await stateMachine.node.getVersion());
