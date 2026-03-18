f825ec0145a1deb4d39415f34582d89d6245996c
AztecBot <tech@aztec-labs.com>
2026-03-18 13:39:09 +0000

    fix: resolve cherry-pick conflicts, bump oracle version to 19, update hash

--- yarn-project/pxe/src/oracle_version.ts
+++ yarn-project/pxe/src/oracle_version.ts
@@ -4,17 +4,9 @@
 ///
 /// @dev Whenever a contract function or Noir test is run, the `aztec_utl_assertCompatibleOracleVersion` oracle is called
 /// and if the oracle version is incompatible an error is thrown.
-<<<<<<< HEAD
-export const ORACLE_VERSION = 18;
-=======
-export const ORACLE_VERSION = 17;
->>>>>>> 3d501c2c36 (feat!(aztec-nr,pxe,txe): make AES128 decrypt oracle return Option)
+export const ORACLE_VERSION = 19;
 
 /// This hash is computed as by hashing the Oracle interface and it is used to detect when the Oracle interface changes,
 /// which in turn implies that you need to update the ORACLE_VERSION constant in this file and in
 /// `noir-projects/aztec-nr/aztec/src/oracle/version.nr`.
-<<<<<<< HEAD
-export const ORACLE_INTERFACE_HASH = 'ab9ba6bb6675a4663d66af494bec195ab1d18fa2b646549323e0f54cac6af1de';
-=======
-export const ORACLE_INTERFACE_HASH = 'f9e965a21bd027693cb6b66dbdf87a3b7411d5ec1c8e0124b3a8852eaac574df';
->>>>>>> 3d501c2c36 (feat!(aztec-nr,pxe,txe): make AES128 decrypt oracle return Option)
+export const ORACLE_INTERFACE_HASH = '038f85d2e84afb5f688ce868249a0c8c9f94d605690bd2f1b59b1eb978b9c670';
