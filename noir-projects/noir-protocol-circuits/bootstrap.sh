7bcaa509bbcbccac724342348dc3a5a6b91b94e9
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2026-03-20 12:27:48 +0000

    chore: use optimized verifier for rollup

--- noir-projects/noir-protocol-circuits/bootstrap.sh
+++ noir-projects/noir-protocol-circuits/bootstrap.sh
@@ -130,8 +130,7 @@ function compile {
       SECONDS=0
       # Generate solidity verifier for this contract.
       # TODO(AD) ensure this passes.
-      #echo "$vk_bytes" | xxd -r -p | $BB write_solidity_verifier --scheme ultra_honk --disable_zk -k - -o $verifier_path --optimized
-      echo "$vk_bytes" | xxd -r -p | $BB write_solidity_verifier --scheme ultra_honk --disable_zk -k - -o $verifier_path
+      echo "$vk_bytes" | xxd -r -p | $BB write_solidity_verifier --scheme ultra_honk --disable_zk -k - -o $verifier_path --optimized
       echo_stderr "Root rollup verifier at: $verifier_path (${SECONDS}s)"
       # Include the verifier path if we create it.
       cache_upload vk-$hash.tar.gz $key_path $verifier_path &> /dev/null
