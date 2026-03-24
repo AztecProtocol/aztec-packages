a4289f921c184c7243695297fa82d6180e541dbe
thunkar <gregojquiros@gmail.com>
2026-03-24 12:13:18 +0100

    let wallet estimate

--- yarn-project/bot/src/config.ts
+++ yarn-project/bot/src/config.ts
@@ -69,9 +69,9 @@ export type BotConfig = {
   maxPendingTxs: number;
   /** Whether to flush after sending each 'setup' transaction */
   flushSetupTransactions: boolean;
-  /** L2 gas limit for the tx (empty to have the bot trigger an estimate gas). */
+  /** L2 gas limit for the tx (empty to use default gas limits). */
   l2GasLimit: number | undefined;
-  /** DA gas limit for the tx (empty to have the bot trigger an estimate gas). */
+  /** DA gas limit for the tx (empty to use default gas limits). */
   daGasLimit: number | undefined;
   /** Token contract to use */
   contract: SupportedTokenContracts;
@@ -243,12 +243,12 @@ export const botConfigMappings: ConfigMappingsType<BotConfig> = {
   },
   l2GasLimit: {
     env: 'BOT_L2_GAS_LIMIT',
-    description: 'L2 gas limit for the tx (empty to have the bot trigger an estimate gas).',
+    description: 'L2 gas limit for the tx (empty to use default gas limits).',
     ...optionalNumberConfigHelper(),
   },
   daGasLimit: {
     env: 'BOT_DA_GAS_LIMIT',
-    description: 'DA gas limit for the tx (empty to have the bot trigger an estimate gas).',
+    description: 'DA gas limit for the tx (empty to use default gas limits).',
     ...optionalNumberConfigHelper(),
   },
   contract: {
