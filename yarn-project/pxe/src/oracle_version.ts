b0090ffc53c1eff8a80b8fd3a05911d9306c4c14
Martin Verzilli <martin@aztec-labs.com>
2026-03-25 20:36:04 +0100

    refactor!: more consistent oracle names (#22018)
    Closes F-99
    
    ---------
    
    Co-authored-by: AztecBot <tech@aztec-labs.com>

--- yarn-project/pxe/src/oracle_version.ts
+++ yarn-project/pxe/src/oracle_version.ts
@@ -4,9 +4,9 @@
 ///
 /// @dev Whenever a contract function or Noir test is run, the `aztec_utl_assertCompatibleOracleVersion` oracle is called
 /// and if the oracle version is incompatible an error is thrown.
-export const ORACLE_VERSION = 19;
+export const ORACLE_VERSION = 20;
 
 /// This hash is computed as by hashing the Oracle interface and it is used to detect when the Oracle interface changes,
 /// which in turn implies that you need to update the ORACLE_VERSION constant in this file and in
 /// `noir-projects/aztec-nr/aztec/src/oracle/version.nr`.
-export const ORACLE_INTERFACE_HASH = 'bf97c9fb1e24c5d87d380a20d829692371bd418306fb5ec56466d5fafc400dc8';
+export const ORACLE_INTERFACE_HASH = '93b5352522338a33462efb253b67808c59a805c1c90ee0e394285b49f34d9691';
