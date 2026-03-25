ff40784166822ff297f4b2477201a41cd3d368fa
jeanmon <jean@aztec-labs.com>
2026-03-25 15:12:07 +0000

    Generated files

--- barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
@@ -141,10 +141,10 @@ namespace bb::avm2 {
 
 struct AvmFlavorVariables {
     static constexpr size_t NUM_PRECOMPUTED_ENTITIES = 122;
-    static constexpr size_t NUM_WITNESS_ENTITIES = 2976;
-    static constexpr size_t NUM_SHIFTED_ENTITIES = 363;
-    static constexpr size_t NUM_WIRES = 2524;
-    static constexpr size_t NUM_ALL_ENTITIES = 3461;
+    static constexpr size_t NUM_WITNESS_ENTITIES = 2974;
+    static constexpr size_t NUM_SHIFTED_ENTITIES = 362;
+    static constexpr size_t NUM_WIRES = 2522;
+    static constexpr size_t NUM_ALL_ENTITIES = 3458;
 
     // Need to be templated for recursive verifier
     template <typename FF_>
