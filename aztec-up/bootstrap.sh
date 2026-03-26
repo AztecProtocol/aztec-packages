03a9107fb375261f94821e2d9dd5b244f57be55c
Nicolas Chamo <nicolas@chamo.com.ar>
2026-03-26 20:14:44 -0300

    fix(aztec-up): pass VERSION to per-version installer and fix release sed (#22083)

--- aztec-up/bootstrap.sh
+++ aztec-up/bootstrap.sh
@@ -141,7 +141,7 @@ function release {
 # It's a manual process, as updating the root installer and alias index requires careful consideration.
 function release_root_installer {
     # Upload root installer assets: aztec-install (with VERSION defaulting to latest), aztec-up, and banner files.
-    sed "s/^VERSION=.*/VERSION=\${VERSION:-latest}/" bin/0.0.1/aztec-install | \
+    sed "s/^VERSION=\${VERSION:-.*}/VERSION=\${VERSION:-latest}/" bin/0.0.1/aztec-install | \
       do_or_dryrun aws s3 cp - "s3://install.aztec.network/aztec-install"
     do_or_dryrun aws s3 cp bin/0.0.1/aztec-up "s3://install.aztec.network/aztec-up"
     do_or_dryrun aws s3 cp bin/0.0.1/aztec-banner "s3://install.aztec.network/aztec-banner"
