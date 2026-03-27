8ce791386d8282a5b2d0a4ba084f7ec6fdb5a0aa
federicobarbacovi <171914500+federicobarbacovi@users.noreply.github.com>
2026-03-27 10:41:04 +0000

    Goblin flush does not perform merge

--- barretenberg/cpp/src/barretenberg/goblin/goblin_flush_circuit.cpp
+++ barretenberg/cpp/src/barretenberg/goblin/goblin_flush_circuit.cpp
@@ -6,12 +6,14 @@
 namespace bb {
 
 UltraCircuitBuilder build_goblin_flush_circuit(const GoblinProof& native_proof,
-                                               const MergeVerifier::InputCommitments& native_merge_commitments)
+                                               const MergeVerifier::TableCommitments& merged_table)
 {
     using Builder = UltraCircuitBuilder;
     using Curve = stdlib::bn254<Builder>;
+    using ECCVMRecursiveVerifier = GoblinRecursiveVerifier::ECCVMVerifier;
+    using TranslatorRecursiveVerifier = GoblinRecursiveVerifier::TranslatorVerifier;
     using RecursiveCommitment = Curve::AffineElement;
-    using RecursiveMergeCommitments = GoblinRecursiveVerifier::MergeCommitments;
+    using RecursiveTableCommitments = GoblinRecursiveVerifier::MergeVerifier::TableCommitments;
     using Transcript = UltraStdlibTranscript;
     using GoblinFlushIO = stdlib::recursion::honk::GoblinFlushIO<Builder>;
 
@@ -21,32 +23,44 @@ UltraCircuitBuilder build_goblin_flush_circuit(const GoblinProof& native_proof,
     GoblinStdlibProof stdlib_proof(builder, native_proof);
 
     // Convert native merge commitments to recursive (stdlib) commitments
-    RecursiveMergeCommitments recursive_merge_commitments;
+    RecursiveTableCommitments recursive_merged_table;
     for (size_t idx = 0; idx < MegaFlavor::NUM_WIRES; idx++) {
-        recursive_merge_commitments.t_commitments[idx] =
-            RecursiveCommitment::from_witness(&builder, native_merge_commitments.t_commitments[idx]);
-        recursive_merge_commitments.T_prev_commitments[idx] =
-            RecursiveCommitment::from_witness(&builder, native_merge_commitments.T_prev_commitments[idx]);
+        recursive_merged_table[idx] = RecursiveCommitment::from_witness(&builder, merged_table[idx]);
         // Remove the free witness tag since these are supposed to be free witnesses
         // Their correctness is checked via public inputs' propagation
-        recursive_merge_commitments.t_commitments[idx].unset_free_witness_tag();
-        recursive_merge_commitments.T_prev_commitments[idx].unset_free_witness_tag();
+        recursive_merged_table[idx].unset_free_witness_tag();
     }
 
-    // Run the GoblinRecursiveVerifier: Merge → ECCVM → Translator
+    // Goblin recursive verification without merge → ECCVM → Translator
     auto transcript = std::make_shared<Transcript>();
-    GoblinRecursiveVerifier verifier{ transcript, stdlib_proof, recursive_merge_commitments, MergeSettings::APPEND };
-    auto result = verifier.reduce_to_pairing_check_and_ipa_opening();
 
-    // Aggregate Merge and Translator pairing points
-    result.translator_pairing_points.aggregate(result.merge_pairing_points);
+    // Step 1: Verify the ECCVM proof
+    ECCVMRecursiveVerifier eccvm_verifier{ transcript, stdlib_proof.eccvm_proof };
+    auto eccvm_result = eccvm_verifier.reduce_to_ipa_opening();
+    vinfo("Goblin: ECCVM reduced to IPA opening successfully: ", eccvm_result.reduction_succeeded ? "true" : "false");
+
+    // Get translation data from ECCVM verifier
+    auto translator_input = eccvm_verifier.get_translator_input_data();
+
+    // Step 2: Verify the Translator proof
+    // - Pass `table_commitments` as the one with which GoblinAvm was initialized (which commits all the ECC ops of the
+    //   circuit containing the AVM recursive verifier).
+    // - `accumulated_result` and corresponding challenges ensure non-native computation matches ECCVM's native result
+    TranslatorRecursiveVerifier translator_verifier{ transcript,
+                                                     stdlib_proof.translator_proof,
+                                                     translator_input.evaluation_challenge_x,
+                                                     translator_input.batching_challenge_v,
+                                                     translator_input.accumulated_result,
+                                                     recursive_merged_table };
+    auto translator_result = translator_verifier.reduce_to_pairing_check();
+    vinfo("Goblin: Translator reduced to pairing check successfully: ",
+          translator_result.reduction_succeeded ? "true" : "false");
 
     // Set GoblinFlushIO as public inputs
     GoblinFlushIO io;
-    io.pairing_inputs = std::move(result.translator_pairing_points);
-    io.ipa_claim = std::move(result.ipa_claim);
-    io.T_prev = std::move(recursive_merge_commitments.T_prev_commitments);
-    io.t = std::move(recursive_merge_commitments.t_commitments);
+    io.pairing_inputs = std::move(translator_result.pairing_points);
+    io.ipa_claim = std::move(eccvm_result.ipa_claim);
+    io.merged_table = std::move(recursive_merged_table);
     io.set_public();
 
     // Store the IPA proof for external verification
