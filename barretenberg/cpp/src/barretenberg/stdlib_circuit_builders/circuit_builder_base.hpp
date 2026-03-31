77ddbb43fba36f43f97a104d853018d8009dda64
ledwards2225 <l.edwards.d@gmail.com>
2026-03-31 21:09:46 +0000

    488: delete unused and potentially sketch circuit builder move constructor

--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/circuit_builder_base.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/circuit_builder_base.hpp
@@ -127,9 +127,9 @@ template <typename FF_> class CircuitBuilderBase {
     CircuitBuilderBase(bool is_write_vk_mode = false);
 
     CircuitBuilderBase(const CircuitBuilderBase& other) = default;
-    CircuitBuilderBase(CircuitBuilderBase&& other) noexcept = default;
+    CircuitBuilderBase(CircuitBuilderBase&& other) = delete;
     CircuitBuilderBase& operator=(const CircuitBuilderBase& other) = default;
-    CircuitBuilderBase& operator=(CircuitBuilderBase&& other) noexcept = default;
+    CircuitBuilderBase& operator=(CircuitBuilderBase&& other) = delete;
     virtual ~CircuitBuilderBase() = default;
 
     bool operator==(const CircuitBuilderBase& other) const = default;
