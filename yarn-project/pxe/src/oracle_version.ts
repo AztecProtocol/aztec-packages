0ba10b0eb369d5853c39d52aba0f4acf673f2a2e
mverzilli <martin@aztec-labs.com>
2026-04-09 09:35:25 +0000

    update oracle hash

--- yarn-project/pxe/src/oracle_version.ts
+++ yarn-project/pxe/src/oracle_version.ts
@@ -9,4 +9,4 @@ export const ORACLE_VERSION = 23;
 /// This hash is computed as by hashing the Oracle interface and it is used to detect when the Oracle interface changes,
 /// which in turn implies that you need to update the ORACLE_VERSION constant in this file and in
 /// `noir-projects/aztec-nr/aztec/src/oracle/version.nr`.
-export const ORACLE_INTERFACE_HASH = 'a695a6a7264078bc700b321bee4695c7c4677959d5db45728a791c0056d990cd';
+export const ORACLE_INTERFACE_HASH = 'f20846f7df1225e2872d3c26ed6c55ef56acf007620858291896d0589dccb12b';
