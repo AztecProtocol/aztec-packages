bf36c37353a025c8f3921a447b453ff89e82587c
mverzilli <martin@aztec-labs.com>
2026-04-30 10:38:09 +0000

    test(pxe): pin CapsuleStore on-disk schema

---
+++ yarn-project/pxe/src/storage/capsule_store_schema.test.ts
@@ -0,0 +1,33 @@
+import { Fr } from '@aztec/foundation/curves/bn254';
+import { openTmpStore } from '@aztec/kv-store/lmdb-v2';
+import { AztecAddress } from '@aztec/stdlib/aztec-address';
+
+import { PXE_DATA_SCHEMA_VERSION } from './metadata.js';
+import { openPxeStores } from './open_pxe_stores.js';
+import { snapshotMap } from './snapshot_kv_entries.js';
+
+describe('CapsuleStore schema compatibility', () => {
+  it('persists set capsules after commit', async () => {
+    const kvStore = await openTmpStore('pxe-schema-capsule-store', true);
+    try {
+      const { capsuleStore } = openPxeStores(kvStore);
+
+      const jobId = 'fixture-job';
+      const contractAddress = AztecAddress.fromBigInt(2n);
+      const scope = AztecAddress.fromBigInt(3n);
+
+      capsuleStore.setCapsule(contractAddress, new Fr(5n), [new Fr(7n), new Fr(11n)], jobId, scope);
+      capsuleStore.setCapsule(contractAddress, new Fr(13n), [new Fr(17n)], jobId, scope);
+      await kvStore.transactionAsync(() => capsuleStore.commit(jobId));
+
+      const capsules = kvStore.openMap<string, Buffer>('capsules');
+
+      expect({
+        schemaVersion: PXE_DATA_SCHEMA_VERSION,
+        capsules: await snapshotMap(capsules),
+      }).toMatchSnapshot();
+    } finally {
+      await kvStore.close();
+    }
+  });
+});
