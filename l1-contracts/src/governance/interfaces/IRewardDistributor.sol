51f587c95486a33c19f573e3846c86560a2392fa
ludamad <domuradical@gmail.com>
2026-05-03 13:05:23 -0400

    no gov

--- l1-contracts/src/governance/interfaces/IRewardDistributor.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.8.27;
-
-interface IRewardDistributor {
-  function claim(address _to, uint256 _amount) external;
-  function recover(address _asset, address _to, uint256 _amount) external;
-  function canonicalRollup() external view returns (address);
-}
