10566eb56153164b5c31e44040eca18f356849cc
ledwards2225 <l.edwards.d@gmail.com>
2026-05-05 22:06:37 +0000

    comment tweak

--- barretenberg/cpp/src/barretenberg/constants.hpp
+++ barretenberg/cpp/src/barretenberg/constants.hpp
@@ -62,7 +62,7 @@ static constexpr uint32_t NUM_TRANSLATION_EVALUATIONS = 5;
 // The number of leading zero rows in the execution trace. Used to enable shifted polynomials.
 static constexpr size_t NUM_ZERO_ROWS = 1;
 
-// The maximum number of app circuits a single kernel can recursively verify in one accumulation group.
+// The maximum number of app circuits a single kernel can recursively verify
 static constexpr uint8_t MAX_APPS_PER_KERNEL = 1;
 
 static constexpr size_t CHONK_MAX_NUM_CIRCUITS = 56 + /*trailing kernels*/ 3;
