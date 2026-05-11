b06e7ad633f6214761077e9dd11a966080fa310c
iakovenkos <sergey.s.yakovenko@gmail.com>
2026-05-11 11:23:35 +0000

    Revert "gate count fix"
    This reverts commit 7f748d2d264f6f239716f4b2d02e7fb1462740da.

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
@@ -113,7 +113,7 @@ constexpr std::tuple<size_t, size_t> HONK_RECURSION_CONSTANTS(
 // ========================================
 
 // Gate count for Chonk recursive verification (Ultra with RollupIO)
-inline constexpr size_t CHONK_RECURSION_GATES = 1474686;
+inline constexpr size_t CHONK_RECURSION_GATES = 1474692;
 
 // ========================================
 // Hypernova Recursion Constants
