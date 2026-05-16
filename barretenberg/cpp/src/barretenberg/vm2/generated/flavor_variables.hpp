0ddd0d0f3f4e228b1e65a381be3de320e76d290f
MirandaWood <miranda@aztecprotocol.com>
2026-05-16 19:23:58 +0000

    chore: regen

--- barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/generated/flavor_variables.hpp
@@ -141,10 +141,10 @@ namespace bb::avm2 {
 
 struct AvmFlavorVariables {
     static constexpr size_t NUM_PRECOMPUTED_ENTITIES = 120;
-    static constexpr size_t NUM_WITNESS_ENTITIES = 2960;
+    static constexpr size_t NUM_WITNESS_ENTITIES = 2951;
     static constexpr size_t NUM_SHIFTED_ENTITIES = 364;
-    static constexpr size_t NUM_WIRES = 2516;
-    static constexpr size_t NUM_ALL_ENTITIES = 3444;
+    static constexpr size_t NUM_WIRES = 2508;
+    static constexpr size_t NUM_ALL_ENTITIES = 3435;
 
     // Need to be templated for recursive verifier
     template <typename FF_>
