36cdcebb1289f1d8961c6311f7c3c6ec84353420
ludamad <domuradical@gmail.com>
2026-05-17 18:31:25 +0000

    perf: remove unused sumcheck header include

--- barretenberg/cpp/src/barretenberg/sumcheck/sumcheck.cpp
+++ barretenberg/cpp/src/barretenberg/sumcheck/sumcheck.cpp
@@ -4,8 +4,6 @@
 // external_2:  { status: not started, auditors: [], commit: }
 // =====================
 
-#include "sumcheck.hpp"
-
 // Hack to make the module compile.
 
 // TODO(https://github.com/AztecProtocol/barretenberg/issues/1102): this makes the module not empty (note the comment
