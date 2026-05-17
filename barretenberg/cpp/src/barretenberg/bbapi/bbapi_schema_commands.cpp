e659348b62818c287c1db49c002451d4709feb70
ludamad <domuradical@gmail.com>
2026-05-17 21:59:16 +0000

    perf: hardcode bbapi msgpack schema

--- barretenberg/cpp/src/barretenberg/bbapi/bbapi_schema_commands.cpp
+++ barretenberg/cpp/src/barretenberg/bbapi/bbapi_schema_commands.cpp
@@ -1,16 +1,1 @@
-#include "barretenberg/serialize/msgpack_impl.hpp"
-#include "bbapi_schema.hpp"
-
-namespace bb::bbapi {
-void pack_msgpack_command_schema_first(MsgpackSchemaPacker& packer);
-void pack_msgpack_command_schema_second(MsgpackSchemaPacker& packer);
-
-void pack_msgpack_command_schema(MsgpackSchemaPacker& packer)
-{
-    packer.pack_array(2);
-    packer.pack("named_union");
-    packer.pack_array(62);
-    pack_msgpack_command_schema_first(packer);
-    pack_msgpack_command_schema_second(packer);
-}
-} // namespace bb::bbapi
+namespace bb::bbapi {}
