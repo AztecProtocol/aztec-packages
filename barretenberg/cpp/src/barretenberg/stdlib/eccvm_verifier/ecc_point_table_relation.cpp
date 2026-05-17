cbd4b339db4b1185104798c09d98f593f9aa24a4
ludamad <domuradical@gmail.com>
2026-05-17 18:39:45 +0000

    perf: trim recursive eccvm relation includes

--- barretenberg/cpp/src/barretenberg/stdlib/eccvm_verifier/ecc_point_table_relation.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/eccvm_verifier/ecc_point_table_relation.cpp
@@ -7,7 +7,6 @@
 #include "barretenberg/flavor/relation_definitions.hpp"
 #include "barretenberg/relations/ecc_vm/ecc_point_table_relation_impl.hpp"
 #include "barretenberg/stdlib/eccvm_verifier/eccvm_recursive_flavor.hpp"
-#include "barretenberg/stdlib/primitives/bigfield/bigfield.hpp"
 
 namespace bb {
 template class ECCVMPointTableRelationImpl<stdlib::bigfield<UltraCircuitBuilder, bb::Bn254FqParams>>;
--- barretenberg/cpp/src/barretenberg/stdlib/eccvm_verifier/ecc_wnaf_relation.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib/eccvm_verifier/ecc_wnaf_relation.cpp
@@ -7,7 +7,6 @@
 #include "barretenberg/flavor/relation_definitions.hpp"
 #include "barretenberg/relations/ecc_vm/ecc_wnaf_relation_impl.hpp"
 #include "barretenberg/stdlib/eccvm_verifier/eccvm_recursive_flavor.hpp"
-#include "barretenberg/stdlib/primitives/bigfield/bigfield.hpp"
 
 namespace bb {
 template class ECCVMWnafRelationImpl<stdlib::bigfield<UltraCircuitBuilder, bb::Bn254FqParams>>;
