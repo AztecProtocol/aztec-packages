fc40821464a43ddaf186c211356da248c69ef87b
ludamad <domuradical@gmail.com>
2026-05-18 05:04:34 +0000

    perf: move pairing point tagging work out of headers

--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/circuit_builder_base.cpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/circuit_builder_base.cpp
@@ -1,8 +1,85 @@
+#include "barretenberg/common/assert.hpp"
 #include "barretenberg/ecc/curves/bn254/bn254.hpp"
 #include "barretenberg/ecc/curves/grumpkin/grumpkin.hpp"
 #include "circuit_builder_base_impl.hpp"
+#include "pairing_points_tagging.hpp"
+
+#include <algorithm>
 
 namespace bb {
+
+PairingPointsTagging::PairingPointsTagging() = default;
+PairingPointsTagging::PairingPointsTagging(const PairingPointsTagging& other) = default;
+PairingPointsTagging::PairingPointsTagging(PairingPointsTagging&& other) noexcept = default;
+PairingPointsTagging& PairingPointsTagging::operator=(const PairingPointsTagging& other) = default;
+PairingPointsTagging& PairingPointsTagging::operator=(PairingPointsTagging&& other) noexcept = default;
+PairingPointsTagging::~PairingPointsTagging() = default;
+
+bool PairingPointsTagging::operator==(const PairingPointsTagging& other) const
+{
+    return pairing_points_tags_ == other.pairing_points_tags_ &&
+           next_pairing_point_tag_ == other.next_pairing_point_tag_ &&
+           has_pairing_points_ == other.has_pairing_points_ &&
+           has_public_pairing_points_ == other.has_public_pairing_points_;
+}
+
+uint32_t PairingPointsTagging::create_pairing_point_tag()
+{
+    has_pairing_points_ = true;
+    uint32_t new_tag = next_pairing_point_tag_++;
+    pairing_points_tags_.emplace_back(new_tag);
+    return new_tag;
+}
+
+void PairingPointsTagging::merge_pairing_point_tags(uint32_t tag1_index, uint32_t tag2_index)
+{
+    BB_ASSERT(!has_public_pairing_points_,
+              "Cannot merge pairing point tags after pairing points have been set to public.");
+
+    uint32_t tag1 = pairing_points_tags_[tag1_index];
+    uint32_t tag2 = pairing_points_tags_[tag2_index];
+
+    if (tag1 != tag2) {
+        for (auto& tag : pairing_points_tags_) {
+            tag = tag == tag2 ? tag1 : tag;
+        }
+    }
+}
+
+bool PairingPointsTagging::has_single_pairing_point_tag() const
+{
+    if (!has_pairing_points_) {
+        return true;
+    }
+    uint32_t unique_tag = pairing_points_tags_[0];
+    return std::ranges::all_of(pairing_points_tags_, [unique_tag](auto const& tag) { return tag == unique_tag; });
+}
+
+uint32_t PairingPointsTagging::num_unique_pairing_points() const
+{
+    std::vector<uint32_t> unique_tags;
+    unique_tags.resize(pairing_points_tags_.size());
+    for (auto const& tag : pairing_points_tags_) {
+        unique_tags[tag] = 1;
+    }
+    uint32_t sum = 0;
+    for (auto v : unique_tags) {
+        sum += v;
+    }
+    return sum;
+}
+
+uint32_t PairingPointsTagging::get_tag(uint32_t tag_index) const
+{
+    return pairing_points_tags_.at(tag_index);
+}
+
+void PairingPointsTagging::set_public_pairing_points()
+{
+    BB_ASSERT(!has_public_pairing_points_,
+              "Trying to set pairing points to public for a circuit that already has public pairing points.");
+    has_public_pairing_points_ = true;
+}
 
 template class CircuitBuilderBase<bb::fr>;
 template class CircuitBuilderBase<grumpkin::fr>;
--- barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/pairing_points_tagging.hpp
+++ barretenberg/cpp/src/barretenberg/stdlib_circuit_builders/pairing_points_tagging.hpp
@@ -1,6 +1,4 @@
 #pragma once
-#include "barretenberg/common/assert.hpp"
-#include <algorithm>
 #include <cstdint>
 #include <vector>
 
@@ -20,27 +18,20 @@ class PairingPointsTagging {
     bool has_public_pairing_points_ = false;
 
   public:
-    PairingPointsTagging() = default;
-    PairingPointsTagging(const PairingPointsTagging& other) = default;
-    PairingPointsTagging(PairingPointsTagging&& other) noexcept = default;
-    PairingPointsTagging& operator=(const PairingPointsTagging& other) = default;
-    PairingPointsTagging& operator=(PairingPointsTagging&& other) noexcept = default;
-    ~PairingPointsTagging() = default;
+    PairingPointsTagging();
+    PairingPointsTagging(const PairingPointsTagging& other);
+    PairingPointsTagging(PairingPointsTagging&& other) noexcept;
+    PairingPointsTagging& operator=(const PairingPointsTagging& other);
+    PairingPointsTagging& operator=(PairingPointsTagging&& other) noexcept;
+    ~PairingPointsTagging();
 
-    bool operator==(const PairingPointsTagging& other) const = default;
+    bool operator==(const PairingPointsTagging& other) const;
 
     /**
      * @brief Create a new unique pairing point tag
      * @return The new tag value
      */
-    uint32_t create_pairing_point_tag()
-    {
-        has_pairing_points_ = true;
-        uint32_t new_tag = next_pairing_point_tag_++;
-        pairing_points_tags_.emplace_back(
-            new_tag); // Each PairingPoints starts with tag equal to the number of PairingPoints created before it
-        return new_tag;
-    }
+    uint32_t create_pairing_point_tag();
 
     /**
      * @brief Merge two pairing point tags
@@ -49,53 +40,19 @@ class PairingPointsTagging {
      * @details If the tags are different, all instances of tag2 are replaced with tag1. We also check that the pairing
      * points have not been set to public yet.
      */
-    void merge_pairing_point_tags(uint32_t tag1_index, uint32_t tag2_index)
-    {
-        BB_ASSERT(!has_public_pairing_points_,
-                  "Cannot merge pairing point tags after pairing points have been set to public.");
-
-        // If different tags, override tag2 with tag1
-        uint32_t tag1 = pairing_points_tags_[tag1_index];
-        uint32_t tag2 = pairing_points_tags_[tag2_index];
-
-        if (tag1 != tag2) {
-            for (auto& tag : pairing_points_tags_) {
-                tag = tag == tag2 ? tag1 : tag;
-            }
-        }
-    }
+    void merge_pairing_point_tags(uint32_t tag1_index, uint32_t tag2_index);
 
     /**
      * @brief Check if all pairing point tags belong to a single equivalence class
      * @return true if there's only one equivalence class (or no tags at all)
      */
-    bool has_single_pairing_point_tag() const
-    {
-        if (!has_pairing_points_) {
-            return true; // No pairing points created
-        }
-        // Check that there is only one tag
-        uint32_t unique_tag = pairing_points_tags_[0];
-        return std::ranges::all_of(pairing_points_tags_, [unique_tag](auto const& tag) { return tag == unique_tag; });
-    }
+    bool has_single_pairing_point_tag() const;
 
     /**
      * @brief Return the number of unique pairing point tags
      * @return The count of unique tags
      */
-    uint32_t num_unique_pairing_points() const
-    {
-        std::vector<uint32_t> unique_tags;
-        unique_tags.resize(pairing_points_tags_.size());
-        for (auto const& tag : pairing_points_tags_) {
-            unique_tags[tag] = 1;
-        }
-        uint32_t sum = 0;
-        for (auto v : unique_tags) {
-            sum += v;
-        }
-        return sum;
-    }
+    uint32_t num_unique_pairing_points() const;
 
     /**
      * @brief Check if any pairing points have been created
@@ -112,17 +69,12 @@ class PairingPointsTagging {
     /**
      * @brief Get the tag for a specific pairing point index
      */
-    uint32_t get_tag(uint32_t tag_index) const { return pairing_points_tags_.at(tag_index); }
+    uint32_t get_tag(uint32_t tag_index) const;
 
     /**
      * @brief Record that pairing points have been set to public
      */
-    void set_public_pairing_points()
-    {
-        BB_ASSERT(!has_public_pairing_points_,
-                  "Trying to set pairing points to public for a circuit that already has public pairing points.");
-        has_public_pairing_points_ = true;
-    }
+    void set_public_pairing_points();
 };
 
 } // namespace bb
