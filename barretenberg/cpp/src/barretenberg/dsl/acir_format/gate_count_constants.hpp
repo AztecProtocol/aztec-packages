591fd78f6403fd873608caee1bd4eeb0df15c09e
AztecBot <tech@aztec-labs.com>
2026-05-27 02:53:09 +0000

    fix(bb): update MegaZK recursion gate count

--- barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_format/gate_count_constants.hpp
@@ -102,7 +102,7 @@ constexpr std::tuple<size_t, size_t> HONK_RECURSION_CONSTANTS(
         if (mode != PredicateTestCase::ConstantTrue) {
             bb::assert_failure("Unhandled mode in MegaZKRecursiveFlavor.");
         }
-        return std::make_tuple(648863, 0);
+        return std::make_tuple(674175, 0);
     } else {
         bb::assert_failure("Unhandled recursive flavor.");
     }
