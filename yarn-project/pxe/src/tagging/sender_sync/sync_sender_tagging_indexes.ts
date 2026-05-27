0d1e1c5d4467fa494d81aca7d9c71e713b755090
Maxim Vezenov <mvezenov@gmail.com>
2026-05-27 15:47:53 -0400

    comment

--- yarn-project/pxe/src/tagging/sender_sync/sync_sender_tagging_indexes.ts
+++ yarn-project/pxe/src/tagging/sender_sync/sync_sender_tagging_indexes.ts
@@ -14,9 +14,7 @@ import { loadAndStoreNewTaggingIndexes } from './utils/load_and_store_new_taggin
 /**
  * Syncs tagging indexes. This function needs to be called whenever a private log is being sent.
  *
- * @param secret - The sender-side tagging key. May be an `ExtendedDirectionalAppTaggingSecret` (unconstrained
- * delivery, unique for a `(sender, recipient, app)` tuple) or a `ConstrainedAppTaggingSecret` (constrained delivery,
- * keyed on the app-siloed shared secret).
+ * @param secret - The sender-side tagging `AppTaggingSecret`.
  * @remarks When syncing the indexes as sender we don't care about the log contents - we only care about the highest
  * pending and highest finalized indexes as that guides the next index choice when sending a log. The next index choice
  * is simply the highest pending index plus one (or finalized if pending is undefined).
