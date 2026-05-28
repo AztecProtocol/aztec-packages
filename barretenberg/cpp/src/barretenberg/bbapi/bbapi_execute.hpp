9731b60a85a17230b482fcdab092e04aac4498ab
AztecBot <tech@aztec-labs.com>
2026-05-28 16:47:29 +0000

    bench: add eccvm browserstack bench harness

--- barretenberg/cpp/src/barretenberg/bbapi/bbapi_execute.hpp
+++ barretenberg/cpp/src/barretenberg/bbapi/bbapi_execute.hpp
@@ -4,6 +4,7 @@
 #include "barretenberg/bbapi/bbapi_chonk.hpp"
 #include "barretenberg/bbapi/bbapi_crypto.hpp"
 #include "barretenberg/bbapi/bbapi_ecc.hpp"
+#include "barretenberg/bbapi/bbapi_eccvm_bench.hpp"
 #include "barretenberg/bbapi/bbapi_ecdsa.hpp"
 #include "barretenberg/bbapi/bbapi_schnorr.hpp"
 #include "barretenberg/bbapi/bbapi_shared.hpp"
@@ -45,6 +46,7 @@ using Command = NamedUnion<AvmProve,
                            Blake2sToField,
                            AesEncrypt,
                            AesDecrypt,
+                           EccvmBench,
                            GrumpkinMul,
                            GrumpkinAdd,
                            GrumpkinBatchMul,
@@ -109,6 +111,7 @@ using CommandResponse = NamedUnion<ErrorResponse,
                                    Blake2sToField::Response,
                                    AesEncrypt::Response,
                                    AesDecrypt::Response,
+                                   EccvmBench::Response,
                                    GrumpkinMul::Response,
                                    GrumpkinAdd::Response,
                                    GrumpkinBatchMul::Response,
