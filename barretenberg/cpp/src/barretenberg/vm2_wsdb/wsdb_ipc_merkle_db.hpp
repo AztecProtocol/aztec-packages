031e54b05e35d27bea0a7fd5d823c49c163f6208
Charlie <5764343+charlielye@users.noreply.github.com>
2026-06-11 17:15:12 +0000

    refactor(wsdb): migrate to generated ipc package

--- barretenberg/cpp/src/barretenberg/vm2_wsdb/wsdb_ipc_merkle_db.hpp
+++ barretenberg/cpp/src/barretenberg/vm2_wsdb/wsdb_ipc_merkle_db.hpp
@@ -9,14 +9,12 @@
 
 #include "barretenberg/vm2/simulation/interfaces/db.hpp"
 #include "barretenberg/world_state/types.hpp"
-#include "barretenberg/wsdb/wsdb_commands.hpp"
-#include "barretenberg/wsdb/wsdb_execute.hpp"
-#include "barretenberg/wsdb/wsdb_ipc_client_generated.hpp"
+#include "barretenberg/wsdb/wsdb_ipc_client.hpp"
 
 #include <optional>
 #include <stack>
 
-namespace bb::wsdb_client {
+namespace bb::avm2::simulation {
 
 class WsdbIpcMerkleDB final : public avm2::simulation::LowLevelMerkleDBInterface {
   public:
@@ -56,9 +54,6 @@ class WsdbIpcMerkleDB final : public avm2::simulation::LowLevelMerkleDBInterface
     uint32_t get_checkpoint_id() const override;
 
   private:
-    template <typename T> static std::vector<uint8_t> serialize_to_msgpack(const T& value);
-    template <typename T> static T deserialize_from_msgpack(const std::vector<uint8_t>& bytes);
-
     /** Invalidate the cached tree roots (call after any write operation). */
     void invalidate_tree_roots_cache();
 
@@ -69,4 +64,4 @@ class WsdbIpcMerkleDB final : public avm2::simulation::LowLevelMerkleDBInterface
     mutable std::optional<avm2::TreeSnapshots> cached_tree_roots_;
 };
 
-} // namespace bb::wsdb_client
+} // namespace bb::avm2::simulation
