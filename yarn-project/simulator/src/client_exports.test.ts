be8a1a26c845d0399a2a3cebef9a97407d9ce4b5
Maxim Vezenov <mvezenov@gmail.com>
2026-06-16 10:15:41 -0400

    fix: drop simulator client export guard test

--- yarn-project/simulator/src/client_exports.test.ts
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'node:fs';
-
-describe('client entrypoint', () => {
-  it('does not export circuit recording modules', () => {
-    const clientEntrypoint = readFileSync(new URL('./client.ts', import.meta.url), 'utf8');
-
-    expect(clientEntrypoint).not.toContain('circuit_recording');
-  });
-});
