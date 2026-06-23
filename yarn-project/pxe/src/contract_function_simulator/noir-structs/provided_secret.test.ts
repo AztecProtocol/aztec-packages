225a2af4b78479e5421d90b16845a134dec02bf1
Maxim Vezenov <mvezenov@gmail.com>
2026-06-23 14:01:14 -0400

    reduce diff

--- yarn-project/pxe/src/contract_function_simulator/noir-structs/provided_secret.test.ts
+++ yarn-project/pxe/src/contract_function_simulator/noir-structs/provided_secret.test.ts
@@ -4,19 +4,21 @@ import { AppTaggingSecretKind } from '@aztec/stdlib/logs';
 import { ProvidedSecret } from './provided_secret.js';
 
 describe('ProvidedSecret', () => {
-  it('deserializes mode 2 as unconstrained', () => {
+  it('deserializes delivery mode 2 as unconstrained', () => {
     const provided = ProvidedSecret.fromFields([new Fr(42), new Fr(2)]);
     expect(provided.secret).toEqual(new Fr(42));
-    expect(provided.mode).toBe(AppTaggingSecretKind.UNCONSTRAINED);
+    expect(provided.mode).toEqual(AppTaggingSecretKind.UNCONSTRAINED);
   });
 
-  it('deserializes mode 3 as constrained', () => {
+  it('deserializes delivery mode 3 as constrained', () => {
     const provided = ProvidedSecret.fromFields([new Fr(42), new Fr(3)]);
     expect(provided.secret).toEqual(new Fr(42));
-    expect(provided.mode).toBe(AppTaggingSecretKind.CONSTRAINED);
+    expect(provided.mode).toEqual(AppTaggingSecretKind.CONSTRAINED);
   });
 
-  it('rejects invalid modes', () => {
-    expect(() => ProvidedSecret.fromFields([new Fr(42), new Fr(1)])).toThrow('Unrecognized delivery mode for tagging');
+  it('rejects an invalid mode value', () => {
+    expect(() => ProvidedSecret.fromFields([Fr.random(), new Fr(99)])).toThrow(
+      'Unrecognized delivery mode for tagging',
+    );
   });
 });
