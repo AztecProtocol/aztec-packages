cc6adabb37c98cf4ac819789ac971ca90ea8d99d
mverzilli <martin@aztec-labs.com>
2026-06-24 15:27:05 +0000

    chore: bump oracle major version for fact origin-block state

--- yarn-project/pxe/src/oracle_version.ts
+++ yarn-project/pxe/src/oracle_version.ts
@@ -10,8 +10,8 @@
 /// used to provide helpful error messages if a contract calls an oracle that doesn't exist. We don't throw immediately
 /// if AZTEC_NR_MINOR > PXE_MINOR because if a contract is updated to use a newer Aztec.nr dependency without actually
 /// using any of the new oracles then there is no reason to throw.
-export const ORACLE_VERSION_MAJOR = 30;
-export const ORACLE_VERSION_MINOR = 1;
+export const ORACLE_VERSION_MAJOR = 31;
+export const ORACLE_VERSION_MINOR = 0;
 
 /// This hash is computed from the `ORACLE_REGISTRY` declaration (each oracle's name, ordered parameter names and
 /// types, and return type) and is used to detect when the oracle interface changes. When it does, you need to either:
