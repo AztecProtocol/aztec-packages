3772e18a71c3b69d98b239015db87cdbfb197f8f
Santiago Palladino <santiago@aztec-labs.com>
2026-07-06 16:09:00 -0300

    docs(operate): add v5 operator changelog for validator signing hardening

--- docs/sidebars-operate.js
+++ docs/sidebars-operate.js
@@ -145,6 +145,7 @@ const sidebars = {
         id: "operators/reference/changelog/changelog",
       },
       items: [
+        "operators/reference/changelog/v5",
         "operators/reference/changelog/v4.3",
         "operators/reference/changelog/v4.2",
         "operators/reference/changelog/v4",
