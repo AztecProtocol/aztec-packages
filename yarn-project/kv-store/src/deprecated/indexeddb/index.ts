862bd423253d143ef19c45c3f49889624cdc04a0
Martin Verzilli <martin@aztec-labs.com>
2026-07-10 18:43:59 +0200

    feat: preserve stores on schema version or rollup address change (#24631)
    Changes store management so that changes in rollup addresses and schema
    versions cause different stores to be used. Previously we only supported
    one store to be present at a time, which meant any version or rollup
    change wiped pre-existing stores (note this includes network changes).
    
    An underlying design decision in this PR is to strip the kv-store
    package from responsibility over location on disk, knowledge about
    rollup addresses, etc, at least as as regards PXE and wallet storage.
    Other users of LMDB-v2 should not be impacted by this change.
    
    In consonance, IndexedDB and SQLite backends drop their `createStore`
    functions, which shoehorned wallet and PXE store creation to an
    homogeneous interface that made it hard to let them independently
    evolve.
    
    Since we're changing this, I decided to also include the chain id as a
    component of the store id, in addition to the already present schema
    version and rollup address. It's not clear that we'll ever work on a
    testnet or a different L1, but doing so is trivial and removes the need
    to deal with this in the future.
    
    Closes F-809
    
    (cherry picked from commit 0bb062854965b50db9e0075d09ddb1dd807a40d4)

--- yarn-project/kv-store/src/deprecated/indexeddb/index.ts
+++ yarn-project/kv-store/src/deprecated/indexeddb/index.ts
@@ -1,33 +1,8 @@
-import { type Logger, createLogger } from '@aztec/foundation/log';
-import type { DataStoreConfig } from '@aztec/stdlib/kv-store';
+import { createLogger } from '@aztec/foundation/log';
 
-import { initStoreForRollupAndSchemaVersion } from '../../utils.js';
 import { AztecIndexedDBStore } from './store.js';
 
 export { AztecIndexedDBStore } from './store.js';
-
-/**
- * @deprecated The IndexedDB backend is being retired. Use `@aztec/kv-store/sqlite-opfs` instead.
- */
-export async function createStore(
-  name: string,
-  config: DataStoreConfig,
-  schemaVersion: number | undefined = undefined,
-  log: Logger = createLogger('kv-store'),
-) {
-  let { dataDirectory } = config;
-  if (typeof dataDirectory !== 'undefined') {
-    dataDirectory = `${dataDirectory}/${name}`;
-  }
-
-  log.info(
-    dataDirectory
-      ? `Creating ${name} data store at directory ${dataDirectory} with map size ${config.dataStoreMapSizeKb} KB`
-      : `Creating ${name} ephemeral data store with map size ${config.dataStoreMapSizeKb} KB`,
-  );
-  const store = await AztecIndexedDBStore.open(createLogger('kv-store:indexeddb'), dataDirectory ?? '', false);
-  return initStoreForRollupAndSchemaVersion(store, schemaVersion, config.rollupAddress, log);
-}
 
 /**
  * @deprecated The IndexedDB backend is being retired. Use `@aztec/kv-store/sqlite-opfs` instead.
