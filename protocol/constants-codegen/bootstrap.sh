22f46552704fa58102307275a6edfd01861e7648
Martin Verzilli <martin@aztec-labs.com>
2026-07-21 14:33:36 +0200

    chore: constants codegen release (#24728)
    Hooks the new package to release workflows (blocked until we decide
    where in npm that should live and make sure the corresponding org/scope
    is created)
    
    Closes F-821

--- protocol/constants-codegen/bootstrap.sh
+++ protocol/constants-codegen/bootstrap.sh
@@ -15,11 +15,17 @@ function build {
 
 function test_cmds {
   echo "$hash cd protocol/constants-codegen && node --test src/*.test.ts"
+  echo "$hash cd protocol/constants-codegen && ./scripts/test-package.sh"
 }
 
 function test {
   echo_header "constants-codegen test"
   test_cmds | filter_test_cmds | parallelize
+}
+
+function release {
+  npm_install_deps
+  retry "deploy_npm ${REF_NAME#v}"
 }
 
 case "$cmd" in
