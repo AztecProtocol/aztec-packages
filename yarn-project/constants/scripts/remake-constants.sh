0d5dc15d047322bd780a839208663b71db643efb
mverzilli <martin@aztec-labs.com>
2026-07-23 16:30:32 +0000

    make constants-codegen a portal dep of yarn-project

--- yarn-project/constants/scripts/remake-constants.sh
+++ yarn-project/constants/scripts/remake-constants.sh
@@ -3,10 +3,9 @@
 set -euo pipefail
 
 repo_root=$(git rev-parse --show-toplevel)
-codegen_dir="$repo_root/protocol/constants-codegen"
 additional_input="$repo_root/noir-projects/noir-protocol-circuits/crates/types/src/blob_data/tx_blob_data.nr:MAX_TX_BLOB_DATA_SIZE_IN_FIELDS"
 typescript_output="$repo_root/yarn-project/constants/src/constants.gen.ts"
 
-node "$codegen_dir/src/cli.ts" \
+"$repo_root/yarn-project/node_modules/.bin/constants-codegen" \
   --include "$additional_input" \
   --typescript "$typescript_output"
