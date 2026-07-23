9e010eab1832ca50c4a9a8b4dc9eac9e67d1ce93
AztecBot <tech@aztec-labs.com>
2026-07-23 16:03:19 +0000

    fix(ci): extend AVM check-circuit timeout

--- yarn-project/end-to-end/bootstrap.sh
+++ yarn-project/end-to-end/bootstrap.sh
@@ -254,10 +254,9 @@ function avm_check_circuit_cmds {
   local dump_dir_from_top="yarn-project/end-to-end/$default_avm_inputs_dump_dir"
 
   # Specify timeout and resources
-  # WARNING: theoretically, transactions could need more CPU and MEM than we allocate by default.
-  # In that case, they might start timing out. For now, all of the e2e test txs seem to be relatively
-  # small and the AVM can run check-circuit with limited resources.
-  local prefix="$hash:ISOLATE=1:TIMEOUT=30s"
+  # WARNING: transactions with larger public traces can need more CPU and MEM than we allocate by default.
+  local timeout="${AVM_CHECK_CIRCUIT_TIMEOUT:-60s}"
+  local prefix="$hash:ISOLATE=1:TIMEOUT=$timeout"
 
   # Find all .bin files in the dump directory (handles nested dirs, e.g. the 3-segment
   # single-node/block-building/block_building dump path needs the 4-level glob).
