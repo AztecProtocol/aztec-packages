5ab469bb6cce70d4aa2db17172d021ac67ed1d0c
AztecBot <tech@aztec-labs.com>
2026-07-23 06:19:33 +0000

    fix(bb): avoid invalid acir component debug test corruption

--- barretenberg/cpp/src/barretenberg/acir_components_check/components_check.test.cpp
+++ barretenberg/cpp/src/barretenberg/acir_components_check/components_check.test.cpp
@@ -544,13 +544,24 @@ TEST_F(AcirComponentsCheckTest, DetectsSplitComponents)
 
 TEST_F(AcirComponentsCheckTest, DetectsUnconstrainedWitnesses)
 {
-    Acir::Circuit circuit = make_circuit({
+    Acir::Circuit acir_circuit = make_circuit({
         Acir::Opcode{ .value = Acir::Opcode::AssertZero{
                           .value = Acir::Expression{
                               .linear_combinations =
                                   {
                                       { bb::fr::one().to_buffer(), make_witness(8) },
-                                      { bb::fr(-1).to_buffer(), make_witness(9) },
+                                      { bb::fr(-1).to_buffer(), make_witness(1000) },
+                                  },
+                              .q_c = bb::fr::zero().to_buffer(),
+                          } } },
+    });
+    Acir::Circuit circuit = make_circuit({
+        Acir::Opcode{ .value = Acir::Opcode::AssertZero{
+                          .value = Acir::Expression{
+                              .linear_combinations =
+                                  {
+                                      { bb::fr::one().to_buffer(), make_witness(7) },
+                                      { bb::fr(-1).to_buffer(), make_witness(8) },
                                   },
                               .q_c = bb::fr::zero().to_buffer(),
                           } } },
@@ -559,10 +570,8 @@ TEST_F(AcirComponentsCheckTest, DetectsUnconstrainedWitnesses)
     auto constraints = circuit_serde_to_acir_format(circuit, IsMegaBuilder<AcirComponentsCheckBuilder>);
     AcirProgram program{ .constraints = constraints, .witness = {} };
     auto builder = create_circuit<AcirComponentsCheckBuilder>(program);
-    // Corrupt the circuit
-    builder.real_variable_index.resize(9);
 
-    acir_components_check::ComponentsChecker checker(circuit, builder);
+    acir_components_check::ComponentsChecker checker(acir_circuit, builder);
     auto errors = checker.check();
     expect_single_error_type(errors, acir_components_check::Error::Type::UNCONSTRAINED);
 }
