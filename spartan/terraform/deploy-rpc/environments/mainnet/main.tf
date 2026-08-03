1bab3d6af795bb83112de51d5acafd93297d0769
Amin Sammara <aminsammara1@gmail.com>
2026-08-03 10:19:07 -0400

    chore: add new client

--- spartan/terraform/deploy-rpc/environments/mainnet/main.tf
+++ spartan/terraform/deploy-rpc/environments/mainnet/main.tf
@@ -84,7 +84,8 @@ locals {
     "mainnet-rpc-consumer-client9",
     "mainnet-rpc-consumer-client10",
     "mainnet-rpc-consumer-client11",
-    "mainnet-rpc-consumer-client12"
+    "mainnet-rpc-consumer-client12",
+    "mainnet-rpc-consumer-client13"
   ]
 
   consumers = {
