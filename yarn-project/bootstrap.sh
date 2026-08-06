a57fe7d9d86ca2ece79cbdd7a52515d435cd4046
Nico Chamo <nicolas@chamo.com.ar>
2026-08-06 15:51:38 -0300

    chore: consume @aztec/cdb instead of generating it locally

--- yarn-project/bootstrap.sh
+++ yarn-project/bootstrap.sh
@@ -4,13 +4,10 @@ source $(git rev-parse --show-toplevel)/ci3/source_bootstrap
 function hash {
   # Nothing under noir-projects/fnd is an input: everything it produces reaches us as a pinned
   # package, hashed via yarn.lock (covered by the yarn-project patterns).
-  # The ipc-codegen/cdb patterns cover the simulator's cdb server codegen, which
-  # runs ipc-codegen against barretenberg's cdb_schema.json on every build.
   hash_str \
     $(../labs-aztec-toolchain/bootstrap.sh hash) \
     $(../noir-projects/labs/noir-contracts/bootstrap.sh hash) \
     $(../noir-projects/labs/aztec-nr/bootstrap.sh hash) \
-    $(cache_content_hash "^ipc-codegen/" "^barretenberg/cpp/src/barretenberg/cdb/") \
     $(cache_content_hash ../yarn-project/.rebuild_patterns)
 }
 
@@ -146,7 +143,6 @@ function compile_all {
     noir-protocol-circuits-types \
     protocol-contracts \
     pxe \
-    simulator \
     standard-contracts
   cat joblog.txt
 
--- yarn-project/simulator/eslint.config.js
+++ yarn-project/simulator/eslint.config.js
@@ -1,9 +1,6 @@
 import config from '@aztec/foundation/eslint';
 
-import { globalIgnores } from 'eslint/config';
-
 export default [
-  globalIgnores(['src/public/cdb/generated/**']),
   ...config,
   {
     files: ['src/public/avm/testing/account_proof_fetcher.ts'],
