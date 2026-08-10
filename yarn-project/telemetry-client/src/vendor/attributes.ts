36c1dc47a31b3210d0f48f93c19bffed9b3b5606
Alex Gherghisan <alexghr@users.noreply.github.com>
2026-08-10 15:42:48 +0000

    feat: track json-rpc metrics

--- yarn-project/telemetry-client/src/vendor/attributes.ts
+++ yarn-project/telemetry-client/src/vendor/attributes.ts
@@ -1,5 +1,6 @@
 // See https://opentelemetry.io/docs/specs/semconv/rpc/json-rpc/
 export const ATTR_JSONRPC_METHOD = 'rpc.method';
+export const ATTR_JSONRPC_SERVICE = 'rpc.service';
 export const ATTR_JSONRPC_REQUEST_ID = 'rpc.jsonrpc.request_id';
 export const ATTR_JSONRPC_ERROR_CODE = 'rpc.jsonrpc.error_code';
 export const ATTR_JSONRPC_ERROR_MSG = 'rpc.jsonrpc.error_message';
