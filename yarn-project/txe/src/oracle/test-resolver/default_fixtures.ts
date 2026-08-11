bbc4459da8a1372f5a40a66e6c5e8c2bbdfcfec5
Nico Chamo <nicolas@chamo.com.ar>
2026-08-11 16:09:02 -0300

    refactor(txe): return BoundedVecs directly from oracles

--- yarn-project/txe/src/oracle/test-resolver/default_fixtures.ts
+++ yarn-project/txe/src/oracle/test-resolver/default_fixtures.ts
@@ -33,10 +33,11 @@ import {
   isStructMapping,
   tryFieldWidth,
 } from '@aztec/pxe/simulator';
-import { FunctionSelector, NoteSelector } from '@aztec/stdlib/abi';
+import { EventSelector, FunctionSelector, NoteSelector } from '@aztec/stdlib/abi';
 import { BlockHash } from '@aztec/stdlib/block';
 import { AppTaggingSecretKind } from '@aztec/stdlib/logs';
 
+import { EVENT_SELECTOR } from '../txe_oracle_registry.js';
 import type { OracleTestScenario } from './resolver.js';
 
 /**
@@ -76,6 +77,7 @@ const SCALAR_IMPLS: ScalarImpl[] = [
   scalar(AZTEC_ADDRESS, seed => AztecAddress.fromNumberUnsafe(seed)),
   scalar(ETH_ADDRESS, seed => EthAddress.fromField(new Fr(seed))),
   scalar(FUNCTION_SELECTOR, seed => FunctionSelector.fromField(new Fr(seed))),
+  scalar(EVENT_SELECTOR, seed => EventSelector.fromField(new Fr(seed))),
   scalar(NOTE_SELECTOR, seed => NoteSelector.fromField(new Fr(seed))),
   scalar(BLOCK_HASH, seed => new BlockHash(new Fr(seed))),
   scalar(SLOT_NUMBER, seed => SlotNumber(seed)),
